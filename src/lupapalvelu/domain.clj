e6173001b2d8c34ccf16c0da78e16168390e5fdc
Tommi Reiman <tommi.reiman@metosin.fi>
2012-08-30 22:02:58 +0300

    fix comment

--- src/lupapalvelu/domain.clj
+++ src/lupapalvelu/domain.clj
@@ -1,6 +1,7 @@
 (ns lupapalvelu.domain)
 
-(defn get-party-role-in-application [party application]
+(defn get-party-role-in-application
   "Gets the party role in application. In case there are many roles for the party, the first one is returned."
+  [party application]
   (if-let [partyId (:id party)]
     (:role (first (filter #(= (:partyId %) partyId) (:roles application))))))
