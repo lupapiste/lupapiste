ca85db283752997c0ba1107ece1488a66102a0fc
Mikko Heikkilä <mikko.heikkila@metosin.fi>
2012-08-31 17:07:11 +0300

    Make robot test browser configurable

--- robot/remote-test.sh
+++ robot/remote-test.sh
@@ -6,4 +6,4 @@ if [ -z $target ] ; then
 	target="tests"
 fi
 
-pybot -d target --variable SELENIUM:http://192.168.7.172:4444/wd/hub --variable SERVER:lupatest.solita.fi $target
+pybot -d target --variable BROWSER:firefox --variable SELENIUM:http://192.168.7.172:4444/wd/hub --variable SERVER:lupatest.solita.fi $target
