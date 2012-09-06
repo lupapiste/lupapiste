4be4d7e3a424c059a868ebfc86151c811fd366cd
jarppe <jarppe@metosin.fi>
2012-09-06 08:29:59 +0300

    some ftw.

--- src/lupapalvelu/domain.clj
+++ src/lupapalvelu/domain.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.domain)
 
-(defn role-in-application [user-id application]
-  (some (fn [[role {id :userId}]] (if (= id user-id) role))
-        (:roles application)))
+(defn role-in-application [user-id {roles :roles}]
+  (some (fn [[role {id :userId}]]
+          (if (= id user-id) role))
+        roles))
