fb3dabf609248c37b94b548eb4658768c9ef9eaa
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-09-10 17:20:26 +0300

    Added scripts for running tests with local IE and Chrome (Windows)

---
+++ robot/local-ie.bat
@@ -0,0 +1,7 @@
+@rem NOTE: requires Selenium > 2.25 version of selenium\webdriver\ie\service.py
+@rem (see http://code.google.com/p/selenium/issues/detail?id=4310).
+@rem Download: http://selenium.googlecode.com/svn-history/r17705/trunk/py/selenium/webdriver/ie/service.py
+@set target=%1
+@if [%target%]==[] set target=tests
+
+pybot -d target --variable BROWSER:internetexplorer %target%
