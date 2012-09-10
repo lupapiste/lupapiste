2312c3f852143af1dd8a997047b156a008a2bf90
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-09-10 13:33:36 +0300

    Added remote-test.bat

---
+++ robot/remote-test.bat
@@ -0,0 +1,4 @@
+@set target=%1
+@if [%target%]==[] set target=tests
+
+pybot -d target --variable BROWSER:firefox --variable SELENIUM:http://192.168.7.172:4444/wd/hub --variable SERVER:lupatest.solita.fi %target%
