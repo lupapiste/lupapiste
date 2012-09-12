8bf66aca2935bc80c96ab1e0e68297b5b4761222
jarppe <jarppe@metosin.fi>
2012-09-12 14:06:56 +0300

    Improved tests.

--- test/lupapalvelu/mongo_test.clj
+++ test/lupapalvelu/mongo_test.clj
@@ -17,23 +17,23 @@
 
 (facts "Facts about with-objectid"
   (against-background
-    (string-to-objectid "foo") => "FOO")
+    (string-to-objectid "foo") => ...id...)
   (fact (with-objectid nil) => nil)
   (fact (with-objectid {:data "data"}) => {:data "data"})
-  (fact (with-objectid {:id "foo" :data "data"}) => {:_id "FOO" :data "data"}))
+  (fact (with-objectid {:id "foo" :data "data"}) => {:_id ...id... :data "data"}))
 
 (facts "Facts about with-id"
   (against-background
-    (objectid-to-string "foo") => "FOO")
+    (objectid-to-string "foo") => ...id...)
   (fact (with-id nil) => nil)
   (fact (with-id {:data "data"}) => {:data "data"})
-  (fact (with-id {:_id "foo" :data "data"}) => {:id "FOO" :data "data"}))
+  (fact (with-id {:_id "foo" :data "data"}) => {:id ...id... :data "data"}))
 
 (facts "Facts about insert"
-  (fact (insert "c" {:id "foo" :data "data"}) => nil
+  (fact (insert "c" {:id "foo" :data "data"}) => ...result...
         (provided
-          (string-to-objectid "foo") => "FOO"
-          (mc/insert "c" {:_id "FOO" :data "data"}) => nil))
-  (fact (insert "c" {:data "data"}) => nil
+          (string-to-objectid "foo") => ...id...
+          (mc/insert-and-return "c" {:_id ...id... :data "data"}) => ...result...))
+  (fact (insert "c" {:data "data"}) => ..result..
         (provided
-          (mc/insert "c" {:data "data"}) => nil)))
+          (mc/insert-and-return "c" {:data "data"}) => ..result..)))
