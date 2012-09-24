2edac53487c3ffa2c494ac199a3a06d44fa69aae
Timo Lehtonen <timo.lehtonen@solita.fi>
2012-09-24 14:25:14 +0300

    Changed application create wizard layout

--- project.clj
+++ project.clj
@@ -11,5 +11,5 @@
   :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
                                   [clj-http "0.5.3"]]
                    :plugins [[lein-midje "2.0.0-SNAPSHOT"]
-                             [lein-buildid "0.1.0-SNAPSHOT"]]}}
+                              [lein-buildid "0.1.0-SNAPSHOT"]]}}
   :min-lein-version "2.0.0")
