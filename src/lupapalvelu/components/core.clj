2b0b6edc08da7e46e3bd59217b0b5195c865cf40
jarppe <jarppe@metosin.fi>
2012-09-26 23:06:24 +0300

    Renamed stuff for clarity.

--- src/lupapalvelu/components/core.clj
+++ src/lupapalvelu/components/core.clj
@@ -3,14 +3,17 @@
 (defn get-component [components c]
   (or (components c) (throw (Exception. (format "Unknown component: %s" (name c))))))
 
+(defn get-component-deps [components c]
+  (or (:depends (get-component components c)) []))
+
 (defn exclude [found depends]
   (filter (comp not (set found)) depends))
 
-(defn get-dependencies [components found component]
-  (conj (reduce (partial get-dependencies components) found (exclude found (or (:depends (get-component components component)) []))) component))
+(defn get-dependencies [components found c]
+  (conj (reduce (partial get-dependencies components) found (exclude found (get-component-deps components c))) c))
 
-(defn component-resources [components kind component]
-  (map (partial str (name component) \/) (kind (get-component components component))))
+(defn component-resources [components kind c]
+  (map (partial str (name c) \/) (kind (get-component components c))))
 
-(defn get-resources [components kind component]
-  (mapcat (partial component-resources components kind) (get-dependencies components [] component)))
+(defn get-resources [components kind c]
+  (mapcat (partial component-resources components kind) (get-dependencies components [] c)))
--- test/lupapalvelu/components/core_test.clj
+++ test/lupapalvelu/components/core_test.clj
@@ -2,38 +2,38 @@
   (:use [lupapalvelu.components.core]
         [midje.sweet]))
 
-(def ui-components
+(def test-components
   {:root  {:js ["root.js"]}
-   :c1    {:js ["c1.js"]
-           :depends [:root]}
-   :c2    {:js ["c2.js"]
-           :depends [:root]}
-   :main  {:js ["main.js" "other.js"]
-           :depends [:c1 :c2]}})
+   :c1    {:depends [:root]
+           :js ["c1.js"]}
+   :c2    {:depends [:root]
+           :js ["c2.js"]}
+   :main  {:depends [:c1 :c2]
+           :js ["main.js" "other.js"]}})
 
 (facts
-  (fact (get-component ui-components :root) => {:js ["root.js"]})
-  (fact (get-component ui-components :foo) => (throws Exception)))
+  (fact (get-component test-components :root) => {:js ["root.js"]})
+  (fact (get-component test-components :foo)  => (throws Exception)))
 
 (facts
-  (fact (exclude [] [] => []))
-  (fact (exclude [:a :b :c] []) => [])
-  (fact (exclude [] [:a :b :c]) => [:a :b :c])
-  (fact (exclude [:a :b :c] [:a :c :e]) => [:e]))
+  (fact (exclude [] []                   => []))
+  (fact (exclude [:a :b :c] [])          => [])
+  (fact (exclude [] [:a :b :c])          => [:a :b :c])
+  (fact (exclude [:a :b :c] [:a :c :e])  => [:e]))
 
 (facts
-  (fact (get-dependencies ui-components [] :root) => [:root])
-  (fact (get-dependencies ui-components [] :c1) => [:root :c1])
-  (fact (get-dependencies ui-components [] :c2) => [:root :c2])
-  (fact (get-dependencies ui-components [] :main) => [:root :c1 :c2 :main]))
+  (fact (get-dependencies test-components [] :root) => [:root])
+  (fact (get-dependencies test-components [] :c1)   => [:root :c1])
+  (fact (get-dependencies test-components [] :c2)   => [:root :c2])
+  (fact (get-dependencies test-components [] :main) => [:root :c1 :c2 :main]))
 
 (facts
-  (fact (component-resources ui-components :js :root) => ["root/root.js"])
-  (fact (component-resources ui-components :js :main) => ["main/main.js" "main/other.js"])
-  (fact (component-resources ui-components :foo :main) => []))
+  (fact (component-resources test-components :js :root)  => ["root/root.js"])
+  (fact (component-resources test-components :js :main)  => ["main/main.js" "main/other.js"])
+  (fact (component-resources test-components :foo :main) => []))
 
 (facts
-  (fact (get-resources ui-components :js :root)  =>  ["root/root.js"])
-  (fact (get-resources ui-components :js :c1)    =>  ["root/root.js" "c1/c1.js"])
-  (fact (get-resources ui-components :js :c2)    =>  ["root/root.js" "c2/c2.js"])
-  (fact (get-resources ui-components :js :main)  =>  ["root/root.js" "c1/c1.js" "c2/c2.js" "main/main.js" "main/other.js"]))
+  (fact (get-resources test-components :js :root)  =>  ["root/root.js"])
+  (fact (get-resources test-components :js :c1)    =>  ["root/root.js" "c1/c1.js"])
+  (fact (get-resources test-components :js :c2)    =>  ["root/root.js" "c2/c2.js"])
+  (fact (get-resources test-components :js :main)  =>  ["root/root.js" "c1/c1.js" "c2/c2.js" "main/main.js" "main/other.js"]))
