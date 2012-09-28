8b31a5dccfcc6c81477e82aa9bcecb118897af96
Tommi Reiman <tommi.reiman@metosin.fi>
2012-09-28 09:29:44 +0300

    attach user-details via security/details

--- src/lupapalvelu/security.clj
+++ src/lupapalvelu/security.clj
@@ -11,6 +11,14 @@
 (defn check-password [candidate hashed] (BCrypt/checkpw candidate hashed))
 (defn create-apikey [] (apply str (take 40 (repeatedly #(rand-int 10)))))
 
+(defn summary [user]
+  "returns common information about the user"
+  {:userId (:id user)
+   :firstName (:firstName user)
+   :lastName (:lastName user)
+   :role     (:role user)
+   :displayName (str (:firstName user) " " (:lastName user))})
+
 (defn login [username password]
   "returns non-private information of first user with the username and password"
   (if-let [user (mongo/select-one mongo/users {:username username})]
