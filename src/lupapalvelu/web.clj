582fc6974cb472480e7eb602cb2e952be3f316bc
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-10-02 14:38:38 +0300

    minor cleanup

--- src/lupapalvelu/web.clj
+++ src/lupapalvelu/web.clj
@@ -164,11 +164,10 @@
   "Replaces all non-ascii chars and other that the allowed punctuation with dash.
    UTF-8 support would have to be browser specific, see http://greenbytes.de/tech/tc2231/"
   [unencoded-filename]
-  (when unencoded-filename
-    (let [de-accented (strings/de-accent unencoded-filename)]
+  (when-let [de-accented (strings/de-accent unencoded-filename)]
       (clojure.string/replace
         (strings/to-max-length de-accented windows-filename-max-length)
-        #"[^a-zA-Z0-9\.\-_ ]" "-"))))
+        #"[^a-zA-Z0-9\.\-_ ]" "-")))
 
 (defn output-attachment [attachmentId download]
   (debug "file download: attachmentId=%s" attachmentId)
