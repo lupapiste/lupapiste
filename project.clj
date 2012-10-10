b4c0428f4f0dcd6e0865bc5e47ac1cf75c53ea4e
jarppe <jarppe@metosin.fi>
2012-10-10 19:16:36 +0300

    Added lein-pedantic

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10" :exclusions [org.clojure/clojure]]
                  [com.novemberain/monger "1.1.2"]
-                 [enlive "1.0.1"]
+                 [enlive "1.0.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.nrepl "0.2.0-beta8"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [clj-http "0.5.3"]]
   :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "2.0.0-SNAPSHOT"]
+                             [lein-pedantic "0.0.5"]
                              [lein-buildid "0.1.0-SNAPSHOT"]]}}
   :main lupapalvelu.server
   :repl-options {:init-ns lupapalvelu.server}
