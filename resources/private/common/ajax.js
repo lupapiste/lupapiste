5661479e20a3ce1c61061df29d7441872888accb
Tommi Reiman <tommi.reiman@metosin.fi>
2012-11-01 14:10:49 +0200

    default successHandler notifys success, not error

--- resources/private/common/ajax.js
+++ resources/private/common/ajax.js
@@ -31,7 +31,7 @@ var ajax = function() {
 			}
 		};
 		
-		self.successHandler = function(e) { notify.error("rest",e); };
+		self.successHandler = function(e) { notify.success("ok",e); };
 		self.errorHandler = function(e) { notify.error("error",e); };
 		self.failHandler = function(jqXHR, textStatus, errorThrown) { error("Ajax: FAIL", jqXHR, textStatus, errorThrown); };
 		self.completeHandler = function(jqXHR, textStatus) { };
