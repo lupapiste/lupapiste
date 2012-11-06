28ddd135f1425e9504fe21e723d98c4347d09a92
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-11-06 17:36:17 +0200

    Changed attachement type grouping for demo purposes

--- src/lupapalvelu/attachment.clj
+++ src/lupapalvelu/attachment.clj
@@ -27,54 +27,56 @@
 
 (def attachment-types-for-permit-type
   {:buildingPermit
-   [{:ordinal 0, :key :oletusliitteet, :s "Oletusliiteryhmä" ; optgroup
+   [{:ordinal 0, :key :jotkuliitteet, :s "Liitetyyppej\u00e4 voi ryhmitell\u00e4 n\u00e4in" ; optgroup
      :types [{:ordinal 0,   :key :asemapiirrosluonnos, :s "Asemapiirrosluonnos"} ; options
              {:ordinal 10,  :key :pohjapiirustusluonnos, :s "Pohjapiirustusluonnos"}
              {:ordinal 20,  :key :leikkauspiirustusluonnos, :s "Leikkauspiirustusluonnos"}
              {:ordinal 30,  :key :julkisivupiirustusluonnos,  :s "Julkisivupiirustusluonnos"}
              {:ordinal 40,  :key :vesikattopiirustusluonnos,  :s "Vesikattopiirustusluonnos"}
-             {:ordinal 50,  :key :muut_paapiirustusluonnokset,  :s "Muut pääpiirustusluonnokset"}
-             {:ordinal 60,  :key :ulkovaritysselvitys, :s "Ulkoväritysselvitys"}
+             {:ordinal 50,  :key :muut_paapiirustusluonnokset,  :s "Muut p\u00e4\u00e4piirustusluonnokset"}
+             {:ordinal 60,  :key :ulkovaritysselvitys, :s "Ulkov\u00e4ritysselvitys"}
              {:ordinal 70,  :key :tonttikartta, :s "Tonttikartta"}
              {:ordinal 80,  :key :tonttikartan_liite, :s "Tonttikartan liite"}
              {:ordinal 90,  :key :pintavaaituskartta, :s "Pintavaaituskartta"}
              {:ordinal 100, :key :kerrosalalaskelma, :s "Kerrosalalaskelma"}
              {:ordinal 110, :key :valtakirja, :s "Valtakirja"}
-             {:ordinal 120, :key :poytakirjaote, :s "Pöytäkirjaote"}
+             {:ordinal 120, :key :poytakirjaote, :s "P\u00f6yt\u00e4kirjaote"}
              {:ordinal 130, :key :kaupparekisteriote, :s "Kaupparekisteriote"}
-             {:ordinal 140, :key :osakeyhtion_perustamiskirja,  :s "Osakeyhtiön perustamiskirja"}
-             {:ordinal 150, :key :jaljennos_yhtiojarjestyksesta,  :s "Jäljennös yhtiöjärjestyksestä"}
-             {:ordinal 160, :key :ositus__ja_perinnonjakokirja,  :s "Ositus- ja perinnönjakokirja"}
-             {:ordinal 170, :key :sopimusjaljennos, :s "Sopimusjäljennös"}
+             {:ordinal 140, :key :osakeyhtion_perustamiskirja,  :s "Osakeyhti\u00f6n perustamiskirja"}
+             {:ordinal 150, :key :jaljennos_yhtiojarjestyksesta,  :s "J\u00e4ljenn\u00f6s yhti\u00f6j\u00e4rjestyksest\u00e4"}
+             {:ordinal 160, :key :ositus__ja_perinnonjakokirja,  :s "Ositus- ja perinn\u00f6njakokirja"}
+             {:ordinal 170, :key :sopimusjaljennos, :s "Sopimusj\u00e4ljenn\u00f6s"}
              {:ordinal 180, :key :rakennesuunnittelusta_vastaavan_lomake,  :s "Rakennesuunnittelusta vastaavan lomake"}
              {:ordinal 190, :key :suunnittelijan_lomake, :s "Suunnittelijan lomake"}
              {:ordinal 200, :key :naapurin_kuuleminen, :s "Naapurin kuuleminen"}
              {:ordinal 210, :key :lehtikuulutus, :s "Lehtikuulutus"}
-             {:ordinal 220, :key :ilmoitus_vaestonsuojasta,  :s "Ilmoitus väestönsuojasta"}
-             {:ordinal 230, :key :esteettomyysselvitys, :s "Esteettömyysselvitys"}
+             {:ordinal 220, :key :ilmoitus_vaestonsuojasta,  :s "Ilmoitus v\u00e4est\u00f6nsuojasta"}
+             {:ordinal 230, :key :esteettomyysselvitys, :s "Esteett\u00f6myysselvitys"}
              {:ordinal 240, :key :poistumistieselvitys, :s "Poistumistieselvitys"}
-             {:ordinal 250, :key :lampohavioiden_tasauslaskelma,  :s "Lämpöhäviöiden tasauslaskelma"}
+             {:ordinal 250, :key :lampohavioiden_tasauslaskelma,  :s "L\u00e4mp\u00f6h\u00e4vi\u00f6iden tasauslaskelma"}
              {:ordinal 260, :key :energiatodistus, :s "Energiatodistus"}
-             {:ordinal 270, :key :rakennusjateselvitys, :s "Rakennusjäteselvitys"}
-             {:ordinal 280, :key :purkujateselvitys, :s "Purkujäteselvitys"}
+             {:ordinal 270, :key :rakennusjateselvitys, :s "Rakennusj\u00e4teselvitys"}
+             {:ordinal 280, :key :purkujateselvitys, :s "Purkuj\u00e4teselvitys"}
              {:ordinal 290, :key :palotekninen_selvitys, :s "Palotekninen selvitys"}
              {:ordinal 300, :key :pohjatutkimus, :s "Pohjatutkimus"}
              {:ordinal 310, :key :riskianalyysi, :s "Riskianalyysi"}
-             {:ordinal 320, :key :poikkeamispaatos, :s "Poikkeamispäätös"}
+             {:ordinal 320, :key :poikkeamispaatos, :s "Poikkeamisp\u00e4\u00e4t\u00f6s"}
              {:ordinal 330, :key :suunnittelutarveratkaisu, :s "Suunnittelutarveratkaisu"}
-             {:ordinal 340, :key :asemakaavoituksen_lausunto, :s "Asemakaavoituksen lausunto"}
+             {:ordinal 460, :key :rh_ilmoitus, :s "RH-ilmoitus"}]}
+    {:ordinal 0, :key :lausunnot, :s "Lausunnot" ; optgroup
+     :types [{:ordinal 340, :key :asemakaavoituksen_lausunto, :s "Asemakaavoituksen lausunto"}
              {:ordinal 350, :key :yleiskaavoituksen_lausunto, :s "Yleiskaavoituksen lausunto"}
              {:ordinal 360, :key :kunnallistekniikan_lausunto, :s "Kunnallistekniikan lausunto"}
              {:ordinal 370, :key :mittauksen_lausunto, :s "Mittauksen lausunto"}
              {:ordinal 380, :key :viherpalveluiden_lausunto,  :s "Viherpalveluiden lausunto"}
              {:ordinal 390, :key :vesilaitoksen_lausunto,  :s "Vesilaitoksen lausunto"}
              {:ordinal 400, :key :paloviranomaisen_lausunto,  :s "Paloviranomaisen lausunto"}
-             {:ordinal 410, :key :vaestonsuojeluviranomaisen_lausunto, :s "Väestönsuojeluviranomaisen lausunto"}
-             {:ordinal 420, :key :terveydenhoitoviranomaisen_lausunto, :s "Terveydenhoitoviranomaisen lausunto"}
-             {:ordinal 430, :key :muu_lausunto, :s "Muu lausunto"}
+             {:ordinal 410, :key :vaestonsuojeluviranomaisen_lausunto, :s "V\u00e4est\u00f6nsuojeluviranomaisen lausunto"}
+             {:ordinal 420, :key :terveydenhoitoviranomaisen_lausunto, :s "Terveydenhoitoviranomaisen lausunto"}]}
+    {:ordinal 0, :key :muut, :s "Muut liitteet" ; optgroup
+     :types [{:ordinal 430, :key :muu_lausunto, :s "Muu lausunto"}
              {:ordinal 440, :key :valokuvia, :s "Valokuvia"}
              {:ordinal 450, :key :neuvottelumuistio, :s "Neuvottelumuistio"}
-             {:ordinal 460, :key :rh_ilmoitus, :s "RH-ilmoitus"}
              {:ordinal 999, :key :muu, :s "Muu liite"}]
      }]})
 
