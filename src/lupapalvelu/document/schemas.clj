87baea05959c5dcb103b4b137e3c179c163ca10b
Tommi Reiman <tommi.reiman@metosin.fi>
2012-11-06 13:21:24 +0200

    paasuunnittelija tiedot -document. reformatted

--- src/lupapalvelu/document/schemas.clj
+++ src/lupapalvelu/document/schemas.clj
@@ -1,27 +1,34 @@
 (ns lupapalvelu.document.schemas)
 
-(defn to-map-by-name [docs] 
+(defn to-map-by-name [docs]
   (into {} (for [doc docs] [(get-in doc [:info :name]) doc])))
 
 (def schemas
   (to-map-by-name [{:info {:name "uusi-rakennus"}
-                  :body [{:name "date" :type "date" }
-                         {:name "varusteet" :type "choice"
-                          :body [{:name "sahko"  :type "checkbox"}
-                                 {:name "kaasu"  :type "checkbox"}
-                                 {:name "hissi"  :type "checkbox"}
-                                 {:name "muu"    :type "string" :size "s"}]}
-                         {:name "materiaali" :type "select"
-                          :body [{:name "puu"}
-                                 {:name "purkka"}
-                                 {:name "betoni"}]}
-                         {:name "story" :type "text"}]}
-                 {:info {:name "hakijan-tiedot"}
-                  :body [{:name "etunimi" :type "string"}
-                         {:name "sukunimi" :type "string"}
-                         {:name "osoite" :type "group"
-                          :body [{:name "katu" :type "string"}
-                                 {:name "postinumeto" :type "string"}
-                                 {:name "postitoimipaikka" :type "string"}]}
-                         {:name "puhelin" :type "string"}
-                         {:name "email" :type "string"}]}]))
+                    :body [{:name "date" :type "date" }
+                           {:name "varusteet" :type "choice"
+                            :body [{:name "sahko"  :type "checkbox"}
+                                   {:name "kaasu"  :type "checkbox"}
+                                   {:name "hissi"  :type "checkbox"}
+                                   {:name "muu"    :type "string" :size "s"}]}
+                           {:name "materiaali" :type "select"
+                            :body [{:name "puu"}
+                                   {:name "purkka"}
+                                   {:name "betoni"}]}
+                           {:name "story" :type "text"}]}
+                   {:info {:name "hakijan-tiedot"}
+                    :body [{:name "etunimi" :type "string"}
+                           {:name "sukunimi" :type "string"}
+                           {:name "osoite" :type "group"
+                            :body [{:name "katu" :type "string"}
+                                   {:name "postinumeto" :type "string"}
+                                   {:name "postitoimipaikka" :type "string"}]}
+                           {:name "puhelin" :type "string"}
+                           {:name "email" :type "string"}]}
+                   {:info {:name "paasuunnittelijan-tiedot"}
+                    :body [{:name "etunimi" :type "string"}
+                           {:name "sukunimi" :type "string"}
+                           {:name "email" :type "string"}
+                           {:name "puhelin" :type "string"}
+                           {:name "koulutus" :type "string"}
+                           {:name "kokemus" :type "string"}]}]))
