18f801c18650a8c6dec0dce40081e0431d37a086
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-11-07 16:40:37 +0200

    Added getURLParameter helper method

--- resources/private/common/pageutil.js
+++ resources/private/common/pageutil.js
@@ -7,15 +7,29 @@ var pageutil = function() {
   function isPageReady(id) {
     return  $('#'+id+'-page-is-ready').length > 0;
   }
-  
+
   function setPageNotReady() {
     $("#pageStatus").children().remove();
   }
-  
+
+  /**
+   * Returns HTTP GET parameter value or null if the parameter is not set.
+   */
+  function getURLParameter(name) {
+    if (location.search) {
+      var value = (location.search.match(RegExp("[?|&]"+name+'=([^&]*)(&|$)'))||[,null])[1];
+      if (value !== null) {
+        return decodeURIComponent(value).replace(/\+/g, " ");
+      }
+    }
+    return null;
+  }
+
   return {
     setPageReady:     setPageReady,
     setPageNotReady:  setPageNotReady,
-    isPageReady:      isPageReady
+    isPageReady:      isPageReady,
+    getURLParameter:  getURLParameter
   };
-  
+
 }();
