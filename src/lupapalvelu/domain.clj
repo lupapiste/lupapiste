f13694649d230de2ed554a35beba0f6f996d60a6
Tommi Reiman <tommi.reiman@metosin.fi>
2012-11-07 13:17:25 +0200

    get document as domain method

--- src/lupapalvelu/domain.clj
+++ src/lupapalvelu/domain.clj
@@ -4,3 +4,8 @@
   (some (fn [[role {id :id}]]
           (if (= id user-id) role))
         roles))
+
+(defn get-document
+  "returns first document from application with the document-id"
+  [{documents :documents} document-id]
+  (first (filter #(= document-id (:id %)) documents)))
--- test/lupapalvelu/domain_test.clj
+++ test/lupapalvelu/domain_test.clj
@@ -3,12 +3,14 @@
         clojure.test
         midje.sweet))
 
-(def application
-  {:roles {:role-x {:id :user-x}
-           :role-y {:id :user-y}}})
+(facts
+  (let [application {:roles {:role-x {:id :user-x} :role-y {:id :user-y}}}]
+    (fact (role-in-application :user-x application) => :role-x)
+    (fact (role-in-application :user-y application) => :role-y)
+    (fact (role-in-application :user-z application) => nil)
+    (fact (role-in-application nil application) => nil)))
 
 (facts
-  (fact (role-in-application :user-x application) => :role-x)
-  (fact (role-in-application :user-y application) => :role-y)
-  (fact (role-in-application :user-z application) => nil)
-  (fact (role-in-application nil application) => nil))
+  (let [application {:documents [{:id 1 :data "jee"} {:id 2 :data "juu"} {:id 1 :data "hidden"}]}]
+    (fact (get-document application 1) => {:id 1 :data "jee"})
+    (fact (get-document application 2) => {:id 2 :data "juu"})))
