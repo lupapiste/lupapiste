a49e4286f4bf3c299dd7b6a8dad084affaf3703e
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-11-08 17:27:21 +0200

    Exclude cheshire

--- project.clj
+++ project.clj
@@ -12,12 +12,12 @@
                  [org.clojure/data.xml "0.0.6"]
                  [fi.sito.oskari/oskari "0.1"]]
   :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
-                                  [clj-webdriver "0.6.0-alpha11"]]
+                                  [clj-webdriver "0.6.0-alpha11" :exclusions [cheshire/cheshire]]]
                    :plugins [[lein-midje "2.0.0"]
                              [lein-buildid "0.1.0"]
                              [lein-nitpicker "0.2.0"]]}
              :itest {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
-                                    [clj-webdriver "0.6.0-alpha11"]]
+                                    [clj-webdriver "0.6.0-alpha11" :exclusions [cheshire/cheshire]]]
                      :test-paths ^:replace ["itest"]}}
 
   :nitpicker {:exts ["clj" "js" "html" "css" "scss"]
