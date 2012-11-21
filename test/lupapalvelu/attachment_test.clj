a5319538d4278d6fc25ef94e8f29cec0fc97fd24
jarppe <jarppe@metosin.fi>
2012-11-21 10:08:03 +0200

    Moved rest of the mime tests.

--- test/lupapalvelu/attachment_test.clj
+++ test/lupapalvelu/attachment_test.clj
@@ -27,30 +27,6 @@
   (fact "Newlines are removed"
      (encode-filename "12345\n678\r\n90") => (just ascii-pattern)))
 
-(facts "Test mime.types parser"
-  (fact "Contains PDF" (get mime-types "pdf") => "application/pdf")
-  (fact "Contains jpeg" (get mime-types "jpeg") => "image/jpeg")
-  (fact "Contains jpg" (get mime-types "jpg") => "image/jpeg"))
-
-(facts "Test allowd file types"
-  (fact "foo.pdf" (allowed-file? "foo.pdf") => truthy)
-  (fact "FOO.PDF" (allowed-file? "FOO.PDF") => truthy)
-  (fact "AutoCAD" (allowed-file? "plan.dwg") => truthy)
-  (fact "jcad" (allowed-file? "plan.pic") => truthy)
-  (fact "Word" (allowed-file? "word.doc") => truthy)
-  (fact "Word OOXML" (allowed-file? "word.docx") => truthy)
-  (fact "Excel" (allowed-file? "excel.xls") => truthy)
-  (fact "Excel OOXML" (allowed-file? "excel.xlsx") => truthy)
-  (fact "LibreOffice" (allowed-file? "text.ods") => truthy)
-  (fact "zip" (allowed-file? "plan.zip") => truthy)
-  (fact "7zip" (allowed-file? "plan.7z") => truthy)
-  (fact "virus.exe" (allowed-file? "virus.exe") => falsey)
-  (fact "virus.bat" (allowed-file? "virus.bat") => falsey)
-  (fact "virus.sh" (allowed-file? "virus.sh") => falsey)
-  (fact "none" (allowed-file? "virus.") => falsey)
-  (fact "empty" (allowed-file? "") => falsey)
-  (fact "nil" (allowed-file? nil) => falsey))
-
 (def test-attachments [{:id "1", :latestVersion {:version {:major 9, :minor 7}}}])
 
 (facts "Test attachment-latest-version"
@@ -60,6 +36,5 @@
 (def next-attachment-version @#'lupapalvelu.attachment/next-attachment-version)
 
 (facts "Facts about next-attachment-version"
-  (fact (next-attachment-version {:major 1 :minor 1} {:role :authority}) => {:major 1 :minor 2})
-  (fact (next-attachment-version {:major 1 :minor 1} {:role :dude}) => {:major 2 :minor 0}))
-
+  (fact (next-attachment-version {:major 1 :minor 1} {:role :authority})  => {:major 1 :minor 2})
+  (fact (next-attachment-version {:major 1 :minor 1} {:role :dude})       => {:major 2 :minor 0}))
