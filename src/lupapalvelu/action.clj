f64492999b571d03c0b31b954b68dee4602ee363
Tommi Reiman <tommi.reiman@metosin.fi>
2012-11-21 15:13:29 +0200

    remove space in email

--- src/lupapalvelu/action.clj
+++ src/lupapalvelu/action.clj
@@ -62,7 +62,7 @@
 (defn invite-body [user id host]
   (format
     (str
-      "Tervehdys,\n\n %s %s lis\u00E4si teid\u00E4t suunnittelijaksi lupahakemukselleen.\n\n"
+      "Tervehdys,\n\n%s %s lis\u00E4si teid\u00E4t suunnittelijaksi lupahakemukselleen.\n\n"
       "Hyv\u00E4ksy\u00E4ksesi rooli ja n\u00E4hd\u00E4ksesi hakemuksen tiedot avaa linkki %s/applicant#!/application/%s\n\n"
       "Yst\u00E4v\u00E4llisin terveisin,\n\n"
       "Lupapiste.fi")
