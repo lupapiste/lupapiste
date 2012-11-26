eb486df0a6e6dbf5d55947bd6945b2fc888f6dd9
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-11-26 13:20:40 +0200

    Added inforequest files

--- src/lupapalvelu/components/ui_components.clj
+++ src/lupapalvelu/components/ui_components.clj
@@ -81,7 +81,15 @@
    :create-application  {:js ["create-application.js"]
                          :html (map (partial format "create-application-%02d.html") (range 1 (inc 3)))}
 
-   :applicant    {:depends [:common :map :application :applications :attachment :create-application :buildinfo :docgen]
+   :create-inforequest  {:depends [:common]
+                         :html ["create-inforequest.html"]}
+
+   :inforequests  {:depends [:common]
+                  :html ["inforequests.html"]}
+
+   :applicant    {:depends [:common :map :applications
+                            :application :attachment :create-application :docgen
+                            :inforequests :create-inforequest :buildinfo]
                   :js ["applicant.js"]
                   :html ["index.html"]}
 
