3ef6303cd1d21f7e45685e46643356bf330935c4
jarppe <jarppe@metosin.fi>
2012-11-29 15:20:31 +0200

    Cleanup.

--- itest/lupapalvelu/attachment_itest.clj
+++ itest/lupapalvelu/attachment_itest.clj
@@ -8,16 +8,13 @@
 ;;
 
 (fact
-  (let [resp (command pena :create-application :x 408048 :y 6693225 :street "s" :city "c" :zip "z")
-        application-id (:id resp)
-        application (:application (query pena :application :id application-id))
-        resp (command veikko :create-attachment
-                      :id application-id
-                      :attachmentType {:type-group "tg"
-                                       :type-id "tid"})
-        attachment-id (:attachmentId resp)
-        application (:application (query pena :application :id application-id))
-        attachment (some #(if (= (:id %) attachment-id) %) (:attachments application))]
+  (let [resp            (command pena :create-application :x 408048 :y 6693225 :street "s" :city "c" :zip "z")
+        application-id  (:id resp)
+        application     (:application (query pena :application :id application-id))
+        resp            (command veikko :create-attachment :id application-id :attachmentType {:type-group "tg" :type-id "tid"})
+        attachment-id   (:attachmentId resp)
+        application     (:application (query pena :application :id application-id))
+        attachment      (some #(if (= (:id %) attachment-id) %) (:attachments application))]
     attachment => (contains
                     {:type {:type-group "tg"
                             :type-id "tid"}
