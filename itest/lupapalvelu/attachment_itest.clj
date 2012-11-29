3bbc1af61dbb6456b26dc192223fb38e7d5887b9
jarppe <jarppe@metosin.fi>
2012-11-29 15:19:25 +0200

    Fixed test.

--- itest/lupapalvelu/attachment_itest.clj
+++ itest/lupapalvelu/attachment_itest.clj
@@ -8,22 +8,18 @@
 ;;
 
 (fact
-  (let [resp (command pena :create-application :x 408048 :y 6693225 :street "s" :city "c" :zip "z" :schemas ["hakija"])
+  (let [resp (command pena :create-application :x 408048 :y 6693225 :street "s" :city "c" :zip "z")
         application-id (:id resp)
-        application (:application (query pena :application :id application-id))]
-    (nil? (:attachments application)) => falsey
-    (empty? (:attachments application)) => truthy
-    (let [resp (command veikko :create-attachment
-                 :id application-id
-                 :attachmentType {:type-group "tg"
-                                  :type-id "tid"})
-          attachment-id (:attachmentId resp)
-          application (:application (query pena :application :id application-id))]
-      (nil? attachment-id) => falsey
-      (count (:attachments application)) => 1
-      (first (:attachments application)) => (contains
-                                              {:type {:type-group "tg"
-                                                      :type-id "tid"}
-                                               :state "requires_user_action"
-                                               :latestVersion {:version {:minor 0 :major 0}}
-                                               :versions []}))))
+        application (:application (query pena :application :id application-id))
+        resp (command veikko :create-attachment
+                      :id application-id
+                      :attachmentType {:type-group "tg"
+                                       :type-id "tid"})
+        attachment-id (:attachmentId resp)
+        application (:application (query pena :application :id application-id))
+        attachment (some #(if (= (:id %) attachment-id) %) (:attachments application))]
+    attachment => (contains
+                    {:type {:type-group "tg"
+                            :type-id "tid"}
+                     :state "requires_user_action"
+                     :versions []})))
