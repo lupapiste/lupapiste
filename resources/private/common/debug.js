4058ada34ed7a8fe0b9e4544fcdd037220412516
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-11-30 11:46:13 +0200

    Cleaned duplicate HTML

--- resources/private/common/debug.js
+++ resources/private/common/debug.js
@@ -1,4 +1,9 @@
 $(function() {
+
+  $("footer").prepend('<label class="checkbox"><input type="checkbox" id="todo" checked="checked">N\u00e4yt\u00e4 toteuttamattomat</label>' +
+      '<label class="checkbox"><input type="checkbox" id="hidden">K\u00e4\u00e4nn\u00e4 piilotetut</label>'+
+      '<label class="checkbox"><input type="checkbox" id="events">N\u00e4yt\u00e4 eventit</label>');
+
   $(".todo").addClass("todo-off");
   $("#todo").click(function() { $(".todo").toggleClass("todo-off"); });
   $("#hidden").click(function() { $(".page").toggleClass("visible"); });
