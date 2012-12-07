625adf79f9dd82e9dee46c3a9ea826581a031f69
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2012-12-07 16:43:46 +0200

    Jotain xml edes jo ulos

--- src/lupapalvelu/xml/emit.clj
+++ src/lupapalvelu/xml/emit.clj
@@ -2,7 +2,10 @@
   (:use [clojure.data.xml]))
 
 (defn element-to-xml [data k model]
-  (let [key (conj k (:tag model))]              
+  (let [key (conj k (:tag model))]
+  ;(println "===================================")
+  (println key)
+  ;(clojure.pprint/pprint data)
   (element (:tag model) (:attr model) (if (:child model)
                          (map #(element-to-xml data key %) (:child model))
                          (str (get-in data key))))))
--- src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
+++ src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
@@ -4,7 +4,7 @@
          [clojure.java.io]))
 
 ;RakVal
-(def tunnus [{:tag :tunnus 
+(def tunnus [{:tag :tunnus
               :child [{:tag :valtakunnallinenNumero}
                       {:tag :jarjestysnumero}
                       {:tag :kiinttun}
@@ -12,30 +12,25 @@
                       {:tag :aanestysalue}]}])
 
 ;YHTEISET
-(def piste [{:tag :piste 
-             :child [{:tag :Point 
+(def piste [{:tag :piste
+             :child [{:tag :Point
                       :child [{:tag :pos}]}]}])
 
-(def postiosoite [{:tag :osoite 
+(def postiosoite {:tag :osoite
                    :child [{:tag :kunta}
-                           {:tag :osoitenimi 
-                            :attr {:xmlns "http://www.paikkatietopalvelu.fi/gml/yhteiset"} 
+                           {:tag :osoitenimi
+                            :attr {:xmlns "http://www.paikkatietopalvelu.fi/gml/yhteiset"}
                             :child [{:tag :teksti}]}
                            {:tag :postinumero}
-                           {:tag :postitoimipaikannimi}]}])
- 
-(def sijantitieto [{:tag :sijaintitieto
-                    :child [{:tag :Sijainti
-                             :child (conj postiosoite piste [{:tag :sijaintiepavarmuus}
-                                                             {:tag :luontitapa}])}]}])
+                           {:tag :postitoimipaikannimi}]})
 
 (def rakennusoikeudet [:tag :rakennusoikeudet
                        :child [{:tag :kayttotarkoitus
                                 :child [{:tag :pintaAla}
                                         {:tag :kayttotarkoitusKoodi}]}]])
 
-(def kiinteisto [{:tag :kiinteisto 
-                  :child (conj [{:tag :kiinteisto 
+(def kiinteisto [{:tag :kiinteisto
+                  :child (conj [{:tag :kiinteisto
                                  :child [{:tag :kylanimi}
                                          {:tag :tilannimi}
                                          {:tag :kiinteistotunnus}
@@ -56,15 +51,15 @@
 
 (def yksilointitieto [{:tag :yksilointitieto}
                       {:tag :alkuHetki}
-                      {:tag :loppuHetki}]) 
+                      {:tag :loppuHetki}])
 
-(def rakennus [{:tag :Rakennuspaikka :child [(conj [] 
+(def rakennus [{:tag :Rakennuspaikka :child [(conj []
                                                    yksilointitieto
                                                    kiinteisto)]}])
 
 (def rakennuspaikka  (conj [{:tag :Rakennuspaikka}
-                            {:tag :kiinteisto 
-                             :child [{:tag :kiinteisto 
+                            {:tag :kiinteisto
+                             :child [{:tag :kiinteisto
                                       :child [{:tag :kylanimi}
                                               {:tag :tilannimi}
                                               {:tag :kiinteistotunnus}
@@ -76,31 +71,49 @@
                             {:tag :kaavanaste}
                             {:tag :kerrosala}
                             {:tag :tasosijainti}
-                            {:tag :rakennusoikeudet 
-                             :child [{:tag :kayttotarkoitus 
+                            {:tag :rakennusoikeudet
+                             :child [{:tag :kayttotarkoitus
                                       :child [{:tag :pintaAla}
                                               {:tag :kayttotarkoitusKoodi}]}]}
                             {:tag :rakennusoikeusYhteensa}
-                            {:tag :uusiKytkin}] 
-                           yksilointitieto 
-                           sijantitieto 
+                            {:tag :uusiKytkin}]
+                           yksilointitieto
+                           sijantitieto
                            postiosoite))
 
-(def rakennelma (conj [{:tag :kuvaus 
-                        :child [{:tag :kuvaus}]}] 
+(def rakennelma (conj [{:tag :kuvaus
+                        :child [{:tag :kuvaus}]}]
                       yksilointitieto
                       sijantitieto
                       tunnus))
 
 (def rakennuslupa_to_krysp
-  [{
-    :tag :Rakennusvalvonta :attr { :xmlns:xs "http://www.w3.org/2001/XMLSchema"
+  {
+    :tag :Rakennusvalvonta :attr {:xmlns:xs "http://www.w3.org/2001/XMLSchema"
                                   :xmlns:rakval "http://www.paikkatietopalvelu.fi/gml/rakennusvalvonta"
                                   :xmlns:gml "http://www.opengis.net/gml"
                                   :xmlns:yht "http://www.paikkatietopalvelu.fi/gml/yhteiset"
                                   :targetNamespace "http://www.paikkatietopalvelu.fi/gml/rakennusvalvonta"
-                                  :elementFormDefault "qualified" 
-                                  :attributeFormDefault "unqualified" 
+                                  :elementFormDefault "qualified"
+                                  :attributeFormDefault "unqualified"
                                   :version "2.0.0"}
-    :child (conj rakennuspaikka rakennus rakennelma)}
-   ])
+    ;:child (conj rakennuspaikka rakennus rakennelma)}
+    :child [{:tag :RakennusvalvontaAsia
+            :child [{:tag :osapuolettieto
+                    :child [{:tag :Osapuolet
+                             :child [{:tag :osapuolitieto
+                                     :child [{:tag :Osapuoli
+                                              :child [{:tag :kuntaRooliKoodi}
+                                                      {:tag :vrkRooliKoodi}
+                                                      {:tag :henkilo
+                                                       :child [{:tag :etunimi}
+                                                               {:tag :sukunimi}
+                                                               {:tag :sahkopostiosoite}
+                                                               {:tag :faksinumero}
+                                                               {:tag :puhelinnumero}
+                                                               {:tag :henkilotunnus}
+                                                               postiosoite]
+                                             }]
+                                    }]}]}]}]
+            }]
+   })
