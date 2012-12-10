216eb70309c81be512f95ec8b241635527b1ff5c
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-12-10 12:42:55 +0200

    Moved definitions to yhteiset

--- src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
+++ src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.xml.krysp.rakennuslupa-mapping
   (:use  [lupapalvelu.xml.emit]
+         [lupapalvelu.xml.krysp.yhteiset]
          [clojure.data.xml]
          [clojure.java.io]))
 
@@ -10,76 +11,6 @@
                       {:tag :kiinttun}
                       {:tag :rakennusnro}
                       {:tag :aanestysalue}]}])
-
-;YHTEISET
-(def piste [{:tag :piste
-             :child [{:tag :Point
-                      :child [{:tag :pos}]}]}])
-
-(def postiosoite {:tag :osoite
-                   :child [{:tag :kunta}
-                           {:tag :osoitenimi
-                            :attr {:xmlns "http://www.paikkatietopalvelu.fi/gml/yhteiset"}
-                            :child [{:tag :teksti}]}
-                           {:tag :postinumero}
-                           {:tag :postitoimipaikannimi}]})
-
-(def rakennusoikeudet [:tag :rakennusoikeudet
-                       :child [{:tag :kayttotarkoitus
-                                :child [{:tag :pintaAla}
-                                        {:tag :kayttotarkoitusKoodi}]}]])
-
-(def kiinteisto [{:tag :kiinteisto
-                  :child (conj [{:tag :kiinteisto
-                                 :child [{:tag :kylanimi}
-                                         {:tag :tilannimi}
-                                         {:tag :kiinteistotunnus}
-                                         {:tag :maaraAlaTunnus}]}
-                                {:tag :palsta}
-                                {:tag :kokotilaKytkin}
-                                {:tag :hallintaperuste}
-                                {:tag :vuokraAluetunnus}
-                                {:tag :kaavanaste}
-                                {:tag :kerrosala}
-                                {:tag :tasosijainti}
-                                {:tag :rakennusoikeusYhteensa}
-                                {:tag :uusiKytkin}]
-                               postiosoite
-                               sijantitieto
-                               rakennusoikeudet
-                               )}])
-
-(def yksilointitieto [{:tag :yksilointitieto}
-                      {:tag :alkuHetki}
-                      {:tag :loppuHetki}])
-
-(def rakennus [{:tag :Rakennuspaikka :child [(conj []
-                                                   yksilointitieto
-                                                   kiinteisto)]}])
-
-(def rakennuspaikka  (conj [{:tag :Rakennuspaikka}
-                            {:tag :kiinteisto
-                             :child [{:tag :kiinteisto
-                                      :child [{:tag :kylanimi}
-                                              {:tag :tilannimi}
-                                              {:tag :kiinteistotunnus}
-                                              {:tag :maaraAlaTunnus}]}
-                                     {:tag :palsta}
-                                     {:tag :kokotilaKytkin}
-                                     {:tag :hallintaperuste}
-                                     {:tag :vuokraAluetunnus}]}
-                            {:tag :kaavanaste}
-                            {:tag :kerrosala}
-                            {:tag :tasosijainti}
-                            {:tag :rakennusoikeudet
-                             :child [{:tag :kayttotarkoitus
-                                      :child [{:tag :pintaAla}
-                                              {:tag :kayttotarkoitusKoodi}]}]}
-                            {:tag :rakennusoikeusYhteensa}
-                            {:tag :uusiKytkin}]
-                           yksilointitieto
-                           sijantitieto
-                           postiosoite))
 
 (def rakennelma (conj [{:tag :kuvaus
                         :child [{:tag :kuvaus}]}]
