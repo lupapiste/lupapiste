72e7a087dade49790fed315f2be5d3d5abc543c1
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-12-10 16:18:27 +0200

    Produce valid KRYSP element structure, element content still invalid/missing

--- src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
+++ src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
@@ -1,6 +1,5 @@
 (ns lupapalvelu.xml.krysp.rakennuslupa-mapping
-  (:use  [lupapalvelu.xml.emit]
-         [lupapalvelu.xml.krysp.yhteiset]
+  (:use  [lupapalvelu.xml.krysp.yhteiset]
          [clojure.data.xml]
          [clojure.java.io]))
 
@@ -19,32 +18,35 @@
                       tunnus))
 
 (def rakennuslupa_to_krysp
-  {
-    :tag :Rakennusvalvonta :attr {:xmlns:xs "http://www.w3.org/2001/XMLSchema"
-                                  :xmlns:rakval "http://www.paikkatietopalvelu.fi/gml/rakennusvalvonta"
+  {:tag :Rakennusvalvonta :attr {:xmlns:xs "http://www.w3.org/2001/XMLSchema"
+                                  :xmlns "http://www.paikkatietopalvelu.fi/gml/rakennusvalvonta"
                                   :xmlns:gml "http://www.opengis.net/gml"
                                   :xmlns:yht "http://www.paikkatietopalvelu.fi/gml/yhteiset"
-                                  :targetNamespace "http://www.paikkatietopalvelu.fi/gml/rakennusvalvonta"
-                                  :elementFormDefault "qualified"
-                                  :attributeFormDefault "unqualified"
-                                  :version "2.0.0"}
+                                  ; XXX not in schema:
+                                  ; :elementFormDefault "qualified"
+                                  ; :attributeFormDefault "unqualified"
+                                  ; :version "2.0.0"
+                                  }
     ;:child (conj rakennuspaikka rakennus rakennelma)}
-    :child [{:tag :RakennusvalvontaAsia
-            :child [{:tag :osapuolettieto
-                    :child [{:tag :Osapuolet
-                             :child [{:tag :osapuolitieto
-                                     :child [{:tag :Osapuoli
-                                              :child [{:tag :kuntaRooliKoodi}
-                                                      {:tag :vrkRooliKoodi}
-                                                      {:tag :henkilo
-                                                       :child [{:tag :etunimi}
-                                                               {:tag :sukunimi}
-                                                               {:tag :sahkopostiosoite}
-                                                               {:tag :faksinumero}
-                                                               {:tag :puhelinnumero}
-                                                               {:tag :henkilotunnus}
-                                                               postiosoite]
-                                             }]
-                                    }]}]}]}]
-            }]
-   })
+   :child [{:tag :toimituksenTiedot :child toimituksenTiedot}
+           {:tag :rakennusvalvontaAsiatieto
+            :child [{:tag :RakennusvalvontaAsia
+                     :child [{:tag :kasittelynTilatieto :child [tilamuutos]}
+                             {:tag :osapuolettieto
+                              :child [{:tag :Osapuolet
+                                       :attr {:xmlns "http://www.paikkatietopalvelu.fi/gml/yhteiset"}
+                                       :child [{:tag :osapuolitieto
+                                                :child [{:tag :Osapuoli
+                                                         :child [{:tag :kuntaRooliKoodi}
+                                                                 {:tag :VRKrooliKoodi}
+                                                                 {:tag :henkilo
+                                                                  :child [{:tag :nimi :child [{:tag :etunimi}, {:tag :sukunimi}]}
+                                                                          postiosoite
+                                                                          {:tag :sahkopostiosoite}
+                                                                          {:tag :faksinumero}
+                                                                          {:tag :puhelin}
+                                                                          {:tag :henkilotunnus}]
+                                                                  }]
+                                                         }]}]}]}]
+                     }]}]
+    })
