4726615aea6dc2ad110e56c04d9432dceb2d55cb
Hannu Toivola <Hannu.Toivola@solita.fi>
2012-12-10 10:49:32 +0200

    Fixed application formating

--- src/lupapalvelu/document/schemas.clj
+++ src/lupapalvelu/document/schemas.clj
@@ -8,36 +8,37 @@
           docs))
 
 (def simple-osoite-body [{:name "katu" :type :string}
-                         {:name "postinumero" :type :string}
-                         {:name "postitoimipaikka" :type :string}])
+                         {:name "postinumero" :type :string :size "s"}
+                         {:name "postitoimipaikka" :type :string :size "m"}])
 
 (def full-osoite-body [{:name "kunta" :type :string}
                        {:name "lahiosoite" :type :string}
                        {:name "osoitenumero" :type :string}
                        {:name "osoitenumero2" :type :string}
-                       {:name "jakokirjain" :type :string}
-                       {:name "jakokirjain2" :type :string}
-                       {:name "porras" :type :string}
-                       {:name "huoneisto" :type :string}
-                       {:name "postinumero" :type :string}
-                       {:name "postitoimipaikka" :type :string}
+                       {:name "jakokirjain" :type :string :size "s"}
+                       {:name "jakokirjain2" :type :string :size "s"}
+                       {:name "porras" :type :string :size "s"}
+                       {:name "huoneisto" :type :string :size "s"}
+                       {:name "postinumero" :type :string :size "s"}
+                       {:name "postitoimipaikka" :type :string :size "m"}
                        {:name "pistesijanti" :type :string}])
 
-(def henkilo-body [{:name "etunimi" :type :string}
+(def yhteystiedot-body [{:name "puhelin" :type :string :subtype :tel}
+                        {:name "email" :type :string :subtype :email}
+                        {:name "fax" :type :string}])
+
+(def henkilotiedot-body [{:name "etunimi" :type :string}
                    {:name "sukunimi" :type :string}
-                   {:name "osoite" :type :group :body simple-osoite-body}
-                   {:name "puhelin" :type :string :subtype :tel}
-                   {:name "email" :type :string :subtype :email}
-                   {:name "fax" :type :string}
                    {:name "hetu" :type :string}])
+
+(def henkilo-body [{:name "henkilotiedot" :type :group :body henkilotiedot-body}
+                   {:name "osoite" :type :group :body simple-osoite-body}
+                   {:name "yhteystiedot" :type :group :body yhteystiedot-body}])
 
 (def yritys-body [{:name "yritysnimi" :type :string}
                    {:name "liikeJaYhteisoTunnus" :type :string}
                    {:name "osoite" :type :group :body simple-osoite-body}
-                   {:name "puhelin" :type :string :subtype :tel}
-                   {:name "email" :type :string :subtype :email}
-                   {:name "fax" :type :string}])
-
+                   {:name "yhteystiedot" :type :group :body yhteystiedot-body}])
 
 ; TODO: Yritys?
 (def suunnittelija-body (concat
