753e80bc5cb29f83ee98bbd6a42e6e4f5f0b0551
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-12-11 14:42:45 +0200

    Support for repeating elements

--- src/lupapalvelu/xml/emit.clj
+++ src/lupapalvelu/xml/emit.clj
@@ -1,26 +1,25 @@
 (ns lupapalvelu.xml.emit
   (:use [clojure.data.xml]))
 
+;(defn- create-element)
+
 (defn element-to-xml [data k model]
   (let [current-key (:tag model)
         key (conj k current-key)
         current-data (get-in data key)]
-    (println "=====================================")
-    (clojure.pprint/pprint key )
-    (println current-key)
-    (println "")
-    (clojure.pprint/pprint current-data)
-    (println "")
-    (clojure.pprint/pprint model)
 
-    (println "=====================================")
-
-
-
-
+    (if (vector? current-data)
+      (for [item current-data]
+                 (element (:tag model) (:attr model)
+                          (if (:child model)
+                            (map #(element-to-xml item [] %) (:child model))
+                            (str item)
+                            ))
+                           )
       (element (:tag model) (:attr model)
-             (if (:child model)
-               (map #(element-to-xml data key %) (:child model))
-               (if (vector? current-data)
-                 (map #(element-to-xml % [current-key] model) current-data)
-                 (str current-data))))))
+               (if (:child model)
+                 (map #(element-to-xml data key %) (:child model))
+                 (str current-data))))
+
+      )
+  )
