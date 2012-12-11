03f03338e094da0ba00dd5846a72f1a7a812c500
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-12-11 14:57:57 +0200

    Cleanup

--- src/lupapalvelu/xml/emit.clj
+++ src/lupapalvelu/xml/emit.clj
@@ -1,25 +1,19 @@
 (ns lupapalvelu.xml.emit
   (:use [clojure.data.xml]))
 
-;(defn- create-element)
+(defn- create-element-hierarcy [data key content model]
+  (element (:tag model) (:attr model)
+           (if (:child model)
+             (map #(element-to-xml data key %) (:child model))
+             (str content))))
 
 (defn element-to-xml [data k model]
   (let [current-key (:tag model)
         key (conj k current-key)
         current-data (get-in data key)]
 
-    (if (vector? current-data)
+    (if (sequential? current-data)
       (for [item current-data]
-                 (element (:tag model) (:attr model)
-                          (if (:child model)
-                            (map #(element-to-xml item [] %) (:child model))
-                            (str item)
-                            ))
-                           )
-      (element (:tag model) (:attr model)
-               (if (:child model)
-                 (map #(element-to-xml data key %) (:child model))
-                 (str current-data))))
-
-      )
+        (create-element-hierarcy item [] item model))
+      (create-element-hierarcy data key current-data model)))
   )
