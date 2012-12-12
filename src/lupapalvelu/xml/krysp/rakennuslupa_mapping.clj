bc7a52d0f123997f0761a62f9343169752d4ebaf
Juha Jokimaki <juha.jokimaki@solita.fi>
2012-12-12 22:52:02 +0200

    Rakennus

--- src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
+++ src/lupapalvelu/xml/krysp/rakennuslupa_mapping.clj
@@ -4,18 +4,46 @@
          [clojure.java.io]))
 
 ;RakVal
-(def tunnus [{:tag :tunnus
-              :child [{:tag :valtakunnallinenNumero}
+(def tunnus-children [{:tag :valtakunnallinenNumero}
                       {:tag :jarjestysnumero}
                       {:tag :kiinttun}
                       {:tag :rakennusnro}
-                      {:tag :aanestysalue}]}])
+                      {:tag :aanestysalue}])
 
 (def rakennelma (conj [{:tag :kuvaus
                         :child [{:tag :kuvaus}]}]
                       yksilointitieto
                       sijantitieto
-                      tunnus))
+                      {:tag :tunnus :child tunnus-children}))
+
+(def rakennus [{:tag :Rakennus
+                :child [sijantitieto
+                        {:tag :rakennuksenTiedot
+                         :child [{:tag :rakennustunnus :child tunnus-children}
+                                 {:tag :kayttotarkoitus}
+                                 {:tag :tilavuus}
+                                 {:tag :kokonaisala}
+                                 {:tag :kellarinpinta-ala}
+                                 {:tag :BIM :child []}
+                                 osoite
+                                 {:tag :rinnakkaisosoite
+                                  :attr {:xmlns "http://www.paikkatietopalvelu.fi/gml/yhteiset"}
+                                  :child postiosoite-children}
+                                 {:tag :kerrosluku}
+                                 {:tag :kerrosala}
+                                 {:tag :rakentamistapa :child []}
+                                 {:tag :kantavaRakennusaine :child []}
+                                 {:tag :julkisivu :child []}
+                                 {:tag :verkostoliittymat :child []}
+                                 {:tag :energialuokka}
+                                 {:tag :paloluokka}
+                                 {:tag :lammitystapa :child []}
+                                 {:tag :lammonlahde :child []}
+                                 {:tag :varusteet :child []}
+                                 {:tag :jaahdytysmuoto}
+                                 {:tag :asuinhuoneistot :child []}
+                                 ]}]}])
+
 
 (def rakennuslupa_to_krysp
   {:tag :Rakennusvalvonta :attr {:xmlns:xs "http://www.w3.org/2001/XMLSchema"
@@ -27,7 +55,6 @@
                                   ; :attributeFormDefault "unqualified"
                                   ; :version "2.0.0"
                                   }
-    ;:child (conj rakennuspaikka rakennus rakennelma)}
    :child [{:tag :toimituksenTiedot :child toimituksenTiedot}
            {:tag :rakennusvalvontaAsiatieto
             :child [{:tag :RakennusvalvontaAsia
@@ -44,6 +71,24 @@
                                                          :child [{:tag :kuntaRooliKoodi}
                                                                  {:tag :VRKrooliKoodi}
                                                                  henkilo]
-                                                         }]}]}]}]
-                     }]}]
-    })
+                                                         }]}]}]}
+                             {:tag :rakennuspaikkatieto}
+                             {:tag :toimenpidetieto
+                              :child [{:tag :Toimenpide
+                                       :child [{:tag :uusi}
+                                               {:tag :laajennus}
+                                               {:tag :kayttotarkoitusmuutos}
+                                               {:tag :perustus}
+                                               {:tag :perusparannus}
+                                               {:tag :uudelleenrakentaminen}
+                                               {:tag :purkaminen}
+                                               {:tag :muuMuutosTyo}
+                                               {:tag :kaupunkikuvaToimenpide}
+                                               {:tag :katselmustieto}
+                                               {:tag :rakennustieto
+                                                :child []}
+                                               {:tag :rakennelmatieto}]}]
+                              }]}
+                    ]
+            }]}
+    )
