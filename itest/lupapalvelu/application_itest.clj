c4a308c4456f231203749d47a5d54d91edba4c74
jarppe <jarppe@metosin.fi>
2013-01-02 12:48:24 +0200

    Added test for create-application with and without comment.

--- itest/lupapalvelu/application_itest.clj
+++ itest/lupapalvelu/application_itest.clj
@@ -5,8 +5,8 @@
 
 (defn- not-empty? [m] (not (empty? m)))
 
-(fact
-  (let [resp            (command pena :create-application :permitType "buildingPermit" :x 444444 :y 6666666 :street "s" :city "c" :zip "z")
+(fact "creating application without message"
+  (let [resp            (command pena :create-application :permitType "buildingPermit" :x 444444 :y 6666666 :address "foo 42, bar" :municipality "753")
         application-id  (:id resp)
         resp            (query pena :application :id application-id)
         application     (:application resp)]
@@ -14,9 +14,21 @@
                               :state "draft"
                               :location {:x 444444 :y 6666666}
                               :permitType "buildingPermit"})
+    (count (:comments application)) => 0
     (first (:auth application)) => (contains
                                      {:firstName "Pena"
                                       :lastName "Panaani"
                                       :type "owner"
                                       :role "owner"})
     (:allowedAttahmentTypes application) => not-empty?))
+
+(fact "creating application message"
+  (let [resp            (command pena :create-application :permitType "buildingPermit" :x 444444 :y 6666666 :address "foo 42, bar" :municipality "753" :message "hello")
+        application-id  (:id resp)
+        resp            (query pena :application :id application-id)
+        application     (:application resp)
+        state           (:state application)
+        comments        (:comments application)]
+    state => "open"
+    (count comments) => 1
+    (-> comments first :text) => "hello"))
