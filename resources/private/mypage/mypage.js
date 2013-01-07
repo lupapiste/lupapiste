6006b40560ed0330e3506ba3f2ab13117df6721d
jarppe <jarppe@metosin.fi>
2013-01-07 16:56:53 +0200

    Change password UI.

--- resources/private/mypage/mypage.js
+++ resources/private/mypage/mypage.js
@@ -1,3 +1,54 @@
 (function() {
-  console.log("My page!");
+
+  function isNotBlank(s) { return !/^\s*$/.test(s); };
+  function equals(s1, s2) { return s1 === s2; };
+  
+  var model = {
+      passwd0: ko.observable(""),
+      passwd1: ko.observable(""),
+      passwd2: ko.observable(""),
+      error: ko.observable(null)
+  };
+
+  model.clear = function() {
+    model.passwd0("");
+    model.passwd1("");
+    model.passwd2("");
+    model.error(null);
+    $("#mypage input[name='passwd0']").focus();
+    return model;
+  }
+  
+
+  model.ok = ko.computed(function() {
+    return isNotBlank(model.passwd0()) && isNotBlank(model.passwd1()) && equals(model.passwd1(), model.passwd2());
+  });
+  
+  model.noMatch = ko.computed(function() {
+    return isNotBlank(model.passwd1()) && isNotBlank(model.passwd2()) && !equals(model.passwd1(), model.passwd2());
+  });
+  
+  model.changePassword = function() {
+    var t = setTimeout(function() { $("#mypage img").show(); }, 200);
+    ajax
+      .command("change-passwd", {"old-pw": model.passwd0(), "new-pw": model.passwd1()})
+      .success(function() {
+        model.clear();
+      })
+      .error(function(data) {
+        model.clear().error(loc("passwd-change." + data.text));
+      })
+      .complete(function() {
+        clearTimeout(t);
+        $("#mypage img").hide();
+      })
+      .call();
+  };
+
+  hub.onPageChange("mypage", model.clear);
+  
+  $(function() {
+    ko.applyBindings(model, $("#mypage")[0]);
+  });
+  
 })();
