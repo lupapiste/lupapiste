cc340dbbc149eedf2c0e81813c9fc365c44b5fa7
jarppe <jarppe@metosin.fi>
2013-01-11 09:24:51 +0200

    Upped dep versiobns, added incanter to :dev profile.

--- project.clj
+++ project.clj
@@ -1,21 +1,22 @@
 (defproject lupapalvelu "0.1.0-SNAPSHOT"
   :description "lupapalvelu"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [noir "1.3.0-beta10" :exclusions [org.clojure/clojure]]
-                 [com.novemberain/monger "1.4.0"]
+                 [noir "1.3.0" :exclusions [org.clojure/clojure]]
+                 [com.novemberain/monger "1.4.1"]
                  [enlive "1.0.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.nrepl "0.2.0-RC1"]
                  [org.mindrot/jbcrypt "0.3m"]
-                 [clj-http "0.5.8"]
+                 [clj-http "0.6.3"]
                  [digest "1.4.2"]
                  [clj-time "0.4.4"]
                  [org.apache.commons/commons-lang3 "3.1"] ; Already a dependency but required explicitly
                  [org.clojure/data.zip "0.1.1"]
-                 [com.draines/postal "1.9.1"]
+                 [com.draines/postal "1.9.2"]
                  [org.clojure/data.xml "0.0.6"]
                  [fi.sito/oskari "0.9.5"]]
   :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
-                                  [clj-webdriver "0.6.0-beta2" :exclusions [cheshire/cheshire]]]
+                                  [clj-webdriver "0.6.0-beta2" :exclusions [cheshire/cheshire]]
+                                  [incanter "1.4.1"]]
                    :plugins [[lein-midje "2.0.1"]
                              [lein-buildid "0.1.0"]
                              [lein-nitpicker "0.3.0"]]}
