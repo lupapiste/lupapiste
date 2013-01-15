452f7d9aad04df518ee19bf5ee904c7c312ac80f
jarppe <jarppe@metosin.fi>
2013-01-15 14:02:18 +0200

    Mooooore data...

--- src/lupapalvelu/operations.clj
+++ src/lupapalvelu/operations.clj
@@ -1,6 +1,16 @@
 (ns lupapalvelu.operations)
 
 (defn operations [municipality]
-  [["Uusi rakennus" [["Omakotitalo" "okt"]
-                     ["Rivitalo" "rvt"]]]
-   ["Purkaminen" "pura"]])
+  [["Rakentaminen ja purkaminen" [["Uuden rakennuksen rakentaminen" [["Asuinrakennus" "a"]
+                                                                     ["Vapaa-ajan asuinrakennus" "v"]
+                                                                     ["Varasto, sauna, autotalli tai muu talousrakennus" "v"]
+                                                                     ["Julkinen rakennus" "j"]
+                                                                     ["Muu" "m"]]]
+                                  ["Rakennuksen laajentaminen tai muuttaminen" [["Rakennuksen laajentaminen tai korjaaminen" "k"]
+                                                                                ["Käyttötarkoituksen muutos, esim loma-asunnon muuttaminen pysyvään käyttöön" "l"]
+                                                                                ["Rakennuksen julkisivun tai katon materiaalin, värin tai muodon muuttaminen" "x"]
+                                                                                ["Asuinhuoneiston jakaminen tai yhdistäminen" "y"]
+                                                                                ["Märkätilan laajentaminen" "z"]
+                                                                                ["Takan ja savuhormin rakentaminen" "a"]
+                                                                                ["Parvekkeen tai terassin lasittaminen" "b"]
+                                                                                ["Muu" "c"]]]] ]])
