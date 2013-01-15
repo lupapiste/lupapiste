5dd16d7bec044fed0509f1ba7bb0ae6171a33391
jarppe <jarppe@metosin.fi>
2013-01-15 13:41:48 +0200

    Actual tree data from back.

--- src/lupapalvelu/operations.clj
+++ src/lupapalvelu/operations.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.operations)
 
 (defn operations [municipality]
-  [])
-
+  [["Uusi rakennus" [["Omakotitalo" "okt"]
+                     ["Rivitalo" "rvt"]]]
+   ["Purkaminen" "pura"]])
