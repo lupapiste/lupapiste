2e104e6eb45eb5996db01968e2bd0b034bf4e3ef
Tommi Reiman <tommi.reiman@metosin.fi>
2013-01-17 15:00:32 +0200

    remove-many

--- src/lupapalvelu/mongo.clj
+++ src/lupapalvelu/mongo.clj
@@ -86,6 +86,10 @@
   [collection conditions document & {:keys [fields sort remove upsert] :or {fields nil sort nil remove false upsert false}}]
   (mc/find-and-modify collection conditions document :return-new true :upsert upsert :remove remove :sort sort :fields fields))
 
+(defn remove-many
+  "Returns all documents matching query."
+  [collection query] (mc/remove collection query))
+
 (defn set-file-id [^GridFSInputFile input ^String id]
   (.setId input id)
   input)
