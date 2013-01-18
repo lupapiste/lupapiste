7e35f50ed176f3fa222bf306446952164837c33b
Tommi Reiman <tommi.reiman@metosin.fi>
2013-01-18 10:47:44 +0200

    core-tests

---
+++ test/sade/core_test.clj
@@ -0,0 +1,24 @@
+(ns sade.core-test
+  (:use clojure.test
+        midje.sweet
+        sade.core))
+
+(facts
+  (ok)                 => {:ok true}
+  (ok :id 1)           => {:ok true :id 1}
+  (ok :id 1 :extra)    => (throws Exception)
+  (ok :text "success") => {:ok true :text "success"})
+
+(facts
+  (fail :kosh)               => {:ok false :text "kosh"}
+  (fail "kosh")              => {:ok false :text "kosh"}
+  (fail "kosh" "er")         => {:ok false :text "kosh"}
+  (fail "kosh%s" "er")       => {:ok false :text "kosher"}
+  (fail "kosh%s%s" "er" "!") => {:ok false :text "kosher!"}
+  (fail "kosh%s%s" "er")     => (throws Exception))
+
+(facts
+  (ok? {:ok true})   => true
+  (ok? {:ok "true"}) => false
+  (ok? {})           => false
+  (ok? {:ok false})  => false)
