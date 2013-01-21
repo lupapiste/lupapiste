22c8c0dc78d92ad264c641acb17315e213145bbb
Tommi Reiman <tommi.reiman@metosin.fi>
2013-01-21 19:10:00 +0200

    remove keys from edn

--- src/lupapalvelu/xml/krysp/reader.clj
+++ src/lupapalvelu/xml/krysp/reader.clj
@@ -1,12 +1,15 @@
 (ns lupapalvelu.xml.krysp.reader
   (:use sade.xml)
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :refer [split]]
+            [clojure.walk :refer [postwalk]]))
 
-(defn logica [id]
+(defn strip-keys [m]
+  (let [f (fn [[k v]] (if (keyword? k) [(-> k name (split #":") last keyword) v] [k v]))]
+    (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+
+(defn building-info [id]
   (let [url (str "http://212.213.116.162/geoserver/wfs?request=GetFeature&typeName=rakval%3AValmisRakennus&outputFormat=KRYSP&filter=%3CPropertyIsEqualTo%3E%3CPropertyName%3Erakval:rakennustieto/rakval:Rakennus/rakval:rakennuksenTiedot/rakval:rakennustunnus/rakval:kiinttun%3C/PropertyName%3E%3CLiteral%3E" id "%3C/Literal%3E%3C/PropertyIsEqualTo%3E")]
-    (-> url parse xml->edn)))
-
-(defn- strip-ns [s] (string/replace s #"<\S*?:" "<"))
+    (-> url parse xml->edn strip-keys)))
 
 #_(-> "./dev-resources/public/krysp/building.xml" slurp parse xml->edn)
 
