48ce2f9e5db66337f635fb7cb22d7c5e5fa61ae5
Tommi Reiman <tommi.reiman@metosin.fi>
2013-01-21 19:10:13 +0200

    facts about stripping-keys

---
+++ test/lupapalvelu/xml/krysp/reader_test.clj
@@ -0,0 +1,13 @@
+(ns lupapalvelu.xml.krysp.reader-test
+  (:use [lupapalvelu.xml.krysp.reader]
+        [midje.sweet]))
+
+(facts
+
+  (fact "takes (recursively) last sub-keyword from keywords"
+        (strip-keys {:a:b:c {:x:y:z 2 :q:w:y 3}}) => {:c {:z 2 :y 3}})
+
+  (fact "does not touch other key types"
+        (strip-keys {[:a :b :c] 1
+                     ":a:b:c" 2}) => {[:a :b :c] 1
+                                      ":a:b:c" 2}))
