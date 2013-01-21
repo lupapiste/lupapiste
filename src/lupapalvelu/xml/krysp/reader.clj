f5431a6cdc59885d73acdc6ef495f85e3e6c334d
Tommi Reiman <tommi.reiman@metosin.fi>
2013-01-21 19:23:08 +0200

    ...

--- src/lupapalvelu/xml/krysp/reader.clj
+++ src/lupapalvelu/xml/krysp/reader.clj
@@ -10,4 +10,4 @@
   (let [url (str "http://212.213.116.162/geoserver/wfs?request=GetFeature&typeName=rakval%3AValmisRakennus&outputFormat=KRYSP&filter=%3CPropertyIsEqualTo%3E%3CPropertyName%3Erakval:rakennustieto/rakval:Rakennus/rakval:rakennuksenTiedot/rakval:rakennustunnus/rakval:kiinttun%3C/PropertyName%3E%3CLiteral%3E" id "%3C/Literal%3E%3C/PropertyIsEqualTo%3E")]
     (-> url parse xml->edn strip-keys)))
 
-(-> "./dev-resources/public/krysp/building.xml" slurp parse xml->edn strip-keys)
+#_(-> "./dev-resources/public/krysp/building.xml" slurp parse xml->edn strip-keys)
