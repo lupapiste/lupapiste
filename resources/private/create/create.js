37b1a2334a224643a6aa8e38627a74acbe4ffe94
jarppe <jarppe@metosin.fi>
2013-01-25 17:56:26 +0200

    WIP...

--- resources/private/create/create.js
+++ resources/private/create/create.js
@@ -1,48 +1,40 @@
 ;(function() {
   "use strict";
 
-  var addressModel = address.createModel(function() { window.location.hash = "#!/create-inforequest2"; });
 
-  var part2Model = new function() {
+  var model = new function() {
     var self = this;
 
+    self.search = address.createModel();
+
+    self.address = ko.observable();
+    
     self.operation = ko.observable();
     self.message = ko.observable("");
-    self.municipality = ko.observable();
-    self.municipalityCode = ko.observable();
     self.links = ko.observableArray();
     self.operations = ko.observable(null);
     self.requestType = ko.observable();
-
+    
+    self.showPart2 = ko.observable(false);
+    
     self.showInfoRequestMessage = ko.observable(false);
     self.showCreateButtons = ko.computed(function() { return self.operation() && !self.showInfoRequestMessage(); });
 
     self.clear = function() {
+      self.search.clear()
       return self
         .operation(null)
         .message("")
-        .municipality("")
-        .municipalityCode(null)
         .requestType(null)
         .showInfoRequestMessage(false);
     };
 
-    self.municipalityCode.subscribe(function(v) {
+    self.search.municipalityCode.subscribe(function(v) {
       self.operations(null).links.removeAll();
       if (!v || v.length === 0) return;
       ajax
         .query("municipality", {municipality: v})
-        .success(function (data) {
-          if (self.municipalityCode() === v) {
-            self.operations(data.operations).links(data.links);
-          }
-        })
-        .fail(function() {
-          if (self.municipalityCode() === v) {
-            // FIXME
-            error("OH-NOES!");
-          }
-        })
+        .success(function (data) { if (self.municipalityCode() === v) self.operations(data.operations).links(data.links); })
         .call();
     });
 
@@ -50,15 +42,15 @@
 
     self.create = function(infoRequest) {
       ajax.command("create-application", {
-        permitType: 'infoRequest',
+        permitType: infoRequest ? "infoRequest" : "buildingPermit", // FIXME: WTF this should be?
         infoRequest: infoRequest,
         operation: self.operation()["op"],
-        y: addressModel.y(),
-        x: addressModel.x(),
-        address: addressModel.address(),
-        propertyId: addressModel.propertyId(),
+        y: self.search.y(),
+        x: self.search.x(),
+        address: self.search.address(),
+        propertyId: self.search.propertyId(),
         message: self.message(),
-        municipality: self.municipalityCode()
+        municipality: self.search.municipalityCode()
       })
       .success(function(data) {
         repository.reloadApplication(data.id);
@@ -71,9 +63,6 @@
     self.createInfoRequest = self.create.bind(self, true);
 
   };
-  
-  addressModel.municipality.subscribe(part2Model.municipality);
-  addressModel.municipalityCode.subscribe(part2Model.municipalityCode);
 
   function toLink(l) {
     return "<li><a href='" + l.url + "' target='_blank'>" + l.nameFin + "</li>";
@@ -84,33 +73,31 @@
     e.setAttribute("class", "tree-result");
     $(e).html(
         "<p>" + value.text + "</p>" +
-        "<ul>" + _.map(part2Model.links(), toLink).join("") + "</ul>");
+        "<ul>" + _.map(model.links(), toLink).join("") + "</ul>");
     return e;
   }
 
-  hub.onPageChange("create", function() {
-    addressModel.clear();
-    part2Model.clear();
-  });
+  hub.onPageChange("create", model.clear);
   
   $(function() {
 
-    addressModel.setMap(gis.makeMap("create-inforequest-map").center(404168, 6840000, 1));
-    ko.applyBindings(addressModel, $("#create")[0]);
+    model.search.setMap(gis.makeMap("create-inforequest-map").center(404168, 6840000, 1));
+    ko.applyBindings(model, $("#create")[0]);
+    
     $("#create-inforequest-search").autocomplete({
       serviceUrl:      "/proxy/find-address",
       deferRequestBy:  500,
       noCache:         true,
-      onSelect:        function(value, data) { addressModel.setAddressData(data); }
+      onSelect:        function(value, data) { model.search.setAddressData(data); }
     });
 
     var tree = selectionTree.create(
         $("#create-inforequest2 .tree-content"),
         $("#create-inforequest2 .tree-breadcrumbs"),
-        part2Model.operation,
+        model.operation,
         generateInfo);
-    part2Model.operations.subscribe(tree.reset);
-    ko.applyBindings(part2Model, $("#create-inforequest2")[0]);
+    model.operations.subscribe(tree.reset);
+    
   });
 
 })();
