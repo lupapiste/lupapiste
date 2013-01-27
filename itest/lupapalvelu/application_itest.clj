2f4b1ac56513efed09a77be11a458b3f1f528c66
jarppe <jarppe@metosin.fi>
2013-01-27 18:01:42 +0200

    Cleanup.

--- itest/lupapalvelu/application_itest.clj
+++ itest/lupapalvelu/application_itest.clj
@@ -6,7 +6,6 @@
             [lupapalvelu.document.schemas :as schemas]))
 
 (fact "creating application without message"
-  (apply-remote-minimal)
   (let [resp            (command pena :create-application
                                  :permitType "buildingPermit"
                                  :operation "asuinrakennus"
@@ -87,36 +86,38 @@
     (count roles-before-assignation) => 1
     (count roles-in-the-end) => 1))
 
-#_(fact "Assert that proper documents are created"
-  (against-background (operations/operations :foo) => {:schema "foo" :required ["a" "b"] :attachments []}
-                      (operations/operations :bar) => {:schema "bar" :required ["b" "c"] :attachments []}
-                      (schemas/schemas "hakija") => {:info {:name "hakija"}, :body []}
-                      (schemas/schemas "foo")    => {:info {:name "foo"}, :body []}
-                      (schemas/schemas "a")      => {:info {:name "a"}, :body []}
-                      (schemas/schemas "b")      => {:info {:name "b"}, :body []}
-                      (schemas/schemas "bar")    => {:info {:name "bar"}, :body []}
-                      (schemas/schemas "c")      => {:info {:name "c"}, :body []})
-  (let [id (:id (command pena :create-application
-                         :operation "foo"
-                         :permitType "buildingPermit"
-                         :propertyId "1"
-                         :x 444444 :y 6666666
-                         :address "foo 42, bar"
-                         :municipality "753"
-                         :message "hello"))
-        app (:application (query pena :application :id id))
-        docs (:documents app)
-        find-by-schema? (fn [docs schema-name] (some (fn [doc] (if (= schema-name (-> doc :schema :info :name)) doc)) docs))]
-    (count docs) => 4 ; foo, a, b and "hakija".
-    (find-by-schema? docs "foo") => truthy
-    (find-by-schema? docs "a") => truthy
-    (find-by-schema? docs "b") => truthy
-    (-> (find-by-schema? docs "foo") :schema :info) => (contains {:op "foo" :removable true}) 
-    ; Add operation:
-    (command pena :add-operation :id id :operation "bar")
-    (let [app (:application (query pena :application :id id))
-          docs (:documents app)]
-      (count docs) => 6 ; foo, a, b and "hakija" + bar and c
-      (find-by-schema? docs "bar") => truthy
-      (find-by-schema? docs "c") => truthy
-      (-> (find-by-schema? docs "bar") :schema :info) => (contains {:op "bar" :removable true}))))
+(comment
+  ; Should rewrite this as a couple of unit tests
+  (fact "Assert that proper documents are created"
+    (against-background (operations/operations :foo) => {:schema "foo" :required ["a" "b"] :attachments []}
+                        (operations/operations :bar) => {:schema "bar" :required ["b" "c"] :attachments []}
+                        (schemas/schemas "hakija") => {:info {:name "hakija"}, :body []}
+                        (schemas/schemas "foo")    => {:info {:name "foo"}, :body []}
+                        (schemas/schemas "a")      => {:info {:name "a"}, :body []}
+                        (schemas/schemas "b")      => {:info {:name "b"}, :body []}
+                        (schemas/schemas "bar")    => {:info {:name "bar"}, :body []}
+                        (schemas/schemas "c")      => {:info {:name "c"}, :body []})
+    (let [id (:id (command pena :create-application
+                           :operation "foo"
+                           :permitType "buildingPermit"
+                           :propertyId "1"
+                           :x 444444 :y 6666666
+                           :address "foo 42, bar"
+                           :municipality "753"
+                           :message "hello"))
+          app (:application (query pena :application :id id))
+          docs (:documents app)
+          find-by-schema? (fn [docs schema-name] (some (fn [doc] (if (= schema-name (-> doc :schema :info :name)) doc)) docs))]
+      (count docs) => 4 ; foo, a, b and "hakija".
+      (find-by-schema? docs "foo") => truthy
+      (find-by-schema? docs "a") => truthy
+      (find-by-schema? docs "b") => truthy
+      (-> (find-by-schema? docs "foo") :schema :info) => (contains {:op "foo" :removable true}) 
+      ; Add operation:
+      (command pena :add-operation :id id :operation "bar")
+      (let [app (:application (query pena :application :id id))
+            docs (:documents app)]
+        (count docs) => 6 ; foo, a, b and "hakija" + bar and c
+        (find-by-schema? docs "bar") => truthy
+        (find-by-schema? docs "c") => truthy
+        (-> (find-by-schema? docs "bar") :schema :info) => (contains {:op "bar" :removable true})))))
