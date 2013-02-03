375913654f18b37153bdc0076414f1a67d9470d4
jarppe <jarppe@metosin.fi>
2013-02-03 12:12:58 +0200

    Type=hint to avoid reflection.

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
              :stest {:test-paths ^:replace ["stest"]}
              :lupadev {:jvm-opts ["-Dtarget_server=http://lupadev.solita.fi"]}
              :lupatest {:jvm-opts ["-Dtarget_server=http://lupatest.solita.fi"]}}
-
+  ::warn-on-reflection true
   :nitpicker {:exts ["clj" "js" "html"]
               :excludes [#"\/jquery" #"\/theme\/default\/" #"\/public\/lib\/" #"openlayers"]}
 
