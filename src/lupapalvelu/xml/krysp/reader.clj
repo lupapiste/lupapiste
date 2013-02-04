ddf82ed4e0baf4ea743bd5dd2a3850db1129870e
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-04 10:54:39 +0200

    reorder

--- src/lupapalvelu/xml/krysp/reader.clj
+++ src/lupapalvelu/xml/krysp/reader.clj
@@ -147,17 +147,17 @@
          :rakennuksenOmistajat (->>
                                  (select rakennus [:rakval:omistaja])
                                  (map ->rakennuksen-omistaja))
-         :osoite {:postinumero      nil
-                  :huoneisto        nil
+         :osoite {:kunta            (-> rakennus (select1 [:yht:kunta]) text)
+                  :lahiosoite       nil
                   :osoitenumero     (-> rakennus (select1 [:yht:osoitenumero]) text)
                   :osoitenumero2    (-> rakennus (select1 [:yht:osoitenumero2]) text)
-                  :kunta            (-> rakennus (select1 [:yht:kunta]) text)
-                  :lahiosoite       nil
-                  :postitoimipaikka nil
                   :jakokirjain      (-> rakennus (select1 [:yht:jakokirjain]) text)
                   :jakokirjain2     (-> rakennus (select1 [:yht:jakokirjain2]) text)
-                  :pistesijanti     nil
-                  :porras           nil}}
+                  :porras           nil
+                  :huoneisto        nil
+                  :postinumero      nil
+                  :postitoimipaikka nil
+                  :pistesijanti     nil}
          :kaytto {:kayttotarkoitus (-> rakennus (select1 [:rakval:kayttotarkoitus]) text)
                   :rakentajaTyyppi (-> rakennus (select1 [:rakval:rakentajaTyyppi]) text)}
          :luokitus {:energialuokka (-> rakennus (select1 [:rakval:energialuokka]) text)
