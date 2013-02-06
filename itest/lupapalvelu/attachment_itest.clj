1a7e10cca7e19ecb9c3b7cf7491466ec50199bc6
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-02-06 12:15:23 +0200

    Make sure the test stays valid by create the application in Veikko's municipality

--- itest/lupapalvelu/attachment_itest.clj
+++ itest/lupapalvelu/attachment_itest.clj
@@ -4,7 +4,13 @@
         [midje.sweet]))
 
 (fact
-  (let [resp            (command pena :create-application :permitType "buildingPermit" :operation "asuinrakennus" :propertyId "1" :x 408048 :y 6693225 :address "foo 42, bar" :municipality "753")
+  (let [resp            (command pena :create-application
+                                 :permitType "buildingPermit"
+                                 :operation "asuinrakennus"
+                                 :propertyId "1"
+                                 :x 408048 :y 6693225
+                                 :address "foo 42, bar"
+                                 :municipality veikko-muni)
         application-id  (:id resp)
         application     (:application (query pena :application :id application-id))
         resp            (command veikko :create-attachment :id application-id :attachmentType {:type-group "tg" :type-id "tid"})
