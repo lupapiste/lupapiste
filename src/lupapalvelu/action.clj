b83893408096c5d86897bf9955e76e0b1410e9df
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-07 17:48:40 +0200

    update-path for user2henkilo depends on the document. FIXME.

--- src/lupapalvelu/action.clj
+++ src/lupapalvelu/action.clj
@@ -177,11 +177,12 @@
             schema         (get schemas/schemas schema-name)]
         (if (nil? document)
           (fail :error.document-not-found)
-          (do
+          ;; FIXME: all users should be modelled the same way.
+          (let [path (if (= "hakija" schema-name) :documents.$.body.hakija :documents.$.body)]
             (info "merging user %s with best effort into document %s" user name)
             (mongo/update
               :applications
               {:_id (:id application)
                :documents {$elemMatch {:id documentId}}}
-              {$set {:documents.$.body (domain/user2henkilo user)
+              {$set {path (domain/user2henkilo user)
                      :modified (:created command)}})))))))
