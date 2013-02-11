f856e8f92c8bd5992dca639c41443c0693782617
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-11 10:42:15 +0200

    get-users-in-application

--- src/lupapalvelu/application.clj
+++ src/lupapalvelu/application.clj
@@ -263,6 +263,14 @@
                                               $pushAll {:attachments (make-attachments created op)}})
         (ok)))))
 
+(defquery "get-users-in-application"
+  {:parameters [:id]
+   :roles-in   [:applicant :authority]}
+  [query]
+  (with-application query
+    (fn [{:keys [auth]}]
+      (ok :users auth))))
+
 ;;
 ;; krysp enrichment
 ;;
