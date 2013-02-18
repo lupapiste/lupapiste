f265d348aaea55b2561c88ba60fa4203e6644f93
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-18 21:09:10 +0200

    don't debug composing pages

--- src/lupapalvelu/singlepage.clj
+++ src/lupapalvelu/singlepage.clj
@@ -88,7 +88,7 @@
     (.toByteArray out)))
 
 (defn compose [kind component]
-  (debugf "Compose %s%s" component kind)
+  (tracef "Compose %s%s" component kind)
   (ByteArrayInputStream.
     (if (= :html kind)
       (compose-html component)
