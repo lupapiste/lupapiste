fb9bf49d49417f5328caf5301485965324b105da
jarppe <jarppe@metosin.fi>
2013-02-19 09:57:13 +0200

    refactored and added throttling.

--- src/lupapalvelu/perf_mon.clj
+++ src/lupapalvelu/perf_mon.clj
@@ -1,9 +1,22 @@
 (ns lupapalvelu.perf-mon
-  (:require [clojure.string :refer [join]]))
+  (:use [clojure.tools.logging]
+        [noir.core :only [defpage]]
+        [monger.operators])
+  (:require [clojure.string :refer [join]]
+            [clojure.java.io :as io]
+            [monger.collection :as mc]
+            [noir.response :as resp]
+            [noir.request :as request]
+            [noir.server :as server]
+            [cheshire.core :as json])
+  (:import [com.mongodb WriteConcern]))
+
+(defonce db-throttle (atom 0))
+(defonce web-throttle (atom 0))
 
 (def ^:dynamic *perf-context* nil)
 
-(defn- wrap-perf [f f-name]
+(defn wrap-perf-mon [f f-name]
   (fn [& args]
     (if-let [context *perf-context*]
       (let [sub-context (atom [])]
@@ -13,26 +26,103 @@
               (apply f args)
               (finally
                 (let [end (System/nanoTime)]
-                  (swap! context conj [f-name args (- end start) @sub-context])))))))
+                  (swap! context conj [f-name (- end start) @sub-context])))))))
       (apply f args))))
 
-(defn instrument [v]
-  (alter-var-root v wrap-perf (str (. v ns) \/ (. v sym))))
+(defn wrap-throttle [f f-name]
+  (fn [& args]
+    (Thread/sleep @db-throttle)
+    (apply f args)))
 
-(defn instrument-ns [& namespaces]
+(defn instrument [f v]
+  (alter-var-root v f (str (. v ns) \/ (. v sym))))
+
+(defn instrument-ns [f & namespaces]
   (doseq [n namespaces
           v (filter (comp fn? deref) (vals (ns-publics n)))]
-    (instrument v)))
+    (instrument f v)))
 
-(defmacro with-perf [result-fn & body]
-  `(binding [*perf-context* (atom [])]
-     (try
-       (do ~@body)
-       (finally
-         (~result-fn @*perf-context*)))))
+(defn perf-mon-middleware [handler]
+  (fn [request]
+    (if (get-in request [:params :nopm])
+      (handler request)
+      (binding [*perf-context* (atom [])]
+        (Thread/sleep @db-throttle)
+        (let [start (System/nanoTime)] 
+          (try
+            (handler request)
+            (finally
+              (let [end (System/nanoTime)]
+                (mc/insert "perf-mon" 
+                           {:ts (System/currentTimeMillis)
+                            :duration (- end start)
+                            :uri (get request :uri)
+                            :user (get-in request [:session :noir :user :username])
+                            :perfmon @*perf-context*}
+                           WriteConcern/NONE)))))))))
 
+(defn get-data [start end]
+  (map (fn [row] (dissoc row :_id))
+       (mc/find-maps "perf-mon" {$and [{:ts {$gte start}}
+                                       {:ts {$lte end}}]})))
+
+(defn- to-long [v]
+  (when v
+    (if (string? v) (Long/parseLong v) (long v))))
+
+(defn- group-by-uri [data]
+  (with-logs "lupapiste"
+    (clojure.pprint/pprint data))
+  data)
+
+(defn- find-min-max-avg [{:keys [min-val max-val avg cnt] :as m} {duration :duration perfmon :perfmon}]
+  (assoc m :min-val (min min-val duration)
+           :max-val (max max-val duration)
+           :avg (double (/ (+ (* avg cnt) duration) (inc cnt)))
+           :cnt (inc cnt)
+           :db-cnt (count perfmon)))
+
+(defn- find-min-max-avg* [[k v]]
+  [k (reduce find-min-max-avg {:min-val Double/MAX_VALUE :max-val Double/MIN_VALUE :avg 0.0 :cnt 0} v)])
+
+(defpage "/perfmon/data" {:keys [start end]}
+  (->> (get-data (or (to-long start) (- (System/currentTimeMillis) (* 5 60 1000)))
+                 (or (to-long end) (System/currentTimeMillis)))
+    (group-by :uri)
+    (map find-min-max-avg*)
+    (resp/json)
+    (resp/status 200)))
+
+(defpage [:get "/perfmon/throttle"] _
+  (resp/status 200 (str "db: " @db-throttle ", web: " @web-throttle)))
+
+(defpage [:get "/perfmon/throttle/:id"] {id :id}
+  (let [throttle ({"db" db-throttle "web" web-throttle} id)]
+    (if throttle
+      (resp/status 200 (str @throttle))
+      (resp/status 404 (str "unknown throttle: '" id "'")))))
+
+(defpage [:post "/perfmon/throttle/:id"] {id :id}
+  (let [throttle ({"db" db-throttle "web" web-throttle} id)
+        value (-> (request/ring-request) :body io/reader json/parse-stream (get "value") str Long/parseLong)]
+    (if throttle
+      (do
+        (reset! throttle value)
+        (resp/status 200 (str "ok: " id " -> " value)))
+      (resp/status 404 (str "unknown throttle: '" id "'")))))
+
+(defn init []
+  (server/add-middleware perf-mon-middleware)
+  (instrument-ns wrap-throttle 'lupapalvelu.mongo)
+  (instrument-ns wrap-perf-mon 'lupapalvelu.mongo))
+
+;;
+;; Here be dragons...
+;;
 
 (comment
+  (mc/remove "perf-mon")
+
   ; Prints call hierarchy:
   (defn perf-logger
     ([context]
