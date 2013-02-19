fd46529166c6b31ab1bf0e4b22fdd2c3c7143980
jarppe <jarppe@metosin.fi>
2013-02-19 13:57:20 +0200

    Load initial throttling values at start, use throttling by-pass.

--- resources/private/common/debug.js
+++ resources/private/common/debug.js
@@ -4,6 +4,7 @@ $(function() {
   function applyMinimal(e) {
     ajax.get(window.location.protocol + "//" + window.location.host + "/api/query/apply-fixture")
       .param("name", "minimal")
+      .param("npm", "true")
       .success(function() { $(e.target).parent().find("span").text(" DONE!").show().delay(1000).fadeOut(); })
       .call();
     return false;
@@ -11,15 +12,19 @@ $(function() {
   
   function throttle(type, e) {
     var t = $(e.target);
-    window.t = t;
-    t.parent().find("b.dev-throttle-" + type).text(t.val());
+    var value = t.val();
+    ajax.post(window.location.protocol + "//" + window.location.host + "/perfmon/throttle/" + type)
+      .json({value: value})
+      .header("npm", "true")
+      .success(function() { t.parent().find("b.dev-throttle-" + type).text(value); })
+      .call();
   }
   
   $("footer")
     .append($("<div>").addClass("dev-debug")
-    .append($("<h3>")
+      .append($("<h3>")
         .append($("<a>").attr("href", "#").text("Development").click(function() { $("footer .dev-debug div").slideToggle(); return false; })))
-    .append($("<div>")
+      .append($("<div>")
         .append($("<input type='checkbox' checked='checked'>").click(function() { $(".todo").toggleClass("todo-off"); }))
         .append($("<label>").text("Unfinished"))
         .append($("<br>"))
@@ -29,11 +34,21 @@ $(function() {
         .append($("<a>").attr("href", "#").text("Apply minimal!").click(applyMinimal))
         .append($("<span>").css("font-weight", "bold").hide())
         .append($("<br>"))
-        .append($("<span>").text("Throttle web: ")).append($("<b>").addClass("dev-throttle-web").text("0"))
-        .append($("<input type='range' value='0' min='0' max='2000' step='20'>").change(_.partial(throttle, "web")))
+        .append($("<span>").text("Throttle web: "))
+        .append($("<b>").addClass("dev-throttle-web").text("0"))
+        .append($("<input type='range' value='0' min='0' max='2000' step='20'>").change(_.throttle(_.partial(throttle, "web"), 500)))
         .append($("<br>"))
-        .append($("<span>").text("Throttle DB: ")).append($("<b>").addClass("dev-throttle-db").text("0"))
-        .append($("<input type='range' value='0' min='0' max='2000' step='20'>").change(_.partial(throttle, "db")))));
+        .append($("<span>").text("Throttle DB: "))
+        .append($("<b>").addClass("dev-throttle-db").text("0"))
+        .append($("<input type='range' value='0' min='0' max='2000' step='20'>").change(_.throttle(_.partial(throttle, "db"), 500)))));
+  
+  ajax.get(window.location.protocol + "//" + window.location.host + "/perfmon/throttle")
+    .success(function(data) {
+      var ranges = $("footer .dev-debug input[type='range']");
+      $(ranges[0]).val(data.web).change();
+      $(ranges[1]).val(data.db).change();
+    })
+    .call();
   
   // Helper function to execute xpath queries. Useful for testing xpath declarations in robot files.
   window.xpath = function(p) { return document.evaluate(p, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; };
