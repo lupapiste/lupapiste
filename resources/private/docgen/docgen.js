d7c1d66e71d5a258e82e853cac76c217edbe17fe
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-20 15:06:29 +0200

    don't show empty names

--- resources/private/docgen/docgen.js
+++ resources/private/docgen/docgen.js
@@ -323,14 +323,17 @@ LUPAPISTE.DocModel = function(spec, model, saveCallback, removeCallback, docId, 
       .command("get-users-in-application", {id: appId})
       .success(function(data) {
         $.each(data.users, function (i, user) {
-          var option = document.createElement("option");
-          var value = user.id;
-          option.value = value;
-          option.appendChild(document.createTextNode(user.firstName+" "+user.lastName));
-          if (selectedOption === value) {
-            option.selected = "selected";
+          // LUPA-89: don't print fully empty names
+          if(user.firstName && user.lastName) {
+            var option = document.createElement("option");
+            var value = user.id;
+            option.value = value;
+            option.appendChild(document.createTextNode(user.firstName+" "+user.lastName));
+            if (selectedOption === value) {
+              option.selected = "selected";
+            }
+            select.appendChild(option);
           }
-          select.appendChild(option);
         });
       })
       .call();
