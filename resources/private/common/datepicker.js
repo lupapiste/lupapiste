495f574f9261106d5a8c885b16d787cbd73809a3
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-25 11:58:36 +0200

    buttonimage

--- resources/private/common/datepicker.js
+++ resources/private/common/datepicker.js
@@ -14,6 +14,7 @@ jQuery(function($){
       dateFormat: 'dd.mm.yy',
       firstDay: 1,
       isRTL: false,
+      buttonImage: '/img/calendar.gif',
       showMonthAfterYear: false,
       yearSuffix: ''};
   $.datepicker.setDefaults($.datepicker.regional['fi']);
