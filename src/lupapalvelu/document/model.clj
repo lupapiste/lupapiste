e77055a516c61cb415ffd4d4124096b344dd399b
Tommi Reiman <tommi.reiman@metosin.fi>
2013-02-25 12:41:16 +0200

    validate dates on server

--- src/lupapalvelu/document/model.clj
+++ src/lupapalvelu/document/model.clj
@@ -4,6 +4,7 @@
         [lupapalvelu.document.schemas :only [schemas]]
         [clojure.walk :only [keywordize-keys]])
   (:require [clojure.string :as s]
+            [clj-time.format :as timeformat]
             [lupapalvelu.document.subtype :as subtype]))
 
 ;;
@@ -33,10 +34,14 @@
 (defmethod validate :boolean [_ v]
   (if (not= (type v) Boolean) [:err "illegal-value:not-a-boolean"]))
 
-;; FIXME
+
+(def dd-mm-yyyy (timeformat/formatter "dd-MM-YYYY"))
+
 (defmethod validate :date [elem v]
-  (info "validated :date " elem v)
-  nil)
+  (try
+    (timeformat/parse dd-mm-yyyy v)
+    nil
+    (catch Exception e [:warn "invalid-date-format"])))
 
 ;; FIXME
 (defmethod validate :checkbox [elem v]
