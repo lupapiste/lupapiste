474ed1f3039f105d920a40509c8e8ef99a4d49e3
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-02-28 21:26:29 +0200

    Added tests

--- itest/lupapalvelu/csrf_itest.clj
+++ itest/lupapalvelu/csrf_itest.clj
@@ -10,9 +10,20 @@
 (fact "CSRF check must not be bypassed if there is no apikey"
       (invalid-csrf-token (query nil :allowed-actions)) => true)
 
-(fact "Calling non-protected resources returns a csrf token"
+(fact "Calling a non-protected resource returns a csrf token"
       (let [resp (c/get (str (server-address) "/"))
             cookie (get-in resp [:cookies "lupapiste-token"])]
         (:status resp) => 200
         cookie => truthy
         (:path cookie) => "/"))
+
+(fact "Sending the cookie and a header passes CSRF protection"
+      (let [resp (c/get (str (server-address) "/api/query/allowed-actions")
+                        {:cookies {"lupapiste-token" {:value "my-token"}}
+                         :headers {"x-anti-forgery-token" "my-token"}})]
+        (success (decode-response resp)) => true))
+
+(fact "Failing to send the header fails CSRF check"
+      (let [resp (c/get (str (server-address) "/api/query/allowed-actions")
+                        {:cookies {"lupapiste-token" {:value "my-token"}}})]
+        (invalid-csrf-token (decode-response resp)) => true))
