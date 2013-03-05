c1352e9b087e504ef15d77bec90a7864a6361476
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-03-05 14:35:07 +0200

    Index municipality code

--- src/lupapalvelu/mongo.clj
+++ src/lupapalvelu/mongo.clj
@@ -138,12 +138,13 @@
   (mc/ensure-index :users {:email 1} {:unique true})
   (mc/ensure-index :users {:municipality 1} {:sparse true})
   (mc/ensure-index :users {:private.apikey 1} {:unique true :sparse true})
+  #_(mc/ensure-index "users" {:personId 1} {:unique true})
   (mc/ensure-index :applications {:municipality 1})
   (mc/ensure-index :applications {:auth.id 1})
   (mc/ensure-index :applications {:auth.invite.user.id 1} {:sparse true})
   (mc/ensure-index :activations {:created-at 1} {:expireAfterSeconds (* 60 60 24 7)})
   (mc/ensure-index :vetuma {:created-at 1} {:expireAfterSeconds (* 60 30)})
-  #_(mc/ensure-index "users" {:personId 1} {:unique true}))
+  (mc/ensure-index :municipalities {:municipalityCode 1}))
 
 (defn clear! []
   (warn "Clearing MongoDB:" mongouri)
