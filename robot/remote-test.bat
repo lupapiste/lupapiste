f2fc94e85088ee9c1fab1221c009b01ecdad8a5d
Tommi Reiman <tommi.reiman@metosin.fi>
2013-03-08 15:14:18 +0200

    new test server

--- robot/remote-test.bat
+++ robot/remote-test.bat
@@ -2,6 +2,6 @@
 @if [%target%]==[] set target=tests
 
 @set server=%2
-@if [%server%]==[] set server=lupatest.solita.fi
+@if [%server%]==[] set server=test.lupapiste.fi
 
 pybot -d target --variable BROWSER:firefox --variable SELENIUM:http://192.168.7.223:4444/wd/hub --variable SERVER:http://%server% %target%
