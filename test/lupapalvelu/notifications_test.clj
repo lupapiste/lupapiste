58f2dd281a529ac89976923dbb8139f478ec263b
Timo Lehtonen <timo.lehtonen@solita.fi>
2013-03-11 14:28:17 +0200

    simplified tests

--- test/lupapalvelu/notifications_test.clj
+++ test/lupapalvelu/notifications_test.clj
@@ -14,17 +14,8 @@
 (defn get-html-body [html]
   (re-find #"(?ms)<body>.*<\/body>" html))
 
-(fact "Email body for new comment is like"
-   (get-html-body (get-message-for-new-comment { :id 123 :permitType "application"} "http://localhost:8000")) => 
-"<body>
-  <p>Hei,</p>
-  <p>Uusi kommentti lisätty <a id=\"conversation-link-fi\" href=\"http://localhost:8000/fi/applicant#!/application/123/conversation\">keskusteluun</a>.</p>
-  <p>Yst\u00E4v\u00E4llisin terveisin, Lupapiste</p>
-  <hr />
-  <p>Hej,</p>
-  <p>Ett nytt komment lagt <a id=\"conversation-link-sv\" href=\"http://localhost:8000/sv/applicant#!/application/123/conversation\">till diskussionet</a>.</p>
-  <p>V\u00E4nliga h\u00E4lsningar, Lupapiste</p>
-</body>")
+(fact "Email for new comment contains link to application"
+   (re-find #"(?ms)\/fi\/applicant#!\/application\/123" (get-message-for-new-comment { :id 123 :permitType "application"} "http://localhost:8000")) => truthy)
 
 ; application opened
 (fact "When application is opened, each use in auth-array gets email."
@@ -34,16 +25,9 @@
              (mongo/by-id :users "c") => {:email "c@foo.com"}))
 
 (fact "Email for application open is like"
-   (get-html-body (get-message-for-application-state-change { :id 123 :state "open"} "http://localhost:8000")) => 
-"<body>
-  <p>Hei,</p>
-  <p><a href=\"http://localhost:8000/fi/applicant#!/application/123\" id=\"application-link-fi\">Hakemuksen</a> tila on nyt: <span id=\"state-fi\">Valmisteilla</span></p>
-  <p>Yst\u00E4v\u00E4llisin terveisin, Lupapiste</p>
-  <hr />
-  <p>Hej,</p>
-  <p>Tillståndet av <a href=\"http://localhost:8000/sv/applicant#!/application/123\" id=\"application-link-sv\">ansökan</a> förändrats till: <span id=\"state-sv\">Inför</span></p>
-  <p>V\u00E4nliga h\u00E4lsningar, Lupapiste</p>
-</body>")
+  (let [msg (get-html-body (get-message-for-application-state-change { :id 123 :state "open"} "http://localhost:8000"))]
+   (re-find #"(?ms)\/fi\/applicant#!\/application\/123" msg) => truthy
+   (re-find #"(?ms)Valmisteilla" msg) => truthy))
 
 (fact "Email for application submitted contains the state string."
    (re-find #"Vireill\u00E4" (get-message-for-application-state-change { :state "submitted"} "")) => truthy)
