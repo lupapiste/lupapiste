511d2d3766ba4bc681ecffe153aa7729538da2e0
Tommi Reiman <tommi.reiman@metosin.fi>
2013-03-20 09:27:03 +0200

    does not compute

--- project.clj
+++ project.clj
@@ -41,8 +41,8 @@
                                     :checksum :ignore}]]
   :plugin-repositories [["solita-archiva" {:url "http://mvn.solita.fi/archiva/repository/solita"
                                             :checksum :ignore}]]
-  :aliases {:integration ["with-profile" "dev,itest" "midje"]
-            :verify      ["with-profile" "dev,alltests" "do" "nitpicker," "midje"]}
+  :aliases {"integration" ["with-profile" "dev,itest" "midje"]
+            "verify"      ["with-profile" "dev,alltests" "do" "nitpicker," "midje"]}
   :main lupapalvelu.server
   :repl-options {:init-ns lupapalvelu.server}
   :min-lein-version "2.0.0")
