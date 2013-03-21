94119fe15c28009bfc8b984f9e0995dbd16c54a8
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-03-21 08:36:14 +0200

    Delegated email sending to sade.email

--- src/sade/email.clj
+++ src/sade/email.clj
@@ -1,20 +1,32 @@
 (ns sade.email
-  (:use sade.core)
-  (:require [postal.core :as postal]))
+  (:use [sade.core]
+        [clojure.tools.logging])
+  (:require [postal.core :as postal]
+            [sade.env :as env]
+            [sade.strings :as s]))
 
 (defn send-mail
   "Sends HTML email and returns a sade.core.ok/fail with :reason telling weather is was ok"
   [to from subject body]
-  (let [status (postal/send-message
-                 {:host "smtp.gmail.com"
-                  :user "metosin.tester"
-                  :pass "karitapio"
-                  :ssl  :yes}
-                 {:from    from
-                  :to      to
-                  :subject subject
-                  :body    [{:type "text/html; charset=utf-8"
-                             :content body}]})]
-    (if (= (:error status) :SUCCESS)
-      (ok)
-      (fail :reason (:msg status)))))
+  (when-let [domain (s/suffix to "@")]
+    (if (or (s/starts-with domain "example.") (= to domain))
+      (do
+        (warn "Not sending email to" to)
+        (ok))
+      (try
+        (let [status (postal/send-message
+                       (:email env/config)
+                       {:from    from
+                        :to      to
+                        :subject subject
+                        :body    [{:type "text/html; charset=utf-8"
+                                   :content body}]})]
+          (if (= (:error status) :SUCCESS) (ok) (fail :reason (:msg status))))
+        (catch Exception e
+          (error e (.getMessage e))
+          (fail :reason (:msg "exeption")))))))
+
+(defn send-mail?
+  [to from subject body]
+  (ok? (send-mail to from subject body))
+  )
