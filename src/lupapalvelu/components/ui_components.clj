1fa4b6432202b54458bc793051c8824e558156a1
jarppe <jarppe@metosin.fi>
2013-03-27 09:40:17 +0200

    Upped jQuery UI.

--- src/lupapalvelu/components/ui_components.clj
+++ src/lupapalvelu/components/ui_components.clj
@@ -21,8 +21,9 @@
   (str ";loc.setTerms(" (json/generate-string (i18n/get-localizations)) ");"))
 
 (def ui-components
-  {:cdn-fallback {:js ["jquery-1.8.0.min.js" "jquery-ui-1.10.1.custom.min.js" "jquery.dataTables.min.js" "knockout-2.1.0.js"]}
-   :jquery       {:js ["jquery.ba-hashchange.js" "jquery.metadata-2.1.js" "jquery.autocomplete.js" "jquery.cookie.js"]}
+  {:cdn-fallback {:js ["jquery-1.8.0.min.js" "jquery-ui.1.10.2.min.js" "jquery.dataTables.min.js" "knockout-2.1.0.js"]}
+   :jquery       {:js ["jquery.ba-hashchange.js" "jquery.metadata-2.1.js" "jquery.cookie.js"]
+                  :css ["jquery-ui.css"]}
    :knockout     {:js ["knockout.mapping-2.3.2.js" "knockout.validation.js" "knockout-repeat-1.4.2.js"]}
    :underscore   {:js ["underscore-1.4.4-min.js" "underscore.string.min.js" "underscore.string.init.js"]}
    :moment       {:js ["moment.min.js"]}
