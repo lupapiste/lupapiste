bb9629de404bef611bad13b4c07a0d8210639625
jarppe <jarppe@metosin.fi>
2013-03-28 12:02:14 +0200

    Added getZoom() and zoomTo() functions to gis.js. Removed unused functions.

--- resources/private/map/gis.js
+++ resources/private/map/gis.js
@@ -69,18 +69,15 @@ var gis = (function() {
       self.map.setCenter(new OpenLayers.LonLat(x, y), zoom);
       return self;
     };
+    
+    self.zoomTo = function(zoom) {
+      self.map.zoomTo(zoom);
+      return self;
+    };
 
     self.getZoom = function() {
-      return self.map.zoom;
-    }
-
-    self.getMaxZoom = function() {
-      return 11;
-    }
-
-    self.centerWithMaxZoom = function(x, y) {
-      return self.center(x, y, self.getMaxZoom());
-    }
+      return self.map.getZoom();
+    };
 
     self.updateSize = function() {
       self.map.updateSize();
