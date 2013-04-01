ecb11c32c70d9b0038fb78c810b5818449cb9ef5
jarppe <jarppe@metosin.fi>
2013-04-01 13:29:48 +0300

    Can;t really do more until maanmittauslaitos starts accepting http connections again.

--- src/lupapalvelu/find_address.clj
+++ src/lupapalvelu/find_address.clj
@@ -1,25 +1,89 @@
 (ns lupapalvelu.find-address
-  (:use [monger.operators])
+  (:use [monger.operators]
+        [lupapalvelu.security :only [current-user]]
+        [clojure.data.zip.xml :only [xml-> text]]
+        [lupapalvelu.i18n :only [*lang*]])
   (:require [clojure.string :as s]
             [monger.query :as q]
-            [lupapalvelu.mongo :as mongo]))
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.wfs :as wfs]))
 
-;; All search-... functions return a sequence of items, where each item is a map
-;; of (at least) following keys:
-;;   :kind       One of :poi, :property-id, or :address
-;;   :location   Map with :x and :y
+;; Should be in util or sumthin...
+
+(defn uniq-by
+  [by coll]
+  (let [step (fn step [xs prev]
+               (lazy-seq
+                 ((fn [[f :as xs] prev]
+                    (when-let [s (seq xs)]
+                      (if (= (by f) prev) 
+                        (recur (rest s) prev)
+                          (cons f (step (rest s) (by f))))))
+                   xs prev)))]
+    (step coll nil)))
+
+(defn- set-kind [kind]
+  (fn [result] (assoc result :kind kind)))
+
+;;;
+;;; All search-... functions return a sequence of items, where each item is a map
+;;; of (at least) following keys:
+;;;   :kind       One of :poi, :property-id, or :address
+;;;   :location   Map with :x and :y
+;;;
 
 (defn search-property-id [property-id]
-  (println (format "PROPID: <%s>" property-id)))
+  ; FIXME: Fix result to property format
+  (map (set-kind :property-id)
+       (wfs/point-by-property-id property-id)))
 
 (defn search-poi-or-street [poi]
-  (println (format "POI: <%s>" poi)))
+  (map (set-kind :poi)
+       (q/with-collection "poi"
+         (q/find {:name {$regex (str \^ (s/lower-case poi))}})
+         (q/limit 10))))
 
 (defn search-street-with-number [street number]
-  (println (format "STREET: <%s> <%s>" street number)))
+  ; FIXME: Fix result to property format
+  (wfs/execute wfs/maasto
+    (wfs/query {"typeName" "oso:Osoitenimi"}
+      (wfs/sort-by ["oso:kuntanimiFin"])
+      (wfs/filter
+        (wfs/and
+          (wfs/property-is-like "oso:katunimi"   (str street "*"))
+          (wfs/property-is-like "oso:katunumero" (str number "*"))
+          (wfs/property-is-less "oso:jarjestysnumero" "10"))))))
+
+(defn search-street-with-city [street city]
+  (let [data (wfs/execute wfs/maasto
+               (wfs/query {"typeName" "oso:Osoitenimi"}
+                 (wfs/sort-by ["oso:katunimi" "oso:katunumero"])
+                 (wfs/filter
+                   (wfs/and
+                     (wfs/property-is-like "oso:katunimi" (str street "*"))
+                     (wfs/or
+                       (wfs/property-is-like "oso:kuntanimiFin" (str city "*"))
+                       (wfs/property-is-like "oso:kuntanimiSwe" (str city "*")))
+                     (wfs/property-is-less "oso:jarjestysnumero" "10")))))]
+    (when data
+      (map (comp (set-kind :address) feature-to-address)
+           (uniq-by :street data)))))
 
 (defn search-address [street number city]
-  (println (format "ADDRESS: <%s> <%s> <%s>" street number city)))
+  (let [data (wfs/execute wfs/maasto
+               (wfs/query {"typeName" "oso:Osoitenimi"}
+                 (wfs/sort-by ["oso:katunimi" "oso:katunumero"])
+                 (wfs/filter
+                   (wfs/and
+                     (wfs/property-is-like "oso:katunimi" (str street "*"))
+                     (wfs/property-is-like "oso:katunumero" (str number "*"))
+                     (wfs/or
+                       (wfs/property-is-like "oso:kuntanimiFin" (str city "*"))
+                       (wfs/property-is-like "oso:kuntanimiSwe" (str city "*")))
+                     (wfs/property-is-less "oso:jarjestysnumero" "10")))))]
+    (when data
+      (map (comp (set-kind :address) feature-to-address)
+           data))))
 
 ;;
 ;; Utils:
@@ -52,8 +116,9 @@
     #"^(\d{14})$"                                 :>> (apply-search search-property-id)
     #"^(\d{1,3})-(\d{1,3})-(\d{1,4})-(\d{1,4})$"  :>> (fn [[_ a b c d]] (search-property-id (to-property-id a b c d)))
     #"^(\S+)$"                                    :>> (apply-search search-poi-or-street)
-    #"^(\S+)\s+(\d+)\s*,?$"                       :>> (apply-search search-street-with-number)
-    #"^(\S+)\s+(\d+)?\s*,?\s*(\S+)$"              :>> (apply-search search-address)
+    #"^(\S+)\s+(\d+)\s*,?\s*$"                    :>> (apply-search search-street-with-number)
+    #"^(\S+)\s+(\S+)$"                            :>> (apply-search search-street-with-city)
+    #"^(\S+)\s+(\d+)\s*,?\s*(\S+)$"               :>> (apply-search search-address)
     []))
 
 ;;
@@ -61,6 +126,16 @@
 ;;
 
 (comment
+  
+  (def e (wfs/execute wfs/maasto
+         (wfs/query {"typeName" "oso:Osoitenimi"}
+           (wfs/sort-by ["oso:katunimi" "oso:katunumero"])
+             (wfs/filter
+               (wfs/and
+                 (wfs/property-is-like "oso:katunimi" (str "piirin" "*"))
+                 (wfs/or
+                   (wfs/property-is-like "oso:kuntanimiFin" (str "tampe" "*"))
+                   (wfs/property-is-like "oso:kuntanimiSwe" (str "tampe" "*"))))))))
   
   (map #(assoc % :kind :poi)
        (q/with-collection "poi"
@@ -71,20 +146,20 @@
     (wfs/execute wfs/maasto
       (cond
         (and (s/blank? number) (s/blank? city)) (wfs/query {"typeName" "oso:Osoitenimi"}
-                                                           (wfs/sort-by "oso:kuntanimiFin")
+                                                           (wfs/sort-by ["oso:kuntanimiFin"])
                                                            (wfs/filter
                                                              (wfs/and
                                                                (wfs/property-is-like "oso:katunimi" (str street "*"))
                                                                (wfs/property-is-equal "oso:jarjestysnumero" "1"))))
         (s/blank? city) (wfs/query {"typeName" "oso:Osoitenimi"}
-                                   (wfs/sort-by "oso:kuntanimiFin")
+                                   (wfs/sort-by ["oso:kuntanimiFin"])
                                    (wfs/filter
                                      (wfs/and
                                        (wfs/property-is-like "oso:katunimi"   (str street "*"))
                                        (wfs/property-is-like "oso:katunumero" (str number "*"))
                                        (wfs/property-is-less "oso:jarjestysnumero" "10"))))
         (s/blank? number) (wfs/query {"typeName" "oso:Osoitenimi"}
-                                     (wfs/sort-by "oso:katunumero")
+                                     (wfs/sort-by ["oso:katunumero"])
                                      (wfs/filter
                                        (wfs/and
                                          (wfs/property-is-like "oso:katunimi" (str street "*"))
@@ -92,7 +167,7 @@
                                                       (wfs/property-is-like "oso:kuntanimiFin" (str city "*"))
                                                       (wfs/property-is-like "oso:kuntanimiSwe" (str city "*"))))))
         :else (wfs/query {"typeName" "oso:Osoitenimi"}
-                         (wfs/sort-by "oso:katunumero")
+                         (wfs/sort-by ["oso:katunumero"])
                          (wfs/filter
                            (wfs/and
                              (wfs/property-is-like "oso:katunimi"     (str street "*"))
