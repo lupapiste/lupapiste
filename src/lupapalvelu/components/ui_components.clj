92dac7de3391bd58ab20141a7c89fe7d6c9b8291
Tommi Reiman <tommi.reiman@metosin.fi>
2013-04-03 11:03:21 +0300

    move more from common to authenticated

--- src/lupapalvelu/components/ui_components.clj
+++ src/lupapalvelu/components/ui_components.clj
@@ -46,12 +46,12 @@
 
    :common       {:depends [:init :jquery :knockout :underscore :moment :i18n :selectm]
                   :js ["util.js" "event.js" "pageutil.js" "notify.js" "ajax.js" "app.js" "nav.js" "combobox.js"
-                       "ko.init.js" "dialog.js" "comment.js" "authorization.js" "datepicker.js"]
+                       "ko.init.js" "dialog.js" "datepicker.js"]
                   :css ["css/main.css"]
                   :html ["error.html"]}
 
    :authenticated {:depends [:init :jquery :knockout :underscore :moment :i18n :selectm]
-                   :js ["municipalities.js"]
+                   :js ["comment.js" "authorization.js" "municipalities.js"]
                    :css ["css/main.css"]
                    :html ["error.html"]}
 
