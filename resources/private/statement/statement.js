05af2ea815239724d32020f977624500351054cd
Tommi Reiman <tommi.reiman@metosin.fi>
2013-04-11 09:02:30 +0300

    statementmodel 12

--- resources/private/statement/statement.js
+++ resources/private/statement/statement.js
@@ -3,18 +3,31 @@ var statement = (function() {
 
   var applicationId = null;
   var statementId = null;
+  var statementModel = new StatementModel();
+
+  function StatementModel() {
+    var self = this;
+
+    self.application = ko.observable();
+  }
+
+  function refresh(application) {
+    statementModel.application(ko.mapping.fromJS(application));
+    console.log("refresh: ",application);
+    console.log(statementModel.application().title());
+  }
+
+  repository.loaded(function(event) {
+    var application = event.applicationDetails.application;
+    if (applicationId === application.id) { refresh(application); }
+  });
 
   hub.onPageChange("statement", function(e) {
     applicationId = e.pagePath[0];
     statementId = e.pagePath[1];
-    console.log(applicationId,statementId,"here");
     repository.load(applicationId);
   });
 
-  $(function() {
-    ko.applyBindings({
-      statementModel: true
-    }, $("#statement")[0]);
-  });
+  $(function() { ko.applyBindings({ statementModel: statementModel }, $("#statement")[0]); });
 
 })();
