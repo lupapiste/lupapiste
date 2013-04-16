aca8344704dfac0f6d7efd77200bc137292e98a8
jarppe <jarppe@metosin.fi>
2013-04-16 21:56:26 +0300

    Fancy stamp and test frame.

--- src/lupapalvelu/stamper.clj
+++ src/lupapalvelu/stamper.clj
@@ -6,7 +6,8 @@
             [sade.strings :as ss]
             [lupapalvelu.mime :as mime])
   (:import [java.io InputStream OutputStream]
-           [java.awt Color Image]
+           [java.awt Color Image BasicStroke AlphaComposite]
+           [java.awt.geom RoundRectangle2D$Float]
            [java.awt.image BufferedImage RenderedImage]
            [javax.imageio ImageIO]
            [com.lowagie.text Document Element Paragraph Phrase Rectangle]
@@ -19,11 +20,69 @@
 ;; Generate stamp:
 ;;
 
+(defn- calculate-text-widths [texts font]
+  (let [i (BufferedImage. 1 1 BufferedImage/TYPE_INT_ARGB)
+        g (.createGraphics i)
+        _ (.setFont g font)
+        fm (.getFontMetrics g)
+        widths (map (fn [text] (.stringWidth fm text)) texts)]
+    (.dispose g)
+    widths))
 
 (defn make-stamp []
-  (with-open [in (io/input-stream (io/resource "public/img/logo.png"))]
-    (ImageIO/read in)))
+  (let [font (java.awt.Font. "Courier" java.awt.Font/BOLD 25)
+        texts ["hyv\u00E4ksytty 16.04.2013"
+               "Veikko Viranomainen"
+               "SIPOO"
+               "LUPAPISTE.fi"]
+        text-widths (calculate-text-widths texts font)
+        width (+ (reduce max text-widths) 80)
+        i (BufferedImage. width 210 BufferedImage/TYPE_INT_ARGB)]
+    (doto (.createGraphics i)
+      (.setColor (Color. 0 0 0 0))
+      (.fillRect 0 0 width 300)
+      (.setStroke (BasicStroke. 16.0 BasicStroke/CAP_ROUND BasicStroke/JOIN_ROUND))
+      (.setColor (Color. 128 16 16 180))
+      (.draw (RoundRectangle2D$Float. 20 20 (- width 40) 170 50 50))
+      (.setFont font)
+      (.drawString (nth texts 0) 40 55)
+      (.drawString (nth texts 1) 40 95)
+      (.drawString (nth texts 2) 40 125)
+      (.drawString (nth texts 3) (int (/ (- width (nth text-widths 3)) 2)) 170)
+      (.dispose))
+    i))
 
+(defn f []
+  (let [f (doto (javax.swing.JFrame. "heelo")
+            (.setContentPane (proxy [javax.swing.JComponent] []
+                               (paint [g]
+                                 (let [w (.getWidth this)
+                                       h (.getHeight this)
+                                       i (make-stamp)
+                                       iw (.getWidth i)
+                                       ih (.getHeight i)]
+                                   (doto g
+                                     (.setColor Color/GRAY)
+                                     (.fillRect 0 0 (int (/ w 2)) (int (/ h 2)))
+                                     (.setColor Color/WHITE)
+                                     (.fillRect (int (/ w 2)) 0 w (int (/ h 2)))
+                                     (.setColor Color/BLACK)
+                                     (.fillRect 0 (int (/ h 2)) (int (/ w 2)) h)
+                                     (.setColor Color/RED)
+                                     (.fillRect (int (/ w 2)) (int (/ h 2)) w h)
+                                     (.setColor Color/RED)
+                                     (.drawLine 0 0 w h)
+                                     (.drawLine 0 h w 0)
+                                     (.drawImage i (int (/ (- w iw) 2)) (int (/ (- h ih) 2)) nil))))))
+            (.setMinimumSize (java.awt.Dimension. 100 100))
+            (.setDefaultCloseOperation javax.swing.JFrame/DISPOSE_ON_CLOSE)
+            (.setAlwaysOnTop true)
+            (.setLocationByPlatform true)
+            (.pack)
+            (.setVisible true))]
+    (add-watch #'make-stamp :key (fn [_ _ _ _] (javax.swing.SwingUtilities/invokeLater (fn [] (.repaint f)))))))
+
+(f)
 ;;
 ;; Stamp with provided image:
 ;;
@@ -74,4 +133,6 @@
     (throw+ "write: can't write stamped image")))
 
 (defn- add-stamp [^BufferedImage image ^Image stamp]
-  (.drawImage (.createGraphics image) stamp 5 (- (.getHeight image nil) (.getHeight stamp nil) 5) nil))
+  (doto (.createGraphics image)
+    (.drawImage stamp 5 (- (.getHeight image nil) (.getHeight stamp nil) 5) nil)
+    (.dispose)))
