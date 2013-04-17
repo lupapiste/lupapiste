5253c5a83440c204c04e5c15e28ced066fd2d172
jarppe <jarppe@metosin.fi>
2013-04-17 11:18:23 +0300

    Improved stamp with dynamic content, cleanup.

--- src/lupapalvelu/stamper.clj
+++ src/lupapalvelu/stamper.clj
@@ -6,15 +6,13 @@
             [sade.strings :as ss]
             [lupapalvelu.mime :as mime])
   (:import [java.io InputStream OutputStream]
-           [java.awt Color Image BasicStroke AlphaComposite]
+           [java.awt Color Image BasicStroke Font]
            [java.awt.geom RoundRectangle2D$Float]
            [java.awt.image BufferedImage RenderedImage]
            [javax.imageio ImageIO]
-           [com.lowagie.text Document Element Paragraph Phrase Rectangle]
-           [com.lowagie.text.pdf BaseFont PdfContentByte PdfGState PdfPageEventHelper PdfTemplate PdfStamper PdfWriter PdfReader]
-           [com.lowagie.text.pdf.draw DottedLineSeparator LineSeparator]))
+           [com.lowagie.text.pdf PdfGState PdfStamper PdfReader]))
 
-(set! *warn-on-reflection* true)
+; (set! *warn-on-reflection* true)
 
 ;;
 ;; Generate stamp:
@@ -29,60 +27,32 @@
     (.dispose g)
     widths))
 
-(defn make-stamp []
-  (let [font (java.awt.Font. "Courier" java.awt.Font/BOLD 25)
-        texts ["hyv\u00E4ksytty 16.04.2013"
-               "Veikko Viranomainen"
-               "SIPOO"
+(defn make-stamp [verdict created username municipality]
+  (let [font (Font. "Courier" Font/BOLD 25)
+        texts [(str verdict \space (format "%td.%<tm.%<tY" (java.util.Date. created)))
+               username
+               municipality
                "LUPAPISTE.fi"]
         text-widths (calculate-text-widths texts font)
         width (+ (reduce max text-widths) 80)
-        i (BufferedImage. width 210 BufferedImage/TYPE_INT_ARGB)]
+        i (BufferedImage. width 190 BufferedImage/TYPE_INT_ARGB)]
     (doto (.createGraphics i)
       (.setColor (Color. 0 0 0 0))
       (.fillRect 0 0 width 300)
       (.setStroke (BasicStroke. 16.0 BasicStroke/CAP_ROUND BasicStroke/JOIN_ROUND))
-      (.setColor (Color. 128 16 16 180))
-      (.draw (RoundRectangle2D$Float. 20 20 (- width 40) 170 50 50))
+      (.setColor (Color. 255 255 255 160))
+      (.fill (RoundRectangle2D$Float. 20 20 (- width 40) 150 30 30))
+      (.setColor (Color. 128 16 16 160))
+      (.draw (RoundRectangle2D$Float. 20 20 (- width 40) 150 30 30))
+      (.setColor (Color. 128 16 16 192))
       (.setFont font)
       (.drawString (nth texts 0) 40 55)
-      (.drawString (nth texts 1) 40 95)
-      (.drawString (nth texts 2) 40 125)
-      (.drawString (nth texts 3) (int (/ (- width (nth text-widths 3)) 2)) 170)
+      (.drawString (nth texts 1) 40 90)
+      (.drawString (nth texts 2) 40 115)
+      (.drawString (nth texts 3) (int (/ (- width (nth text-widths 3)) 2)) 150)
       (.dispose))
     i))
 
-(defn f []
-  (let [f (doto (javax.swing.JFrame. "heelo")
-            (.setContentPane (proxy [javax.swing.JComponent] []
-                               (paint [g]
-                                 (let [w (.getWidth this)
-                                       h (.getHeight this)
-                                       i (make-stamp)
-                                       iw (.getWidth i)
-                                       ih (.getHeight i)]
-                                   (doto g
-                                     (.setColor Color/GRAY)
-                                     (.fillRect 0 0 (int (/ w 2)) (int (/ h 2)))
-                                     (.setColor Color/WHITE)
-                                     (.fillRect (int (/ w 2)) 0 w (int (/ h 2)))
-                                     (.setColor Color/BLACK)
-                                     (.fillRect 0 (int (/ h 2)) (int (/ w 2)) h)
-                                     (.setColor Color/RED)
-                                     (.fillRect (int (/ w 2)) (int (/ h 2)) w h)
-                                     (.setColor Color/RED)
-                                     (.drawLine 0 0 w h)
-                                     (.drawLine 0 h w 0)
-                                     (.drawImage i (int (/ (- w iw) 2)) (int (/ (- h ih) 2)) nil))))))
-            (.setMinimumSize (java.awt.Dimension. 100 100))
-            (.setDefaultCloseOperation javax.swing.JFrame/DISPOSE_ON_CLOSE)
-            (.setAlwaysOnTop true)
-            (.setLocationByPlatform true)
-            (.pack)
-            (.setVisible true))]
-    (add-watch #'make-stamp :key (fn [_ _ _ _] (javax.swing.SwingUtilities/invokeLater (fn [] (.repaint f)))))))
-
-(f)
 ;;
 ;; Stamp with provided image:
 ;;
@@ -136,3 +106,58 @@
   (doto (.createGraphics image)
     (.drawImage stamp 5 (- (.getHeight image nil) (.getHeight stamp nil) 5) nil)
     (.dispose)))
+
+;;
+;; Swing frame for testing stamp:
+;;
+
+(defn- lorem-line [c]
+  (let [sb (StringBuilder.)]
+    (while (< (.length sb) c)
+      (.append sb (rand-nth ["lorem" "ipsum" "dolor" "sit" "amet" "consectetur" "adipisicing" "elit" "sed" "do" "eiusmod" "tempor" "incididunt" "ut" "labore" "et" "dolore" "magna" "aliqua"]))
+      (.append sb \space))
+    (str sb)))
+
+(defn lorem-ipsum [g w h]
+  (.setFont g (Font. Font/SERIF Font/PLAIN 18))
+  (let [fm (.getFontMetrics g)
+        cw (.stringWidth fm "l")
+        ch (+ (.getAscent fm) (.getDescent fm))]
+    (doseq [[y text] (map vector (range ch h ch) (repeatedly (partial lorem-line (int (/ w cw)))))]
+      (.drawString g text cw y))))
+
+(defn- paint-component [g w h]
+  (let [i (make-stamp "hyv\u00E4ksytty" (System/currentTimeMillis) "Veikko Viranomainen" "SIPOO")
+        iw (.getWidth i)
+        ih (.getHeight i)]
+    (if false
+      (doto g
+        (.setColor Color/GRAY)
+        (.fillRect 0 0 (int (/ w 2)) (int (/ h 2)))
+        (.setColor Color/WHITE)
+        (.fillRect (int (/ w 2)) 0 w (int (/ h 2)))
+        (.setColor Color/RED)
+        (.fillRect 0 (int (/ h 2)) (int (/ w 2)) h)
+        (.setColor Color/BLACK)
+        (.fillRect (int (/ w 2)) (int (/ h 2)) w h)))
+    (if true
+      (lorem-ipsum g w h))
+    (.drawImage g i (int (/ (- w iw) 2)) (int (/ (- h ih) 2)) nil)))
+
+(defn make-frame []
+  (let [f (doto (javax.swing.JFrame. "heelo")
+            (.setContentPane
+              (doto (proxy [javax.swing.JComponent] []
+                      (paint [g]
+                        (paint-component g (.getWidth this) (.getHeight this))))))
+            (.setMinimumSize (java.awt.Dimension. 100 100))
+            (.setDefaultCloseOperation javax.swing.JFrame/DISPOSE_ON_CLOSE)
+            (.setAlwaysOnTop true)
+            (.setLocationByPlatform true)
+            (.pack)
+            (.setVisible true))
+        repaint (fn [_ _ _ _]
+                  (javax.swing.SwingUtilities/invokeLater (fn [] (.repaint f))))]
+    (doseq [v [#'make-stamp #'paint-component #'lorem-line #'lorem-ipsum]]
+      (add-watch v :repaint repaint))
+    f))
