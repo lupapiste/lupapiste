1d265d6ba86cb413d11550ed50fa6c34a2e0eac0
Tommi Reiman <tommi.reiman@metosin.fi>
2013-04-27 12:50:56 +0300

    new rules

--- itest/lupapalvelu/inforequest_itest.clj
+++ itest/lupapalvelu/inforequest_itest.clj
@@ -4,8 +4,7 @@
         [clojure.pprint :only [pprint]])
   (:require [lupapalvelu.operations :as operations]
             [lupapalvelu.domain :as domain]
-            [lupapalvelu.document.schemas :as schemas])
-  )
+            [lupapalvelu.document.schemas :as schemas]))
 
 (facts "inforequest workflow"
   (let [resp (create-app pena :messages ["hello"] :infoRequest true :municipality sonja-muni)
@@ -14,32 +13,25 @@
     (success resp) => true
 
     (fact "inforequest was created with message"
-          (let [resp        (query pena :application :id id)
-                application (:application resp)]
-            (:state application) => "info"
-            (:opened application) => truthy
-            (count (:comments application)) => 1
-            (-> (:comments application) first :text) => "hello"))
+      (let [application (query-application pena id)]
+        (:state application) => "info"
+        (:opened application) => truthy
+        (count (:comments application)) => 1
+        (-> (:comments application) first :text) => "hello"))
 
     (fact "Veikko can not assign inforequest to himself"
-          (let [resp (command veikko :assign-application :id id :assigneeId veikko-id)]
-            (unauthorized resp) => true))
+      (let [resp (command veikko :assign-application :id id :assigneeId veikko-id)]
+        (unauthorized resp) => true))
 
     (fact "Sonja can assign inforequest to herself"
-          (let [resp (command sonja :assign-application :id id :assigneeId sonja-id)]
-            (success resp) => true))
+      (let [resp (command sonja :assign-application :id id :assigneeId sonja-id)]
+        (success resp) => true))
 
-    (fact "Pena can't mark inforequest answered"
-          (let [resp (command pena :mark-inforequest-answered :id id)]
-            (unauthorized resp) => true))
-
-    (fact "Sonja can mark inforequest answered"
-          (let [resp (command sonja :mark-inforequest-answered :id id)]
-            (success resp) => true)
-          (let [inforequest  (:application (query pena :application :id id))]
-            (:state inforequest) => "answered"))
+    (fact "Commenting on inforequest marks it answered"
+      (query-application pena id) => (in-state? :info)
+      (comment-application id sonja) => ok?
+      (query-application pena id) => (in-state? :answered))
 
     (fact "Pena can convert-to-application"
-          (let [resp (command pena :convert-to-application :id id)]
-            (success resp) => true))
-))
+      (let [resp (command pena :convert-to-application :id id)]
+        (success resp) => true))))
