9ac0abe801da1aa243e215dacab349c1f1f841f3
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-05-06 12:50:30 +0300

    Display map and property id

--- resources/private/application/change-location.js
+++ resources/private/application/change-location.js
@@ -1,17 +1,31 @@
 LUPAPISTE.ChangeLocationModel = function() {
   var self = this;
+
+  self.map = {};
   self.id = 0;
+  self.municipalityCode = 0;
   self.dialogSelector = "#dialog-change-location";
-  self.x = ko.observable(0);
-  self.y = ko.observable(0);
+  self.x = 0;
+  self.y = 0;
   self.address = ko.observable("");
   self.propertyId = ko.observable("");
   self.errorMessage = ko.observable(null);
 
+  self.propertyId.subscribe(function(id) {
+    var human = util.prop.toHumanFormat(id);
+    if (human != id) {
+      self.propertyId(human);
+    }
+  });
+
+  self.ok = ko.computed(function() {
+    return util.prop.isPropertyId(self.propertyId()) && self.address();
+  });
+
   self.reset = function(app) {
     self.id = app.id();
-    self.x(app.location().x());
-    self.y(app.location().y());
+    self.x = app.location().x();
+    self.y = app.location().y();
     self.address(app.address());
     self.propertyId(app.propertyId());
     self.errorMessage(null);
@@ -28,7 +42,7 @@ LUPAPISTE.ChangeLocationModel = function() {
   };
 
   self.saveNewLocation = function() {
-    var data = {id: self.id, x: self.x(), y: self.y(), address: self.address(), propertyId: self.propertyId()};
+    var data = {id: self.id, x: self.x, y: self.y, address: self.address(), propertyId: self.propertyId()};
     ajax.command("change-location", data).success(self.onSuccess).error(self.onError).call();
     return false;
   };
@@ -36,5 +50,11 @@ LUPAPISTE.ChangeLocationModel = function() {
   self.changeLocation = function(app) {
     self.reset(app);
     LUPAPISTE.ModalDialog.open(self.dialogSelector);
+    self.map.updateSize();
+    self.map.clear().center(self.x, self.y, 10).add(self.x, self.y);
   };
+
+  $(function() {
+    self.map = gis.makeMap("change-location-map").center([{x: 404168, y: 6693765}], 10);
+  });
 };
