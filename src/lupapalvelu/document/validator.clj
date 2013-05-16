e0f026e4101300c075b1bb5803aed0c891f6aeef
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-16 15:13:55 +0300

    start to move parameters to macro

--- src/lupapalvelu/document/validator.clj
+++ src/lupapalvelu/document/validator.clj
@@ -2,26 +2,25 @@
 
 (def validators (atom {}))
 
-(defmacro defvalidator [doc-string {:keys [code document fields]} bindings & body]
+(defmacro defvalidator [doc-string {:keys [code document fields]} & body]
   `(swap! validators assoc (keyword ~code)
      {:doc ~doc-string
-      :fn (fn [~@bindings]
-            ~@body)}))
+      :fn (fn [~'_]
+            (let [~'kerrosala 10
+                  ~'kokonaisala 9]
+              ~@body))}))
 
 (defvalidator "Kokonaisalan oltava vähintään kerrosala"
   {:code "vrk:CR326"
    :document "uusiRakennus"
    :fields [kokonaisala [:mitat :kokonaisala]
             kerrosala   [:mitat :kerrosala]]}
-  [data]
-  (let [kokonaisala (-> data :mitat :kokonaisala)
-        kerrosala   (-> data :mitat :kerrosala)]
-    (when
-      (and
-        kokonaisala
-        kerrosala
-        (> kerrosala kokonaisala))
-      [:kosh])))
+  (when
+    (and
+      kokonaisala
+      kerrosala
+      (> kerrosala kokonaisala))
+    [:kosh]))
 
 (defn validate
   "Runs all validators, returning list of validation results."
@@ -34,7 +33,6 @@
     (map #(apply % [(:data document)]))
     (filter (comp not nil?))))
 
-(assert (= [[:kosh]] (validate {:data {:mitat {:kerrosala 10 :kokonaisala  9}}})))
-(assert (= []        (validate {:data {:mitat {:kerrosala 10 :kokonaisala 10}}})))
+(validate {:data {:mitat {:kerrosala 10 :kokonaisala 10}}})
 
 
