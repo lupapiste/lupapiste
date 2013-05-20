e351e7e0f43d161510135c1883b9f4403d40089e
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-20 08:55:38 +0300

    validators & test

--- src/lupapalvelu/document/validator.clj
+++ src/lupapalvelu/document/validator.clj
@@ -10,31 +10,29 @@
     deref
     vals
     (map #(apply % [document]))
+    (reduce concat)
     (filter (comp not nil?))))
 
 (defmacro defvalidator [doc-string {:keys [schema fields]} & body]
   (let [paths (->> fields (partition 2) (map last) vec)]
     `(swap! validators assoc (keyword ~doc-string)
-       (fn [{~'data :data}]
-         (let
-           ~(reduce into
-              (for [[k v] (partition 2 fields)]
-                [k `(get-in ~'data ~v)]))
-           (try
-             (when-let [resp# ~@body]
-               (map (fn [path#] {:path   path#
-                                 :result [:warn (name resp#)]}) ~paths))
-             (catch Exception e#
-               (map (fn [path#] {:path   path#
-                                 :result [:warn (str "validator")]
-                                 :reason (str e#)}) ~paths))))))))
+       (fn [{~'data :data {{~'doc-schema :name} :info} :schema}]
+         (when (or (not ~schema) (= ~schema ~'doc-schema))
+           (let
+             ~(reduce into
+                (for [[k v] (partition 2 fields)]
+                  [k `(get-in ~'data ~v)]))
+             (try
+               (when-let [resp# (do ~@body)]
+                 (map (fn [path#] {:path   path#
+                                   :result [:warn (name resp#)]}) ~paths))
+               (catch Exception e#
+                 {:result [:warn (str "validator")]
+                  :reason (str e#)}))))))))
 
-(defvalidator "Kokonaisalan oltava vähintään kerrosala"
-  {:schema "uusiRakennus"
-   :fields [kokonaisala [:mitat :kokonaisala]
-            kerrosala   [:mitat :kerrosala]]}
-  (and kokonaisala kerrosala (> kerrosala kokonaisala) :vrk:CR326))
-
-(println "warn:" (validate {:data {:mitat {:kerrosala 10 :kokonaisala 9}}}))
-(println " err:" (validate {:data {:mitat {:kerrosala 10 :kokonaisala "abba"}}}))
-(println "  ok:" (validate {:data {:mitat {:kerrosala 10 :kokonaisala 10}}}))
+(comment
+  (defvalidator "Kokonaisalan oltava vähintään kerrosala"
+    {:schema "uusiRakennus"
+     :fields [kokonaisala [:mitat :kokonaisala]
+              kerrosala   [:mitat :kerrosala]]}
+    (and kokonaisala kerrosala (> kerrosala kokonaisala) :vrk:CR326)))
