625c3e9073442fb17e7ad9ae57768a344bebb829
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-27 08:35:22 +0300

    scraping enums from xsd

---
+++ src/lupapalvelu/xml/krysp/enums.clj
@@ -0,0 +1,19 @@
+(ns lupapalvelu.xml.krysp.enums
+  (:require [clojure.string :as s]
+            [net.cgrand.enlive-html :as e]
+            [sade.util :refer [fn->]]
+            [lupapalvelu.clojure15 :refer [some->>]]
+            [sade.xml :as xml]))
+
+(defn extract [schema name mapper]
+  (->>
+    (e/select schema [(e/attr= :name name) :xs:restriction :xs:enumeration])
+    (map (fn-> :attrs :value mapper))))
+
+(defn ->kayttotarkoitus [x]
+  [(some->> x (re-matches #"(\d+) .*") last keyword)
+   (s/replace x #"\d+\s*" "")])
+
+(comment
+  (def schema (xml/parse "./resources/krysp/rakennusvalvonta.xsd"))
+  (extract schema "RakennuksenTiedotType" ->kayttotarkoitus))
