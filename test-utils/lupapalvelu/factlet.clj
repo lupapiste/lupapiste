aee26791f4caf3816fbbc7060b14406531029e00
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-27 09:25:55 +0300

    fact*

--- test-utils/lupapalvelu/factlet.clj
+++ test-utils/lupapalvelu/factlet.clj
@@ -1,15 +1,25 @@
 (ns lupapalvelu.factlet
-  (:use [midje.sweet]))
+  (:use midje.sweet
+        clojure.walk)
+  (:require [midje.parsing.util.recognizing :as recognize]))
 
 (defn create-facts
   [c]
   (reduce
     (fn [form [k v]]
-      (if (= k (symbol =>))
-        (conj form '_ `(fact ~(str (-> form butlast last)) ~(-> form butlast last) => ~v))
+      (if (recognize/all-arrows (str k)) ;; will change for midje 1.6, bugga.
+        (conj form '_ `(fact ~(str (-> form butlast last)) ~(-> form butlast last) ~k ~v))
         (conj form k v)))
     [] (partition 2 c)))
 
 (defmacro factlet [letform & body]
   `(let ~(create-facts letform)
      ~@body))
+
+(defmacro fact* [& form]
+  (let [form (prewalk
+               (fn [x]
+                 (if (and (list? x) (= 'let (first x)))
+                   `(let ~(create-facts (second x)) ~@(-> x next next))
+                   x)) form)]
+    `(do ~@form)))
--- test/lupapalvelu/factlet_test.clj
+++ test/lupapalvelu/factlet_test.clj
@@ -18,3 +18,17 @@
     b => 3)
   @fact-call-count => 2
   @value-call-count => 2)
+
+(fact "about fact*"
+  (reset! fact-call-count 0)
+  (reset! value-call-count 0)
+  (facts "can be nested"
+    (fact "or even deeper nested"
+      (fact* "here's the beef"
+        (fact "which can haev more nesting under"
+          (let [a (plus 1 1) =not=> (result 2)
+                b (plus a 1) => (result 3)]
+            a => 2
+            b => 3)))))
+  @fact-call-count => 2
+  @value-call-count => 2)
