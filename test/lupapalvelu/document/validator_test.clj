106e5c03f2eb49beaa5d51eb9cb2a1e9bbfeea45
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-29 13:54:17 +0300

    wip

--- test/lupapalvelu/document/validator_test.clj
+++ test/lupapalvelu/document/validator_test.clj
@@ -3,6 +3,8 @@
         [midje.sweet])
   (:require [sade.util :refer [->int]]))
 
+;; normal validator
+
 (defvalidator :too-much-health
   {:doc    "health validator"
    :schema "test"
@@ -10,26 +12,59 @@
             max-health [:game :max-health]]}
   (and health max-health (> health max-health)))
 
-(def document {:schema {:info {:name "invalid"}}
-               :data {:player {:health     10}
-                      :game   {:max-health 10}}})
-
 (facts
-  (fact "is valid to begin with"
-    (validate document) => empty?)
+  (let [document {:schema {:info {:name "invalid"}}
+                  :data   {:player {:health     10}
+                           :game   {:max-health 10}}}]
 
-  (fact "is valid with wrong data and wrong schema"
-    (->
-      document
-      (assoc-in [:schema :info :name] "test")
-      (assoc-in [:data :player :health] 11)
-      validate) => [{:path [:player :health]
-                     :result [:warn "too-much-health"]}
-                    {:path [:game :max-health]
-                     :result [:warn "too-much-health"]}])
+    (fact "is valid to begin with"
+      (validate document) => empty?)
 
-  (fact "is invalid with wrong data and right schema"
-    (->
-      document
-      (assoc-in [:data :player :health] 11)
-      validate) => empty?))
+    (fact "is valid with wrong data and wrong schema"
+      (->
+        document
+        (assoc-in [:schema :info :name] "test")
+        (assoc-in [:data :player :health] 11)
+        validate) => [{:path [:player :health]
+                       :result [:warn "too-much-health"]}
+                      {:path [:game :max-health]
+                       :result [:warn "too-much-health"]}])
+
+    (fact "is invalid with wrong data and right schema"
+      (->
+        document
+        (assoc-in [:data :player :health] 11)
+        validate) => empty?)))
+
+;; child validator
+
+(defvalidator :all-alive
+  {:doc    "everyone in team is alive"
+   :schema "test2"
+   :childs [:team]
+   :fields [health [:health]]}
+  (> health 0))
+
+#_(facts
+  (let [document {:schema {:info {:name "test2"}}
+                  :data   {:team {:0 {:health 8}
+                                  :1 {:health 4}}}}]
+
+    (fact "everyone is alive"
+      (validate document) => empty?)
+
+   #_ (fact "is valid with wrong data and wrong schema"
+      (->
+        document
+        (assoc-in [:schema :info :name] "test")
+        (assoc-in [:data :player :health] 11)
+        validate) => [{:path [:player :health]
+                       :result [:warn "too-much-health"]}
+                      {:path [:game :max-health]
+                       :result [:warn "too-much-health"]}])
+
+    #_(fact "is invalid with wrong data and right schema"
+      (->
+        document
+        (assoc-in [:data :player :health] 11)
+        validate) => empty?)))
