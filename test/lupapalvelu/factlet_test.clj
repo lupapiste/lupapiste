d18b31e47b43f4b0e18f1cd599c07aeb9125f03a
Tommi Reiman <tommi.reiman@metosin.fi>
2013-05-30 11:26:40 +0300

    fact*

--- test/lupapalvelu/factlet_test.clj
+++ test/lupapalvelu/factlet_test.clj
@@ -8,27 +8,24 @@
 (defn result [x] (swap! fact-call-count inc) x)
 (defn plus [x y] (swap! value-call-count inc) (+ x y))
 
-(facts "about factlet"
-  (reset! fact-call-count 0)
-  (reset! value-call-count 0)
-  (factlet
-    [a (plus 1 1) => (result 2)
-     b (plus a 1) => (result 3)]
-    a => 2
-    b => 3)
-  @fact-call-count => 2
-  @value-call-count => 2)
-
 (fact "about fact*"
   (reset! fact-call-count 0)
   (reset! value-call-count 0)
-  (facts "can be nested"
-    (fact "or even deeper nested"
-      (fact* "here's the beef"
-        (fact "which can haev more nesting under"
-          (let [a (plus 1 1) =not=> (result 3)
-                b (plus a 1) => (result 3)]
-            a => 2
-            b => 3)))))
+  (fact*
+    (let [a (plus 1 1) =not=> (result 3)
+          b (plus a 1) => (result 3)]
+      a => 2
+      b => 3))
+  @fact-call-count => 2
+  @value-call-count => 2)
+
+(fact "about facts*"
+  (reset! fact-call-count 0)
+  (reset! value-call-count 0)
+  (facts*
+    (let [a (plus 1 1) =not=> (result 3)
+          b (plus a 1) => (result 3)]
+      a => 2
+      b => 3))
   @fact-call-count => 2
   @value-call-count => 2)
