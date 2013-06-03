92f30d567487f11b3c6f22eda6dc24f19c87bded
Tommi Reiman <tommi.reiman@metosin.fi>
2013-06-03 08:46:29 +0300

    kill da HAX.

---
+++ resources/private/common/currentUser.js
@@ -0,0 +1,13 @@
+var currentUser = (function() {
+  "use strict";
+
+  var user = {};
+
+  return {
+    set: function(u) { user = u; },
+    get: function() { return user; },
+    isAuthority: function() { return user.role === "authority"; }
+    isApplicant: function() { return user.role === "applicant"; }
+  };
+
+})();
