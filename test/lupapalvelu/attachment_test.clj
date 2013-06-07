fd0625c980853c08b85888f8f95bacada2999474
Tommi Reiman <tommi.reiman@metosin.fi>
2013-06-07 14:11:52 +0300

    recommit.

--- test/lupapalvelu/attachment_test.clj
+++ test/lupapalvelu/attachment_test.clj
@@ -66,25 +66,24 @@
     (latest-version-after-removing-file attachments :attachment2 :file1) => {:version { :major 1 :minor 1}
                                                                              :fileId :file2}))
 
-#_(fact "make attachments"
-  (make-attachments 999 [:a :b]) => [{:id "123"
-                                      :locked false
-                                      :authority false
-                                      :modified 999
-                                      :op nil
-                                      :state :requires_user_action
-                                      :target nil
-                                      :type :a
-                                      :versions []}
-                                     {:id "123"
-                                      :locked false
-                                      :authority false
-                                      :modified 999
-                                      :op nil
-                                      :state :requires_user_action
-                                      :target nil
-                                      :type :b
-                                      :versions []}]
+(fact "make attachments"
+  (make-attachments 999 [:a :b]) => (just
+                                      [{:id "123"
+                                        :locked false
+                                        :modified 999
+                                        :op nil
+                                        :state :requires_user_action
+                                        :target nil
+                                        :type :a
+                                        :versions []}
+                                       {:id "123"
+                                        :locked false
+                                        :modified 999
+                                        :op nil
+                                        :state :requires_user_action
+                                        :target nil
+                                        :type :b
+                                        :versions []}])
   (provided
     (mongo/create-id) => "123"))
 
