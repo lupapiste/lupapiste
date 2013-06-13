ebd0e214f0f4895b396218ccd498900ec56c074c
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-06-13 13:46:42 +0300

    Cleanup

--- test/lupapalvelu/security_test.clj
+++ test/lupapalvelu/security_test.clj
@@ -32,23 +32,3 @@
     (applicant? {:role :applicant}) => true
     (applicant? {}) => false))
 
-
-#_(facts
-  (against-background
-    (session/get :user) => :session-user
-    (security/login-with-apikey "123") => :apikey-user)
-  (current-user {}) => :session-user
-  (current-user {:apikey "123"}) => :apikey-user (provided (session/get :user) => nil))
-
-
-#_(deftest login-tests
-  (testing "can't login with bad credentials"
-           (is nil? (login "tommi" "<<hacked>>")))
-  (testing "can login with right credentials"
-           (is not (nil? (login "tommi" "abba")))))
-
-#_(deftest apikey-tests
-  (testing "can't login with bad apikey"
-           (is nil? (login-with-apikey "<<hacked>>")))
-  (testing "can login with right apikey"
-           (is not (nil? (login-with-apikey "502cb9e58426c613c8b85abc")))))
