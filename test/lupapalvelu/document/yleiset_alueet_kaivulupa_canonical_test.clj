494cb3cc624819686bf37b6671abc0629d33c6a6
Jari Asp <jari.asp@solita.fi>
2013-07-01 11:00:46 +0300

    - Added schema definitions back (for schema names) for test application's parts. - Divided further hakija and tyomaasta-vastaava.

--- test/lupapalvelu/document/yleiset_alueet_kaivulupa_canonical_test.clj
+++ test/lupapalvelu/document/yleiset_alueet_kaivulupa_canonical_test.clj
@@ -60,9 +60,25 @@
                                       :firstName "Pena",
                                       :username "pena"}}]}])
 
-;;
-;; TODO: Tämäkin on suoraan saamaa "party-public-area" tyyppiä kuin maksaja => yhteinen deffi näille?
-;;
+(def henkilo_hakija {:henkilotiedot {:etunimi {:value "Pena"},
+                                     :hetu {:modified 1372342041161, :value "260886-027R"},
+                                     :sukunimi {:value "Panaani"}},
+                     :osoite {:katu {:value "Paapankuja 12"},
+                              :postinumero {:modified 1372342044504, :value "44565"},
+                              :postitoimipaikannimi {:value "Piippola"}},
+                     :userId {:value "777777777777777777000020"},
+                     :yhteystiedot {:email {:value "pena@example.com"},
+                                    :puhelin {:value "0102030405"}}})
+
+(def yritys_hakija {:liikeJaYhteisoTunnus {:modified 1372331320811, :value "2492773-2"},
+                    :osoite {:katu {:value "Paapankuja 12"},
+                             :postinumero {:modified 1372331328334, :value "33530"},
+                             :postitoimipaikannimi {:value "Piippola"}},
+                    :vastuuhenkilo {:henkilotiedot {:etunimi {:value "Pena"}, :sukunimi {:value "Panaani"}},
+                                    :yhteystiedot {:email {:value "pena@example.com"},
+                                                   :puhelin {:value "0102030405"}}},
+                    :yritysnimi {:modified 1372331257700, :value "Yritys Oy Ab"}})
+
 (def hakija {:id "51cc1cab23e74941fee4f498",
              :created 1372331179008,
              :schema {:info
@@ -72,89 +88,73 @@
                        :type "party",
                        :order 3}},
              :data {:_selected {:modified 1372342070624, :value "yritys"},
-                    :henkilo {:henkilotiedot {:etunimi {:value "Pena"},
-                                              :hetu {:modified 1372342041161, :value "260886-027R"},
-                                              :sukunimi {:value "Panaani"}},
-                              :osoite {:katu {:value "Paapankuja 12"},
-                                       :postinumero {:modified 1372342044504, :value "44565"},
-                                       :postitoimipaikannimi {:value "Piippola"}},
-                              :userId {:value "777777777777777777000020"},
-                              :yhteystiedot {:email {:value "pena@example.com"},
-                                             :puhelin {:value "0102030405"}}},
-                    :yritys {:liikeJaYhteisoTunnus {:modified 1372331320811, :value "2492773-2"},
-                             :osoite {:katu {:value "Paapankuja 12"},
-                                      :postinumero {:modified 1372331328334, :value "33530"},
-                                      :postitoimipaikannimi {:value "Piippola"}},
-                             :vastuuhenkilo {:henkilotiedot {:etunimi {:value "Pena"}, :sukunimi {:value "Panaani"}},
-                                             :yhteystiedot {:email {:value "pena@example.com"},
-                                                            :puhelin {:value "0102030405"}}},
-                             :yritysnimi {:modified 1372331257700, :value "Yritys Oy Ab"}}}})
+                    :henkilo henkilo_hakija,
+                    :yritys yritys_hakija}})
 
-;;
-;; TODO: Tämäkin on suoraan saamaa "party-public-area" tyyppiä kuin maksaja => yhteinen deffi näille?
-;;
+(def henkilo_tyomaasta-vastaava {:henkilotiedot {:etunimi {:modified 1372341939920, :value "Pena"},
+                                                 :hetu {:modified 1372341952297, :value "260886-027R"},
+                                                 :sukunimi {:modified 1372341939920, :value "Panaani"}},
+                                 :osoite {:katu {:modified 1372341939920, :value "Paapankuja 12"},
+                                          :postinumero {:modified 1372341955504, :value "33456"},
+                                          :postitoimipaikannimi {:modified 1372341939920, :value "Piippola"}},
+                                 :userId {:modified 1372341939964, :value "777777777777777777000020"},
+                                 :yhteystiedot {:email {:modified 1372341939920, :value "pena@example.com"},
+                                                :puhelin {:modified 1372341939920, :value "0102030405"}}})
+
+(def yritys_tyomaasta-vastaava {:liikeJaYhteisoTunnus {:modified 1372331412694, :value "2492773-2"},
+                                :osoite {:katu {:modified 1372331429288, :value "Panaanikatu 6 C 33"},
+                                         :postinumero {:modified 1372331432415, :value "33450"},
+                                         :postitoimipaikannimi {:modified 1372331438285, :value "Ikaalinen"}},
+                                :vastuuhenkilo {:henkilotiedot {:etunimi {:modified 1372331444068, :value "Jarmo"},
+                                                                :sukunimi {:modified 1372331461059, :value "Panaaniberg"}},
+                                                :yhteystiedot {:email {:modified 1372331491470, :value "jarmonen@pannu.fi"},
+                                                               :puhelin {:modified 1372331474135, :value "0407890123"}}},
+                                :yritysnimi {:modified 1372331404592, :value "Vastaava Yritys"}})
+
 (def tyomaasta-vastaava {:id "51cc1cab23e74941fee4f496",
                          :created 1372331179008,
                          :schema {:info
                                   {:op {:id "51cc1cab23e74941fee4f495",
-                                        :name "yleiset-alueet-kaivuulupa",
                                         :created 1372331179008,
+                                        :name "yleiset-alueet-kaivuulupa",
                                         :operation-type "publicArea"},
                                    :name "tyomaastaVastaava",
                                    :removable true,
                                    :type "party",
                                    :order 61}},
                          :data {:_selected {:modified 1372342063565, :value "henkilo"},
-                                :henkilo {:henkilotiedot {:etunimi {:modified 1372341939920, :value "Pena"},
-                                                          :hetu {:modified 1372341952297, :value "260886-027R"},
-                                                          :sukunimi {:modified 1372341939920, :value "Panaani"}},
-                                          :osoite {:katu {:modified 1372341939920, :value "Paapankuja 12"},
-                                                   :postinumero {:modified 1372341955504, :value "33456"},
-                                                   :postitoimipaikannimi {:modified 1372341939920, :value "Piippola"}},
-                                          :userId {:modified 1372341939964, :value "777777777777777777000020"},
-                                          :yhteystiedot {:email {:modified 1372341939920, :value "pena@example.com"},
-                                                         :puhelin {:modified 1372341939920, :value "0102030405"}}},
-                                :yritys {:liikeJaYhteisoTunnus {:modified 1372331412694, :value "2492773-2"},
-                                         :osoite {:katu {:modified 1372331429288, :value "Panaanikatu 6 C 33"},
-                                                  :postinumero {:modified 1372331432415, :value "33450"},
-                                                  :postitoimipaikannimi {:modified 1372331438285, :value "Ikaalinen"}},
-                                         :vastuuhenkilo {:henkilotiedot {:etunimi {:modified 1372331444068, :value "Jarmo"},
-                                                                         :sukunimi {:modified 1372331461059, :value "Panaaniberg"}},
-                                                         :yhteystiedot {:email {:modified 1372331491470, :value "jarmonen@pannu.fi"},
-                                                                        :puhelin {:modified 1372331474135, :value "0407890123"}}},
-                                         :yritysnimi {:modified 1372331404592, :value "Vastaava Yritys"}}}})
+                                :henkilo henkilo_tyomaasta-vastaava,
+                                :yritys yritys_tyomaasta-vastaava}})
 
-(def henkilo {:henkilotiedot {:etunimi {:modified 1372341693897, :value "MaksajaPena"},
-                              :hetu {:modified 1372341552376, :value "260886-027R"},
-                              :sukunimi {:modified 1372331493576, :value "Panaani"}},
-              :osoite {:katu {:modified 1372331493576, :value "Paapankuja 12"},
-                       :postinumero {:modified 1372341557255, :value "33455"},
-                       :postitoimipaikannimi {:modified 1372331493576, :value "Piippola"}},
-              :userId {:modified 1372331493599, :value "777777777777777777000020"},
-              :yhteystiedot {:email {:modified 1372331493576, :value "pena@example.com"},
-                             :puhelin {:modified 1372331493576, :value "0102030405"}}})
+(def henkilo_maksaja {:henkilotiedot {:etunimi {:modified 1372341693897, :value "MaksajaPena"},
+                                      :hetu {:modified 1372341552376, :value "260886-027R"},
+                                      :sukunimi {:modified 1372331493576, :value "Panaani"}},
+                      :osoite {:katu {:modified 1372331493576, :value "Paapankuja 12"},
+                               :postinumero {:modified 1372341557255, :value "33455"},
+                               :postitoimipaikannimi {:modified 1372331493576, :value "Piippola"}},
+                      :userId {:modified 1372331493599, :value "777777777777777777000020"},
+                      :yhteystiedot {:email {:modified 1372331493576, :value "pena@example.com"},
+                                     :puhelin {:modified 1372331493576, :value "0102030405"}}})
 
-(def yritys {:liikeJaYhteisoTunnus {:modified 1372331520985, :value "2492773-2"},
-             :osoite {:katu {:modified 1372331533697, :value "Maksajakatu 1 a 2"},
-                      :postinumero {:modified 1372331536989, :value "33459"},
-                      :postitoimipaikannimi {:modified 1372331543428, :value "Maksajakunta"}},
-             :vastuuhenkilo {:henkilotiedot {:etunimi {:modified 1372331565465, :value "Vastuumo"},
-                                             :sukunimi {:modified 1372331561940, :value "Henkilonen"}},
-                             :yhteystiedot {:email {:modified 1372331587669, :value "maksaja@firma.fi"},
-                                            :puhelin {:modified 1372331574431, :value "0405678123"}}},
-             :yritysnimi {:modified 1372331517235, :value "Maksaja"}})
+(def yritys_maksaja {:liikeJaYhteisoTunnus {:modified 1372331520985, :value "2492773-2"},
+                     :osoite {:katu {:modified 1372331533697, :value "Maksajakatu 1 a 2"},
+                              :postinumero {:modified 1372331536989, :value "33459"},
+                              :postitoimipaikannimi {:modified 1372331543428, :value "Maksajakunta"}},
+                     :vastuuhenkilo {:henkilotiedot {:etunimi {:modified 1372331565465, :value "Vastuumo"},
+                                                     :sukunimi {:modified 1372331561940, :value "Henkilonen"}},
+                                     :yhteystiedot {:email {:modified 1372331587669, :value "maksaja@firma.fi"},
+                                                    :puhelin {:modified 1372331574431, :value "0405678123"}}},
+                     :yritysnimi {:modified 1372331517235, :value "Maksaja"}})
 
 (def laskuviite {:modified 1372331605911, :value "1234567890"})
 
-(def party-public-area {:id "51cc1cab23e74941fee4f499",
-                        :created 1372331179008,
-                        :data {:_selected {:modified 1372341924880, :value "henkilo"},
-                               :henkilo henkilo,
-                               :yritys yritys,
-                               :laskuviite laskuviite},
-                        :schema {:info {:name "yleiset-alueet-maksaja", :type "party", :order 62}}})
-
-(def maksaja party-public-area)
+(def maksaja {:id "51cc1cab23e74941fee4f499",
+              :created 1372331179008,
+              :data {:_selected {:modified 1372341924880, :value "henkilo"},
+                     :henkilo henkilo_maksaja,
+                     :yritys yritys_maksaja,
+                     :laskuviite laskuviite},
+              :schema {:info {:name "yleiset-alueet-maksaja", :type "party", :order 62}}})
 
 (def hankkeen-kuvaus {:id "51cc1cab23e74941fee4f49a",
                       :created 1372331179008,
@@ -170,16 +170,16 @@
                 tyoaika])
 
 (def application
-  {:state "open",
+  {:created 1372331179008,
+   :opened 1372331643985,
+   :modified 1372342070624,
+   :state "open",
+   :title "Latokuja 1",
+   :address "Latokuja 1",
    :location {:x 404335.789, :y 6693783.426},
    :attachments attachments,
-   :title "Latokuja 1",
-   :opened 1372331643985,
-   :created 1372331179008,
    :propertyId "75341600550007",
    :documents documents,
-   :modified 1372342070624,
-   :address "Latokuja 1",
    :municipality municipality
    ;; Statements kopioitu Rakennuslupa_canonical_test.clj:sta, joka on identtinen yleisten lueiden puolen kanssa.
    :statements [{:id "518b3ee60364ff9a63c6d6a1"
