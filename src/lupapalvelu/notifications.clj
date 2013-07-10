d9818dde074ad55b6136dcaae59008cbfa87b591
jarppe <jarppe@metosin.fi>
2013-07-10 19:15:09 +0300

    Same fix as in develop, the return value is used somewhere as rest reply.

--- src/lupapalvelu/notifications.clj
+++ src/lupapalvelu/notifications.clj
@@ -55,7 +55,8 @@
     (doseq [recipient recipients]
       (if (email/send-mail? recipient title msg)
         (info "email was sent successfully." recipients title)
-        (error "email could not be delivered." recipients title msg)))))
+        (error "email could not be delivered." recipients title msg))))
+  nil)
 
 (defn get-email-title [{:keys [title]} & [title-key]]
   (i18n/with-lang "fi"
