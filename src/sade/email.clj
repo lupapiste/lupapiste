ef12232e20369ee705aa6e3ffed208e15fa73c2c
jarppe <jarppe@metosin.fi>
2013-07-20 09:26:25 +0300

    jack shaving

--- src/sade/email.clj
+++ src/sade/email.clj
@@ -8,20 +8,16 @@
 (defn send-mail [& {:keys [from to subject html text] :or {from "lupapiste@lupapiste.fi"}}]
   (assert (or html text) "must provide at least one body")
   (assert (and to subject) "must provide both 'to' and 'subject'")
-  (let [html-body (when html {:type "text/html; charset=utf-8"
-                              :content html})
-        text-body (when text {:type "text/plain; charset=utf-8"
-                              :content text})
+  (let [html-body (when html {:content html :type "text/html; charset=utf-8"})
+        text-body (when text {:content text :type "text/plain; charset=utf-8"})
         body      (if (and html-body text-body)
                     [:alternative text-body html-body]
                     [(or html-body text-body)])
         error     (postal/send-message
                     (env/value :email)
-                    {:from    from
-                     :to      to
-                     :subject subject
-                     :body    body})]
+                    {:from     from
+                     :to       to
+                     :subject  subject
+                     :body     body})]
     (when-not (= (:error error) :SUCCESS)
       error)))
-
-
