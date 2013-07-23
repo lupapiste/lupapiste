36efb4b776a6bdc31de5fe66e15f6d8ade3561ef
jarppe <jarppe@metosin.fi>
2013-07-23 10:39:01 +0300

    s/text/plain/

--- src/sade/email.clj
+++ src/sade/email.clj
@@ -5,19 +5,19 @@
             [sade.env :as env]
             [sade.strings :as s]))
 
-(defn send-mail [& {:keys [from to subject html text] :or {from "lupapiste@lupapiste.fi"}}]
-  (assert (or html text) "must provide at least one body")
+(defn send-mail [& {:keys [from to subject plain html] :or {from "lupapiste@lupapiste.fi"}}]
+  (assert (or plain html) "must provide at least one body")
   (assert (and to subject) "must provide both 'to' and 'subject'")
-  (let [html-body (when html {:content html :type "text/html; charset=utf-8"})
-        text-body (when text {:content text :type "text/plain; charset=utf-8"})
-        body      (if (and html-body text-body)
-                    [:alternative text-body html-body]
-                    [(or html-body text-body)])
-        error     (postal/send-message
-                    (env/value :email)
-                    {:from     from
-                     :to       to
-                     :subject  subject
-                     :body     body})]
+  (let [plain-body (when plain {:content plain :type "text/plain; charset=utf-8"})
+        html-body  (when html {:content html :type "text/html; charset=utf-8"})
+        body       (if (and plain-body html-body)
+                     [:alternative plain-body html-body]
+                     [(or plain-body html-body)])
+        error      (postal/send-message
+                     (env/value :email)
+                     {:from     from
+                      :to       to
+                      :subject  subject
+                      :body     body})]
     (when-not (= (:error error) :SUCCESS)
       error)))
