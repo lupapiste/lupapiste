15e147b770ccf361116dba0c6de40acc7ad9968a
Tommi Reiman <tommi.reiman@metosin.fi>
2013-07-30 11:55:14 +0300

    cleanup

--- src/lupapalvelu/document/ymparisto_schemas.clj
+++ src/lupapalvelu/document/ymparisto_schemas.clj
@@ -1,23 +1,21 @@
 (ns lupapalvelu.document.ymparisto-schemas
   (:use [lupapalvelu.document.schemas]))
 
-
-
 (def sijainti (body simple-osoite
-                    {:name "karttapiirto" :type :text :max-len 4000}))
+                {:name "karttapiirto" :type :text :max-len 4000}))
 
 (def kesto (body {:name "kesto" :type :group
-                     :body [{:name "alku" :type :date}
-                            {:name "loppu" :type :date}
-                            {:name "kello" :type :group
-                             :body [{:name "arkisin" :type :string :size "s"}
-                                    {:name "lauantait" :type :string :size "s"}
-                                    {:name "pyhat" :type :string :size "s"}]}]}))
+                  :body [{:name "alku" :type :date}
+                         {:name "loppu" :type :date}
+                         {:name "kello" :type :group
+                          :body [{:name "arkisin" :type :string :size "s"}
+                                 {:name "lauantait" :type :string :size "s"}
+                                 {:name "pyhat" :type :string :size "s"}]}]}))
 
 
 (def kesto-mini (body {:name "kesto" :type :group
-                     :body [{:name "alku" :type :date}
-                            {:name "loppu" :type :date}]}))
+                       :body [{:name "alku" :type :date}
+                              {:name "loppu" :type :date}]}))
 
 (def meluilmoitus (body
                     {:name "rakentaminen" :type :group
@@ -26,8 +24,8 @@
                                     {:name "murskaus"}
                                     {:name "paalutus"}]}
                             {:name "muu-rakentaminen" :type :string :size "m"}
-                     {:name "kuvaus" :type :text :max-len 4000}
-                     {:name "koneet" :type :text :max-len 4000}]}
+                            {:name "kuvaus" :type :text :max-len 4000}
+                            {:name "koneet" :type :text :max-len 4000}]}
 
                     {:name "tapahtuma" :type :group
                      :body [{:name "nimi" :type :string :size "m"}
@@ -41,27 +39,29 @@
 
 (def pima (body {:name "kuvaus" :type :text :max-len 4000}))
 
-(def ottamismaara (body {:name "kokonaismaara" :type :string :unit "m3" :size "m"}
-                        {:name "vuotuinenOtto" :type :string :unit "m3" :size "m"}
-                        {:name "ottamisaika" :type :string :unit "y" :size "m"}))
+(def ottamismaara (body
+                    {:name "kokonaismaara" :type :string :unit "m3" :size "m"}
+                    {:name "vuotuinenOtto" :type :string :unit "m3" :size "m"}
+                    {:name "ottamisaika" :type :string :unit "y" :size "m"}))
 
-(def ottamis-suunnitelma (body {:name "selvitykset" :type :group
-                                :body [{:name "toimenpiteet" :type :text :max-len 4000}
-                                       {:name "tutkimukset" :type :text :max-len 4000}
-                                       {:name "ainesLaatu" :type :text :max-len 4000}
-                                       {:name "ainesMaara" :type :text :max-len 4000}]}
-                               {:name "Luonnonolot" :type :group
-                                :body [{:name "maisemakuva" :type :text :max-len 4000}
-                                       {:name "kasvillisuusJaElaimmisto" :type :text :max-len 4000}
-                                       {:name "kaavoitustilanne" :type :text :max-len 4000}]}
-                               {:name "pohjavesiolot" :type :group
-                                :body [{:name "luokitus" :type :text :max-len 4000}
-                                       {:name "suojavyohykkeet" :type :text :max-len 4000}]}
-                               {:name "vedenottamot" :type :text :max-len 4000}
-                               {:name "vakuus" :type :select
-                                :body [{:name "EiAloiteta"}
-                                       {:name "Rahaa"}
-                                       {:name "Pankkitakaus"}]}))
+(def ottamis-suunnitelma (body
+                           {:name "selvitykset" :type :group
+                            :body [{:name "toimenpiteet" :type :text :max-len 4000}
+                                   {:name "tutkimukset" :type :text :max-len 4000}
+                                   {:name "ainesLaatu" :type :text :max-len 4000}
+                                   {:name "ainesMaara" :type :text :max-len 4000}]}
+                           {:name "Luonnonolot" :type :group
+                            :body [{:name "maisemakuva" :type :text :max-len 4000}
+                                   {:name "kasvillisuusJaElaimmisto" :type :text :max-len 4000}
+                                   {:name "kaavoitustilanne" :type :text :max-len 4000}]}
+                           {:name "pohjavesiolot" :type :group
+                            :body [{:name "luokitus" :type :text :max-len 4000}
+                                   {:name "suojavyohykkeet" :type :text :max-len 4000}]}
+                           {:name "vedenottamot" :type :text :max-len 4000}
+                           {:name "vakuus" :type :select
+                            :body [{:name "EiAloiteta"}
+                                   {:name "Rahaa"}
+                                   {:name "Pankkitakaus"}]}))
 
 (defschemas
   [{:info {:name "meluilmoitus"
@@ -93,5 +93,5 @@
    {:info {:name "paatoksen-toimitus"
            :order 9999}
     :body [{:name "paatoksenToimittaminen" :type :select
-           :body [{:name "Noudetaan"}
-                  {:name "Postitetaan"}]}]}])
+            :body [{:name "Noudetaan"}
+                   {:name "Postitetaan"}]}]}])
