46d6669e810f9dc2b24f39f3172804f0545803cf
Tommi Reiman <tommi.reiman@metosin.fi>
2013-08-02 11:31:17 +0300

    cleanup operations

--- src/lupapalvelu/operations.clj
+++ src/lupapalvelu/operations.clj
@@ -5,69 +5,95 @@
             [lupapalvelu.document.ymparisto-schemas :as ympschemas]
             [lupapalvelu.document.yleiset-alueet-schemas :as yleiset-alueet]
             [lupapalvelu.domain :as domain]
+            [lupapalvelu.permit :as permit]
             [lupapalvelu.core :refer [fail]]
             [sade.env :as env]))
 
 (def default-description "operations.tree.default-description")
 
-(def ^:private operations-tree
-  (concat [["Rakentaminen ja purkaminen" [["Uuden rakennuksen rakentaminen" [["Asuinrakennus" :asuinrakennus]
-                                                                     ["Vapaa-ajan asuinrakennus" :vapaa-ajan-asuinrakennus]
-                                                                     ["Varasto, sauna, autotalli tai muu talousrakennus" :varasto-tms]
-                                                                     ["Julkinen rakennus" :julkinen-rakennus]
-                                                                     ["Muun rakennuksen rakentaminen" :muu-uusi-rakentaminen]]]
-                                  ["Rakennuksen korjaaminen tai muuttaminen" [["Rakennuksen laajentaminen tai korjaaminen" :laajentaminen]
-                                                                              ["Perustusten tai kantavien rakenteiden muuttaminen tai korjaaminen" :perus-tai-kant-rak-muutos]
-                                                                              ["Kayttotarkoituksen muutos" :kayttotark-muutos]
-                                                                              ["Rakennuksen julkisivun tai katon muuttaminen" :julkisivu-muutos]
-                                                                              ["Asuinhuoneiston jakaminen tai yhdistaminen" :jakaminen-tai-yhdistaminen]
-                                                                              ["Markatilan laajentaminen" :markatilan-laajentaminen]
-                                                                              ["Takan ja savuhormin rakentaminen" :takka-tai-hormi]
-                                                                              ["Parvekkeen tai terassin lasittaminen" :parveke-tai-terassi]
-                                                                              ["Muu rakennuksen muutostyo" :muu-laajentaminen]]]
-                                  ["Rakennelman rakentaminen" [["Auto- tai grillikatos, vaja, kioski tai vastaava" :auto-katos]
-                                                               ["Masto, piippu, sailio, laituri tai vastaava" :masto-tms]
-                                                               ["Mainoslaite" :mainoslaite]
-                                                               ["Aita" :aita]
-                                                               ["Maalampokaivon poraaminen tai lammonkeruuputkiston asentaminen" :maalampo]
-                                                               ["Rakennuksen jatevesijarjestelman uusiminen" :jatevesi]
-                                                               ["Muun rakennelman rakentaminen" :muu-rakentaminen]]]
-                                  ["Rakennuksen purkaminen" :purkaminen]]]
-   ["Elinympariston muuttaminen" [["Maisemaa muutava toimenpide" [["Kaivaminen, louhiminen tai maan tayttaminen" :kaivuu]
-                                                                  ["Puun kaataminen" :puun-kaataminen]
-                                                                  ["Muu maisemaa muuttava toimenpide" :muu-maisema-toimenpide]]]
-                                  ["Tontti tai korttelialueen jarjestelymuutos" [["Tontin ajoliittyman muutos" :tontin-ajoliittyman-muutos]
-                                                                  ["Paikoitusjarjestelyihin liittyvat muutokset" :paikoutysjarjestus-muutos]
-                                                                  ["Korttelin yhteisiin alueisiin liittyva muutos" :kortteli-yht-alue-muutos]
-                                                                  ["Muu-tontti-tai-korttelialueen-jarjestelymuutos" :muu-tontti-tai-kort-muutos]]]]]]
-          (when (env/feature? :poikkari) [["Poikkeusluvat ja suunnittelutarveratkaisut" [["Poikkeuslupa" :poikkeuslupa]
-                                                                                        ["Suunnittelutarveratkaisu" :suunnittelutarveratkaisu]]]])
-          (when (env/feature? :ymparisto) [["Ymp\u00e4rist\u00f6luvat" [["Meluilmoitus" :meluilmoitus]
-                                                                        ["Pima" :pima]
-                                                                        ["maa-ainesten_ottaminen" :maa-aineslupa]]]])
-          (when (env/feature? :yleiset-alueet) [["yleisten-alueiden-luvat" [["kaivuulupa" :yleiset-alueet-kaivuulupa]
-                                                                            ["kayttolupa"
-                                                                             [["tyomaasuojat-ja-muut-rakennelmat" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["mainoslaitteet-ja-opasteviitat" :mainostus-ja-viitoituslupa]
-                                                                              ["muut-yleisten-alueiden-tilojen-kaytot" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["messujen-ja-tapahtumien-alueiden-kaytot" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["kadulta-tapahtuvat-nostot" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["kiinteistojen-tyot-jotka-varaavat-yleisen-alueen-tyomaaksi" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["rakennustelineet-kadulla" :yleiset-alueet-kayttolupa] ;; TODO
-                                                                              ["muu-kayttolupa" :yleiset-alueet-kayttolupa]]] ;; TODO
-                                                                            ["sijoituslupa"
-                                                                             [["pysyvien-maanalaisten-rakenteiden-sijoittaminen" :yleiset-alueet-sijoituslupa] ;; TODO
-                                                                              ["pysyvien-maanpaallisten-rakenteiden-sijoittaminen" :yleiset-alueet-sijoituslupa] ;; TODO
-                                                                              ["muu-sijoituslupa" :yleiset-alueet-sijoituslupa]] ;; TODO
-                                                                            #_["liikennetta-haittaavan-tyon-lupa" :liikennetta-haittaavan-tyon-lupa] ;; TODO
-                                                                            ]]]])))
+(def ^:private operations-for-R
+  {:permit-type permit/R
+   :tree [["Rakentaminen ja purkaminen"
+           [["Uuden rakennuksen rakentaminen"
+             [["Asuinrakennus" :asuinrakennus]
+              ["Vapaa-ajan asuinrakennus" :vapaa-ajan-asuinrakennus]
+              ["Varasto, sauna, autotalli tai muu talousrakennus" :varasto-tms]
+              ["Julkinen rakennus" :julkinen-rakennus]
+              ["Muun rakennuksen rakentaminen" :muu-uusi-rakentaminen]]]
+            ["Rakennuksen korjaaminen tai muuttaminen"
+             [["Rakennuksen laajentaminen tai korjaaminen" :laajentaminen]
+              ["Perustusten tai kantavien rakenteiden muuttaminen tai korjaaminen" :perus-tai-kant-rak-muutos]
+              ["Kayttotarkoituksen muutos" :kayttotark-muutos]
+              ["Rakennuksen julkisivun tai katon muuttaminen" :julkisivu-muutos]
+              ["Asuinhuoneiston jakaminen tai yhdistaminen" :jakaminen-tai-yhdistaminen]
+              ["Markatilan laajentaminen" :markatilan-laajentaminen]
+              ["Takan ja savuhormin rakentaminen" :takka-tai-hormi]
+              ["Parvekkeen tai terassin lasittaminen" :parveke-tai-terassi]
+              ["Muu rakennuksen muutostyo" :muu-laajentaminen]]]
+            ["Rakennelman rakentaminen"
+             [["Auto- tai grillikatos, vaja, kioski tai vastaava" :auto-katos]
+              ["Masto, piippu, sailio, laituri tai vastaava" :masto-tms]
+              ["Mainoslaite" :mainoslaite]
+              ["Aita" :aita]
+              ["Maalampokaivon poraaminen tai lammonkeruuputkiston asentaminen" :maalampo]
+              ["Rakennuksen jatevesijarjestelman uusiminen" :jatevesi]
+              ["Muun rakennelman rakentaminen" :muu-rakentaminen]]]
+            ["Rakennuksen purkaminen" :purkaminen]]]
+           ["Elinympariston muuttaminen"
+            [["Maisemaa muutava toimenpide"
+              [["Kaivaminen, louhiminen tai maan tayttaminen" :kaivuu]
+               ["Puun kaataminen" :puun-kaataminen]
+               ["Muu maisemaa muuttava toimenpide" :muu-maisema-toimenpide]]]
+             ["Tontti tai korttelialueen jarjestelymuutos"
+              [["Tontin ajoliittyman muutos" :tontin-ajoliittyman-muutos]
+               ["Paikoitusjarjestelyihin liittyvat muutokset" :paikoutysjarjestus-muutos]
+               ["Korttelin yhteisiin alueisiin liittyva muutos" :kortteli-yht-alue-muutos]
+               ["Muu-tontti-tai-korttelialueen-jarjestelymuutos" :muu-tontti-tai-kort-muutos]]]]]]})
 
+(def ^:private operations-for-YA
+  {:permit-type permit/YA
+   :tree [["yleisten-alueiden-luvat"
+           [["kaivuulupa" :yleiset-alueet-kaivuulupa]
+            ["kayttolupa"
+             [["tyomaasuojat-ja-muut-rakennelmat" :yleiset-alueet-kayttolupa] ;; TODO
+              ["mainoslaitteet-ja-opasteviitat" :mainostus-ja-viitoituslupa]
+              ["muut-yleisten-alueiden-tilojen-kaytot" :yleiset-alueet-kayttolupa] ;; TODO
+              ["messujen-ja-tapahtumien-alueiden-kaytot" :yleiset-alueet-kayttolupa] ;; TODO
+              ["kadulta-tapahtuvat-nostot" :yleiset-alueet-kayttolupa] ;; TODO
+              ["kiinteistojen-tyot-jotka-varaavat-yleisen-alueen-tyomaaksi" :yleiset-alueet-kayttolupa] ;; TODO
+              ["rakennustelineet-kadulla" :yleiset-alueet-kayttolupa] ;; TODO
+              ["muu-kayttolupa" :yleiset-alueet-kayttolupa]]] ;; TODO
+            ["sijoituslupa"
+             [["pysyvien-maanalaisten-rakenteiden-sijoittaminen" :yleiset-alueet-sijoituslupa] ;; TODO
+              ["pysyvien-maanpaallisten-rakenteiden-sijoittaminen" :yleiset-alueet-sijoituslupa] ;; TODO
+              ["muu-sijoituslupa" :yleiset-alueet-sijoituslupa]] ;; TODO
+             #_["liikennetta-haittaavan-tyon-lupa" :liikennetta-haittaavan-tyon-lupa] ;; TODO
+             ]]]]})
+
+(def ^:private operations-for-P
+  {:permit-type permit/P
+   :tree [["Poikkeusluvat ja suunnittelutarveratkaisut"
+           [["Poikkeuslupa" :poikkeuslupa]
+            ["Suunnittelutarveratkaisu" :suunnittelutarveratkaisu]]]]})
+
+(def ^:private operations-for-Y
+  {:permit-type permit/Y
+   :tree [["Ymp\u00e4rist\u00f6luvat"
+           [["Meluilmoitus" :meluilmoitus]
+            ["Pima" :pima]
+            ["maa-ainesten_ottaminen" :maa-aineslupa]]]]})
+
+(defn all-operations []
+  (concat operations-for-R
+    (when (env/feature? :poikkari) operations-for-P)
+    (when (env/feature? :ymparisto) operations-for-Y)
+    (when (env/feature? :yleiset-alueet) operations-for-YA)))
 
 ;; TODO: implement
-(defn municipality-operations [municipality] operations-tree)
+(defn municipality-operations [municipality] (all-operations))
 
 ;; TODO: implement
-(defn organization-operations [organization-id] operations-tree)
+(defn organization-operations [organization-id] (all-operations))
 
 ; Operations must be the same as in the tree structure above.
 ; Mappings to schemas and attachments are currently random.
