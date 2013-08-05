89ce9608483422a4bdbf215bb720230ce1dedbde
jarppe <jarppe@metosin.fi>
2013-08-05 20:23:16 +0300

    defmigration also defines a function is callers namespace that executes the migration.

--- src/lupapalvelu/migration/core.clj
+++ src/lupapalvelu/migration/core.clj
@@ -12,32 +12,6 @@
 (defn- ->assertion  [body]
   `(fn [] ~@(map #(cons 'assert (list %)) body)))
 
-(defmacro defmigration
-  "Defines a migration. First argument is a migration name, followed by (optional) map with options,
-   followed by a body of actual migration code.
-   Supported options are:
-     :pre   A vector of pre assertions
-     :post  A vector of post assertions
-     :apply-when  A body of a function
-   The 'apply-when' function is evaluated before migration is executed. If the result of the migration
-   is truthy the migration is executed, otherwise the migration is skipped. After the migration has
-   been executed the 'apply-when' is evaluated for the second time. If it still evaluates to truthy
-   migration is considered to be a failure."
-  [migration-name & body]
-  (let [name-str       (name migration-name)
-        order          (swap! migration-order inc)
-        has-opts?      (and (map? (first body)) (> (count body) 1))
-        {:keys [pre post apply-when]} (when has-opts? (first body))
-        pre            (->assertion pre)
-        post           (->assertion post)
-        body           (if has-opts? (rest body) body)]
-    `(swap! migrations assoc ~name-str {:name ~name-str
-                                        :order ~order
-                                        :pre ~pre
-                                        :post ~post
-                                        :apply-when (when (quote ~apply-when) (fn [] ~apply-when))
-                                        :fn (fn [] (do ~@body))})))
-
 (defn migration-history []
   (q/with-collection "migrations"
     (q/sort {:time 1})))
@@ -80,3 +54,33 @@
   (let [all-migrations (sort-by :order (vals @migrations))
         executed-migration-names (set (map :name (filter :ok (migration-history))))]
     (filter (comp (complement executed-migration-names) :name) all-migrations)))
+
+(defmacro defmigration
+  "Defines a migration. First argument is a migration name, followed by (optional) map with options,
+   followed by a body of actual migration code.
+   Supported options are:
+     :pre   A vector of pre assertions
+     :post  A vector of post assertions
+     :apply-when  A body of a function
+   The 'apply-when' function is evaluated before migration is executed. If the result of the migration
+   is truthy the migration is executed, otherwise the migration is skipped. After the migration has
+   been executed the 'apply-when' is evaluated for the second time. If it still evaluates to truthy
+   migration is considered to be a failure."
+  [migration-name & body]
+  (let [name-str       (name migration-name)
+        order          (swap! migration-order inc)
+        has-opts?      (and (map? (first body)) (> (count body) 1))
+        {:keys [pre post apply-when]} (when has-opts? (first body))
+        pre            (->assertion pre)
+        post           (->assertion post)
+        body           (if has-opts? (rest body) body)]
+    `(do
+       (swap! migrations assoc ~name-str {:name ~name-str
+                                        :order ~order
+                                        :pre ~pre
+                                        :post ~post
+                                        :apply-when (when (quote ~apply-when) (fn [] ~apply-when))
+                                        :fn (fn [] (do ~@body))})
+       (defn ~migration-name []
+         (execute-migration! ~(name name-str))))))
+
