87bad38978e052481a0048d9b5cef8c8c0c53cc2
jarppe <jarppe@metosin.fi>
2013-08-05 20:37:54 +0300

    tuning migration

--- src/lupapalvelu/migration/migrations.clj
+++ src/lupapalvelu/migration/migrations.clj
@@ -8,13 +8,10 @@
   (mongo/update :applications {:permitType {$exists false}} {$set {:permitType "R"}} :multi true))  
 
 (defmigration add-scope-to-organizations
-  (let [organizations                        (mongo/select :organizations)
-        without-scope                        (filter (comp not :scope) organizations)
-        organizations-without-scope-before   (count without-scope)]
+  {:apply-when (pos? (mongo/count :organizations {:scope {$exists false}}))}
+  (let [without-scope (mongo/select :organizations {:scope {$exists false}})]
     (doseq [{:keys [id municipalities]} without-scope]
       (let [scopes (map (fn [municipality] {:municipality municipality :permitType "R"}) municipalities)]
         (mongo/update-by-id :organizations id {$set {:scope scopes}})))
-    {:organizations-total                 (count organizations)
-     :organizations-without-scope-before  organizations-without-scope-before
-     :organizations-without-scope-after   (mongo/count :organizations {:scope {$exists false}})}))
-
+    {:fixed-organizations   (count without-scope)
+     :organizations-total   (mongo/count :organizations)}))
