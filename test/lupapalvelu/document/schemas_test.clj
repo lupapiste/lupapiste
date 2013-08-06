5b57857730217907da949027c06396a407f06e8e
Tommi Reiman <tommi.reiman@metosin.fi>
2013-08-06 12:53:22 +0300

    .

--- test/lupapalvelu/document/schemas_test.clj
+++ test/lupapalvelu/document/schemas_test.clj
@@ -1,3 +1,25 @@
 (ns lupapalvelu.document.schemas-test
   (:use [lupapalvelu.document.schemas]
         [midje.sweet]))
+
+(facts "Facts about to-map-by-name"
+  (fact (to-map-by-name []) => {})
+  (fact (to-map-by-name [{:info {:name "a"}}]) => {"a" {:info {:name "a"}}})
+  (fact (to-map-by-name
+          [{:info {:name "a"}}
+           {:info {:name "b"}}])
+        =>
+          {"a" {:info {:name "a"}}
+           "b" {:info {:name "b"}}}))
+
+(facts "body"
+  (fact "flattens stuff into lists"    (body 1 2 [3 4] 5) => [1 2 3 4 5])
+  (fact "does not flatten recursively" (body 1 2 [3 4 [5]]) => [1 2 3 4 [5]]))
+
+(facts "repeatable"
+  (fact (repeatable "beers" {:name :beer
+                             :type :string}) => [{:name "beers"
+                                                  :type :group
+                                                  :repeating true
+                                                  :body [{:name :beer
+                                                          :type :string}]}]))
