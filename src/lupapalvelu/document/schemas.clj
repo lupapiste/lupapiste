4aa2732e95473161bb8aa51cff576a5b5ff27b42
Tommi Reiman <tommi.reiman@metosin.fi>
2013-08-06 12:28:01 +0300

    .

--- src/lupapalvelu/document/schemas.clj
+++ src/lupapalvelu/document/schemas.clj
@@ -117,9 +117,10 @@
                        henkilotiedot-minimal
                        yhteystiedot)}))
 
-(def party [{:name "_selected" :type :radioGroup :body [{:name "henkilo"} {:name "yritys"}]}
-            {:name "henkilo" :type :group :body henkilo}
-            {:name "yritys" :type :group :body yritys}])
+(def party (body
+             {:name "_selected" :type :radioGroup :body [{:name "henkilo"} {:name "yritys"}]}
+             {:name "henkilo" :type :group :body henkilo}
+             {:name "yritys" :type :group :body yritys}))
 
 (def party-with-required-hetu (body
                                 [{:name "_selected" :type :radioGroup :body [{:name "henkilo"} {:name "yritys"}]}
