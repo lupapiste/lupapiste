3894104fae7f0f301d631fc8859c02390a4ede29
Tommi Reiman <tommi.reiman@metosin.fi>
2013-08-07 15:55:27 +0300

    .

--- src/lupapalvelu/notifications.clj
+++ src/lupapalvelu/notifications.clj
@@ -143,7 +143,6 @@
       (send-mail-to-recipients! recipients title msg))))
 
 (defn send-notifications-on-new-targetted-comment! [application to-email host]
-  (println "******" to-email)
   (let [msg        (get-message-for-new-comment application host)
         title      (get-email-title application "new-comment")]
     (send-mail-to-recipients! [to-email] title msg)))
