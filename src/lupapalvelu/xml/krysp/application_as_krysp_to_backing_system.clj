59e34fa130e7ac95d5442a74d92740a69ad0d27c
Jari Asp <jari.asp@solita.fi>
2013-08-08 10:38:45 +0300

    Removed duplication.

--- src/lupapalvelu/xml/krysp/application_as_krysp_to_backing_system.clj
+++ src/lupapalvelu/xml/krysp/application_as_krysp_to_backing_system.clj
@@ -5,7 +5,8 @@
 
 (defn save-application-as-krysp [application lang submitted-application organization]
   (assert (= (:id application) (:id submitted-application)) "Not same application ids.")
-  (let [permit-type (keyword (permit/permit-type application))]
-    (condp = permit-type
-      :YA (ya-mapping/save-application-as-krysp application (-> command :data :lang) submitted-application organization)
-      :R  (rl-mapping/save-application-as-krysp application (-> command :data :lang) submitted-application organization))))
+  (let [permit-type (keyword (permit/permit-type application))
+        krysp-fn (condp = permit-type
+                   :YA ya-mapping/save-application-as-krysp
+                   :R  rl-mapping/save-application-as-krysp)]
+    (krysp-fn application (-> command :data :lang) submitted-application organization))
