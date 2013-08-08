bdc887d8a5e10360f1d1d30f4b786253fafcb608
Tommi Reiman <tommi.reiman@metosin.fi>
2013-08-08 09:57:08 +0300

    irrevant it is.

--- itest/lupapalvelu/application_itest.clj
+++ itest/lupapalvelu/application_itest.clj
@@ -176,7 +176,7 @@
   (let [{id :id}  (create-and-submit-application pena)]
     (fact "applicant can't comment with to"
       pena => (not-allowed? :can-target-comment-to-authority)
-      pena => (not-allowed? :add-comment :id id :to sonja-id)
+      pena => (not-allowed? :add-comment :id id :to irrelevant)
       (command pena :add-comment :id id :text "comment1" :target "application") => ok?
       (command pena :add-comment :id id :text "comment1" :target "application" :to sonja-id) => not-ok?)
     (fact "authority can comment with to"
