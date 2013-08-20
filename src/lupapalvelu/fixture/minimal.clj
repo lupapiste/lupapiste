f8be1268d204edac23f526f2545017143bcf7d83
Jari Asp <jari.asp@solita.fi>
2013-08-20 16:57:47 +0300

    Removed :municipalities key from organizations, as it is not used anymore.

--- src/lupapalvelu/fixture/minimal.clj
+++ src/lupapalvelu/fixture/minimal.clj
@@ -243,7 +243,6 @@
 
 (def organizations [{:id "186-R"
                      :name {:fi "J\u00E4rvenp\u00E4\u00E4n rakennusvalvonta"}
-                     :municipalities ["186"]
                      :scope [{:municipality "186" :permitType "R"}]
                      :links [{:name {:fi "J\u00E4rvenp\u00E4\u00E4" :sv "Tr\u00E4skenda"}
                               :url "http://www.jarvenpaa.fi"}
@@ -251,7 +250,6 @@
                               :url "http://www.jarvenpaa.fi/sivu/index.tmpl?sivu_id=182"}]}
                     {:id "753-R"
                      :name {:fi "Sipoon rakennusvalvonta"}
-                     :municipalities ["753"]
                      :scope [{:municipality "753" :permitType "R"}]
                      :links [{:name {:fi "Sipoo", :sv "Sibbo"}
                               :url "http://sipoo.fi"}
@@ -274,7 +272,6 @@
                                          :name "Sonja Sibbo"}]}
                     {:id "753-YA"
                      :name {:fi "Sipoon rakennusvalvonta"}
-                     :municipalities ["753"]
                      :scope [{:municipality "753" :permitType "YA"}]
                      :links [{:name {:fi "Sipoo", :sv "Sibbo"}
                               :url "http://sipoo.fi"}
@@ -290,7 +287,6 @@
 
                     {:id "753-P"
                      :name {:fi "Sipoon poikkari"}
-                     :municipalities ["753"]
                      :scope [{:municipality "753" :permitType "P"}]
                      :links [{:name {:fi "Sipoo", :sv "Sibbo"}
                               :url "http://sipoo.fi"}
@@ -306,7 +302,6 @@
 
                     {:id "837-R"
                      :name {:fi "Tampereen rakennusvalvonta"}
-                     :municipalities ["837"]
                      :scope [{:municipality "837" :permitType "R"}]
                      :links [{:name {:fi "Tampere" :sv "Tammerfors"}
                               :url "http://tampere.fi"}
@@ -316,7 +311,6 @@
                               :url "http://www.tampere.fi/asuminenjarakentaminen/rakennusvalvonta/lomakkeet.html"}]}
                     {:id "638-R"
                      :name {:fi "Porvoon rakennusvalvonta"}
-                     :municipalities ["638"]
                      :scope [{:municipality "638" :permitType "R"}]
                      :links [{:name {:fi "Porvoo", :sv "Borg\u00e5"}
                               :url "http://www.porvoo.fi"}
@@ -324,7 +318,6 @@
                               :url "http://www.porvoo.fi/fi/haku/palveluhakemisto/?a=viewitem&itemid=1030"}]}
                     {:id "564-R"
                      :name {:fi "Oulun rakennusvalvonta"}
-                     :municipalities ["564"]
                      :scope [{:municipality "564" :permitType "R"}]
                      :links [{:name {:fi "Oulu", :sv "Ule\u00E5borg"}
                               :url "http://www.ouka.fi"}
@@ -332,11 +325,9 @@
                               :url "http://oulu.ouka.fi/rakennusvalvonta/"}]}
                     {:id "529-R"
                      :name {:fi "Naantalin rakennusvalvonta"}
-                     :municipalities ["529"]
                      :scope [{:municipality "529" :permitType "R"}]}
                     {:id "069-R"
                      :name {:fi "Peruspalvelukuntayhtym\u00E4 Sel\u00E4nne"}
-                     :municipalities ["069","317","626","691"]
                      :scope [{:municipality "069" :permitType "R"}
                              {:municipality "317" :permitType "R"}
                              {:municipality "626" :permitType "R"}
