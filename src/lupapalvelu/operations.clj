953f0cb43d6002996fd6d30517b75cc858d58801
jarppe <jarppe@metosin.fi>
2013-09-01 12:40:10 +0300

    Moved test to unit-tests.

--- src/lupapalvelu/operations.clj
+++ src/lupapalvelu/operations.clj
@@ -313,16 +313,6 @@
     (throw (Exception. (format "Operation %s does not have permit-type set." op)))))
 
 ;;
-;; Sanity scheck
-;;
-
-; TODO: Move to unit-tests:
-(doseq [[op {:keys [schema required]}] operations
-        schema (cons schema required)]
-  (if-not (schemas/get-schema schema)
-    (throw (Exception. (format "Operation '%s' refers to missing schema '%s'" op schema)))))
-
-;;
 ;; Actions
 ;;
 
