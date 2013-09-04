8244698ee12f6c1a30cfeedf49f64b6728a51f4f
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2013-09-04 10:15:01 +0300

    Added deep-find

--- src/lupapalvelu/document/tools.clj
+++ src/lupapalvelu/document/tools.clj
@@ -141,3 +141,20 @@
   [schema element-name]
   (assoc schema :body (schema-body-without-element-by-name (:body schema) element-name)))
 
+(defn deep-find
+  "Finds 0..n locations in the m structured where target is found. Target can be any deep vector of key values.
+   Returns list of vectors that first value contains key to location and second val is value found in."
+  ([m target]
+  (deep-find m target [] []))
+  ([m target current-location result]
+  (if (get-in m target)
+    (conj result [current-location (get-in m target)])
+    (reduce concat (for [[k v] m]
+                     (if (map? v)
+                       (if (not (:value v))
+                         (concat result (deep-find v target (conj current-location k) result))
+                         result)))))))
+
+
+
+
