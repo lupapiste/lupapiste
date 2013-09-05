54c61df47d52132e713f4e9560d929e779816584
jarppe <jarppe@metosin.fi>
2013-09-05 14:10:56 +0300

    migration for removing schema.

--- src/lupapalvelu/migration/migrations.clj
+++ src/lupapalvelu/migration/migrations.clj
@@ -53,3 +53,15 @@
 (defmigration add-enableds-for-inforequest-and-new-application-to-organizations
   (add-key-to-database :inforequest-enabled)
   (add-key-to-database :new-application-enabled))
+
+(defn drop-schema-data [document]
+  (let [schema-info (-> document :schema :info (assoc :version 1))]
+    (-> document
+      (assoc :schema-info schema-info)
+      (dissoc :schema))))
+
+(defmigration schemas-be-gonez
+  {:apply-when (pos? (mongo/count :applications {:schema-version {$exists false}}))}
+  (doseq [application (mongo/select :applications {:schema-version {$exists false}} {:documents true})]
+    (mongo/update-by-id :applications (:id application) {$set {:schema-version 1
+                                                               :documents (map drop-schema-data (:documents application))}})))
