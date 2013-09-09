36d7a5e1f916fa8d4ff24f182ea8ef375da994cc
Jari Asp <jari.asp@solita.fi>
2013-09-09 16:12:36 +0300

    Removed the feature flags: "neighbors" and "targetted-comments".

--- src/lupapalvelu/application.clj
+++ src/lupapalvelu/application.clj
@@ -274,7 +274,7 @@
   (when (and to (not (security/authority? user)))
     (fail :error.to-settable-only-by-authority)))
 
-(defquery can-target-comment-to-authority {:roles [:authority] :feature [:targetted-comments]})
+(defquery can-target-comment-to-authority {:roles [:authority]})
 
 (defcommand add-comment
   {:parameters [:id :text :target]
