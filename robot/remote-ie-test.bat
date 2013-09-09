871e4e6fcec028aa5888cfaee5e81ba36b0aa856
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-09-09 09:37:12 +0300

    Run tests on hubert instead of bianca

--- robot/remote-ie-test.bat
+++ robot/remote-ie-test.bat
@@ -4,4 +4,8 @@
 @set server=%2
 @if [%server%]==[] set server=lupadev.solita.fi
 
-pybot -d target --exclude integration --exclude fail --variable BROWSER:internetexplorer --variable SELENIUM:http://192.168.7.223:4444/wd/hub --variable SERVER:http://%server% %target%
+set hubert=192.168.7.223
+
+set remote=%hubert%
+
+pybot -d target --exclude integration --exclude fail --variable BROWSER:internetexplorer --variable SELENIUM:http://%remote%:4444/wd/hub --variable SERVER:http://%server% %target%
