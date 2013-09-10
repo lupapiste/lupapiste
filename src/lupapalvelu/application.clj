a1b602d23d1cb4068e8982403179d61b95e7e2b5
Timo Lehtonen <timo.lehtonen@solita.fi>
2013-09-10 15:16:46 +0300

    use model updates instead of mongo/update

--- src/lupapalvelu/application.clj
+++ src/lupapalvelu/application.clj
@@ -675,19 +675,16 @@
     (fn [{:keys [organization propertyId] :as application}]
       (if-let [legacy (organization/get-legacy organization)]
         (let [doc-name     "rakennuksen-muuttaminen"
-              document     (domain/get-document-by-id (:documents application) documentId)
+              document     (domain/get-document-by-id application documentId)
               old-body     (:data document)
               kryspxml     (krysp/building-xml legacy propertyId)
               new-body     (or (krysp/->rakennuksen-tiedot kryspxml buildingId) {})
-              with-value-metadata (tools/timestamped (add-value-metadata new-body {:source :krysp}) created)]
-          ;; TODO: update via model
-          (mongo/update
-            :applications
-            {:_id (:id application)
-             :documents {$elemMatch {:id documentId}}}
-            {$set {:documents.$.data with-value-metadata
-                   :modified created}})
-          (ok))
+              with-value-metadata (tools/timestamped (add-value-metadata new-body {:source :krysp}) created)
+              updates      (-> (or (krysp/->rakennuksen-tiedot kryspxml buildingId) {}) tools/unwrapped tools/path-vals)]
+          (do
+            (infof "merging data into %s %s" doc-name documentId)
+            (commands/persist-model-updates id document updates created)
+            (ok)))
         (fail :no-legacy-available)))))
 
 (defcommand get-building-info-from-legacy
