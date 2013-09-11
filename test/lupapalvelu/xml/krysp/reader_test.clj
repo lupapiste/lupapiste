dae871e3f507994e411826db993e84aa7a3bffbd
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-09-11 13:53:22 +0300

    Got sample from CGI, added initial test

--- test/lupapalvelu/xml/krysp/reader_test.clj
+++ test/lupapalvelu/xml/krysp/reader_test.clj
@@ -12,85 +12,108 @@
 (fact "property-equals returns url-encoded xml-encoded data"
   (property-equals "<a>" "<b>") => "%3CPropertyIsEqualTo%3E%3CPropertyName%3E%26lt%3Ba%26gt%3B%3C%2FPropertyName%3E%3CLiteral%3E%26lt%3Bb%26gt%3B%3C%2FLiteral%3E%3C%2FPropertyIsEqualTo%3E")
 
-(let [xml (sade.xml/parse (slurp "resources/public/krysp/permit.xml"))
+(facts "KRYSP verdict"
+  (let [xml (sade.xml/parse (slurp "resources/public/krysp/permit.xml"))
       cases (->permits xml)]
 
-  (fact "xml is parsed" cases => truthy)
-  (fact "xml has 2 cases" (count cases) => 2)
-  (fact "second case has 2 permits" (-> cases last count) => 2)
+    (fact "xml is parsed" cases => truthy)
+    (fact "xml has 2 cases" (count cases) => 2)
+    (fact "second case has 2 permits" (-> cases last :paatokset count) => 2)
 
-  (fact "kuntalupatunnus" (:kuntalupatunnus (last cases)) => "13-0185-R")
+    (fact "kuntalupatunnus" (:kuntalupatunnus (last cases)) => "13-0185-R")
 
-  (let [permit (first (:paatokset (last cases)))
-        lupamaaraykset (:lupamaaraykset permit)
-        paivamaarat    (:paivamaarat permit)
-        poytakirjat    (:poytakirjat permit)]
+    (let [permit (first (:paatokset (last cases)))
+          lupamaaraykset (:lupamaaraykset permit)
+          paivamaarat    (:paivamaarat permit)
+          poytakirjat    (:poytakirjat permit)]
 
-    (facts "lupamaaraukset data is correct"
-      lupamaaraykset => truthy
-      (:autopaikkojaEnintaan lupamaaraykset) => 10
-      (:autopaikkojaVahintaan lupamaaraykset) => 1
-      (:autopaikkojaRakennettava lupamaaraykset) => 2
-      (:autopaikkojaRakennettu lupamaaraykset) => 0
-      (:autopaikkojaKiinteistolla lupamaaraykset) => 7
-      (:autopaikkojaUlkopuolella lupamaaraykset) => 3
-      (:kerrosala lupamaaraykset) => "100"
-      (:kokonaisala lupamaaraykset) => "110"
-      (:vaaditutTyonjohtajat lupamaaraykset) => "Jokim\u00e4ki"
-      (let [katselmukset (:vaaditutKatselmukset lupamaaraykset)
-            maaraykset   (:maaraykset lupamaaraykset)]
-        (facts "katselmukset"
-          (count katselmukset) => 2
-          (:katselmuksenLaji (first katselmukset)) => "aloituskokous"
-          (:tarkastuksenTaiKatselmuksenNimi (last katselmukset)) => "katselmus2")
-        (facts "m\u00e4\u00e4r\u00e4ykset"
-          (count maaraykset) => 2
-          (:sisalto (first maaraykset)) => "Maarays 1"
-          (:maaraysaika (first maaraykset)) => (to-timestamp "2013-08-28")
-          (:toteutusHetki (last maaraykset)) => (to-timestamp "2013-08-31")))
+      (facts "lupamaaraukset data is correct"
+        lupamaaraykset => truthy
+        (:autopaikkojaEnintaan lupamaaraykset) => 10
+        (:autopaikkojaVahintaan lupamaaraykset) => 1
+        (:autopaikkojaRakennettava lupamaaraykset) => 2
+        (:autopaikkojaRakennettu lupamaaraykset) => 0
+        (:autopaikkojaKiinteistolla lupamaaraykset) => 7
+        (:autopaikkojaUlkopuolella lupamaaraykset) => 3
+        (:kerrosala lupamaaraykset) => "100"
+        (:kokonaisala lupamaaraykset) => "110"
+        (:vaaditutTyonjohtajat lupamaaraykset) => "Jokim\u00e4ki"
+        (let [katselmukset (:vaaditutKatselmukset lupamaaraykset)
+              maaraykset   (:maaraykset lupamaaraykset)]
+          (facts "katselmukset"
+            (count katselmukset) => 2
+            (:katselmuksenLaji (first katselmukset)) => "aloituskokous"
+            (:tarkastuksenTaiKatselmuksenNimi (last katselmukset)) => "katselmus2")
+          (facts "m\u00e4\u00e4r\u00e4ykset"
+            (count maaraykset) => 2
+            (:sisalto (first maaraykset)) => "Maarays 1"
+            (:maaraysaika (first maaraykset)) => (to-timestamp "2013-08-28")
+            (:toteutusHetki (last maaraykset)) => (to-timestamp "2013-08-31")))
 
-      (facts "second permit"
-        (let [katselmukset2 (-> cases last :paatokset last :lupamaaraykset :vaaditutKatselmukset)]
-          (count katselmukset2) => 1
-          katselmukset2 => sequential?)
-        (let [maaraykset2 (-> cases last :paatokset last :lupamaaraykset :maaraykset)]
-          (count maaraykset2) => 1
-          maaraykset2 => sequential?))
+        (facts "second permit"
+          (let [katselmukset2 (-> cases last :paatokset last :lupamaaraykset :vaaditutKatselmukset)]
+            (count katselmukset2) => 1
+            katselmukset2 => sequential?)
+          (let [maaraykset2 (-> cases last :paatokset last :lupamaaraykset :maaraykset)]
+            (count maaraykset2) => 1
+            maaraykset2 => sequential?))
+        )
+
+      (facts "paivamaarat data is correct"
+        paivamaarat    => truthy
+        (:aloitettava paivamaarat) => (to-timestamp "2013-09-01")
+        (:lainvoimainen paivamaarat) => (to-timestamp "2013-09-02")
+        (:voimassaHetki paivamaarat) => (to-timestamp "2013-09-03")
+        (:raukeamis paivamaarat) => (to-timestamp "2013-09-04")
+        (:anto paivamaarat) => (to-timestamp "2013-09-05")
+        (:viimeinenValitus paivamaarat) => (to-timestamp "2013-09-06")
+        (:julkipano paivamaarat) => (to-timestamp "2013-09-07"))
+
+      (facts "p\u00f6yt\u00e4kirjat data is correct"
+        poytakirjat    => truthy
+        (count poytakirjat) => 2
+
+        (let [pk1   (first poytakirjat)
+              liite (:liite pk1)]
+          (:paatos pk1) => "P\u00e4\u00e4t\u00f6s 1"
+          (:paatoskoodi pk1) => "my\u00f6nnetty"
+          (:paatoksentekija pk1) => "viranomainen"
+          (:paatospvm pk1) => (to-timestamp "2013-08-01")
+          (:pykala pk1) => 1
+          (:kuvaus liite) => "kuvaus 1"
+          (:linkkiliitteeseen liite) => "file:///file.ext"
+          (:muokkausHetki liite) => (to-timestamp "2013-09-01T12:00:00")
+          (:versionumero liite) => "1"
+          (get-in liite [:tekija :henkilo :nimi :sukunimi]) => "Tarkkanen"
+          (:tyyppi liite) => "tyyppi 1"
+          (:metadata liite) => {:nimi "arvo"})
+
+        (facts "second permit"
+          (let [poytakirjat2 (-> cases last :paatokset last :poytakirjat)]
+            (count poytakirjat2) => 1
+            poytakirjat2 => sequential?))))))
+
+(facts "CGI sample verdict"
+  (let [xml (sade.xml/parse (slurp "dev-resources/krysp/cgi-permit.xml"))
+        cases (->permits xml)]
+    (fact "xml is parsed" cases => truthy)
+    (fact "xml has 1 case" (count cases) => 1)
+    (fact "case has 1 permit" (-> cases last :paatokset count) => 1)
+
+    (fact "kuntalupatunnus" (:kuntalupatunnus (last cases)) => "13-0185-R")
+
+    (let [permit         (-> cases first :paatokset first)
+          lupamaaraykset (:lupamaaraykset permit)
+          paivamaarat    (:paivamaarat permit)
+          poytakirjat    (:poytakirjat permit)
+          katselmukset   (:vaaditutKatselmukset lupamaaraykset)]
+
+      (fact "paatos" permit => truthy)
+      (fact "lupamaaraykset is parsed" lupamaaraykset => truthy)
+
+      (facts "katselmukset"
+          (count katselmukset) => 1
+          (:katselmuksenLaji (first katselmukset)) => "loppukatselmus")
+
       )
-
-    (facts "paivamaarat data is correct"
-      paivamaarat    => truthy
-      (:aloitettava paivamaarat) => (to-timestamp "2013-09-01")
-      (:lainvoimainen paivamaarat) => (to-timestamp "2013-09-02")
-      (:voimassaHetki paivamaarat) => (to-timestamp "2013-09-03")
-      (:raukeamis paivamaarat) => (to-timestamp "2013-09-04")
-      (:anto paivamaarat) => (to-timestamp "2013-09-05")
-      (:viimeinenValitus paivamaarat) => (to-timestamp "2013-09-06")
-      (:julkipano paivamaarat) => (to-timestamp "2013-09-07"))
-
-    (facts "p\u00f6yt\u00e4kirjat data is correct"
-      poytakirjat    => truthy
-      (count poytakirjat) => 2
-
-      (let [pk1   (first poytakirjat)
-            liite (:liite pk1)]
-        (:paatos pk1) => "P\u00e4\u00e4t\u00f6s 1"
-        (:paatoskoodi pk1) => "my\u00f6nnetty"
-        (:paatoksentekija pk1) => "viranomainen"
-        (:paatospvm pk1) => (to-timestamp "2013-08-01")
-        (:pykala pk1) => 1
-        (:kuvaus liite) => "kuvaus 1"
-        (:linkkiliitteeseen liite) => "file:///file.ext"
-        (:muokkausHetki liite) => (to-timestamp "2013-09-01T12:00:00")
-        (:versionumero liite) => "1"
-        (get-in liite [:tekija :henkilo :nimi :sukunimi]) => "Tarkkanen"
-        (:tyyppi liite) => "tyyppi 1"
-        (:metadata liite) => {:nimi "arvo"})
-
-      (facts "second permit"
-        (let [poytakirjat2 (-> cases last :paatokset last :poytakirjat)]
-          (count poytakirjat2) => 1
-          poytakirjat2 => sequential?)))
-
-    )
-  )
+    ))
