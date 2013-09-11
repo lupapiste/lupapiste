af7f50b4d4e8f6dedb2c2dfcf065865dd086b9c5
Timo Lehtonen <timo.lehtonen@solita.fi>
2013-09-11 16:00:26 +0300

    commit

--- resources/private/application/application.js
+++ resources/private/application/application.js
@@ -359,25 +359,6 @@
       var url = '/oskari/fullmap.html?coord=' + self.location().x() + '_' + self.location().y() + '&zoomLevel=12' + '&addPoint=1' + '&addArea=1';
       window.open(url);
       var applicationId = self.id();
-
-      // FIXME: Can't just subscribe repeatedly.
-      hub.subscribe("map-initialized", function() {
-        if(self.shapes && self.shapes().length > 0) {
-          oskariDrawShape(self.shapes()[0]);
-        }
-
-        oskariSetMarker(self.location().x(), self.location().y());
-      });
-
-      // FIXME: Can't just subscribe repeatedly.
-      hub.subscribe("map-draw-done", function(e) {
-        var drawing = "" + e.data.drawing;
-        ajax.command("save-application-shape", {id: applicationId, shape: drawing})
-        .success(function() {
-          repository.load(applicationId);
-        })
-        .call();
-      });
     };
 
     self.submitApplication = function() {
@@ -544,21 +525,6 @@
       .call();
   }
 
-  function oskariDrawShape(shape) {
-    hub.send("map-viewvectors", {
-      drawing: shape,
-      style: {fillColor: "#3CB8EA", fillOpacity: 0.35, strokeColor: "#0000FF"},
-      clear: false
-    });
-  }
-
-  function oskariSetMarker(x, y) {
-    hub.send("documents-map", {
-      data:  [{location: {x: x, y: y}}],
-      clear: true
-    });
-  }
-
   application.assignee.subscribe(function(v) { updateAssignee(v); });
 
   function resolveApplicationAssignee(authority) {
@@ -571,6 +537,36 @@
       authorityInfos.push(new AuthorityInfo(authority.id, authority.firstName, authority.lastName));
     });
     authorities(authorityInfos);
+  }
+  
+  // Oskari map
+  hub.subscribe("map-initialized", function() {
+    var shapes = application.shapes();
+    if(shapes.length) {
+      // only one shape per application is currently supported
+      hub.send("map-viewvectors", {
+        drawing: shapes[0],
+        style: {fillColor: "#3CB8EA", fillOpacity: 0.35, strokeColor: "#0000FF"},
+        clear: false
+      });
+    }
+    oskariSetMarker(self.location().x(), self.location().y());
+  });
+
+  hub.subscribe("map-draw-done", function(e) {
+    var drawing = "" + e.data.drawing;
+    ajax.command("save-application-shape", {id: currentId, shape: drawing})
+    .success(function() {
+      repository.load(currentId);
+    })
+    .call();
+  });
+
+  function oskariSetMarker(x, y) {
+    hub.send("documents-map", {
+      data:  [{location: {x: x, y: y}}],
+      clear: true
+    });
   }
 
   function showApplication(applicationDetails) {
@@ -634,7 +630,11 @@
 
       var map = getOrCreateMap(application.infoRequest() ? "inforequest" : "application");
       map.clear().center(x, y, 10).add(x, y);
-      if (application.shapes && application.shapes().length > 0) map.drawShape(application.shapes()[0]);
+      
+      if (application.shapes && application.shapes().length > 0) {
+        debugger;
+        map.drawShape(application.shapes()[0]);
+      }
 
       if (application.infoRequest()) {
         ajax.command("mark-seen", {id: app.id, type: "comments"}).call();
