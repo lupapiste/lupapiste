deffa375f092b1befb6125312da032d5ed79a096
jarppe <jarppe@metosin.fi>
2013-09-18 09:58:43 +0300

    Using buildid 0.2.0 (includes hg branch name).

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                                   [ring-mock "0.1.5"]
                                   [clj-ssh "0.5.6"]]
                    :plugins [[lein-midje "3.1.1"]
-                             [lein-buildid "0.1.0"]
+                             [lein-buildid "0.2.0"]
                              [lein-nitpicker "0.4.0"]
                              [lein-hgnotes "0.2.0-SNAPSHOT"]]
                    :source-paths ["test-utils"]
