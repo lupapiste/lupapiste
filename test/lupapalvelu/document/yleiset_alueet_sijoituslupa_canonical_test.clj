59c000641108c20ef0889efe83188fe30ce9c3be
Jari Asp <jari.asp@solita.fi>
2013-09-19 15:56:04 +0300

    Refactored the test application further.

--- test/lupapalvelu/document/yleiset_alueet_sijoituslupa_canonical_test.clj
+++ test/lupapalvelu/document/yleiset_alueet_sijoituslupa_canonical_test.clj
@@ -1,86 +1,40 @@
 (ns lupapalvelu.document.yleiset-alueet-sijoituslupa-canonical-test)
 
 
-(def documents [{:created 1379592634015,
-                 :data
-                 {:_selected {:modified 1379592879374, :value "yritys"},
-                  :henkilo
-                  {:henkilotiedot
-                   {:hetu {:modified 1379592871249, :value "260880-023L"},
-                    :etunimi {:modified 1379592634015, :value "Pena"},
-                    :sukunimi {:modified 1379592634015, :value "Panaani"}},
-                   :osoite
-                   {:katu {:modified 1379592634015, :value "Paapankuja 12"},
-                    :postinumero {:modified 1379592875469, :value "33580"},
-                    :postitoimipaikannimi
-                    {:modified 1379592634015, :value "Piippola"}},
-                   :userId
-                   {:modified 1379592634015, :value "777777777777777777000020"},
-                   :yhteystiedot
-                   {:email {:modified 1379592634015, :value "pena@example.com"},
-                    :puhelin {:modified 1379592634015, :value "0102030405"}}},
-                  :yritys
-                  {:liikeJaYhteisoTunnus
-                   {:modified 1379592855027, :value "1234567-1"},
-                   :osoite
-                   {:katu {:modified 1379592634015, :value "Paapankuja 12"},
-                    :postinumero {:modified 1379592858416, :value "33580"},
-                    :postitoimipaikannimi
-                    {:modified 1379592634015, :value "Piippola"}},
-                   :yhteyshenkilo
-                   {:henkilotiedot
-                    {:etunimi {:modified 1379592634015, :value "Pena"},
-                     :sukunimi {:modified 1379592634015, :value "Panaani"}},
-                    :yhteystiedot
-                    {:email {:modified 1379592634015, :value "pena@example.com"},
-                     :puhelin {:modified 1379592634015, :value "0102030405"}}},
-                   :yritysnimi {:modified 1379592848441, :value "Yritys Oy Ab"}}},
-                 :id "523ae9ba94a7542b3520e64b",
-                 :schema-info
-                 {:approvable true,
-                  :subtype "hakija",
-                  :name "hakija-ya",
-                  :removable false,
-                  :repeating false,
-                  :version 1,
-                  :type "party",
-                  :order 3}}
-                {:created 1379592634015,
-                 :data
-                 {:kaivuLuvanTunniste {:modified 1379592648571, :value "1234567890"},
-                  :kayttotarkoitus
-                  {:modified 1379592642729, :value "Hankkeen kuvaus."}},
-                 :id "523ae9ba94a7542b3520e64a",
-                 :schema-info
-                 {:order 65,
-                  :version 1,
-                  :repeating false,
-                  :name "yleiset-alueet-hankkeen-kuvaus-sijoituslupa",
-                  :op
-                  {:id "523ae9ba94a7542b3520e649",
-                   :name
-                   "ya-sijoituslupa-pysyvien-maanalaisten-rakenteiden-sijoittaminen",
-                   :created 1379592634015},
-                  :removable false}}
-                {:created 1379592634015,
-                 :data
-                 {:lisatietoja-sijoituskohteesta
-                  {:modified 1379592841005, :value "Lisätietoja."},
-                  :muu-sijoituksen-tarkoitus
-                  {:modified 1379592733099, :value "Muu sijoituksen tarkoitus."},
-                  :sijoituksen-tarkoitus {:modified 1379592651471, :value "other"}},
-                 :id "523ae9ba94a7542b3520e64c",
-                 :schema-info
-                 {:name "sijoituslupa-sijoituksen-tarkoitus",
-                  :removable false,
-                  :repeating false,
-                  :version 1,
-                  :order 66}}])
+(def operation {:id "523ae9ba94a7542b3520e649",
+                :created 1379592634015,
+                :name "ya-sijoituslupa-pysyvien-maanalaisten-rakenteiden-sijoittaminen"})
+
+(def hankkeen-kuvaus-sijoituslupa {:id "523ae9ba94a7542b3520e64a",
+                                   :created 1379592634015,
+                                   :schema-info {:order 65,
+                                                 :version 1,
+                                                 :repeating false,
+                                                 :removable false,
+                                                 :name "yleiset-alueet-hankkeen-kuvaus-sijoituslupa",
+                                                 :op operation},
+                                   :data {:kaivuLuvanTunniste {:modified 1379592648571, :value "1234567890"},
+                                          :kayttotarkoitus {:modified 1379592642729, :value "Hankkeen kuvaus."}}})
+
+(def sijoituksen-tarkoitus {:id "523ae9ba94a7542b3520e64c",
+                            :created 1379592634015,
+                            :schema-info {:name "sijoituslupa-sijoituksen-tarkoitus",
+                                          :removable false,
+                                          :repeating false,
+                                          :version 1,
+                                          :order 66},
+                            :data {:lisatietoja-sijoituskohteesta {:modified 1379592841005, :value "Lisätietoja."},
+                                   :sijoituksen-tarkoitus {:modified 1379592651471, :value "other"},
+                                   ;; Huom: tama näkyy vain, jos yllaolevan :sijoituksen-tarkoitus:n value on "other"
+                                   :muu-sijoituksen-tarkoitus {:modified 1379592733099, :value "Muu sijoituksen tarkoitus."}}})
+
+(def documents [hakija
+                hankkeen-kuvaus-sijoituslupa
+                sijoituksen-tarkoitus])
 
 (def operation {:id "523ae9ba94a7542b3520e649",
-                :name
-                "ya-sijoituslupa-pysyvien-maanalaisten-rakenteiden-sijoittaminen",
-                :created 1379592634015})
+                :created 1379592634015,
+                :name "ya-sijoituslupa-pysyvien-maanalaisten-rakenteiden-sijoittaminen"})
 
 ;(def neighbors {:523aeb5794a7542b3520e7e4
 ;                {:neighbor
