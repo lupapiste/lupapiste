5e33f2c845f4acf409a2bf2eb00155b1676ad88a
jarppe <jarppe@metosin.fi>
2013-09-19 14:03:05 +0300

    get email address

--- src/lupapalvelu/open_inforequest.clj
+++ src/lupapalvelu/open_inforequest.clj
@@ -1,4 +1,7 @@
-(ns lupapalvelu.open-inforequest)
+(ns lupapalvelu.open-inforequest
+  (:require [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.organization :as organization]))
 
 (defn new-open-inforequest! [application]
-  (println "***: new open inforequest:" (:id application)))
+  (println "***: new open inforequest:" (:id application))
+  (println "***: send email:" (:open-inforequest-email (organization/get-organization (:organization application)))))
