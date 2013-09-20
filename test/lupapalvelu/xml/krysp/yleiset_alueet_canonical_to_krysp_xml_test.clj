08d43bada03e98ebf4dd2cc394455eeb0562f7af
Jari Asp <jari.asp@solita.fi>
2013-09-20 12:19:18 +0300

    Added a fact for sijoituslupa-application.

--- test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
+++ test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
@@ -3,6 +3,7 @@
         [lupapalvelu.document.yleiset-alueet-canonical :only [application-to-canonical]]
         [lupapalvelu.document.yleiset-alueet-kaivulupa-canonical-test :only [kaivulupa-application]]
         [lupapalvelu.document.yleiset-alueet-kayttolupa-canonical-test :only [kayttolupa-application]]
+        [lupapalvelu.document.yleiset-alueet-sijoituslupa-canonical-test :only [sijoituslupa-application]]
         [lupapalvelu.xml.emit]
         [lupapalvelu.xml.krysp.yleiset-alueet-mapping :only [get-yleiset-alueet-krysp-mapping]]
         [clojure.data.xml]
@@ -56,4 +57,7 @@
       (do-test kaivulupa-application))
 
     (fact "Kayttolupa application -> canonical -> xml"
-      (do-test kayttolupa-application))))
+      (do-test kayttolupa-application))
+
+    (fact "Sijoituslupa application -> canonical -> xml"
+      (do-test sijoituslupa-application))))
