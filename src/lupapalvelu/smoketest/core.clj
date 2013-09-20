0821e4b120243a2bc4e72077d12d20335e317f1e
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-09-20 17:19:52 +0300

    Added execute-all-tests and error handling

--- src/lupapalvelu/smoketest/core.clj
+++ src/lupapalvelu/smoketest/core.clj
@@ -2,14 +2,31 @@
 
 (defonce tests (atom {}))
 
-(defn execute-test! [test-name]
-  (if-let [m (@tests test-name)]
-    (if-let [result ((:fn m))]
-      (if (and (map? result) (contains? result :ok))
-        result
-        {:ok false :error "invalid test: must return a map with :ok key or nil"})
-      {:ok true})
-    {:ok false :error "unknown test"}))
+(defn execute-test [test-name]
+  (try
+    (if-let [m (@tests test-name)]
+      (if-let [result ((:fn m))]
+        (if (and (map? result) (contains? result :ok))
+          result
+          {:ok false :error "invalid test: must return a map with :ok key or nil"})
+        {:ok true})
+      {:ok false :error "unknown test"})
+    (catch Exception e
+      (.printStackTrace e)
+      {:ok false :error (.getMessage e)})
+    (catch AssertionError e
+      (.printStackTrace e)
+      {:ok false :error (.getMessage e)})))
+
+(defn execute-all-tests
+  "Lets all the monsters loose. Returns a map where with test names as keys.
+   Value is either :ok (if the test passes) or the failure report."
+  []
+  (into {} (map (fn [name]
+                [name (let [result (execute-test name)]
+                        (if (true? (:ok result))
+                          :ok
+                          (or (:results result) (:error result))))]) (keys @tests))))
 
 (defmacro defmonster
   "Defines a smoke test. First argument is the name, followed by the body of actual test.
@@ -20,4 +37,4 @@
        (swap! tests assoc ~name-str {:name ~name-str
                                      :fn (fn [] (do ~@body))})
        (defn ~test-name []
-         (execute-test! ~(name name-str))))))
+         (execute-test ~(name name-str))))))
