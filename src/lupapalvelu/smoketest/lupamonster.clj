a6271b8c1804b7333bac52eefc1a1d479f1ae6ad
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-09-20 17:21:03 +0300

    Print errors

--- src/lupapalvelu/smoketest/lupamonster.clj
+++ src/lupapalvelu/smoketest/lupamonster.clj
@@ -1,8 +1,14 @@
 (ns lupapalvelu.smoketest.lupamonster
   (:require [lupapalvelu.mongo :as mongo]
-            [lupapalvelu.cmoketest.core :refer :all]))
+            [lupapalvelu.smoketest.core :refer :all]))
 
 (defn -main [& args]
-  (mongo/connect! "lupaci.solita.fi" 27018)
-  
-  )
+  (if-not @mongo/connected
+    (do
+      (mongo/connect! "lupaci.solita.fi" 27018)
+      (let [report (execute-all-tests)]
+        (doseq [[test-name v] report]
+          (printf "%-40s %s\n" (str test-name ":") (if (= :ok v) "OK" (str "FAIL:" v))))
+        )
+      (mongo/disconnect!))
+    (println "ERROR: Already connected to a MongoDB!")))
