d377d7de8c56b9e0ec4d10f6441560bc6f05c536
jarppe <jarppe@metosin.fi>
2013-09-26 19:01:46 +0300

    go back, go back, go back...

--- resources/private/admin/admin-users.js
+++ resources/private/admin/admin-users.js
@@ -1,12 +1,5 @@
 ;(function() {
   "use strict";
 
-  var usersModel = users.create(editUserModel, 'admin');
-
-  hub.onPageChange("admin-users", usersModel.load);
-
-  $(function() {
-    $("#admin-users").applyBindings(usersModel);
-  });
 
 })();
