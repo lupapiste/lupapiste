736e282f25fe4dd112154c06410a68bba562e015
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-09-28 23:07:28 +0300

    Clojure 1.5.1, Ring 1.2.0 and Monger 1.6.0

--- src/lupapalvelu/document/schemas.clj
+++ src/lupapalvelu/document/schemas.clj
@@ -158,7 +158,7 @@
 
 (def designer-basic (body
                       henkilotiedot-minimal
-                      {:name "yritys" :type :group :body (clojure.walk/postwalk (fn [c] (if (contains? c :required)
+                      {:name "yritys" :type :group :body (clojure.walk/postwalk (fn [c] (if (and (map? c) (contains? c :required))
                                                                                           (assoc c :required false)
                                                                                           c)) yritys-minimal)}
                       simple-osoite
