d0695a325245ccb2d64d6475862d2c5d7754e6ec
Jari Asp <jari.asp@solita.fi>
2013-10-07 19:16:44 +0300

    Added js functions for adding screenmessages, and for resetting messages from mongo.

--- resources/private/screenmessages/screenmessage.js
+++ resources/private/screenmessages/screenmessage.js
@@ -7,9 +7,10 @@ LUPAPISTE.Screenmessage = function () {
 
   self.messages = ko.observableArray([]);
 
-  self.refresh = function(application) {
-    ajax.get("/system/screenmessage")
+  self.refresh = function() {
+    ajax.query("screenmessages")
     .success(function(data) {
+      console.log("screenmessages (Get) Success, data: ", data);
       self.messages(data.screenmessages);
     })
     .error(function(e) {
@@ -18,4 +19,30 @@ LUPAPISTE.Screenmessage = function () {
     })
     .call();
   };
+
+
+  self.add = function() {
+    ajax.command("screenmessages-add", {fi : $("#add-text-fi").val(), sv : $("#add-text-sv").val()})
+    .success(function(data) {
+      console.log("screenmessages-add Success, data: ", data);
+      self.refresh();
+    })
+    .error(function(e) {
+      debug("Could not add screen message", e);
+    })
+    .call();
+  };
+
+  self.reset = function() {
+    ajax.command("screenmessages-reset")
+    .success(function(data) {
+      console.log("screenmessages-reset Success, data: ", data);
+      self.refresh();
+    })
+    .error(function(e) {
+      debug("Could not reset screen messages", e);
+    })
+    .call();
+  };
+
 }
