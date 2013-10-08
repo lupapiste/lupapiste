ce403cba5acda541e4345fbb9de1506700b43d6d
Timo Lehtonen <timo.lehtonen@solita.fi>
2013-10-08 15:01:11 +0300

    edit dialog v0.01

--- resources/private/admin/organizations.js
+++ resources/private/admin/organizations.js
@@ -18,10 +18,36 @@
 
   var organizationsModel = new OrganizationsModel();
 
+  function EditOrganizationModel() {
+    var self = this;
+
+    self.organization = ko.observable();
+
+    self.open = function(organization) {
+      self.organization(organization);
+      LUPAPISTE.ModalDialog.open("#dialog-edit-organization");
+      return self;
+    };
+
+    self.execute = function(attachments) {
+      alert("exe");
+    };
+
+    $(function() {
+      self.selectm = $("#dialog-edit-organization .organizations").selectm();
+      self.selectm
+        .allowDuplicates(false)
+        .ok(self.execute)
+        .cancel(LUPAPISTE.ModalDialog.close);
+    });
+  }
+
+  var editOrganizationModel = new EditOrganizationModel();
+
   hub.onPageChange("organizations", organizationsModel.load);
 
   $(function() {
-    $("#organizations").applyBindings(organizationsModel);
+    $("#organizations").applyBindings({ "organizationsModel": organizationsModel});
   });
 
 })();
