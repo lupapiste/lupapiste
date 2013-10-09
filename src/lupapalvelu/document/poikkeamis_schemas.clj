e5238e950913d3c01dd5d5ae67e54e6eda4a3f94
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2013-10-09 08:39:37 +0300

    small tuning

--- src/lupapalvelu/document/poikkeamis_schemas.clj
+++ src/lupapalvelu/document/poikkeamis_schemas.clj
@@ -4,7 +4,8 @@
 
 
 (def rakennushanke {:info {:name "rakennushanke"
-                           :order 50}
+                           :order 50
+                           :removable false}
                     :body [{:name "kaytettykerrosala" :type :group
                             :body [{:name "pintaAla" :type :string :size "s" :unit "m2" :subtype :number}
                                    {:name "kayttotarkoitusKoodi" :type :select
@@ -89,8 +90,9 @@
                            {:name "toimenpiteet"
                             :type :group
                             :repeating true
+                            :approvable true
                             :body [{:name "kayttotarkoitus" :type :select
-                                   :body [{:name yhden-asunnon-talot}
+                                    :body [{:name yhden-asunnon-talot}
                                           {:name "012 kahden asunnon talot"}
                                           {:name "013 muut erilliset talot"}
                                           {:name "021 rivitalot"}
