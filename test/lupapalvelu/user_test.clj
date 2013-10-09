39ba16caeb3880f56f776545617014ea332adf65
jarppe <jarppe@metosin.fi>
2013-10-09 16:20:00 +0300

    tests, moar test, even more...

--- test/lupapalvelu/user_test.clj
+++ test/lupapalvelu/user_test.clj
@@ -1,11 +1,68 @@
 (ns lupapalvelu.user-test
   (:require [midje.sweet :refer :all]
             [lupapalvelu.user :refer :all]
-            [slingshot.slingshot :refer [try+]]))
+            [slingshot.slingshot :refer [try+]]
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.security :as security]))
+
+;;
+;; ==============================================================================
+;; Utils:
+;; ==============================================================================
+;;
+
+(facts non-private
+  (fact "strips away private keys from map"
+    (non-private {:name "tommi" :private {:secret "1234"}}) => {:name "tommi"})
+  (fact ".. but not non-recursively"
+    (non-private {:name "tommi" :child {:private {:secret "1234"}}}) => {:name "tommi" :child {:private {:secret "1234"}}}))
+
+(facts summary
+  (fact (summary nil) => nil)
+  (let [user {:id "1"
+              :firstName "Simo"
+              :username  "simo@salminen.com"
+              :lastName "Salminen"
+              :role "comedian"
+              :private "SECRET"}]
+    (fact (summary user) => (just (dissoc user :private)))))
+
+(fact authority?
+  (authority? {:role "authority"})  => truthy
+  (authority? {:role :authority})   => truthy
+  (authority? {:role "applicant"})  => falsey
+  (authority? {})                   => falsey
+  (authority? nil)                  => falsey)
+
+(fact applicant?
+  (applicant? {:role "applicant"})  => truthy
+  (applicant? {:role :applicant})   => truthy
+  (applicant? {:role "authority"})  => falsey
+  (applicant? {})                   => falsey
+  (applicant? nil)                  => falsey)
+
+(fact same-user?
+  (same-user? {:id "foo"} {:id "foo"}) => truthy
+  (same-user? {:id "foo"} {:id "bar"}) => falsey)
+
+;;
+;; ==============================================================================
+;; Finding user data:
+;; ==============================================================================
+;;
 
 (facts user-query
   (user-query) => (throws AssertionError)
+  (user-query :id "x") => {:_id "x"}
+  (user-query :email "x") => {:email "x"}
+  (user-query :email "XyZq") => {:email "xyzq"}
   (user-query :id "x" :username "UserName" :email "Email@AddreSS.FI" :foo "BoZo") => {:_id "x" :username "username" :email "email@address.fi" :foo "BoZo"})
+
+;;
+;; ==============================================================================
+;; User role:
+;; ==============================================================================
+;;
 
 (facts
   (applicationpage-for "applicant")      => "applicant"
@@ -13,31 +70,59 @@
   (applicationpage-for "authorityAdmin") => "authority-admin"
   (applicationpage-for "admin")          => "admin")
 
-(facts "non-private"
-  (fact "strips away private keys from map"
-    (non-private {:name "tommi" :private {:secret "1234"}}) => {:name "tommi"})
-  (fact ".. but not non-recursively"
-    (non-private {:name "tommi" :child {:private {:secret "1234"}}}) => {:name "tommi" :child {:private {:secret "1234"}}}))
+(facts user-in-role
+  
+  (fact "role is overridden"
+    (user-in-role {:id 1 :role :applicant} :reader) => {:id 1 :role :reader})
+  
+  (fact "takes optional name & value parameter pair"
+    (user-in-role {:id 1 :role :applicant} :reader :age 16) => {:id 1 :role :reader :age 16})
+  
+  (fact "takes optional name & value parameter pairS"
+    (user-in-role {:id 1 :role :applicant} :reader :age 16 :size :L) => {:id 1 :role :reader :age 16 :size :L})
+  
+  (fact "fails with uneven optional parameter pairs"
+    (user-in-role {:id 1 :role :applicant} :reader :age) => (throws Exception)))
 
-(let [user {:id "1"
-            :firstName "Simo"
-            :username  "simo@salminen.com"
-            :lastName "Salminen"
-            :role "comedian"
-            :private "SECRET"}]
-  (facts "summary"
-    (fact (summary nil) => nil)
-    (fact (summary user) => (just (dissoc user :private)))))
+;;
+;; ==============================================================================
+;; Getting non-private user data:
+;; ==============================================================================
+;;
 
-(fact "authority"
-  (authority? {:role "authority"}) => true
-  (authority? {:role :authority}) => true
-  (authority? {}) => false)
+(facts get-user-by-id
+  (get-user-by-id ...id...) => {:id ...id... :email ...email...}
+    (provided (mongo/select-one :users {:_id ...id...}) => {:id ...id... :email ...email... :private ...private...}))
 
-(fact "applicant"
-  (applicant? {:role "applicant"}) => true
-  (applicant? {:role :applicant}) => true
-  (applicant? {}) => false)
+(facts get-user-by-email
+  (get-user-by-email "email") => {:id ...id... :email "email"}
+    (provided (mongo/select-one :users {:email "email"}) => {:id ...id... :email "email" :private ...private...}))
+
+(facts get-user-with-password
+  
+  (fact happy-case
+    (get-user-with-password "username" "password") => {:id ...id... :enabled true}
+      (provided (mongo/select-one :users {:username "username"}) => {:id ...id... :enabled true :private {:password "from-db"}}
+                (security/check-password "password" "from-db") => true))
+    
+  (fact wrong-password
+    (get-user-with-password "username" "password") => nil
+      (provided (mongo/select-one :users {:username "username"}) => {:id ...id... :enabled true :private {:password "from-db"}}
+                (security/check-password "password" "from-db") => false))
+  
+  (fact disabled-user
+    (get-user-with-password "username" "password") => nil
+      (provided (mongo/select-one :users {:username "username"}) => {:id ...id... :enabled false :private {:password "from-db"}}))
+  
+  (fact unknown-user
+    (get-user-with-password "username" "password") => nil
+      (provided (mongo/select-one :users {:username "username"}) => nil)))
+
+
+
+
+
+
 
 (fact "is a map with all the data"
   (create-user-entity {:id             ..id..
