98b363e8ef9c5440a8960ab32c67d6da96e5a86d
jarppe <jarppe@metosin.fi>
2013-11-05 21:40:37 +0200

    works

--- resources/private/admin/admin-users.js
+++ resources/private/admin/admin-users.js
@@ -2,10 +2,35 @@
   "use strict";
 
   var usersList = null;
+  var usersTableConfig = {
+    ops: [{name: "enable",
+           showFor: function(user) { return !user.enabled; },
+           operation: function(email, callback) {
+             ajax
+               .command("set-user-enabled", {email: email, enabled: true})
+               .success(function() { callback(true); })
+               .call();
+           }},
+          {name: "disable",
+           showFor: function(user) { return user.enabled; },
+           operation: function(email, callback) {
+             ajax
+               .command("set-user-enabled", {email: email, enabled: false})
+               .success(function() { callback(true); })
+               .call();
+           }},
+          {name: "resetPassword",
+           showFor: function(user) { return user.enabled; },
+           operation: function(email, callback) {
+             ajax
+               .command("reset-password", {email: email})
+               .success(function() { callback(true); })
+               .call();
+           }}]
+  };
   
   hub.onPageChange("users", function() {
-    if (!usersList) usersList = users.create($("#users .fancy-users-table"));
+    if (!usersList) usersList = users.create($("#users .fancy-users-table"), usersTableConfig);
   });
-
 
 })();
--- resources/private/users/users.js
+++ resources/private/users/users.js
@@ -4,36 +4,7 @@ var users = (function() {
   function toLoc(data, type, row) { return loc(data); }
   function toActive(data, type, row) { return loc(["users.data.enabled", data]); }
   function toOrgs(data, type, row) { return data ? data.join(", ") : ""; }
-  
-  function addOps(td, sData, oData, iRow, iCol) {
-    var td = $(td).empty().text("");
-    _.each(oData[5], function(op) {
-      td.append($("<a>")
-        .attr("href", "#")
-        .attr("data-op", op)
-        .text("[" + loc("users.op." + op) + "]"));
-    });
-  }
-  
-  var dataTableConfig = {
-    bProcessing:      true, // don't hide this, it brakes layout.
-    bServerSide:      true,
-    sAjaxSource:      "",
-    aoColumnDefs:     [{aTargets: [2], mRender: toLoc},
-                       {aTargets: [3], mRender: toOrgs, bSortable: false},
-                       {aTargets: [4], mRender: toActive},
-                       {aTargets: [5], fnCreatedCell: addOps, bSortable: false}],
-    aaSorting:        [[0, "desc"]],
-    sDom:             "<t><<r><p><i><l>>", // <'table-filter' f>
-    iDisplayLength:   10,
-    oLanguage:        {sLengthMenu:   loc("users.table.lengthMenu"),
-                       sProcessing:   "&nbsp;",
-                       sSearch:       loc("search") + ":",
-                       sZeroRecords:  loc("users.table.zeroRecords"),
-                       oPaginate:     {"sNext": loc("next"), "sPrevious": loc("previous")},
-                       sInfo:         loc("users.table.info"),
-                       sInfoFiltered: ""}
-  };
+    
 
   function UsersModel(component, opts) {
     var self = this;
@@ -41,7 +12,7 @@ var users = (function() {
     self.component = component;
     self.table$ = $("table", component);
     
-    self.availableRoles = _([null, "admin", "authority", "applicant"])
+    self.availableRoles = _([null, "admin", "authority", "authorityAdmin", "applicant", "dummy"])
       .map(function(id) { return {id: id, name: loc(id ? id : "users.filters.role.all")}; })
       .value();
     
@@ -54,19 +25,38 @@ var users = (function() {
       enabled:  ko.observable(),
       search:   ko.observable()
     };
-    
+
+    self.show = {
+      filters:        !opts.hideFilters,
+      roleFilter:     !opts.hideRoleFilter,
+      enabledFilter:  !opts.hideEnabledFilter,
+      search:         !opts.hideSearch
+    };
+
     self.ops = function(user) {
-      return user.enabled ? ["disable", "resetPassword"] : ["enable"];
+      return _.filter(opts.ops || [], function(op) { return op.showFor(user); });
+    };
+  
+    self.toOps = function(td, sData, oData, iRow, iCol) {
+      var user = oData.user,
+          ops = _.filter(opts.ops || [], function(op) { return op.showFor(user); }),
+          td = $(td);
+      _.each(ops, function(op) {
+        td.append($("<a>")
+          .attr("href", "#")
+          .attr("data-op", op.name)
+          .text("[" + loc("users.op." + op.name) + "]"));
+      });
     };
     
     self.userToRow = function(user) {
-      return {id: user._id,
+      return {user: user,
               0: user.email,
               1: user.firstName + " " + user.lastName,
               2: user.role,
               3: user.organizations ? user.organizations : [],
               4: user.enabled,
-              5: self.ops(user),};
+              5: ""}; // col 5 will be set by toOps 
     };
     
     self.processResults = function(r) {
@@ -92,37 +82,44 @@ var users = (function() {
       $(row).attr("data-user-email", data[0]);
     };
     
+    self.redrawCallback = function(redraw) { if (redraw) self.redraw(); };
+    
     self.table$.click(function(e) {
       var target = $(e.target),
-          op = target.attr("data-op"),
+          opName = target.attr("data-op"),
+          op = _.find(opts.ops, function(op) { return op.name === opName; }),
           email = target.parent().parent().attr("data-user-email");
-      if (!op || !email) return true;
+      if (!op || !email) return false;
       LUPAPISTE.ModalDialog.showDynamicYesNo(
-          loc("users.op." + op + ".title"),
-          loc("users.op." + op + ".message"),
-          {title: loc("yes"), fn: function() { self["op" + _.capitalize(op)](email); }},
+          loc("users.op." + op.name + ".title"),
+          loc("users.op." + op.name + ".message"),
+          {title: loc("yes"), fn: function() { op.operation(email, self.redrawCallback); }},
           {title: loc("cancel")});
       return false;
     });
 
-    self.opResetPassword = function(email) {
-      ajax.command("reset-password", {email: email}).call();
-    };
-
-    self.opDisable = function(email) { self.setUserEnabled(email, false); };
-    self.opEnable = function(email) { self.setUserEnabled(email, true); };
-
-    self.setUserEnabled = function(email, enabled) {
-      ajax
-        .command("set-user-enabled", {email: email, enabled: enabled})
-        .success(self.redraw)
-        .call();
-    };
-    
-    var config = _.clone(dataTableConfig);
-    config.fnServerData = self.fetch;
-    config.fnCreatedRow = self.rowCreated;
-    self.dataTable = self.table$.dataTable(config);
+    self.dataTable = self.table$.dataTable({
+      bProcessing:      true, // don't hide this, it brakes layout.
+      bServerSide:      true,
+      sAjaxSource:      "",
+      aoColumnDefs:     [{aTargets: [1,2,3,4,5], bSortable: false},
+                         {aTargets: [2], mRender: toLoc},
+                         {aTargets: [3], mRender: toOrgs},
+                         {aTargets: [4], mRender: toActive},
+                         {aTargets: [5], fnCreatedCell: self.toOps}],
+      aaSorting:        [[0, "desc"]],
+      sDom:             "<t><<r><p><i><l>>", // <'table-filter' f>
+      iDisplayLength:   10,
+      oLanguage:        {sLengthMenu:   loc("users.table.lengthMenu"),
+                         sProcessing:   "&nbsp;",
+                         sSearch:       loc("search") + ":",
+                         sZeroRecords:  loc("users.table.zeroRecords"),
+                         oPaginate:     {"sNext": loc("next"), "sPrevious": loc("previous")},
+                         sInfo:         loc("users.table.info"),
+                         sInfoFiltered: ""},
+      fnServerData: self.fetch,
+      fnCreatedRow: self.rowCreated
+    });
 
     self.redraw = function() { self.dataTable.fnDraw(true); };
     
