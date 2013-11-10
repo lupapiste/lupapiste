0e3b5e24b5ddfbe6c7deb0489bd8a7592552a113
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-11-10 19:32:19 +0200

    Added test for subject

--- itest/lupapalvelu/statement_api_itest.clj
+++ itest/lupapalvelu/statement_api_itest.clj
@@ -2,12 +2,13 @@
   (:require [lupapalvelu.itest-util :refer :all]
             [midje.sweet :refer :all]))
 
-(facts "new statement person receives email"
+(facts "statements"
   (command sipoo :create-statement-person :email "ronja.sibbo@sipoo.fi" :text "<b>bold</b>") => ok?
   (let [email (last-email)]
     (:to email) => (email-for "ronja")
 
-    (fact "which contains the (html escaped) input text"
+    (fact "new statement person receives email which contains the (html escaped) input text"
       email => has-html-and-plain?
+      (:subject email) => "Lupapiste.fi: Lausunnot"
       (get-in email [:body :plain]) => (contains "<b>bold</b>")
       (get-in email [:body :html]) => (contains "&lt;b&gt;bold&lt;/b&gt;"))))
