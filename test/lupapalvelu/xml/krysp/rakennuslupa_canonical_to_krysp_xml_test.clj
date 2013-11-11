eee43c41f835a29c28fb47c1c7b7314161c7b246
Jari Asp <jari.asp@solita.fi>
2013-11-11 15:35:58 +0200

    - Also testing Työnjohtaja and Suunnttelija, which required some refactoring - :use -> :require

--- test/lupapalvelu/xml/krysp/rakennuslupa_canonical_to_krysp_xml_test.clj
+++ test/lupapalvelu/xml/krysp/rakennuslupa_canonical_to_krysp_xml_test.clj
@@ -1,21 +1,25 @@
 (ns lupapalvelu.xml.krysp.rakennuslupa_canonical_to_krysp_xml_test
-  (:use [midje.sweet]
-        [lupapalvelu.document.rakennuslupa_canonical :only [application-to-canonical]]
-        [lupapalvelu.document.rakennuslupa_canonical-test :only [application]]
-        [lupapalvelu.xml.emit]
-        [lupapalvelu.xml.krysp.rakennuslupa-mapping :only [rakennuslupa_to_krysp save-aloitusilmoitus-as-krysp]]
-        [lupapalvelu.xml.krysp.validator :only [validate]]
-        [lupapalvelu.xml.krysp.canonical-to-krysp-xml-test-common :only [has-tag]]
-        [clojure.data.xml]
-        [clojure.java.io])
-  (:require [lupapalvelu.xml.krysp.application-as-krysp-to-backing-system :as mapping-to-krysp]))
+  (:require [lupapalvelu.xml.krysp.application-as-krysp-to-backing-system :refer :all :as mapping-to-krysp]
+            [lupapalvelu.document.rakennuslupa_canonical :refer [application-to-canonical]]
+            [lupapalvelu.document.rakennuslupa_canonical-test :refer [application-rakennuslupa
+                                                                      application-tyonjohtajan-nimeaminen
+                                                                      application-suunnittelijan-nimeaminen]]
+            [lupapalvelu.xml.krysp.rakennuslupa-mapping :refer [rakennuslupa_to_krysp
+                                                                save-aloitusilmoitus-as-krysp]]
+            [lupapalvelu.xml.krysp.validator :refer [validate]]
+            [lupapalvelu.xml.krysp.canonical-to-krysp-xml-test-common :refer [has-tag]]
+            [lupapalvelu.xml.krysp.validator :refer :all :as validator]
+            [lupapalvelu.xml.emit :refer :all]
+            [midje.sweet :refer :all]
+            [clojure.data.xml :refer :all]
+            [clojure.java.io :refer :all]))
 
-(fact ":tag is set" (has-tag rakennuslupa_to_krysp) => true)
-
-(facts "Rakennuslupa to canonical and then to rakennuslupa xml with schema validation"
+(defn- do-test [application]
   (let [canonical (application-to-canonical application "fi")
         xml (element-to-xml canonical rakennuslupa_to_krysp)
         xml-s (indent-str xml)]
+
+    (fact ":tag is set" (has-tag rakennuslupa_to_krysp) => true)
 
     ;(println xml-s)
     ;Alla oleva tekee jo validoinnin, mutta annetaan olla tuossa alla viela validointi, jottei tule joku riko olemassa olevaa validointia
@@ -29,22 +33,41 @@
     ;    (emit xml out-file))
     (fact "xml exist" xml => truthy)
 
-    (validate xml-s)  ;; in lupapalvelu.xml.krysp.validator
+    (validator/validate xml-s)
 
-    #_(save-aloitusilmoitus-as-krysp (assoc application :state "verdictGiven") "fi" "" (lupapalvelu.core/now) {:jarjestysnumero 1 :rakennusnro "123"} {:id "777777777777777777000017"
-                                                                                      :email "jussi.viranomainen@tampere.fi"
-                                                                                      :enabled true
-                                                                                      :role "authority"
-                                                                                      :username "jussi"
-                                                                                      :organizations ["837-YA"]
-                                                                                      :firstName "Jussi"
-                                                                                      :lastName "Viranomainen"
-                                                                                      :street "Katuosoite 1 a 1"
-                                                                                      :phone "1231234567"
-                                                                                      :zip "33456"
-                                                                                      :city "Tampere"
-                                                                                      :private {:salt "$2a$10$Wl49diVWkO6UpBABzjYR4e"
-                                                                                                :password "$2a$10$Wl49diVWkO6UpBABzjYR4e8zTwIJBDKiEyvw1O2EMOtV9fqHaXPZq" ;; jussi
-                                                                                                :apikey "5051ba0caa2480f374dcfefg"}})
+    #_(save-aloitusilmoitus-as-krysp
+      (assoc application :state "verdictGiven")
+      "fi"
+      ""
+      (lupapalvelu.core/now)
+      {:jarjestysnumero 1 :rakennusnro "123"}
+      {:id "777777777777777777000017"
+       :email "jussi.viranomainen@tampere.fi"
+       :enabled true
+       :role "authority"
+       :username "jussi"
+       :organizations ["837-YA"]
+       :firstName "Jussi"
+       :lastName "Viranomainen"
+       :street "Katuosoite 1 a 1"
+       :phone "1231234567"
+       :zip "33456"
+       :city "Tampere"
+       :private {:salt "$2a$10$Wl49diVWkO6UpBABzjYR4e"
+                 :password "$2a$10$Wl49diVWkO6UpBABzjYR4e8zTwIJBDKiEyvw1O2EMOtV9fqHaXPZq" ;; jussi
+                 :apikey "5051ba0caa2480f374dcfefg"}})
 
     ))
+
+(facts "Rakennusvalvonta type of permits to canonical and then to xml with schema validation"
+
+  (fact "Rakennuslupa application -> canonical -> xml"
+    (do-test application-rakennuslupa))
+
+  (fact "Ty\u00f6njohtaja application -> canonical -> xml"
+    (do-test application-tyonjohtajan-nimeaminen))
+
+  (fact "Suunnittelija application -> canonical -> xml"
+    (do-test application-suunnittelijan-nimeaminen)))
+
+
