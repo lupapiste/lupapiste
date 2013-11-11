e56b7d50016fbd578159041e038bcdf58fa16f00
Jari Asp <jari.asp@solita.fi>
2013-11-11 15:33:42 +0200

    - cleanup - :use -> :require

--- test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
+++ test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
@@ -1,20 +1,20 @@
 (ns lupapalvelu.xml.krysp.yleiset-alueet-canonical-to-krysp-xml-test
-  (:use [midje.sweet]
-        [lupapalvelu.document.yleiset-alueet-canonical :only [application-to-canonical]]
-        [lupapalvelu.document.yleiset-alueet-kaivulupa-canonical-test :only [kaivulupa-application]]
-        [lupapalvelu.document.yleiset-alueet-kayttolupa-canonical-test :only [kayttolupa-application]]
-        [lupapalvelu.document.yleiset-alueet-sijoituslupa-canonical-test :only [sijoituslupa-application]]
-        [lupapalvelu.document.yleiset-alueet-kayttolupa-mainostus-viitoitus-canonical-test
-         :only [mainostus-application viitoitus-application]]
-        [lupapalvelu.xml.emit]
-        [lupapalvelu.xml.krysp.yleiset-alueet-mapping :only [get-yleiset-alueet-krysp-mapping]]
-        [clojure.data.xml]
-        [clojure.java.io])
-  (:require [lupapalvelu.xml.krysp.application-as-krysp-to-backing-system :as mapping-to-krysp]
-            [lupapalvelu.xml.krysp.validator :as validator]
-            [lupapalvelu.xml.krysp.canonical-to-krysp-xml-test-common :as xml-test-common]
-            [lupapalvelu.xml.krysp.mapping-common :as mapping-common]
-            [lupapalvelu.document.canonical-common :refer [ya-operation-type-to-schema-name-key]]))
+  (:require [lupapalvelu.document.yleiset-alueet-canonical :refer [application-to-canonical]]
+            [lupapalvelu.document.yleiset-alueet-kaivulupa-canonical-test :refer [kaivulupa-application]]
+            [lupapalvelu.document.yleiset-alueet-kayttolupa-canonical-test :refer [kayttolupa-application]]
+            [lupapalvelu.document.yleiset-alueet-sijoituslupa-canonical-test :refer [sijoituslupa-application]]
+            [lupapalvelu.document.yleiset-alueet-kayttolupa-mainostus-viitoitus-canonical-test
+             :refer [mainostus-application viitoitus-application]]
+            [lupapalvelu.xml.emit :refer :all]
+            [lupapalvelu.xml.krysp.yleiset-alueet-mapping :refer [get-yleiset-alueet-krysp-mapping]]
+            [lupapalvelu.xml.krysp.application-as-krysp-to-backing-system :refer :all :as mapping-to-krysp]
+            [lupapalvelu.xml.krysp.validator :refer :all :as validator]
+            [lupapalvelu.xml.krysp.canonical-to-krysp-xml-test-common :refer :all :as xml-test-common]
+            [lupapalvelu.xml.krysp.mapping-common :refer :all :as mapping-common]
+            [lupapalvelu.document.canonical-common :refer [ya-operation-type-to-schema-name-key]]
+            [midje.sweet :refer :all]
+            [clojure.data.xml :refer :all]
+            [clojure.java.io :refer :all]))
 
 (defn- do-test [application]
   (let [operation-name-key (-> application :operations first :name keyword)
@@ -53,8 +53,6 @@
 
 (facts "YA permits to canonical and then to xml with schema validation"
 
-  (let [operation-name-key (-> kayttolupa-application :operations first :name keyword)]
-
     (fact "Kaivulupa application -> canonical -> xml"
       (do-test kaivulupa-application))
 
@@ -68,4 +66,4 @@
       (do-test mainostus-application))
 
     (fact "Viitoituslupa application -> canonical -> xml"
-      (do-test viitoitus-application))))
+      (do-test viitoitus-application)))
