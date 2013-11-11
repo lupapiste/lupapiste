0be22676f94c4410bb2704cc3a44662c08e7cb3e
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-11-11 12:49:48 +0200

    Moved fact to inforequest-itest

--- itest/lupapalvelu/application_itest.clj
+++ itest/lupapalvelu/application_itest.clj
@@ -269,22 +269,3 @@
         (get-in doc-after [:data :muutostyolaji :value]) => "muut muutosty\u00f6t"
         (get-in doc-after [:data :kaytto :kayttotarkoitus :source]) => "krysp"))
 
-(facts "Open inforequest"
-  ; Reset emails
-  (last-email)
-
-  (let [application-id (create-app-id pena :municipality "433" :infoRequest true)
-        application    (query-application pena application-id)]
-
-    (fact "Inforequest was created"
-      (:infoRequest application) => true)
-
-    (fact "Inforequest is an open inforequest"
-      (:openInfoRequest application) => true)
-
-    (let [email          (last-email)
-          [_ token lang] (re-find #"(?sm)/api/raw/openinforequest\?token-id=([A-Za-z0-9-]+)&lang=([a-z]{2})" (get-in email [:body :plain]))]
-      (:to email) => "erajorma@takahikia.fi"
-      (:subject email) => "Uusi neuvontapyyntö"
-      (count token) => pos?)))
-
