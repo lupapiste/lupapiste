356f3db314384c1a979255a21b172259e171d66e
Timo Lehtonen <timo.lehtonen@solita.fi>
2013-11-29 14:59:46 +0200

    Oskari version update

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [ontodev/excel "0.2.0" :exclusions [[xml-apis]]]
                  [com.googlecode.htmlcompressor/htmlcompressor "1.5.2"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.7" :exclusions [rhino/js]] ; http://jira.xwiki.org/browse/XWIKI-6148?focusedCommentId=59523#comment-59523
-                 [fi.sito/oskari "0.9.24"]
+                 [fi.sito/oskari "0.9.25"]
                  [slingshot "0.10.3"]
                  [com.google.zxing/javase "2.2"]
                  [digest "1.4.3"]
