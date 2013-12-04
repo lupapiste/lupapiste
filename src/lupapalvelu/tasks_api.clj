957ddffc7abfe8a7f5c8d8a69a0c1a0935fded53
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-12-04 13:57:01 +0200

    Added TODOs

--- src/lupapalvelu/tasks_api.clj
+++ src/lupapalvelu/tasks_api.clj
@@ -8,8 +8,10 @@
 (defcommand delete-task
   {:parameters [id taskId]
    :input-validators [(partial non-blank-parameters [:id :taskId])]
+   ; TODO :validators [task-state...]]
    :roles [:authority]}
   [{created :created :as command}]
+  ; TODO otetaan huomioon taskin tila: jos siirretty, ei saa poistaa
   (update-application command
     {$pull {:tasks {:id taskId}}
      $set  {:modified  created}}))
