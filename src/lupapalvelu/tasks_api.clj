fed2caff3e65c86f65d418bb1874ce757add8e40
Juha Jokimaki <juha.jokimaki@solita.fi>
2013-12-09 12:28:03 +0200

    Added approve/reject commands

--- src/lupapalvelu/tasks_api.clj
+++ src/lupapalvelu/tasks_api.clj
@@ -5,13 +5,53 @@
             [lupapalvelu.mongo :as mongo]
             [monger.operators :refer :all]))
 
+;; Helpers
+
+(defn- task-state-in [states {{task-id :taskId} :data } {tasks :tasks}]
+  (if-let [task (some #(when (= (:id %) id) %) tasks)]
+    (when-not ((set states) (keyword (:state task)))
+      (fail :error.command-illegal-state))
+    (fail :error.task-not-found)))
+
+(defn- set-state [{created :created :as command} task-id state]
+  (update-application command
+    {:tasks {$elemMatch {:id task-id}}}
+    {$set {:tasks.$.state state :modified created}}))
+
+;; API
+
 (defcommand delete-task
   {:parameters [id taskId]
-   :input-validators [(partial non-blank-parameters [:id :taskId])]
-   ; TODO :validators [task-state...]]
+   :input-validators [(partial non-blank-parameters [:id :taskId])
+                      (partial task-state-in [:requires_user_action :requires_authority_action :ok])]
    :roles [:authority]}
   [{created :created :as command}]
-  ; TODO otetaan huomioon taskin tila: jos siirretty, ei saa poistaa
   (update-application command
     {$pull {:tasks {:id taskId}}
      $set  {:modified  created}}))
+
+(defcommand approve-task
+  {:description "Authority can approve task, moves to ok"
+   :parameters  [id taskId]
+   :input-validators [(partial non-blank-parameters [:id :taskId])]
+   :roles       [:authority]}
+  [command]
+  (set-state command taskId :ok))
+
+(defcommand reject-task
+  {:description "Authority can reject task, requires user action."
+   :parameters  [id taskId]
+   :input-validators [(partial non-blank-parameters [:id :taskId])]
+   :roles       [:authority]}
+  [command]
+  (set-state command taskId :requires_user_action))
+
+(defcommand send-task
+  {:description "Authority can send task info to municipality backend system."
+   :parameters  [id taskId]
+   :input-validators [(partial non-blank-parameters [:id :taskId])
+                      (partial task-state-in [:ok])]
+   :roles       [:authority]}
+  [command]
+  ; TODO form KRYSP message
+  (set-state command taskId :sent))
