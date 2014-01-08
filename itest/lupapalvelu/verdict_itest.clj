25d6e225053a5328fe81066117966f34644d971e
Jari Asp <jari.asp@solita.fi>
2014-01-08 09:44:31 +0200

    started/ready app itest: Testing also emails

--- itest/lupapalvelu/verdict_itest.clj
+++ itest/lupapalvelu/verdict_itest.clj
@@ -11,7 +11,7 @@
     (let [application-id  (create-app-id user :municipality sonja-muni :address "Paatoskuja 9")
           resp            (command user :submit-application :id application-id) => ok?
           application     (query-application user application-id)
-          email           (last-email)]
+          email           (last-email) => truthy]
       (:state application) => "submitted"
       (:to email) => (email-for-key user)
       (:subject email) => "Lupapiste.fi: Paatoskuja 9 - hakemuksen tila muuttunut"
@@ -88,10 +88,6 @@
     (:area building3) => "22"
     (:created building3) => "2013"))
 
-;;
-;; TODO: Aloitetulla hakemuksella valittavana vain "hae jatkoaikaa"-komento.
-;;       Suljetulla hakemuksella ei mitään.
-;;
 (fact* "Application can be set to Started state after verdict has been given, and after that to Closed state."
   (let [application-id         (create-app-id sonja
                                  :operation "ya-katulupa-vesi-ja-viemarityot"
@@ -110,14 +106,28 @@
 
         started-resp           (command sonja :inform-construction-started :id application-id :startedTimestampStr "31.12.2013") => ok?
         application            (query-application sonja application-id) => truthy
-        app-state              (:state application) => "constructionStarted"
-        closed-resp2           (command sonja :inform-construction-ready :id application-id :readyTimestampStr "31.12.2013" :lang "fi") => ok?
-        application            (query-application sonja application-id) => truthy
-        ]
-    (:state application) => "closed"
-    sonja =not=> (allowed? :inform-construction-started :id application-id)
-    sonja =not=> (allowed? :create-continuation-period-permit :id application-id)
-    ))
+        email                  (last-email) => truthy]
+
+    sonja => (allowed? :create-continuation-period-permit :id application-id)
+
+    (:state application) => "constructionStarted"
+    (:to email) => (email-for-key sonja)
+    (:subject email) => "Lupapiste.fi: Paatoskuja 11 - hakemuksen tila muuttunut"
+    (get-in email [:body :plain]) => (contains "Rakennusty\u00f6t aloitettu")
+    email => (partial contains-application-link? application-id)
+
+    (let [closed-resp            (command sonja :inform-construction-ready :id application-id :readyTimestampStr "31.12.2013" :lang "fi") => ok?
+          application            (query-application sonja application-id) => truthy
+          email                  (last-email)]
+
+      (:state application) => "closed"
+      sonja =not=> (allowed? :inform-construction-started :id application-id)
+      sonja =not=> (allowed? :create-continuation-period-permit :id application-id)
+
+      (:to email) => (email-for-key sonja)
+      (:subject email) => "Lupapiste.fi: Paatoskuja 11 - hakemuksen tila muuttunut"
+      (get-in email [:body :plain]) => (contains "Valmistunut")
+      email => (partial contains-application-link? application-id))))
 
 (fact* "Application cannot be set to Started state if it is not an YA type of application."
   (let [application            (create-and-submit-application sonja :municipality sonja-muni :address "Paatoskuja 11")
@@ -126,6 +136,5 @@
         verdict-resp           (command sonja :give-verdict :id application-id :verdictId "aaa" :status 42 :name "Paatoksen antaja" :given 123 :official 124) => ok?
         application            (query-application sonja application-id) => truthy
         app-state              (:state application) => "verdictGiven"]
-    (command sonja :inform-construction-started :id application-id :startedTimestampStr "31.12.2013") => (partial expected-failure? "error.invalid-permit-type")
-    ))
+    (command sonja :inform-construction-started :id application-id :startedTimestampStr "31.12.2013") => (partial expected-failure? "error.invalid-permit-type")))
 
