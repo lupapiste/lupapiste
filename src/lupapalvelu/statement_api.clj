53db7f44d250f0d8ec45ece9798f8c2003265ab9
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-01-13 16:36:11 +0200

    Give statementPerson role in auth array, save user-id

--- src/lupapalvelu/statement_api.clj
+++ src/lupapalvelu/statement_api.clj
@@ -67,45 +67,27 @@
    :roles       [:authority]
    :states      [:draft :info :open :submitted :complement-needed]
    :notified    true
-   :description "Adds statement-requests to the application and ensures writer-permission to all new users."}
+   :description "Adds statement-requests to the application and ensures permission to all new users."}
   [{user :user {:keys [organization] :as application} :application now :created :as command}]
   (organization/with-organization organization
     (fn [{:keys [statementPersons]}]
-      (let [personIdSet    (set personIds)
-            persons        (filter #(personIdSet (:id %)) statementPersons)
-            details        (map #(hash-map :person %) persons)]
-        (map 
-          (-> 
-            #(assoc % :user (user/get-user-by-email (get-in % :person :email)))
-            #(if-not (:user %) (fail! :error.not-found) %)
-            #(assoc % :statement {:id        (mongo/create-id)
-                                  :person    {:id (comment personista)
-                                              :user-id (comment userin id)
-                                              :text (comment personista)}
-                                  ;:user      (:user %)
-                                  :requested now
-                                  :given     nil
-                                  :status    nil})) details)
-            #(assoc-in % :user :statementId (get-in % :statement :id))
-            
-            (map 
-              details        (fn [person] )
-            
-              ->user-or-fail #(assoc (or (user/get-user-by-email (:email %)) (fail! :error.not-found)) :text (:text %))
-              users          (->user-or-fail persons) 
-              writers        (map #(user/user-in-role % :stementPerson) users)
-              ;new-writers    (filter #(not (domain/has-auth? application (:id %))) writers)
-              ;unique-writers (distinct new-writers)
-              ->statement    (fn [person] {:id        (mongo/create-id)
-                                           :person    person
-                                           :user      (some #(when (= (:email person) (:username %)) %) writers)
-                                           :requested now
-                                           :given     nil
-                                           :status    nil})
-              statements    (map ->statement persons)
-              writers   (map #(assoc (:user %) :stementId (:id %))  statements)]
-          (update-application command {$pushAll {:statements statements :auth writers}})
-          (notifications/notify! :request-statement (assoc command :data {:users users}))))))
+      (let [personIdSet (set personIds)
+            persons     (filter #(personIdSet (:id %)) statementPersons)
+            details     (map #(let [user (or (user/get-user-by-email (:email %)) (fail! :error.not-found))
+                                    statement-id (mongo/create-id)
+                                    statement-person (assoc % :userId (:id user))]
+                                {:statement {:id statement-id
+                                             :person    statement-person
+                                             :requested now
+                                             :given     nil
+                                             :status    nil}
+                                 :auth (user/user-in-role user :statementPerson :statementId statement-id)
+                                 :mail-list (assoc user :email (:email %))}) persons)
+            statements (map :statement details)
+            auth       (map :auth details)
+            mail-list  (map :mail-list details)]
+          (update-application command {$pushAll {:statements statements :auth auth}})
+          (notifications/notify! :request-statement (assoc command :data {:users mail-list}))))))
 
 (defcommand delete-statement
   {:parameters [id statementId]
