e813a75a90bf23b8c671688553e02ada4485c229
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-01-23 14:33:04 +0200

    Old version did not work any better

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
                  [cljts "0.2.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]
                                   [ring-mock "0.1.5"]
-                                  [clj-ssh "0.5.6"]]
+                                  [clj-ssh "0.5.7"]]
                    :plugins [[lein-midje "3.1.1"]
                              [lein-buildid "0.2.0"]
                              [lein-nitpicker "0.4.0"]
