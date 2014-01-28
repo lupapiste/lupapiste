94071f74893c2d463382736d4bc50e415a4fbad1
Samppa Saarela <samppa.saarela@solita.fi>
2014-01-28 15:51:02 +0200

    Remove suorakieltoKytkin from application.

--- src/lupapalvelu/document/rakennuslupa_canonical.clj
+++ src/lupapalvelu/document/rakennuslupa_canonical.clj
@@ -195,7 +195,7 @@
 (defn- get-lisatiedot [documents lang]
   (let [lisatiedot (:data (first (:lisatiedot documents)))
         aloitusoikeus (:data (first (:aloitusoikeus documents)))]
-    {:Lisatiedot {:suoramarkkinointikieltoKytkin (true? (-> lisatiedot :suoramarkkinointikielto :value))
+    {:Lisatiedot {:suoramarkkinointikieltoKytkin false
                   :asioimiskieli (if (= lang "sv")
                                    "ruotsi"
                                    "suomi")}}))
