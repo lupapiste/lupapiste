3a8fb84b1efe44148ed489c5d762dd562f9c3790
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2014-02-04 09:20:09 +0200

    Skemojen validisuus testaus lisatty

--- test/lupapalvelu/document/ymparisto_ilmoitukset_canonical_test.clj
+++ test/lupapalvelu/document/ymparisto_ilmoitukset_canonical_test.clj
@@ -1,7 +1,86 @@
 (ns lupapalvelu.document.ymparisto-ilmoitukset-canonical-test
-  (:require [lupapalvelu.document.ymparisto-ilmoitukset-canonical :as yic]
+  (:require [midje.sweet :refer :all]
+            [lupapalvelu.document.ymparisto-ilmoitukset-canonical :as yic]
             [lupapalvelu.factlet :as fl]
-            [midje.sweet :refer :all]))
+            [lupapalvelu.document.canonical-test-common :refer :all]))
+
+(def hakija {:created 1391415025497,
+             :data
+             {:_selected {:value "henkilo"},
+              :henkilo
+              {:henkilotiedot
+               {:etunimi
+                {:modified 1391415662591, :value "Pekka"},
+                :hetu {:modified 1391415675117, :value "133450-2356"},
+                :sukunimi {:modified 1391415662591, :value "Borga"}},
+               :osoite
+               {:katu {:modified 1391415683882, :value "Murskaajankatu 5"},
+                :postinumero {:modified 1391415686665, :value "36570"},
+                :postitoimipaikannimi
+                {:modified 1391415696674, :value "Kaivonto"}},
+               :userId
+               {:modified 1391415662621, :value "777777777777777777000033"},
+               :yhteystiedot
+               {:email {:modified 1391415662591, :value "pekka.borga@porvoo.fi"},
+                :puhelin {:modified 1391415662591, :value "121212"}}}},
+             :id "52ef4ef14206428d3c0394b6",
+             :schema-info
+             {:approvable true,
+              :subtype "hakija",
+              :name "hakija",
+              :removable true,
+              :repeating true,
+              :version 1,
+              :type "party",
+              :order 3}})
+
+(def kesto {:created 1391415025497,
+            :data
+            {:kesto
+             {:alku {:modified 1391415615718, :value "03.02.2014"},
+              :kello
+              {:arkisin {:modified 1391415637288, :value "07.00 - 16:00"},
+               :lauantait {:modified 1391415639677, :value "-"},
+               :pyhat {:modified 1391415640276, :value "-"}},
+              :loppu {:modified 1391415618809, :value "07.02.2014"}}},
+            :id "52ef4ef14206428d3c0394b7",
+            :schema-info {:name "ymp-ilm-kesto", :version 1, :order 60}})
+
+(def meluilmo {:created 1391415025497,
+               :data
+               {:melu
+                {:melu10mdBa {:modified 1391415596372, :value "150"},
+                 :mittaus {:modified 1391415612510, :value "dbsid?"},
+                 :paivalla {:modified 1391415601672
+                            :value "150"},
+                 :yolla {:modified 1391415602101, :value "0"}},
+                :rakentaminen
+                {:koneet
+                 {:modified 1391415557870,
+                  :value
+                  "Murskauksen ja rammeroinnin vaatimat koneet, sekä pyöräkuormaaja. "},
+                 :kuvaus
+                 {:modified 1391415519512,
+                  :value
+                  "Meluilmoitus louhinnasta, rammeroinnista ja murskauksesta"},
+                 :melua-aihettava-toiminta
+                 {:modified 1391415423129, :value "louhinta"}},
+                :tapahtuma
+                {:kuvaus
+                 {:modified 1391415593261,
+                  :value "Vähän virkistystä tähän väliin"},
+                 :nimi {:modified 1391415570121, :value "Louhijouden saunailta"},
+                 :ulkoilmakonsertti {:modified 1391415571551, :value true}}},
+               :id "52ef4ef14206428d3c0394b5",
+                                 :schema-info
+                                 {:order 50,
+                                  :version 1,
+                                  :name "meluilmoitus",
+                                  :op
+                                  {:id "52ef4ef14206428d3c0394b4",
+                                   :name "meluilmoitus",
+                                   :created 1391415025497},
+                                  :removable true}})
 
 (def meluilmoitus-application {:sent nil,
                                :neighbors {},
@@ -45,82 +124,9 @@
                                :propertyId "63844900010004",
                                :verdicts [],
                                :documents
-                               [{:created 1391415025497,
-                                 :data
-                                 {:_selected {:value "henkilo"},
-                                  :henkilo
-                                  {:henkilotiedot
-                                   {:etunimi
-                                    {:modified 1391415662591, :value "Pekka"},
-                                    :hetu {:modified 1391415675117, :value "133450-2356"},
-                                    :sukunimi {:modified 1391415662591, :value "Borga"}},
-                                   :osoite
-                                   {:katu {:modified 1391415683882, :value "Murskaajankatu 5"},
-                                    :postinumero {:modified 1391415686665, :value "36570"},
-                                    :postitoimipaikannimi
-                                    {:modified 1391415696674, :value "Kaivonto"}},
-                                   :userId
-                                   {:modified 1391415662621, :value "777777777777777777000033"},
-                                   :yhteystiedot
-                                   {:email {:modified 1391415662591, :value "pekka.borga@porvoo.fi"},
-                                    :puhelin {:modified 1391415662591, :value "121212"}}}},
-                                 :id "52ef4ef14206428d3c0394b6",
-                                 :schema-info
-                                 {:approvable true,
-                                  :subtype "hakija",
-                                  :name "hakija",
-                                  :removable true,
-                                  :repeating true,
-                                  :version 1,
-                                  :type "party",
-                                  :order 3}}
-                                {:created 1391415025497,
-                                 :data
-                                 {:melu
-                                  {:melu10mdBa {:modified 1391415596372, :value "150"},
-                                   :mittaus {:modified 1391415612510, :value "dbsid?"},
-                                   :paivalla {:modified 1391415601672
-                                              :value "150"},
-                                   :yolla {:modified 1391415602101, :value "0"}},
-                                  :rakentaminen
-                                  {:koneet
-                                   {:modified 1391415557870,
-                                    :value
-                                    "Murskauksen ja rammeroinnin vaatimat koneet, sekä pyöräkuormaaja. "},
-                                   :kuvaus
-                                   {:modified 1391415519512,
-                                    :value
-                                    "Meluilmoitus louhinnasta, rammeroinnista ja murskauksesta"},
-                                   :melua-aihettava-toiminta
-                                   {:modified 1391415423129, :value "louhinta"}},
-                                  :tapahtuma
-                                  {:kuvaus
-                                   {:modified 1391415593261,
-                                    :value "Vähän virkistystä tähän väliin"},
-                                   :nimi {:modified 1391415570121, :value "Louhijouden saunailta"},
-                                   :ulkoilmakonsertti {:modified 1391415571551, :value true}}},
-                                 :id "52ef4ef14206428d3c0394b5",
-                                 :schema-info
-                                 {:order 50,
-                                  :version 1,
-                                  :name "meluilmoitus",
-                                  :op
-                                  {:id "52ef4ef14206428d3c0394b4",
-                                   :name "meluilmoitus",
-                                   :created 1391415025497},
-                                  :removable true}}
-                                {:created 1391415025497,
-                                 :data
-                                 {:kesto
-                                  {:alku {:modified 1391415615718, :value "03.02.2014"},
-                                   :kello
-
-                                   {:arkisin {:modified 1391415637288, :value "07.00 - 16:00"},
-                                    :lauantait {:modified 1391415639677, :value "-"},
-                                    :pyhat {:modified 1391415640276, :value "-"}},
-                                   :loppu {:modified 1391415618809, :value "07.02.2014"}}},
-                                 :id "52ef4ef14206428d3c0394b7",
-                                 :schema-info {:name "ymp-ilm-kesto", :version 1, :order 60}}],
+                               [hakija
+                                meluilmo
+                                kesto],
                                :_statements-seen-by {},
                                :modified 1391415696674,
                                :comments [],
@@ -129,10 +135,15 @@
                                :id "LP-638-2014-00001",
                                :municipality "638"})
 
+(fact "Meta test: hakija"          hakija         => valid-against-current-schema?)
+(fact "Meta test: kesto"           kesto          => valid-against-current-schema?)
+(fact "Meta test: meluilmo"        meluilmo       => valid-against-current-schema?)
+
+
 (fl/facts* "Meluilmoitus to canonical"
            (let [canonical (yic/meluilmoitus-canonical meluilmoitus-application "fi") => truthy
                  Ilmoitukset (:Ilmoitukset canonical) => truthy
-                 toimutuksenTiedot (:toimutuksenTiedot Ilmoitukset) => truthyu
+                 toimutuksenTiedot (:toimutuksenTiedot Ilmoitukset) => truthy
                  aineistonnimi (:aineistonnimi toimutuksenTiedot) => (:title meluilmoitus-application)
                  ]
              (clojure.pprint/pprint canonical)))
