581c8141a527045d922e759676069bedd7aefd21
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-03-24 14:25:39 +0200

    Removed invalid test

--- test/lupapalvelu/document/maa_aines_canonical_test.clj
+++ test/lupapalvelu/document/maa_aines_canonical_test.clj
@@ -115,13 +115,6 @@
         (:laskuviite maksaja) => nil))
 
     (facts "sijainti"
-      (fact "alueenKiinteistonSijainti"
-        (let [sijainti (-> hakemus :alueenKiinteistonSijainti :Sijainti) => truthy
-              osoite (:osoite sijainti)]
-
-          (:osoitenimi osoite) => {:teksti "Londb\u00f6lentie 97"}
-          (:piste sijainti) => {:Point {:pos "428195.77099609 6686701.3931274"}}))
-
       (fact "osoite"
         (let [sijainti (-> maa-aineslupa :sijaintitieto first :Sijainti) => truthy
              osoite (:osoite sijainti)]
