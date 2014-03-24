09f838d32958f7d1f544d0c39e864914f8de92b1
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2014-03-24 10:50:35 +0200

    v

--- src/lupapalvelu/document/vesihuolto_canonical.clj
+++ src/lupapalvelu/document/vesihuolto_canonical.clj
@@ -1,5 +1,8 @@
 (ns lupapalvelu.document.vesihuolto-canonical
-  (require [lupapalvelu.document.vesihuolto-schemas :as vh-schemas]))
+  (require [lupapalvelu.document.vesihuolto-schemas :as vh-schemas]
+           [lupapalvelu.document.canonical-common :refer :all]))
 
 (defn vapautus-canonical [application lang]
+  {:Vesihuoltolaki
+   {:toimituksenTiedot :d}}
   )
