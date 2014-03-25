5e929987f6a530ecf6f41ffdef9cc98006076126
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2014-03-25 12:20:48 +0200

    kanoonista muunnosta

--- src/lupapalvelu/document/vesihuolto_canonical.clj
+++ src/lupapalvelu/document/vesihuolto_canonical.clj
@@ -1,8 +1,23 @@
 (ns lupapalvelu.document.vesihuolto-canonical
   (require [lupapalvelu.document.vesihuolto-schemas :as vh-schemas]
-           [lupapalvelu.document.canonical-common :refer :all]))
+           [lupapalvelu.document.canonical-common :refer :all]
+           [lupapalvelu.document.tools :as tools]))
 
 (defn vapautus-canonical [application lang]
-  {:Vesihuoltolaki
-   {:toimituksenTiedot :d}}
+  (let [application (tools/unwrapped application)
+        documents (documents-by-type-without-blanks application)]
+    {:Vesihuoltolaki
+     {:toimituksenTiedot (toimituksen-tiedot application lang)
+      :vapautukset
+      {:Vapautus
+       {:kasittelytietotieto (get-kasittelytieto-ymp application :KasittelyTieto)
+        :luvanTunnistetiedot (lupatunnus (:id application))
+        :lausuntotieto (get-statements (:statements application))
+        :vapautusperuste nil
+        :vapautushakemustieto
+        {:Vapautushakemus
+         {:hakija ()}}
+        {}
+        :asianKuvaus (:kuvaus (first (:hankkeen-kuvaus-vesihuolto documents)))
+        }}}})
   )
