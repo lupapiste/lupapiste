effec422c02df4fe8b633738535a6211f3f5ee4a
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2014-03-26 13:00:08 +0200

    Canoonista muunninta

--- src/lupapalvelu/document/vesihuolto_canonical.clj
+++ src/lupapalvelu/document/vesihuolto_canonical.clj
@@ -3,6 +3,27 @@
            [lupapalvelu.document.canonical-common :refer :all]
            [lupapalvelu.document.tools :as tools]))
 
+
+(defn get-talousvedet [talousvedet]
+  {:talousvedet
+   {:hankinta (:hakinta talousvedet)
+    :johdatus (:johdatus talousvedet)
+    :riittavyys (:riittavyys talousvedet)}})
+
+
+(defn get-vapautus-kohde [{property-id :PropertyId} documents]
+    (let [kiinteisto (first (:vesihuolto-kiinteisto documents))]
+      (merge {:kiinteistorekisteritunnus property-id
+             :kiinteistonRakennusTieto
+             (for [rakennus (:kiinteistoonKuuluu (:vesihuolto-kiinteisto documents))]
+               {:KiinteistonRakennus
+                {:kayttotarkoitustieto (:rakennuksenTyypi rakennus)
+                 :kohteenVarustelutaso (map (fn [[k v]] (when v (name k))) (:kohteenVarustelutaso rakennus))
+                 :vapautus (true? (:vapautus rakennus))}})}
+             {:hulevedet (:hulevedet (first (:hulevedet documents)))}
+             (get-talousvedet (first (:talousvedet documents)))
+             {:jatevedet (:kuvaus (first (:jatevedet documents)))})))
+
 (defn vapautus-canonical [application lang]
   (let [application (tools/unwrapped application)
         documents (documents-by-type-without-blanks application)]
@@ -13,11 +34,13 @@
        {:kasittelytietotieto (get-kasittelytieto-ymp application :KasittelyTieto)
         :luvanTunnistetiedot (lupatunnus (:id application))
         :lausuntotieto (get-statements (:statements application))
-        :vapautusperuste nil
+        :vapautusperuste ""
         :vapautushakemustieto
         {:Vapautushakemus
-         {:hakija ()}}
-        {}
+         {:hakija (get-henkilo (first (:hakija documents)))
+          :kohde (get-vapautus-kohde application documents)}}
+        :sijaintitieto (get-sijaintitieto application)
+
         :asianKuvaus (:kuvaus (first (:hankkeen-kuvaus-vesihuolto documents)))
         }}}})
   )
