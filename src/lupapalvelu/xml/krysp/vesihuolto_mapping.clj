e64ec1bf709524b8bc0f03c63ce4d051333e6638
Tero Tukiainen <Tero.Tukiainen@solita.fi>
2014-03-27 13:21:19 +0200

    Vesihuollon mappays

--- src/lupapalvelu/xml/krysp/vesihuolto_mapping.clj
+++ src/lupapalvelu/xml/krysp/vesihuolto_mapping.clj
@@ -7,14 +7,30 @@
 ; :xsi:schemaLocation (mapping-common/schemalocation "ymparisto/vesihuoltolaki" "2.1.1")
 
 (def vesihuolto-to-krysp [{:tag :ymv:VesihuoltolakiType
-                           :child [{:tag :toimituksenTiedot :child mapping-common/toimituksenTiedot}
-                                   {:tag :vapatukset
-                                    :child [{:tag :Vapautus
-                                             :child [{:tag :kasittelytietotieto :child [mapping-common/ymp-kasittelytieto-children]}
-                                                     {:tag :luvanTunnistetiedot
-                                                      :child [mapping-common/lupatunnus]}
-                                                     {:tag :lausuntotieto
-                                                      :child [mapping-common/lausunto_213]}]}]}
-                                   {:tag :vapautushakemustieto :child []}
-                                   {:tag :liitetieto :child [{:tag :Liite :child mapping-common/liite-children_213}]}
-                                   ]}])
+                           :child
+                           [{:tag :toimituksenTiedot :child mapping-common/toimituksenTiedot}
+                            {:tag :vapatukset
+                             :child [{:tag :Vapautus
+                                      :child [{:tag :kasittelytietotieto :child [mapping-common/ymp-kasittelytieto-children]}
+                                              {:tag :luvanTunnistetiedot
+                                               :child [mapping-common/lupatunnus]}
+                                              {:tag :lausuntotieto
+                                               :child [mapping-common/lausunto_213]}
+                                              {:tag :vapautusperuste}
+                                              {:tag :vapautushakemustieto
+                                               :child [{:tag :Vapautushakemus
+                                                        :child [{:tag :hakija
+                                                                 :child [mapping-common/henkilo-child-ns-yht]}
+                                                                {:tag :kohde
+                                                                 :child [{:tag :kiinteistorekisteritunnus}
+                                                                         {:tag :kiinteistinRakennusTieto
+                                                                          :child [{:tag :KiinteistinRakennus
+                                                                                   :child [{:tag :kayttotarkoitustieto}
+                                                                                           {:tag :kohteenVarustelutaso}
+                                                                                           {:tag :haetaanVapautustaKytkin}]}]}]}
+                                                                (mapping-common/sijaintitieto "yht")]}]}
+                                              {:tag :liitetieto
+                                               :child [{:tag :Liite :child mapping-common/liite-children_213}]}
+                                              {:tag :asianKuvaus}]}]}
+
+                            {:tag :liitetieto :child [{:tag :Liite :child mapping-common/liite-children_213}]}]}])
