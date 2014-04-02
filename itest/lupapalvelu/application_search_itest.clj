4801a61f65ad0e96d0dfcc755d3bccbe88437540
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-02 16:46:54 +0300

    Added base for itest

---
+++ itest/lupapalvelu/application_search_itest.clj
@@ -0,0 +1,17 @@
+(ns lupapalvelu.application-search-itest
+  (:require [midje.sweet :refer :all]
+            [lupapalvelu.itest-util :refer :all]
+            [lupapalvelu.factlet  :refer :all]
+            ))
+
+(apply-remote-minimal)
+
+(facts "Search"
+  (let [application-id (create-app-id mikko :municipality sonja-muni :address "Hakukuja 123")]
+
+    application-id => truthy
+    (datatables mikko :applications-for-datatables :params {:filter-kind "applications" :filter-state "all"}) => ok?
+
+    )
+  )
+
