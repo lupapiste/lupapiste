3ca08c71fdd65c4632d6115e4be07a4a85e494b8
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-03 10:12:01 +0300

    Search by property id

--- itest/lupapalvelu/application_search_itest.clj
+++ itest/lupapalvelu/application_search_itest.clj
@@ -6,40 +6,59 @@
 
 (apply-remote-minimal)
 
+(defn- num-of-results? [n response]
+  (and
+    (= n (get-in response [:data :iTotalDisplayRecords]))
+    (= n (count (get-in response [:data :aaData])))))
+
+(def no-results? (partial num-of-results? 0))
+(def one-result? (partial num-of-results? 1))
+
 (facts* "Search"
-  (let [application-id-addr (create-app-id mikko :municipality sonja-muni :address "Hakukuja 123") => truthy]
+  (let [property-id (str sonja-muni "-123-0000-1234")
+        application-id-addr (create-app-id mikko :municipality sonja-muni :address "Hakukuja 123" :propertyId (sade.util/to-property-id property-id)) => truthy]
 
     (facts "by address"
 
       (fact "no matches"
         (let [results0 (datatables mikko :applications-for-datatables :params {:filter-search "hakukatu"})]
           results0 => ok?
-          (get-in results0 [:data :iTotalDisplayRecords]) => 0
-          (count (get-in results0 [:data :aaData])) => 0))
+          results0 => no-results?)
 
       (fact "one match"
         (let [results1 (datatables mikko :applications-for-datatables :params {:filter-search "hakukuja"})]
           results1 => ok?
-          (get-in results1 [:data :iTotalDisplayRecords]) => 1
-          (count (get-in results1 [:data :aaData])) => 1
-          (get-in results1 [:data :aaData 0 :id]) => application-id-addr)))
+          results1 => one-result?
+          (get-in results1 [:data :aaData 0 :id]) => application-id-addr))))
 
     (facts "by ID"
 
       (fact "no matches"
         (let [results0 (datatables mikko :applications-for-datatables :params {:filter-search (str "LP-" sonja-muni "-2010-00001")})]
           results0 => ok?
-          (get-in results0 [:data :iTotalDisplayRecords]) => 0
-          (count (get-in results0 [:data :aaData])) => 0))
+          results0 => no-results?)
 
       (fact "one match"
         (let [results1 (datatables mikko :applications-for-datatables :params {:filter-search application-id-addr})]
           results1 => ok?
-          (get-in results1 [:data :iTotalDisplayRecords]) => 1
-          (count (get-in results1 [:data :aaData])) => 1
-          (get-in results1 [:data :aaData 0 :id]) => application-id-addr))
-      )
+          results1 => one-result?
+          ;(clojure.pprint/pprint results1)
+          (get-in results1 [:data :aaData 0 :id]) => application-id-addr))))
 
-    )
-  )
+    (facts "by property ID"
+
+      (fact "no matches"
+        (let [results0 (datatables mikko :applications-for-datatables :params {:filter-search (str sonja-muni "-123-0000-1230")})]
+          results0 => ok?
+          results0 => no-results?)
+
+      (fact "one match"
+        (let [results1 (datatables mikko :applications-for-datatables :params {:filter-search property-id})]
+          results1 => ok?
+          results1 => one-result?
+          (get-in results1 [:data :aaData 0 :id]) => application-id-addr))))
+
+
+
+    ))
 
