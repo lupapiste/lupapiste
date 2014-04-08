dfbb206efde5d702c8c2769b86e1afb582c76865
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-08 12:04:20 +0300

    Created issues for missing validators

--- src/lupapalvelu/document/model.clj
+++ src/lupapalvelu/document/model.clj
@@ -65,13 +65,15 @@
     (when-not (or (s/blank? v) (contains? accepted-values v))
       [:warn "illegal-value:select"])))
 
-;; FIXME implement validator, the same as :select?
+;; FIXME https://support.solita.fi/browse/LUPA-1453
+;; implement validator, the same as :select?
 (defmethod validate-field :radioGroup [elem v] nil)
 
 (defmethod validate-field :buildingSelector [elem v] (subtype/subtype-validation {:subtype :rakennusnumero} v))
 (defmethod validate-field :newBuildingSelector [elem v] (subtype/subtype-validation {:subtype :number} v))
 
-;; FIXME implement validator (mongo id, check that user exists)
+;; FIXME https://support.solita.fi/browse/LUPA-1454
+;; implement validator (mongo id, check that user exists)
 (defmethod validate-field :personSelector [elem v] nil)
 
 (defmethod validate-field nil [_ _]
