1dc67e989d28738ad49b94d4e9c566fe533e94c7
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-09 18:46:49 +0300

    Fixed test

--- test/lupapalvelu/document/canonical_common_test.clj
+++ test/lupapalvelu/document/canonical_common_test.clj
@@ -16,7 +16,6 @@
                 :closed (* 6 day))
               (update-in [:verdicts] conj {:timestamp (* 7 day)}))]
 
-   (fact "draft" (state-timestamp (assoc app :state "draft")) => 0)
    (fact "open" (state-timestamp (assoc app :state "open")) => day)
    (fact "open" (state-timestamp (dissoc (assoc app :state "open") :opened)) => 0)
    (fact "complement-needed" (state-timestamp (assoc app :state "complement-needed")) => (* 2 day))
@@ -26,8 +25,7 @@
    (fact "closed" (state-timestamp (assoc app :state "closed")) => (* 6 day))
    (fact "verdictGiven" (state-timestamp (assoc app :state "verdictGiven")) => (* 7 day))
 
-   (all-state-timestamps app) => {:draft 0
-                                  :open day
+   (all-state-timestamps app) => {:open day
                                   :complement-needed (* 2 day)
                                   :submitted (* 3 day)
                                   :sent (* 3 day)
@@ -35,8 +33,7 @@
                                   :closed (* 6 day)
                                   :verdictGiven (* 7 day)}
 
-   (all-state-timestamps (dissoc app :closed :verdicts :started :complementNeeded)) => {:draft 0
-                                                                                        :open day
+   (all-state-timestamps (dissoc app :closed :verdicts :started :complementNeeded)) => {:open day
                                                                                         :complement-needed nil
                                                                                         :submitted (* 3 day)
                                                                                         :sent (* 3 day)
