471da4dcff1a5fa346585dc760249bc5a9fbf1db
Jari Asp <jari.asp@solita.fi>
2014-04-14 12:46:47 +0300

    cleanup

--- src/lupapalvelu/batchrun.clj
+++ src/lupapalvelu/batchrun.clj
@@ -2,7 +2,6 @@
   (:require [lupapalvelu.notifications :as notifications]
             [lupapalvelu.neighbors :as neighbors]
             [lupapalvelu.open-inforequest :as inforequest]
-;            [clj-time.local :refer [local-now to-local-date-time]]
             [clj-time.core :refer [weeks months ago]]
             [clj-time.coerce :refer [to-long]]
             [monger.operators :refer :all]
@@ -12,15 +11,12 @@
             [lupapalvelu.user :as user]
             [lupapalvelu.notifications :as notifications]
             [sade.util :as util]
-;            [lupapalvelu.itest-util :refer [last-email] ] ;; TODO: Poista tama lopuksi!
             [sade.env :as env]))
 
 
 ; default pattern is daily at 2am
-(def ^:dynamic ^:String *send-interval-pattern* "0 2") ; TODO: Joka yo backuppien jalkeen, selvita infralta, koska ne menevat
+;(def ^:dynamic ^:String *send-interval-pattern* "0 2") ; TODO: Joka yo backuppien jalkeen, selvita infralta, koska ne menevat
 
-;  to-local-date-time
-;  today-at-midnight
 (defn- get-timestamp-from-now [time-key amount]
   {:pre [(#{:week :month} time-key)]}
   (let [time-fn (case time-key
@@ -29,16 +25,13 @@
                   )]
     (to-long (-> amount time-fn ago))))
 
-;; TODO: Pitaisiko tama funktio olla jossain utils-namespacessa?
 (defn- get-app-owner-email [application]
-  (println "\n app-id: " (:id application))
   (let [owner (domain/get-auths-by-role application :owner)
 ;        owner-id (-> owner first :id)
 ;        user (user/get-user-by-id owner-id)
         ]
 ;    (:email user)
-    (-> owner first :username)
-    ))
+    (-> owner first :username)))
 
 
 ;; For the "open info request reminder"
@@ -65,17 +58,11 @@
 
 
 (defn- send-reminder-emails []
-
   (let [timestamp-1-week-ago (get-timestamp-from-now :week 1)
         timestamp-1-month-ago (get-timestamp-from-now :month 1)]
-    (println "\n timestamp-1-week-ago: " timestamp-1-week-ago "\n")
-    (println "\n timestamp-1-month-ago: " timestamp-1-month-ago "\n")
-
 
     ;; * _Lausuntopyynto_:
     ;; Pyyntoon ei ole vastattu viikon kuluessa ja hakemuksen tila on valmisteilla tai vireilla. Lahetetaan viikoittain uudelleen.
-    ;;
-    ;; - kts. statement_api.clj
     ;;
     (let [apps (mongo/select :applications
                  {:state {$in ["open" "submitted"]}
@@ -91,17 +78,14 @@
             (notifications/notify! :reminder-request-statement {:application app
                                                                 :data {:email (get-app-owner-email app)
                                                                        :created-date (util/to-local-date requested)}})))
-        apps)
-      )
+        apps))
 
 
     ;; * _Neuvontapyynto_:
     ;; Neuvontapyyntoon ei ole vastattu viikon kuluessa eli neuvontapyynnon tila on avoin. Lahetetaan viikoittain uudelleen.
     ;;
-    ;; *** TODO: Tarkoittaako se, että "last-used" on nil sitä, että oir:iin ei ole vastattu? ***
-    ;;
     (let [oirs (mongo/select :open-inforequest-token {:created {$lt timestamp-1-week-ago}
-                                                      :last-used nil})]
+                                                      :last-used nil})]  ;; TODO: Tarkoittaako "last-used" == nil sita, etta oir:iin ei ole vastattu?
       (map
         (fn [oir]
           (let [app (mongo/by-id :applications (:application-id oir))]
@@ -154,11 +138,6 @@
     ;; Hakemuksen tila on valmisteilla tai vireilla, mutta edellisesta paivityksesta on aikaa yli kuukausi. Lahetetaan kuukausittain uudelleen.
     ;;
     (let [apps (mongo/select :applications {:modified {$lt timestamp-1-month-ago}})]
-      (println "\n apps: ")
-      (clojure.pprint/pprint apps)
-      (println "\n")
-
-      ; TODO: Kaytetaan Owner-tyypin authin emailia - onko ok?
       (map
         (fn [app] (notifications/notify! :reminder-application-state {:application app
                                                                       :data {:email (get-app-owner-email app)}})) ; TODO: Kaytetaan Owner-tyypin authin emailia - onko ok?
