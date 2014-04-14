d37834ab8b2e30fc01b6847bb069c5bc1dc5f2a3
Jari Asp <jari.asp@solita.fi>
2014-04-14 13:08:21 +0300

    indentation

--- itest/lupapalvelu/neighbor_itest.clj
+++ itest/lupapalvelu/neighbor_itest.clj
@@ -139,99 +139,99 @@
        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :yhteystiedot :puhelin) => "040-2345678"))
 
     (fact "neighbor application query does not return hetu"
-     (let [resp        (query pena :neighbor-application
-                                   :applicationId application-id
-                                   :neighborId neighborId
-                                   :token token)
-           application (:application resp)
-           hakija-doc  (tools/unwrapped (domain/get-document-by-id application hakija-doc-id))
-           uusirak-doc (tools/unwrapped (domain/get-document-by-id application uusirak-doc-id))]
+      (let [resp        (query pena :neighbor-application
+                                    :applicationId application-id
+                                    :neighborId neighborId
+                                    :token token)
+            application (:application resp)
+            hakija-doc  (tools/unwrapped (domain/get-document-by-id application hakija-doc-id))
+            uusirak-doc (tools/unwrapped (domain/get-document-by-id application uusirak-doc-id))]
 
-       resp => truthy
-       resp => (contains {:ok true})
-       application => truthy
+        resp => truthy
+        resp => (contains {:ok true})
+        application => truthy
 
-       (-> hakija-doc :data :henkilo :henkilotiedot :etunimi) => "Zebra"
-       (-> hakija-doc :data :henkilo :henkilotiedot :sukunimi) => "Zorro"
-       (-> hakija-doc :data :henkilo :henkilotiedot :hetu) => nil
-       (-> hakija-doc :data :henkilo :henkilotiedot :turvakieltoKytkin) => nil
-       (-> hakija-doc :data :henkilo :yhteystiedot  :puhelin) => nil
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :etunimi) => "Gustav"
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :sukunimi) => "Golem"
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :turvakieltoKytkin) => nil
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :hetu) => nil
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :yhteystiedot :puhelin) => nil
-       (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :osoite :katu) => nil
+        (-> hakija-doc :data :henkilo :henkilotiedot :etunimi) => "Zebra"
+        (-> hakija-doc :data :henkilo :henkilotiedot :sukunimi) => "Zorro"
+        (-> hakija-doc :data :henkilo :henkilotiedot :hetu) => nil
+        (-> hakija-doc :data :henkilo :henkilotiedot :turvakieltoKytkin) => nil
+        (-> hakija-doc :data :henkilo :yhteystiedot  :puhelin) => nil
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :etunimi) => "Gustav"
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :sukunimi) => "Golem"
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :turvakieltoKytkin) => nil
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :henkilotiedot :hetu) => nil
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :yhteystiedot :puhelin) => nil
+        (-> uusirak-doc :data :rakennuksenOmistajat :0 :henkilo :osoite :katu) => nil
 
-       (facts "random testing about content"
-        (:comments application) => nil
-        (count (:documents application)) => 4 ; evil
+        (facts "random testing about content"
+          (:comments application) => nil
+          (count (:documents application)) => 4 ; evil
 
-        (fact "attachments"
-          (fact "there are some attachments"
-            (->> application :attachments count) => pos?)
-          (fact "everyone is paapiirustus"
-            (->> application :attachments (some (fn-> :type :type-group (not= "paapiirustus")))) => falsey)
+          (fact "attachments"
+            (fact "there are some attachments"
+              (->> application :attachments count) => pos?)
+            (fact "everyone is paapiirustus"
+              (->> application :attachments (some (fn-> :type :type-group (not= "paapiirustus")))) => falsey)
 
-          (let [file-id (->> application :attachments first :latestVersion :fileId)]
-            (fact "downloading should be possible"
-              (raw nil "neighbor-download-attachment" :neighbor-id neighborId :token token :file-id file-id) => http200?)
+            (let [file-id (->> application :attachments first :latestVersion :fileId)]
+              (fact "downloading should be possible"
+                (raw nil "neighbor-download-attachment" :neighbor-id neighborId :token token :file-id file-id) => http200?)
 
-            (fact "downloading with wrong token should not be possible"
-              (raw nil "neighbor-download-attachment" :neighbor-id neighborId :token "h4x3d token" :file-id file-id) => http401?)))
+              (fact "downloading with wrong token should not be possible"
+                (raw nil "neighbor-download-attachment" :neighbor-id neighborId :token "h4x3d token" :file-id file-id) => http401?)))
 
-        (:auth application) => nil)))
+          (:auth application) => nil)))
 
     (fact "without tupas, neighbor can't give response"
-     (command pena :neighbor-response
-       :applicationId application-id
-       :neighborId (name neighborId)
-       :token token
-       :stamp "INVALID"
-       :response "ok"
-       :message "kehno suunta") => invalid-vetuma?)
+      (command pena :neighbor-response
+        :applicationId application-id
+        :neighborId (name neighborId)
+        :token token
+        :stamp "INVALID"
+        :response "ok"
+        :message "kehno suunta") => invalid-vetuma?)
 
     (fact "with vetuma"
-     (let [stamp (vetuma-stamp!)]
+      (let [stamp (vetuma-stamp!)]
 
-       (fact "neighbor cant give ill response"
-         (command pena :neighbor-response
-           :applicationId application-id
-           :neighborId (name neighborId)
-           :stamp stamp
-           :token token
-           :response "ime parsaa!"
-           :message "kehno suunta") => invalid-response?)
+        (fact "neighbor cant give ill response"
+          (command pena :neighbor-response
+            :applicationId application-id
+            :neighborId (name neighborId)
+            :stamp stamp
+            :token token
+            :response "ime parsaa!"
+            :message "kehno suunta") => invalid-response?)
 
-       (fact "neighbor can give response"
-         (command pena :neighbor-response
-           :applicationId application-id
-           :neighborId (name neighborId)
-           :stamp stamp
-           :token token
-           :response "comments"
-           :message "kehno suunta") => ok?)
+        (fact "neighbor can give response"
+          (command pena :neighbor-response
+            :applicationId application-id
+            :neighborId (name neighborId)
+            :stamp stamp
+            :token token
+            :response "comments"
+            :message "kehno suunta") => ok?)
 
-       (fact "neighbour cant regive response 'cos vetuma has expired"
-         (command pena :neighbor-response
-           :applicationId application-id
-           :neighborId (name neighborId)
-           :stamp stamp
-           :token token
-           :response "comments"
-           :message "kehno suunta") => invalid-vetuma?)
+        (fact "neighbour cant regive response 'cos vetuma has expired"
+          (command pena :neighbor-response
+            :applicationId application-id
+            :neighborId (name neighborId)
+            :stamp stamp
+            :token token
+            :response "comments"
+            :message "kehno suunta") => invalid-vetuma?)
 
-       (fact "neighbour cant regive response with new tupas 'con token has expired"
-         (command pena :neighbor-response
-           :applicationId application-id
-           :neighborId (name neighborId)
-           :stamp (vetuma-stamp!)
-           :token token
-           :response "comments"
-           :message "kehno suunta") => invalid-token?)
+        (fact "neighbour cant regive response with new tupas 'con token has expired"
+          (command pena :neighbor-response
+            :applicationId application-id
+            :neighborId (name neighborId)
+            :stamp (vetuma-stamp!)
+            :token token
+            :response "comments"
+            :message "kehno suunta") => invalid-token?)
 
-       (fact "neighbour cant see application anymore"
-         (query pena :neighbor-application
-           :applicationId application-id
-           :neighborId (name neighborId)
-           :token token) => invalid-token?))))))
+        (fact "neighbour cant see application anymore"
+          (query pena :neighbor-application
+            :applicationId application-id
+            :neighborId (name neighborId)
+            :token token) => invalid-token?))))))
