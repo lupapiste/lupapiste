193d3c3b2338e483558db04a9ff5c582b736c1f7
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-15 10:53:42 +0300

    Check valid mac

---
+++ src/lupapalvelu/idf/idf_core.clj
@@ -0,0 +1,12 @@
+(ns lupapalvelu.idf.idf-core
+  (:require [digest]))
+
+(def ^:private secrets {"lupapiste" "TAMAN-MUODOSTI-LUPAPISTE"})
+
+(defn known-partner? [partner-name]
+  (contains? secrets partner-name))
+
+(defn calculate-mac [first-name last-name email phone street zip city marketing architect app id ts]
+  {:pre [(known-partner? app)]}
+  (println (str first-name last-name email phone street zip city marketing architect app id ts (secrets app)))
+  (digest/sha-256 (str first-name last-name email phone street zip city marketing architect app id ts (secrets app))))
