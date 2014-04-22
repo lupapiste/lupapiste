53f72566537f0769298460ba3a42bed39c16db1c
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-22 14:25:45 +0300

    Scaffod for IDF client

--- src/lupapalvelu/idf/idf_client.clj
+++ src/lupapalvelu/idf/idf_client.clj
@@ -1,6 +1,38 @@
 (ns lupapalvelu.idf.idf-client
   "Identity federation client: create users to partner applications"
-  (:require [lupapalvelu.user :as user]
-            [sade.http :as http])
-  )
+  (:require [taoensso.timbre :as timbre :refer [debug debugf info infof warn warnf error errorf]]
+            [clojure.set :refer [rename-keys]]
+            [sade.http :as http]
+            [lupapalvelu.core :refer [now]]
+            [lupapalvelu.user :as user]
+            [lupapalvelu.idf.idf-core :refer :all]))
 
+(defn send-user-data [user partner-name]
+  {:pre [(known-partner? partner-name)]}
+  (let [url (url-for-partner partner-name)
+        params (-> user
+                 (rename-keys {:firstName :etunimi
+                               :lastName  :sukunimi
+                               :phone     :puhelin
+                               :city      :postinumero
+                               :street    :katuosoite
+                               :zip       :postitoimipaikka
+                               :allowDirectMarketing :suoramarkkinointilupa
+                               :architect :ammattilainen})
+                 (select-keys [:id :etunimi :sukunimi :email :puhelin
+                               :katuosoite :postinumero :postitoimipaikka
+                               :suoramarkkinointilupa :ammattilainen])
+                 (assoc :app "lupapiste"))
+        ts (now)
+        form-params (assoc params :ts ts :mac (calculate-mac query-params "lupapiste" ts))
+        _  (debugf "Send user %s data to %s" (:email user) url)
+        resp (http/post url {:form-params form-params, :follow-redirects false, :throw-exceptions false})]
+    (if (= 200 (:status resp))
+      (do
+        )
+      (errorf "Unable link %s to %s: %s" (:email user) partner-name)
+      )
+
+
+    )
+  )
