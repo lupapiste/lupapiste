3b38038290a3aeea2c885bb1af97407850de31fa
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-22 15:41:18 +0300

    Removed partnerApplications key at creation time, will be added later

--- src/lupapalvelu/user_api.clj
+++ src/lupapalvelu/user_api.clj
@@ -123,7 +123,6 @@
       (select-keys [:email :username :role :firstName :lastName :personId
                     :phone :city :street :zip :enabled :organization
                     :allowDirectMarketing :architect])
-      (util/assoc-when :partnerApplications (:partnerApplications user-data))
       (as-> user-data (merge {:firstName "" :lastName "" :username email} user-data))
       (assoc
         :email email
@@ -409,7 +408,7 @@
     (when-not vetuma-data (fail! :error.create-user))
     (try
       (infof "Registering new user: %s - details from vetuma: %s" (dissoc data :password) vetuma-data)
-      (if-let [user (create-new-user (user/current-user) (merge data vetuma-data {:email email :role "applicant"}))]
+      (if-let [user (create-new-user (user/current-user) (merge data vetuma-data {:email email :role "applicant" :enabled false}))]
         (do
           (vetuma/consume-user stamp)
           (ok :id (:_id user)))
