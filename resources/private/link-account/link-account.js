eff57b16631f643e12a6c2cdbef79f961ab380c9
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-04-24 09:42:14 +0300

    Link account phase 1

--- resources/private/link-account/link-account.js
+++ resources/private/link-account/link-account.js
@@ -1,8 +1,34 @@
 ;(function() {
   "use strict";
 
+  var statusModel = new LUPAPISTE.StatusModel();
+
+  hub.onPageChange("link-account", function() {
+
+    var token = pageutil.subPage();
+
+    if (!token) {
+      window.location.hash = "#!/register";
+      return false;
+    }
+
+    var urlPrefix = "/app/" + loc.getCurrentLanguage() + "/welcome";
+
+    $.get("/api/vetuma", {success: urlPrefix + "#!/link-account-2/" + token,
+                          cancel:  urlPrefix + "#!/link-account/cancel",
+                          error:   urlPrefix + "#!/link-account/error"}, function(d) {
+      $("#vetuma-link-account")
+        .html(d).find(":submit").addClass("btn btn-primary")
+                                .attr("value",loc("register.action"))
+                                .attr("id", "vetuma-linking-init");
+    });
+
+    statusModel.subPage(pageutil.subPage());
+
+  });
+
   $(function(){
-    //$("#link-account").applyBindings(statusModel);
+    $("#link-account").applyBindings(statusModel);
     $("#link-account-2").applyBindings({});
     $("#link-account-3").applyBindings({});
   });
