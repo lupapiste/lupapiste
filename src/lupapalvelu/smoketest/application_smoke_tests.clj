2682b58ca0d2917f451edb46a75cbe3b4efc96d9
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-05-09 12:26:09 +0300

    Smoke test now ignores invalid person ids in submitted applications

--- src/lupapalvelu/smoketest/application_smoke_tests.clj
+++ src/lupapalvelu/smoketest/application_smoke_tests.clj
@@ -8,22 +8,23 @@
 (def applications (delay (mongo/select :applications)))
 (def submitted-applications (delay (mongo/select :submitted-applications)))
 
-(defn- validate-doc [{id :id schema-info :schema-info :as doc}]
+(defn- validate-doc [ignored-errors {id :id schema-info :schema-info :as doc}]
   (if (and (:name schema-info) (:version schema-info))
-    (let [results (filter (fn [{result :result}] (= :err (first result))) (model/validate doc))]
+    (let [ignored (set ignored-errors)
+          results (filter (fn [{result :result}] (and (= :err (first result)) (not (ignored (second result))))) (model/validate doc))]
       (when (seq results)
         {:document-id id :schema-info schema-info :results results}))
     {:document-id id :schema-info schema-info :results "Schema name or version missing"}))
 
-(defn- validate-documents [{id :id state :state documents :documents }]
-  (let [results (filter seq (map validate-doc documents))]
+(defn- validate-documents [ignored-errors {id :id state :state documents :documents}]
+  (let [results (filter seq (map (partial validate-doc ignored-errors) documents))]
     (when (seq results)
       {:id id
        :state state
        :results results})))
 
-(defn- documents-are-valid [applications]
-  (if-let [validation-results (seq (filter seq (map validate-documents applications)))]
+(defn- documents-are-valid [applications & ignored-errors]
+  (if-let [validation-results (seq (filter seq (map (partial validate-documents ignored-errors) applications)))]
     {:ok false :results validation-results}
     {:ok true}))
 
@@ -33,7 +34,7 @@
   (documents-are-valid @applications))
 
 (defmonster submitted-applications-documents-are-valid
-  (documents-are-valid @submitted-applications))
+  (documents-are-valid @submitted-applications "illegal-hetu"))
 
 ;; Documents have operation information
 
