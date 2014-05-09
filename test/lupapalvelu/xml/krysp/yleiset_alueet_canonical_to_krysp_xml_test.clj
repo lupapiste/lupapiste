af164e5093f6116574eaa5f76c4c7a86dee86c39
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-05-09 20:44:34 +0300

    Added XML test for IDs

--- test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
+++ test/lupapalvelu/xml/krysp/yleiset_alueet_canonical_to_krysp_xml_test.clj
@@ -14,7 +14,8 @@
             [lupapalvelu.document.canonical-common :refer [ya-operation-type-to-schema-name-key]]
             [midje.sweet :refer :all]
             [clojure.data.xml :refer :all]
-            [clojure.java.io :refer :all]))
+            [sade.xml :as xml]
+            [sade.common-reader :as cr]))
 
 (defn- do-test [application]
   (let [operation-name-key (-> application :operations first :name keyword)
@@ -23,8 +24,9 @@
         xml-212 (yleisetalueet-element-to-xml canonical lupa-name-key "2.1.2")
         xml-213 (yleisetalueet-element-to-xml canonical lupa-name-key "2.1.3")
         xml-212s (indent-str xml-212)
-        xml-213s (indent-str xml-213)]
-
+        xml-213s (indent-str xml-213)
+        lp-xml-212 (cr/strip-xml-namespaces (xml/parse xml-212s))
+        lp-xml-213 (cr/strip-xml-namespaces (xml/parse xml-213s))]
 
     (fact ":tag is set"
       (xml-test-common/has-tag
@@ -47,7 +49,17 @@
 
     (validator/validate xml-212s (:permitType application) "2.1.2")
     (validator/validate xml-213s (:permitType application) "2.1.3")
-   ))
+
+    (fact "LP-tunnus"
+      (xml/get-text lp-xml-212 [:MuuTunnus :tunnus]) => (:id application)
+      (xml/get-text lp-xml-213 [:MuuTunnus :sovellus]) => "Lupapiste")
+
+    (when (:linkPermitData application)
+      (fact "Link permit"
+        (let [muut-tunnukset (xml/select lp-xml-213 [:MuuTunnus])
+              link-permit (second muut-tunnukset)]
+          (xml/get-text link-permit [:tunnus]) => (-> application :linkPermitData first :id)
+          (xml/get-text link-permit [:sovellus]) => "Viitelupa")))))
 
 
 (facts "YA permits to canonical and then to xml with schema validation"
