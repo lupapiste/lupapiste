2cff3ecae09c17c395dd265b6cc9d8789759497e
Jari Asp <jari.asp@solita.fi>
2014-05-20 15:26:11 +0300

    Checking the application parameter differently in "validate-permit-type-is" and "validate-permit-type-is-not" validation functions. Plus added unit tests.

--- src/lupapalvelu/permit.clj
+++ src/lupapalvelu/permit.clj
@@ -102,23 +102,19 @@
 
 (defn validate-permit-type-is-not [validator-permit-type]
   (fn [_ application]
-    ;;
-    ;; TODO: Pitaisiko tassa heittaa fail, jos application on nil?
-    ;;
-    (when application
+    (if application
       (let [application-permit-type (permit-type application)]
         (when (= (keyword application-permit-type) (keyword validator-permit-type))
-          (fail :error.invalid-permit-type :permit-type validator-permit-type))))))
+          (fail :error.invalid-permit-type :permit-type validator-permit-type)))
+      (fail :error.invalid-application-parameter))))
 
 (defn validate-permit-type-is [validator-permit-type]
   (fn [_ application]
-    ;;
-    ;; TODO: Pitaisiko tassa heittaa fail, jos application on nil?
-    ;;
-    (when application
+    (if application
       (let [application-permit-type (permit-type application)]
         (when-not (= (keyword application-permit-type) (keyword validator-permit-type))
-          (fail :error.invalid-permit-type :permit-type validator-permit-type))))))
+          (fail :error.invalid-permit-type :permit-type validator-permit-type)))
+      (fail :error.invalid-application-parameter))))
 
 (defn is-valid-subtype [permitSubtype {permitType :permitType}]
   (when-not (some #(= permitSubtype %) (permit-subtypes permitType))
