535da7292db08225ba20b4c4d9fe6097139ccaf9
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-06-12 15:45:40 +0300

    naming

--- src/lupapalvelu/attachment.clj
+++ src/lupapalvelu/attachment.clj
@@ -396,42 +396,42 @@
 
 (defn attachment-file-ids
   "Gets all file-ids from attachment."
-  [application attachmentId]
-  (->> (get-attachment-info application attachmentId) :versions (map :fileId)))
+  [application attachment-id]
+  (->> (get-attachment-info application attachment-id) :versions (map :fileId)))
 
 (defn attachment-latest-file-id
   "Gets latest file-id from attachment."
-  [application attachmentId]
-  (last (attachment-file-ids application attachmentId)))
+  [application attachment-id]
+  (last (attachment-file-ids application attachment-id)))
 
 (defn file-id-in-application?
   "tests that file-id is referenced from application"
-  [application attachmentId file-id]
-  (let [file-ids (attachment-file-ids application attachmentId)]
+  [application attachment-id file-id]
+  (let [file-ids (attachment-file-ids application attachment-id)]
     (boolean (some #{file-id} file-ids))))
 
 (defn delete-attachment
   "Delete attachement with all it's versions. does not delete comments. Non-atomic operation: first deletes files, then updates document."
-  [{:keys [attachments] :as application} attachmentId]
-  (info "1/3 deleting files of attachment" attachmentId)
-  (dorun (map mongo/delete-file-by-id (attachment-file-ids application attachmentId)))
-  (info "2/3 deleted files of attachment" attachmentId)
-  (update-application (application->command application) {$pull {:attachments {:id attachmentId}}})
-  (info "3/3 deleted meta-data of attachment" attachmentId))
+  [{:keys [attachments] :as application} attachment-id]
+  (info "1/3 deleting files of attachment" attachment-id)
+  (dorun (map mongo/delete-file-by-id (attachment-file-ids application attachment-id)))
+  (info "2/3 deleted files of attachment" attachment-id)
+  (update-application (application->command application) {$pull {:attachments {:id attachment-id}}})
+  (info "3/3 deleted meta-data of attachment" attachment-id))
 
 (defn delete-attachment-version
   "Delete attachment version. Is not atomic: first deletes file, then removes application reference."
-  [{:keys [id attachments] :as application} attachmentId fileId]
-  (let [latest-version (latest-version-after-removing-file attachments attachmentId fileId)]
-    (infof "1/3 deleting file %s of attachment %s" fileId attachmentId)
+  [{:keys [id attachments] :as application} attachment-id fileId]
+  (let [latest-version (latest-version-after-removing-file attachments attachment-id fileId)]
+    (infof "1/3 deleting file %s of attachment %s" fileId attachment-id)
     (mongo/delete-file-by-id fileId)
-    (infof "2/3 deleted file %s of attachment %s" fileId attachmentId)
+    (infof "2/3 deleted file %s of attachment %s" fileId attachment-id)
     (update-application
       (application->command application)
-      {:attachments {$elemMatch {:id attachmentId}}}
+      {:attachments {$elemMatch {:id attachment-id}}}
       {$pull {:attachments.$.versions {:fileId fileId}}
        $set  {:attachments.$.latestVersion latest-version}})
-    (infof "3/3 deleted meta-data of file %s of attachment" fileId attachmentId)))
+    (infof "3/3 deleted meta-data of file %s of attachment" fileId attachment-id)))
 
 (defn get-attachment-as
   "Returns the attachment if user has access to application, otherwise nil."
