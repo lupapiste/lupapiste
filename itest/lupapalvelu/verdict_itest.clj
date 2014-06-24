5cf60d8a97185cf662705890932b709ed47e85f7
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-06-24 17:42:16 +0300

    Added comment and attachment tests

--- itest/lupapalvelu/verdict_itest.clj
+++ itest/lupapalvelu/verdict_itest.clj
@@ -4,43 +4,64 @@
             [lupapalvelu.factlet :refer :all]
             [lupapalvelu.domain :as domain]))
 
-(fact* "Authority is able to add an attachment to an application after verdict has been given for it"
-  (doseq [user [sonja pena]]
-    (last-email) ; Inbox zero
+(fact* "Give verdict"
+  (last-email) ; Inbox zero
 
-    (let [application-id  (create-app-id user :municipality sonja-muni :address "Paatoskuja 9")
-          resp            (command user :submit-application :id application-id) => ok?
-          application     (query-application user application-id)
-          email           (last-email) => truthy]
-      (:state application) => "submitted"
-      (:to email) => (email-for-key user)
-      (:subject email) => "Lupapiste.fi: Paatoskuja 9 - hakemuksen tila muuttunut"
-      (get-in email [:body :plain]) => (contains "Vireill\u00e4")
-      email => (partial contains-application-link? application-id)
+  (let [application-id  (create-app-id pena :municipality sonja-muni :address "Paatoskuja 9")
+        resp            (command pena :submit-application :id application-id) => ok?
+        application     (query-application pena application-id)
+        email           (last-email) => truthy]
+    (:state application) => "submitted"
+    (:to email) => (email-for-key pena)
+    (:subject email) => "Lupapiste.fi: Paatoskuja 9 - hakemuksen tila muuttunut"
+    (get-in email [:body :plain]) => (contains "Vireill\u00e4")
+    email => (partial contains-application-link? application-id)
 
-      (let [new-verdict-resp (command sonja :new-verdict-draft :id application-id) => ok?
-            verdict-id (:verdictId new-verdict-resp) => truthy
-            resp        (command sonja :save-verdict-draft :id application-id :verdictId verdict-id :backendId "aaa" :status 42 :name "Paatoksen antaja" :given 123 :official 124 :text "" :agreement false :section "") => ok?
-            application (query-application sonja application-id)
-            verdict     (first (:verdicts application))
-            paatos      (first (:paatokset verdict))
-            poytakirja  (first (:poytakirjat paatos))]
-        (count (:verdicts application)) => 1
-        (count (:paatokset verdict)) => 1
-        (count (:poytakirjat paatos)) => 1
+    (let [new-verdict-resp (command sonja :new-verdict-draft :id application-id) => ok?
+          verdict-id (:verdictId new-verdict-resp) => truthy
+          resp        (command sonja :save-verdict-draft :id application-id :verdictId verdict-id :backendId "aaa" :status 42 :name "Paatoksen antaja" :given 123 :official 124 :text "" :agreement false :section "") => ok?
+          application (query-application sonja application-id)
+          verdict     (first (:verdicts application))
+          paatos      (first (:paatokset verdict))
+          poytakirja  (first (:poytakirjat paatos))]
+      (count (:verdicts application)) => 1
+      (count (:paatokset verdict)) => 1
+      (count (:poytakirjat paatos)) => 1
 
-        (:kuntalupatunnus verdict) => "aaa"
-        (:status poytakirja) => 42
-        (:paatoksentekija poytakirja) => "Paatoksen antaja"
-        (get-in paatos [:paivamaarat :anto]) => 123
-        (get-in paatos [:paivamaarat :lainvoimainen]) => 124
+      (:kuntalupatunnus verdict) => "aaa"
+      (:status poytakirja) => 42
+      (:paatoksentekija poytakirja) => "Paatoksen antaja"
+      (get-in paatos [:paivamaarat :anto]) => 123
+      (get-in paatos [:paivamaarat :lainvoimainen]) => 124
 
-        (fact "publish verdict" (command sonja :publish-verdict :id application-id :verdictId verdict-id) => ok?)
+      (fact "Comment verdict"
+        (command sonja :add-comment :id application-id :text "hello" :to nil :target {:type "verdict" :id verdict-id} :openApplication false :roles [:authority]) => ok?
+        ;(fact "Nobody got mail" (last-email) => nil)
+        )
+
+      (fact "Upload attachment to draft"
+        (upload-attachment-to-target sonja application-id nil true verdict-id "verdict")
+        ;(fact "Nobody got mail" (last-email) => nil)
+        )
+
+      (fact "Pena does not see comment or attachment"
+        (let [{:keys [comments attachments]} (query-application pena application-id)]
+          (count comments) => 0
+          (count (keep :latestVersion attachments)) => 0))
+
+      (fact "Sonja sees comment and attachment"
+        (let [{:keys [comments attachments]} (query-application sonja application-id)]
+          (count comments) => 2 ; comment and new attachment auto-comment
+          (count (keep :latestVersion attachments)) => 1))
+
+      (fact "Publish verdict" (command sonja :publish-verdict :id application-id :verdictId verdict-id) => ok?)
+
+      (fact "Authority is still able to add an attachment"
         (let [application (query-application sonja application-id)
               first-attachment (get-in application [:attachments 0])]
 
           (let [email (last-email)]
-            (:to email) => (email-for-key user)
+            (:to email) => (email-for-key pena)
             (:subject email) => "Lupapiste.fi: Paatoskuja 9 - p\u00e4\u00e4t\u00f6s"
             email => (partial contains-application-link-with-tab? application-id "verdict"))
 
