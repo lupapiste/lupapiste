0022f9e9b27139ce86e291ad6db815d6f7ba55d9
Jari Asp <jari.asp@solita.fi>
2014-09-02 10:56:52 +0300

    Giving the "requiredFieldsFillingObligatory" meta data a False value as default in backend.

--- src/lupapalvelu/application_meta_fields.clj
+++ src/lupapalvelu/application_meta_fields.clj
@@ -97,7 +97,7 @@
 (defn- organization-meta [_ app]
   (let [org (organization/get-organization (:organization app))]
     {:name (organization/get-organization-name org)
-     :requiredFieldsFillingObligatory (:app-required-fields-filling-obligatory org)}))
+     :requiredFieldsFillingObligatory (or (:app-required-fields-filling-obligatory org) false)}))
 
 (defn- indicator-sum [_ app]
   (apply + (map (fn [[k v]] (if (#{:documentModifications :unseenStatements :unseenVerdicts} k) v 0)) app)))
