3b710ffda2281199077c66dc0f63040a07c4d582
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-09-03 16:38:52 +0300

    Configured known operation price classes

---
+++ test/lupapalvelu/exports_test.clj
@@ -0,0 +1,11 @@
+(ns lupapalvelu.exports-test
+  (:require [midje.sweet :refer :all]
+            [midje.util :refer [testable-privates]]
+            [clojure.set :refer [difference]]
+            [lupapalvelu.exports :refer [price-classes-for-operation]]
+            [lupapalvelu.operations :as ops]))
+
+(def keyset (comp set keys))
+
+(fact "Every operation has price class definition"
+  (difference (keyset ops/operations) (keyset price-classes-for-operation) ) => #{})
