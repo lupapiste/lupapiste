31b2a618a53b30f78d8da5b9561689a2a8a0d5b3
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-09-04 10:50:03 +0300

    Read price classes from xlsx

--- test/lupapalvelu/exports_test.clj
+++ test/lupapalvelu/exports_test.clj
@@ -2,10 +2,23 @@
   (:require [midje.sweet :refer :all]
             [midje.util :refer [testable-privates]]
             [clojure.set :refer [difference]]
-            [lupapalvelu.exports :refer [price-classes-for-operation]]
-            [lupapalvelu.operations :as ops]))
+            [lupapalvelu.exports :refer [kayttotarkoitus-hinnasto price-classes-for-operation]]
+            [lupapalvelu.application :as app]
+            [lupapalvelu.operations :as ops]
+            [lupapalvelu.document.schemas :as schemas]))
+
+(testable-privates lupapalvelu.exports resolve-price-class)
 
 (def keyset (comp set keys))
 
 (fact "Every operation has price class definition"
-  (difference (keyset ops/operations) (keyset price-classes-for-operation) ) => #{})
+  (difference (keyset ops/operations) (keyset price-classes-for-operation) ) => empty?)
+
+(fact "Every kayttotarkoitus has price class"
+  (let [every-kayttotarkoitus (remove (partial = "ei tiedossa") (map :name schemas/rakennuksen-kayttotarkoitus))]
+    (difference (set every-kayttotarkoitus) (keyset @kayttotarkoitus-hinnasto))) => empty?)
+
+(fact "Uusi kerrostalo"
+  (let [application (app/make-application "asuinrakennus" 0 0 "address" "01234567891234" "753" "753-R" false false [] {} 123)
+        op (resolve-price-class application (first (:operations application)))]
+    (:priceClass op) => "E"))
