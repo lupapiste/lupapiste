5c1f5c5ec1b8fc1442610e710f25686f06edf770
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-09-30 17:59:49 +0300

    Moved password reset js to own file

--- resources/private/login/login.js
+++ resources/private/login/login.js
@@ -47,25 +47,6 @@
     $("#login-message").text(loc(e.text)).css('display', 'block');
   });
 
-  function Reset() {
-    var self = this;
-    self.email = ko.observable();
-    self.ok = ko.computed(function() { var v = self.email(); return v && v.length > 0; });
-    self.sent = ko.observable(false);
-    self.fail = ko.observable();
-    self.send = function() {
-      var email = self.email();
-      if (!_.isBlank(email)) {
-        ajax
-          .postJson("/api/reset-password", {"email": email})
-          .raw(false)
-          .success(function() { self.sent(true).fail(null).email(""); })
-          .error(function(e) { self.sent(false).fail(e.text); $("#reset input:first").focus(); })
-          .call();
-      }
-    };
-  }
-
   function quality(pw) {
     if (!pw)            { return null;   }
     if (pw.length < 6)  { return "poor"; }
@@ -75,34 +56,6 @@
     return "excellent";
   }
 
-  function SetPW() {
-    var self = this;
-
-    self.token = ko.observable();
-    self.password1 = ko.observable();
-    self.password2 = ko.observable();
-    self.passwordQuality = ko.computed(function() { return quality(self.password1()); });
-    self.ok = ko.computed(function() {
-      var t = self.token(),
-          p1 = self.password1(),
-          p2 = self.password2();
-      return t && t.length && p1 && p1.length > 5 && p1 === p2;
-    });
-    self.success = ko.observable(false);
-    self.fail = ko.observable(false);
-
-    self.send = function() {
-      ajax
-        .post("/api/token/" + self.token())
-        .json({password: self.password1()})
-        .success(function() { self.success(true).fail(false).password1("").password2(""); })
-        .fail(function() { self.success(false).fail(true).password1("").password2(""); $("#setpw input:first").focus(); })
-        .call();
-    };
-
-    hub.onPageChange("setpw", function(e) { self.token(e.pagePath[0]); });
-
-  }
 
   function NewCompanyUser() {
     var self = this;
@@ -235,8 +188,6 @@
 
   $(function() {
     $("section#login").applyBindings({rememberMe: rememberMe});
-    $("section#reset").applyBindings(new Reset());
-    $("section#setpw").applyBindings(new SetPW());
     $("section#new-company-user").applyBindings(new NewCompanyUser());
     $("section#invite-company-user").applyBindings(new InviteCompanyUser());
     $("section#accept-company-invitation").applyBindings(new AcceptCompanyInvitation());
