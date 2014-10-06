a46e17fcd116d3268ef545005d487302f15260dc
Tommi Reinikainen <tommi.reinikainen@solita.fi>
2014-10-06 15:30:48 +0300

    remove person ids from sonja & ronja - authority users not supposed to have them

--- src/lupapalvelu/fixture/minimal.clj
+++ src/lupapalvelu/fixture/minimal.clj
@@ -138,7 +138,6 @@
     :organizations ["753-R" "753-YA" "998-R-TESTI-2"]
     :firstName "Sonja"
     :lastName "Sibbo"
-    :personId "120464-121C"
     :phone "03121991"
     :street "Katuosoite 1 a 1"
     :zip "33456"
@@ -154,7 +153,6 @@
     :organizations ["753-R"]
     :firstName "Ronja"
     :lastName "Sibbo"
-    :personId "120464-121C"
     :phone "03121991"
     :street "Katuosoite 1 a 1"
     :zip "33456"
