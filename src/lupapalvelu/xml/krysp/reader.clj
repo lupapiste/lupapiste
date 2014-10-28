402925835c7b17a93a9dfdb7d935f13f6485d080
Jari Asp <jari.asp@solita.fi>
2014-10-28 19:46:57 +0200

    - Added new function "get-app-info-from-message" to reader.clj, that reads all the needed app data from xml message.

--- src/lupapalvelu/xml/krysp/reader.clj
+++ src/lupapalvelu/xml/krysp/reader.clj
@@ -1,5 +1,5 @@
 (ns lupapalvelu.xml.krysp.reader
-  (:require [taoensso.timbre :as timbre :refer [debug warn]]
+  (:require [taoensso.timbre :as timbre :refer [debug warn error]]
             [clojure.string :as s]
             [clojure.walk :refer [postwalk prewalk]]
             [clj-time.format :as timeformat]
@@ -414,3 +414,59 @@
       {:buildings summary})))
 
 (permit/register-function permit/R :verdict-extras-krysp-reader buildings-summary-for-application)
+
+(defn get-app-info-from-message [xml ->function kuntalupatunnus]
+  (let [xml-no-ns (cr/strip-xml-namespaces xml)
+
+        toimituksenTiedot (cr/all-of xml-no-ns [:toimituksenTiedot])
+        asiat (select-asiat xml)
+        ;; Take first asia with given kuntalupatunnus. There should be only one. If there are many throw error.
+        asiat-with-kuntalupatunnus (filter (fn [a]
+                                             (println "(->kuntalupatunnus a): " (->kuntalupatunnus a) "\n")
+                                             (when (= kuntalupatunnus (->kuntalupatunnus a)) a)) asiat)
+        ]
+
+;    (println "\n get-app-info-from-message, asiat: ")
+;    (clojure.pprint/pprint asiat)
+;    (println "\n get-app-info-from-message, asiat-with-kuntalupatunnus count : " (count asiat-with-kuntalupatunnus))
+;    (println "\n get-app-info-from-message, asiat-with-kuntalupatunnus:")
+;    (clojure.pprint/pprint asiat-with-kuntalupatunnus)
+;    (println "\n")
+
+    (when (pos? (count asiat-with-kuntalupatunnus))
+      (when (> (count asiat-with-kuntalupatunnus) 1)
+        (error "Creating application from previous permit. There were more than one with kuntalupatunnus " kuntalupatunnus "."))
+
+      (let [asia (first asiat-with-kuntalupatunnus)   #_(select1 xml-no-ns case-elem-selector)
+
+            viitelupatiedot (map cr/all-of (select asia [:viitelupatieto :LupaTunnus]))
+            luvanTunnisteTiedot (cr/all-of asia [:luvanTunnisteTiedot :LupaTunnus])
+            kasittelynTilatiedot (->> (select xml-no-ns [:kasittelynTilatieto])
+                                   (map #(-> (cr/all-of % [:Tilamuutos]) (cr/convert-keys-to-timestamps [:pvm])))
+                                   (sort-by :pvm))
+            viimeisin-tila (last kasittelynTilatiedot)
+            asianTiedot (cr/all-of asia [:asianTiedot :Asiantiedot])
+;            verdicts (->verdicts xml ->function)
+            ]
+
+        (println "\n get-app-info-from-message, kuntalupatunnukset: " (map ->kuntalupatunnus asiat))
+
+        (println "\n get-app-info-from-message, toimituksenTiedot: ")
+        (clojure.pprint/pprint toimituksenTiedot)
+        (println "\n")
+
+
+        {:id (->lp-tunnus asia)
+         :kuntalupatunnus (->kuntalupatunnus asia)
+         :vahainenPoikkeaminen (:vahainenPoikkeaminen asianTiedot)
+         :rakennusvalvontaasianKuvaus (:rakennusvalvontaasianKuvaus asianTiedot)
+         :viitelupatiedot viitelupatiedot                ;; TODO: Mita nailla?
+         :kasittelynTilatiedot kasittelynTilatiedot
+         :viimeisin-tila viimeisin-tila
+;         :luvanTunnisteTiedot luvanTunnisteTiedot        ;; TODO: Poista?
+;         :paatokset verdicts                             ;; TODO: Haetaanko vaan myohemmin get-verdictsilla taman sijaan?
+         })
+      )
+
+    )
+  )
