a9dc7f2493bced3c91dc47e931bc6ee39c4aa30c
Joni Hamalainen <joni.hamalainen@solita.fi>
2014-11-03 13:29:24 +0200

    stamping v2 skeletons

--- resources/private/application/attachments-tab-model.js
+++ resources/private/application/attachments-tab-model.js
@@ -6,6 +6,7 @@ LUPAPISTE.AttachmentsTabModel = function(appModel) {
   self.appModel = appModel;
 
   self.stampModel = new LUPAPISTE.StampModel();
+  self.stampingMode = ko.observable(false);
 
   var postVerdictStates = {verdictGiven:true, constructionStarted:true, closed:true};
 
@@ -184,6 +185,19 @@ LUPAPISTE.AttachmentsTabModel = function(appModel) {
       }},
       {title: loc("no")});
   };
+
+  self.startStamping = function() {
+    self.stampingMode(true);
+    self.stampModel.init(self.appModel);
+  };
+
+  self.cancelStamping = function() {
+    self.stampingMode(false);
+  };
+
+  self.selectStampRow = function(data) {
+    console.log("stamp row:", data);
+  }
 
   self.attachmentTemplatesModel = new function() {
     var templateModel = this;
