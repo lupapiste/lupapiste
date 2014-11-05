11d23dacc95e9393a0e0adad641b80c6c853d898
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2014-11-05 12:36:01 +0200

    more refactorings, adds pdf-export to list of loaded web components

--- src/lupapalvelu/web.clj
+++ src/lupapalvelu/web.clj
@@ -27,7 +27,8 @@
             [lupapalvelu.proxy-services :as proxy-services]
             [lupapalvelu.organization]
             [lupapalvelu.application :as application]
-            [lupapalvelu.ke6666 :as ke6666]
+            [lupapalvelu.ke6666 :as ke6666]                 ;TODO remove
+            [lupapalvelu.pdf-export :as pdf-export]
             [lupapalvelu.mongo :as mongo]
             [lupapalvelu.token :as token]
             [lupapalvelu.activation :as activation]
