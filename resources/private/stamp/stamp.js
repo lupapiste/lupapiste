976cffee881b53cdf9406c4e9238dd5de1e4d59b
Joni Hamalainen <joni.hamalainen@solita.fi>
2014-11-12 13:33:40 +0200

    stamping mode works with refresh

--- resources/private/stamp/stamp.js
+++ resources/private/stamp/stamp.js
@@ -2,6 +2,8 @@ var stamping = (function() {
 
   var self = this;
 
+  var postVerdictStates = {verdictGiven:true, constructionStarted:true, closed:true}; // TODO make global var
+
   self.stampingMode = ko.observable(false);
   self.appModel = null;
   self.attachments = null;
@@ -25,7 +27,28 @@ var stamping = (function() {
   };
 
   hub.onPageChange('stamping', function(e) {
-    self.stampingMode(self.appModel !== null);
+    if ( !self.appModel ) {
+      if ( e.pagePath[0] ) {
+        var appId = e.pagePath[0];
+        repository.load(appId, null, function(application) {
+          self.appModel = new LUPAPISTE.ApplicationModel(authorization.create());
+          ko.mapping.fromJS(application, {}, self.appModel);
+
+          var attachments = _.filter(ko.mapping.toJS(self.appModel.attachments), function(attachment) {
+            return self.appModel.inPostVerdictState() ? postVerdictStates[attachment.applicationState] : !postVerdictStates[attachment.applicationState];
+          });
+
+          self.attachments = ko.observableArray(attachmentUtils.getGroupByOperation(attachments, true, self.appModel.allowedAttachmentTypes()));
+
+          self.stampingMode(self.appModel !== null); // show
+        });
+      } else {
+        error("No application ID provided for stamping");
+        LUPAPISTE.ModalDialog.open("#dialog-application-load-error");
+      }
+    } else { // appModel already initialized, show stamping
+      self.stampingMode(true);
+    }
   });
 
   hub.subscribe('start-stamping', function(param) {
