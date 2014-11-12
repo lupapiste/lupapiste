38ac34832896ba0ec084cdcf91f9eab7f12ef1f2
Jari Asp <jari.asp@solita.fi>
2014-11-12 15:56:21 +0200

    requiring sade.core instead of lupapalvelu.core

--- src/lupapalvelu/xml/krysp/application_from_krysp.clj
+++ src/lupapalvelu/xml/krysp/application_from_krysp.clj
@@ -2,7 +2,7 @@
   (:require [taoensso.timbre :as timbre :refer [trace debug debugf info infof warn error fatal]]
             [lupapalvelu.organization :as organization]
             [lupapalvelu.permit :as permit]
-            [lupapalvelu.core :refer [fail!]]))
+            [sade.core :refer [fail!]]))
 
 (defn get-application-xml [{:keys [id permitType] :as application} & [raw? kuntalupatunnus?]]
   (if-let [{url :url} (organization/get-krysp-wfs application)]
