b5f2ba89bec0f51b71b02e4a060d4e4f1f35ab6a
Eetu Sutinen <eetu.sutinen@solita.fi>
2014-11-17 13:06:57 +0200

    missing from last commit

--- src/lupapalvelu/web.clj
+++ src/lupapalvelu/web.clj
@@ -429,7 +429,7 @@
         result (execute-command "upload-attachment" upload-data request)]
     (if (core/ok? result)
       (resp/redirect "/html/pages/upload-ok.html")
-      (resp/redirect (str (hiccup.util/url "/html/pages/upload-1.47.html"
+      (resp/redirect (str (hiccup.util/url "/html/pages/upload-1.52.html"
                                         (-> (:params request)
                                           (dissoc :upload)
                                           (dissoc ring.middleware.anti-forgery/token-key)
