e2287df5890da995a9854560677a7432d3b1b81c
Jari Asp <jari.asp@solita.fi>
2014-11-19 18:09:09 +0200

    moe itests done

--- itest/lupapalvelu/application_from_prev_permit_itest.clj
+++ itest/lupapalvelu/application_from_prev_permit_itest.clj
@@ -1,11 +1,15 @@
 (ns lupapalvelu.application-itest
   (:require [midje.sweet :refer :all]
             [clojure.java.io :as io]
+            [sade.core :refer [def-]]
             [sade.xml :as xml]
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.factlet  :refer :all]
             [lupapalvelu.domain :as domain]
-            [lupapalvelu.xml.krysp.application-from-krysp :as krysp-fetch-api]))
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.organization :as organization]
+            [lupapalvelu.xml.krysp.application-from-krysp :as krysp-fetch-api]
+            [lupapalvelu.xml.krysp.reader :as krysp-reader]))
 
 (apply-remote-minimal)
 
@@ -36,12 +40,14 @@
 
 ; (provided (cr/get-xml anything anything anything) => (xml/parse (slurp (clojure.java.io/resource "mml/yhteystiedot-KP.xml"))))
 
+(def- example-kuntalupatunnus "14-0241-R 3")
+
 (defn- create-app-from-prev-permit [apikey & args]
   (let [args (->> args
                (apply hash-map)
                (merge {:operation "aiemmalla-luvalla-hakeminen"
                        :municipality "186"  ;; Jarvenpaa
-                       :kuntalupatunnus "14-0241-R 3"
+                       :kuntalupatunnus example-kuntalupatunnus
                        :y 0
                        :x 0
                        :address ""
@@ -51,10 +57,14 @@
 
 (fact* "Successfully creating new application based on a prev permit"
 
-  (let [example-xml (xml/parse (slurp (io/resource "../resources/krysp/sample/verdict-rakval-from-kuntalupatunnus-query.xml")))]
+  (let [example-xml (xml/parse (slurp (io/resource "../resources/krysp/sample/verdict-rakval-from-kuntalupatunnus-query.xml")))
+        example-app-info (krysp-reader/get-app-info-from-message example-xml example-kuntalupatunnus)]
 ;    (println "\n example-xml: ")
 ;    (clojure.pprint/pprint example-xml)
 ;    (println "\n")
+    (println "\n example-app-info: ")
+    (clojure.pprint/pprint example-app-info)
+    (println "\n")
 
 
   (fact "missing parameters"
@@ -62,14 +72,13 @@
     (create-app-from-prev-permit pena :operation "") => (partial expected-failure? "error.missing-parameters"))
 
 
-  ; 1: Kannassa on app, jonka organization ja verdictin kuntalupatunnus matchaa
+  ; 1: Kannassa on app, jonka organization ja verdictin kuntalupatunnus matchaa haettuihin
   (facts "db has app that has the kuntalupatunnus in its verdict and its organization matches"
     ; 1 a: avaa hakemus, jos on oikat -> (ok :id lupapiste-tunnus)
     (fact "has rights to the found app"
       (create-app-from-prev-permit pena) => (contains {:ok true
                                                        :id "lupis-id"})
       (provided
-        (domain/get-application anything) => {:id "lupis-id"}
         (domain/owner-or-writer? anything anything) => true))
     ; 1 b: jos ei oikkia -> (fail :error.lupapiste-application-already-exists-but-unauthorized-to-access-it :id (:id app-with-verdict))
     (fact "does not have rights to the found app"
@@ -77,33 +86,66 @@
                                                        :id "lupis-id"
                                                        :text "error.lupapiste-application-already-exists-but-unauthorized-to-access-it"})
       (provided
-        (domain/get-application anything) => {:id "lupis-id"}
-        (domain/owner-or-writer? anything anything) => false)))
-
-  ; Muuten, jos kannassa ei ole appista...
+        (domain/owner-or-writer? anything anything) => false))
+    ;; patee molemmille yllaoleville
+    (against-background
+      (domain/get-application anything) => {:id "lupis-id"}))
 
   ; 2: jos taustajarjestelmasta ei saada xml-sisaltoa -> (fail :error.no-previous-permit-found-from-backend)
-;  (fact "no xml content received from backend with the kuntalupatunnus"
-;    (create-app-from-prev-permit pena) => (partial expected-failure? "error.no-previous-permit-found-from-backend")
-;    (provided (krysp-fetch-api/get-application-xml anything false true) => nil))
+  (fact "no xml content received from backend with the kuntalupatunnus"
+    (create-app-from-prev-permit pena) => (partial expected-failure? "error.no-previous-permit-found-from-backend")
+    (provided
+      (krysp-fetch-api/get-application-xml anything false true) => nil))
 
   ; 3: jos (krysp-reader/get-app-info-from-message xml kuntalupatunnus) palauttaa nillin -> (fail :error.no-previous-permit-found-from-backend)
+  (fact "no application info could be parsed"
+    (create-app-from-prev-permit pena) => (partial expected-failure? "error.no-previous-permit-found-from-backend")
+    (provided
+      (krysp-reader/get-app-info-from-message anything anything) => nil))
 
   ; 4: jos parametrina annettu organisaatio ja app-infosta ratkaistu organisaatio ei matchaa -> (fail :error.previous-permit-found-from-backend-is-of-different-organization)
+  (fact "ids of the given and resolved organizations do not match"
+    (create-app-from-prev-permit pena) => (partial expected-failure? "error.previous-permit-found-from-backend-is-of-different-organization")
+    (provided
+      (krysp-reader/get-app-info-from-message anything anything) => {:municipality "753"}))
 
   ; 5: jos sanomassa ei ollut rakennuspaikkaa, ja ei alunperin annettu tarpeeksi parametreja -> (fail :error.more-prev-app-info-needed :needMorePrevPermitInfo true)
+  (fact "no 'rakennuspaikkatieto' element in the received xml, need more info"
+    (create-app-from-prev-permit pena) => (contains {:ok false
+                                                     :needMorePrevPermitInfo true
+                                                     :text "error.more-prev-app-info-needed"})
+    (provided
+      (krysp-reader/get-app-info-from-message anything anything) => (dissoc example-app-info :rakennuspaikka)))
 
   ; 6) sanomassa tulee lupapiste-id
-;  (facts "message includes lupapiste id"
-;    ; 6 a: jota ei kuitenkaan ole jarjestelmassa -> (fail :error.not-able-to-open-with-lupapiste-id-that-previous-permit-included :id lupapiste-tunnus)
-;    (fact "the lupapiste id is not found from database though"
-;      (create-app-from-prev-permit pena) => (contains {:ok false
-;                                                       :id "LP-186-2014-00290"
-;                                                       :text "error.not-able-to-open-with-lupapiste-id-that-previous-permit-included"}))
-;    ; 6 b: on jarjestelmassa, mutta kayttajalla ei oikkia sille -> (fail :lupapiste-application-already-exists-but-unauthorized-to-access-it :id lupapiste-tunnus)
-;    ; 6 c: on jarjestelmassa, ja kayttajalla on oikat sille     -> (ok :id lupapiste-tunnus)
+  (facts "message includes lupapiste id"
+    ; 6 a: jota ei kuitenkaan ole jarjestelmassa -> (fail :error.not-able-to-open-with-lupapiste-id-that-previous-permit-included :id lupapiste-tunnus)
+    (fact "the lupapiste id is not found from database though"
+      (create-app-from-prev-permit pena) => (contains {:ok false
+                                                       :id "valid-lupapiste-id"
+                                                       :text "error.not-able-to-open-with-lupapiste-id-that-previous-permit-included"}))
+    ;; TODO
+;    (facts "app id found in the database"
+;      ; 6 b: on jarjestelmassa, mutta kayttajalla ei oikkia sille -> (fail :error.lupapiste-application-already-exists-but-unauthorized-to-access-it :id lupapiste-tunnus)
+;      (fact "we do not have permissions to application"
+;        (create-app-from-prev-permit pena) => (contains {:ok false
+;                                                         :id "valid-lupapiste-id"
+;                                                         :text "error.lupapiste-application-already-exists-but-unauthorized-to-access-it"})
+;        (provided
+;          (domain/owner-or-writer? anything anything) => false))
+;      ; 6 c: on jarjestelmassa, ja kayttajalla on oikat sille     -> (ok :id lupapiste-tunnus)
+;      (fact "got the permissions"
+;        (create-app-from-prev-permit pena) => (contains {:ok true
+;                                                         :id "valid-lupapiste-id"})
+;        (provided
+;          (domain/owner-or-writer? anything anything) => true))
 ;
-;    )
+;      (against-background
+;        (mongo/by-id :applications anything) => {:id "valid-lupapiste-id"} :times 1))
+
+    (against-background
+      (krysp-reader/get-app-info-from-message anything anything) => (assoc example-app-info :id "valid-lupapiste-id")))
+
 
   ; 7: do-create-application heittaa jonkin poikkeuksen -> (fail :error.no-previous-permit-found-from-backend)
 
