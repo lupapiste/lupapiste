78a89dc3b73e80fda9502a5f78dbd2016fe8e8d3
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-11-26 12:40:21 +0200

    Added valtakunnallinenNumero to test data

--- test/lupapalvelu/xml/krysp/reader_test.clj
+++ test/lupapalvelu/xml/krysp/reader_test.clj
@@ -349,7 +349,8 @@
     (count buildings) => 1
     (:jarjestysnumero building1) => "31216"
     (:kiinttun building1) => "63820130310000"
-    (:rakennusnro building1) => "123"))
+    (:rakennusnro building1) => "123"
+    (:valtakunnallinenNumero building1) => "123456789A"))
 
 (facts "wfs-krysp-url works correctly"
   (fact "without ? returns url with ?" (wfs-krysp-url "http://localhost" rakval-case-type (property-equals "test" "lp-1")) =>  "http://localhost?request=GetFeature&typeName=rakval%3ARakennusvalvontaAsia&filter=%3CPropertyIsEqualTo%3E%3CPropertyName%3Etest%3C%2FPropertyName%3E%3CLiteral%3Elp-1%3C%2FLiteral%3E%3C%2FPropertyIsEqualTo%3E")
