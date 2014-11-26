42078580465bc608de3ecdeed32cfe1c47fcf747
Eetu Sutinen <eetu.sutinen@solita.fi>
2014-11-26 16:54:08 +0200

    show adding foreman modal dialog

--- resources/private/application/foreman-model.js
+++ resources/private/application/foreman-model.js
@@ -2,16 +2,36 @@ LUPAPISTE.ForemanModel = function() {
   "use strict";
   var self = this;
   self.application = null;
+  self.email = ko.observable();
+  self.error = ko.observable();
+  self.processing = ko.observable();
+  self.pending = ko.observable();
+  self.disabled = ko.computed(function() {
+    return self.email() && !util.isValidEmailAddress(self.email());
+  });
 
   self.refresh = function(application) {
     self.application = application;
   }
 
   self.inviteForeman = function() {
-    console.log("invite");
+    LUPAPISTE.ModalDialog.open('#dialog-invite-foreman')
   }
 
   self.submit = function(model) {
-    console.log("submit");
+    self.error(undefined);
+    // TODO
+    // 1. invite foreman to current application (new role-parameter to invite command)
+    // 2. create "työnjohtajan ilmoitus" application (new link-parameter to create command)
+    // 3. invite foreman to new application
+    // 4. open new application
+    self.processing(true);
+    self.pending(true);
+    setTimeout(function() {
+      console.log("foo");
+      self.processing(false);
+      self.pending(false);
+      LUPAPISTE.ModalDialog.close();
+    }, 2500)
   }
 }
