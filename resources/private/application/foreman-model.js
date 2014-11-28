6b865a69202fca0ac750b7cd567ca000c74ea258
Eetu Sutinen <eetu.sutinen@solita.fi>
2014-11-28 16:42:29 +0200

    invite foreman from UI

--- resources/private/application/foreman-model.js
+++ resources/private/application/foreman-model.js
@@ -20,17 +20,78 @@ LUPAPISTE.ForemanModel = function() {
 
   self.submit = function(model) {
     self.error(undefined);
-    // TODO
+
+    function inviteToApplication(id, cb, errorCb) {
+      if (!errorCb) {
+        errorCb = cb;
+      }
+
+      ajax.command("invite", { id: id,
+                               documentName: "",
+                               documentId: "",
+                               path: "",
+                               email: self.email(),
+                               title: "",
+                               text: "" })
+        .processing(self.processing)
+        .pending(self.pending)
+        .success(function(data) {
+          cb(data);
+        })
+        .error(function(err) {
+          errorCb(err);
+        })
+        .call();
+    }
+
+    function linkToApplication(id) {
+      // 3. Link new application to current
+      ajax.command("add-link-permit", { id: self.application.id,
+                                        linkPermitId: id,
+                                        propertyId: self.application.propertyId })
+        .processing(self.processing)
+        .pending(self.pending)
+        .success(function() {
+          LUPAPISTE.ModalDialog.close();
+          // 4. open new application
+          repository.load(id);
+          window.location.hash = "!/application/" + id;
+        })
+        .error(function(err) {
+          self.error(loc(err.text))
+        })
+        .call();
+      return false;
+    }
+
+    function createApplication() {
+      // 2. create "työnjohtajan ilmoitus" application
+      ajax.command("create-application", { infoRequest: false,
+                                           operation: "tyonjohtajan-nimeaminen",
+                                           y: self.application.location.y,
+                                           x: self.application.location.x,
+                                           address: self.application.address,
+                                           propertyId: self.application.propertyId,
+                                           messages: [],
+                                           municipality: self.application.municipality })
+        .processing(self.processing)
+        .pending(self.pending)
+        .success(function(data) {
+          // 3. invite foreman to new application
+          inviteToApplication(data.id, function() {
+            linkToApplication(data.id);
+          }, function(err) {
+            self.error(loc(err.text));
+          });
+        })
+        .error(function(err) {
+          self.error(loc(err.text));
+        })
+        .call();
+    }
     // 1. invite foreman to current application (new role-parameter to invite command)
-    // 2. create "työnjohtajan ilmoitus" application (new link-parameter to create command)
-    // 3. invite foreman to new application
-    // 4. open new application
-    self.processing(true);
-    self.pending(true);
-    setTimeout(function() {
-      self.processing(false);
-      self.pending(false);
-      LUPAPISTE.ModalDialog.close();
-    }, 2500)
+    inviteToApplication(self.application.id, createApplication, createApplication)
+    return false;
   }
+
 }
