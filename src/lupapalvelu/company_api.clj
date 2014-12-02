52fa65be526ab143a1815e5012395a0e7b426cf3
Juha Jokimaki <juha.jokimaki@solita.fi>
2014-12-02 20:16:55 +0200

    Restricted access

--- src/lupapalvelu/company_api.clj
+++ src/lupapalvelu/company_api.clj
@@ -25,7 +25,7 @@
 ;;
 
 (defquery company
-  {:roles [:anonymous]
+  {:roles [:applicant :authority]
    :input-validators [validate-user-is-admin-or-company-member]
    :parameters [company]}
   [{{:keys [users]} :data}]
@@ -33,18 +33,18 @@
       :users   (and users (c/find-company-users company))))
 
 (defquery companies
-  {:roles [:anonymous]}
+  {:roles [:applicant :authority :admin]}
   [_]
   (ok :companies (c/find-companies)))
 
 (defcommand company-update
-  {:roles [:anonymous]
+  {:roles [:applicant]
    :input-validators [validate-user-is-admin-or-company-member]
    :parameters [company updates]}
   (ok :company (c/update-company! company updates)))
 
 (defcommand company-user-update
-  {:roles [:anonymous]
+  {:roles [:applicant :admin]
    :parameters [user-id op value]}
   [{caller :user}]
   (let [target-user (u/get-user-by-id! user-id)]
@@ -58,7 +58,7 @@
     (ok)))
 
 (defquery company-invite-user
-  {:roles [:anonymous]
+  {:roles [:applicant]
    :input-validators [validate-user-is-admin-or-company-admin]
    :parameters [email]}
   [{caller :user}]
@@ -76,7 +76,7 @@
         (ok :result :invited)))))
 
 (defcommand company-add-user
-  {:roles [:anonymous]
+  {:roles [:applicant]
    :parameters [firstName lastName email]}
   [{user :user {:keys [admin]} :params}]
   (if-not (or (= (:role user) "admin")
