98fb06c122dd759769eba29ee3b462377aac8f8b
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2014-12-05 10:40:38 +0200

    fix rest of tests

--- itest/lupapalvelu/autom_check_verdicts_itest.clj
+++ itest/lupapalvelu/autom_check_verdicts_itest.clj
@@ -16,14 +16,12 @@
       organizations (map (fn [org] (update-in org [:krysp] #(assoc-in % [:R :url] krysp-url))) minimal/organizations)]
   (dorun (map (partial mongo/insert :organizations) organizations)))
 
-(facts* "Automatic checking for verdicts"
-
-
- (let [application-submitted         (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 17") => truthy
+(facts "Automatic checking for verdicts"
+ (let [application-submitted         (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 17")
        application-id-submitted      (:id application-submitted)
-       application-sent              (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 18") => truthy
+       application-sent              (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 18")
        application-id-sent           (:id application-sent)
-       application-verdict-given     (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 19") => truthy
+       application-verdict-given     (create-and-submit-local-application sonja :municipality sonja-muni :address "Paatoskuja 19")
        application-id-verdict-given  (:id application-verdict-given)]
 
    (local-command sonja :approve-application :id application-id-sent :lang "fi") => ok?
