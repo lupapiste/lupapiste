2a6022e2bbb118de8819504e57906a02c99d41db
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-01-07 16:54:00 +0200

    skeleton for asianhallinta canonical

---
+++ src/lupapalvelu/document/asianhallinta_canonical.clj
@@ -0,0 +1,21 @@
+(ns lupapalvelu.document.asianhallinta_canonical
+  (require [lupapalvelu.document.canonical-common :refer :all]
+           [lupapalvelu.document.tools :as tools]
+           [clojure.string :as s]))
+
+
+;; Uusi asia element
+
+(def uusi-asia {:UusiAsia
+                {:Tyyppi nil
+                 :Kuvaus nil
+                 :Kuntanumero nil
+                 :Hakijat nil
+                 :Maksaja nil
+                 :HakemusTunnus nil
+                 :VireilletuloPvm
+                 :Liitteet
+                 :Asiointikieli
+                 :Toimenpiteet
+                 :Viiteluvat}})
+
