fb2c9fd4ec9547112194cc5cd5b4da4699424b3f
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-01-08 12:01:48 +0200

    canonical test file

---
+++ test/lupapalvelu/document/asianhallinta_canonical_test.clj
@@ -0,0 +1,10 @@
+(ns lupapalvelu.document.asianhallinta_canonical_test
+  (:require [lupapalvelu.factlet :as fl]
+            [lupapalvelu.document.canonical-test-common :as ctc]
+            [lupapalvelu.document.tools :as tools]
+            [midje.sweet :refer :all]
+            [midje.util :refer [testable-privates]]
+            [lupapalvelu.document.poikkeamis-canonical-test :as poikkeus-test]))
+
+
+
