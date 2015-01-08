0e84bda85cefd1c87856d2361c67dca2c100e825
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-01-08 18:03:13 +0200

    TDD for asianhallinta canonical

--- test/lupapalvelu/document/asianhallinta_canonical_test.clj
+++ test/lupapalvelu/document/asianhallinta_canonical_test.clj
@@ -1,10 +1,12 @@
 (ns lupapalvelu.document.asianhallinta_canonical_test
   (:require [lupapalvelu.factlet :as fl]
+            [lupapalvelu.document.asianhallinta_canonical :as ah]
             [lupapalvelu.document.canonical-test-common :as ctc]
             [lupapalvelu.document.tools :as tools]
             [midje.sweet :refer :all]
             [midje.util :refer [testable-privates]]
             [lupapalvelu.document.poikkeamis-canonical-test :as poikkeus-test]))
 
-
+(fl/facts* "UusiAsia canonical"
+  (let [canonical (ah/application-to-asianhallinta-canonical poikkeus-test/poikkari-hakemus "fi") => truthy]))
 
