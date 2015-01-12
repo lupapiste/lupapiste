111af57bf1e33d788873b126cd500e9a56827d55
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-01-12 14:35:08 +0200

    yhteyshenkilo and yhteystiedot types

--- src/lupapalvelu/xml/asianhallinta/mapping_common.clj
+++ src/lupapalvelu/xml/asianhallinta/mapping_common.clj
@@ -2,3 +2,15 @@
   (:require [lupapalvelu.document.asianhallinta_canonical :as ahc]
             [sade.util :as util]))
 
+(def yhteystiedot-type
+  [{:tag :Jakeluosoite}
+   {:tag :Postinumero}
+   {:tag :Postitoimipaikka}
+   {:tag :Maa}
+   {:tag :Email}
+   {:tag :Puhelinnumero}])
+
+(def yhteyshenkilo-type
+  [{:tag :Etunimi}
+   {:tag :Sukunimi}
+   {:tag :Yhteystiedot :child [yhteystiedot-type]}])
