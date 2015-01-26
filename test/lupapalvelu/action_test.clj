7d9da89f853af47dd9322edd3dd994fc807e4ef9
Jari Asp <jari.asp@solita.fi>
2015-01-26 14:40:58 +0200

    Fixed expected error key from the validator "vector-parameters-with-non-blank-items"

--- test/lupapalvelu/action_test.clj
+++ test/lupapalvelu/action_test.clj
@@ -233,9 +233,9 @@
 
 (facts "vector-parameters-with-non-blank-items"
   (vector-parameters-with-non-blank-items [:foo] {:data {:foo ["aa"]}})     => nil
-  (vector-parameters-with-non-blank-items [:foo] {:data {:foo ["aa" nil]}}) => {:ok false :text "error.vector-with-blank-parameters" :parameters [:foo]}
-  (vector-parameters-with-non-blank-items [:foo] {:data {:foo ["aa" ""]}})  => {:ok false :text "error.vector-with-blank-parameters" :parameters [:foo]}
-  (vector-parameters-with-non-blank-items [:foo :bar] {:data {:foo [nil] :bar [" "]}}) => {:ok false :text "error.vector-with-blank-parameters" :parameters [:foo :bar]}
+  (vector-parameters-with-non-blank-items [:foo] {:data {:foo ["aa" nil]}}) => {:ok false :text "error.vector-parameters-with-blank-items" :parameters [:foo]}
+  (vector-parameters-with-non-blank-items [:foo] {:data {:foo ["aa" ""]}})  => {:ok false :text "error.vector-parameters-with-blank-items" :parameters [:foo]}
+  (vector-parameters-with-non-blank-items [:foo :bar] {:data {:foo [nil] :bar [" "]}}) => {:ok false :text "error.vector-parameters-with-blank-items" :parameters [:foo :bar]}
   (vector-parameters-with-non-blank-items [:foo :bar] {:data {:foo nil :bar " "}})     => {:ok false :text "error.non-vector-parameters"        :parameters [:foo :bar]})
 
 (facts "feature requirements"
