49965a21a30637b203cad7d9de7c1b7052edbf48
Jari Asp <jari.asp@solita.fi>
2015-01-27 15:28:04 +0200

    using kebab-case in names of function parameters

--- src/lupapalvelu/attachment.clj
+++ src/lupapalvelu/attachment.clj
@@ -289,16 +289,16 @@
   {:pre [application]}
   (get-attachment-types-by-permit-type (:permitType application)))
 
-(defn make-attachment [now target required? requestedByAuthority? locked? applicationState op attachment-type & [attachment-id]]
+(defn make-attachment [now target required? requested-by-authority? locked? application-state op attachment-type & [attachment-id]]
   {:id (or attachment-id (mongo/create-id))
    :type attachment-type
    :modified now
    :locked locked?
-   :applicationState applicationState
+   :applicationState application-state
    :state :requires_user_action
    :target target
    :required required?       ;; true if the attachment is added from from template along with the operation, or when attachment is requested by authority
-   :requestedByAuthority requestedByAuthority?  ;; true when authority is adding a new attachment template by hand
+   :requestedByAuthority requested-by-authority?  ;; true when authority is adding a new attachment template by hand
    :notNeeded false
    :forPrinting false
    :op op
@@ -307,12 +307,12 @@
 
 (defn make-attachments
   "creates attachments with nil target"
-  [now applicationState attachment-types locked? required? requestedByAuthority?]
-  (map (partial make-attachment now nil required? requestedByAuthority? locked? applicationState nil) attachment-types))
+  [now application-state attachment-types locked? required? requested-by-authority?]
+  (map (partial make-attachment now nil required? requested-by-authority? locked? application-state nil) attachment-types))
 
-(defn create-attachment [application attachment-type op now target locked? required? requestedByAuthority? & [attachment-id]]
+(defn create-attachment [application attachment-type op now target locked? required? requested-by-authority? & [attachment-id]]
   {:pre [(map? application)]}
-  (let [attachment (make-attachment now target required? requestedByAuthority? locked? (:state application) op attachment-type attachment-id)]
+  (let [attachment (make-attachment now target required? requested-by-authority? locked? (:state application) op attachment-type attachment-id)]
     (update-application
       (application->command application)
       {$set {:modified now}
@@ -320,9 +320,9 @@
 
     (:id attachment)))
 
-(defn create-attachments [application attachment-types now locked? required? requestedByAuthority?]
+(defn create-attachments [application attachment-types now locked? required? requested-by-authority?]
   {:pre [(map? application)]}
-  (let [attachments (make-attachments now (:state application) attachment-types locked? required? requestedByAuthority?)]
+  (let [attachments (make-attachments now (:state application) attachment-types locked? required? requested-by-authority?)]
     (update-application
       (application->command application)
       {$set {:modified now}
@@ -444,11 +444,11 @@
    Otherwise a new attachment is created."
   [{:keys [application attachment-id attachment-type op file-id filename content-type size comment-text created user target locked required] :as options}]
   {:pre [(map? application)]}
-  (let [requestedByAuthority? (and (ss/blank? attachment-id) (user/authority? (:user options)))
+  (let [requested-by-authority? (and (ss/blank? attachment-id) (user/authority? (:user options)))
         att-id (cond
-                 (ss/blank? attachment-id) (create-attachment application attachment-type op created target locked required requestedByAuthority?)
+                 (ss/blank? attachment-id) (create-attachment application attachment-type op created target locked required requested-by-authority?)
                  (pos? (mongo/count :applications {:_id (:id application) :attachments.id attachment-id})) attachment-id
-                 :else (create-attachment application attachment-type op created target locked required requestedByAuthority? attachment-id))]
+                 :else (create-attachment application attachment-type op created target locked required requested-by-authority? attachment-id))]
     (set-attachment-version (assoc options :attachment-id att-id :now created :stamped false))))
 
 (defn parse-attachment-type [attachment-type]
