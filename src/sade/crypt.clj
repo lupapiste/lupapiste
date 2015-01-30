f86041df37bb5d33dc2b24489a41cbb9c0720b36
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-01-30 14:55:58 +0200

    move onnistuu crypt to sade/crypt.clj

---
+++ src/sade/crypt.clj
@@ -0,0 +1,44 @@
+(ns lupapalvelu.onnistuu.crypt
+  (:require [crypto.random :as cr])
+  (:import [org.bouncycastle.crypto BufferedBlockCipher]
+           [org.bouncycastle.crypto.engines RijndaelEngine]
+           [org.bouncycastle.crypto.modes CBCBlockCipher]
+           [org.bouncycastle.crypto.paddings PaddedBufferedBlockCipher ZeroBytePadding]
+           [org.bouncycastle.crypto.params KeyParameter ParametersWithIV]
+           [org.apache.commons.codec.binary Base64]))
+
+(set! *warn-on-reflection* true)
+
+(defn make-iv []
+  (cr/bytes 32))
+
+(defn- cipher [encrypt? crypto-key crypto-iv]
+  (doto (-> (RijndaelEngine. 256)
+            (CBCBlockCipher.)
+            (PaddedBufferedBlockCipher. (ZeroBytePadding.)))
+    (.init encrypt? (ParametersWithIV. (KeyParameter. (into-array Byte/TYPE crypto-key))
+                                       (into-array Byte/TYPE crypto-iv)))))
+
+(defn- crypt ^bytes [^BufferedBlockCipher cipher ^bytes data]
+  (let [in-size  (alength data)
+        out-size (.getOutputSize cipher in-size)
+        out      (byte-array out-size)
+        out-len  (.processBytes cipher data 0 in-size out 0)
+        out-len  (+ out-len (.doFinal cipher out out-len))]
+    (if (< out-len out-size)
+      (byte-array out-len out)
+      out)))
+
+(defn encrypt [crypto-key crypto-iv data]
+  (crypt (cipher true crypto-key crypto-iv) data))
+
+(defn decrypt [crypto-key crypto-iv data]
+  (crypt (cipher false crypto-key crypto-iv) data))
+
+(defn str->bytes ^bytes [^String s] (.getBytes s "UTF-8"))
+(defn bytes->str ^String [^bytes b] (String. b "UTF-8"))
+
+(defn base64-encode [^bytes data] (Base64/encodeBase64 data))
+(defn base64-decode [^bytes data] (Base64/decodeBase64 data))
+
+(defn url-encode [^String s] (java.net.URLEncoder/encode s "UTF-8"))
