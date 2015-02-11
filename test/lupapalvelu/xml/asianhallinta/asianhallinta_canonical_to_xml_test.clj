ff26f2f7e1c739a01f3af5796e0247668d4c5f04
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-02-11 15:49:09 +0200

    refactor let

--- test/lupapalvelu/xml/asianhallinta/asianhallinta_canonical_to_xml_test.clj
+++ test/lupapalvelu/xml/asianhallinta/asianhallinta_canonical_to_xml_test.clj
@@ -7,19 +7,23 @@
             [lupapalvelu.document.poikkeamis-canonical-test :as poikkeus-test]
             [lupapalvelu.xml.emit :refer [element-to-xml]]
             [lupapalvelu.xml.validator :as validator]
-            [clojure.data.xml :as xml]))
+            [clojure.data.xml :as xml]
+            [sade.common-reader :as reader]))
 
 (fact ":tag is set for UusiAsia" (has-tag ua-mapping/uusi-asia) => true)
 
 (fl/facts* "UusiAsia xml from poikkeus"
-  (let [canonical (ah/application-to-asianhallinta-canonical poikkeus-test/poikkari-hakemus "fi") => truthy
+  (let [application poikkeus-test/poikkari-hakemus
+        canonical (ah/application-to-asianhallinta-canonical application "fi") => truthy
         xml (element-to-xml canonical ua-mapping/uusi-asia) => truthy
         xml-s (xml/indent-str xml) => truthy
-        permit-type (:permitType poikkeus-test/poikkari-hakemus)
+        permit-type (:permitType application)
         schema-version "ah-1.1"]
     (fact "Validator for Asianhallinta exists"
       ((keyword permit-type) validator/supported-versions-by-permit-type) => (contains schema-version))
     (fact "Validate UusiAsia XML"
-      (validator/validate xml-s (:permitType poikkeus-test/poikkari-hakemus) schema-version) => nil)
+      (validator/validate xml-s permit-type schema-version) => nil)
+    (facts "XML elements"
+      (let [xml-parsed (reader/strip-xml-namespaces (sade.xml/parse xml-s))]))
     ; TODO check xml elements, ie deep elements, document values with _selected are correct in xml etc..
     ))
