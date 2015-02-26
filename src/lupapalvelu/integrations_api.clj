3ec2fa6bf7d0abb6a3682326a3b28fe1ab503610
Joni Hamalainen <joni.hamalainen@solita.fi>
2015-02-26 17:04:54 +0200

    change pre-check to return nil

--- src/lupapalvelu/integrations_api.clj
+++ src/lupapalvelu/integrations_api.clj
@@ -152,15 +152,15 @@
 ;; Asianhallinta
 ;;
 
-(defn- has-asianhallinta-operation? [_ {:keys [operations]}]
-  (operations/get-operation-metadata (:name (first operations)) :asianhallinta))
+(defn- has-asianhallinta-operation [_ {:keys [operations]}]
+  (when-not (operations/get-operation-metadata (:name (first operations)) :asianhallinta)))
 
 (defcommand application-to-asianhallinta
   {:parameters [id lang]
    :roles      [:authority]
    :notified   true
    :on-success (notify :application-state-change)
-   :pre-checks [has-asianhallinta-operation?]
+   :pre-checks [has-asianhallinta-operation]
    :states     [:submitted :complement-needed]}
   [{:keys [application created user]:as command}]
   (let [submitted-application (mongo/by-id :submitted-applications id)
