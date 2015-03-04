18dec1d7f1d1240ff7b66e824d78c1ed3603f8a3
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-03-04 15:43:40 +0200

    List actions that have not been called

--- resources/private/admin/actions.js
+++ resources/private/admin/actions.js
@@ -5,12 +5,19 @@
     var self = this;
 
     self.actions = ko.observable();
+    self.uncalledActions = ko.observable();
 
     self.load = function() {
       ajax
         .query("actions")
-        .success(function(d) { self.actions(_.values(d.actions)); })
+        .success(function(d) { self.actions(_.values(d.actions).filter(function(a){return a.type && a.name;})); })
         .call();
+
+      ajax.get("/system/action-counters").success(function(e){
+        var uncalled = _.pick(e, function(c) {return c === 0;});
+        self.uncalledActions(_.keys(uncalled));
+        }).raw(true).call();
+
     };
   }
 
