6cf8a04b10c04d7d9a892202a893befcadac6bbf
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-03-05 07:42:01 +0200

    commented out unused command

--- src/lupapalvelu/construction_api.clj
+++ src/lupapalvelu/construction_api.clj
@@ -29,35 +29,36 @@
                                        :state  :constructionStarted}}))
   (ok))
 
-(defcommand inform-building-construction-started
-  {:parameters ["id" buildingIndex startedDate lang]
-   :roles      [:NONE] ;FIXME rakentamisen aikaisen toimminan yhteydessa korjataan oikeae
-   :states     [:verdictGiven :constructionStarted]
-   :notified   true
-   :pre-checks [(permit/validate-permit-type-is permit/R)]
-   :input-validators [(partial action/non-blank-parameters [:buildingIndex :startedDate :lang])]}
-  [{:keys [user created application] :as command}]
-  (let [timestamp     (util/to-millis-from-local-date-string startedDate)
-        app-updates   (merge
-                        {:modified created}
-                        (when (= "verdictGiven" (:state application))
-                          {:started created
-                           :state  :constructionStarted}))
-        application   (merge application app-updates)
-        organization  (organization/get-organization (:organization application))
-        ftp-user?     (organization/has-ftp-user? organization (permit/permit-type application))
-        building      (or
-                        (some #(when (= (str buildingIndex) (:index %)) %) (:buildings application))
-                        (fail! :error.unknown-building))]
-    (when ftp-user?
-      (mapping-to-krysp/save-aloitusilmoitus-as-krysp application lang organization timestamp building user))
-    (update-application command
-      {:buildings {$elemMatch {:index (:index building)}}}
-      {$set (merge app-updates {:buildings.$.constructionStarted timestamp
-                                :buildings.$.startedBy (select-keys user [:id :firstName :lastName])})})
-    (when (= "verdictGiven" (:state application))
-      (notifications/notify! :application-state-change command))
-    (ok :integrationAvailable ftp-user?)))
+(comment
+  (defcommand inform-building-construction-started
+       {:parameters ["id" buildingIndex startedDate lang]
+        :roles      [:NONE] ;FIXME rakentamisen aikaisen toimminan yhteydessa korjataan oikeae
+        :states     [:verdictGiven :constructionStarted]
+        :notified   true
+        :pre-checks [(permit/validate-permit-type-is permit/R)]
+        :input-validators [(partial action/non-blank-parameters [:buildingIndex :startedDate :lang])]}
+       [{:keys [user created application] :as command}]
+       (let [timestamp     (util/to-millis-from-local-date-string startedDate)
+             app-updates   (merge
+                             {:modified created}
+                             (when (= "verdictGiven" (:state application))
+                               {:started created
+                                :state  :constructionStarted}))
+             application   (merge application app-updates)
+             organization  (organization/get-organization (:organization application))
+             ftp-user?     (organization/has-ftp-user? organization (permit/permit-type application))
+             building      (or
+                             (some #(when (= (str buildingIndex) (:index %)) %) (:buildings application))
+                             (fail! :error.unknown-building))]
+         (when ftp-user?
+           (mapping-to-krysp/save-aloitusilmoitus-as-krysp application lang organization timestamp building user))
+         (update-application command
+           {:buildings {$elemMatch {:index (:index building)}}}
+           {$set (merge app-updates {:buildings.$.constructionStarted timestamp
+                                     :buildings.$.startedBy (select-keys user [:id :firstName :lastName])})})
+         (when (= "verdictGiven" (:state application))
+           (notifications/notify! :application-state-change command))
+         (ok :integrationAvailable ftp-user?))))
 
 (defcommand inform-construction-ready
   {:parameters ["id" readyTimestampStr lang]
