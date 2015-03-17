45e01eb2156cd89241f213ba71f746d74884ad4a
Jari Asp <jari.asp@solita.fi>
2015-03-17 12:17:42 +0200

    Added tests for multiple emails support

--- itest/lupapalvelu/kopiolaitos_itest.clj
+++ itest/lupapalvelu/kopiolaitos_itest.clj
@@ -4,10 +4,14 @@
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.factlet :refer :all]
             [lupapalvelu.kopiolaitos :refer :all]
+            [lupapalvelu.organization :as organization]
             [sade.crypt :as crypt])
   (:import  [java.util.zip ZipInputStream]))
 
-(testable-privates lupapalvelu.kopiolaitos get-kopiolaitos-html-table)
+(testable-privates lupapalvelu.kopiolaitos
+  get-kopiolaitos-html-table
+  separate-emails
+  get-kopiolaitos-email-addresses)
 
 (fact "Sonja sets default values for organization kopiolaitos info"
   (command sipoo :set-kopiolaitos-info
@@ -36,7 +40,7 @@
         :unSelectedAttachmentIds []) => ok?
 
       (let [app (query-application sonja app-id) => map?
-            attachments (get-in app [:attachments]) => sequential?]
+            attachments (:attachments app) => sequential?]
         (fact "Two attachments have forPrinting flags set to true"
           (count (filter :forPrinting attachments)) => 2)))
 
@@ -119,3 +123,25 @@
             :lang "fi"
             :attachmentsWithAmounts (filter :forPrinting attachments-with-amount)
             :orderInfo order-info) => (partial expected-failure? "no-kopiolaitos-email-defined"))))))
+
+
+;; Testing the support for multiple email addresses
+
+(facts "multiple emails concatenated as one kopiolaitos email address"
+
+  (facts "separating emails from the kopiolaitos email addresses"
+    (fact "same email multiple times plus different separator chars"
+      (separate-emails "pena.bulkki@example.com,pena.bulkki@example.com" email-address-separation-regexp) => #{"pena.bulkki@example.com"})
+
+    (fact "different separator chars"
+      (separate-emails "pena.bulkki@example.com, pena.bulkkinen@example.com;mikko.alakesko@example.com" email-address-separation-regexp)
+        => #{"pena.bulkki@example.com" "pena.bulkkinen@example.com" "mikko.alakesko@example.com"})
+
+    (fact "one email"
+      (separate-emails "pena.bulkki@example.com" email-address-separation-regexp) => #{"pena.bulkki@example.com"}))
+
+  (fact "one kopiolaitos email is invalid"
+    (get-kopiolaitos-email-addresses sonja-muni) => (throws Exception #"kopiolaitos-invalid-email")
+    (provided
+      (organization/with-organization sonja-muni :kopiolaitos-email) => "pena.bulkki@example.com;mikko.nodomain")))
+
