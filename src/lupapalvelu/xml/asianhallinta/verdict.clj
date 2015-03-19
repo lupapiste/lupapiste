c6b1dc476664ef5689cbea44a97d3591edf0ba06
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-03-19 15:59:45 +0200

    setting up zip in test, first lines of unpacking zip in verdict processor

--- src/lupapalvelu/xml/asianhallinta/verdict.clj
+++ src/lupapalvelu/xml/asianhallinta/verdict.clj
@@ -1,4 +1,30 @@
-(ns lupapalvelu.xml.asianhallinta.verdict)
+(ns lupapalvelu.xml.asianhallinta.verdict
+  (:require [clojure.string :as s]
+            [clojure.java.io :as io]
+            [sade.core :refer [ok fail] :as sade])
+  (:import (java.io FileNotFoundException)))
+
+(defn- make-path [& args]
+  (let [args (vec args)
+        path (s/join "/" args)]
+    (io/make-parents (s/join "/" (conj args ".")))
+    path))
+
+
+(defn- unzip-file [path-to-zip]
+  (let [zip-file (io/as-file path-to-zip)]
+    (when-not (.exists zip-file)
+      (fail :error.unknown))
+
+    (let [base-dir  (System/getProperty "java.io.tmpdir")
+          base-name (str (sade/now))
+          tmp-dir   (make-path base-dir base-name)]
+      (println tmp-dir)
+      (ok))))
 
 (defn process-ah-verdict [path-to-zip ftp-user]
-  )
+  (try
+    (unzip-file path-to-zip)
+    (catch FileNotFoundException e
+      (println "File not found:" (.getMessage e))
+      )))
