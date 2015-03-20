d3b1b250c23de6882e6bc9f309eece0382091742
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-03-20 10:00:14 +0200

    start using raynes/fs

--- src/lupapalvelu/xml/asianhallinta/verdict.clj
+++ src/lupapalvelu/xml/asianhallinta/verdict.clj
@@ -1,26 +1,20 @@
 (ns lupapalvelu.xml.asianhallinta.verdict
   (:require [clojure.string :as s]
             [clojure.java.io :as io]
-            [sade.core :refer [ok fail] :as sade])
+            [sade.core :refer [ok fail] :as sade]
+            [me.raynes.fs :as fs])
   (:import (java.io FileNotFoundException)))
-
-(defn- make-path [& args]
-  (let [args (vec args)
-        path (s/join "/" args)]
-    (io/make-parents (s/join "/" (conj args ".")))
-    path))
-
 
 (defn- unzip-file [path-to-zip]
   (let [zip-file (io/as-file path-to-zip)]
     (when-not (.exists zip-file)
       (fail :error.unknown))
 
-    (let [base-dir  (System/getProperty "java.io.tmpdir")
-          base-name (str (sade/now))
-          tmp-dir   (make-path base-dir base-name)]
-      (println tmp-dir)
+    (let [tmp-dir (fs/temp-dir "ah")]
       (ok))))
+
+
+
 
 (defn process-ah-verdict [path-to-zip ftp-user]
   (try
