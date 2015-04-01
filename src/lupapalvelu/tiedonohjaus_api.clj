3272161d2031c04c673b374a8a7459dcb494cac3
Markus Penttila <markus.penttila@solita.fi>
2015-04-01 16:33:45 +0300

    Bind operation TOS function code value and implement persistence api for setting the code

--- src/lupapalvelu/tiedonohjaus_api.clj
+++ src/lupapalvelu/tiedonohjaus_api.clj
@@ -1,7 +1,10 @@
 (ns lupapalvelu.tiedonohjaus-api
-  (:require [lupapalvelu.action :refer [defquery non-blank-parameters]]
+  (:require [lupapalvelu.action :refer [defquery defcommand non-blank-parameters]]
             [sade.core :refer [ok]]
-            [lupapalvelu.tiedonohjaus :as t]))
+            [lupapalvelu.tiedonohjaus :as t]
+            [lupapalvelu.organization :as o]
+            [lupapalvelu.organization-api :as oa]
+            [monger.operators :refer :all]))
 
 (defquery available-tos-functions
           {:user-roles #{:anonymous}
@@ -9,3 +12,11 @@
            :input-validators [(partial non-blank-parameters [:organizationId])]}
           (let [functions (t/get-functions-from-toj organizationId)]
             (ok :functions functions)))
+
+(defcommand set-tos-function-for-operation
+            {:parameters [operation functionCode]
+             :user-roles #{:authorityAdmin}
+             :input-validators [(partial non-blank-parameters [:functionCode :operation])]}
+            [{user :user}]
+            (o/update-organization (oa/authority-admins-organization-id user) {$set {(str "operations-tos-functions." operation) functionCode}})
+            (ok))
