9fda392825619da1abdcec01cfe53ce9130e538a
Jari Asp <jari.asp@solita.fi>
2015-04-01 19:03:47 +0300

    no need to resolve the whole organization, use organizationId directly

--- src/lupapalvelu/application.clj
+++ src/lupapalvelu/application.clj
@@ -744,12 +744,11 @@
   (let [enough-info-from-parameters (and
                                       (not (ss/blank? address)) (not (ss/blank? propertyId))
                                       (-> x util/->double pos?) (-> y util/->double pos?))
-        permit-type (operations/permit-type-of-operation operation)
-        organization (organization/get-organization organizationId)]
+        permit-type (operations/permit-type-of-operation operation)]
 
     ;; Prevent creating many applications based on the same kuntalupatunnus:
     ;; Check if we have in database an application of same organization that has a verdict with the given kuntalupatunnus.
-    (if-let [app-with-verdict (domain/get-application-no-access-checking {:organization (:id organization)
+    (if-let [app-with-verdict (domain/get-application-no-access-checking {:organization organizationId
                                                                           :verdicts {$elemMatch {:kuntalupatunnus kuntalupatunnus}}})]
 
       ;; Found an application of same organization that has a verdict with the given kuntalupatunnus. Open it if user has rights, otherwise show error.
@@ -760,7 +759,7 @@
       ;; Fetch xml data needed for application creation from backing system with the provided kuntalupatunnus.
       ;; Then extract needed data from it to "app info".
       (let [xml (krysp-fetch-api/get-application-xml
-                  {:id kuntalupatunnus :permitType permit-type :organization (:id organization)}
+                  {:id kuntalupatunnus :permitType permit-type :organization organizationId}
                   false true)]
         (when-not xml (fail! :error.no-previous-permit-found-from-backend))  ;; Show error if could not receive the verdict message xml for the given kuntalupatunnus
 
@@ -771,7 +770,7 @@
           (when (empty? app-info)
             (fail! :error.no-previous-permit-found-from-backend))
           ;; Given organization and the organization in the verdict message xml differ from each other
-          (when-not (= (:id organization) (:id (organization/resolve-organization (:municipality app-info) permit-type)))
+          (when-not (= organizationId (:id (organization/resolve-organization (:municipality app-info) permit-type)))
             (fail! :error.previous-permit-found-from-backend-is-of-different-organization))
           ;; This needs to be last of these error chekcs! Did not get the "rakennuspaikkatieto" element in the xml, so let's ask more needed location info from user.
           (when-not (or rakennuspaikka-exists enough-info-from-parameters)
