daa0319087fe3437dcd0e6b46456152705be9361
Markus Penttila <markus.penttila@solita.fi>
2015-04-02 16:10:05 +0300

    Add tests for storing the TOS function for an operation

--- src/lupapalvelu/tiedonohjaus_api.clj
+++ src/lupapalvelu/tiedonohjaus_api.clj
@@ -13,16 +13,19 @@
           (let [functions (t/get-functions-from-toj organizationId)]
             (ok :functions functions)))
 
+(defn- store-function-code [operation function-code user]
+  (let [orgId (oa/authority-admins-organization-id user)
+        organization (o/get-organization orgId)
+        operation-valid? (some #{operation} (:selected-operations organization))
+        code-valid? (some #{function-code} (map :code (t/get-functions-from-toj orgId)))]
+    (if (and operation-valid? code-valid?)
+      (do (o/update-organization orgId {$set {(str "operations-tos-functions." operation) function-code}})
+          (ok))
+      (fail "Invalid organization or operation"))))
+
 (defcommand set-tos-function-for-operation
             {:parameters [operation functionCode]
              :user-roles #{:authorityAdmin}
              :input-validators [(partial non-blank-parameters [:functionCode :operation])]}
             [{user :user}]
-            (let [orgId (oa/authority-admins-organization-id user)
-                  organization (o/get-organization orgId)
-                  operation-valid? (some #{operation} (:selected-operations organization))
-                  code-valid? (some #{functionCode} (map :code (t/get-functions-from-toj orgId)))]
-              (if (and operation-valid? code-valid?)
-                (do (o/update-organization orgId {$set {(str "operations-tos-functions." operation) functionCode}})
-                    (ok))
-                (fail "Invalid organization or operation"))))
+            (store-function-code operation functionCode user))
