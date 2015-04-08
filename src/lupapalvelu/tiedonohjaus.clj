1f93f45483a1c3e52c66e14ad5c9b4a4e336ee23
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-04-08 13:13:36 +0300

    indentation tuning

--- src/lupapalvelu/tiedonohjaus.clj
+++ src/lupapalvelu/tiedonohjaus.clj
@@ -6,9 +6,11 @@
 (defn- build-url [& path-parts]
   (apply str (env/value :toj :host) path-parts))
 
-(def get-functions-from-toj (memo/ttl (fn [organization]
-                                        (try (let [response (http/get (build-url "/tiedonohjaus/api/org/" organization "/asiat") {:as :json})]
-                                          (:body response))
-                                             (catch Exception e
-                                               [])))
-                                      :ttl/threshold 10000))
+(def get-functions-from-toj
+  (memo/ttl (fn [organization]
+              (try
+                (let [response (http/get (build-url "/tiedonohjaus/api/org/" organization "/asiat") {:as :json})]
+                     (:body response))
+                (catch Exception e
+                  [])))
+            :ttl/threshold 10000))
--- src/lupapalvelu/tiedonohjaus_api.clj
+++ src/lupapalvelu/tiedonohjaus_api.clj
@@ -7,11 +7,11 @@
             [monger.operators :refer :all]))
 
 (defquery available-tos-functions
-          {:user-roles #{:anonymous}
-           :parameters [organizationId]
-           :input-validators [(partial non-blank-parameters [:organizationId])]}
-          (let [functions (t/get-functions-from-toj organizationId)]
-            (ok :functions functions)))
+  {:user-roles       #{:anonymous}
+   :parameters       [organizationId]
+   :input-validators [(partial non-blank-parameters [:organizationId])]}
+  (let [functions (t/get-functions-from-toj organizationId)]
+    (ok :functions functions)))
 
 (defn- store-function-code [operation function-code user]
   (let [orgId (oa/authority-admins-organization-id user)
@@ -24,8 +24,8 @@
       (fail "Invalid organization or operation"))))
 
 (defcommand set-tos-function-for-operation
-            {:parameters [operation functionCode]
-             :user-roles #{:authorityAdmin}
-             :input-validators [(partial non-blank-parameters [:functionCode :operation])]}
-            [{user :user}]
-            (store-function-code operation functionCode user))
+  {:parameters       [operation functionCode]
+   :user-roles       #{:authorityAdmin}
+   :input-validators [(partial non-blank-parameters [:functionCode :operation])]}
+  [{user :user}]
+  (store-function-code operation functionCode user))
