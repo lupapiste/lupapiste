2e282c55d891ebb179003433973471876d87341e
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-04-14 16:32:33 +0300

    fetch companies

--- resources/private/ui-components/company-selector/company-selector-model.js
+++ resources/private/ui-components/company-selector/company-selector-model.js
@@ -1,4 +1,28 @@
 LUPAPISTE.CompanySelectorModel = function(params) {
   "use strict";
+
+  function mapCompany(company) {
+    company.displayName = ko.pureComputed(function() {
+      return company.name + ", " + (company.address1 ? company.address1 + " ": "") + (company.po ? company.po : "");
+    });
+    return company;
+  }
+
+  var self = this;
+
   console.log("CompanySelectorModel", params);
+
+  self.companies = ko.observableArray();
+  self.pending = ko.observable();
+  self.selected = ko.observable();
+
+  _.defer(function() {
+    ajax
+      .query("companies")
+      .pending(self.pending)
+      .success(function(response) {
+        self.companies(_.map(response.companies, mapCompany));
+      })
+      .call();
+  });
 };
