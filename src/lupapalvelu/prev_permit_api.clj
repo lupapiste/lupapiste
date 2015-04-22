aefb09a8fd7def85fefa6bbf712d7a121d8f0ae1
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-04-22 13:05:21 +0300

    rest endpoint for fetching previously created permits from municipality backends

--- src/lupapalvelu/prev_permit_api.clj
+++ src/lupapalvelu/prev_permit_api.clj
@@ -11,18 +11,56 @@
             [lupapalvelu.xml.krysp.reader :as krysp-reader]
             [lupapalvelu.organization :as organization]
             [lupapalvelu.action :refer [defcommand defraw]]
-            [lupapalvelu.prev-permit :as prev-permit]))
+            [lupapalvelu.prev-permit :as prev-permit]
+            [noir.response :as resp]))
+
+
+(defn- try-fetch-prev-application! [kuntalupatunnus organization permit-type command]
+  (let [dummy-application {:id kuntalupatunnus :permitType permit-type :organization organization}
+        xml (krysp-fetch-api/get-application-xml dummy-application :kuntalupatunnus)]
+    (when-not xml (fail! :error.no-previous-permit-found-from-backend)) ;; Show error if could not receive the verdict message xml for the given kuntalupatunnus
+
+    (let [app-info (krysp-reader/get-app-info-from-message xml kuntalupatunnus)
+          rakennuspaikka-exists (and (:rakennuspaikka app-info) (every? (-> app-info :rakennuspaikka keys set) [:x :y :address :propertyId]))
+          lupapiste-tunnus (:id app-info)]
+      ;; Could not extract info from verdict message xml
+      (when (empty? app-info)
+        (fail! :error.no-previous-permit-found-from-backend))
+      ;; Given organization and the organization in the verdict message xml differ from each other
+      (when-not (= organization (:id (organization/resolve-organization (:municipality app-info) permit-type)))
+        (fail! :error.previous-permit-found-from-backend-is-of-different-organization))
+      ;; This needs to be last of these error chekcs! Did not get the "rakennuspaikkatieto" element in the xml, so let's ask more needed location info from user.
+      (when-not rakennuspaikka-exists
+        (fail! :error.more-prev-app-info-needed :needMorePrevPermitInfo true))
+
+      (if (ss/blank? lupapiste-tunnus)
+        ;; NO LUPAPISTE ID FOUND -> create the application
+        (let [location-info (:rakennuspaikka app-info)
+              created-app-id (prev-permit/do-create-application-from-previous-permit command xml app-info location-info)]
+          (ok :id created-app-id))
+        ;; LUPAPISTE ID WAS FOUND -> open it if user has rights, otherwise show error
+        (if-let [existing-application (domain/get-application-as lupapiste-tunnus (:user command))]
+          (resp/status 200 (str (ok :id (:id existing-application))))
+          (fail :error.lupapiste-application-already-exists-but-unauthorized-to-access-it :id lupapiste-tunnus))))))
 
 (defraw get-lp-id-from-previous-permit
   {:parameters [:kuntalupatunnus]
    :input-validators [(partial action/non-blank-parameters [:kuntalupatunnus])]
-   :user-roles #{:anonymous}}
-  [{{:keys [kuntalupatunnus]} :data}]
+   :user-roles #{:rest-api}}
+  [{{:keys [kuntalupatunnus]} :data :keys [user] :as command}]
+  (let [command (update-in command [:data] merge {:operation :aiemmalla-luvalla-hakeminen})
+        existing-app (domain/get-application-as {:organization (first (:organizations user))
+                                                 :verdicts {$elemMatch {:kuntalupatunnus kuntalupatunnus}}} user)]
+    (if existing-app
+      (resp/status 200 (str (merge (ok :id (:id existing-app))
+                                   {:status :already-existing-application})))
+      (resp/status 200 (str (merge (try-fetch-prev-application! kuntalupatunnus
+                                                                (first (:organizations user))
+                                                                (operations/permit-type-of-operation :aiemmalla-luvalla-hakeminen)
+                                                                command)
+                                   {:status :created-new-application}))))))
 
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body (str "kuntalupatunnus: " kuntalupatunnus)}
-  )
+
 
 (defcommand create-application-from-previous-permit
   {:parameters       [:lang :operation :x :y :address :propertyId :organizationId :kuntalupatunnus]
