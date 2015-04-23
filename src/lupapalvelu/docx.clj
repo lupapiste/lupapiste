7f81b2ca939719b119f12f778d3cc84e420be254
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-04-23 13:41:41 +0300

    Refactored PoC to an API

--- src/lupapalvelu/docx.clj
+++ src/lupapalvelu/docx.clj
@@ -1,9 +1,10 @@
 (ns lupapalvelu.docx
-  (:require [clojure.java.io :as io]
+  (:require [taoensso.timbre :as timbre :refer [debug debugf info warn error]]
+            [clojure.java.io :as io]
             [clojure.walk :as walk]
             [sade.core :refer :all]
             [sade.util :as util])
-  (:import [java.io FileOutputStream]
+  (:import [java.io OutputStream InputStream ByteArrayOutputStream ByteArrayInputStream]
            [fr.opensagres.xdocreport.converter Options ConverterTypeTo ConverterTypeVia]
            [fr.opensagres.xdocreport.document IXDocReport]
            [fr.opensagres.xdocreport.document.registry XDocReportRegistry]
@@ -14,32 +15,42 @@
 
 (def ^Options to-pdf (-> (Options/getTo ConverterTypeTo/PDF) (.via ConverterTypeVia/XWPF)))
 
-(defn- ^IContext create-context
-  ([^IXDocReport report m]
-    (create-context report m (constantly "")))
-  ([^IXDocReport report m nil-convert]
-    (reduce
-      (fn [^IContext c, [k v]] (.put c k v) c)
-      (.createContext report)
-      ; Freemarker throws exception from null values by default
-      (-> m walk/stringify-keys (util/convert-values #(nil? %2) nil-convert)))))
+(defn- freemarker-compliant [m]
+  ; Freemarker throws exception from null values by default
+  (-> m walk/stringify-keys (util/convert-values #(nil? %2) (constantly ""))))
 
-(defn poc []
-  (with-open [in (-> "yritystilisopimus.docx" io/resource io/input-stream)
-              out (FileOutputStream. (io/file "out.pdf"))]
+(defn- ^IContext create-context [^IXDocReport report m]
+  (reduce (fn [^IContext c, [k v]] (.put c k v) c) (.createContext report) m))
+
+(def- yritystilisopimus-default-model {:date ""
+                                       :company {:name "", :y "", :address1 "" :address2 ""}
+                                       :contact {:firstName "", :lastName ""}
+                                       :account {:type "", :price ""}})
+
+(defn ^InputStream yritystilisopimus [company contact account timestamp]
+  {:pre [(map? company) (map? contact) (map? account)]}
+  (with-open [out (ByteArrayOutputStream. (* 60 1024)) ; output will be about 60 KB
+              in (-> "yritystilisopimus.docx" io/resource io/input-stream)]
     (let [report  (.loadReport (XDocReportRegistry/getRegistry) in TemplateEngineKind/Freemarker )
-          context (create-context report {:company {:name "Asiakas Oy",
-                                                    :y "123456-1"
-                                                    :address1 "Osoiterivi 1"
-                                                    :address2 nil}
-                                          :contact {:firstName "Etu",
-                                                    :lastName "Suku"}
-                                          :account {:type "TEST",
-                                                    :price "100"}})
+          model (freemarker-compliant (util/deep-merge
+                                        yritystilisopimus-default-model
+                                        {:date (util/to-local-date timestamp)
+                                         :company company
+                                         :contact contact
+                                         :account account}))
+          context (create-context report model)
           starting (now)]
-
       (.convert report context to-pdf out)
-      (println "Conversion took" (- (now) starting) "ms")
-      )
-    )
-  )
+      (debugf "Conversion took %d ms" (- (now) starting))
+      (ByteArrayInputStream. (.toByteArray out)))))
+
+(defn- poc []
+  (with-open [pdf (yritystilisopimus
+                    {:name "Asiakas Oy",
+                     :y "123456-1"
+                     :address1 "Osoiterivi 1"}
+                    {:firstName "Etu", :lastName "Suku"}
+                    {:type "TEST", :price "100"}
+                    (+ (now) (* 1000 60 60 24)))
+              out (io/output-stream "out.pdf")]
+    (io/copy pdf out)))
