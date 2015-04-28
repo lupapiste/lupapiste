b81dc2b689188b60f767341a4caced61023eb864
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-04-28 11:06:47 +0300

    Fixed comment as suggested

--- src/lupapalvelu/user.clj
+++ src/lupapalvelu/user.clj
@@ -66,7 +66,7 @@
 (def canonize-email (comp ss/lower-case ss/trim))
 
 (defn organization-ids
-  "Returns a set of organization IDs where user some role."
+  "Returns user's organizations as a set"
   [{org-authz :orgAuthz :as user}]
   (->> org-authz keys (map name) set))
 
