727a63f6316c58b893f5509e46102e5ae42428e4
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-05-07 21:54:58 +0300

    enable keyboard navigation and selection

--- resources/private/ui-components/autocomplete/autocomplete-model.js
+++ resources/private/ui-components/autocomplete/autocomplete-model.js
@@ -13,6 +13,8 @@ LUPAPISTE.AutocompleteModel = function(params) {
   self.showResult = ko.observable(false);
   self.selected = ko.observable("");
 
+  self.index = ko.observable(0);
+
   self.selectInput = function() {
     self.inputSelected(true);
     self.showResult(true);
@@ -21,6 +23,24 @@ LUPAPISTE.AutocompleteModel = function(params) {
   self.selectItem = function(item) {
     self.value(item);
     self.selected(item.label);
+    self.inputSelected(false);
     self.showResult(false);
+  };
+
+  self.navigate = function(data, event) {
+    if (event.keyCode === 13) {
+      self.selectItem(self.dataProvider.data()[self.index()]);
+    }
+    else if (event.keyCode === 38) {
+      self.index(self.index() > 0 ? self.index() - 1 : 0);
+    }
+    else if (event.keyCode === 40) {
+      self.index(self.index() + 1 < self.dataProvider.data().length ? self.index() + 1 : self.index());
+    }
+    return true;
+  };
+
+  self.changeIndex = function(index) {
+    self.index(index);
   };
 };
