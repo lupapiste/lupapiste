6511387bc0140de3e44783d464cc40199d18262a
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-05-15 16:24:20 +0300

    support disabled options in invoice operators

--- resources/private/ui-components/invoice-operator-selector/invoice-operator-selector-model.js
+++ resources/private/ui-components/invoice-operator-selector/invoice-operator-selector-model.js
@@ -3,14 +3,19 @@ LUPAPISTE.InvoiceOperatorSelectorModel = function(params) {
   var self = this;
 
   self.operators = _(LUPAPISTE.config.eInvoiceOperators).map(function(operator) {
-      var name = loc("osapuoli.yritys.verkkolaskutustieto.valittajaTunnus." + operator);
-      return {
-        name: name,
-        code: operator
-      }
-    }).sortBy(function(operator) {
-      return operator.name;
-    }).value();
+    var name = loc("osapuoli.yritys.verkkolaskutustieto.valittajaTunnus." + operator.name);
+    return {
+      name: name,
+      code: operator,
+      disabled: operator.disabled
+    }
+  }).sortBy(function(operator) {
+    return operator.name;
+  }).value();
+
+  self.setOptionDisable = function(option, item) {
+    ko.applyBindingsToNode(option, {disable: item.disabled}, item);
+  }
 
   self.selected = params.selected || ko.observable();
   self.enabled = params.enabled || true;
--- src/lupapalvelu/components/ui_components.clj
+++ src/lupapalvelu/components/ui_components.clj
@@ -34,7 +34,7 @@
                  :userAttachmentTypes (map #(str "osapuolet." (name %)) attachment-types-osapuoli)
                  :attachmentScales  attachment-scales
                  :attachmentSizes   attachment-sizes
-                 :eInvoiceOperators (map :name schemas/e-invoice-operators)
+                 :eInvoiceOperators schemas/e-invoice-operators
                  :postVerdictStates lupapalvelu.application-meta-fields/post-verdict-states
                  :stampableMimes    (filter identity (map mime/mime-types file-types))
                  :foremanRoles      (:body (first lupapalvelu.document.schemas/kuntaroolikoodi-tyonjohtaja))
