1290a7ce2e2a8d8519a527c0ea3505332256cb31
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-05-17 19:53:47 +0300

    Even more minimal PDF. https://blog.idrsolutions.com/2010/10/grow-your-own-pdf-file-%E2%80%93-part-3-diy-blank-page/

--- robot/tests/21_stamping/variables.py
+++ robot/tests/21_stamping/variables.py
@@ -13,63 +13,22 @@ JPG_TESTFILE_PATH = os.path.join(os.path.expanduser('~'), JPG_TESTFILE_NAME)
 
 PDF_TESTFILE_NAME1 = 'robotframework-testfile-21_stamping_01v2.pdf'
 PDF_TESTFILE_CONTENT = """\
-%PDF-1.1
-%¥±ë
-
-1 0 obj
-  << /Type /Catalog
-     /Pages 2 0 R
-  >>
+%PDF-1.6
+1 0 obj <</Type /Catalog /Pages 2 0 R>>
 endobj
-
-2 0 obj
-  << /Type /Pages
-     /Kids [3 0 R]
-     /Count 1
-     /MediaBox [0 0 300 144]
-  >>
+2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>>
 endobj
-
-3 0 obj
-  <<  /Type /Page
-      /Parent 2 0 R
-      /Resources
-       << /Font
-           << /F1
-               << /Type /Font
-                  /Subtype /Type1
-                  /BaseFont /Times-Roman
-               >>
-           >>
-       >>
-      /Contents 4 0 R
-  >>
+3 0 obj<> /MediaBox [0 0 500 800]>>
 endobj
-
-4 0 obj
-  << /Length 55 >>
-stream
-  BT
-    /F1 18 Tf
-    0 0 Td
-    (Hello World) Tj
-  ET
-endstream
-endobj
-
 xref
-0 5
+0 4
 0000000000 65535 f
-0000000018 00000 n
-0000000077 00000 n
-0000000178 00000 n
-0000000457 00000 n
-trailer
-  <<  /Root 1 0 R
-      /Size 5
-  >>
+0000000010 00000 n
+0000000060 00000 n
+0000000115 00000 n
+trailer <</Size 4/Root 1 0 R>>
 startxref
-565
+199
 %%EOF
 """
 PDF_TESTFILE_DESCRIPTION = "attachment-pdf-foo"
