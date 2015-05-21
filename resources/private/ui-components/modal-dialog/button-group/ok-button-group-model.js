863a2e134b723844ddf43f61008f3f6890cb9682
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-05-21 13:47:35 +0300

    fix foreman history ok dialog

--- resources/private/ui-components/modal-dialog/button-group/ok-button-group-model.js
+++ resources/private/ui-components/modal-dialog/button-group/ok-button-group-model.js
@@ -2,11 +2,13 @@ LUPAPISTE.OkButtonGroupModel = function (params) {
   "use strict";
   var self = this;
 
-  self.okTitle = params.okTitle || "ok";
+  self.okTitle = util.getIn(params, ["okTitle"]) || "ok";
+
+  var okFn = util.getIn(params, ["okTitle"]);
 
   self.ok = function() {
-    if (_.isFunction(params.okFn)) {
-      params.okFn();
+    if (_.isFunction(okFn)) {
+      okFn();
     }
     hub.send("close-dialog");
   };
