2c128158e1b084730c9f3ce2eba85b3d4ff26a4e
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-05-21 16:10:54 +0300

    add notification for new user

--- src/lupapalvelu/user_api.clj
+++ src/lupapalvelu/user_api.clj
@@ -179,7 +179,10 @@
                       (assoc user-entry :id (:id old-user))
                       (assoc user-entry :id (mongo/create-id)))
         email       (:email new-user)
-        {old-id :id old-role :role}  old-user]
+        {old-id :id old-role :role}  old-user
+        notification {:titleI18nkey "user.notification.firstLogin.title"
+                      :messageI18nkey "user.notification.firstLogin.message"}
+        new-user   (assoc new-user :notification notification)]
     (try
       (condp = old-role
         nil     (do
