a4e3611484ffcfcbb5c13613bb1a5605d09f277d
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-05-22 09:42:38 +0300

    Removed unused functionality

--- resources/private/common/notify.js
+++ resources/private/common/notify.js
@@ -1,42 +1,18 @@
 var notify = (function() {
   "use strict";
 
-  var level = {
-    DEBUG : "DEBUG",
-    DEPLOY : "DEPLOY"
-  };
-  var env = {
-    level : level.DEPLOY
-  };
-
-  function setLevel(level) {
-    env.level = level;
-  }
-
-  function success(title, data) {
-    LUPAPISTE.ModalDialog.close();
-    LUPAPISTE.ModalDialog.showDynamicOk(title, data);
-  }
-
-  function error(title, data) {
+  function displayMessage(defautlTitle, title, data) {
     LUPAPISTE.ModalDialog.close();
     if (data !== undefined) {
       LUPAPISTE.ModalDialog.showDynamicOk(title, data);
     } else {
-      LUPAPISTE.ModalDialog.showDynamicOk(loc("error.dialog.title"), title);
+      LUPAPISTE.ModalDialog.showDynamicOk(defaultTitle, data);
     }
   }
 
-  function info(title, data) {
-    debug(title, data);
-  }
-
   return {
-    info : info,
-    error : error,
-    success : success,
-    level : level,
-    setLevel : setLevel
+    error: _.partial(displayMessage, loc("error.dialog.title")),
+    success: _.partial(displayMessage, loc("success.dialog.title"))
   };
 
 })();
