7c87e482743f4a4ede3f72871e21e8627b480f23
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-05-22 09:28:00 +0300

    One parameter version of notify.error

--- resources/private/common/notify.js
+++ resources/private/common/notify.js
@@ -19,7 +19,11 @@ var notify = (function() {
 
   function error(title, data) {
     LUPAPISTE.ModalDialog.close();
+    if (data !== undefined) {
     LUPAPISTE.ModalDialog.showDynamicOk(title, data);
+    } else {
+      LUPAPISTE.ModalDialog.showDynamicOk(loc("error.dialog.title"), title);
+    }
   }
 
   function info(title, data) {
