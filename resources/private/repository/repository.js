4fe6c9efce90d586691c31346a582c5f550b2e29
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-05-26 12:06:37 +0300

    flow: Created branch 'feature/LPK-304_Hankkeen_paatoimenpide_osoitteen_alle'.

--- resources/private/repository/repository.js
+++ resources/private/repository/repository.js
@@ -85,6 +85,7 @@ var repository = (function() {
       }
 
       function setOperation(application, doc) {
+        console.log("setOperation", application, doc);
         var schemaInfo = doc["schema-info"];
         if (schemaInfo.op) {
           var op = _.findWhere(application.operations, {id: schemaInfo.op.id});
