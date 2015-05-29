c4311951ed6ef023e86eaa40697464d11723e848
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-05-29 09:57:11 +0300

    Save signed document URL and use the name given in registration form

--- src/lupapalvelu/onnistuu/process.clj
+++ src/lupapalvelu/onnistuu/process.clj
@@ -182,7 +182,7 @@
            identifier
            name
            document)
-    (let [company  (c/create-company (merge (:company process) {:name name, :process-id process-id}))
+    (let [company  (c/create-company (merge (:company process) {:process-id process-id, :document document}))
           token-id (if (nil? (:currentUser signer)) (c/add-user-after-company-creation! signer company :admin))
           mail-model (assoc (select-keys process [:company :signer]) :document document :signature (first signatures))]
       (infof "sign:success:%s: company-created: y [%s], company: [%s], id: [%s], token: [%s]"
