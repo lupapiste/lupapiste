a6f12f1ef94b05aa46719cacde374b3644217822
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-06-03 08:57:20 +0300

    fix TODO

--- test/lupapalvelu/document/rakennuslupa_canonical_test.clj
+++ test/lupapalvelu/document/rakennuslupa_canonical_test.clj
@@ -1178,7 +1178,7 @@
                              "Pohjakatselmus 1"
                              1354532324658
                              [{:rakennus {:rakennusnro "002" :jarjestysnumero 1 :kiinttun "01234567891234"}
-                               :tila     {:tila nil :kayttoonottava nil}}] ; TODO test these
+                               :tila     {:tila "pidetty" :kayttoonottava false}}]
                              authority-user-jussi
                              "pohjakatselmus"
                              :katselmus
@@ -1237,7 +1237,11 @@
                  lasnaolijat (:lasnaolijat Katselmus ) => "Tiivi Taavi, Hipsu ja Lala"
                  poikkeamat (:poikkeamat Katselmus) => "Ei poikkeamisia"
                  tarkastuksenTaiKatselmuksenNimi (:tarkastuksenTaiKatselmuksenNimi Katselmus) => "Pohjakatselmus 1"
-                 kayttotapaus (:kayttotapaus RakennusvalvontaAsia) => "Uusi katselmus"]
+                 kayttotapaus (:kayttotapaus RakennusvalvontaAsia) => "Uusi katselmus"
+
+                 rakennustieto (first (:katselmuksenRakennustieto Katselmus)) => truthy
+                 rakennusOsittainen (get-in rakennustieto [:KatselmuksenRakennus :katselmusOsittainen]) => "pidetty"
+                 rakennusKayttoonotto (get-in rakennustieto [:KatselmuksenRakennus :kayttoonottoKytkin]) => false]
 
              (:kuvaus huomautus) => "Saunan ovi pit\u00e4\u00e4 vaihtaa 900mm leve\u00e4ksi.\nPiha-alue siivottava v\u00e4litt\u00f6m\u00e4sti."
              (:maaraAika huomautus) => "2014-05-05"
