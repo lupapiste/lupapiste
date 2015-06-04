922ba77ef56dd25f6bb186f7ea5e19a04d5de988
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-06-04 10:06:02 +0300

    moved perfmon endpoints to perfmon-api; refactored browser-timing to use defcommand

--- src/lupapalvelu/perf_mon.clj
+++ src/lupapalvelu/perf_mon.clj
@@ -3,13 +3,11 @@
             [noir.core :refer [defpage]]
             [monger.operators :refer :all]
             [clojure.string :refer [join]]
-            [clojure.java.io :as io]
             [monger.collection :as mc]
-            [noir.response :as resp]
-            [noir.request :as request]
+
             [noir.server :as server]
-            [cheshire.core :as json])
-  (:import [com.mongodb WriteConcern]))
+            [sade.env :as env]
+            [sade.core :refer :all]))
 
 ;;
 ;; Instrumenting:
@@ -102,7 +100,7 @@
        (mc/find-maps "perf-mon" {$and [{:ts {$gte start}}
                                        {:ts {$lte end}}]})))
 
-(defn- to-long [v]
+(defn to-long [v]
   (when v
     (if (string? v) (Long/parseLong v) (long v))))
 
@@ -113,41 +111,16 @@
            :cnt (inc cnt)
            :db-cnt (count perfmon)))
 
-(defn- find-min-max-avg* [[k v]]
+(defn find-min-max-avg* [[k v]]
   [k (reduce find-min-max-avg {:min-val Double/MAX_VALUE :max-val Double/MIN_VALUE :avg 0.0 :cnt 0} v)])
-
-(defpage "/perfmon/data" {:keys [start end]}
-  (->> (get-data (or (to-long start) (- (System/currentTimeMillis) (* 5 60 1000)))
-                 (or (to-long end) (System/currentTimeMillis)))
-       (group-by :uri)
-       (map find-min-max-avg*)
-       (resp/json)
-       (resp/status 200)))
-
-(defpage [:get "/perfmon/throttle"] _
-  (->> {:db @db-throttle :web @web-throttle} (resp/json) (resp/status 200)))
-
-(defpage [:post "/perfmon/throttle/:id"] {id :id value :value}
-  (let [throttle ({"db" db-throttle "web" web-throttle} id)]
-    (when (and throttle value)
-      (reset! throttle (to-long value))
-      (->> {id value} (resp/json) (resp/status 200)))))
-
-(defpage [:post "/api/perfmon/browser-timing"] {timing :timing}
-  (let [user-agent (-> (request/ring-request) :headers (get "user-agent"))]
-    (mc/insert "perf-mon-timing"
-      {:ts (java.util.Date.)
-       :ua user-agent
-       :timing timing}
-      WriteConcern/NONE))
-  (->> {:ok true} (resp/json) (resp/status 200)))
 
 ;;
 ;; Initialize: register middlewares and wrap vars:
 ;;
 
 (defn init []
+  (when (env/dev-mode?)
+    (server/add-middleware throttle-middleware)
+    (instrument-ns wrap-db-throttle 'lupapalvelu.mongo))
   (server/add-middleware perf-mon-middleware)
-  (server/add-middleware throttle-middleware)
-  (instrument-ns wrap-db-throttle 'lupapalvelu.mongo)
   (instrument-ns wrap-perf-mon 'lupapalvelu.mongo))
