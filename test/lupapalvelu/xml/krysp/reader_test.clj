ee64c57298589ab0ec94e53370e8495d600c45db
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-06-04 10:25:23 +0300

    fix broken test

--- test/lupapalvelu/xml/krysp/reader_test.clj
+++ test/lupapalvelu/xml/krysp/reader_test.clj
@@ -463,7 +463,7 @@
     (fact "rakennusvalvontaasianKuvaus" rakennusvalvontaasianKuvaus => "Rakennetaan yksikerroksinen lautaverhottu omakotitalo jossa kytketty autokatos/ varasto.")
     (fact "vahainenPoikkeaminen" vahainenPoikkeaminen => "Poikekkaa meill\u00e4!")
     (facts "hakijat"
-      (fact "count" (count hakijat) => 5)
+      (fact "count" (count hakijat) => 6)
       (fact "with email address" (filter identity (map #(get-in % [:henkilo :sahkopostiosoite]) hakijat)) => (just #{"pena@example.com" "mikko@example.com" " \\t   "})))
 
     (facts "Rakennuspaikka"
