46214cdabc4c90977b4413df2ae5a63cb01d7534
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-06-16 15:29:17 +0300

    save accountType and customAccountLimit from modal dialog

--- resources/private/ui-components/admin/company-edit-model.js
+++ resources/private/ui-components/admin/company-edit-model.js
@@ -4,9 +4,22 @@ LUPAPISTE.CompanyEditModel = function(params) {
   var self = this;
 
   self.company = ko.mapping.fromJS(params.company);
-  self.accountTypes = (LUPAPISTE.config.accountTypes).push("custom");
 
+
+  self.accountTypes = LUPAPISTE.config.accountTypes.slice(); // copy
+  self.accountTypes.push({name: "custom"});
+  self.showCustomCount = ko.pureComputed(function() {
+    return self.company.accountType() === "custom";
+  });
 
   self.saveCompany = function() {
+    ajax
+      .command("company-update",
+               {company: self.company.id(),
+                updates: _.pick(ko.mapping.toJS(self.company), ["accountType", "customAccountLimit"])})
+      .success(function() {
+        hub.send("company-updated");
+        hub.send("close-dialog");})
+      .call();
   };
 };
