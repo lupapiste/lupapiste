607aec67ca414a27f5453f7baed0a0e9d8e8b20b
Michael Holopainen <michael.holopainen@solita.fi>
2015-06-18 12:48:21 +0300

    Preview code cleanup

--- src/lupapalvelu/preview.clj
+++ src/lupapalvelu/preview.clj
@@ -6,42 +6,46 @@
            (java.awt RenderingHints)
            (java.io FileOutputStream ByteArrayOutputStream ByteArrayInputStream)))
 
-(defn pdf-preview-file [filename]
+(def rez 600.0)
+
+(defn pdf-preview-file
+  "Quick and dirty preview image file creation. "
+  [filename]
   (try (let [document (PDDocument/load filename)
              pdfWriter (PDFImageWriter.)]
-         (.writeImage pdfWriter
-                      document
-                      "jpg"
-                      ""
-                      1
-                      Integer/MAX_VALUE
-                      "preview_"
-                      1
-                      16
-                      ))
+         (.writeImage pdfWriter document "jpg" "" 1 Integer/MAX_VALUE "preview_" 1 16))
        true
        (catch Exception e false)))
 
-(def rez 600.0)
-(defn scale-image [image] (let [
-                                crop-x (if (< (/ (.getHeight image) (.getWidth image)) 5/7) (- (.getWidth image) (/ (.getHeight image) 5/7) ) 0)
-                                crop-y (if (< (/ (.getWidth image) (.getHeight image)) 5/7) (- (.getHeight image) (/ (.getWidth image) 5/7) ) 0)
-                                scale (min (/ rez (- (.getWidth image) crop-x)) (/ rez (- (.getHeight image) crop-y)))
-                                width (* scale (- (.getWidth image) crop-x))
-                                height (* scale (- (.getHeight image) crop-y))
-                                scaled-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
-                            (println "pdf rez: " (.getWidth image) "x" (.getHeight image) " crop: " crop-x "x" crop-y " scale by " scale)
-                            (doto (.createGraphics scaled-image)
-                              (.setRenderingHint RenderingHints/KEY_INTERPOLATION, RenderingHints/VALUE_INTERPOLATION_BICUBIC)
-                              (.setRenderingHint RenderingHints/KEY_RENDERING, RenderingHints/VALUE_RENDER_QUALITY)
-                              (.setRenderingHint RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
-                              (.drawImage image, 0, 0, width, height, crop-x, crop-y, (.getWidth image), (.getHeight image), nil)
-                              (.dispose))
-                            scaled-image))
+(defn scale-image
+  "Crops and scales BufferedImage to predefined resolution"
+  [image]
+  (let [
+        original-height (.getHeight image)
+        original-width (.getWidth image)
+        crop-x (if (< (/ original-height original-width) 5/7) (- original-width (/ original-height 5/7)) 0)
+        crop-y (if (< (/ original-width original-height) 5/7) (- original-height (/ original-width 5/7)) 0)
+        scale (min (/ rez (- original-width crop-x)) (/ rez (- original-height crop-y)))
+        width (* scale (- original-width crop-x))
+        height (* scale (- original-height crop-y))
+        new-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
+    (println "pdf rez: " original-width "x" original-height " crop: " crop-x "x" crop-y " scale by " scale)
+    (doto (.createGraphics new-image)
+      (.setRenderingHint RenderingHints/KEY_INTERPOLATION, RenderingHints/VALUE_INTERPOLATION_BICUBIC)
+      (.setRenderingHint RenderingHints/KEY_RENDERING, RenderingHints/VALUE_RENDER_QUALITY)
+      (.setRenderingHint RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
+      (.drawImage image, 0, 0, width, height, crop-x, crop-y, original-width, original-height, nil)
+      (.dispose))
+    new-image))
 
-(defn output-image [image file-output-stream compression] (ImageIOUtil/writeImage image "jpg" file-output-stream ImageIOUtil/DEFAULT_SCREEN_RESOLUTION compression))
+(defn output-image
+  "Writes BufferedImage to OutputStream"
+  [image file-output-stream compression]
+  (ImageIOUtil/writeImage image "jpg" file-output-stream ImageIOUtil/DEFAULT_SCREEN_RESOLUTION compression))
 
-(defn pdf-to-image-output [pdf-input out]
+(defn pdf-to-image-output
+  "Converts 1. page from PDF to BufferedImage scaling and cropping to predefined resolution and writes it to given OutputStream"
+  [pdf-input out]
   (let [document (PDDocument/load pdf-input)]
     (try
       (output-image (->> (.. document getDocumentCatalog getAllPages iterator next convertToImage) scale-image) out 0.5)
@@ -49,8 +53,9 @@
       (catch Exception e (println e))
       (finally (.close document)))))
 
-
-(defn pdf-to-image-input-stream [pdf-input]
+(defn pdf-to-image-input-stream
+  "Converts 1. page from PDF to scaled and cropped predefined resolution jpg InputStream"
+  [pdf-input]
   (let [content (ByteArrayOutputStream.)]
     (pdf-to-image-output pdf-input content)
     (ByteArrayInputStream. (.toByteArray content))))
