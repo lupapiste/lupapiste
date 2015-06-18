c821b0a0efc5e1c902eda1328f1f0db194a80d49
Michael Holopainen <michael.holopainen@solita.fi>
2015-06-18 11:41:38 +0300

    Preview : added crop to 5/7 (A4) if ratio smaller (landscape or portrait)

--- src/lupapalvelu/preview.clj
+++ src/lupapalvelu/preview.clj
@@ -22,15 +22,20 @@
        true
        (catch Exception e false)))
 
-(defn scale-image [image] (let [scale (min (/ 600.0 (.getWidth image)) (/ 600.0 (.getHeight image)))
-                                width (* scale (.getWidth image))
-                                height (* scale (.getHeight image))
+(def rez 600.0)
+(defn scale-image [image] (let [
+                                crop-x (if (< (/ (.getHeight image) (.getWidth image)) 5/7) (- (.getWidth image) (/ (.getHeight image) 5/7) ) 0)
+                                crop-y (if (< (/ (.getWidth image) (.getHeight image)) 5/7) (- (.getHeight image) (/ (.getWidth image) 5/7) ) 0)
+                                scale (min (/ rez (- (.getWidth image) crop-x)) (/ rez (- (.getHeight image) crop-y)))
+                                width (* scale (- (.getWidth image) crop-x))
+                                height (* scale (- (.getHeight image) crop-y))
                                 scaled-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
+                            (println "pdf rez: " (.getWidth image) "x" (.getHeight image) " crop: " crop-x "x" crop-y " scale by " scale)
                             (doto (.createGraphics scaled-image)
                               (.setRenderingHint RenderingHints/KEY_INTERPOLATION, RenderingHints/VALUE_INTERPOLATION_BICUBIC)
                               (.setRenderingHint RenderingHints/KEY_RENDERING, RenderingHints/VALUE_RENDER_QUALITY)
                               (.setRenderingHint RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
-                              (.drawImage image, 0, 0, width, height, nil)
+                              (.drawImage image, 0, 0, width, height, crop-x, crop-y, (.getWidth image), (.getHeight image), nil)
                               (.dispose))
                             scaled-image))
 
@@ -48,6 +53,6 @@
 (defn pdf-to-image-input-stream [pdf-input]
   (let [content (ByteArrayOutputStream.)]
     (pdf-to-image-output pdf-input content)
-  (ByteArrayInputStream. (.toByteArray content))))
+    (ByteArrayInputStream. (.toByteArray content))))
 
 ;;(io/copy (pdf-to-image-input-stream "/home/michaelho/ws/lupapalvelu/problematic-pdfs/Yhdistelmakartta_asema-johto_Oksapolku-Pihkakatu.pdf") (FileOutputStream. "/tmp/a1.jpg"))
