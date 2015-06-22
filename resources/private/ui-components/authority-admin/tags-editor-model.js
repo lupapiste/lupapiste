48b0644241e1c2feba39c91fd587a07a44f5ce82
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-06-22 15:58:27 +0300

    adding tags

--- resources/private/ui-components/authority-admin/tags-editor-model.js
+++ resources/private/ui-components/authority-admin/tags-editor-model.js
@@ -13,6 +13,9 @@ LUPAPISTE.TagsEditorModel = function(params) {
   self.indicator = ko.observable().extend({notify: "always"});
 
   self.save = _.debounce(function() {
+    self.tags.remove(function(item) {
+      return _.isEmpty(ko.unwrap(item.label));
+    });
     var tags = _.map(self.tags(), function(t) { return ko.unwrap(t.label); });
     ajax
       .command("save-organization-tags", {tags: tags})
@@ -42,19 +45,25 @@ LUPAPISTE.TagsEditorModel = function(params) {
     })
     .call();
 
+  self.addTag = function() {
+    var model = new TagModel();
+    model.edit(true);
+    self.tags.push(model);
+  };
+
   self.removeTag = function(item) {
     item.edit(false);
     self.tags.remove(item);
-  }
+  };
 
   self.editTag = function(item) {
     item.edit(true);
-  }
+  };
 
   self.onKeypress = function(item, event) {
     if (event.keyCode === 13) {
       item.edit(false);
     }
     return true;
-  }
+  };
 };
