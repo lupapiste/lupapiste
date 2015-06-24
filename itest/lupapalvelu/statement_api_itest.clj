9357e842ba9570be97c45eef9256132d71d71c62
Jari Asp <jari.asp@solita.fi>
2015-06-24 19:10:56 +0300

    testing a statement cannot be given with invalid status value

--- itest/lupapalvelu/statement_api_itest.clj
+++ itest/lupapalvelu/statement_api_itest.clj
@@ -96,6 +96,9 @@
         (fact "Applicant can not see unsubmitted statements"
           (query mikko :should-see-unsubmitted-statements :id application-id) => unauthorized?)
 
+        (fact "Statement cannot be given with invalid status"
+          (command veikko :give-statement :id application-id :statementId (:id statement) :status "yes" :text "I will approve" :lang "fi") => (partial expected-failure? "error.missing-parameters"))
+
         (get-in statement [:person :email]) => veikko-email
         (command veikko :give-statement :id application-id :statementId (:id statement) :status "puoltaa" :text "I will approve" :lang "fi") => ok?
 
