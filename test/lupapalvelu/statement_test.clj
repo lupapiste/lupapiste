ae1e3456c91330cfdc4b372cc15d87bfb97f5d76
Jari Asp <jari.asp@solita.fi>
2015-06-25 12:46:24 +0300

    tests for the function "possible-statement-statuses"

--- test/lupapalvelu/statement_test.clj
+++ test/lupapalvelu/statement_test.clj
@@ -1,9 +1,10 @@
 (ns lupapalvelu.statement-test
   (:require [midje.sweet :refer :all]
             [midje.util :refer [testable-privates]]
+            [lupapalvelu.organization :as organization]
             [lupapalvelu.statement]))
 
-(testable-privates lupapalvelu.statement version-is-greater-or-equal)
+(testable-privates lupapalvelu.statement version-is-greater-or-equal possible-statement-statuses)
 
 (facts "version-is-greater-or-equal"
   (fact "source (evaluated) version"
@@ -15,3 +16,19 @@
     (version-is-greater-or-equal 2.1     {:major 2 :minor 1 :micro 5}) => (throws AssertionError)
     (version-is-greater-or-equal "2.1.4" "2.1.5")                      => (throws AssertionError)
     (version-is-greater-or-equal "2.1.4" {:major 2 :minor 1})          => (throws AssertionError)))
+
+
+(let [test-app {:municipality 753 :permitType "R"}]
+  (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.3"
+   (possible-statement-statuses test-app) => (contains ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+   (provided
+     (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.5"}}}))
+
+  (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.5"
+    (possible-statement-statuses test-app) => (contains ["puoltaa" "ei-puolla" "ehdoilla"
+                                                         "ei-huomautettavaa" "ehdollinen" "puollettu"
+                                                         "ei-puollettu" "ei-lausuntoa" "lausunto"
+                                                         "kielteinen" "palautettu" "poydalle"] :in-any-order)
+    (provided
+      (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.6"}}})))
+
