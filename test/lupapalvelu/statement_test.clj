983585d79e35091b15883062ae2915e1ae043a5d
Jari Asp <jari.asp@solita.fi>
2015-06-30 13:34:55 +0300

    "just" is what we seek

--- test/lupapalvelu/statement_test.clj
+++ test/lupapalvelu/statement_test.clj
@@ -20,15 +20,15 @@
 
 (let [test-app {:municipality 753 :permitType "R"}]
   (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.3"
-   (possible-statement-statuses test-app) => (contains ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+   (possible-statement-statuses test-app) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
    (provided
      (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.5"}}}))
 
   (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.5"
-    (possible-statement-statuses test-app) => (contains ["puoltaa" "ei-puolla" "ehdoilla"
-                                                         "ei-huomautettavaa" "ehdollinen" "puollettu"
-                                                         "ei-puollettu" "ei-lausuntoa" "lausunto"
-                                                         "kielteinen" "palautettu" "poydalle"] :in-any-order)
+    (possible-statement-statuses test-app) => (just ["puoltaa" "ei-puolla" "ehdoilla"
+                                                     "ei-huomautettavaa" "ehdollinen" "puollettu"
+                                                     "ei-puollettu" "ei-lausuntoa" "lausunto"
+                                                     "kielteinen" "palautettu" "poydalle"] :in-any-order)
     (provided
       (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.6"}}})))
 
