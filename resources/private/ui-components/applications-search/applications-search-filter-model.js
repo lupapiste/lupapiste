38f88eddb9bd6f0d707a7a632e786e3e61c653c4
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-07-02 16:51:26 +0300

    init data provider for 'handlers' aka users of organization

--- resources/private/ui-components/applications-search/applications-search-filter-model.js
+++ resources/private/ui-components/applications-search/applications-search-filter-model.js
@@ -1,3 +1,32 @@
+LUPAPISTE.HandlersDataProvider = function() {
+  "use strict";
+
+  var self = this;
+
+  self.query = ko.observable();
+
+  var data = ko.observable();
+
+  ajax
+    .query("users-in-same-organizations")
+    .error(_.noop)
+    .success(function(data) {
+      data(data.users);
+    })
+    .call();
+
+  self.data = ko.pureComputed(function() {
+    var q = self.query() || "";
+    return _.filter(data, function(item) {
+      return _.reduce(q.split(" "), function(result, word) {
+        return _.contains(item.toUpperCase(), word.toUpperCase()) && result;
+      }, true);
+    });
+  });
+
+};
+
+
 LUPAPISTE.ApplicationsSearchFilterModel = function(params) {
   "use strict";
 
