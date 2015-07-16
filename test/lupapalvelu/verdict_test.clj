b6d3967be33abaeccf7322aa76dc989e12d513c6
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-07-16 15:53:47 +0300

    start testing tj/suunnittelija verdicts

--- test/lupapalvelu/verdict_test.clj
+++ test/lupapalvelu/verdict_test.clj
@@ -1,8 +1,11 @@
 (ns lupapalvelu.verdict-test
   (require [midje.sweet :refer :all]
            [midje.util :refer [testable-privates]]
+           [lupapalvelu.application-meta-fields :as meta-fields]
+           [lupapalvelu.xml.krysp.application-from-krysp :as krysp-fetch]
            [lupapalvelu.verdict :refer :all]
            [lupapalvelu.permit :as permit]
+           [lupapalvelu.organization :as organization]
            [sade.core :refer [now]]))
 
 (testable-privates lupapalvelu.verdict get-verdicts-with-attachments)
@@ -12,3 +15,20 @@
     (fact "No verdicts found in the attachment parsing phase"
       (count (get-verdicts-with-attachments {:permitType "R"} {} (now) xml (permit/get-verdict-reader "R"))) => 0
       )))
+
+(def tj-app {:id "2"
+             :municipality "753"
+             :permitType "R"
+             :primaryOperation {:name "tyonjohtajan-nimeaminen-v2"}
+             :linkPermitData [{:id "1", :type "lupapistetunnus", :operation "kerrostalo-rivitalo"}]})
+
+(def link-app {:id "1"
+               :municipality "753"
+               :permitType "R"
+               :primaryOperation {:name "kerrostalo-rivitalo"}})
+
+(facts "Tyonjohtaja and suunnittelijan nimeaminen tests"
+  (provided
+    (organization/resolve-organization "753" "R") => {:krysp {:R {:version "2.1.8"}}}
+    (meta-fields/enrich-with-link-permit-data ..something..) => tj-app
+    (krysp-fetch/get-application-xml link-app :application-id) => (sade.xml/parse (slurp "krysp/sample/verdict - 2.1.8 - Tekla.xml"))))
