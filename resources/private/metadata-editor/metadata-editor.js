13332bf1836b42d1c0a8c871a0ff8236b6dd5b3a
Markus Penttila <markus.penttila@solita.fi>
2015-07-21 15:49:34 +0300

    Metadata editor now works for retention subtable

--- resources/private/metadata-editor/metadata-editor.js
+++ resources/private/metadata-editor/metadata-editor.js
@@ -1,37 +1,16 @@
 (function() {
   "use strict";
 
-  var makeSubObjectObservable = function(data) {
-    return _.object(_.map(data, function(value, key) {
-      if (_.isObject(value)) {
-        value = ko.observable(value);
-      }
-      return [key, value];
-    }));
-  };
-
   var model = function(params) {
     var self = this;
     self.attachmentId = params.attachmentId;
     self.metadata = params.metadata();
     self.editable = ko.observable(false);
-    self.editedMetadata = ko.observable(makeSubObjectObservable(self.metadata));
+    self.editedMetadata = ko.observable(ko.mapping.fromJS(self.metadata));
     self.schema = ko.observableArray();
     // It does not seem to be possible to use visible: !editable, so we need another observable for that.
     self.displayOnly = ko.pureComputed(function() {
       return !self.editable();
-    });
-    self.dependencies = ko.pureComputed(function() {
-      var depMap = {};
-      _.foreach(self.schema(), function(item) {
-        if (item.dependencies) {
-          depMap[item.type] = {};
-          _.foreach(item.dependencies, function(val, key) {
-            depMap[item.type][key] = val;
-          });
-        }
-      });
-      return depMap;
     });
 
     ajax.query('tos-metadata-schema')
@@ -45,14 +24,12 @@
     };
 
     self.cancelEdit = function() {
-      self.editedMetadata(makeSubObjectObservable(self.metadata));
+      self.editedMetadata(ko.mapping.fromJS(self.metadata));
       self.editable(false);
     };
 
     self.save = function() {
       self.metadata = ko.mapping.toJS(self.editedMetadata);
-      // The bindings in view-only mode won't update if editedMetadata is not set here again. Why?
-      self.editedMetadata(makeSubObjectObservable(self.metadata));
       self.editable(false);
     }
   };
