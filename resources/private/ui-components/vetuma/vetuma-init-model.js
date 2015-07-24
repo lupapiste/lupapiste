c3c1fd69ed29b3be356f5c023ce3a0d9c445a46c
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-07-24 15:03:33 +0300

    Generate link with params

--- resources/private/ui-components/vetuma/vetuma-init-model.js
+++ resources/private/ui-components/vetuma/vetuma-init-model.js
@@ -2,8 +2,15 @@ LUPAPISTE.VetumaInitModel = function(params) {
   "use strict";
 
   var self = this;
+  var VETUMA_PARAMS = ["success", "error", "cancel"];
+  var VETUMA_BASE = "/api/vetuma?";
 
+  function appendParam(url, param) {
+    return url + param + "=" + encodeURIComponent(ko.unwrap(params.vetuma[param])) +  "&";
+  }
+
+  self.id = _.isUndefined(params.id) ?  "vetuma-init" : params.id;
   self.visible = _.isUndefined(params.visible) ?  true : params.visible;
-  self.href = "";
+  self.href = ko.pureComputed(function() {return _.reduce(VETUMA_PARAMS, appendParam, VETUMA_BASE);});
 
 };
