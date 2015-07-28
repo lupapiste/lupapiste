b52fe006df9d2048e1d60ac613a98c8e605316fc
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-07-28 12:36:01 +0300

    Do not fallback to default error handler if success handler returs something

--- resources/private/common/ajax.js
+++ resources/private/common/ajax.js
@@ -39,10 +39,13 @@ var ajax = (function($) {
       rawData:   false,
       complete:  self.onComplete,
       success: function(e) {
-        var handler = (self.rawData || e.ok) ? self.successHandler : resolveErrorHandler(e);
-        var res = handler.call(self.savedThis, e);
-        if ( res && !res.ok ) {
-          defaultError(e);
+        if (self.rawData || e.ok) {
+          self.successHandler.call(self.savedThis, e);
+        } else {
+          var res = resolveErrorHandler(e).call(self.savedThis, e);
+          if (res && res.ok === false) {
+            defaultError(e);
+          }
         }
       },
       error: function(jqXHR, textStatus, errorThrown) {
