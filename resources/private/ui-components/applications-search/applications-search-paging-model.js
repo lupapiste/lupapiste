493c96dbe1b3c27e4ae603a90efac5154ef83525
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-07-30 15:59:05 +0300

    use computed values to show end and start values of the current paging

--- resources/private/ui-components/applications-search/applications-search-paging-model.js
+++ resources/private/ui-components/applications-search/applications-search-paging-model.js
@@ -18,4 +18,13 @@ LUPAPISTE.ApplicationsSearchPagingModel = function(params) {
 
   self.next = _.partial(self.paging, 1);
   self.prev = _.partial(self.paging, -1);
+
+  self.start = ko.pureComputed(function() {
+    return self.dataProvider.skip() + 1;
+  });
+
+  self.end = ko.pureComputed(function() {
+    var endValue = self.dataProvider.skip() + self.dataProvider.limit();
+    return self.count() < endValue ? self.count() : endValue;
+  });
 };
