2188c24dba7c568fb7265c3ee480d6fbef6cbec9
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-07-31 17:22:26 +0300

    Set eastwood version in project.clj

--- project.clj
+++ project.clj
@@ -69,6 +69,7 @@
                              [lein-buildid "0.2.0"]
                              [lein-nitpicker "0.4.0"]
                              [lein-hgnotes "0.2.0-SNAPSHOT"]
+                             [jonase/eastwood "0.2.1" :exclusions [[org.clojure/tools.namespace] org.clojure/clojure]]
                              [lein-scss-compiler "0.1.4"]]
                    :resource-paths ["dev-resources"]
                    :source-paths ["dev-src" "test-utils"]
