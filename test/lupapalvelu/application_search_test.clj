81db0822c0ed6fad02cc62790fff4e407a7fb8b1
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-08-21 10:57:59 +0300

    testing polygon query

--- test/lupapalvelu/application_search_test.clj
+++ test/lupapalvelu/application_search_test.clj
@@ -1,10 +1,11 @@
 (ns lupapalvelu.application-search-test
    (:require [midje.sweet :refer :all]
              [midje.util :refer [testable-privates]]
+             [monger.operators :refer [$in]]
              [lupapalvelu.test-util :refer :all]
              [lupapalvelu.application-search :refer :all]))
 
-(testable-privates lupapalvelu.application-search make-sort operation-names)
+(testable-privates lupapalvelu.application-search make-sort operation-names make-area-query)
 
 (facts "operation-names"
   (operation-names "bil") => ["auto-katos"]
@@ -42,3 +43,101 @@
 
 (fact "Tags are present in query"
   (-> (make-query {} {:applicationTags ["test1" "test2"]} {}) (get "$and") last :tags) => {"$in" ["test1" "test2"]})
+
+
+(def multi-feature-simple {:id "multi-simple",
+                           :properties {:nimi "simple multi"},
+                           :type "Feature"
+                           :geometry
+                           {:type "MultiPolygon"
+                            :coordinates
+                            [[[[402644.2941 6693912.6002]
+                               [401799.0131 6696356.5649]
+                               [406135.6722 6695272.4001]
+                               [406245.9263 6693673.7164]
+                               [404059.221 6693545.0867]
+                               [404059.221 6693545.0867]
+                               [402644.2941 6693912.6002]]]]}})
+
+(def multi-feature {:id "multi-polygon",
+                    :properties {:nimi "multi polygon"},
+                    :type "Feature"
+                    :geometry
+                    {:type "MultiPolygon"
+                     :coordinates
+                     [[[[402644.2941 6693912.6002]
+                        [401799.0131 6696356.5649]
+                        [406135.6722 6695272.4001]
+                        [406245.9263 6693673.7164]
+                        [404059.221 6693545.0867]
+                        [404059.221 6693545.0867]
+                        [402644.2941 6693912.6002]]]
+                      [[[409884.3098 6694316.865]
+                        [413394.0636 6688105.8871]
+                        [410894.9719 6687076.8493]
+                        [409094.1558 6685367.9116]
+                        [407421.9694 6683989.736]
+                        [403985.7183 6689300.3059]
+                        [404114.348 6693140.8219]
+                        [409884.3098 6694316.865]]]]}})
+
+(def polygon-feature {:id "polygon",
+                      :properties {:nimi "Test"},
+                      :type "Feature"
+                      :geometry
+                      {:type "Polygon"
+                       :coordinates
+                       [[[402644.2941 6693912.6002]
+                         [401799.0131 6696356.5649]
+                         [406135.6722 6695272.4001]
+                         [406245.9263 6693673.7164]
+                         [404059.221 6693545.0867]
+                         [404059.221 6693545.0867]
+                         [402644.2941 6693912.6002]]]}})
+
+(fact "Multimethod for features' MultiPolygon/Polygon coordinates"
+  (fact "Returns Polygon from MultiPolygon when only one Polygon is present"
+    (resolve-coordinates multi-feature-simple) => (resolve-coordinates polygon-feature))
+  (fact "For , returns two Polygons"
+    (count (resolve-coordinates multi-feature)) => 2))
+
+(facts "Area query is in correct form"
+  (make-area-query ["polygon" "multi-polygon"] {:orgAuthz {:753-R #{:authority}}}) =>
+  {"$or" ; two polygons are extracted from MultiPolygon
+   [{:location
+     {"$geoWithin"
+      {"$polygon"
+       [[402644.2941 6693912.6002]
+        [401799.0131 6696356.5649]
+        [406135.6722 6695272.4001]
+        [406245.9263 6693673.7164]
+        [404059.221 6693545.0867]
+        [404059.221 6693545.0867]
+        [402644.2941 6693912.6002]]}}}
+    {:location
+     {"$geoWithin"
+      {"$polygon"
+       [[409884.3098 6694316.865]
+        [413394.0636 6688105.8871]
+        [410894.9719 6687076.8493]
+        [409094.1558 6685367.9116]
+        [407421.9694 6683989.736]
+        [403985.7183 6689300.3059]
+        [404114.348 6693140.8219]
+        [409884.3098 6694316.865]]}}}
+    {:location
+     {"$geoWithin"
+      {"$polygon"
+       [[402644.2941 6693912.6002]
+        [401799.0131 6696356.5649]
+        [406135.6722 6695272.4001]
+        [406245.9263 6693673.7164]
+        [404059.221 6693545.0867]
+        [404059.221 6693545.0867]
+        [402644.2941 6693912.6002]]}}}]}
+  (provided
+    (lupapalvelu.mongo/select
+      :organizations
+      {:_id {$in #{"753-R"}} :areas.features.id {$in ["polygon" "multi-polygon"]}} [:areas]) => [{:id "753-R"
+                                                                                                 :areas {:type "FeatureCollection"
+                                                                                                         :features [multi-feature polygon-feature]}}]))
