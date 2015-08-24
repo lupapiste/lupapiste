f307c12a22f7b186d08c9b314a93c4ce32d0903a
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-08-24 09:56:09 +0300

    Initial version of rotation with pdftk

--- src/lupapalvelu/pdftk.clj
+++ src/lupapalvelu/pdftk.clj
@@ -1,12 +1,17 @@
 (ns lupapalvelu.pdftk
-  (:require [taoensso.timbre :as timbre :refer [trace debug debugf info warn error fatal]]
-            [clojure.string :as s]
-            [clojure.java.io :as io]
+  (:require [taoensso.timbre :as timbre :refer [debug info warn error]]
+            ;[clojure.string :as s]
+            ;[clojure.java.io :as io]
             [clojure.java.shell :as shell]))
 
+(defn- handle-result [result]
+  ; POSIX return code 0 signals success, others are failure codes
+  (when-not (zero? (:exit result))
+    (throw (RuntimeException. (str "pdftk returned " (:exit result) ", STDOUT: " (str (:out result) ", STDERR: " (:err result)))))))
 
 (defn create-pdftk-file [in file-name]
-  (let [result (shell/sh "pdftk" "-" "output" file-name :in in)]
-    ; POSIX return code 0 signals success, others are failure codes
-    (when-not (zero? (:exit result))
-      (throw (RuntimeException. (str "pdftk returned " (:exit result) ", STDOUT: " (str (:out result) ", STDERR: " (:err result))))))))
+  (handle-result (shell/sh "pdftk" "-" "output" file-name :in in)))
+
+(defn rotate-pdf [in file-name rotation]
+  (let [rot (case rotation -90 "left", 90 "right", 180 "down")]
+    (handle-result (shell/sh "pdftk" "-" "cat" (str "1-end" rot) "output" file-name :in in))))
