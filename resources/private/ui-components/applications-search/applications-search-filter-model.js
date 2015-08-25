a527dc109baa243dc9bc514b0f7ecb231c7e2159
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-08-25 17:35:23 +0300

    grouping for areas if authority in many organizations

--- resources/private/ui-components/applications-search/applications-search-filter-model.js
+++ resources/private/ui-components/applications-search/applications-search-filter-model.js
@@ -127,32 +127,49 @@ LUPAPISTE.HandlersDataProvider = function() {
   });
 };
 
-LUPAPISTE.AreasDataProvider = function() {
+LUPAPISTE.AreasDataProvider = function(filtered) {
   "use strict";
 
   var self = this;
 
   self.query = ko.observable();
+  self.filtered = filtered || ko.observableArray([]);
 
-  var areas = ko.observable();
+  var orgsAreas = ko.observable();
 
   ajax
     .query("get-organization-areas")
     .error(_.noop)
     .success(function(res) {
-      // TODO for now fetch features from the first organization only
-      var firstOrganizationId = _.first(_.keys(res.areas));
-      var features = util.getIn(res, ["areas", firstOrganizationId, "areas", "features"]);
-      areas(_.map(features, function(f) {
-        var nimi = util.getIn(f, ["properties", "nimi"]);
-        var id = f.id;
-        return {name: nimi, id: id};
-      }));
+      orgsAreas(res.areas);
     })
     .call();
 
   self.data = ko.computed(function() {
-    return areas();
+    var result = [];
+
+    _.forEach(orgsAreas(), function(org) {
+      if (org.areas && org.areas.features) {
+        var header = {label: org.name[loc.currentLanguage], groupHeader: true};
+
+        var features = _.map(org.areas.features, function(feature) {
+          var nimi = util.getIn(feature, ["properties", "nimi"]);
+          var id = feature.id;
+          return {label: nimi, id: id};
+        });
+
+        var filteredData = util.filterDataByQuery(features, self.query() || "", self.filtered());
+        // append group header and group items to result data
+        if (filteredData.length > 0) {
+          if (_.keys(orgsAreas()).length > 1) {
+            result = result.concat(header);
+          }
+          result = result.concat(filteredData);
+        }
+      }
+    });
+
+    return result;
   });
 };
 
@@ -212,6 +229,6 @@ LUPAPISTE.ApplicationsSearchFilterModel = function(params) {
     self.operationsDataProvider = new LUPAPISTE.OperationsDataProvider(self.dataProvider.applicationOperations);
     self.organizationsFilterDataProvider = new LUPAPISTE.OrganizationsFilterDataProvider(self.dataProvider.applicationOrganizations);
     self.organizationTagsDataProvider = new LUPAPISTE.OrganizationTagsDataProvider(self.dataProvider.applicationTags);
-    self.organizationAreasDataProvider = new LUPAPISTE.AreasDataProvider();
+    self.organizationAreasDataProvider = new LUPAPISTE.AreasDataProvider(self.dataProvider.areas);
   }
 };
