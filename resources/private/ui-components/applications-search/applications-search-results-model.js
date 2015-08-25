d761d74a0937808eacd7507fb52cf7655d14ad2d
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-08-25 13:54:45 +0300

    reset page skip when limit, tab or sort order is changed

--- resources/private/ui-components/applications-search/applications-search-results-model.js
+++ resources/private/ui-components/applications-search/applications-search-results-model.js
@@ -18,9 +18,11 @@ LUPAPISTE.ApplicationsSearchResultsModel = function(params) {
   self.selectTab = function(item) {
     hub.send("track-click", {category:"Applications", label: item, event:"radioTab"});
     self.selectedTab(item);
+    self.dataProvider.skip(0);
   };
 
   self.sortBy = function(target) {
+    self.dataProvider.skip(0);
     var sortObj = self.dataProvider.sort;
     if ( target === sortObj.field() ) {
       sortObj.asc(!sortObj.asc()); // toggle direction
