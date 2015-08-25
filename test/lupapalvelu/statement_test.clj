b8be4409d31ae5c0e351a5602176442bc7b50085
Jari Asp <jari.asp@solita.fi>
2015-08-25 16:40:22 +0300

    Fixed LPK-632: additional puoltotieto selection values only available for the permit types R and P.

--- test/lupapalvelu/statement_test.clj
+++ test/lupapalvelu/statement_test.clj
@@ -6,17 +6,48 @@
 
 (testable-privates lupapalvelu.statement possible-statement-statuses)
 
-(let [test-app {:municipality 753 :permitType "R"}]
-  (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.3"
-   (possible-statement-statuses test-app) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+(let [test-app-R  {:municipality 753 :permitType "R"}
+      test-app-P  {:municipality 753 :permitType "P"}
+      test-app-YA {:municipality 753 :permitType "YA"}]
+
+  ;; permit type R
+
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.3"
+   (possible-statement-statuses test-app-R) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
    (provided
      (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.5"}}}))
 
-  (fact "get-possible-statement-statuses, krysp yhteiset version 2.1.5"
-    (possible-statement-statuses test-app) => (just ["puoltaa" "ei-puolla" "ehdoilla"
-                                                     "ei-huomautettavaa" "ehdollinen" "puollettu"
-                                                     "ei-puollettu" "ei-lausuntoa" "lausunto"
-                                                     "kielteinen" "palautettu" "poydalle"] :in-any-order)
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.5"
+    (possible-statement-statuses test-app-R) => (just ["puoltaa" "ei-puolla" "ehdoilla"
+                                                       "ei-huomautettavaa" "ehdollinen" "puollettu"
+                                                       "ei-puollettu" "ei-lausuntoa" "lausunto"
+                                                       "kielteinen" "palautettu" "poydalle"] :in-any-order)
     (provided
-      (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.6"}}})))
+      (organization/resolve-organization anything anything) => {:krysp {:R {:version "2.1.6"}}}))
 
+  ;; permit type P
+
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.3"
+   (possible-statement-statuses test-app-P) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+   (provided
+     (organization/resolve-organization anything anything) => {:krysp {:P {:version "2.1.5"}}}))
+
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.5"
+    (possible-statement-statuses test-app-P) => (just ["puoltaa" "ei-puolla" "ehdoilla"
+                                                       "ei-huomautettavaa" "ehdollinen" "puollettu"
+                                                       "ei-puollettu" "ei-lausuntoa" "lausunto"
+                                                       "kielteinen" "palautettu" "poydalle"] :in-any-order)
+    (provided
+      (organization/resolve-organization anything anything) => {:krysp {:P {:version "2.2.0"}}}))
+
+  ;; permit type YA
+
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.3"
+   (possible-statement-statuses test-app-YA) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+   (provided
+     (organization/resolve-organization anything anything) => {:krysp {:YA {:version "2.1.3"}}}))
+
+  (fact "get-possible-statement-statuses, permit type R, krysp yhteiset version 2.1.5"
+    (possible-statement-statuses test-app-YA) => (just ["puoltaa" "ei-puolla" "ehdoilla"] :in-any-order)
+    (provided
+      (organization/resolve-organization anything anything) => {:krysp {:YA {:version "2.2.0"}}})))
