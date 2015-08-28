3cb662a40b860fa9846da5da1119fe0972a4d721
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-08-28 15:57:47 +0300

    indicator component

---
+++ resources/private/ui-components/indicator/indicator-model.js
@@ -0,0 +1,27 @@
+LUPAPISTE.IndicatorModel = function(params) {
+  "use strict";
+  var self = this;
+
+  self.showIndicator = ko.observable().extend({notify: "always"});
+  self.indicatorStyle = ko.observable("neutral");
+  self.iconStyle = ko.observable("");
+  self.message = ko.observable("");
+
+  hub.subscribe("indicator", function(e) {
+    if (e.style === "positive") {
+      self.iconStyle("lupicon-circle-check");
+      self.message("form.saved");
+    }
+    else if (e.style === "negative") {
+      self.iconStyle("lupicon-circle-attention");
+      self.message("form.err");
+    }
+
+    self.indicatorStyle(e.style);
+    self.showIndicator(true);
+    _.delay(function() {
+      self.showIndicator(false);
+    }, 1000);
+
+  });
+};
