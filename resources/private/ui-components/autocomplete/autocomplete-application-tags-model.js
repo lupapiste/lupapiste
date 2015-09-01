d1d59747e8d02205953098f8037134d5cd8afe22
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-09-01 14:48:45 +0300

    sort application tags on autocomplete

--- resources/private/ui-components/autocomplete/autocomplete-application-tags-model.js
+++ resources/private/ui-components/autocomplete/autocomplete-application-tags-model.js
@@ -9,16 +9,15 @@ LUPAPISTE.AutocompleteApplicationTagsModel = function(params) {
   self.data = ko.pureComputed(function() {
     var organizationTags = lupapisteApp.services.organizationTagsService.currentApplicationOrganizationTags();
 
-    var filteredData = _.filter(organizationTags, function(tag) {
-      return !_.some(self.selected(), tag);
-    });
     var q = self.query() || "";
-    filteredData = _.filter(filteredData, function(tag) {
-      return _.reduce(q.split(" "), function(result, word) {
-        return tag.label && _.contains(tag.label.toUpperCase(), word.toUpperCase()) && result;
-      }, true);
-    });
-    return filteredData;
+    return _(organizationTags)
+      .filter(function(tag) {
+        return _.reduce(q.split(" "), function(result, word) {
+          return !_.some(self.selected(), tag) && tag.label && _.contains(tag.label.toUpperCase(), word.toUpperCase()) && result;
+        }, true);
+      })
+      .sortBy("label")
+      .value();
   });
 
 };
