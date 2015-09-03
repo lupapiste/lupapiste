028ddb5dbfd95822b09f7a1a8269febdea24ef25
Jari Asp <jari.asp@solita.fi>
2015-09-03 18:37:17 +0300

    to the defvalidator :rakennusvalitsin-muu, added the schemas that include rakennusvalitsin

--- src/lupapalvelu/document/document_field_validators.clj
+++ src/lupapalvelu/document/document_field_validators.clj
@@ -2,10 +2,13 @@
   (:require [clojure.string :as s]
             [lupapalvelu.document.validator :refer :all]))
 
-
 (defvalidator :rakennusvalitsin-muu
   {:doc "Jos rakennusvalitsimesta on valittu vaihtoehto Muu, on Manuaalinen rakennusnumero -kenttaan syotettava arvo"
-   :schemas ["rakennuksen-muuttaminen"]
+   :schemas ["rakennuksen-muuttaminen"
+             "rakennuksen-muuttaminen-ei-huoneistoja"
+             "rakennuksen-muuttaminen-ei-huoneistoja-ei-ominaisuuksia"
+             "rakennuksen-laajentaminen"
+             "purkaminen"]
    :fields [buildingId [:buildingId]
             muu        [:manuaalinen_rakennusnro]]
    :facts {:ok   []
