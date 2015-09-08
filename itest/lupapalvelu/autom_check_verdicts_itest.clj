a0cd8e9afdd3da7502e71025ec4e48f1566c9ab2
Antti Ramo <antti.ramo@solita.fi>
2015-09-08 15:27:58 +0300

    add with-db wrappers to autom_check_verdicts_itest and migration.core_itest

--- itest/lupapalvelu/autom_check_verdicts_itest.clj
+++ itest/lupapalvelu/autom_check_verdicts_itest.clj
@@ -2,59 +2,68 @@
   (:require [midje.sweet :refer :all]
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.factlet :refer [fact* facts*]]
+            [sade.core :refer [now]]
             [sade.dummy-email-server :as dummy-email-server]
             [lupapalvelu.mongo :as mongo]
             [lupapalvelu.integrations-api]
             [lupapalvelu.verdict-api]
             [lupapalvelu.fixture.minimal :as minimal]
+            [lupapalvelu.fixture.core :as fixture]
             [lupapalvelu.batchrun :as batchrun]))
 
+(def local-db-name (str "test_autom_check_verdicts_itest_" (now)))
+
 (mongo/connect!)
-(mongo/remove-many :organizations {})
-(mongo/remove-many :applications {})
+(mongo/with-db local-db-name (fixture/apply-fixture "minimal"))
 
-(let [krysp-url (str (server-address) "/dev/krysp")
-      organizations (map (fn [org] (update-in org [:krysp] #(assoc-in % [:R :url] krysp-url))) minimal/organizations)]
-  (dorun (map (partial mongo/insert :organizations) organizations)))
+(mongo/with-db local-db-name 
+  (mongo/remove-many :organizations {})
+  (mongo/remove-many :applications {}))
 
-(facts "Automatic checking for verdicts"
- (let [application-submitted         (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 17")
-       application-id-submitted      (:id application-submitted)
-       application-sent              (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 18")
-       application-id-sent           (:id application-sent)
-       application-verdict-given     (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 19")
-       application-id-verdict-given  (:id application-verdict-given)]
+(mongo/with-db local-db-name 
+  (let [krysp-url (str (server-address) "/dev/krysp")
+        organizations (map (fn [org] (update-in org [:krysp] #(assoc-in % [:R :url] krysp-url))) minimal/organizations)]
+    (dorun (map (partial mongo/insert :organizations) organizations))))
 
-   (local-command sonja :approve-application :id application-id-sent :lang "fi") => ok?
-   (local-command sonja :approve-application :id application-id-verdict-given :lang "fi") => ok?
-   (give-local-verdict sonja application-id-verdict-given :verdictId "aaa" :status 42 :name "Paatoksen antaja" :given 123 :official 124) => ok?
+(mongo/with-db local-db-name 
+  (facts "Automatic checking for verdicts"
+    (let [application-submitted         (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 17")
+          application-id-submitted      (:id application-submitted)
+          application-sent              (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 18")
+          application-id-sent           (:id application-sent)
+          application-verdict-given     (create-and-submit-local-application sonja :propertyId sipoo-property-id :address "Paatoskuja 19")
+          application-id-verdict-given  (:id application-verdict-given)]
 
-   (let [application-submitted (query-application local-query sonja application-id-submitted) => truthy
-         application-sent (query-application local-query sonja application-id-sent) => truthy
-         application-verdict-given (query-application local-query sonja application-id-verdict-given) => truthy]
-     (:state application-submitted) => "submitted"
-     (:state application-sent) => "sent"
-     (:state application-verdict-given) => "verdictGiven")
+      (local-command sonja :approve-application :id application-id-sent :lang "fi") => ok?
+      (local-command sonja :approve-application :id application-id-verdict-given :lang "fi") => ok?
+      (give-local-verdict sonja application-id-verdict-given :verdictId "aaa" :status 42 :name "Paatoksen antaja" :given 123 :official 124) => ok?
 
-   (dummy-email-server/messages :reset true) ; Inbox zero
+      (let [application-submitted (query-application local-query sonja application-id-submitted) => truthy
+            application-sent (query-application local-query sonja application-id-sent) => truthy
+            application-verdict-given (query-application local-query sonja application-id-verdict-given) => truthy]
+        (:state application-submitted) => "submitted"
+        (:state application-sent) => "sent"
+        (:state application-verdict-given) => "verdictGiven")
 
-   (fact "checking verdicts and sending emails to the authorities related to the applications"
-     (count (batchrun/fetch-verdicts)) => pos?)
+      (dummy-email-server/messages :reset true) ; Inbox zero
 
-   (fact "Verifying the sent emails"
-     (Thread/sleep 100) ; batchrun includes a parallel operation
-     (let [emails (dummy-email-server/messages :reset true)]
-       (fact "email count" (count emails) => 1)
-       (let [email (last emails)]
-         (fact "email check"
-           (:to email) => (contains (email-for-key sonja))
-           (:subject email) => "Lupapiste.fi: Paatoskuja 18 - p\u00e4\u00e4t\u00f6s"
-           email => (partial contains-application-link-with-tab? application-id-sent "verdict" "authority")
-           (get-in email [:body :plain]) => (contains "Hakemukseesi on annettu p\u00e4\u00e4t\u00f6s")))))
+      (fact "checking verdicts and sending emails to the authorities related to the applications"
+        (count (batchrun/fetch-verdicts)) => pos?)
 
-   (let [application-submitted (query-application local-query sonja application-id-submitted) => truthy
-         application-sent (query-application local-query sonja application-id-sent) => truthy
-         application-verdict-given (query-application local-query sonja application-id-verdict-given) => truthy]
-     (:state application-submitted) => "submitted"
-     (:state application-sent) => "verdictGiven"
-     (:state application-verdict-given) => "verdictGiven")))
+      (fact "Verifying the sent emails"
+        (Thread/sleep 100) ; batchrun includes a parallel operation
+        (let [emails (dummy-email-server/messages :reset true)]
+          (fact "email count" (count emails) => 1)
+          (let [email (last emails)]
+            (fact "email check"
+              (:to email) => (contains (email-for-key sonja))
+              (:subject email) => "Lupapiste.fi: Paatoskuja 18 - p\u00e4\u00e4t\u00f6s"
+              email => (partial contains-application-link-with-tab? application-id-sent "verdict" "authority")
+              (get-in email [:body :plain]) => (contains "Hakemukseesi on annettu p\u00e4\u00e4t\u00f6s")))))
+
+      (let [application-submitted (query-application local-query sonja application-id-submitted) => truthy
+            application-sent (query-application local-query sonja application-id-sent) => truthy
+            application-verdict-given (query-application local-query sonja application-id-verdict-given) => truthy]
+        (:state application-submitted) => "submitted"
+        (:state application-sent) => "verdictGiven"
+        (:state application-verdict-given) => "verdictGiven"))))
