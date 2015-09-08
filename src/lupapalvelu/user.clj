a9a01dad28becbcaed8784ad3212d373b4fdfd0b
Antti Ramo <antti.ramo@solita.fi>
2015-09-08 10:46:38 +0300

    support for sort and handler filter in schema and command

--- src/lupapalvelu/user.clj
+++ src/lupapalvelu/user.clj
@@ -81,7 +81,10 @@
            (sc/optional-key :notification)        {:messageI18nkey sc/Str
                                                    :titleI18nkey   sc/Str}
            (sc/optional-key :applicationFilters)  [{(sc/optional-key :title) sc/Str
-                                                    :filter                  {(sc/optional-key :tags) (sc/pred vector? "Tag filter should have ids in a vector")
+                                                    :sort                    {:field     (sc/enum "type" "location" "operation" "applicant" "submitted" "modified" "state" "handler")
+                                                                              :asc       sc/Bool}
+                                                    :filter                  {(sc/optional-key :handler) sc/Str
+                                                                              (sc/optional-key :tags) (sc/pred vector? "Tag filter should have ids in a vector")
                                                                               (sc/optional-key :operations) (sc/pred vector? "Op filter should have ids in a vector")
                                                                               (sc/optional-key :organizations) (sc/pred vector? "Org filter should have ids in a vector")
                                                                               (sc/optional-key :areas) (sc/pred vector? "Area filter should have ids in a vector")}}]})
