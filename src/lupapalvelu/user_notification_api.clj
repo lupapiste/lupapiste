7e4ad6ac30cbcbab166450749bea56e8a6577f76
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-09-10 10:00:19 +0300

    update users with new notification message

--- src/lupapalvelu/user_notification_api.clj
+++ src/lupapalvelu/user_notification_api.clj
@@ -1,10 +1,21 @@
 (ns lupapalvelu.user-notification-api
   (:require [lupapalvelu.action :refer [defcommand defquery]]
+            [monger.operators :refer :all]
             [lupapalvelu.mongo :as mongo]
             [sade.core :refer [ok]]))
 
 (defcommand notifications-update
-  {:parameters [applicants authorities message]
+  {:parameters [applicants authorities title-fi message-fi]
    :user-roles #{:admin}}
   [_]
-  (prn "foo" applicants authorities message))
+  (let [to (->> []
+                (#(if applicants
+                   (conj % :applicant)
+                   %))
+                (#(if authorities
+                   (conj % :authority)
+                   %)))
+        notification {:title title-fi
+                      :message message-fi}]
+    (let [n (mongo/update-by-query :users {:role {$in to}} {$set {:notification notification}})]
+      (ok {:updates n}))))
