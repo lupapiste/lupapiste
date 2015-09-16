a7869f918b1f450526ce2e1d66488ab21c79d544
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-09-16 15:40:04 +0300

    check that only a pair of coordinates is given

--- src/lupapalvelu/geojson.clj
+++ src/lupapalvelu/geojson.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.geojson
-  (:require [sade.coordinate :as coord]))
+  (:require [sade.coordinate :as coord]
+            [sade.core :refer [fail]]))
 
 ;; Resolve polygons from GeoJSON Features
 (defmulti resolve-polygons (comp :type :geometry))
@@ -11,10 +12,15 @@
   (map (partial apply concat) (get-in feature [:geometry :coordinates])))
 
 
+(defn validate-polygon [polygon]
+  (or
+    (when-not (= (count polygon) 2) (fail :error.point-coordinate-wrong))
+    (coord/validate-coordinates polygon)))
+
 (defn validate-feature [feature]
   (reduce
     (fn [res polygon]
-      (or res (coord/validate-coordinates polygon)))
+      (or res (validate-polygon polygon)))
     nil
     (apply concat (resolve-polygons feature))))
 
