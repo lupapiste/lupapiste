66df217a394d540094903f2814ffb2bf8d5dd6ea
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-09-16 08:47:19 +0300

    save named filter

--- resources/private/ui-components/applications-search/applications-search-filters-list-model.js
+++ resources/private/ui-components/applications-search/applications-search-filters-list-model.js
@@ -2,10 +2,25 @@ LUPAPISTE.ApplicationsSearchFiltersListModel = function(params) {
   "use strict";
   var self = this;
 
+  var dataProvider = params.dataProvider || {};
+
   self.showSavedFilters = ko.observable(false);
   self.newFilterName = ko.observable();
 
   self.saveFilter = function() {
-    console.log("save");
+    var filter = {
+      handlers:      _.map(ko.unwrap(lupapisteApp.services.handlerFilterService.selected), "id"), //util.getIn(self.dataProvider, ["handler", "id"]),
+      tags:          _.map(ko.unwrap(lupapisteApp.services.tagFilterService.selected), "id"),
+      operations:    _.map(ko.unwrap(lupapisteApp.services.operationFilterService.selected), "id"),
+      organizations: _.map(ko.unwrap(lupapisteApp.services.organizationFilterService.selected), "id"),
+      areas:         _.map(ko.unwrap(lupapisteApp.services.areaFilterService.selected), "id")
+    };
+
+    ajax
+    .command("save-application-filter", {title: self.newFilterName(), filter: filter, sort: ko.toJS(dataProvider.sort)})
+    .error(util.showSavedIndicator)
+    .success(util.showSavedIndicator)
+    .call();
   };
+
 };
