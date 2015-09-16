c423196dacdf2000524dd9b2c1a81c86fd49c0aa
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-09-16 16:09:51 +0300

    show stored filter name, remove filter, set filter as default

--- resources/private/ui-components/applications-search/applications-search-filters-list-model.js
+++ resources/private/ui-components/applications-search/applications-search-filters-list-model.js
@@ -7,9 +7,35 @@ LUPAPISTE.ApplicationsSearchFiltersListModel = function(params) {
   self.showSavedFilters = ko.observable(false);
   self.newFilterName = ko.observable();
 
-  self.savedFilters = lupapisteApp.models.currentUser.applicationFilters;
+  self.savedFilters = ko.pureComputed(function() {
+    return _.map(lupapisteApp.models.currentUser.applicationFilters(), function (filter) {
+      filter.edit = ko.observable(false);
+      filter.removeFilter = function(filter) {
+        ajax
+        .command("remove-application-filter", {"filter-id": filter.id()})
+        .error(util.showSavedIndicator)
+        .success(function() {
+          hub.send("reload-current-user");
+        })
+        .call();
+      };
+      filter.defaultFilter = function(filter) {
+        ajax
+        .command("update-default-application-filter", {"filter-id": filter.id()})
+        .error(util.showSavedIndicator)
+        .success(function() {
+          hub.send("reload-current-user");
+        })
+        .call();
+      };
+      filter.isDefaultFilter = filter.id() === util.getIn(lupapisteApp.models.currentUser, ["defaultFilter", "id"]);
+      return filter;
+    });
+  });
 
   self.saveFilter = function() {
+    var title = self.newFilterName();
+
     var filter = {
       handlers:      _.map(ko.unwrap(lupapisteApp.services.handlerFilterService.selected), "id"), //util.getIn(self.dataProvider, ["handler", "id"]),
       tags:          _.map(ko.unwrap(lupapisteApp.services.tagFilterService.selected), "id"),
@@ -18,10 +44,14 @@ LUPAPISTE.ApplicationsSearchFiltersListModel = function(params) {
       areas:         _.map(ko.unwrap(lupapisteApp.services.areaFilterService.selected), "id")
     };
 
+    // TODO filter-id from selected filter
     ajax
-    .command("save-application-filter", {title: self.newFilterName(), filter: filter, sort: ko.toJS(dataProvider.sort), "filter-id": "69"})
+    .command("save-application-filter", {title: title, filter: filter, sort: ko.toJS(dataProvider.sort), "filter-id": filter.id})
     .error(util.showSavedIndicator)
-    .success(util.showSavedIndicator)
+    .success(function(res) {
+      util.showSavedIndicator(res);
+      hub.send("reload-current-user");
+    })
     .call();
   };
 
