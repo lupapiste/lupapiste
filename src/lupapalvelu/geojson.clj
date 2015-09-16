b58766e24d87c4ef086b771066147b0e64dc0cf4
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-09-16 15:38:22 +0300

    move validating to geojson namespace

--- src/lupapalvelu/geojson.clj
+++ src/lupapalvelu/geojson.clj
@@ -1,4 +1,5 @@
-(ns lupapalvelu.geojson)
+(ns lupapalvelu.geojson
+  (:require [sade.coordinate :as coord]))
 
 ;; Resolve polygons from GeoJSON Features
 (defmulti resolve-polygons (comp :type :geometry))
@@ -8,3 +9,15 @@
 
 (defmethod resolve-polygons "MultiPolygon" [feature]
   (map (partial apply concat) (get-in feature [:geometry :coordinates])))
+
+
+(defn validate-feature [feature]
+  (reduce
+    (fn [res polygon]
+      (or res (coord/validate-coordinates polygon)))
+    nil
+    (apply concat (resolve-polygons feature))))
+
+(defn validate-features [features]
+  (reduce (fn [res feature] (or res (validate-feature feature))) nil features))
+
