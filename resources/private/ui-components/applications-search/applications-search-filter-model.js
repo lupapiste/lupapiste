1cc8a06eb2ee042f097e801d3fe743b4d22aa2e0
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-09-25 10:08:12 +0300

    remove dead code

--- resources/private/ui-components/applications-search/applications-search-filter-model.js
+++ resources/private/ui-components/applications-search/applications-search-filter-model.js
@@ -6,17 +6,7 @@ LUPAPISTE.ApplicationsSearchFilterModel = function(params) {
 
   self.savedFilters = lupapisteApp.services.applicationFiltersService.savedFilters;
 
-  self.query = ko.observable();
-
   self.searchFieldSelected = ko.observable(false);
-
-  self.filterCount = ko.computed(function() {
-    return _.size(ko.unwrap(lupapisteApp.services.handlerFilterService.selected)) +
-           _.size(ko.unwrap(lupapisteApp.services.tagFilterService.selected)) +
-           _.size(ko.unwrap(lupapisteApp.services.operationFilterService.selected)) +
-           _.size(ko.unwrap(lupapisteApp.services.organizationFilterService.selected)) +
-           _.size(ko.unwrap(lupapisteApp.services.areaFilterService.selected));
-  });
 
   self.showAdvancedFilters = ko.observable(false);
 };
