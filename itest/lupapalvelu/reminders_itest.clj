3c3848c4a3b143465d6483423bc8e0b4ff1d24ff
Jari Asp <jari.asp@solita.fi>
2015-09-28 14:56:31 +0300

    added itests for "ya-work-time-is-expiring-reminder"

--- itest/lupapalvelu/reminders_itest.clj
+++ itest/lupapalvelu/reminders_itest.clj
@@ -175,9 +175,68 @@
       :email "jyba.skebamies@example.com"
       :application-id (:id reminder-application-non-matching-to-inforequest))))
 
+(def- ya-reminder-application
+   (merge
+     domain/application-skeleton
+     {:id "LP-753-2015-01234",
+      :applicant "Panaani Pena",
+      :propertyId "75341600550007",
+      :location [404369.304 6693806.957],
+      :address "Latokuja 3",
+      :title "Latokuja 3",
+      :primaryOperation {:created 1443177714333,
+                         :description nil,
+                         :name "ya-katulupa-vesi-ja-viemarityot",
+                         :id "560524f2dacf322aecce862e"},
+      :secondaryOperations [],
+      :authority {:id "777777777777777777000023",
+                  :role "authority",
+                  :lastName "Sibbo",
+                  :firstName "Sonja",
+                  :username "sonja"},
+      :schema-version 1,
+      :_applicantIndex ["Panaani Pena"],
+      :created 1443177714333,
+      :opened 1443177714333,
+      :sent 1443177770260,
+      :modified 1443177860463,
+      :submitted 1443177768040,
+      :_attachment_indicator_reset 1443177770260,
+      :municipality "753",
+      :state "verdictGiven",
+      :permitType "YA",
+      :organization "753-YA",
+      :auth [{:lastName "Panaani"
+              :firstName "Pena"
+              :username "pena"
+              :type "owner"
+              :role "owner"
+              :id "777777777777777777000020"}],
+      :verdicts [{:id "5605252edacf322aecce8636",
+                  :kuntalupatunnus "523",
+                  :timestamp 1443177774181,
+                  :paatokset[{:poytakirjat
+                              [{:urlHash "302aadf76080d26d6bc0b0ae7c1db81f87404dae"}],
+                              :paivamaarat {:paatosdokumentinPvm 1390953600000},
+                              :lupamaaraykset {:muutMaaraykset
+                                               ["Kaivu: Alkukatselmus pidett\u00e4v\u00e4."
+                                                "Kaivu: Viheralueet rakennettava."],
+                                               :takuuaikaPaivat "760"}}]}],
+      :documents [{:schema-info {:version 1,
+                                 :order 63,
+                                 :repeating false,
+                                 :removable false,
+                                 :type "group",
+                                 :name "tyoaika"},
+                   :meta {:_indicator_reset {:timestamp 1443177770260}},
+                   :id "560524f2dacf322aecce8633",
+                   :data {:tyoaika-paattyy-pvm {:value "25.09.2015", :modified 1443177751909},
+                          :tyoaika-alkaa-pvm {:value "30.09.2015", :modified 1443177749608}},
+                   :created 1443177714333}]}))
 
 
-(defn- check-sent-reminder-email [to subject bodypart & [application-id link-role]]
+(defn- check-sent-reminder-email [to subject bodyparts & [application-id link-role]]
+  {:pre (vector? bodyparts)}
 
   ;; dummy-email-server/messages sometimes returned nil for the email
   ;; (because the email sending is asynchronous). Thus applying sleep here.
@@ -191,7 +250,8 @@
       (fact "email check"
         (:to email) => (contains to)
         (:subject email) => subject
-        (get-in email [:body :plain]) => (contains bodypart)
+        (doseq [bodypart bodyparts]
+          (get-in email [:body :plain]) => (contains bodypart))
         (when link-role
           email => (partial contains-application-link? application-id link-role))))))
 
@@ -202,6 +262,7 @@
     (mongo/insert :applications reminder-application-non-matching-neighbors)
     (mongo/insert :applications reminder-application-matching-to-inforequest)
     (mongo/insert :applications reminder-application-non-matching-to-inforequest)
+    (mongo/insert :applications ya-reminder-application)
     (mongo/insert :open-inforequest-token open-inforequest-entry-non-matching)
     (mongo/insert :open-inforequest-token open-inforequest-entry-matching)
     (mongo/insert :open-inforequest-token open-inforequest-entry-with-application-with-non-matching-state)
@@ -223,7 +284,7 @@
          (check-sent-reminder-email
           (-> statement-matching :person :email)
           "Lupapiste.fi: Naapurikuja 3 - Muistutus lausuntopyynn\u00f6st\u00e4"
-          "Sinulta on pyydetty lausuntoa lupahakemukseen"
+          ["Sinulta on pyydetty lausuntoa lupahakemukseen"]
           (:id reminder-application) "authority")
          )))
 
@@ -242,7 +303,7 @@
        (check-sent-reminder-email
         (-> statement-matching :person :email)
         "Lupapiste.fi: Naapurikuja 3 - Muistutus lausuntopyynn\u00f6st\u00e4"
-        "Sinulta on pyydetty lausuntoa lupahakemukseen"
+        ["Sinulta on pyydetty lausuntoa lupahakemukseen"]
         (:id reminder-application) "authority"))
      ))
 
@@ -264,7 +325,7 @@
          (check-sent-reminder-email
           (:email open-inforequest-entry-matching)
           "Lupapiste.fi: Naapurikuja 3 - Muistutus avoimesta neuvontapyynn\u00f6st\u00e4"
-          "Organisaatiollasi on vastaamaton neuvontapyynt\u00f6")
+          ["Organisaatiollasi on vastaamaton neuvontapyynt\u00f6"])
          )))
 
    (fact "the \"reminder-sent\" timestamp already exists"
@@ -281,7 +342,7 @@
          (check-sent-reminder-email
           (:email open-inforequest-entry-matching)
           "Lupapiste.fi: Naapurikuja 3 - Muistutus avoimesta neuvontapyynn\u00f6st\u00e4"
-          "Organisaatiollasi on vastaamaton neuvontapyynt\u00f6")
+          ["Organisaatiollasi on vastaamaton neuvontapyynt\u00f6"])
          ))))
 
 
@@ -307,7 +368,7 @@
             (check-sent-reminder-email
              (-> neighbor-matching :status second :email)
              "Lupapiste.fi: Naapurikuja 3 - Muistutus naapurin kuulemisesta"
-             "T\u00e4m\u00e4 on muistutusviesti. Rakennuspaikan rajanaapurina Teille ilmoitetaan")
+             ["T\u00e4m\u00e4 on muistutusviesti. Rakennuspaikan rajanaapurina Teille ilmoitetaan"])
             ))))
 
    (fact "the \"reminder-sent\" status already exists - no emails sent"
@@ -331,7 +392,7 @@
            (check-sent-reminder-email
             "pena@example.com"
             "Lupapiste.fi: Naapurikuja 3 - Muistutus aktiivisesta hakemuksesta"
-            "Sinulla on Lupapiste-palvelussa aktiivinen lupahakemus")
+            ["Sinulla on Lupapiste-palvelussa aktiivinen lupahakemus"])
            ))))
 
    (fact "the \"reminder-sent\" timestamp already exists"
@@ -347,8 +408,66 @@
          (check-sent-reminder-email
           "pena@example.com"
           "Lupapiste.fi: Naapurikuja 3 - Muistutus aktiivisesta hakemuksesta"
-          "Sinulla on Lupapiste-palvelussa aktiivinen lupahakemus")
+          ["Sinulla on Lupapiste-palvelussa aktiivinen lupahakemus"])
          ))))
 
+
+ (facts "ya-work-time-is-expiring-reminder"
+
+   (fact "the \"ya-work-time-is-expiring\" timestamp does not pre-exist"
+     (mongo/with-db db-name
+       (let [app (mongo/by-id :applications (:id ya-reminder-application))]
+
+         (:work-time-expiring-reminder-sent app) => nil?
+
+         (batchrun/ya-work-time-is-expiring-reminder)
+
+         (let [app (mongo/by-id :applications (:id ya-reminder-application))
+               tyoaika-paattyy (->> ya-reminder-application :documents (filter #(= "tyoaika" (-> % :schema-info :name))) first :data :tyoaika-paattyy-pvm :value)]
+
+           (:work-time-expiring-reminder-sent app) => number?
+
+           (check-sent-reminder-email
+             "pena@example.com"
+             "Lupapiste.fi: Latokuja 3 - Yleisten alueiden lupasi p\u00e4\u00e4ttymisajankohta l\u00e4hestyy"
+             ["Hakemukselle on merkitty luvan p\u00e4\u00e4ttymisajankohdaksi"
+              tyoaika-paattyy
+              (:address app)])))))
+
+   (fact "the \"ya-work-time-is-expiring\" timestamp already exists"
+     (mongo/with-db db-name
+       (update-application (application->command ya-reminder-application)
+         {$set {:work-time-expiring-reminder-sent timestamp-the-beginning-of-time}})
+
+       (batchrun/ya-work-time-is-expiring-reminder)
+
+       (let [app (mongo/by-id :applications (:id ya-reminder-application))]
+         (= (:work-time-expiring-reminder-sent app) timestamp-the-beginning-of-time) => true?
+         (dummy-email-server/messages) => empty?
+         )))
+
+   (fact "the \"ya-work-time-is-expiring\" reminder is sent also to applications in state 'construction-started'"
+     (mongo/with-db db-name
+       (update-application (application->command ya-reminder-application)
+         {$unset {:work-time-expiring-reminder-sent 1}
+          $set {:state "constructionStarted"}})
+       (batchrun/ya-work-time-is-expiring-reminder)
+       (check-sent-reminder-email
+         "pena@example.com"
+         "Lupapiste.fi: Latokuja 3 - Yleisten alueiden lupasi p\u00e4\u00e4ttymisajankohta l\u00e4hestyy"
+         ["Hakemukselle on merkitty luvan p\u00e4\u00e4ttymisajankohdaksi"])
+       ))
+
+   (fact "applications without 'tyoaika' document are not reacted to"
+     (mongo/with-db db-name
+       (update-application (application->command ya-reminder-application)
+           {$unset {:work-time-expiring-reminder-sent 1}
+            $set {:documents (remove
+                               #(= "tyoaika" (-> % :schema-info :name))
+                               (:documents ya-reminder-application))}})
+
+       (batchrun/ya-work-time-is-expiring-reminder)
+       (dummy-email-server/messages) => empty?
+       )))
  )
 
