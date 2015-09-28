bb45f1f2f75170b97bb57c0a0c16cca314a6fa06
Antti Ramo <antti.ramo@solita.fi>
2015-09-28 15:01:39 +0300

    aes cipher

--- test/sade/crypt_test.clj
+++ test/sade/crypt_test.clj
@@ -11,6 +11,15 @@
         result      (String. decrypted "UTF-8")]
     plain => result))
 
+(fact "plain -> encrypt-aes -> decrypt-aes -> plain"
+  (let [plain       "Hello, world!"
+        crypto-key  (range 32)
+        crypto-iv   (range 16)
+        encrypted   (c/encrypt crypto-key crypto-iv (.getBytes plain "UTF-8") :aes)
+        decrypted   (c/decrypt crypto-key crypto-iv encrypted :aes)
+        result      (String. decrypted "UTF-8")]
+    plain => result))
+
 (fact "text -> base 64 encode -> base 64 decode -> text"
   (-> "Hello, world!"
       (.getBytes "UTF-8")
