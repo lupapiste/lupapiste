f5b8c98ef4083dde10e912b3f6543c94a6be17bb
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-10-09 13:55:04 +0300

    move finnish-y? and finnish-ovt?

--- src/sade/validators.clj
+++ src/sade/validators.clj
@@ -16,3 +16,23 @@
     (and
       (valid-email? email)
       (or (env/value :email :skip-mx-validation) (dns/valid-mx-domain? email)))))
+
+(defn finnish-y? [y]
+  (if y
+    (if-let [[_ number check] (re-matches #"(\d{7})-(\d)" y)]
+      (let [cn (mod (reduce + (map * [7 9 10 5 8 4 2] (map #(Long/parseLong (str %)) number))) 11)
+            cn (if (zero? cn) 0 (- 11 cn))]
+        (= (Long/parseLong check) cn)))
+    false))
+
+(defn finnish-ovt?
+  "OVT-tunnus SFS 5748 standardin mukainen OVT-tunnus rakentuu ISO6523 -standardin
+   mukaisesta Suomen verohallinnon tunnuksesta 0037, Y-tunnuksesta
+   (8 merkki\u00e4 ilman v\u00e4liviivaa) sek\u00e4 vapaamuotoisesta 5 merkist\u00e4,
+   jolla voidaan antaa organisaation alataso tai kustannuspaikka.
+   http://www.tieke.fi/pages/viewpage.action?pageId=17104927"
+  [ovt]
+  (if ovt
+    (if-let [[_ y c] (re-matches #"0037(\d{7})(\d)\w{0,5}" ovt)]
+      (finnish-y? (str y \- c)))
+    false))
