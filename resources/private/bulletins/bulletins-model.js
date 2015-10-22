f9bb6ee23d472cddea1d402d39325ca8af707aef
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-10-22 16:16:46 +0300

    sync with lpk-824

--- resources/private/bulletins/bulletins-model.js
+++ resources/private/bulletins/bulletins-model.js
@@ -2,31 +2,33 @@ LUPAPISTE.BulletinsModel = function() {
   "use strict";
   var self = this;
 
-  self.supportedPages = ["bulletins", "bulletin"];
+  var supportedPages = ["bulletins", "bulletin"];
 
-  self.page = ko.observable().extend({limited: {values: self.supportedPages, defaultValue: "bulletins"}});
-
-  self.pageParams = ko.observable({});
-
-  hub.onPageLoad("bulletins", function() {
-    self.pageParams({});
-    self.page("bulletins");
+  self.page = ko.observable().extend({
+    limited: {values: supportedPages, defaultValue: "bulletins"}
   });
 
-  hub.onPageLoad("bulletin", function(event) {
-    self.pageParams({bulletinId: event.pagePath[0]});
-    self.page("bulletin");
+  var bulletinService = new LUPAPISTE.ApplicationBulletinsService();
+
+  self.pageParams = ko.pureComputed(function () {
+    var defaultParams = {
+      bulletinService: bulletinService
+    };
+
+    return self.page() === "bulletin" ?
+      _.extend(defaultParams, { bulletinId: bulletinId }) :
+      defaultParams;
   });
 
-  function onLoad() {
-    var currPage = pageutil.getPage();
-    if (currPage === "bulletin") {
-      self.pageParams({bulletinId: pageutil.subPage()});
-      self.page("bulletin");
-    } else {
-      self.page(currPage);
-    }
-  }
+  hub.onPageLoad("bulletins", function(e) {
+    self.page(e.pageId);
+  });
 
-  onLoad();
+  hub.onPageLoad("bulletin", function(e) {
+    bulletinId = _.first(e.pagePath);
+    self.page(e.pageId);
+  });
+
+  self.page(pageutil.getPage());
+  var bulletinId = pageutil.subPage();
 };
