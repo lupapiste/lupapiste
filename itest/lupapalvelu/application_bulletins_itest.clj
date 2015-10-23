aac3439dd48a34afee59aa032443a955d877f8b2
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-10-23 15:08:50 +0300

    make ym application and use Olli

--- itest/lupapalvelu/application_bulletins_itest.clj
+++ itest/lupapalvelu/application_bulletins_itest.clj
@@ -3,10 +3,15 @@
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.factlet :refer :all]))
 
+(apply-remote-minimal)
+
 (facts "Publishing bulletins"
-  (let [app    (create-and-submit-application pena)
+  (let [app (create-and-submit-application pena :operation "jatteen-keraystoiminta"
+                                                :propertyId oulu-property-id
+                                                :x 430109.3125 :y 7210461.375
+                                                :address "Oulu 10")
         app-id (:id app)]
     (fact "Authority can publish bulletin"
-      (command sonja :publish-bulletin :id app-id) => ok?)
+      (command olli :publish-bulletin :id app-id) => ok?)
     (fact "Regular user can't publish bulletin"
       (command pena :publish-bulletin :id app-id) => fail?)))
