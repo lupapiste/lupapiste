ac5fc47d87a88473bc3b59a68bfaa4b04607618b
Antti Ramo <antti.ramo@solita.fi>
2015-10-28 07:14:52 +0200

    set murmeli as remote robot test machine

---
+++ robot/remote-config.sh
@@ -0,0 +1,10 @@
+hubert='hubert.solita.fi'
+murmeli='murmeli.solita.fi'
+hubertxp='192.168.7.122'
+murmelixp='192.168.7.122'
+
+#remote=$hubert
+remote=$murmeli
+
+#remotexp=$hubertxp
+remotexp=$murmelixp
