f47322040a438b255df4c81dbb2047075e731e3b
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-10-30 16:48:30 +0200

    show comment field

--- resources/private/bulletins/application-bulletin/bulletin-comment/bulletin-comment-model.js
+++ resources/private/bulletins/application-bulletin/bulletin-comment/bulletin-comment-model.js
@@ -2,6 +2,8 @@ LUPAPISTE.BulletinCommentModel = function() {
   "use strict";
   var self = this;
 
+  self.comment = ko.observable();
+
   self.addComment = function() {
     hub.send("bulletinService::commentAdded", {comment: "I am a teapot"});
   };
