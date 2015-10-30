6f087cf0a89b30d9947b3b59f0935e4dcea8578d
Antti Ramo <antti.ramo@solita.fi>
2015-10-30 11:31:05 +0200

    refactor - separate side effects and logic

--- src/lupapalvelu/document/transformations.clj
+++ src/lupapalvelu/document/transformations.clj
@@ -10,18 +10,21 @@
   (->> (map (fn [[k v]] [(get mappings k k) (if (map? v) (rename-keys-deep mappings v) v)]) m)
        (into {})))
 
-(defn rakennusjateselvitys-updates [{application :application created :created :as command}]
+(defn rakennusjateselvitys-updates [{application :application created :created :as command} selvitys-doc]
   (when-let [suunnitelma-doc (domain/get-document-by-name application "rakennusjatesuunnitelma")]
     (let [field-mappings {:suunniteltuMaara :toteutunutMaara}
-          selvitys-doc (or (domain/get-document-by-name application "rakennusjateselvitys")
-                           (persistence/create-empty-doc command "rakennusjateselvitys"))
           updates (->> (:data suunnitelma-doc)
                        (rename-keys-deep field-mappings)
                        (persistence/data-model->model-updates [])
                        (filter (comp not-empty second)))]
       (persistence/validated-model-updates application :documents selvitys-doc updates created))))
 
+(defn create-rakennusjateselvitys-from-rakennusjatesuunnitelma [{application :application :as command}]
+  (->>  (or (domain/get-document-by-name application "rakennusjateselvitys")
+            (persistence/create-empty-doc command "rakennusjateselvitys"))
+        (rakennusjateselvitys-updates command)))
+
 (defn get-state-transition-updates [command next-state]
   (case next-state
-    :verdictGiven (rakennusjateselvitys-updates command)
+    :verdictGiven (create-rakennusjateselvitys-from-rakennusjatesuunnitelma command)
     {}))
