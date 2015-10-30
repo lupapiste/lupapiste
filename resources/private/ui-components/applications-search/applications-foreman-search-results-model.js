77b8e0b33c733f45d31b802ac90a684d9931d149
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-10-30 13:59:04 +0200

    fix creating foreman columns

--- resources/private/ui-components/applications-search/applications-foreman-search-results-model.js
+++ resources/private/ui-components/applications-search/applications-foreman-search-results-model.js
@@ -5,13 +5,22 @@ LUPAPISTE.ApplicationsForemanSearchResultsModel = function(params) {
   ko.utils.extend(self, new LUPAPISTE.ApplicationsSearchResultsModel(params));
 
   self.columns = [
-    self.createColumn("first", "applications.indicators", {colspan: lupapisteApp.models.currentUser.isAuthority() ? "4" : "3", sortable: false}),
-    self.createColumn("second", "applications.id", {sortField: "id"}),
-    self.createColumn("third", "applications.type", {sortField: "type"}),
-    self.createColumn("fourth", "applications.location", {sortField: "location"}),
-    self.createColumn("fifth", "applications.foreman-name", {sortField: "foreman"}),
-    self.createColumn("sixth", "applications.foreman-role", {sortField: "foremanRole"}),
-    self.createColumn("seventh", "applications.sent", {sortField: "submitted"}),
-    self.createColumn("eight", "applications.status", {sortField: "state"})
+    self.createSortableColumn("first", "applications.indicators",   {colspan: lupapisteApp.models.currentUser.isAuthority() ? "4" : "3",
+                                                                     sortable: false,
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("second", "applications.id",          {sortField: "id",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("third", "applications.type",         {sortField: "type",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("fourth", "applications.location",    {sortField: "location",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("fifth", "applications.foreman-name", {sortField: "foreman",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("sixth", "applications.foreman-role", {sortField: "foremanRole",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("seventh", "applications.sent",       {sortField: "submitted",
+                                                                     currentSort: self.dataProvider.sort}),
+    self.createSortableColumn("eight", "applications.status",       {sortField: "state",
+                                                                     currentSort: self.dataProvider.sort})
   ];
 };
