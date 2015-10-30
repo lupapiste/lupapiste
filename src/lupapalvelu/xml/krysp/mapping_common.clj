b024f0cf272176c1865867c91aa24d77fae505d8
Jari Asp <jari.asp@solita.fi>
2015-10-30 10:29:08 +0200

    - added the "osapuoli-body_216" mapping - refactoring + marked some defs private

--- src/lupapalvelu/xml/krysp/mapping_common.clj
+++ src/lupapalvelu/xml/krysp/mapping_common.clj
@@ -304,19 +304,24 @@
 (def yritys_213 {:tag :yritys :ns "yht" :child yritys-child_213})
 (def yritys_215 {:tag :yritys :ns "yht" :child yritys-child_215})
 
-(def osapuoli-body_211 {:tag :Osapuoli
-                        :child [{:tag :kuntaRooliKoodi}
-                                {:tag :VRKrooliKoodi}
-                                henkilo
-                                yritys_211
-                                {:tag :turvakieltoKytkin}]})
+(def- osapuoli-body_211 {:tag :Osapuoli
+                         :child [{:tag :kuntaRooliKoodi}
+                                 {:tag :VRKrooliKoodi}
+                                 henkilo
+                                 yritys_211
+                                 {:tag :turvakieltoKytkin}]})
 
-(def osapuoli-body_213 (update-in osapuoli-body_211 [:child] update-child-element [:yritys] yritys_213))
+(def- osapuoli-body_213 (update-in osapuoli-body_211 [:child] update-child-element [:yritys] yritys_213))
 
-(def osapuoli-body_215
-  (-> osapuoli-body_213
-    (update-in [:child] update-child-element [:henkilo] henkilo_215)
-    (update-in [:child] update-child-element [:yritys] yritys_215)))
+(def- osapuoli-body_215 (-> osapuoli-body_213
+                          (update-in [:child] update-child-element [:henkilo] henkilo_215)
+                          (update-in [:child] update-child-element [:yritys] yritys_215)))
+
+(def- osapuoli-body_216 (-> osapuoli-body_215
+                          (update-in [:child] vec)
+                          (update-in [:child] conj
+                            {:tag :suoramarkkinointikieltoKytkin}
+                            {:tag :selite})))
 
 (def- naapuri {:tag :naapuritieto
                :child [{:tag :Naapuri
@@ -456,28 +461,25 @@
            naapuri]})
 
 (def osapuolet_212
-  {:tag :Osapuolet :ns "yht"
-   :child [{:tag :osapuolitieto :child [osapuoli-body_211]}
-           suunnittelijatieto_211
-           tyonjohtajatieto_212
-           naapuri]})
+  (-> osapuolet_211
+    (update-in [:child] update-child-element [:tyonjohtajatieto] tyonjohtajatieto_212)))
 
 (def osapuolet_213
-  {:tag :Osapuolet :ns "yht"
-   :child [{:tag :osapuolitieto :child [osapuoli-body_213]}
-           suunnittelijatieto_213
-           tyonjohtajatieto_213
-           naapuri]})
+  (-> osapuolet_212
+    (update-in [:child] update-child-element [:osapuolitieto] osapuoli-body_213)
+    (update-in [:child] update-child-element [:suunnittelijatieto] suunnittelijatieto_213)
+    (update-in [:child] update-child-element [:tyonjohtajatieto] tyonjohtajatieto_213)))
 
 (def osapuolet_215
-  {:tag :Osapuolet :ns "yht"
-   :child [{:tag :osapuolitieto :child [osapuoli-body_215]}
-           suunnittelijatieto_215
-           tyonjohtajatieto_215
-           naapuri]})
+  (-> osapuolet_213
+    (update-in [:child] update-child-element [:osapuolitieto] osapuoli-body_215)
+    (update-in [:child] update-child-element [:suunnittelijatieto] suunnittelijatieto_215)
+    (update-in [:child] update-child-element [:tyonjohtajatieto] tyonjohtajatieto_215)))
 
 (def osapuolet_216
-  (update-in osapuolet_215 [:child] update-child-element [:naapuritieto] naapuri-216))
+  (-> osapuolet_215
+    (update-in [:child] update-child-element [:osapuolitieto] osapuoli-body_216)
+    (update-in [:child] update-child-element [:naapuritieto] naapuri-216)))
 
 
 (def tilamuutos
