8d4d4f436f616cafc54c2b92af6580c3f729eb9d
Antti Ramo <antti.ramo@solita.fi>
2015-11-02 13:17:34 +0200

    no rakennusjateselvitys creation if rakennusjatesuunnitelma does not exist + some refactoring for testability

--- src/lupapalvelu/document/transformations.clj
+++ src/lupapalvelu/document/transformations.clj
@@ -7,18 +7,19 @@
 ;; Application state change updates
 ;;
 
-(defn rakennusjateselvitys-updates [{application :application created :created :as command} selvitys-doc]
-  (when-let [suunnitelma-doc (domain/get-document-by-name application "rakennusjatesuunnitelma")]
-    (let [field-mappings {:suunniteltuMaara :toteutunutMaara}
-          updates (->> (:data suunnitelma-doc)
-                       (util/postwalk-map #(clojure.set/rename-keys % field-mappings))
-                       (persistence/data-model->model-updates [])
-                       (filter (comp not-empty second)))]
-      (persistence/validated-model-updates application :documents selvitys-doc updates created))))
+(defn rakennusjateselvitys-updates [suunnitelma-doc]
+  (let [field-mappings {:suunniteltuMaara :toteutunutMaara}]
+    (->> (:data suunnitelma-doc)
+         (util/postwalk-map #(clojure.set/rename-keys % field-mappings))
+         (persistence/data-model->model-updates [])
+         (filter (comp not-empty second)))))
 
-(defn create-rakennusjateselvitys-from-rakennusjatesuunnitelma [{application :application :as command}]
-  (when-not (domain/get-document-by-name application "rakennusjateselvitys")
-    (rakennusjateselvitys-updates command (persistence/create-empty-doc command "rakennusjateselvitys"))))
+(defn create-rakennusjateselvitys-from-rakennusjatesuunnitelma [{application :application created :created :as command}]
+  (when-let [suunnitelma-doc (domain/get-document-by-name application "rakennusjatesuunnitelma")]  
+    (when-not (domain/get-document-by-name application "rakennusjateselvitys")
+      (let [updates (rakennusjateselvitys-updates suunnitelma-doc)
+            selvitys-doc (persistence/create-empty-doc command "rakennusjateselvitys")] 
+        (persistence/validated-model-updates application :documents selvitys-doc updates created)))))
 
 (defn get-state-transition-updates [command next-state]
   (case next-state
