1a4468723b289349be4df5790de4ef769149debf
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-11-02 12:07:52 +0200

    add comment buttons

--- resources/private/bulletins/application-bulletin/bulletin-comment/bulletin-comment-model.js
+++ resources/private/bulletins/application-bulletin/bulletin-comment/bulletin-comment-model.js
@@ -4,8 +4,11 @@ LUPAPISTE.BulletinCommentModel = function() {
 
   self.comment = ko.observable();
 
-  self.addComment = function() {
-    hub.send("bulletinService::commentAdded", {comment: "I am a teapot"});
+  self.addAttachment = function() {
+    // TODO add attachment to comment before it is send
   };
 
+  self.sendComment = function() {
+    hub.send("bulletinService::commentAdded", {comment: "I am a teapot"});
+  };
 };
