a934c6c007a8c53c30c375432f049728becd0de0
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-11-11 12:46:23 +0200

    additional fields for proclaim application form

--- resources/private/services/publish-bulletin-service.js
+++ resources/private/services/publish-bulletin-service.js
@@ -3,7 +3,10 @@ LUPAPISTE.PublishBulletinService = function() {
   var self = this;
 
   hub.subscribe("publishBulletinService::publishBulletin", function(event) {
-    ajax.command("publish-bulletin", {id: event.id})
+    ajax.command("publish-bulletin", {id:                   event.appId,
+                                      proclamationEndsAt:   event.proclamationEndsAt,
+                                      proclamationStartsAt: event.proclamationStartsAt,
+                                      proclamationText:     event.proclamationText})
       .success(_.noop)
       .error(_.noop)
       .call();
