b6699121f3246530705a4a3b5f2fe0409093d056
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-11-11 13:00:16 +0200

    authentication logic, triggered by "vetumaService::authenticateUser" event

--- resources/private/bulletins/vetuma-service.js
+++ resources/private/bulletins/vetuma-service.js
@@ -2,5 +2,20 @@ LUPAPISTE.VetumaService = function() {
   "use strict";
   var self = this;
 
-  
+  self.authenticated = ko.observable(false);
+  self.userInfo = ko.mapping.fromJS({
+    firstName: undefined,
+    lastName: undefined
+  });
+
+  hub.subscribe("vetumaService::authenticateUser", function() {
+    vetuma.getUser(function(resp) { // onFound
+      console.log("FOUND!");
+      ko.mapping.fromJS(_.pick(resp, ["firstName", "lastName"]), self.userInfo);
+      self.authenticated(true);
+    }, function() { // onNotFound
+      console.log("NOT FOUND");
+      self.authenticated(false);
+    });  
+  });
 };
