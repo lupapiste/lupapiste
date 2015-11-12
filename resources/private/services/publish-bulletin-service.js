fd9457479339844d3dbe2ede5620393a6fe8e674
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-11-12 13:44:40 +0200

    send proclaim-bulletin command send pending events

--- resources/private/services/publish-bulletin-service.js
+++ resources/private/services/publish-bulletin-service.js
@@ -3,27 +3,44 @@ LUPAPISTE.PublishBulletinService = function() {
   var self = this;
 
   self.bulletin = ko.observable();
+  self.publishPending = ko.observable(false);
 
-  hub.subscribe("publishBulletinService::publishBulletin", function(event) {
-    ajax.command("publish-bulletin", {id:                   event.appId,
-                                      proclamationEndsAt:   event.proclamationEndsAt,
-                                      proclamationStartsAt: event.proclamationStartsAt,
-                                      proclamationText:     event.proclamationText})
-      .success(function(res) {
-        console.log("succes", res);
-      })
-      .error(_.noop)
-      .call();
+  ko.computed(function() {
+    var state = self.publishPending() ? "pending" : "finished";
+    hub.send("publishBulletinService::publishProcessing", {state: state});
   });
 
-  hub.subscribe("publishBulletinService::fetchBulletinVersions", function(event) {
-    console.log("id", event);
-    ajax.query("bulletin-versions", {bulletinId: event.bulletinId})
+  var publishBulletin = function(state, event) {
+    ajax.command(state + "-bulletin", {id:                  event.id,
+                                      proclamationEndsAt:   event.proclamationEndsAt,
+                                      proclamationStartsAt: event.proclamationStartsAt,
+                                      proclamationText:     event.proclamationText || ""})
+      .pending(self.publishPending)
+      .success(function() {
+        hub.send("publishBulletinService::publishProcessed", {status: "success"});
+      })
+      .error(function() {
+        hub.send("publishBulletinService::publishProcessed", {status: "failed"});
+      })
+      .call();
+  };
+
+  hub.subscribe("publishBulletinService::proclaimBulletin", function(event) {
+    publishBulletin("proclaim", event);
+  });
+
+  var fetchBulletinVersions = _.debounce(function(bulletinId) {
+    ajax.query("bulletin-versions", {bulletinId: bulletinId})
       .success(function(res) {
-        if (res.bulletin.id) {
+        if (util.getIn(res, ["bulletin", "id"])) {
           self.bulletin(res.bulletin);
         }
       })
       .call();
-    });
+  });
+
+  hub.subscribe("publishBulletinService::fetchBulletinVersions", function(event) {
+    fetchBulletinVersions(event.bulletinId);
+  });
+
 };
