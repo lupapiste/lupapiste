0dc0e3bd6858476b344c24a6c4875e69e28b00f3
Antti Ramo <antti.ramo@solita.fi>
2015-11-16 10:04:34 +0200

    add schema and path in document data service

--- resources/private/services/document-data-service.js
+++ resources/private/services/document-data-service.js
@@ -22,16 +22,17 @@ LUPAPISTE.DocumentDataService = function() {
   }
 
   self.addDocument = function(doc, options) {
-    var docData = {
-      id: doc.id,
-      name: doc.schema.info.name,
-      isDisabled: options && options.disabled
-    };
     if (self.findDocumentById(doc.id)) {
       return -1;
     } else {
-      return self.model.push( _.extend( docData,
-        createDataModel(_.extend({type: 'document'}, doc.schema.info, doc.schema), doc.data, [doc.schema.info.name])
+      return self.model.push( _.extend({
+          id: doc.id,
+          path: [],
+          name: doc.schema.info.name,
+          schema: doc.schema,
+          isDisabled: options && options.disabled
+        },
+        createDataModel(_.extend({type: "document"}, doc.schema.info, doc.schema), doc.data, [])
       ));
     }
   }
@@ -45,37 +46,42 @@ LUPAPISTE.DocumentDataService = function() {
     return self.model.remove(self.findDocumentById(id));
   }
 
-  //
-  // Public utilities
-  //
-
-  self.createDataModel = createDataModel;
-  self.getAsRaw = getAsRaw;
 
   //
   // Repeating utilities
   //
 
+  function createRepeatingGroupDataModel(schema, rawModel, path, index) {
+    return _.extend(
+        {index: index},
+        createGroupDataModel(schema, rawModel, path.concat(index))
+    );
+  }
+
   function createRepeatingDataModel(schema, rawModel, path) {
-    return ko.observableArray(_.map(rawModel, function(subModel, index) {
-      return _.extend({index: index}, createGroupDataModel(schema, subModel, path.concat(index)));
-    }));
+    return {
+      path: path,
+      schema: schema,
+      model: ko.observableArray(_.map(rawModel, function(subModel, index) {
+        return createRepeatingGroupDataModel(schema, subModel, path, index);
+      }))
+    };
   }
 
   function findByIndex(repeatingModel, index) {
-    return _.find(repeatingModel(), function(rep) {
-      return rep.index === index;
+    return _.find(repeatingModel.model ? repeatingModel.model() : repeatingModel(), function(rep) {
+      return _.parseInt(rep.index) === _.parseInt(index);
     });
   }
 
   function removeByIndex(repeatingModel, index) {
-    return repeatingModel.remove(findByIndex(repeatingModel, index));
+    return repeatingModel.model.remove(findByIndex(repeatingModel, index));
   }
 
-  function pushToRepeating(schema, repeatingModel, rawModel) {
-    var ind = _(repeatingModel()).map('index').max() + 1;
-    var path = repeatingModel() && repeatingModel().path;
-    return repeatingModel.push(createRepeatingUnitDataModel(schema, rawModel, path, _.max([0, ind])));
+  function pushToRepeating(repeatingModel, rawModel) {
+    var ind = _.parseInt( _(repeatingModel.model()).map("index").max() ) + 1 || 0;
+    var path = repeatingModel.path;
+    return repeatingModel.model.push(createRepeatingGroupDataModel(repeatingModel.schema, rawModel, path, ind));
   }
 
   //
@@ -85,8 +91,9 @@ LUPAPISTE.DocumentDataService = function() {
   function createGroupDataModel(schema, rawModel, path) {
     return {
       path: path,
+      schema: schema,
       model: _(schema.body).map(function(subSchema) {
-        return [subSchema.name, createDataModel(subSchema, rawModel[subSchema.name], path.concat(subSchema.name))]
+        return [subSchema.name, createDataModel(subSchema, rawModel[subSchema.name], path.concat(subSchema.name))];
       }).zipObject().value()
     };
   }
@@ -97,6 +104,7 @@ LUPAPISTE.DocumentDataService = function() {
 
   function createInputDataModel(schema, rawModel, path) {
     return {path: path,
+            schema: schema,
             model: ko.observable(rawModel && rawModel.value)};
   }
 
@@ -105,11 +113,11 @@ LUPAPISTE.DocumentDataService = function() {
   //
 
   function isRepeating(schema) {
-    return schema.repeating || schema.type === 'table';
+    return schema.repeating || schema.type === "table";
   }
 
   function isGroupType(schema) {
-    return _.contains(['group', 'table', 'location', 'document'], schema.type);
+    return _.contains(["group", "table", "location", "document"], schema.type);
   }
 
   function createDataModel(schema, rawModel, path) {
@@ -123,7 +131,7 @@ LUPAPISTE.DocumentDataService = function() {
   }
 
   function get(model, key) {
-    if (_.has(model, 'model')) {
+    if (_.has(model, "model")) {
       return get(model.model, key);
     } else if (ko.isObservable(model) && _.isArray(model())) {
       return findByIndex(model, key);
@@ -143,10 +151,10 @@ LUPAPISTE.DocumentDataService = function() {
     if (ko.isObservable(model)) {
       return getAsRaw(model());
 
-    } else if (_.has(model, 'model')) {
+    } else if (_.has(model, "model")) {
       return getAsRaw(model.model);
 
-    } else if (_.has(_.first(model), 'index')) {
+    } else if (_.has(_.first(model), "index")) {
       return _(model)
         .map(function(rep) { return [rep.index, rep.model]; })
         .zipObject()
