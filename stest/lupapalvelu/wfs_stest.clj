b36aa0fa8db44aac0a3e2d8f5cb801378dd07af6
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-11-17 14:56:24 +0200

    fixed test

--- stest/lupapalvelu/wfs_stest.clj
+++ stest/lupapalvelu/wfs_stest.clj
@@ -1,6 +1,7 @@
 (ns lupapalvelu.wfs-stest
   (:require [midje.sweet :refer :all]
             [lupapalvelu.wfs :as wfs]
+            [lupapalvelu.itest-util :refer [server-address]]
             [sade.env :as env]))
 
 (fact "ktjkii returns"
@@ -17,10 +18,10 @@
 (def local-wfs  (str (server-address) "/dev/krysp"))
 (def local-private-wfs  (str (server-address) "/dev/private-krysp"))
 
-(fact "nil url is not alive" (wfs-is-alive? nil nil nil) => falsey)
-(fact "blank url is not alive" (wfs-is-alive? "" nil nil) => falsey)
-(fact "invalid krysp wfs url is not alive" (wfs-is-alive? "invalid_url" nil nil) => falsey)
-(fact "local test krysp wfs url is alive" (wfs-is-alive? local-wfs nil nil) => true)
-(fact "local private test krysp wfs url without credentials is not alive" (wfs-is-alive? local-private-wfs nil nil) => falsey)
-(fact "local private test krysp wfs url without password is not alive" (wfs-is-alive? local-private-wfs "pena" nil) => falsey)
-(fact "local private test krysp wfs url with credentials is alive" (wfs-is-alive? local-private-wfs "pena" "pena") => true)
+(fact "nil url is not alive" (wfs/wfs-is-alive? nil nil nil) => falsey)
+(fact "blank url is not alive" (wfs/wfs-is-alive? "" nil nil) => falsey)
+(fact "invalid krysp wfs url is not alive" (wfs/wfs-is-alive? "invalid_url" nil nil) => falsey)
+(fact "local test krysp wfs url is alive" (wfs/wfs-is-alive? local-wfs nil nil) => true)
+(fact "local private test krysp wfs url without credentials is not alive" (wfs/wfs-is-alive? local-private-wfs nil nil) => falsey)
+(fact "local private test krysp wfs url without password is not alive" (wfs/wfs-is-alive? local-private-wfs "pena" nil) => falsey)
+(fact "local private test krysp wfs url with credentials is alive" (wfs/wfs-is-alive? local-private-wfs "pena" "pena") => true)
