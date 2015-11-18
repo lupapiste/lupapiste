21416c23aa6f099c5aebfa0dacc69798f8716268
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-11-18 14:53:10 +0200

    save files to gridfs

--- src/lupapalvelu/file_upload_api.clj
+++ src/lupapalvelu/file_upload_api.clj
@@ -1,17 +1,20 @@
 (ns lupapalvelu.file-upload-api
   (:require [noir.core :refer [defpage]]
             [noir.response :as resp]
-            [lupapalvelu.mongo :as mongo]))
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.mime :as mime]
+            [lupapalvelu.vetuma :as vetuma]))
 
 (defn- save-file [file]
-  (let [file-id (mongo/create-id)]
+  (let [file-id (mongo/create-id)
+        sanitized-filename (mime/sanitize-filename (:filename file))]
+    (mongo/upload file-id sanitized-filename (:content-type file) (:tempfile file) :sessionId (vetuma/session-id))
     {:id file-id
      :filename (:filename file)
      :size (:size file)}))
 
 (defpage [:post "/upload/file"] {files :files}
-  (prn "asdasd" files)
-  (let [file-info {:files (map save-file files)}]
+  (let [file-info {:files (pmap save-file files)}]
     (->> (assoc file-info :ok true)
       (resp/json)
       (resp/content-type "text/plain")
