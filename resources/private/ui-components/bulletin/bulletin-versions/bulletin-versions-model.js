94c3cd690c15170ed15a5be3a53d53bcc8311962
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-11-19 16:57:58 +0200

    use component in jpano tab, content as accordion

--- resources/private/ui-components/bulletin/bulletin-versions/bulletin-versions-model.js
+++ resources/private/ui-components/bulletin/bulletin-versions/bulletin-versions-model.js
@@ -3,4 +3,16 @@ LUPAPISTE.BulletinVersionsModel = function(params) {
   var self = this;
   self.params = params;
 
+  self.bulletin = params.bulletin;
+
+  ko.utils.extend(self, new LUPAPISTE.ComponentBaseModel(params));
+
+  console.log(self.params);
+
+  ko.computed(function() {
+    var id = self.params.appId();
+    if(id) {
+      self.sendEvent("publishBulletinService", "fetchBulletinVersions", {bulletinId: id});
+    }
+  });
 };
