6ffb0feaafcf43e0dc4c66bc2fc4daf280e01c0b
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-11-19 13:54:26 +0200

    remove unused code

--- src/lupapalvelu/application_bulletins.clj
+++ src/lupapalvelu/application_bulletins.clj
@@ -53,13 +53,3 @@
                      :created      created
                      :contact-info contact-info}]
     new-comment))
-
-(defn store-files [bulletin-id comment-id files]
-  (let [store-file-fn (fn [file] (let [file-id (mongo/create-id)
-                                       sanitized-filename (mime/sanitize-filename (:filename file))]
-                                   (mongo/upload file-id sanitized-filename (:content-type file) (:tempfile file) :bulletinId bulletin-id :commentId comment-id)
-                                   {:id file-id
-                                    :filename sanitized-filename
-                                    :size (:size file)
-                                    :contentType (:content-type file)}))]
-    (map store-file-fn files)))
