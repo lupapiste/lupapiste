a9324055c368a67b736b015458530d00bb20be9b
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-11-25 10:02:22 +0200

    move fetch to wrapper component

--- resources/private/ui-components/bulletin/bulletin-tab/bulletin-tab-model.js
+++ resources/private/ui-components/bulletin/bulletin-tab/bulletin-tab-model.js
@@ -2,10 +2,19 @@ LUPAPISTE.BulletinTabModel = function(params) {
   "use strict";
   var self = this;
 
+  ko.utils.extend(self, new LUPAPISTE.ComponentBaseModel(params));
+
   self.appId = params.appId;
   self.appState = params.appState;
   self.authModel = params.authModel;
 
   self.bulletin = params.bulletinService.bulletin;
 
+
+  ko.computed(function() {
+    var id = self.appId();
+    if(id) {
+      self.sendEvent("publishBulletinService", "fetchBulletinVersions", {bulletinId: id});
+    }
+  });
 };
--- resources/private/ui-components/bulletin/bulletin-versions/bulletin-versions-model.js
+++ resources/private/ui-components/bulletin/bulletin-versions/bulletin-versions-model.js
@@ -3,19 +3,10 @@ LUPAPISTE.BulletinVersionsModel = function(params) {
   var self = this;
   self.params = params;
 
-  ko.utils.extend(self, new LUPAPISTE.ComponentBaseModel(params));
-
   self.bulletin = params.bulletin;
 
   self.versions = ko.pureComputed(function() {
     return self.bulletin() ? self.bulletin().versions : [];
-  });
-
-  ko.computed(function() {
-    var id = self.params.appId();
-    if(id) {
-      self.sendEvent("publishBulletinService", "fetchBulletinVersions", {bulletinId: id});
-    }
   });
 
   self.versionCommentsCount = function(version) {
