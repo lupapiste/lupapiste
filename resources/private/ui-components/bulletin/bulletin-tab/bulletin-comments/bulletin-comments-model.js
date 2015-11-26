56e7e137a7739d4ba57680f6ca0907a379dae407
Eetu Sutinen <eetu.sutinen@solita.fi>
2015-11-26 09:45:51 +0200

    proclaimed header

--- resources/private/ui-components/bulletin/bulletin-tab/bulletin-comments/bulletin-comments-model.js
+++ resources/private/ui-components/bulletin/bulletin-tab/bulletin-comments/bulletin-comments-model.js
@@ -7,11 +7,24 @@ LUPAPISTE.BulletinCommentsModel = function(params) {
   self.showVersionComments = params.showVersionComments;
 
   self.bulletin = params.bulletin
+
   self.comments = ko.computed(function() {
-    return util.getIn(self, ["bulletin", "comments", util.getIn(self, ["showVersionComments", "id"])]);
+    return util.getIn(self, ["bulletin", "comments", util.getIn(self, ["showVersionComments", "id"])], []);
   });
 
   self.hideComments = function() {
     self.showVersionComments(undefined);
   };
+
+  self.proclaimedHeader = ko.pureComputed(function() {
+    var start  = util.getIn(self, ["showVersionComments", "proclamationStartsAt"], "");
+    var end    = util.getIn(self, ["showVersionComments", "proclamationEndsAt"], "");
+    var amount = util.getIn(self.comments().length);
+    console.log("start", start, end, amount);
+    console.log("foo", self.showVersionComments());
+    if (start && end) {
+      return "Hakemuksen kuulutusaikana " + moment(start).format("D.M.YYYY") + " - " + moment(end).format("D.M.YYYY") +
+        " annettuja mielipiteitä ja muistutuksia yhteensä " + amount + " kpl."
+    }
+  });
 };
