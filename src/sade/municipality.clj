7462f5842708f5ce942b6fb698087cf4adc0714a
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-11-30 09:05:55 +0200

    Added municipality namespace

---
+++ src/sade/municipality.clj
@@ -0,0 +1,9 @@
+(ns sade.municipality)
+
+(def
+   ^{:doc "Kuntaliitokset"}
+  municipality-mapping
+  {"476" "297" ; Maaninka -> Kuopio (2015)
+   "413" "609" ; Lavia -> Pori (2015)
+   "838" "423" ; Tarvasjoki -> Lieto (2015)
+   })
