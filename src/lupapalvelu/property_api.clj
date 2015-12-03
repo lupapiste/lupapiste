b62aebff8b584397ea012079a2b82a9fd166d2db
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-03 10:35:34 +0200

    Added property-borders endpoint

--- src/lupapalvelu/property_api.clj
+++ src/lupapalvelu/property_api.clj
@@ -1,6 +1,7 @@
 (ns lupapalvelu.property-api
   (:require [sade.core :refer :all]
             [sade.property :as p]
+            [sade.validators :as v]
             [lupapalvelu.action :refer [defquery] :as action]))
 
 (defquery municipality-by-property-id
@@ -10,3 +11,11 @@
   (if-let [municipality (p/municipality-id-by-property-id propertyId)]
     (ok :municipality municipality)
     (fail :municipalitysearch.notfound)))
+
+(defquery property-borders
+  {:parameters [propertyIds]
+   :description "Returns property borders as POLYGON WKT strings in a map"
+   :user-roles #{:authority} ; At the time of writing the only use case is the neighbour map
+   :input-validators [(partial action/vector-parameter-of :propertyIds v/kiinteistotunnus?)]}
+  [_]
+  (ok :borders (zipmap propertyIds (repeat "POLYGON()"))))
