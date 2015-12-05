60c654e33ec7e6fed890ba5c3ef06e2d8477b196
Antti Ramo <antti.ramo@solita.fi>
2015-12-05 14:17:24 +0200

    reduce boiler plate in dynamic schema creation

--- src/sade/schemas.clj
+++ src/sade/schemas.clj
@@ -15,14 +15,18 @@
 (def dynamically-created-schemas (atom {}))
 (def custom-generators (atom {}))
 
-(defn dynamic-schema [schema-key predicate]
-  {:pre [(keyword? schema-key)]}
-  (locking dynamically-created-schemas
-    (get @dynamically-created-schemas schema-key 
-         (-> (swap! dynamically-created-schemas assoc schema-key predicate) schema-key))))
-
 (defn add-generator [schema generator]
   (swap! custom-generators assoc schema generator))
+
+(defn dynamic-schema 
+  ([schema-key predicate]
+   {:pre [(keyword? schema-key)]}
+   (locking dynamically-created-schemas
+     (get @dynamically-created-schemas schema-key 
+          (-> (swap! dynamically-created-schemas assoc schema-key predicate) schema-key))))
+  ([schema-key predicate generator]
+   (doto (dynamic-schema schema-key predicate)
+     (add-generator generator))))
 
 (defn generate
   ([schema]         (generate schema {}))
@@ -35,13 +39,13 @@
 ;; Predicate / constraint
 
 (defn min-len-constraint [max-len]
-  (fn [v] (when (>= (count v) max-len) v)))
+  (fn [v] (>= (count v) max-len)))
 
 (defn max-len-constraint [max-len]
-  (fn [v] (when (<= (count v) max-len) v)))
+  (fn [v] (<= (count v) max-len)))
 
 (defn fixed-len-constraint [len]
-  (fn [v] (when (= (count v) len) v)))
+  (fn [v] (= (count v) len)))
 
 ;;
 ;; Schemas
@@ -54,8 +58,8 @@
 (add-generator BlankStr (gen/elements ["" nil]))
 
 (schema/defschema Email
-  "A prismatic schema for email"
-  (schema/constrained schema/Str (comp validators/valid-email? (max-len-constraint 255))))
+  "A simple schema for email"
+  (schema/constrained schema/Str (every-pred validators/valid-email? (max-len-constraint 255))))
 
 (add-generator Email (gen/fmap
                       (fn [[name domain]]
@@ -71,30 +75,27 @@
             (gen/vector gen/char len)))
 
 (defn fixed-length-string [len]
-  (doto (dynamic-schema 
-         (keyword (str "fixed-len-string-" len))
-         (schema/constrained schema/Str (fixed-len-constraint len)))
-    (add-generator (fixed-len-string-generator len))))
+  (dynamic-schema (keyword (str "fixed-len-string-" len))
+                  (schema/constrained schema/Str (fixed-len-constraint len))
+                  (fixed-len-string-generator len)))
 
 (defn min-len-string-generator [min-len]
   (gen/bind (gen/fmap #(+ min-len %) gen/pos-int)
             fixed-len-string-generator))
 
 (defn min-length-string [min-len]
-  (doto (dynamic-schema
-         (keyword (str "min-len-string-" min-len))
-         (schema/constrained schema/Str (min-len-constraint min-len)))
-    (add-generator (min-len-string-generator min-len))))
+  (dynamic-schema (keyword (str "min-len-string-" min-len))
+                  (schema/constrained schema/Str (min-len-constraint min-len))
+                  (min-len-string-generator min-len)))
 
 (defn max-len-string-generator [max-len]
   (gen/fmap clojure.string/join
             (gen/vector gen/char 0 max-len)))
 
 (defn max-length-string [max-len]
-  (doto (dynamic-schema
-         (keyword (str "max-len-string-" max-len))
-         (schema/constrained schema/Str (max-len-constraint max-len)))
-    (add-generator (max-len-string-generator max-len))))
+  (dynamic-schema (keyword (str "max-len-string-" max-len))
+                  (schema/constrained schema/Str (max-len-constraint max-len))
+                  (max-len-string-generator max-len)))
 
 
 
