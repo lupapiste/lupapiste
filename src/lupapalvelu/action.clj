8ca2a11c9513e645a94db2e7906e28d78a7b9a57
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-05 19:23:02 +0200

    Picked property id validation from LPK-1123 branch

--- src/lupapalvelu/action.clj
+++ src/lupapalvelu/action.clj
@@ -115,6 +115,11 @@
 (defn string-parameters [params command]
   (filter-params-of-command params command (complement string?) "error.illegal-value:not-a-string"))
 
+(defn property-id-parameters [params command]
+  (when-let [invalid (seq (filter #(not (v/kiinteistotunnus? (get-in command [:data %]))) params))]
+    (trace "invalid property id parameters:" (s/join ", " invalid))
+    (fail :error.invalid-property-id)))
+
 (defn map-parameters [params command]
   (filter-params-of-command params command (complement map?) :error.unknown-type))
 
