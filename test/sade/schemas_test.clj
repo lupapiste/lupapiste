f2e3c874a5268eb23794be80bd9681ed71ac9c46
Antti Ramo <antti.ramo@solita.fi>
2015-12-07 12:03:33 +0200

    use defspec with test-check instead of midje

--- test/sade/schemas_test.clj
+++ test/sade/schemas_test.clj
@@ -2,7 +2,7 @@
   (:require [sade.schemas :refer :all]
             [midje.sweet :refer :all]
             [schema.core :as schema]
-            [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
@@ -18,32 +18,24 @@
 
 ;; crooked way to test generators with generators
 (def fixed-len-generator-prop
-  (prop/for-all [n gen/pos-int
-                 len gen/pos-int]
-    (->> (gen/sample (fixed-len-string-generator len) n)
-         (every? #(and (string? %) 
-                       (= (count %) len))))))
+  (prop/for-all [len gen/pos-int]
+    (->> (gen/generate (fixed-len-string-generator len))
+         (every-pred string? #(= (count %) len)))))
 
-(fact fixed-len-string-generator
-  (tc/quick-check 100 fixed-len-generator-prop) => (contains #{[:result true]}))
+(defspec fixed-len-string-generator-test 100 fixed-len-generator-prop)
 
 (def min-len-generator-prop
-  (prop/for-all [n gen/pos-int
-                 len gen/pos-int]
-    (->> (gen/sample (min-len-string-generator len) n)
-         (every? #(and (string? %) 
-                       (>= (count %) len))))))
+  (prop/for-all [len gen/pos-int]
+                (->> (gen/generate (min-len-string-generator len))
+                     (every-pred string? #(>= (count %) len)))))
 
-(fact min-len-string-generator
-  (tc/quick-check 100 min-len-generator-prop) => (contains #{[:result true]}))
+(defspec min-len-string-generator-test 100 min-len-generator-prop)
 
 (def max-len-generator-prop
-  (prop/for-all [n gen/pos-int
-                 len gen/pos-int]
-    (->> (gen/sample (max-len-string-generator len) n)
-         (every? #(and (string? %) 
-                       (<= (count %) len))))))
+  (prop/for-all [len gen/pos-int]
+                (->> (gen/generate (max-len-string-generator len))
+                     (every-pred string? #(<= (count %) len)))))
 
-(fact max-len-string-generator
-  (tc/quick-check 100 max-len-generator-prop) => (contains #{[:result true]}))
+(defspec max-len-string-generator-test 100 max-len-generator-prop)
+
 
