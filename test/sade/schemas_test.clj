e8ecefd5ef30376c06b60aa37b16009c82dad170
Antti Ramo <antti.ramo@solita.fi>
2015-12-07 13:35:06 +0200

    cleaning schemas test

--- test/sade/schemas_test.clj
+++ test/sade/schemas_test.clj
@@ -1,20 +1,15 @@
 (ns sade.schemas-test
   (:require [sade.schemas :refer :all]
             [midje.sweet :refer :all]
-            [schema.core :as schema]
-            [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]))
+            [schema.core :as sc]))
 
 (facts max-length-constraint
-  (fact (schema/check (schema/pred (max-len-constraint 1)) []) => nil)
-  (fact (schema/check (schema/pred (max-len-constraint 1)) [1]) => nil)
-  (fact (schema/check (schema/pred (max-len-constraint 1)) [1 2]) =not=> nil))
+  (fact (sc/check (sc/pred (max-len-constraint 1)) []) => nil)
+  (fact (sc/check (sc/pred (max-len-constraint 1)) [1]) => nil)
+  (fact (sc/check (sc/pred (max-len-constraint 1)) [1 2]) =not=> nil))
 
 (facts max-length-string
-  (fact (schema/check (max-length-string 1) "a") => nil)
-  (fact (schema/check (max-length-string 1) "ab") =not=> nil)
-  (fact (schema/check (max-length-string 1) [1]) =not=> nil))
-
-
+  (fact (sc/check (max-length-string 1) "a") => nil)
+  (fact (sc/check (max-length-string 1) "ab") =not=> nil)
+  (fact (sc/check (max-length-string 1) [1]) =not=> nil))
 
