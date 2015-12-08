1248bc0bc2632c149320fa2a0c915d05d680272a
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-12-08 13:33:16 +0200

    move send-file to itest util

--- test-utils/lupapalvelu/itest_util.clj
+++ test-utils/lupapalvelu/itest_util.clj
@@ -716,3 +716,16 @@
                target-file-name)))))
       (catch com.jcraft.jsch.JSchException e
         (error e (str "SSH connection " user "@" server))))))
+
+
+(defn send-file [cookie-store & [filename]]
+  (set-anti-csrf! false)
+  (let [filename    (or filename "dev-resources/sipoon_alueet.zip")
+        uploadfile  (io/file filename)
+        uri         (str (server-address) "/api/upload/file")
+        resp        (http-post uri
+                               {:multipart [{:name "files[]" :content uploadfile}]
+                                :throw-exceptions false
+                                :cookie-store cookie-store})]
+    (set-anti-csrf! true)
+    resp))
