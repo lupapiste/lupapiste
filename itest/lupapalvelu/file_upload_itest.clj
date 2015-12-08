1bf3d49cbad62f275178c56096b5b82a90e32ef1
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-12-08 14:22:53 +0200

    added deleting endpoint tests

--- itest/lupapalvelu/file_upload_itest.clj
+++ itest/lupapalvelu/file_upload_itest.clj
@@ -3,13 +3,25 @@
             [lupapalvelu.itest-util :refer :all]
             [cheshire.core :as json]))
 
+(apply-remote-minimal)
+
 (facts "Upload and remove attachment for a bulletin comment"
-  (let [store        (atom {})
-        cookie-store (doto (->cookie-store store)
-                       (.addCookie test-db-cookie))
-        upload-resp  (-> (send-file cookie-store)
-                         :body
-                         (json/decode keyword))]
+  (let [store         (atom {})
+        cookie-store  (doto (->cookie-store store)
+                        (.addCookie test-db-cookie))
+        upload-resp   (-> (send-file cookie-store)
+                          :body
+                          (json/decode keyword))
+        uploaded-file (first (:files upload-resp))]
 
     (fact "Uploaded file is ok"
-      upload-resp => ok?)))
+      upload-resp => ok?)
+
+    (facts "Deleting"
+      (fact "Must define attachment id"
+        (command pena :remove-uploaded-file :cookie-store cookie-store) => (partial expected-failure? :error.missing-parameters))
+      (fact "Attachment id must match with uploaded file"
+        (command pena :remove-uploaded-file :attachmentId "asdasd" :cookie-store cookie-store) => (partial expected-failure? :error.attachment.not-found))
+      (fact "Attachment can be deleted"
+        (command pena :remove-uploaded-file :attachmentId (:id uploaded-file) :cookie-store cookie-store) => ok?)
+      )))
