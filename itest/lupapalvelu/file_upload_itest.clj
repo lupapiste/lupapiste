d6e0efb529ae1fc213c50b96d775fdb8d0594271
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-12-08 15:45:17 +0200

    test deleting an uploaded file that has been linked to a bulletin

--- itest/lupapalvelu/file_upload_itest.clj
+++ itest/lupapalvelu/file_upload_itest.clj
@@ -1,7 +1,9 @@
 (ns lupapalvelu.file-upload-itest
   (:require [midje.sweet :refer :all]
             [lupapalvelu.itest-util :refer :all]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [sade.util :as util]
+            [lupapalvelu.vetuma-itest-util :as vetuma-util]))
 
 (apply-remote-minimal)
 
@@ -24,4 +26,37 @@
         (command pena :remove-uploaded-file :attachmentId "asdasd" :cookie-store cookie-store) => (partial expected-failure? :error.file-upload.not-found))
       (fact "Attachment can be deleted"
         (command pena :remove-uploaded-file :attachmentId (:id uploaded-file) :cookie-store cookie-store) => ok?)
-      )))
+      (fact "Attachment can not be deleted after linking it to post"
+        (let [starts  (util/get-timestamp-ago :day 1)
+              ends    (util/get-timestamp-from-now :day 1)
+              app (create-and-send-application sonja :operation "lannan-varastointi"
+                                               :propertyId sipoo-property-id
+                                               :x 406898.625 :y 6684125.375
+                                               :address "Hitantine 108"
+                                               :state "sent")
+              m2p1 (command sonja :move-to-proclaimed
+                            :id (:id app)
+                            :proclamationStartsAt starts
+                            :proclamationEndsAt ends
+                            :proclamationText "testi"
+                            :cookie-store cookie-store)
+              bulletin (:bulletin (query pena :bulletin :bulletinId (:id app) :cookie-store cookie-store))
+
+              upload-resp   (-> (send-file cookie-store)
+                                :body
+                                (json/decode keyword))
+              uploaded-file (first (:files upload-resp))]
+
+          m2p1 => ok?
+          upload-resp => ok?
+
+          (vetuma-util/authenticate-to-vetuma! cookie-store)
+
+          ;attach to comment
+          (command sonja :add-bulletin-comment :bulletinId (:id app) :bulletinVersionId (:versionId bulletin) :comment "foobar" :files [uploaded-file] :cookie-store cookie-store) => ok?
+
+          ;try to remove
+          (command pena :remove-uploaded-file :attachmentId (:id uploaded-file) :cookie-store cookie-store) => (partial expected-failure? :error.file-upload.already-linked))
+          )
+        )
+      ))
