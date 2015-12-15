e41c65f28b834e09ea45bc86741daaf444ce2551
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-15 16:26:08 +0200

    Use valid IDs

--- itest/lupapalvelu/notice_api_itest.clj
+++ itest/lupapalvelu/notice_api_itest.clj
@@ -30,25 +30,25 @@
 
     (facts "Application tags" ; tags are in minimal fixture
       (fact "user can't set application tags"
-        (command pena :add-application-tags :id id :tags ["111" "222"]) =not=> ok?)
+        (command pena :add-application-tags :id id :tags ["111111111111111111111111" "222222222222222222222222"]) =not=> ok?)
       (fact "authority can set application tags"
-        (command sonja :add-application-tags :id id :tags ["111" "222"]) => ok?)
+        (command sonja :add-application-tags :id id :tags ["111111111111111111111111" "222222222222222222222222"]) => ok?)
       (fact "authority can't set tags that are not defined in organization"
         (command sonja :add-application-tags :id id :tags ["foo" "bar"]) => (partial expected-failure? "error.unknown-tags"))
 
       (let [query (query-application sonja id)
             org-tags (get-in query [:organizationMeta :tags])]
-        (:tags query) => ["111" "222"]
+        (:tags query) => ["111111111111111111111111" "222222222222222222222222"]
 
         (fact "application's organization meta includes correct tags with ids as keys"
-          org-tags => {:111 "yl\u00E4maa", :222 "ullakko"}))
+          org-tags => {:111111111111111111111111 "yl\u00E4maa", :222222222222222222222222 "ullakko"}))
 
       (fact "When tag is removed, it is also removed from applications"
         (let [id (create-app-id sonja)]
-          (command sipoo :save-organization-tags :tags [{:id "123" :label "foo"} {:id "321" :label "bar"}]) => ok?
-          (command sonja :add-application-tags :id id :tags ["123" "321"]) => ok?
-          (:tags (query-application sonja id)) => (just ["123" "321"])
-          (command sipoo :save-organization-tags :tags [{:id "123" :label "foo"}]) => ok?
+          (command sipoo :save-organization-tags :tags [{:id "123000000000000000000000" :label "foo"} {:id "321000000000000000000000" :label "bar"}]) => ok?
+          (command sonja :add-application-tags :id id :tags ["123000000000000000000000" "321000000000000000000000"]) => ok?
+          (:tags (query-application sonja id)) => (just ["123000000000000000000000" "321000000000000000000000"])
+          (command sipoo :save-organization-tags :tags [{:id "123000000000000000000000" :label "foo"}]) => ok?
 
           (fact "only 123 is left as 321 was removed"
-            (:tags (query-application sonja id)) => (just ["123"])))))))
+            (:tags (query-application sonja id)) => (just ["123000000000000000000000"])))))))
