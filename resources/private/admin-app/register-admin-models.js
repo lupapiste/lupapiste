7f6b5a0cdf3b011d93c3cdd61f7b65274e2f427e
juha.jokimaki@solita.fi <juha.jokimaki@solita.fi>
2015-12-17 07:46:09 +0200

    Redeleted

--- resources/private/admin-app/register-admin-models.js
+++
@@ -1,6 +0,0 @@
-;(function() {
-  "use strict";
-  lupapisteApp.models.globalAuthModel = authorization.create();
-  lupapisteApp.models.globalAuthModel.refresh();
-  lupapisteApp.models.currentUser = new LUPAPISTE.CurrentUser();
-})();
