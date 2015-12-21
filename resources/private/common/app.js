9735ce9b89400a978c9c552bf5de2f6a2c02f85e
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-21 14:18:18 +0200

    Try to keep authorization model in sync with lockdown status

--- resources/private/common/app.js
+++ resources/private/common/app.js
@@ -146,11 +146,10 @@ var LUPAPISTE = LUPAPISTE || {};
         .error(function(e) {
           if (e.text === "error.unauthorized") {
             hub.send("connection", {status: "session-dead"});
-            setTimeout(self.connectionCheck, 10000);
           } else {
-            hub.send("indicator", {style: "negative", message: e.text});
-            setTimeout(self.connectionCheck, 10000);
+            hub.send("connection", {status: "lockdown", text: e.text});
           }
+          setTimeout(self.connectionCheck, 10000);
         })
         .fail(function() {
           hub.send("connection", {status: "offline"});
@@ -178,6 +177,16 @@ var LUPAPISTE = LUPAPISTE || {};
 
     var offline = false;
     var wasLoggedIn = false;
+    var lockdown = false;
+
+    function unlock() {
+      if (lockdown) {
+        lockdown = false;
+        if (lupapisteApp.models.globalAuthModel) {
+          lupapisteApp.models.globalAuthModel.refreshWithCallback({});
+        }
+      }
+    }
 
     hub.subscribe("login", function() { wasLoggedIn = true; });
 
@@ -186,6 +195,7 @@ var LUPAPISTE = LUPAPISTE || {};
         offline = false;
         pageutil.hideAjaxWait();
       }
+      unlock();
     });
 
     hub.subscribe({type: "connection", status: "offline"}, function () {
@@ -201,6 +211,15 @@ var LUPAPISTE = LUPAPISTE || {};
             {title: loc("session-dead.logout"), fn: self.redirectToHashbang});
         hub.subscribe("dialog-close", self.redirectToHashbang, true);
       }
+      unlock();
+    });
+
+    hub.subscribe({type: "connection", status: "lockdown"}, function (e) {
+      if (!lockdown && lupapisteApp.models.globalAuthModel) {
+        lupapisteApp.models.globalAuthModel.refreshWithCallback({});
+      }
+      lockdown = true;
+      hub.send("indicator", {style: "negative", message: e.text});
     });
 
     self.initSubscribtions = function() {
