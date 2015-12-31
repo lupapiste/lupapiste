3e879eb928dd3fc5f712508315bae6baad3b2561
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2015-12-31 16:12:59 +0200

    use add-scope endpoint in create-scope component

--- resources/private/admin/create-scope-model.js
+++ resources/private/admin/create-scope-model.js
@@ -12,10 +12,35 @@ LUPAPISTE.CreateScopeModel = function(params) {
 
   self.permitType = ko.observable("R");
   self.municipality = ko.observable(_.find(self.municipalities(), {id: municipalityNumber}));
-  self.applicationsEnabled = ko.observable(false);
+  self.applicationEnabled = ko.observable(false);
   self.infoRequests = ko.observable(true);
   self.openInfoRequests = ko.observable(false);
-  self.openInfoRequestEmail = ko.observable();
+  self.openInfoRequestEmail = ko.observable("");
+  self.opening = ko.observable();
+
+
+  self.saveScope = function() {
+    var openingMills = null;
+    if (self.opening()) {
+      openingMills = new Date(self.opening()).getTime();
+    }
+    var data = {organization: self.organization.id(),
+                permitType: self.permitType(),
+                municipality: "297",
+                inforequestEnabled: self.infoRequests(),
+                applicationEnabled: self.applicationEnabled(),
+                openInforequestEnabled: self.openInfoRequests(),
+                openInforequestEmail: self.openInfoRequestEmail(),
+                opening: openingMills};
+
+    ajax
+      .command("add-scope", data)
+      .success(function() {
+        hub.send("organization::scope-added", {orgId: self.organization.id()});
+        hub.send("close-dialog"); // dispose
+      })
+      .call();
+  };
 
 
 };
