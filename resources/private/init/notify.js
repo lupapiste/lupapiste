91b20fc68cb5b2e85a7d30aabf5fe357e55d3a02
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-07 10:10:27 +0200

    pass loc keys in notify initialization

--- resources/private/init/notify.js
+++ resources/private/init/notify.js
@@ -1,18 +1,18 @@
 var notify = (function() {
   "use strict";
 
-  function displayMessage(defaultTitle, title, data) {
+  function displayMessage(defaultTitleKey, title, data) {
     LUPAPISTE.ModalDialog.close();
     if (data !== undefined) {
       LUPAPISTE.ModalDialog.showDynamicOk(title, data);
     } else {
-      LUPAPISTE.ModalDialog.showDynamicOk(defaultTitle, title);
+      LUPAPISTE.ModalDialog.showDynamicOk(loc(defaultTitleKey), title);
     }
   }
 
   return {
-    error: _.partial(displayMessage, loc("error.dialog.title")),
-    success: _.partial(displayMessage, loc("success.dialog.title"))
+    error: _.partial(displayMessage, "error.dialog.title"),
+    success: _.partial(displayMessage, "success.dialog.title")
   };
 
 })();
