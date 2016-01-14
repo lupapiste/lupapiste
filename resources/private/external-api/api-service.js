85bb6436b2504e4ae0b8b031ae30db782203ffea
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-14 15:20:39 +0200

    remove tabs

--- resources/private/external-api/api-service.js
+++ resources/private/external-api/api-service.js
@@ -2,14 +2,14 @@ LUPAPISTE.ExternalApiService = function() {
   "use strict";
 
   hub.subscribe("external-api::show-on-map", function(data) {
-  	var eventData = _.omit(data, "type"); // drop event type
-  	window.parent.LupapisteApi.showPermitOnMap(eventData);
+    var eventData = _.omit(data, "type"); // drop event type
+    window.parent.LupapisteApi.showPermitOnMap(eventData);
   });
 
   return {
-  	enabled: function() {
-  		return window.parent.LupapisteApi;
-  	}
+    enabled: function() {
+      return window.parent.LupapisteApi;
+    }
   };
 };
 
