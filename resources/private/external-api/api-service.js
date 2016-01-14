5909ef511bbada97abd7c3be04dc752cc9a8139a
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-14 17:58:16 +0200

    docstring

--- resources/private/external-api/api-service.js
+++ resources/private/external-api/api-service.js
@@ -1,6 +1,19 @@
 LUPAPISTE.ExternalApiService = function() {
   "use strict";
 
+  /*
+   * LupapisteApi.showPermitOnMap
+   * Description: Show permit on map using location info
+   * Parameters:
+   *   Object, where properties are:
+   *     id (String): id of the permit (ie. 'LP-123-2016-00001')
+   *     address (String): address or description of the permit
+   *     applicant (String): applicant's lastname and firstname concatenated
+   *     authority (String): authority's lastname and firstname concatenated
+   *     location (Object): location object with properties 'x' and 'y' (EPSG:3067 / TM35FIN)
+   *     municipality (String): municipality code of the permit
+   *     operation (String): primary operation name localized in user's language
+   */
   hub.subscribe("external-api::show-on-map", function(data) {
     var eventData = _.omit(data, "type"); // drop event type
     window.parent.LupapisteApi.showPermitOnMap(eventData);
