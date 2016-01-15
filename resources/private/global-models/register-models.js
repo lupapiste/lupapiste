afeb31e7303d38e05ab8dbdcc3e82ca30a872050
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-15 16:49:09 +0200

    rootVMO construction back to 'correct' place

--- resources/private/global-models/register-models.js
+++ resources/private/global-models/register-models.js
@@ -1,6 +1,5 @@
 ;(function() {
   "use strict";
-  lupapisteApp.models.rootVMO = new LUPAPISTE.RootViewModel();
   lupapisteApp.models.application = new LUPAPISTE.ApplicationModel(); // model globally available
 
   lupapisteApp.models.applicationAuthModel = authorization.create();
@@ -8,6 +7,7 @@
   lupapisteApp.models.globalAuthModel = authorization.create();
   lupapisteApp.models.globalAuthModel.refreshWithCallback({}, _.partial(hub.send, "global-auth-model-loaded")); // no application bound
 
+  lupapisteApp.models.rootVMO = new LUPAPISTE.RootViewModel();
 
   lupapisteApp.models.currentUser = new LUPAPISTE.CurrentUser();
 })();
