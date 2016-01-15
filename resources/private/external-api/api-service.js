3051853e0f085f5a0dc385c5d2bd3e0f4e5d82d8
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-15 11:24:38 +0200

    define PermitFilter and fix documentation

--- resources/private/external-api/api-service.js
+++ resources/private/external-api/api-service.js
@@ -1,34 +1,55 @@
 LUPAPISTE.ExternalApiService = function() {
   "use strict";
+ /**
+  * @typedef PermitFilter
+  * @type {object}
+  * @property {string} id - asiointitunnus
+  * @property {string} address - address or description of the permit
+  * @property {string} applicant - applicant's lastname and firstname concatenated
+  * @property {string} authority - authority's lastname and firstname concatenated
+  * @property {object} location - location object with properties 'x' and 'y' (EPSG:3067 / TM35FIN)
+  * @property {string} municipality - municipality code of the permit
+  * @property {string} type - is the permit 'application' or 'inforequest'
+  * @property {string} operation - primary operation name localized in user's language
+  */
 
   /*
    * LupapisteApi.showPermitOnMap
    * Description: Show permit on map using location info
-   * Parameters:
-   *   Object, where properties are:
-   *     id (String): id of the permit (ie. 'LP-123-2016-00001')
-   *     address (String): address or description of the permit
-   *     applicant (String): applicant's lastname and firstname concatenated
-   *     authority (String): authority's lastname and firstname concatenated
-   *     location (Object): location object with properties 'x' and 'y' (EPSG:3067 / TM35FIN)
-   *     municipality (String): municipality code of the permit
-   *     operation (String): primary operation name localized in user's language
+   * @param {PermitFilter}
    */
   hub.subscribe("external-api::show-on-map", function(data) {
-    var eventData = _.omit(data, "type"); // drop event type
+    var eventData = _.omit(data, "eventType"); // drop event type
     window.parent.LupapisteApi.showPermitOnMap(eventData);
   });
 
+  /*
+   * LupapisteApi.openPermit
+   * Description: Open permit, provides ID of the permit.
+   * @param {string} id id of the permit (ie. 'LP-123-2016-00001')
+   * @param {string} type'application' OR 'inforequest'
+   */
   hub.subscribe("external-api::open-application", function(data) {
     var id = _.get(data, "id");
-    window.parent.LupapisteApi.openPermit(id);
+    var type = _.get(data, "type");
+    window.parent.LupapisteApi.openPermit(id, type);
   });
 
+  /*
+   * LupapisteApi.showPermitsOnMap
+   * Description: Show given permits on map.
+   * @param {Array<PermitFilter>} data Array of PermitFilter objects
+   */
   hub.subscribe("external-api::filtered-permits", function(data) {
-    var eventData = _.omit(data, "type"); // drop event type
+    var eventData = _.omit(data, "eventType"); // drop event type
     window.parent.LupapisteApi.showPermitsOnMap(eventData);
   });
 
+  /*
+   * LupapisteApi.integrationSent
+   * Description: Function is called when integration (KRYSP) was successully sent.
+   * @param {string} id Lupapiste ID for permit
+   */
   hub.subscribe("external-api::integration-sent", function(data) {
     var id = _.get(data, "id");
     window.parent.LupapisteApi.integrationSent(id);
