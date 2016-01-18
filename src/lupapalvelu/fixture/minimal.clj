875c6ba6c510b663a41913d84304bef27f0d9cf3
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-01-18 09:20:06 +0200

    add IPv6 localhost IP

--- src/lupapalvelu/fixture/minimal.clj
+++ src/lupapalvelu/fixture/minimal.clj
@@ -737,7 +737,7 @@
                                {:name {:fi "Rakennusvalvonta", :sv "Rakennusvalvonta"}
                                 :url "http://www.porvoo.fi/fi/haku/palveluhakemisto/?a=viewitem&itemid=1030"}]
                        :selected-operations (map first (filter (fn [[_ v]] (#{"R" "YI" "YL"} (name (:permit-type v)))) operations/operations))
-                       :allowedAutologinIPs ["127.0.0.1" "109.204.231.126"]
+                       :allowedAutologinIPs ["0:0:0:0:0:0:0:1" "127.0.0.1" "109.204.231.126"]
                        :permanent-archive-enabled false}
 
                       ;; Oulu R
