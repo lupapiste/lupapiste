231d0030f5ea2e60907170211f63e0c91b6e1969
Antti Ramo <antti.ramo@solita.fi>
2016-02-02 11:03:36 +0200

    do not try to update document validation errors of undefined

--- resources/private/services/document-data-service.js
+++ resources/private/services/document-data-service.js
@@ -115,13 +115,13 @@ LUPAPISTE.DocumentDataService = function(params) {
       )
       .success(function(e) {
         var doc = self.findDocumentById(documentId);
-        doc.validationResults(e.results);
+        doc && doc.validationResults(e.results);
         indicator({type: "saved"});
         cb(e);
       })
       .error(function (e) {
         var doc = self.findDocumentById(documentId);
-        doc.validationResults(e.results);
+        doc && doc.validationResults(e.results);
         indicator({type: "err"});
       })
       .fail(function () {
