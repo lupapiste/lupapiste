3bd8d1908f3112ad17f85d06de365bc30483b6d8
Vespe Savikko <vespe.savikko@solita.fi>
2016-02-09 11:51:54 +0200

    Changes after code review.
    The id check reinstalled also as an explicit check,
    because it is the most performant way to search
    for fully-formed id.

--- src/lupapalvelu/application_search.clj
+++ src/lupapalvelu/application_search.clj
@@ -55,17 +55,17 @@
   Sample matching: 'ear onk' will match 'year of the monkey' after fuzzying"
   [term]
   (let [whitespace "[\\s\u00a0]+"
-        fuzzy (->> (ss/split term (re-pattern whitespace))
-                   (map #(java.util.regex.Pattern/quote %))
-                   (ss/join (str ".*" whitespace ".*")))]
+        fuzzy      (->> (ss/split term (re-pattern whitespace))
+                        (map #(java.util.regex.Pattern/quote %))
+                        (ss/join (str ".*" whitespace ".*")))]
     (str "^.*" fuzzy ".*$")))
 
 
 (defn- make-free-text-query [filter-search]
-  (let [search-keys   [:address :verdicts.kuntalupatunnus :_applicantIndex :foreman :_id]
-        fuzzy  (fuzzy-re filter-search)
-        or-query      {$or (map #(hash-map % {$regex fuzzy $options "i"}) search-keys)}
-        ops           (operation-names filter-search)]
+  (let [search-keys [:address :verdicts.kuntalupatunnus :_applicantIndex :foreman :_id]
+        fuzzy       (fuzzy-re filter-search)
+        or-query    {$or (map #(hash-map % {$regex fuzzy $options "i"}) search-keys)}
+        ops         (operation-names filter-search)]
     (if (seq ops)
       (update-in or-query [$or] concat [{:primaryOperation.name {$in ops}}
                                         {:secondaryOperations.name {$in ops}}])
@@ -74,6 +74,7 @@
 (defn make-text-query [filter-search]
   {:pre [filter-search]}
   (cond
+    (re-matches #"^([Ll][Pp])-\d{3}-\d{4}-\d{5}$" filter-search) {:_id (ss/upper-case filter-search)}
     (re-matches p/property-id-pattern filter-search) {:propertyId (p/to-property-id filter-search)}
     :else (make-free-text-query filter-search)))
 
