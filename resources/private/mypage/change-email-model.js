e11981e1dfeddae81eeb7d2fefce72ab7433096d
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-02-23 15:54:56 +0200

    command stub

--- resources/private/mypage/change-email-model.js
+++ resources/private/mypage/change-email-model.js
@@ -3,18 +3,29 @@ LUPAPISTE.ChangeEmailModel = function(params) {
   var self = this;
   self.newEmail = ko.observable("").extend({email: true});
   self.validated = ko.validatedObservable([self.newEmail]);
+  self.processing = ko.observable(false);
+  self.pending = ko.observable(false);
 
-  // TODO change command name after a new command is added
-  self.authorized = _.partial(params.authorization.ok, "change-passwd");
+  self.authorized = _.partial(params.authorization.ok, "change-email");
   self.ok = ko.pureComputed(function() {
-    return self.newEmail() && self.validated.isValid() && self.newEmail() !== params.data.email() && self.authorized();
+    return self.newEmail() && self.validated.isValid() && self.newEmail() !== params.data.email() && self.authorized() && !self.processing();
   });
+
+  self.save = function() {
+    if (self.ok()) {
+      ajax.command("change-email", {email: self.newEmail()})
+        .success(debug) // TODO show help?
+        .processing(self.processing)
+        .pending(self.pending)
+        .call();
+    }
+  };
 
   // Extend the accordion component, render content with change-email-template.
   // Parameters for the parent component:
   var superParams = {ltitle: params.ltitle,
                      accordionContentTemplate: "change-email-template",
-                     accordionContentTemplateData: _.merge(params.data, _.pick(self, ["newEmail", "authorized", "ok"]))};
+                     accordionContentTemplateData: _.merge(params.data, _.pick(self, ["newEmail", "authorized", "processing", "pending", "ok", "save"]))};
   ko.utils.extend(self, new LUPAPISTE.AccordionModel(superParams));
 
   // Initialize new email with current value
