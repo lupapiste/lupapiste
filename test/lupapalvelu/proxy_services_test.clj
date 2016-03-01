d6479ff45413e73263ce75c464705214873227f2
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-03-01 15:03:12 +0200

    fixed test

--- test/lupapalvelu/proxy_services_test.clj
+++ test/lupapalvelu/proxy_services_test.clj
@@ -17,8 +17,8 @@
   (fact (parse-address "t\u00F6n\u00F6 42, h\u00E4me")  => ["t\u00F6n\u00F6" "42" "h\u00E4me"]))
 
 (fact
-  ((secure (fn [r _] r) "WMS") {:headers {"set-cookie" "cookie" "x-foo" "bar"}}) => {:headers {"x-foo" "bar"}}
-  ((secure identity) {:headers {"cookie" "cookie-x=value;cookie-y=1" "x-foo" "bar"}}) => {:headers {"x-foo" "bar"}}
+  ((secure (fn [r _] r) "WMS") {:headers {"set-cookie" "cookie" "foo" "bar"}}) => {:headers {"foo" "bar"}}
+  ((secure identity) {:headers {"cookie" "cookie-x=value;cookie-y=1" "foo" "bar"}}) => {:headers {"foo" "bar"}}
   ((secure identity) {:params {:a "null-char(\0);"}
                       :query-params {:WKT "POLYGON(1,\n\n2);" :term "it\u00E4inen"}
                       :form-params {:WKT "POLYGON(1,\t2);"}}) => {:params {:a "null-char();"}
