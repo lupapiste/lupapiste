2d13b898e63c667811cbfd085937d563dae2adf7
Aki Helin <aki.helin@solita.fi>
2016-03-10 12:56:39 +0200

    added muncipality-borders stub and itest

--- src/lupapalvelu/municipality_api.clj
+++ src/lupapalvelu/municipality_api.clj
@@ -3,3 +3,8 @@
             [sade.core :refer :all]
             [lupapalvelu.action :refer [defquery]]))
 
+(defquery municipality-borders
+  {:user-roles #{:anonymous}
+   :description "Get municipality borders in GeoJSON format."}
+  [command]
+  (ok :data {}))
