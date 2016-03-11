275713044510f52a0842d7ea66fd98c4d0d97833
Antti Ramo <antti.ramo@solita.fi>
2016-03-11 14:28:31 +0200

    update appeal data model - added decision and modified target

--- src/lupapalvelu/appeal.clj
+++ src/lupapalvelu/appeal.clj
@@ -6,21 +6,32 @@
 
 (defschema Appeal
   "Schema for a verdict appeal."
-  {:id            ssc/ObjectIdStr       ;; 
-   :paatos-id     ssc/ObjectIdStr       ;; Refers to verdicts.paatokset.id
-   :appellant     {:firstName  sc/Str   ;; Person who made the appeal
-                   :lastName   sc/Str}  ;;
-   :made          ssc/Timestamp         ;; Date of appeal made - defined manually by authority
-   :created       ssc/Timestamp})
+  {:id                         ssc/ObjectIdStr  ;; 
+   :target                     {:type   (sc/enum :verdict :appeal-verdict)
+                                :id     ssc/ObjectIdStr}
+   (sc/optional-key :decision) ssc/ObjectIdStr  ;; Decision for appeal made by court or commitee
+   :appellant                  sc/Str           ;; Name of the person who made the appeal
+   :made                       ssc/Timestamp})  ;; Date of appeal made - defined manually by authority
 
-(defn create-appeal [paatos-id appellant made now]
+(defn create-appeal-for-verdict [paatos-id appellant made]
   {:id        (mongo/create-id)
-   :paatos-id paatos-id
+   :target    {:type :verdit
+               :id    paatos-id}
    :appellant appellant
-   :made      made
-   :created   now})
+   :made      made})
 
-(defn new-appeal-mongo-updates [paatos-id appellant made now]
-  (when-let [appeal (->> (create-appeal paatos-id appellant made now)
+(defn create-appeal-for-appeal-verdict [appeal-verdict-id appellant made]
+  {:id        (mongo/create-id)
+   :target    {:type :appeal-verdit
+               :id   appeal-verdict-id}
+   :appellant appellant
+   :made      made})
+
+(defn appeal-decision-updates [target-verdict-id decisive-verdict-id]
+  {:query   {:appeals.target.id target-verdict-id}
+   :updates {$set {:appeals.$.decision decisive-verdict-id}}})
+
+(defn new-appeal-for-verdict-mongo-updates [paatos-id appellant made now]
+  (when-let [appeal (->> (create-appeal-for-verdict paatos-id appellant made now)
                          (sc/check Appeal))]
     {$push {:appeals appeal}}))
