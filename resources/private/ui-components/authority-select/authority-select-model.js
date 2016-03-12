b823ddba6c86af2ea56b51e0491353731899ffa4
Vespe Savikko <vespe.savikko@solita.fi>
2016-03-12 13:10:03 +0200

    Authority select without dialog.

--- resources/private/ui-components/authority-select/authority-select-model.js
+++ resources/private/ui-components/authority-select/authority-select-model.js
@@ -1,18 +1,64 @@
-LUPAPISTE.AuthoritySelectModel = function(params) {
+// Assign an authority to the application.
+LUPAPISTE.AuthoritySelectModel = function() {
   "use strict";
   var self = this;
 
-  // view properties
-  self.kind = params.kind;
+  ko.utils.extend( self, new LUPAPISTE.ComponentBaseModel());
 
-  // global models
-  self.authorization = lupapisteApp.models.applicationAuthModel;
-  self.authority = lupapisteApp.models.application.authority;
+  // Application id can be thought as a link to the application
+  // changes. By unwrapping it in the computeds below, we
+  // make sure that the authority information is in sync with
+  // the current actual application.
+  var appId = self.disposedPureComputed( function() {
+    return lupapisteApp.models.application.id();
+  });
 
-  self.edit = _.partial(hub.send, "show-dialog",
-                       {ltitle: "application.assignee",
-                        size: "small",
-                        component: "authority-select-dialog",
-                        componentParams: params});
+  self.canEdit = self.disposedPureComputed( function() {
+    return lupapisteApp.models.applicationAuthModel.ok( "assign-application");
+  });
 
+  self.assigneeId = ko.observable();
+
+  self.oldAssignee = self.disposedComputed( function() {
+    var old = appId()
+            ? ko.mapping.toJS( lupapisteApp.models.application.authority())
+            : {};
+    self.assigneeId( old.id );
+    return old;
+  });
+
+  self.pending = ko.observable(false);
+
+  self.authorities = ko.observableArray();
+
+  self.fullName = function( obj ) {
+    obj = _.defaults( ko.mapping.toJS( obj ), {firstName: "", lastName: ""});
+
+    return obj.lastName + " " + obj.firstName;
+  };
+
+  self.disposedComputed( function() {
+    if( appId() && self.canEdit() ) {
+      ajax.query("application-authorities", {id: appId()})
+      .success(function(res) {
+        self.authorities( res.authorities );
+      })
+      .pending(self.pending)
+      .call();
+    }
+  });
+
+
+  self.disposedComputed( function() {
+    var assigneeId = self.assigneeId() || "";
+    var oldId = self.oldAssignee().id || "";
+    if ( self.canEdit() && appId() && assigneeId !== oldId) {
+      ajax.command("assign-application", {id: appId(), assigneeId: assigneeId})
+        .processing(self.pending)
+        .success(function() {
+          repository.load(appId, self.pending, null, false);
+        })
+        .call();
+    }
+  });
 };
