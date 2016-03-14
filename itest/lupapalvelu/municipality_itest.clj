eac03840b005e8c1ca882a40a18b05754b9a0af8
Erno Kuusela <erno.kuusela@solita.fi>
2016-03-14 13:57:17 +0000

    make municipality itest also test active-municipalities

--- itest/lupapalvelu/municipality_itest.clj
+++ itest/lupapalvelu/municipality_itest.clj
@@ -9,18 +9,29 @@
     resp => ok?
     (:data resp) => empty?))
 
-
 (facts "municipality-active"
   (fact "only info requests enabled"
     (let [m (query pena :municipality-active :municipality "997")]
       (:applications m) => empty?
       (:infoRequests m) => ["R"]
-      (:opening m) => empty?))
+      (:opening m) => empty?)
+    (let [all-active-m (:municipalities (query pena :active-municipalities))
+          m (first (filter #(= (:id %) "997") all-active-m))]
+      (:applications m) => empty?
+      (:infoRequests m) => ["R"]
+      (:opening m) => empty?
+      ))
   (fact "only applications enabled"
     (let [m (query pena :municipality-active :municipality "998")]
       (:applications m) => ["R"]
       (:infoRequests m) => empty?
+      (:opening m) => empty?)
+    (let [all-active-m (:municipalities (query pena :active-municipalities))
+          m (first (filter #(= (:id %) "998") all-active-m))]
+      (:applications m) => ["R"]
+      (:infoRequests m) => empty?
       (:opening m) => empty?))
+
   (fact "nothing enabled, but coming"
     (command admin :update-organization
       :permitType "R"
@@ -29,9 +40,10 @@
       :applicationEnabled false
       :openInforequestEnabled false
       :openInforequestEmail "someone@localhost"
-
       :opening 123)
     (let [m (query pena :municipality-active :municipality "999")]
       (:applications m) => empty?
       (:infoRequests m) => empty?
-      (:opening m) => [{:permitType "R", :opening 123}])))
+      (:opening m) => [{:permitType "R", :opening 123}])
+    (let [all-active-m (:municipalities (query pena :active-municipalities))]
+      (:opening  (first (filter #(= (:id %) "999") all-active-m))) => [{:opening 123, :permitType "R"}])))
