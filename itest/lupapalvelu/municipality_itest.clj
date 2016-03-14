c8ff7701f6bf863b5aa07139414d4f7ce1b4e9cc
Erno Kuusela <erno.kuusela@solita.fi>
2016-03-14 13:56:34 +0000

    move municipality itest from organization_itest to municipality_itest

--- itest/lupapalvelu/municipality_itest.clj
+++ itest/lupapalvelu/municipality_itest.clj
@@ -1,5 +1,5 @@
 (ns lupapalvelu.municipality-itest
-  (:require [lupapalvelu.itest-util :refer [query apply-remote-minimal pena ok?]]
+  (:require [lupapalvelu.itest-util :refer [admin command query apply-remote-minimal pena ok?]]
             [midje.sweet :refer :all]))
 
 (apply-remote-minimal)
@@ -8,3 +8,30 @@
   (let [resp (query pena :municipality-borders)]
     resp => ok?
     (:data resp) => empty?))
+
+
+(facts "municipality-active"
+  (fact "only info requests enabled"
+    (let [m (query pena :municipality-active :municipality "997")]
+      (:applications m) => empty?
+      (:infoRequests m) => ["R"]
+      (:opening m) => empty?))
+  (fact "only applications enabled"
+    (let [m (query pena :municipality-active :municipality "998")]
+      (:applications m) => ["R"]
+      (:infoRequests m) => empty?
+      (:opening m) => empty?))
+  (fact "nothing enabled, but coming"
+    (command admin :update-organization
+      :permitType "R"
+      :municipality "999"
+      :inforequestEnabled false
+      :applicationEnabled false
+      :openInforequestEnabled false
+      :openInforequestEmail "someone@localhost"
+
+      :opening 123)
+    (let [m (query pena :municipality-active :municipality "999")]
+      (:applications m) => empty?
+      (:infoRequests m) => empty?
+      (:opening m) => [{:permitType "R", :opening 123}])))
--- itest/lupapalvelu/organization_itest.clj
+++ itest/lupapalvelu/organization_itest.clj
@@ -274,32 +274,6 @@
           (:kopiolaitos-orderer-email org) => kopiolaitos-orderer-email
           (get-in organizationMeta [:kopiolaitos :kopiolaitosOrdererEmail]) => kopiolaitos-orderer-email)))))
 
-(facts "municipality-active"
-  (fact "only info requests enabled"
-    (let [m (query pena :municipality-active :municipality "997")]
-      (:applications m) => empty?
-      (:infoRequests m) => ["R"]
-      (:opening m) => empty?))
-  (fact "only applications enabled"
-    (let [m (query pena :municipality-active :municipality "998")]
-      (:applications m) => ["R"]
-      (:infoRequests m) => empty?
-      (:opening m) => empty?))
-  (fact "nothing enabled, but coming"
-    (command admin :update-organization
-      :permitType "R"
-      :municipality "999"
-      :inforequestEnabled false
-      :applicationEnabled false
-      :openInforequestEnabled false
-      :openInforequestEmail "someone@localhost"
-
-      :opening 123)
-    (let [m (query pena :municipality-active :municipality "999")]
-      (:applications m) => empty?
-      (:infoRequests m) => empty?
-      (:opening m) => [{:permitType "R", :opening 123}])))
-
 
 (facts "organization-operations-attachments"
   (fact "Invalid operation is rejected"
