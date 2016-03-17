5f038dafe8f54cc563073fd99a78d84bf72e60aa
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-03-17 09:34:43 +0200

    LPK-1514 fix vector problem with double redirect-after-login query-param + sanitize hashbang

--- itest/lupapalvelu/web_itest.clj
+++ itest/lupapalvelu/web_itest.clj
@@ -24,3 +24,24 @@
       (let [{body :body :as resp} (http-get (str (server-address) "/api/query/redirect-after-login") (assoc params :as :json))]
         resp => http200?
         (:url body) => "foo/bar"))))
+
+(facts "2x redirect-after-login"
+  (let [store (atom {})
+        initial-params {:cookie-store (->cookie-store store)
+                        :follow-redirects false
+                        :throw-exceptions false}
+        resp (http-get (str (server-address) "/app/fi/applicant" "?redirect-after-login=/foo/bar&redirect-after-login=/foo/bar2") initial-params)
+        anti-csrf-token (codec/url-decode (.getValue (@store "anti-csrf-token")))
+        params (assoc-in initial-params [:headers "x-anti-forgery-token"] anti-csrf-token)]
+    (:status resp) => 302
+    (:headers resp) => (contains {"Location" (str (server-address) (env/value :frontpage :fi))})
+
+    (fact "Login succeeds"
+      (let [{body :body :as login} (http-post (str (server-address) "/api/login") (assoc params :form-params {:username "pena" :password "pena"} :as :json))]
+        body => ok?
+        login => http200?))
+
+    (fact "url is returned"
+      (let [{body :body :as resp} (http-get (str (server-address) "/api/query/redirect-after-login") (assoc params :as :json))]
+        resp => http200?
+        (:url body) => "foo/bar2"))))
