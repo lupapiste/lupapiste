9f96b7798129098b489790f75450b0938d3d82b0
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-03-17 14:23:21 +0200

    add created and :type enum for appeal

--- src/lupapalvelu/appeal.clj
+++ src/lupapalvelu/appeal.clj
@@ -6,18 +6,28 @@
 
 (defschema Appeal
   "Schema for a verdict appeal."
-  {:id                         ssc/ObjectIdStr  ;; 
-   :target-verdict             ssc/ObjectIdStr  ;; refers to verdicts.paatokset.id
-   :appellant                  sc/Str           ;; Name of the person who made the appeal
-   :made                       ssc/Timestamp})  ;; Date of appeal made - defined manually by authority
+  {:id             ssc/ObjectIdStr                          ;;
+   :target-verdict ssc/ObjectIdStr                          ;; refers to verdicts.paatokset.id
+   :type           (sc/enum
+                     "appeal"                               ;; Valitus
+                     "rectification"                        ;; Oikasuvaatimus
+                     )
+   :appellant      sc/Str                                   ;; Name of the person who made the appeal
+   :made           ssc/Timestamp                            ;; Date of appeal made - defined manually by authority
+   :created        ssc/Timestamp                            ;; Entry creation time
+   })
 
-(defn create-appeal [target-verdict-id appellant made]
+(defn create-appeal [target-verdict-id type appellant made created]
   {:id             (mongo/create-id)
    :target-verdict target-verdict-id
+   :type           type
    :appellant      appellant
-   :made           made})
+   :made           made
+   :created        created})
 
-(defn new-appeal-mongo-updates [target-verdict-id appellant made now]
-  (when-let [appeal (->> (create-appeal target-verdict-id appellant made now)
+(defn new-appeal-mongo-updates
+  "Returns $push mongo update map of appeal to :appeals property"
+  [target-verdict-id type appellant made now]
+  (when-let [appeal (->> (create-appeal target-verdict-id type appellant made now)
                          (sc/check Appeal))]
     {$push {:appeals appeal}}))
