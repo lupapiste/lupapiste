9b68cfdc033d5965ecf59b15a1b09e9066005b9f
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-03-18 10:30:03 +0200

    create-appeal command pushes valid appeal to application's appeals array

---
+++ itest/lupapalvelu/appeal_itest.clj
@@ -0,0 +1,53 @@
+(ns lupapalvelu.appeal-itest
+  (:require [midje.sweet :refer :all]
+            [lupapalvelu.factlet :refer :all]
+            [lupapalvelu.itest-util :refer :all]
+            [sade.core :refer [now]]
+            [lupapalvelu.mongo :as mongo]))
+
+(apply-remote-minimal)
+
+(facts "Creating appeal"
+  (let [{app-id :id} (create-and-submit-application pena :operation "kerrostalo-rivitalo" :propertyId sipoo-property-id)]
+    app-id => string?
+    (fact "Can't add appeal before verdict"
+      (command sonja :create-appeal
+               :id app-id
+               :targetId (mongo/create-id)
+               :type "appeal"
+               :appellant "Pena"
+               :made 123456
+               :text "foo") => (partial expected-failure? :error.command-illegal-state))
+
+    (let [{vid :verdict-id} (give-verdict sonja app-id :verdictId "321-2016")]
+      vid => string?
+      (fact "wrong verdict ID"
+        (command sonja :create-appeal
+                 :id app-id
+                 :targetId (mongo/create-id)
+                 :type "appeal"
+                 :appellant "Pena"
+                 :made 123456
+                 :text "foo") => (partial expected-failure? :error.verdict-not-found))
+      (fact "successful appeal"
+        (command sonja :create-appeal
+                 :id app-id
+                 :targetId vid
+                 :type "appeal"
+                 :appellant "Pena"
+                 :made (now)
+                 :text "foo") => ok?)
+      (fact "text is optional"
+        (command sonja :create-appeal
+                 :id app-id
+                 :targetId vid
+                 :type "rectification"
+                 :appellant "Pena"
+                 :made (now)) => ok?)
+      (fact "appeal is saved to application to be viewed"
+        (map :type (:appeals (query-application pena app-id))) => (just ["appeal" "rectification"]))
+      (fact "appeal query is OK"
+        (let [response-data (:data (query pena :appeals :id app-id))]
+          (keys response-data) => (just [:appeals :appealVerdicts])
+          (count (:appeals response-data)) => 2
+          (count (:appealVerdicts response-data)) => 0)))))
--- src/lupapalvelu/appeal_api.clj
+++ src/lupapalvelu/appeal_api.clj
@@ -1,23 +1,37 @@
 (ns lupapalvelu.appeal-api
   (:require [sade.core :refer :all]
+            [sade.util :as util]
             [lupapalvelu.action :refer [defquery defcommand] :as action]
             [lupapalvelu.appeal :as appeal]
             [lupapalvelu.appeal-verdict :as appeal-verdict]
             [lupapalvelu.states :as states]
-            [sade.util :as util]))
+            [lupapalvelu.mongo :as mongo]))
 
 (defn- verdict-exists
   "Input validator to validate that for selected verdictId a verdict exists"
-  [{{:keys [verdicts]} :application {verdictId :targetId} :data}]
+  [{{verdictId :targetId} :data} {:keys [verdicts]}]
   (when-not (util/find-first #(= verdictId (:id %)) verdicts)
     (fail :error.verdict-not-found)))
 
 (defcommand create-appeal
-  {:parameters [targetId type appellant made]
-   :user-roles #{:authority}
-   :states     states/post-verdict-states
-   :input-validators [verdict-exists
-                      appeal/input-validator
-                      (partial action/number-parameters [:made])]}
+  {:parameters          [id targetId type appellant made]
+   :optional-parameters [text]
+   :user-roles          #{:authority}
+   :states              states/post-verdict-states
+   :input-validators    [appeal/input-validator
+                         (partial action/number-parameters [:made])]
+   :pre-checks          [verdict-exists]}
+  [{app :application :as command}]
+  (if-let [updates (appeal/new-appeal-mongo-updates targetId type appellant made text)]
+    (action/update-application
+      command
+      updates)
+    (fail :error.invalid-appeal)))
+
+(defquery appeals
+  {:parameters [id]
+   :user-roles #{:authority :applicant}
+   :states     states/post-verdict-states}
   [command]
-  (println "hi"))
+  (ok :data (-> (mongo/by-id :applications id [:appeals :appealVerdicts])
+                (dissoc :id))))
