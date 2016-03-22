cee5ce7eae09a0c874f60cbd98ef4ff01047e6b8
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-03-22 13:45:09 +0200

    delete appeal commands + itests

--- itest/lupapalvelu/appeal_itest.clj
+++ itest/lupapalvelu/appeal_itest.clj
@@ -54,7 +54,7 @@
           (count (get response-data verdictid-key)) => 2))
 
       (fact* "updating appeal when appealId is given"
-        (let [appeals-before  (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals-before  (appeals-for-verdict pena app-id vid)
               target-appeal   (first appeals-before)
               _ (command sonja :upsert-appeal :id app-id
                          :verdictId vid
@@ -62,7 +62,7 @@
                          :appellant "Teppo"
                          :datestamp created
                          :appealId (:id target-appeal)) => ok?
-              appeals-after   (get (:data (query pena :appeals :id app-id)) (keyword vid))
+              appeals-after   (appeals-for-verdict pena app-id vid)
               updated-target-appeal   (first appeals-after)]
 
           (fact "Count of appeals is the same"
@@ -81,7 +81,7 @@
                    :datestamp created
                    :appealId "foobar") => (partial expected-failure? :error.unknown-appeal))
 
-        (let [appeals (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals (appeals-for-verdict pena app-id vid)
               test-appeal (first appeals)]
           (fact "Appeal must be valid when upserting"
             (command sonja :upsert-appeal :id app-id
@@ -89,7 +89,28 @@
                      :type "trolol"
                      :appellant "Teppo"
                      :datestamp created
-                     :appealId (:id test-appeal)) => (partial expected-failure? :error.invalid-appeal)))))))
+                     :appealId (:id test-appeal)) => (partial expected-failure? :error.invalid-appeal))))
+
+      (fact "Delete"
+        (let [appeals       (appeals-for-verdict pena app-id vid)
+              first-appeal  (first appeals)
+              second-appeal (second appeals)]
+          (count appeals) => 2
+
+          (command sonja :delete-appeal :id app-id
+                   :verdictId vid
+                   :appealId (:id first-appeal)) => ok?
+
+          (fact "sane parameters"
+            (command sonja :delete-appeal :id app-id
+                     :verdictId vid
+                     :appealId "foobar") => (partial expected-failure? :error.unknown-appeal)
+            (command sonja :delete-appeal :id app-id
+                     :verdictId "foofaa"
+                     :appealId (:id second-appeal)) => (partial expected-failure? :error.verdict-not-found))
+
+          (fact "one appeal left"
+            (count (appeals-for-verdict pena app-id vid)) => 1))))))
 
 (facts "Upserting appeal verdicts"
   (let [{app-id :id} (create-and-submit-application pena :operation "kerrostalo-rivitalo" :propertyId sipoo-property-id)
@@ -151,7 +172,7 @@
           (:type (second (get response-data verdictid-key))) => "appealVerdict"))
 
       (fact "appeal can't be edited after appeal verdict is created"
-        (let [appeals (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals (appeals-for-verdict pena app-id vid)
               test-appeal (first appeals)]
           (command sonja :upsert-appeal
                    :id app-id
@@ -166,26 +187,26 @@
             (:editable test-appeal) => false)))
 
       (fact "upsert is validated"
-        (let [appeals             (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals             (appeals-for-verdict pena app-id vid)
               test-appeal-verdict (second appeals)
               appeal              (first appeals)]
           (fact "Can't update appeal with appeal-verdict endpoint"
             (command sonja :upsert-appeal-verdict :id app-id
                      :verdictId vid
                      :giver "Teppo"
-                     :datestamp created
+                     :datestamp (+ created 1)
                      :appealId (:id appeal)) => (partial expected-failure? :error.unknown-appeal-verdict))))
 
       (fact* "Upsert updates successfully"
-        (let [appeals-before  (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals-before  (appeals-for-verdict pena app-id vid)
               target-appeal-verdict   (second appeals-before)
               _ (:editable target-appeal-verdict) => true
               _ (command sonja :upsert-appeal-verdict :id app-id
                          :verdictId vid
                          :giver "Seppo"
-                         :datestamp created
+                         :datestamp (+ created 1)
                          :appealId (:id target-appeal-verdict)) => ok?
-              appeals-after   (get (:data (query pena :appeals :id app-id)) (keyword vid))
+              appeals-after   (appeals-for-verdict pena app-id vid)
               updated-target-appeal-verdict   (second appeals-after)]
 
           (fact "Count of appeals is the same"
@@ -203,13 +224,52 @@
                  :appellant "Pena again"
                  :datestamp (now)
                  :text "new appeal") => ok?
-        (let [appeals (get (:data (query pena :appeals :id app-id)) (keyword vid))
+        (let [appeals (appeals-for-verdict pena app-id vid)
               target-appeal-verdict (second appeals)]
-          (command sonja :upsert-appeal-verdict :id app-id
-                   :verdictId vid
-                   :giver "Seppo"
-                   :datestamp created
-                   :appealId (:id target-appeal-verdict)) => (partial expected-failure? :error.appeal-already-exists)
+          (fact "upsert"
+            (command sonja :upsert-appeal-verdict :id app-id
+                     :verdictId vid
+                     :giver "Seppo"
+                     :datestamp created
+                     :appealId (:id target-appeal-verdict)) => (partial expected-failure? :error.appeal-already-exists))
+
+          (fact "delete"
+            (command sonja :delete-appeal-verdict :id app-id
+                     :verdictId vid
+                     :appealId (:id target-appeal-verdict)) => (partial expected-failure? :error.appeal-already-exists))
 
           (fact "query has editable flag set correctly"
-            (:editable target-appeal-verdict) => false))))))
+            (:editable target-appeal-verdict) => false)))
+
+      (facts "Deleting"
+        (let [appeals (appeals-for-verdict sonja app-id vid)]
+          ; types of first second last:
+          (map :type appeals) => (just ["rectification" "appealVerdict" "appeal"])
+          (fact "Pena can't remove"
+            (command pena :delete-appeal :id app-id
+                     :verdictId vid
+                     :appealId (-> appeals last :id)) => unauthorized?)
+          (fact "First remove appeal"
+            (command sonja :delete-appeal :id app-id
+                     :verdictId vid
+                     :appealId (-> appeals last :id)) => ok?)
+          (fact "Can't still remove first rectification as verdict exists"
+            (command sonja :delete-appeal :id app-id
+                     :verdictId vid
+                     :appealId (-> appeals first :id)) => (partial expected-failure? :error.appeal-verdict-already-exists))
+          (fact "Can't remove verdict using command for appeals"
+            (command sonja :delete-appeal :id app-id
+                     :verdictId vid
+                     :appealId (-> appeals second :id)) => (partial expected-failure? :error.unknown-appeal))
+          (fact "Parameters must be sane"
+            (command sonja :delete-appeal-verdict :id app-id
+                     :verdictId vid
+                     :appealId "foobar") => (partial expected-failure? :error.unknown-appeal-verdict))
+          (fact "Appeal verdict can now be removed with correct parameters and application state"
+            (command sonja :delete-appeal-verdict :id app-id
+                     :verdictId vid
+                     :appealId (-> appeals second :id)) => ok?)
+
+          (let [appeals-after (appeals-for-verdict sonja app-id vid)]
+            (count appeals-after) => 1
+            (:editable (last appeals-after)) => true))))))
