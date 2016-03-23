01700e097fcbcd79024f8e922deb66290a7b6c07
Vespe Savikko <vespe.savikko@solita.fi>
2016-03-23 18:53:12 +0200

    JSHint fixes.

--- resources/private/common/register-components.js
+++ resources/private/common/register-components.js
@@ -118,7 +118,7 @@ jQuery(document).ready(function() {
     {name: "cell-span"},
     {name: "cell-textarea", model: "cell-model"},
     {name: "cell-date"},
-    {name: "cell-select"},
+    {name: "cell-select"}
 ];
 
   _.forEach(components, function(component) {
