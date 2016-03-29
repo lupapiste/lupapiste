319d3195d4ef95eaea5c6bad56aaedd6bb1da17f
Michael Holopainen <michael.holopainen@solita.fi>
2016-03-29 18:35:03 +0300

    rewrote the dynamic table code in libre template

--- src/lupapalvelu/pdf/libreoffice_template.clj
+++ src/lupapalvelu/pdf/libreoffice_template.clj
@@ -26,6 +26,56 @@
 (defn- localized-text [lang value]
   (if (nil? value) "" (xml-escape (i18n/localize lang value))))
 
+(defn- build-xml-history-row [{:keys [type text version category contents ts user]} lang]
+  ;(debug "row data:" data)
+  [""
+   (str
+     (case category
+       :document (i18n/localize lang "caseFile.documentSubmitted")
+       :request-statement (i18n/localize lang "caseFile.operation.statement.request")
+       :request-neighbor (i18n/localize lang "caseFile.operation.neighbor.request")
+       :request-review (i18n/localize lang "caseFile.operation.review.request")
+       :review (i18n/localize lang "caseFile.operation.review")
+       :tos-function-change (i18n/localize lang "caseFile.tosFunctionChange")
+       "")
+     ": "
+     (cond
+       text text
+       (map? type) (i18n/localize lang "attachmentType" (:type-group type) (:type-id type))
+       type (i18n/localize lang type))
+     (when contents
+       (str ", " contents))
+     (when (seq version)
+       (str ", v. " (:major version) "." (:minor version))))
+   (or (util/to-local-date ts) "-")
+   user])
+
+(defn- build-history-child-rows [action docs lang]
+  ;  (debug " docs: " (with-out-str (clojure.pprint/pprint docs)))
+  (loop [docs-in docs
+         result []]
+    (let [[doc-attn & others] docs-in]
+      ;(debug " doc-attn: " doc-attn)
+      (if (nil? doc-attn)
+        result
+        (recur others (conj result (build-history-row doc-attn lang)))))))
+
+(defn- build-history-rows [application lang]
+  (let [data (toj/generate-case-file-data application)]
+    ;(debug " data: " (with-out-str (clojure.pprint/pprint data)))
+    (loop [data-in data
+           result []]
+      (let [[history & older] data-in
+            new-result (-> result
+                           (conj [(:action history) "" "" (or (util/to-local-date (:start history)) "-") (:user history)])
+                           (into (build-history-child-rows " " (:documents history) lang)))]
+        (if (nil? older)
+          new-result
+          (recur older new-result))))))
+
+(defn build-xml-history [application lang]
+  (clojure.string/join " " (map #(apply xml-table-row %) (build-history-rows application lang))))
+
 (defn- get-authority [lang {authority :authority :as application}]
   (if (and (:authority application)
            (domain/assigned? application))
@@ -40,7 +90,7 @@
   {"FOOTER_PAGE" (localized-text lang "application.export.page")
    "FOOTER_DATE" (util/to-local-date (System/currentTimeMillis))
 
-
+   "FIELD001"    (localized-text lang "caseFile.heading")
    "FIELD002"    (xml-escape (:address application))
 
    "FIELD003A"   (localized-text lang "application.muncipality")
@@ -96,57 +146,8 @@
         (when-let [table-rows (get fields (get-table-name line))]
           (write-table! rdr wrtr table-rows fields))))))
 
-(defn- build-history-row [data lang]
-  ;(debug "row data:" data)
-  [""
-   (case (:category data)
-     :attachment (i18n/localize lang "document")
-     :document (i18n/localize lang "application.applicationSummary")
-     :request-statement (i18n/localize lang "caseFile.operation.statement.request")
-     :request-neighbor (i18n/localize lang "caseFile.operation.neighbor.request")
-     :request-review (i18n/localize lang "caseFile.operation.review.request")
-     :review (i18n/localize lang "caseFile.operation.review")
-     "")
-   (str
-     (if (= (:category data) :attachment)
-       (i18n/localize lang "attachmentType" (get-in data [:type :type-group]) (get-in data [:type :type-id]))
-       (:type data))
-     " " (:contents data)
-     (if (get-in data [:version :major])
-       (str ", v. " (get-in data [:version :major]) "." (get-in data [:version :minor]))
-       ""))
-   (or (util/to-local-date (:ts data)) "-")
-   (:user data)])
-
-(defn- build-history-child-rows [action docs lang]
-  ;  (debug " docs: " (with-out-str (clojure.pprint/pprint docs)))
-  (loop [docs-in docs
-         result []]
-    (let [[doc-attn & others] docs-in]
-      ;(debug " doc-attn: " doc-attn)
-      (if (nil? doc-attn)
-        result
-        (recur others (conj result (build-history-row doc-attn lang)))))))
-
-(defn- build-history-rows [application lang]
-  (let [data (toj/generate-case-file-data application)]
-    ;(debug " data: " (with-out-str (clojure.pprint/pprint data)))
-    (loop [data-in data
-           result []]
-      (let [[history & older] data-in
-            new-result (-> result
-                           (conj [(:action history) "" "" (or (util/to-local-date (:start history)) "-") (:user history)])
-                           (into (build-history-child-rows " " (:documents history) lang)))]
-        (if (nil? older)
-          new-result
-          (recur older new-result))))))
-
-(defn build-xml-history [application lang]
-  (clojure.string/join " " (map #(apply xml-table-row %) (build-history-rows application lang))))
-
 (defn write-history-libre-doc [application lang file]
   (create-libre-doc (io/resource history-template-file) file (assoc (common-field-map application lang)
-                                                               "FIELD001" (i18n/localize lang "caseFile.heading")
                                                                "COLTITLE1" (i18n/localize lang "caseFile.action")
                                                                "COLTITLE2" (i18n/localize lang "applications.operation")
                                                                "COLTITLE3" (i18n/localize lang "document")
@@ -154,76 +155,11 @@
                                                                "COLTITLE5" (i18n/localize lang "lisaaja")
                                                                "HISTORYTABLE" (build-history-rows application lang))))
 
-
-(defn- verdict-dates [lang dates]
-  (cond-> []
-          (:anto dates) (conj [(i18n/localize lang "verdict.anto") (or (util/to-local-date (:anto dates)) "-")])
-          (:lainvoimainen dates) (conj [(i18n/localize lang "verdict.lainvoimainen") (or (util/to-local-date (:lainvoimainen dates)) "-")])
-          (:paatosdokumentinPvm dates) (conj [(i18n/localize lang "verdict.paatosdokumentinPvm") (or (util/to-local-date (:paatosdokumentinPvm dates)) "-")])
-          (:paatosPvm dates) (conj [(i18n/localize lang "verdict.paatosPvm") (or (util/to-local-date (:paatosPvm dates)) "-")])
-          (:julkipano dates) (conj [(i18n/localize lang "verdict.julkipano") (or (util/to-local-date (:julkipano dates)) "-")])
-          (:viimeinenValitus dates) (conj [(i18n/localize lang "verdict.viimeinenValitus") (or (util/to-local-date (:viimeinenValitus dates)) "-")])
-          (:aloitettava dates) (conj [(i18n/localize lang "verdict.aloitettava") (or (util/to-local-date (:aloitettava dates)) "-")])
-          (:voimassaHetki dates) (conj [(i18n/localize lang "verdict.voimassaHetki") (or (util/to-local-date (:voimassaHetki dates)) "-")])
-          (:raukeamis dates) (conj [(i18n/localize lang "verdict.raukeamis") (or (util/to-local-date (:raukeamis dates)) "-")])))
-
-(defn- verdict-lupamaaraykset [lang lupamaaraykset]
-  (cond-> []
-          (:autopaikkojaEnintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaEnintaan") (:autopaikkojaEnintaan lupamaaraykset)])
-          (:autopaikkojaVahintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaVahintaan") (:autopaikkojaVahintaan lupamaaraykset)])
-          (:autopaikkojaRakennettava lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettava") (:autopaikkojaRakennettava lupamaaraykset)])
-          (:autopaikkojaRakennettu lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettu") (:autopaikkojaRakennettu lupamaaraykset)])
-          (:autopaikkojaKiinteistolla lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaKiinteistolla") (:autopaikkojaKiinteistolla lupamaaraykset)])
-          (:autopaikkojaUlkopuolella lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaUlkopuolella") (:autopaikkojaUlkopuolella lupamaaraykset)])
-          (:kerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.kerrosala") (:kerrosala lupamaaraykset)])
-          (:rakennusoikeudellinenKerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.rakennusoikeudellinenKerrosala") (:rakennusoikeudellinenKerrosala lupamaaraykset)])
-          (:kokonaisala lupamaaraykset) (conj [(i18n/localize lang "verdict.kokonaisala") (:kokonaisala lupamaaraykset)])))
-
-(defn write-verdict-libre-doc [application id paatos-id lang file]
-  (let [verdict (first (filter #(= id (:id %)) (:verdicts application)))
-        paatos (first (filter #(= paatos-id (:id %)) (:paatokset verdict)))
-        _ (debug "write-verdict-libre-doc verdict: " (with-out-str (clojure.pprint/pprint verdict)) )
-        lupamaaraykset (:lupamaaraykset paatos)
-        reviews (map (fn [val] [(str (:katselmuksenLaji val)) (str (:tarkastuksenTaiKatselmuksenNimi val))]) (:vaaditutKatselmukset lupamaaraykset))
-        orders (map (fn [val] [(str (:sisalto val))]) (:maaraykset lupamaaraykset))
-        ]
-    (create-libre-doc (io/resource history-verdict-file) file (assoc (common-field-map application lang)
-                                                                "FIELD001" (if (:sopimus verdict) (i18n/localize lang "userInfo.company.contract") (i18n/localize lang "application.verdict.title"))
-                                                                "FIELD012" (str (i18n/localize lang "verdict.id") ": " (:kuntalupatunnus verdict))
-                                                                "FIELD013" (i18n/localize lang "date")
-                                                                "DATESTABLE" (verdict-dates lang (:paivamaarat paatos))
-
-                                                                "LUPAMAARAYKSETHEADER" (i18n/localize lang "verdict.lupamaaraukset")
-                                                                "LUPAMAARAYKSETTABLE" (verdict-lupamaaraykset lang lupamaaraykset)
-
-                                                                "FIELD015A" (i18n/localize lang "foreman.requiredForemen")
-                                                                "FIELD015B" (get-in paatos [:lupamaaraykset :vaaditutTyonjohtajat])
-
-                                                                "PLANSHEADER" (i18n/localize lang "verdict.vaaditutErityissuunnitelmat")
-                                                                ;;TODO: get real data to see the structure
-                                                                "PLANSTABLE" (map #(str %) (:vaaditutErityissuunnitelmat lupamaaraykset))
-
-                                                                "LP-HEADER-OTHER" (i18n/localize lang "verdict.muutMaaraykset")
-                                                                ;;TODO: get real data to see the structure
-                                                                "OTHERTABLE" (map #(str %) (:muutMaaraykset lupamaaraykset))
-
-                                                                "FIELD016" (i18n/localize lang "verdict.vaaditutKatselmukset")
-                                                                "REVIEWSTABLE" reviews
-
-                                                                "FIELD17" (i18n/localize lang "verdict.maaraykset")
-                                                                "ORDERSTABLE" orders
-
-                                                                "MINUTESHEADER" (i18n/localize lang "verdict.poytakirjat")
-
-                                                                "MINUTESCOL1" (i18n/localize lang "verdict.status")
-                                                                "MINUTESCOL2" (i18n/localize lang "verdict.pykala")
-                                                                "MINUTESCOL3" (i18n/localize lang "verdict.name")
-                                                                "MINUTESCOL4" (i18n/localize lang "verdict.paatospvm")
-                                                                "MINUTESCOL5" (i18n/localize lang "verdict.attachments")
-
-                                                                "MINUTESTABLE" (map (fn [val] [(str (if (:status val) (i18n/localize lang "verdict.status" (:status val)) "") " (" (:paatoskoodi val) ") " (:paatos val))
-                                                                                               (str (:pykala val))
-                                                                                               (str (:paatoksentekija val))
-                                                                                               (or (util/to-local-date (:paatospvm val)) "-")
-                                                                                               "TODO"]) (:poytakirjat paatos))
-                                                                ))))
+(defn write-verdict-libre-doc [application lang file]
+  (create-libre-doc (io/resource history-template-file) file (assoc (common-field-map application lang)
+                                                               "COLTITLE1" (i18n/localize lang "caseFile.action")
+                                                               "COLTITLE2" (i18n/localize lang "applications.operation")
+                                                               "COLTITLE3" (i18n/localize lang "document")
+                                                               "COLTITLE4" (i18n/localize lang "caseFile.documentDate")
+                                                               "COLTITLE5" (i18n/localize lang "lisaaja")
+                                                               )))
