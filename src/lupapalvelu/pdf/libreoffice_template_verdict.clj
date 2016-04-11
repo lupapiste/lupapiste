462f8d4eeafd7bf0d82a2a41e11fef3935021f04
Michael Holopainen <michael.holopainen@solita.fi>
2016-04-11 15:20:39 +0300

    Libreoffice-template verdict data gathering done except fr attachments

--- src/lupapalvelu/pdf/libreoffice_template_verdict.clj
+++ src/lupapalvelu/pdf/libreoffice_template_verdict.clj
@@ -6,39 +6,79 @@
             [clojure.string :as s]
             [clojure.java.io :as io]))
 
-(defn- verdict-dates [lang dates]
-  (cond-> []
-          (:anto dates) (conj [(i18n/localize lang "verdict.anto") (or (util/to-local-date (:anto dates)) "-")])
-          (:lainvoimainen dates) (conj [(i18n/localize lang "verdict.lainvoimainen") (or (util/to-local-date (:lainvoimainen dates)) "-")])
-          (:paatosdokumentinPvm dates) (conj [(i18n/localize lang "verdict.paatosdokumentinPvm") (or (util/to-local-date (:paatosdokumentinPvm dates)) "-")])
-          (:paatosPvm dates) (conj [(i18n/localize lang "verdict.paatosPvm") (or (util/to-local-date (:paatosPvm dates)) "-")])
-          (:julkipano dates) (conj [(i18n/localize lang "verdict.julkipano") (or (util/to-local-date (:julkipano dates)) "-")])
-          (:viimeinenValitus dates) (conj [(i18n/localize lang "verdict.viimeinenValitus") (or (util/to-local-date (:viimeinenValitus dates)) "-")])
-          (:aloitettava dates) (conj [(i18n/localize lang "verdict.aloitettava") (or (util/to-local-date (:aloitettava dates)) "-")])
-          (:voimassaHetki dates) (conj [(i18n/localize lang "verdict.voimassaHetki") (or (util/to-local-date (:voimassaHetki dates)) "-")])
-          (:raukeamis dates) (conj [(i18n/localize lang "verdict.raukeamis") (or (util/to-local-date (:raukeamis dates)) "-")])))
-
-(defn- verdict-lupamaaraykset [lang lupamaaraykset]
-  (cond-> []
-          (:autopaikkojaEnintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaEnintaan") (:autopaikkojaEnintaan lupamaaraykset)])
-          (:autopaikkojaVahintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaVahintaan") (:autopaikkojaVahintaan lupamaaraykset)])
-          (:autopaikkojaRakennettava lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettava") (:autopaikkojaRakennettava lupamaaraykset)])
-          (:autopaikkojaRakennettu lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettu") (:autopaikkojaRakennettu lupamaaraykset)])
-          (:autopaikkojaKiinteistolla lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaKiinteistolla") (:autopaikkojaKiinteistolla lupamaaraykset)])
-          (:autopaikkojaUlkopuolella lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaUlkopuolella") (:autopaikkojaUlkopuolella lupamaaraykset)])
-          (:kerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.kerrosala") (:kerrosala lupamaaraykset)])
-          (:rakennusoikeudellinenKerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.rakennusoikeudellinenKerrosala") (:rakennusoikeudellinenKerrosala lupamaaraykset)])
-          (:kokonaisala lupamaaraykset) (conj [(i18n/localize lang "verdict.kokonaisala") (:kokonaisala lupamaaraykset)])))
 
 (defn- verdict-attachments [application id]
   (s/join "\n" (map (fn [verdict-att] (str (get-in verdict-att [:latestVersion :filename]))) (filter (fn [att] (and (= :verdict (keyword (get-in att [:target :type]))) (= id (get-in att [:target :id])))) (:attachments application)))))
 
+(defn- get-lupamaaraykset [application verdict-id paatos-idx]
+  (:lupamaaraykset (nth (:paatokset (first (filter #(= verdict-id (:id %)) (:verdicts application)))) paatos-idx)))
+
+(defn- filter-tasks [application verdict-id type]
+  (filter (fn [task]
+            (log/debug "task: " task "=" (= (get-in task [:schema-info :name]) type))
+            (and (= (get-in task [:schema-info :name]) type)
+                 (= (get-in task [:source :type]) "verdict")
+                 (= (get-in task [:source :id]) verdict-id))) (:tasks application)))
+
+(defn- verdict-dates [lang dates]
+  (cond-> []
+          (:anto dates) (conj [(i18n/localize lang "verdict.anto") (or (util/to-local-date (:anto dates)) "-")])
+          (:lainvoimainen dates) (conj [(i18n/localize lang "verdict.lainvoimainen") (or (util/to-local-date (:lainvoimainen dates)) "-")])
+          (:aloitettava dates) (conj [(i18n/localize lang "verdict.aloitettava") (or (util/to-local-date (:aloitettava dates)) "-")])
+          (:voimassaHetki dates) (conj [(i18n/localize lang "verdict.voimassaHetki") (or (util/to-local-date (:voimassaHetki dates)) "-")])
+          (:viimeinenValitus dates) (conj [(i18n/localize lang "verdict.viimeinenValitus") (or (util/to-local-date (:viimeinenValitus dates)) "-")])
+          (:raukeamis dates) (conj [(i18n/localize lang "verdict.raukeamis") (or (util/to-local-date (:raukeamis dates)) "-")])
+          (:paatosdokumentinPvm dates) (conj [(i18n/localize lang "verdict.paatosdokumentinPvm") (or (util/to-local-date (:paatosdokumentinPvm dates)) "-")])
+          (:julkipano dates) (conj [(i18n/localize lang "verdict.julkipano") (or (util/to-local-date (:julkipano dates)) "-")])
+          (:paatosPvm dates) (conj [(i18n/localize lang "verdict.paatosPvm") (or (util/to-local-date (:paatosPvm dates)) "-")])))
+
+(defn- verdict-lupamaaraykset [application verdict-id paatos-idx lang]
+  (let [lupamaaraykset (get-lupamaaraykset application verdict-id paatos-idx)]
+    (cond-> []
+            (:autopaikkojaEnintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaEnintaan") (:autopaikkojaEnintaan lupamaaraykset)])
+            (:autopaikkojaVahintaan lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaVahintaan") (:autopaikkojaVahintaan lupamaaraykset)])
+            (:autopaikkojaRakennettava lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettava") (:autopaikkojaRakennettava lupamaaraykset)])
+            (:autopaikkojaRakennettu lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaRakennettu") (:autopaikkojaRakennettu lupamaaraykset)])
+            (:autopaikkojaKiinteistolla lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaKiinteistolla") (:autopaikkojaKiinteistolla lupamaaraykset)])
+            (:autopaikkojaUlkopuolella lupamaaraykset) (conj [(i18n/localize lang "verdict.autopaikkojaUlkopuolella") (:autopaikkojaUlkopuolella lupamaaraykset)])
+            (:takuuaikaPaivat lupamaaraykset) (conj [(i18n/localize lang "verdict.takuuaikaPaivat") (:takuuaikaPaivat lupamaaraykset)])
+            (:kerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.kerrosala") (:kerrosala lupamaaraykset)])
+            (:rakennusoikeudellinenKerrosala lupamaaraykset) (conj [(i18n/localize lang "verdict.rakennusoikeudellinenKerrosala") (:rakennusoikeudellinenKerrosala lupamaaraykset)])
+            (:kokonaisala lupamaaraykset) (conj [(i18n/localize lang "verdict.kokonaisala") (:kokonaisala lupamaaraykset)]))))
+
+;; (sc/optional-key :vaadittuTyonjohtajatieto)       [sc/Str]
+(defn- verdict-foremen [application verdict-id paatos-idx]
+  (into []
+  (if-let [krysp-foremen (:vaadittuTyonjohtajatieto (get-lupamaaraykset application verdict-id paatos-idx))]
+    (map (fn [row] [(str row)]) krysp-foremen)
+    (map (fn [row] [(str (:taskname row))]) (filter-tasks application verdict-id "task-vaadittu-tyonjohtaja")))))
+
+;(sc/optional-key :vaaditutErityissuunnitelmat)    [sc/Str]
+(defn- verdict-vaaditutErityissuunnitelmat [application verdict-id paatos-idx]
+  (into [] (map (fn [row] [(str row)]) (:vaaditutErityissuunnitelmat (get-lupamaaraykset application verdict-id paatos-idx)))))
+
+;;(sc/optional-key :muutMaaraykset)                 [(sc/maybe sc/Str)]
+(defn- verdict-muutMaaraykset [application verdict-id paatos-idx]
+  (into []
+  (if-let [krysp-other-requirements (:muutMaaraykset (get-lupamaaraykset application verdict-id paatos-idx))]
+    (map (fn [row] [(str row)]) krysp-other-requirements)
+    (map (fn [row] [(str (:taskname row))]) (filter-tasks application verdict-id "task-lupamaarays")))))
+
+;;(sc/optional-key :vaaditutKatselmukset)           [Katselmus]
+(defn- verdict-vaaditutKatselmukset [application verdict-id paatos-idx lang]
+  (if-let [krysp-other-requirements (:vaaditutKatselmukset (get-lupamaaraykset application verdict-id paatos-idx))]
+    (map (fn [val] [(str (:katselmuksenLaji val)) (str (:tarkastuksenTaiKatselmuksenNimi val))])
+         krysp-other-requirements)
+    (map (fn [child] [(i18n/localize (name lang) (str (get-in child [:schema-info :i18nprefix]) "." (get-in child [:data :katselmuksenLaji :value])))])
+         (filter-tasks application verdict-id "task-katselmus"))))
+
+;(sc/optional-key :maaraykset)                     [Maarays]
+(defn- verdict-maaraykset [application verdict-id paatos-idx]
+  (into [] (map (fn [val] [(str (:sisalto val))]) (:maaraykset (get-lupamaaraykset application verdict-id paatos-idx)))))
+
 (defn write-verdict-libre-doc [application id paatos-idx lang file]
   (let [verdict (first (filter #(= id (:id %)) (:verdicts application)))
-        paatos (nth (:paatokset verdict) paatos-idx)
-        lupamaaraykset (:lupamaaraykset paatos)
-        reviews (map (fn [val] [(str (:katselmuksenLaji val)) (str (:tarkastuksenTaiKatselmuksenNimi val))]) (:vaaditutKatselmukset lupamaaraykset))
-        orders (map (fn [val] [(str (:sisalto val))]) (:maaraykset lupamaaraykset))]
+        paatos (nth (:paatokset verdict) paatos-idx)]
     (when (nil? paatos) (throw (Exception. (str "verdict.paatos.id [" paatos-idx "] not found in verdict:\n" (with-out-str (clojure.pprint/pprint verdict))))))
     (template/create-libre-doc (io/resource "private/lupapiste-verdict-template.fodt")
                                file
@@ -49,24 +89,22 @@
                                  "DATESTABLE" (verdict-dates lang (:paivamaarat paatos))
 
                                  "LUPAMAARAYKSETHEADER" (i18n/localize lang "verdict.lupamaaraukset")
-                                 "LUPAMAARAYKSETTABLE" (verdict-lupamaaraykset lang lupamaaraykset)
+                                 "LUPAMAARAYKSETTABLE" (verdict-lupamaaraykset application id paatos-idx lang)
 
-                                 "FIELD015A" (i18n/localize lang "foreman.requiredForemen")
-                                 "FIELD015B" (get-in paatos [:lupamaaraykset :vaaditutTyonjohtajat])
+                                 "FOREMENHEADER" (i18n/localize lang "foreman.requiredForemen")
+                                 "FOREMENTABLE" (verdict-foremen application id paatos-idx)
 
                                  "PLANSHEADER" (i18n/localize lang "verdict.vaaditutErityissuunnitelmat")
-                                 ;;TODO: get real data to see the structure
-                                 "PLANSTABLE" (map #(str %) (:vaaditutErityissuunnitelmat lupamaaraykset))
+                                 "PLANSTABLE" (verdict-vaaditutErityissuunnitelmat application id paatos-idx)
 
-                                 "LP-HEADER-OTHER" (i18n/localize lang "verdict.muutMaaraykset")
-                                 ;;TODO: get real data to see the structure
-                                 "OTHERTABLE" (map #(str %) (:muutMaaraykset lupamaaraykset))
+                                 "OTHERHEADER" (i18n/localize lang "verdict.muutMaaraykset")
+                                 "OTHERTABLE" (verdict-muutMaaraykset application id paatos-idx)
 
-                                 "FIELD016" (i18n/localize lang "verdict.vaaditutKatselmukset")
-                                 "REVIEWSTABLE" reviews
+                                 "REVIEWHEADER" (i18n/localize lang "verdict.vaaditutKatselmukset")
+                                 "REVIEWSTABLE" (verdict-vaaditutKatselmukset application id paatos-idx lang)
 
                                  "FIELD17" (i18n/localize lang "verdict.maaraykset")
-                                 "ORDERSTABLE" orders
+                                 "ORDERSTABLE" (verdict-maaraykset application id paatos-idx)
 
                                  "MINUTESHEADER" (i18n/localize lang "verdict.poytakirjat")
 
