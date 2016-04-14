68318a528064646869585b284f711dd14daf8279
Antti Ramo <antti.ramo@solita.fi>
2016-04-14 14:35:03 +0300

    indent

--- resources/private/application/verdicts-model.js
+++ resources/private/application/verdicts-model.js
@@ -21,39 +21,39 @@ LUPAPISTE.VerdictsModel = function() {
     var verdicts = _(application.verdicts || [])
       .cloneDeep()
       .map(function(verdict) {
-      var paatokset = _.map(verdict.paatokset || [], function(paatos) {
-        var poytakirjat = _.map(paatos.poytakirjat || [], function(pk) {
-          var myAttachments = _.filter(application.attachments || [], function(attachment) {
-            var target = attachment.target;
-            var idMatch = false;
-            if (target && target.type === "verdict") {
-              if (target.poytakirjaId) {
-                idMatch = target.poytakirjaId === pk.id;
-              } else if (target.urlHash) {
-                idMatch = target.urlHash === pk.urlHash;
-              } else {
-                idMatch = target.id === verdict.id;
+        var paatokset = _.map(verdict.paatokset || [], function(paatos) {
+          var poytakirjat = _.map(paatos.poytakirjat || [], function(pk) {
+            var myAttachments = _.filter(application.attachments || [], function(attachment) {
+              var target = attachment.target;
+              var idMatch = false;
+              if (target && target.type === "verdict") {
+                if (target.poytakirjaId) {
+                  idMatch = target.poytakirjaId === pk.id;
+                } else if (target.urlHash) {
+                  idMatch = target.urlHash === pk.urlHash;
+                } else {
+                  idMatch = target.id === verdict.id;
+                }
               }
-            }
-            return idMatch;
-          }) || [];
-          pk.attachments = myAttachments;
-          return pk;
+              return idMatch;
+            }) || [];
+            pk.attachments = myAttachments;
+            return pk;
+          });
+          paatos.poytakirjat = poytakirjat;
+          paatos.signatures = verdict.signatures;
+          paatos.verdict = verdict;
+          return paatos;
         });
-        paatos.poytakirjat = poytakirjat;
-        paatos.signatures = verdict.signatures;
-        paatos.verdict = verdict;
-        return paatos;
-      });
-      verdict.paatokset = paatokset;
+        verdict.paatokset = paatokset;
 
-      var pk = util.getIn(paatokset, [0, "poytakirjat", 0]) || {};
-      var dates = util.getIn(paatokset, [0, "paivamaarat"]) || {};
-      verdict.canBePublished = verdict.kuntalupatunnus && pk.status && pk.paatoksentekija && dates.anto && dates.lainvoimainen;
+        var pk = util.getIn(paatokset, [0, "poytakirjat", 0]) || {};
+        var dates = util.getIn(paatokset, [0, "paivamaarat"]) || {};
+        verdict.canBePublished = verdict.kuntalupatunnus && pk.status && pk.paatoksentekija && dates.anto && dates.lainvoimainen;
 
-      return verdict;
-    })
-      .filter(function(v) {return !_.isEmpty(v.paatokset)});
+        return verdict;
+      })
+      .filter(function(v) {return !_.isEmpty(v.paatokset);});
 
     self.verdicts(verdicts);
     self.authorities = authorities;
