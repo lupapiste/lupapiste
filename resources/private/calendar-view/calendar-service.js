98b9093e88fd7bacc37c844b2851e656de958630
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-04-27 13:55:48 +0300

    service refactoring: retain week and year settings between events

--- resources/private/calendar-view/calendar-service.js
+++ resources/private/calendar-view/calendar-service.js
@@ -5,10 +5,23 @@ LUPAPISTE.CalendarService = function() {
   self.calendar = ko.observable();
   self.calendarWeekdays = ko.observableArray();
 
-  self.calendarQuery = {};
+  self.calendarQuery = {
+    calendarId: ko.observable(),
+    week: ko.observable(),
+    year: ko.observable()
+  };
 
   var doFetchCalendarSlots = function(event) {
-    ajax.query("calendar-slots", {calendarId: event.id, week: event.week, year: event.year})
+    if (event.id) {
+      self.calendarQuery.calendarId(event.id);
+    }
+    if (event.week && event.year) {
+      self.calendarQuery.week(event.week);
+      self.calendarQuery.year(event.year);
+    }
+    ajax.query("calendar-slots", { calendarId: self.calendarQuery.calendarId(),
+                                   week: self.calendarQuery.week,
+                                   year: self.calendarQuery.year })
       .success(function(data) {
         var startOfWeek = moment().isoWeek(event.week).year(event.year).startOf('isoWeek').valueOf();
         var weekdays = _.map([1, 2, 3, 4, 5], function(i) {
@@ -32,7 +45,8 @@ LUPAPISTE.CalendarService = function() {
     ajax.query("calendar", {calendarId: event.id})
       .success(function(data) {
         self.calendar(data.calendar);
-        doFetchCalendarSlots({id: data.calendar.id, week: moment().isoWeek(), year: moment().year()});
+        self.calendarQuery.calendarId(data.calendar.id);
+        doFetchCalendarSlots({week: moment().isoWeek(), year: moment().year()});
       })
       .call();
   });
@@ -48,13 +62,14 @@ LUPAPISTE.CalendarService = function() {
         if (event.modalClose) {
           LUPAPISTE.ModalDialog.close();
         }
-        doFetchCalendarSlots({id: data.calendar.id, week: moment().isoWeek(), year: moment().year()});
+        doFetchCalendarSlots();
       })
       .call();
   });
 
   self.dispose = function() {
     hub.unsubscribe(_fetchCalendar);
+    hub.unsubscribe(_fetchSlots);
     hub.unsubscribe(_createSlots);
   };
 };
