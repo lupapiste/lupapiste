46a057dce89ef21fd7450d9ad427e1b3c053ad8b
Antti Ramo <antti.ramo@solita.fi>
2016-05-13 14:44:27 +0300

    include operation-id in building data

--- src/lupapalvelu/building.clj
+++ src/lupapalvelu/building.clj
@@ -5,16 +5,17 @@
             [lupapalvelu.mongo :as mongo]
             [sade.util :as util]))
 
-(defn- doc->building-data [{{national-id :value} :valtakunnallinenNumero {short-id :value} :tunnus}]
+(defn- doc->building-data [{{{national-id :value} :valtakunnallinenNumero {short-id :value} :tunnus} :data
+                            {{operation-id :id} :op} :schema-info}]
   (when (or national-id short-id)
-    {:national-id national-id
+    {:operation-id operation-id
+     :national-id national-id
      :short-id    short-id}))
 
 (defn building-ids
   "Gathers building-id data from documents."
-  [{:keys [documents buildings] :as application}]
-  (->> (map :data documents)
-       (map doc->building-data)
+  [{:keys [documents] :as application}]
+  (->> (map doc->building-data documents)
        (remove nil?)))
 
 (defn building-id-mapping
