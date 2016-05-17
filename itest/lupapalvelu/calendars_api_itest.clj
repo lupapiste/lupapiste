61bd2365be140b5e2955006031e7e4758bbd5e62
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-05-17 14:11:21 +0300

    itests for calendar api

--- itest/lupapalvelu/calendars_api_itest.clj
+++ itest/lupapalvelu/calendars_api_itest.clj
@@ -1,6 +1,8 @@
 (ns lupapalvelu.calendars-api-itest
   (require [midje.sweet :refer :all]
-           [lupapalvelu.itest-util :refer :all]))
+           [lupapalvelu.itest-util :refer :all]
+           [lupapalvelu.fixture.minimal :as minimal]
+           [lupapalvelu.user :as u]))
 
 (apply-remote-minimal) ; minimal ensures wanted organization tags exist
 
@@ -10,8 +12,7 @@
 (fact "authority-admin users should be able to use the calendar admin API"
   (query sipoo :calendars-for-authority-admin) => ok?)
 
-#_(fact "calendars-for-authority-admin returns the users in the appropriate organization"
-      (->> (query sipoo :calendars-for-authority-admin)
-           :users
-           (group-by :organization)
-           keys) => (just #{"753-R"}))
+(fact "calendars-for-authority-admin returns the users in the appropriate organization"
+      (let [result   (:users (query sipoo :calendars-for-authority-admin))
+            expected (filter #(u/user-is-authority-in-organization? % "753-R") minimal/users)]
+        (map :id result) => (just (map :id expected) :in-any-order)))
