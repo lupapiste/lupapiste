e4802717a9c0c2e37add1bec8210760f6c9a1f41
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-05-17 11:24:34 +0300

    itests for calendar api. enabled ajanvaraus in dev environment.

---
+++ itest/lupapalvelu/calendars_api_itest.clj
@@ -0,0 +1,17 @@
+(ns lupapalvelu.calendars-api-itest
+  (require [midje.sweet :refer :all]
+           [lupapalvelu.itest-util :refer :all]))
+
+(apply-remote-minimal) ; minimal ensures wanted organization tags exist
+
+(fact "non-authority-admin users should not be able to use the calendar admin API"
+  (query pena :calendars-for-authority-admin) => unauthorized?)
+
+(fact "authority-admin users should be able to use the calendar admin API"
+  (query sipoo :calendars-for-authority-admin) => ok?)
+
+(fact "calendars-for-authority-admin returns the users in the appropriate organization"
+      (->> (query sipoo :calendars-for-authority-admin)
+           :users
+           (group-by :organization)
+           keys) => (just #{"753-R"}))
