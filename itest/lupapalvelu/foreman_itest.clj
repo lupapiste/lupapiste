a299117d37196c23aaff57f01894c210e5c372d8
Vespe Savikko <vespe.savikko@solita.fi>
2016-05-27 18:47:37 +0300

    Nil applicant foreman auth tests.

--- itest/lupapalvelu/foreman_itest.clj
+++ itest/lupapalvelu/foreman_itest.clj
@@ -15,23 +15,30 @@
 (mongo/connect!)
 (mongo/with-db db-name (fixture/apply-fixture "minimal")
   (facts "Creating invites for foreman application"
-         (let [test-auths [{:invite {:user {:id "C-123"}}}
-                           {:username "testi@example.com" :id "alsdkfjalsdjflsdf"}
-                           {:username "pena" :id "777777777777777777000020"}
-                           {:username "kaino@solita.fi" :id "kainosolita"}]
+         (let [test-auths             [{:invite {:user {:id "C-123"}}}
+                                       {:username "testi@example.com" :id "alsdkfjalsdjflsdf"}
+                                       {:username "pena" :id "777777777777777777000020"}
+                                       {:username "kaino@solita.fi" :id "kainosolita"}]
                                         ; Unwrapped documents!
                non-authed-henkilo-doc {:schema-info {:subtype "hakija"}
                                        :data {:_selected "henkilo"
                                               :henkilo {:yhteystiedot {:email "heppu@example.com"}}}}
-               authed-henkilo-doc {:schema-info {:subtype "hakija"}
-                                   :data {:_selected "henkilo"
-                                          :henkilo {:yhteystiedot {:email "pena@example.com"}}}}
+               authed-henkilo-doc     {:schema-info {:subtype "hakija"}
+                                       :data {:_selected "henkilo"
+                                              :henkilo {:yhteystiedot {:email "pena@example.com"}}}}
                companyid-doc          {:schema-info {:subtype "hakija"}
                                        :data {:_selected "yritys"
                                               :yritys {:companyId "C-123"}}}
                contact-person-doc     {:schema-info {:subtype "hakija"}
                                        :data {:_selected "yritys"
-                                              :yritys {:yhteyshenkilo {:yhteystiedot {:email "kaino@solita.fi"}}}}}]
+                                              :yritys {:yhteyshenkilo
+                                                       {:yhteystiedot {:email "kaino@solita.fi"}}}}}
+               bad-henkilo-doc        {:schema-info {:subtype "hakija"}
+                                       :data {:_selected "henkilo"
+                                              :henkilo {:yhteystiedot {:email nil}}}}
+               bad-contact-doc        {:schema-info {:subtype "hakija"}
+                                       :data {:_selected "yritys"
+                                              :yritys {:yhteyshenkilo {:yhteystiedot {:email nil}}}}}]
            (facts "Henkilo invite"
                   (henkilo-invite nil test-auths) => (throws AssertionError)
                   (henkilo-invite {} test-auths) => (throws AssertionError)
@@ -42,7 +49,9 @@
                         (henkilo-invite non-authed-henkilo-doc test-auths) => nil)
                   (fact "authed henkilo is invited"
                         (henkilo-invite authed-henkilo-doc test-auths) => {:email "pena@example.com"
-                                                                           :role "writer"}))
+                                                                           :role "writer"})
+                  (fact "Nil email is ignored"
+                        (henkilo-invite bad-henkilo-doc test-auths) => nil))
 
            (facts "Yritys invite"
                   (yritys-invite nil test-auths) => (throws AssertionError)
@@ -61,4 +70,6 @@
                         (yritys-invite contact-person-doc test-auths) => {:email "kaino@solita.fi"
                                                                           :role "writer"})
                   (fact "No invite for contact person if not present in auth array"
-                        (yritys-invite contact-person-doc (drop-last test-auths)) => nil)))))
+                        (yritys-invite contact-person-doc (drop-last test-auths)) => nil)
+                  (fact "Nil contact email is ignored"
+                        (yritys-invite bad-contact-doc test-auths) => nil)))))
