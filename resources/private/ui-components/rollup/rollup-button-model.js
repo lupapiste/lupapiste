60838cdbdf1c07192a9f17b8caff8cc7ccf3fc09
Vespe Savikko <vespe.savikko@solita.fi>
2016-06-06 12:39:26 +0300

    Safety commit.

--- resources/private/ui-components/rollup/rollup-button-model.js
+++ resources/private/ui-components/rollup/rollup-button-model.js
@@ -2,5 +2,6 @@ LUPAPISTE.RollupButtonModel = function( params ) {
   "use strict";
   var self = this;
   self.open = params.open;
-  self.text = params.text;
+  self.text = params.ltext ? loc( params.ltext ) : params.text;
+  self.css = _.defaults( {toggled: self.open}, params.css || {});
 };
---
+++ resources/private/ui-components/rollup/rollup-status-button-model.js
@@ -0,0 +1,12 @@
+LUPAPISTE.RollupStatusButtonModel = function( params ) {
+  "use strict";
+  var self = this;
+  var APPROVED = "ok";
+  var REJECTED = "requires_user_action";
+
+  self.params = params;
+  self.status = params.status;
+
+
+
+};
