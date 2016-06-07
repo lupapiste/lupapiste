6365a39382016890aa45e767a8b5191778c2de7f
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-06-07 15:36:27 +0300

    New fixture link to development panel

--- src/lupapalvelu/fixture/ajanvaraus.clj
+++ src/lupapalvelu/fixture/ajanvaraus.clj
@@ -91,4 +91,5 @@
   (mongo/insert-batch :ssoKeys [{:_id "12342424c26b7342d92a4321" :ip "127.0.0.1" :key "ozckCE8EESo+wMKWklGevQ==" :crypto-iv "V0HaDa6lpWKj+W0uMKyHBw=="}
                                 {:_id "12342424c26b7342d92a9876" :ip "109.204.231.126" :key "ozckCE8EESo+wMKWklGevQ==" :crypto-iv "V0HaDa6lpWKj+W0uMKyHBw=="}])
   (mongo/insert-batch :users users)
+  (mongo/insert-batch :organizations organizations)
   (lupapalvelu.calendars-api/clear-database))
