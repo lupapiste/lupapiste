9fd62291c4e738071bd7b57b1f9512644b2920ef
Vespe Savikko <vespe.savikko@solita.fi>
2016-06-07 18:03:34 +0300

    More general help-toggle.

--- resources/private/ui-components/attachments/attachments-listing-model.js
+++ resources/private/ui-components/attachments/attachments-listing-model.js
@@ -3,7 +3,27 @@ LUPAPISTE.AttachmentsListingModel = function() {
   var self = this;
   ko.utils.extend(self, new LUPAPISTE.ComponentBaseModel());
 
-  
+  var legendTemplate = _.template( "<div class='like-btn'>"
+                                   + "<i class='<%- icon %>'></i>"
+                                   + "<span><%- text %></span>"
+                                   + "</div>");
+  var legend = [["lupicon-circle-check positive", "ok.title"],
+                ["lupicon-circle-star primary", "new.title"],
+                ["lupicon-circle-attention negative", "missing.title"],
+                ["lupicon-circle-pen positive", "attachment.signed"],
+                ["lupicon-circle-arrow-up positive", "application.attachmentSentDate"],
+                ["lupicon-circle-stamp positive", "attachment.stamped"],
+                ["lupicon-circle-section-sign positive", "attachment.verdict-attachment"],
+                ["lupicon-lock primary", "attachment.not-public-attachment"]];
+
+  self.legendHtml = _( legend )
+    .map( function( leg ) {
+      return legendTemplate( {icon: _.first( leg ),
+                              text: loc( _.last( leg ))});
+    })
+    .value()
+    .join("<br>");
+
   self.service = lupapisteApp.services.attachmentsService;
   self.attachmentsHierarchy = ko.computed(self.service.getAttachmentsHierarchy);
   self.rollupToggle = ko.observable();
