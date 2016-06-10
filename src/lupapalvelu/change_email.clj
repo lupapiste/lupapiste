f3d0b96a8993047e66118be23da383b2126f1540
Ville Outamaa <ville.outamaa@solita.fi>
2016-06-10 15:59:21 +0300

    refactor email change logic from lupapalvelu.change-email-api to lupapalvelu.change-email

---
+++ src/lupapalvelu/change_email.clj
@@ -0,0 +1,64 @@
+(ns lupapalvelu.change-email
+  (:require [monger.operators :refer :all]
+            [sade.core :refer :all]
+            [sade.strings :as ss]
+            [lupapalvelu.notifications :as notifications]
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.token :as token]
+            [lupapalvelu.ttl :as ttl]
+            [lupapalvelu.user :as usr]
+            [lupapalvelu.vetuma :as vetuma]))
+
+(defn- notify-init-email-change [user new-email]
+  (let [token-id (token/make-token :change-email user {:new-email new-email}
+                                   :auto-consume false
+                                   :ttl ttl/change-email-token-ttl)
+        token (token/get-token token-id)]
+    (notifications/notify! :change-email
+                           {:user (assoc user :email new-email)
+                            :data {:old-email (:email user)
+                                   :new-email new-email
+                                   :token     token}})))
+
+(defn init-email-change [user email]
+  (let [email (usr/canonize-email email)]
+    (if-not (usr/get-user-by-email email)
+      (notify-init-email-change user email)
+      (fail :error.duplicate-email))))
+
+(defn- change-email-with-token [token stamp]
+  {:pre [(map? token)]}
+
+  (let [vetuma-data (vetuma/get-user stamp)
+        new-email (get-in token [:data :new-email])
+        {hetu :personId old-email :email :as user} (usr/get-user-by-id! (:user-id token))]
+
+    (cond
+      (not= (:token-type token) :change-email) (fail! :error.token-not-found)
+      (not= hetu (:userid vetuma-data)) (fail! :error.personid-mismatch)
+      (usr/get-user-by-email new-email) (fail! :error.duplicate-email))
+
+    ; Strictly this atomic update is enough.
+    ; Access to applications is determined by user id.
+    (usr/update-user-by-email old-email {:personId hetu} {$set {:username new-email :email new-email}})
+
+    ; Update application.auth arrays.
+    ; They might have duplicates due to old bugs, ensure everything is updated.
+    (loop [n 1]
+      (when (pos? n)
+        ; loop exists when no applications with the old username were found
+        (recur (mongo/update-by-query :applications {:auth {$elemMatch {:id (:id user), :username old-email}}} {$set {:auth.$.username new-email}}))))
+
+    ; Cleanup tokens
+    (vetuma/consume-user stamp)
+    (token/get-token (:id token) :consume true)
+
+    ; Send notifications
+    (notifications/notify! :email-changed {:user user, :data {:new-email new-email}})
+
+    (ok)))
+
+(defn change-email [tokenId stamp]
+  (if-let [token (token/get-token tokenId)]
+    (change-email-with-token token stamp)
+    (fail :error.token-not-found)))
--- src/lupapalvelu/change_email_api.clj
+++ src/lupapalvelu/change_email_api.clj
@@ -1,18 +1,13 @@
 (ns lupapalvelu.change-email-api
-  (:require [taoensso.timbre :as timbre :refer [debug info infof warn warnf error]]
-            [monger.operators :refer :all]
-            [sade.core :refer :all]
+  (:require [sade.core :refer :all]
             [sade.env :as env]
             [sade.strings :as ss]
             [sade.util :as util]
-            [lupapalvelu.mongo :as mongo]
             [lupapalvelu.action :refer [defquery defcommand defraw email-validator] :as action]
             [lupapalvelu.notifications :as notifications]
-            [lupapalvelu.token :as token]
-            [lupapalvelu.ttl :as ttl]
             [lupapalvelu.user :as usr]
             [lupapalvelu.company :as com]
-            [lupapalvelu.vetuma :as vetuma]))
+            [lupapalvelu.change-email :as change-email]))
 
 (defn change-email-link [lang token]
   (str (env/value :host) "/app/" lang "/welcome#!/email/" token))
@@ -31,15 +26,14 @@
 (notifications/defemail :email-changed
   {:recipients-fn (fn [{user :user}]
                     (if-let [company-id (get-in user [:company :id])]
-                      (->> (com/find-company-admins company-id) (remove #(= (:id %) (:id user))) (cons user))
+                      (->> (com/find-company-admins company-id)
+                           (remove #(= (:id %) (:id user)))
+                           (cons user))
                       [user]))
    :model-fn (fn [{:keys [user data]} conf recipient]
-               {:name (:firstName recipient) :old-email (:email user) :new-email (:new-email data)})})
-
-(defn- init-email-change [user new-email]
-  (let [token-id (token/make-token :change-email user {:new-email new-email} :auto-consume false :ttl ttl/change-email-token-ttl)
-        token (token/get-token token-id)]
-    (notifications/notify! :change-email {:user (assoc user :email new-email), :data {:old-email (:email user), :new-email new-email, :token token}})))
+               {:name      (:firstName recipient)
+                :old-email (:email user)
+                :new-email (:new-email data)})})
 
 (defn- has-person-id? [user]
   (if-let [user-id (:id user)]
@@ -47,56 +41,23 @@
       (not (ss/blank? (:personId full-user))))
     false))
 
+(defn- validate-has-person-id [{user :user} _]
+  (when-not (has-person-id? user)
+    (fail :error.unauthorized)))
+
 (defcommand change-email-init
   {:parameters [email]
    :user-roles #{:applicant :authority}
    :input-validators [(partial action/non-blank-parameters [:email])
                       action/email-validator]
-   :pre-checks [(fn [{user :user} _] (when-not (has-person-id? user) (fail :error.unauthorized)))]
+   :pre-checks [validate-has-person-id]
    :description "Starts the workflow for changing user password"}
   [{user :user}]
-  (let [email (usr/canonize-email email)]
-    (if-not (usr/get-user-by-email email)
-      (init-email-change user email)
-      (fail :error.duplicate-email))))
-
-(defn- change-email [token stamp]
-  {:pre [(map? token)]}
-
-  (let [vetuma-data (vetuma/get-user stamp)
-        new-email (get-in token [:data :new-email])
-        {hetu :personId old-email :email :as user} (usr/get-user-by-id! (:user-id token))]
-
-    (cond
-      (not= (:token-type token) :change-email) (fail! :error.token-not-found)
-      (not= hetu (:userid vetuma-data)) (fail! :error.personid-mismatch)
-      (usr/get-user-by-email new-email) (fail! :error.duplicate-email))
-
-    ; Strictly this atomic update is enough.
-    ; Access to applications is determined by user id.
-    (usr/update-user-by-email old-email {:personId hetu} {$set {:username new-email :email new-email}})
-
-    ; Update application.auth arrays.
-    ; They might have duplicates due to old bugs, ensure everything is updated.
-    (loop [n 1]
-      (when (pos? n)
-        ; loop exists when no applications with the old username were found
-        (recur (mongo/update-by-query :applications {:auth {$elemMatch {:id (:id user), :username old-email}}} {$set {:auth.$.username new-email}}))))
-
-    ; Cleanup tokens
-    (vetuma/consume-user stamp)
-    (token/get-token (:id token) :consume true)
-
-    ; Send notifications
-    (notifications/notify! :email-changed {:user user, :data {:new-email new-email}})
-
-    (ok)))
+  (change-email/init-email-change user email))
 
 (defcommand change-email
   {:parameters [tokenId stamp]
    :input-validators [(partial action/non-blank-parameters [:tokenId :stamp])]
    :user-roles #{:anonymous}}
   [_]
-  (if-let [token (token/get-token tokenId)]
-    (change-email token stamp)
-    (fail :error.token-not-found)))
+  (change-email/change-email tokenId stamp))
