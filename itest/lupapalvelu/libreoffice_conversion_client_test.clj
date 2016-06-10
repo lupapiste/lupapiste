b7e78ae1f3e42383d28fad389eb22e4ee42304a7
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-06-10 12:38:54 +0300

    always delete the temp file after facts

--- itest/lupapalvelu/libreoffice_conversion_client_test.clj
+++ itest/lupapalvelu/libreoffice_conversion_client_test.clj
@@ -10,20 +10,25 @@
 (def file-uri (str (.toURI (io/resource "sample-paatosote.rtf"))))
 
 ;;TODO: run multiple simoultanious requests in pararaller threads
-(facts "Test localhost pdfa-conversion service [in normal code floe conversion is not called when feature is disabled, so no :archivabilityError. Here we call the service even when it is disabled and assume it service is not running. So if the service is running in the ci/local env but feature disabled = fail]"
-       (with-open
-         [xin (io/input-stream file-uri)]
-         (let [response (client/convert-to-pdfa file-uri xin)
-               file-out (File/createTempFile "test-libre-rtf-" ".pdf")]
-           (if client/enabled?
-             (do (fact "libre enabled, No connnection error expected "
-                       (:archivabilityError response) => nil)
-                 (io/copy (:content response) file-out)
-                 (let [pdf-content (pdfbox/extract (.getAbsolutePath file-out))
-                       rows (remove str/blank? (str/split pdf-content #"\r?\n"))]
-                   (fact "PDF data rows "
-                         (count rows) => 100
-                         (nth rows 1) => "Once there was a miller who was poor, but who had a beautiful"))
-                 (.delete file-out))
-             (fact "libre is not enabled in this ENV [so the service should *NOT* be running in this ENV], expect connection error"
-                   (:archivabilityError response) => :libre-conversion-error)))))
+(facts "pdfa-conversion service"
+  ; In normal code flow conversion is not called when feature is disabled, so no :archivabilityError.
+  ; Here we call the service even when it is disabled and assume it service is not running.
+  ; So if the service is running in the ci/local env but feature disabled = fail
+  (with-open [xin (io/input-stream file-uri)]
+    (let [response (client/convert-to-pdfa file-uri xin)
+          file-out (File/createTempFile "test-libre-rtf-" ".pdf")]
+      (if client/enabled?
+        (try
+          (fact "libre enabled, No connnection error expected" (:archivabilityError response) => nil)
+
+          (io/copy (:content response) file-out)
+          (let [pdf-content (pdfbox/extract (.getAbsolutePath file-out))
+                rows (remove str/blank? (str/split pdf-content #"\r?\n"))]
+            (fact "PDF data rows "
+              (count rows) => 100
+              (nth rows 1) => "Once there was a miller who was poor, but who had a beautiful"))
+          (finally
+            (.delete file-out)))
+
+        (fact "libre is not enabled in this ENV [so the service should *NOT* be running in this ENV], expect connection error"
+          (:archivabilityError response) => :libre-conversion-error)))))
