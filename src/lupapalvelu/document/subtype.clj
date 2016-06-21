3bde3ff15e6339ccda522672003ef99348b47bf8
Aki Helin <aki.helin@solita.fi>
2016-06-21 12:05:15 +0300

    nil is allowed in recent-year

--- src/lupapalvelu/document/subtype.clj
+++ src/lupapalvelu/document/subtype.clj
@@ -39,17 +39,21 @@
         (> number max-double) [:warn "illegal-decimal:too-big"]))))
 
 (defmethod subtype-validation :recent-year [{:keys [range]} v]
-  (if (re-matches #"^[1-9][0-9]*$" v) ;; > 0
-    (let [given (Integer/parseInt v)
-          current (+ 1900 (.getYear (java.util.Date.)))]
-      (cond
-        ((> given current)
-          [:warn "illegal-recent-year:future"])
-        ((< given (- current range))
-          [:warn "illegal-recent-year:too-past"])
-        :else
-          nil))
-     [:warn "illegal-recent-year:not-integer"]))
+  (cond
+    (nil? v)
+      nil
+    (re-matches #"^[1-9][0-9]*$" v) ;; n>0
+      (let [given (util/->int v 0)
+            current (+ 1900 (.getYear (java.util.Date.)))]
+        (cond
+          ((> given current)
+            [:warn "illegal-recent-year:future"])
+          ((< given (- current range))
+            [:warn "illegal-recent-year:too-past"])
+          :else
+            nil))
+    :else
+      [:warn "illegal-recent-year:not-integer"]))
 
 (defmethod subtype-validation :digit [_ v]
   (cond
