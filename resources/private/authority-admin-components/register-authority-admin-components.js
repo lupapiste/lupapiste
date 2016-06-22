40bc7aa5554de19e273194b902397e34e78f9558
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-06-22 16:42:46 +0300

    Display Suti API configuration component

--- resources/private/authority-admin-components/register-authority-admin-components.js
+++ resources/private/authority-admin-components/register-authority-admin-components.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
   "use strict";
 
   var components = [{name: "server-settings"},
+                    {name: "suti-api"},
                     {name: "municipality-maps"},
                     {name: "municipality-maps-layers"},
                     {name: "municipality-maps-map"}];
