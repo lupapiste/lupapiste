7d5c72889143b000ef4f46f5cf961bd22a01229b
Markus Penttila <markus.penttila@solita.fi>
2016-06-29 11:20:36 +0300

    Use namespace prefix for elements when outputting case file xml with jaxb

--- java-src/lupapalvelu/tiedonohjaus/package-info.java
+++ java-src/lupapalvelu/tiedonohjaus/package-info.java
@@ -1,2 +1,10 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.lupapiste.fi/onkalo/schemas/sahke2-case-file/2016/6/1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(
+        namespace = "http://www.lupapiste.fi/onkalo/schemas/sahke2-case-file/2016/6/1",
+        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+        xmlns = {
+                @XmlNs(prefix="lp", namespaceURI="http://www.lupapiste.fi/onkalo/schemas/sahke2-case-file/2016/6/1")
+        }
+)
 package lupapalvelu.tiedonohjaus;
+
+import javax.xml.bind.annotation.XmlNs;
