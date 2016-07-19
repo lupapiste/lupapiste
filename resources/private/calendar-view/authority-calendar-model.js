96bdb37ec3058b3f12e010be921bcfd437d39ff6
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-07-19 15:32:07 +0300

    Invited user combobox implemented

--- resources/private/calendar-view/authority-calendar-model.js
+++ resources/private/calendar-view/authority-calendar-model.js
@@ -1,2 +1,9 @@
 LUPAPISTE.ApplicationAuthorityCalendarModel = function (params) {
+
+  self.authorizedParties = lupapisteApp.models.application.roles;
+
+  self.partyFullName = function(party) {
+    return party.firstName() + " " + party.lastName();
+  };
+
 };
