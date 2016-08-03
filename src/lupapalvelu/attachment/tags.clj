2bc686881857815825f4ce3ade84fded51cc5b78
Antti Rämö <antti.ramo@solita.fi>
2016-08-03 11:34:54 +0300

    rewrite attachment tag grouping

--- src/lupapalvelu/attachment/tags.clj
+++ src/lupapalvelu/attachment/tags.clj
@@ -36,23 +36,55 @@
         (att-type/tag-by-type attachment)]
        (remove nil?)))
 
-(defn attachment-tag-groups
-  "WIP Get hierarchical attachment grouping by attachments tags.
-  There are one and two level groups in tag hierarchy (operations are two level groups).
-  eg. [[:default] [:parties] [:building-site] [opid1 [:paapiirustus :iv_suunnitelma]] [opid2 [:kvv_suunnitelma]]]"
-  [attachments]
-  (let [groups     (->> (map :groupType attachments)
-                        (map keyword)
-                        (remove #{:operation})
-                        (remove nil?)
-                        distinct
-                        (map vector))
-        operations (->> (map (comp :id :op) attachments)
-                        (remove nil?)
-                        distinct
-                        (map #(vector % att-type/type-groups)))]
-    (concat [[:default]] groups operations)))
+(defn- attachments-group-types [attachments]
+  (->> (map #(if ((comp :id :op) %) :operation (:groupType %)) attachments)
+       (remove nil?)
+       (map keyword)
+       distinct))
 
+(defn- attachments-operation-ids [attachments]
+  (->> (map (comp :id :op) attachments)
+       (remove nil?)
+       distinct))
+
+(def type-grouping-stresshold 1)
+
+(defn- type-groups-for-operation
+  "Returns attachment type based grouping. Type group is created only if amount of attachments
+  of same type exceeds type-grouping-stresshold value."
+  [attachments operation-id]
+  (->> (filter (comp #{operation-id} :id :op) attachments)
+       (map att-type/tag-by-type)
+       (remove nil?)
+       frequencies
+       (filter (comp (partial < type-grouping-stresshold) val))
+       keys))
+
+(defn- operation-grouping
+  "Creates subgrouping for operations attachments if needed."
+  [type-groups operation-id]
+  (if (empty? type-groups)
+    [operation-id]
+    [operation-id (conj (vec type-groups) :default)]))
+
+(defmulti tag-grouping-for-group-type (fn [attachments group-type] group-type))
+
+(defmethod tag-grouping-for-group-type :default [_ group-type]
+  [[group-type]])
+
+(defmethod tag-grouping-for-group-type :operation [attachments _]
+  (->> (attachments-operation-ids attachments)
+       (map #(-> (type-groups-for-operation attachments %)
+                 (operation-grouping %)))))
+
+(defn attachment-tag-groups
+  "Get hierarchical attachment grouping by attachments tags.
+  There are one and two level groups in tag hierarchy (operations are two level groups).
+  eg. [[:default] [:parties] [:building-site] [opid1 [:paapiirustus :iv_suunnitelma :default]] [opid2 [:kvv_suunnitelma :default]]]"
+  [attachments]
+  (->> (filter (set (attachments-group-types attachments)) attachment-groups) ;; keep sorted
+       (cons :default)
+       (mapcat (partial tag-grouping-for-group-type attachments))))
 
 (defn attachments-filters
   "Get all possible filters with default values for attachments based on attachment data."
