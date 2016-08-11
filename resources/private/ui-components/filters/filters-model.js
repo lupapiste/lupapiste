493451c832aec5f6da97559f21dbf94eb7e20386
Antti Rämö <antti.ramo@solita.fi>
2016-08-11 13:11:46 +0300

    remove console.log

--- resources/private/ui-components/filters/filters-model.js
+++ resources/private/ui-components/filters/filters-model.js
@@ -27,7 +27,6 @@ LUPAPISTE.FiltersModel = function() {
   });
 
   self.clickGroupState = function() {
-    console.log("filters-model: clickGroupState called");
     var state = self.groupState();
     _.each( self.filters(), function( m ) {
       m.filter( state !== "all");
