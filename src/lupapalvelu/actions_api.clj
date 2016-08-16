5ecc6b8fb6a0994037d183049607c8556e0a5090
Antti Rämö <antti.ramo@solita.fi>
2016-08-16 11:52:08 +0300

    query for getting allowed actions for all attachments at once

--- src/lupapalvelu/actions_api.clj
+++ src/lupapalvelu/actions_api.clj
@@ -1,6 +1,8 @@
 (ns lupapalvelu.actions-api
-  (:require [sade.env :as env]
+  (:require [clojure.set :refer [difference]]
+            [sade.env :as env]
             [sade.core :refer :all]
+            [sade.util :refer [fn->]]
             [lupapalvelu.action :refer [defquery] :as action]
             [lupapalvelu.authorization :as auth]))
 
@@ -8,7 +10,7 @@
 ;; Default actions
 ;;
 
-(defn foreach-action [web user data application]
+(defn- foreach-action [web user application data]
   (map
     #(let [{type :type} (action/get-meta %)]
       (assoc
@@ -25,14 +27,45 @@
    :description "List of all actions and their meta-data."} [_]
   (ok :actions (action/serializable-actions)))
 
+(defn- get-allowed-actions [web user application data]
+  (let [results  (->> (foreach-action web user application data)
+                      (map validated))
+        filtered (if (env/dev-mode?)
+                   results
+                   (filter (comp :ok first vals) results))]
+    (into {} filtered)))
+
+(defmulti allowed-actions-for-category (fn-> :data :category keyword))
+
+(defmethod allowed-actions-for-category :default
+  [_]
+  nil)
+
+(defn- build-attachment-query-params [{application-id :id} {attachment-id :id latest-version :latestVersion}]
+  {:id           application-id
+   :attachmentId attachment-id
+   :fileId       latest-version})
+
+(defmethod allowed-actions-for-category :attachments
+  [{:keys [web user application]}]
+  (->> (:attachments application)
+       (map (partial build-attachment-query-params application))
+       (map (partial get-allowed-actions web user application))
+       (zipmap (map :id (:attachments application)))))
+
 (defquery allowed-actions
   {:user-roles       #{:anonymous}
    :user-authz-roles auth/all-authz-roles
    :org-authz-roles  auth/reader-org-authz-roles}
   [{:keys [web data user application]}]
-  (let [results  (map validated (foreach-action web user data application))
-        filtered (if (env/dev-mode?)
-                   results
-                   (filter (comp :ok first vals) results))
-        actions  (into {} filtered)]
-    (ok :actions actions)))
+  (ok :actions (get-allowed-actions web user application data)))
+
+(defquery allowed-actions-for-category
+  {:description      "Returns map of allowed actions for a category (attachments, tasks, etc.)"
+   :user-roles       #{:anonymous}
+   :user-authz-roles auth/all-authz-roles
+   :org-authz-roles  auth/reader-org-authz-roles}
+  [command]
+  (if-let [actions-by-id (allowed-actions-for-category command)]
+    (ok :actionsById actions-by-id)
+    (fail :error.invalid-category)))
