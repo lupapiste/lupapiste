8cb73bdcd632e5ef1c698b89b45fd0e6a82df576
Antti Slunga <Antti.Slunga@solita.fi>
2016-08-17 16:04:05 +0300

    LPK-2014 PDF wrapper refactoring

--- src/lupapalvelu/attachment/pdf_wrapper.clj
+++ src/lupapalvelu/attachment/pdf_wrapper.clj
@@ -33,39 +33,43 @@
     (.setInput reader (FileImageInputStream. file) false false)
     (read-images reader 0 [])))
 
+(defn- picture-to-pdf [doc jpeg-file]
+  (doseq [{:keys [width length content]} (read-jpeg jpeg-file)]
+    (let [intended-width (float width)
+          intended-length (float length)
+          page (PDPage. (PDRectangle. intended-width intended-length))]
+      (with-open [contents (PDPageContentStream. doc page)]
+        (.addPage doc page)
+        (.drawImage contents (LosslessFactory/createFromImage doc content) 0.0 0.0 intended-width intended-length)))))
+
+(defn- metadata-to-pdf [doc pdf-title]
+  (with-open [xmp-os (ByteArrayOutputStream.)
+              color-is (io/input-stream (io/resource color-profile))]
+
+    ;; Add the metadata and output intent to make the file PDF/A-1b compliant
+    ;; Note that we don't copy any existing XMP data (if any) from the TIFF file here
+    (let [xmp (XMPMetadata/createXMPMetadata)
+          dc (.createAndAddDublinCoreSchema xmp)
+          id (.createAndAddPFAIdentificationSchema xmp)
+          serializer (XmpSerializer.)
+          metadata (PDMetadata. doc)]
+      (.setTitle dc pdf-title)
+      (doto id
+        (.setPart (int 1))
+        (.setConformance "B"))
+      (.serialize serializer xmp xmp-os true)
+      (.importXMPMetadata metadata (.toByteArray xmp-os))
+      (let [intent (doto (PDOutputIntent. doc color-is)
+                     (.setInfo color-space)
+                     (.setOutputCondition color-space)
+                     (.setOutputConditionIdentifier color-space)
+                     (.setRegistryName color-registry))]
+        (doto (.getDocumentCatalog doc)
+          (.setMetadata metadata)
+          (.addOutputIntent intent))))))
+
 (defn wrap! [^File jpeg-file ^File output-file pdf-title]
   (with-open [doc (PDDocument.)]
-
-    (doseq [{:keys [width length content]} (read-jpeg jpeg-file)]
-      (let [intended-width (float width)
-            intended-length (float length)
-            page (PDPage. (PDRectangle. intended-width intended-length))]
-        (with-open [contents (PDPageContentStream. doc page)]
-          (.addPage doc page)
-          (.drawImage contents (LosslessFactory/createFromImage doc content) 0.0 0.0 intended-width intended-length))))
-
-    (with-open [xmp-os (ByteArrayOutputStream.)
-                color-is (io/input-stream (io/resource color-profile))]
-
-      ;; Add the metadata and output intent to make the file PDF/A-1b compliant
-      ;; Note that we don't copy any existing XMP data (if any) from the TIFF file here
-      (let [xmp (XMPMetadata/createXMPMetadata)
-            dc (.createAndAddDublinCoreSchema xmp)
-            id (.createAndAddPFAIdentificationSchema xmp)
-            serializer (XmpSerializer.)
-            metadata (PDMetadata. doc)]
-        (.setTitle dc pdf-title)
-        (doto id
-          (.setPart (int 1))
-          (.setConformance "B"))
-        (.serialize serializer xmp xmp-os true)
-        (.importXMPMetadata metadata (.toByteArray xmp-os))
-        (let [intent (doto (PDOutputIntent. doc color-is)
-                       (.setInfo color-space)
-                       (.setOutputCondition color-space)
-                       (.setOutputConditionIdentifier color-space)
-                       (.setRegistryName color-registry))]
-          (doto (.getDocumentCatalog doc)
-            (.setMetadata metadata)
-            (.addOutputIntent intent)))))
+    (picture-to-pdf doc jpeg-file)
+    (metadata-to-pdf doc pdf-title)
     (.save doc output-file)))
