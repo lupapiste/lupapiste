c2e579b0438049ca395a27abc484774df99f7013
Ville Kähkönen <ville.kahkonen@solita.fi>
2016-08-24 14:21:54 +0300

    accept an appointment by updating existing calendar event

--- src/lupapalvelu/calendar_emails.clj
+++ src/lupapalvelu/calendar_emails.clj
@@ -84,7 +84,7 @@
                                          reservation (assoc reservation :attendee (assoc recipient :partstat PartStat/DECLINED)
                                                                         :unique-id (:unique-id reservation)
                                                                         :sequence (inc (:sequence reservation))
-                                                                        :method Method/REPLY)]
+                                                                        :method Method/REQUEST)]
                                      (cal/update-reservation application
                                                              (:reservationId result)
                                                              {$set {:reservations.$.sequence (:sequence reservation)}})
@@ -102,7 +102,7 @@
                                          reservation (assoc reservation :attendee (assoc recipient :partstat PartStat/ACCEPTED)
                                                                         :unique-id (:unique-id reservation)
                                                                         :sequence (inc (:sequence reservation))
-                                                                        :method Method/REPLY)]
+                                                                        :method Method/REQUEST)]
                                      (cal/update-reservation application
                                                              (:reservationId result)
                                                              {$set {:reservations.$.sequence (:sequence reservation)}})
