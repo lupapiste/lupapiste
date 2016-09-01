41de66028fbfe72ab3cc374244ea3fc2dc9eda16
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-09-01 09:50:43 +0300

    Added pre-cehck function to check foreman access to foreman application only

--- src/lupapalvelu/foreman.clj
+++ src/lupapalvelu/foreman.clj
@@ -144,6 +144,13 @@
 
 (defn foreman-app? [application] (= :tyonjohtajan-nimeaminen-v2 (-> application :primaryOperation :name keyword)))
 
+(defn allow-foreman-only-in-foreman-app
+  "If user has :forman role in current application, check that the application is a forman application."
+  [{:keys [application user]}]
+  (when (and (auth/has-auth-role? application (:id user) :foreman)
+             (not (foreman-app? application)))
+    unauthorized))
+
 (defn notice?
   "True if application is foreman application and of type notice (ilmoitus)"
   [application]
