1c280ad64c2135d751d031c2e4ddc9fb537e6464
Antti Rämö <antti.ramo@solita.fi>
2016-09-23 10:05:17 +0300

    remove broken commented code

--- src/lupapalvelu/construction_api.clj
+++ src/lupapalvelu/construction_api.clj
@@ -31,43 +31,6 @@
                                          :started timestamp}})))
   (ok))
 
-(comment
-  (defcommand inform-building-construction-started
-       {:parameters ["id" buildingIndex startedDate lang]
-        ; rakentamisen aikaisen toimminan yhteydessa korjataan oikeae
-        ;:user-roles ???
-        :states     #{:verdictGiven :constructionStarted}
-        :notified   true
-        :pre-checks [(permit/validate-permit-type-is permit/R)]
-        :input-validators [(partial action/non-blank-parameters [:buildingIndex :startedDate :lang])]}
-       [{:keys [user created application organization] :as command}]
-    (let [timestamp    (util/to-millis-from-local-date-string startedDate)
-          app-updates  (merge
-                        {:modified created}
-                        (when
-                            {:started created
-                             :state  :constructionStarted}))
-          application  (merge application app-updates)
-          organization @organization
-          krysp?       (organization/krysp-integration? organization (permit/permit-type application))
-          building     (or
-                        (some #(when (= (str buildingIndex) (:index %)) %) (:buildings application))
-                        (fail! :error.unknown-building))]
-         (when krysp?
-           (mapping-to-krysp/save-aloitusilmoitus-as-krysp application lang organization timestamp building user))
-         (update-application command
-           {:buildings {$elemMatch {:index (:index building)}}}
-           (util/deep-merge
-             {$set {:modified created
-                    :buildings.$.constructionStarted timestamp
-                    :buildings.$.startedBy (select-keys user [:id :firstName :lastName])}}
-             (when (states/verdict-given-states (keyword (:state application)))
-               (application/state-transition-update :constructionStarted created user))
-             ))
-         (when (states/verdict-given-states (keyword (:state application)))
-           (notifications/notify! :application-state-change command))
-         (ok :integrationAvailable krysp?))))
-
 (defcommand inform-construction-ready
   {:parameters ["id" readyTimestampStr lang]
    :user-roles #{:authority}
