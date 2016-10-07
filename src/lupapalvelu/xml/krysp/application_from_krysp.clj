0b9b1645bb7412f069e69731e3301c35e930f23e
Antti Rämö <antti.ramo@solita.fi>
2016-10-07 06:50:10 +0300

    split application xmls when fetched in chucks from krysp

--- src/lupapalvelu/xml/krysp/application_from_krysp.clj
+++ src/lupapalvelu/xml/krysp/application_from_krysp.clj
@@ -1,8 +1,28 @@
 (ns lupapalvelu.xml.krysp.application-from-krysp
-  (:require [lupapalvelu.organization :as organization]
+  (:require [clojure.set :refer [rename-keys]]
+            [lupapalvelu.organization :as organization]
             [lupapalvelu.permit :as permit]
-            [lupapalvelu.document.kiinteistotoimitus-canonical :refer [operation-name]]
+            [sade.common-reader :as scr]
+            [sade.xml :as sxml]
+            [sade.util :refer [fn->>]]
             [sade.core :refer [fail!]]))
+
+(defn- get-lp-tunnus [xml-without-ns]
+  (->> (sxml/select1 xml-without-ns [:luvanTunnisteTiedot :LupaTunnus :muuTunnustieto :tunnus])
+       :content
+       first))
+
+(defn- get-kuntalupatunnus [xml-without-ns]
+  (->> (sxml/select1 xml-without-ns [:luvanTunnisteTiedot :LupaTunnus :kuntalupatunnus])
+       :content
+       first))
+
+(defn- split-content-per-application [xml-without-ns]
+  (let [toimituksen-tiedot (sxml/select xml-without-ns [:toimituksenTiedot])]
+    (->> (:content xml-without-ns)
+         (remove (comp #{:toimituksenTiedot} :tag))
+         (map (fn->> (vector toimituksen-tiedot)
+                     (assoc xml-without-ns :content))))))
 
 (defn- get-application-xml [organization-id permit-type ids search-type raw?]
   (if-let [{url :url credentials :credentials} (organization/get-krysp-wfs {:organization organization-id :permitType permit-type})]
@@ -13,9 +33,26 @@
 (defn get-application-xml-by-application-id [{:keys [id organization permitType] :as application} & [raw?]]
   (get-application-xml organization permitType [id] :application-id raw?))
 
-(defn get-multiple-application-xmls-by-application-ids [organization-id permit-type application-ids & [raw?]]
-  (get-application-xml organization-id permit-type application-ids :application-id raw?))
-
 (defn get-application-xml-by-backend-id [{:keys [id organization permitType] :as application} backend-id & [raw?]]
   (when backend-id
     (get-application-xml organization permitType [backend-id] :kuntalupatunnus raw?)))
+
+(defmulti get-application-xmls
+  "Get application xmls from krysp"
+  {:arglists '([organization-id permit-type search-type applications & [raw?]])}
+  (fn [org-id permit-type search-type & args]
+    (keyword search-type)))
+
+(defmethod get-application-xmls :application-id
+  [organization-id permit-type search-type application-ids]
+  (->> (get-application-xml organization-id permit-type application-ids :application-id false)
+       (scr/strip-xml-namespaces)
+       (split-content-per-application)
+       (group-by get-lp-tunnus)))
+
+(defmethod get-application-xmls :kuntalupatunnus
+  [organization-id permit-type search-type backend-ids]
+  (->> (get-application-xml organization-id permit-type backend-ids :kuntalupatunnus false)
+       (scr/strip-xml-namespaces)
+       (split-content-per-application)
+       (group-by get-kuntalupatunnus)))
