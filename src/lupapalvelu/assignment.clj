264b793700e5672a74e37e60554a7e3f1cf0cf35
Ville Outamaa <ville.outamaa@solita.fi>
2016-10-18 10:03:07 +0300

    refine assignment schema

--- src/lupapalvelu/assignment.clj
+++ src/lupapalvelu/assignment.clj
@@ -2,39 +2,50 @@
   (:require [monger.operators :refer [$set]]
             [schema.core :as sc]
             [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.user :as usr]
             [sade.schemas :as ssc])
   (:import [org.bson.types ObjectId]))
 
+(sc/defschema AssignmentStatus
+  (sc/enum "active" "inactive" "completed"))
+
 (sc/defschema Assignment
-  {:id              ssc/ObjectIdStr
-   :organization-id sc/Str
-   :application-id  sc/Str
-   :target          sc/Any
-   :created         ssc/Timestamp
-   :creator-id      ssc/ObjectIdStr
-   :recipient-id    ssc/ObjectIdStr
-   :completed       (sc/maybe ssc/Timestamp)
-   :completer-id    (sc/maybe sc/Str)
-   :active          sc/Bool
-   :description     sc/Str})
+  {:id             ssc/ObjectIdStr
+   :organizationId sc/Str
+   :applicationId  sc/Str
+   :target         sc/Any
+   :created        ssc/Timestamp
+   :creator        usr/SummaryUser
+   :recipient      usr/SummaryUser
+   :completed      (sc/maybe ssc/Timestamp)
+   :completer      (sc/maybe usr/SummaryUser)
+   :status         AssignmentStatus
+   :description    sc/Str})
 
 (sc/defschema NewAssignment
   (select-keys Assignment
-               [:organization-id
-                :application-id
-                :creator-id
-                :recipient-id
+               [:organizationId
+                :applicationId
+                :creator
+                :recipient
                 :target
                 :description]))
 
-(defn- new-assignment-fields [timestamp]
-  {:id           (mongo/create-id)
-   :created      timestamp
-   :completed    nil
-   :completer-id nil
-   :inactive     false})
+(sc/defn ^:private new-assignment :- Assignment
+  [assignment :- NewAssignment
+   timestamp  :- ssc/Timestamp]
+  (merge assignment
+         {:id        (mongo/create-id)
+          :created   timestamp
+          :completed nil
+          :completer nil
+          :status    :active}))
 
-(defn get-assignments
+;;
+;; Querying assignments
+;;
+
+(sc/defn ^:always-validate get-assignments :- [Assignment]
   ([]
    (get-assignments {}))
   ([query]
@@ -42,9 +53,16 @@
   ([query projection]
    (mongo/select :assignments query projection)))
 
+(defn get-assignments-for-application [application-id]
+  (get-assignments {:applicationId application-id}))
+
+;;
+;; Inserting and modifying assignments
+;;
+
 (sc/defn ^:always-validate insert-assignment [assignment :- NewAssignment
                                               timestamp  :- ssc/Timestamp]
-  (mongo/insert :assignments (merge assignment (new-assignment-fields timestamp))))
+  (mongo/insert :assignments (new-assignment assignment timestamp)))
 
 (sc/defn ^:always-validate update-assignment [assignment-id      :- ssc/ObjectIdStr
                                               assignment-changes]
@@ -52,7 +70,19 @@
                       assignment-id
                       assignment-changes))
 
-(defn complete-assignment [assignment-id completer-id timestamp]
+(sc/defn ^:always-validate complete-assignment [assignment-id :- ssc/ObjectIdStr
+                                                completer     :- usr/SummaryUser
+                                                timestamp     :- ssc/Timestamp]
   (update-assignment assignment-id
-                     {$set {:completed    timestamp
-                            :completer-id completer-id}}))
+                     {$set {:completed timestamp
+                            :status    "completed"
+                            :completer completer}}))
+
+; A temporary test function, to be removed before merge to develop
+(defn test-assignment [application-id target description]
+  {:organizationId "753-R"
+   :applicationId application-id
+   :creator (usr/summary (usr/get-user {:username "sonja"}))
+   :recipient (usr/summary (usr/get-user {:username "pena"}))
+   :target target
+   :description description})
