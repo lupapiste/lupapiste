947ad01508c3eda4b6d6c358f353157ba5107f09
Ville Outamaa <ville.outamaa@solita.fi>
2016-10-18 14:21:26 +0300

    add lupapalelu.assignment/add-assignment

--- itest/lupapalvelu/assignment_itest.clj
+++ itest/lupapalvelu/assignment_itest.clj
@@ -5,6 +5,21 @@
 
 (apply-remote-minimal)
 
-(fact "only authorities can have assignments"
-      (query sonja :assignments) => ok?
-      (query pena :assignments)  => #(= (:text %) "error.unauthorized"))
+(facts "Querying assignments"
+  (fact "only authorities can have assignments"
+    (query sonja :assignments) => ok?
+    (query pena :assignments)  => unauthorized?))
+
+(facts "Creating assignments"
+  (let [{id :id} (create-app sonja :propertyId sipoo-property-id)]
+    (fact "only authorities can create assignments"
+      (command sonja :add-assignment
+               :id          id
+               :recipient   "pena"
+               :target      ["target"]
+               :description "desc") => ok?
+      (command pena :add-assignment
+               :id          id
+               :recipient   "pena"
+               :target      ["target"]
+               :description "desc") => unauthorized?)))
--- src/lupapalvelu/assignment_api.clj
+++ src/lupapalvelu/assignment_api.clj
@@ -1,9 +1,31 @@
 (ns lupapalvelu.assignment-api
-  (:require [lupapalvelu.action :refer [defquery]]
+  (:require [lupapalvelu.action :refer [defcommand defquery non-blank-parameters vector-parameters]]
             [lupapalvelu.assignment :as assignment]
+            [lupapalvelu.user :as usr]
             [sade.core :refer :all]))
 
 (defquery assignments
   {:description "Return the entire collection"
    :user-roles #{:authority}}
+  [_]
   (ok :assignments (assignment/get-assignments)))
+
+(defcommand add-assignment
+  {:description "Add an assignment"
+   :user-roles #{:authority}
+   :parameters [recipient target description]
+   :input-validators [(partial non-blank-parameters [:recipient :description])
+                      (partial vector-parameters [:target])]}
+  [{user                      :user
+    created                   :created
+    {:keys [organization id]} :application}]
+  (let [recipient-summary (-> {:username recipient} (usr/get-user) (usr/summary))
+        creator-summary (-> user (usr/summary))]
+    (assignment/insert-assignment {:organizationId organization
+                                   :applicationId  id
+                                   :creator        creator-summary
+                                   :recipient      recipient-summary
+                                   :target         target
+                                   :description    description}
+                                  created))
+  (ok))
