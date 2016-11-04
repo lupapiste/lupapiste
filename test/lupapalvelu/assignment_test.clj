7cc835ed53d0c4d33c7da3799248af4077b5d8d2
Antti Rämö <antti.ramo@solita.fi>
2016-11-04 12:13:58 +0200

    added non-party document assignment target

--- test/lupapalvelu/assignment_test.clj
+++ test/lupapalvelu/assignment_test.clj
@@ -3,25 +3,24 @@
             [midje.util :refer [testable-privates]]
             [schema.core :as sc]
             [lupapalvelu.assignment :refer :all]
+            [lupapalvelu.document.schemas :as schemas]
             [lupapalvelu.document.data-schema :as dds]
             [sade.schema-generators :as ssg]
             [sade.util :as util]
             ;; Ensure assignment targets are registered
-            [lupapalvelu.document.document]))
+            [lupapalvelu.document.document]
+            ;; Ensure all document schemas are registered
+            [lupapalvelu.document.poikkeamis-schemas]
+            [lupapalvelu.document.vesihuolto-schemas]
+            [lupapalvelu.document.waste-schemas]
+            [lupapalvelu.document.yleiset-alueet-schemas]
+            [lupapalvelu.document.ymparisto-schemas]))
 
-(facts assignment-targets
-  (fact "Hakija-r assignment is valid target"
-    (let [target-groups (assignment-targets {:documents [(ssg/generate (dds/doc-data-schema "hakija-r"))]})]
-      (sc/check [TargetGroup] target-groups) => nil))
-
-  (fact "Maksaja assignment is valid target"
-    (let [target-groups (assignment-targets {:documents [(ssg/generate (dds/doc-data-schema "maksaja"))]})]
-      (sc/check [TargetGroup] target-groups) => nil))
-
-  (fact "Paasuunnittelija assignment is valid target"
-    (let [target-groups (assignment-targets {:documents [(ssg/generate (dds/doc-data-schema "paasuunnittelija"))]})]
-      (sc/check [TargetGroup] target-groups) => nil))
-
-  (fact "Tyonjohtaja assignment is valid target"
-    (let [target-groups (assignment-targets {:documents [(ssg/generate (dds/doc-data-schema "tyonjohtaja-v2"))]})]
+(defn validate-target [doc-schema-name]
+  (fact {:midje/description (str doc-schema-name " is valid assignment target")}
+    (let [target-groups (assignment-targets {:documents [(ssg/generate (dds/doc-data-schema doc-schema-name))]})]
       (sc/check [TargetGroup] target-groups) => nil)))
+
+(facts "Validate assignment targets to all documents"
+  (doseq [doc-schema-name (->> (schemas/get-all-schemas) vals (apply merge) keys)]
+    (validate-target doc-schema-name)))
