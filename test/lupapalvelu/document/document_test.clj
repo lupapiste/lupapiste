807c5ff676f94bcb59b9e7a8d0e3c807e03f839c
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-11-07 10:29:21 +0200

    add operation description info for operation documents

--- test/lupapalvelu/document/document_test.clj
+++ test/lupapalvelu/document/document_test.clj
@@ -74,7 +74,8 @@
 
 (facts document-assignment-info
   (fact "yritys"
-    (document-assignment-info {:id "4e2d57b9eb6b91890f33efd7",
+    (document-assignment-info nil
+                              {:id "4e2d57b9eb6b91890f33efd7",
                                :schema-info {:name "hakija-r",
                                              :i18name "osapuoli",
                                              :type "party",
@@ -92,7 +93,8 @@
     => {:id "4e2d57b9eb6b91890f33efd7", :type "hakija-r", :description "Firma 5"})
 
   (fact "henkilo"
-    (document-assignment-info {:id "4e2d57b9eb6b91890f33efd7",
+    (document-assignment-info nil
+                              {:id "4e2d57b9eb6b91890f33efd7",
                                :schema-info {:name "hakija-r",
                                              :i18name "osapuoli",
                                              :type "party",
@@ -110,7 +112,8 @@
     => {:id "4e2d57b9eb6b91890f33efd7", :type "hakija-r", :description "Pena Panaani"})
 
   (fact "without accordion fields"
-    (document-assignment-info {:id "4e2d57b9eb6b91890f33efd7",
+    (document-assignment-info nil
+                              {:id "4e2d57b9eb6b91890f33efd7",
                                :schema-info {:name "hakija-r",
                                              :i18name "osapuoli",
                                              :type "party",,
@@ -121,4 +124,36 @@
                                                                 :sukunimi {:value "Panaani", :modified 1458471382290}}},
                                       :yritys {:yritysnimi {:value "Firma 5", :modified 1458471382290},
                                                :yhteyshenkilo {:henkilotiedot {:etunimi {:value ""}, :sukunimi {:value ""}}}}}})
-    => {:id "4e2d57b9eb6b91890f33efd7", :type "hakija-r"}))
+    => {:id "4e2d57b9eb6b91890f33efd7", :type "hakija-r"})
+
+  (fact "operation doc description > accordion-field"
+    (document-assignment-info [{:id "581c7225b5a61a6ed47bfb43"
+                                :description "FOO"}]
+                              {:id "581c7226b5a61a6ed47bfb48",
+                               :schema-info {:op {:id "581c7225b5a61a6ed47bfb43",
+                                                  :name "kerrostalo-rivitalo",
+                                                  :description nil,
+                                                  :created 1478259237586},
+                                             :removable true,
+                                             :name "uusiRakennus",
+                                             :approvable true,
+                                             :accordion-fields [["valtakunnallinenNumero"]],
+                                             :version 1},
+                               :created 1478259237586,
+                               :data {:valtakunnallinenNumero {:value "1234"}}}) => {:id "581c7226b5a61a6ed47bfb48", :type "uusiRakennus", :description "FOO"})
+
+  (fact "without operation description"
+    (document-assignment-info [{:id "581c7225b5a61a6ed47bfb43"
+                                :description nil}]
+                              {:id "581c7226b5a61a6ed47bfb48",
+                               :schema-info {:op {:id "581c7225b5a61a6ed47bfb43",
+                                                  :name "kerrostalo-rivitalo",
+                                                  :description nil,
+                                                  :created 1478259237586},
+                                             :removable true,
+                                             :name "uusiRakennus",
+                                             :approvable true,
+                                             :accordion-fields [["valtakunnallinenNumero"]],
+                                             :version 1},
+                               :created 1478259237586,
+                               :data {:valtakunnallinenNumero {:value "1234"}}}) => {:id "581c7226b5a61a6ed47bfb48", :type "uusiRakennus", :description "1234"}))
