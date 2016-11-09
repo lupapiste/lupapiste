2819b73f079f3f98897863275edbbd0866a89e10
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-11-09 13:12:43 +0200

    saml logout on change-email page

--- resources/private/change-email/change-email.js
+++ resources/private/change-email/change-email.js
@@ -25,6 +25,15 @@
   var urlPrefix = "/app/" + loc.getCurrentLanguage() + "/welcome";
   var vetumaParams = new LUPAPISTE.VetumaButtonModel(urlPrefix, "vetuma-init-email", "email", "change-email");
 
+  function logoutRedirect(result) {
+    var url = util.getIn(LUPAPISTE.config, ["identMethods", "logoutUrl"]);
+    if (url) {
+      window.location = _.escape(url) + "?return=/app/" + loc.getCurrentLanguage() + "/welcome#!/change-email2/" + result;
+    } else {
+      pageutil.openPage("register3");
+    }
+  }
+
 
   hub.onPageLoad("email", function() {
     var token = getToken();
@@ -45,9 +54,9 @@
     vetuma.getUser(
         function(vetumaData) {
           ajax.command("change-email", {tokenId: token, stamp: vetumaData.stamp})
-            .success(_.partial(changingModel.success, true))
+            .success(_.partial(logoutRedirect, "ok"))
             .error(function(e) {
-              changingModel.error(e.text);
+              _.partial(logoutRedirect, e.text);
             })
             .complete(_.partial(changingModel.done, true))
             .call();
@@ -58,9 +67,19 @@
         });
   });
 
+  hub.onPageLoad("change-email2", function() {
+    var returnStatus = pageutil.lastSubPage();
+    if (returnStatus === "ok") {
+      changingModel.success(true);
+    } else {
+      changingModel.error(returnStatus);
+    }
+  });
+
   $(function(){
     $("#email").applyBindings({error: initError, status: statusModel, vetuma: vetumaParams, info: infoModel});
     $("#change-email").applyBindings(changingModel);
+    $("#change-email2").applyBindings(changingModel);
   });
 
 })();
