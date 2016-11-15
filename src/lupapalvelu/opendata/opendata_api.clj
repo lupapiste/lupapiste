fdbed9adfeb6e99dad91d67f4fd448121fbdf62e
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-11-15 16:54:27 +0200

    defendpoints can have parameters with descriptive titles

--- src/lupapalvelu/opendata/opendata_api.clj
+++ src/lupapalvelu/opendata/opendata_api.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.opendata.opendata-api
   (:require [ring.swagger.swagger2 :as rs]
+            [ring.swagger.json-schema :as rjs]
             [noir.core :refer [defpage]]
             [noir.response :as resp]
             [schema.core :as sc]
@@ -8,35 +9,49 @@
             [sade.http :as http]
             [sade.schemas :as ssc]
             [lupapalvelu.mongo :as mongo]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [sade.util :as util]))
 
 (defonce endpoints (atom []))
 
-(defmacro defendpoint [path params & content]
-  (let [meta-data (when (map? (first content)) (first content))]
-    `(let [[m# p#] (if (string? ~path) [:get ~path] ~path)]
+(defmacro defendpoint [path & content]
+  (let [meta-data (when (map? (first content)) (first content))
+        params (->> (util/select-values meta-data [:parameters :optional-parameters])
+                     (apply concat)
+                     (apply assoc {}))
+        letkeys (keys params)]
+    `(let [[m# p#]  (if (string? ~path) [:get ~path] ~path)]
        (swap! endpoints conj {:method (keyword m#)
                               :path p#
-                              :meta ~meta-data
-                              :params ~params})
-       (defpage ~path ~params
+                              :meta ~meta-data})
+       (defpage ~path {:keys ~letkeys}
          (let [response-data# (do ~@content)]
            (resp/set-headers
              http/no-cache-headers
              (resp/json response-data#)))))))
 
+(sc/defschema Asiointitunnus
+  (rjs/field sc/Str {:description "Hakemuksen asiointitunnus esim. LP-2016-000-90001"}))
 
 (sc/defschema PublicApplicationData
-  {:id #"^[0-9a-f]{24}$"})
+  {:asiointitunnus Asiointitunnus})
 
-(defendpoint "/opendata/applications" []
-  {:returns PublicApplicationData}
-  (mongo/select :applications {} {:_id 1}))
+(sc/defschema OrganizationId
+  (rjs/field sc/Str {:description "Organisaation tunnus"}))
+
+(defendpoint "/opendata/applications"
+  {:summary "Palauttaa organisaation kaikki vireillä olevat hankkeet."
+   :parameters [:organization OrganizationId]
+   :returns PublicApplicationData}
+  (mongo/select :applications {:organization organization} {:_id 1}))
 
 (defn paths []
   (let [paths (map
                 (fn [{:keys [path method meta]}]
-                  {path {method {:responses {200 {:schema (:returns meta)}}}}})
+                  (let [parameters (apply assoc {} (:parameters meta))]
+                    {path {method {:summary (:summary meta)
+                                   :parameters {:query parameters}
+                                   :responses {200 {:schema (:returns meta)}}}}}))
                 @endpoints)]
     (rs/swagger-json {:paths (into {} paths)})))
 
