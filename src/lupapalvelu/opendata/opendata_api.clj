bfe0e99e806d7ff0c69b46ac3fd7d7bd5da7086e
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-11-15 18:05:17 +0200

    Input and output schema validation

--- src/lupapalvelu/opendata/opendata_api.clj
+++ src/lupapalvelu/opendata/opendata_api.clj
@@ -10,6 +10,7 @@
             [sade.schemas :as ssc]
             [lupapalvelu.mongo :as mongo]
             [schema.core :as s]
+            [sade.core :refer [fail!]]
             [sade.util :as util]))
 
 (defonce endpoints (atom []))
@@ -20,15 +21,26 @@
                      (apply concat)
                      (apply assoc {}))
         letkeys (keys params)]
-    `(let [[m# p#]  (if (string? ~path) [:get ~path] ~path)]
+    `(let [[m# p#]        (if (string? ~path) [:get ~path] ~path)
+           retval-schema# (get ~meta-data :returns)
+           valid-inputs?# (fn [request#]
+                            (not-every? (fn [key#]
+                                          (let [input-schema# (or (get ~params key#) sc/Any)
+                                                input-val#    (get request# key#)]
+                                            (sc/check input-schema# input-val#))) (keys ~params)))]
        (swap! endpoints conj {:method (keyword m#)
-                              :path p#
-                              :meta ~meta-data})
-       (defpage ~path {:keys ~letkeys}
-         (let [response-data# (do ~@content)]
-           (resp/set-headers
-             http/no-cache-headers
-             (resp/json response-data#)))))))
+                              :path   p#
+                              :meta   ~meta-data})
+       (defpage ~path {:keys ~letkeys :as request#}
+         ; Input schema validation
+         (if (valid-inputs?# request#)
+           (let [response-data# (do ~@content)]
+             ; Response data schema validation
+             (sc/validate retval-schema# response-data#)
+             (resp/set-headers
+               http/no-cache-headers
+               (resp/json response-data#)))
+           (resp/status 400 "input-validation-error"))))))
 
 (sc/defschema Asiointitunnus
   (rjs/field sc/Str {:description "Hakemuksen asiointitunnus esim. LP-2016-000-90001"}))
@@ -42,16 +54,17 @@
 (defendpoint "/opendata/applications"
   {:summary "Palauttaa organisaation kaikki vireillä olevat hankkeet."
    :parameters [:organization OrganizationId]
-   :returns PublicApplicationData}
-  (mongo/select :applications {:organization organization} {:_id 1}))
+   :returns [PublicApplicationData]}
+  (map #(util/map-keys {:id :asiointitunnus} %)
+       (mongo/select :applications {:organization organization} [:id])))
 
 (defn paths []
   (let [paths (map
                 (fn [{:keys [path method meta]}]
                   (let [parameters (apply assoc {} (:parameters meta))]
-                    {path {method {:summary (:summary meta)
+                    {path {method {:summary    (:summary meta)
                                    :parameters {:query parameters}
-                                   :responses {200 {:schema (:returns meta)}}}}}))
+                                   :responses  {200 {:schema (:returns meta)}}}}}))
                 @endpoints)]
     (rs/swagger-json {:paths (into {} paths)})))
 
