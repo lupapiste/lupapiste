bad52a46d0037d0382dc1ffd8955d3ef77056dd0
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-11-17 12:23:59 +0200

    detailed schema

--- src/lupapalvelu/opendata/schemas.clj
+++ src/lupapalvelu/opendata/schemas.clj
@@ -1,7 +1,6 @@
 (ns lupapalvelu.opendata.schemas
   (:require [schema.core :as sc]
             [ring.swagger.json-schema :as rjs]
-            [schema.core :as s]
             [sade.util :as util]))
 
 (defn- field [schema desc]
@@ -14,34 +13,55 @@
   (field sc/Str "Kiinteistötunnus"))
 
 (sc/defschema SijaintiETRS
-  (field [(s/one sc/Num "lon")
-          (s/one sc/Num "lat")] "Rakennuspaikan sijainti (ETRS-TM35FIN), pituus- ja leveysasteet"))
+  (field [(sc/one sc/Num "lon")
+          (sc/one sc/Num "lat")] "Rakennuspaikan sijainti (ETRS-TM35FIN), pituus- ja leveysasteet"))
 
 (sc/defschema Kuntakoodi
   (field sc/Str "Kuntakoodi"))
 
 (sc/defschema DateString
-  (field (s/constrained sc/Str (partial re-matches #"\d{4}-\d{2}-\d{2}"))
+  (field (sc/constrained sc/Str (partial re-matches #"\d{4}-\d{2}-\d{2}"))
          "Päivämäärä muodossa yyyy-MM-dd, esim. 2016-05-23."))
 
 (sc/defschema Osoite
   (field sc/Str "Rakennuspaikan katuosoite"))
 
+(sc/defschema Huoneisto
+  (field (util/map-keys sc/optional-key
+                        {:muutostapa sc/Str
+                         :huoneluku sc/Str
+                         :keittionTyyppi sc/Str
+                         :huoneistoala sc/Str
+                         :varusteet sc/Any
+                         :huoneistonTyyppi sc/Str
+                         :huoneistotunnus sc/Any}) "Huoneiston tiedot"))
+
+(sc/defschema Kayttotarkoitus
+  (field (util/map-keys sc/optional-key {:pintaAla sc/Str
+                                         :kayttotarkoitusKoodi sc/Str}) "Esittää pinta-alat käyttötarkoituksittain"))
+
+(sc/defschema LaajennuksenTiedot
+  (field (util/map-keys sc/optional-key
+                        {:tilavuus sc/Str
+                         :kerrosala sc/Str
+                         :rakennusoikeudellinenKerrosala sc/Str
+                         :kokonaisala sc/Str
+                         :huoneistoala (sc/maybe [Kayttotarkoitus])}) "Laajennuksen tiedot"))
+
 (sc/defschema Toimenpide
               {(sc/optional-key :uusi) {:kuvaus sc/Str}
-               (sc/optional-key :muuMuutosTyo) {:muutostyonLaji sc/Str
-                                                :kuvaus sc/Str
-                                                :perusparannusKytkin sc/Bool}
+               (sc/optional-key :muuMuutosTyo) {:kuvaus sc/Str
+                                                :perusparannusKytkin sc/Bool
+                                                (sc/optional-key :muutostyonLaji) sc/Str}
                (sc/optional-key :laajennus) {:kuvaus sc/Str
-                                             :perusparannusKytkin sc/Bool
-                                             :laajennuksentiedot sc/Any}
-               (sc/optional-key :purkaminen) (util/map-keys sc/optional-key
-                                               {:kuvaus sc/Str
-                                                :purkamisenSyy sc/Str
-                                                :poistumaPvm sc/Any})
+                                             :laajennuksentiedot LaajennuksenTiedot
+                                             (sc/optional-key :perusparannusKytkin) sc/Bool}
+               (sc/optional-key :purkaminen) {:kuvaus sc/Str
+                                              (sc/optional-key :purkamisenSyy) sc/Str
+                                              (sc/optional-key :poistumaPvm) DateString}
                (sc/optional-key :kaupunkikuvaToimenpide) {:kuvaus sc/Str}
                (sc/optional-key :rakennuksenTiedot) (util/map-keys sc/optional-key
-                                                      {:rakennustunnus sc/Str
+                                                      {:rakennustunnus sc/Any
                                                        :kayttotarkoitus sc/Str
                                                        :tilavuus sc/Str
                                                        :kokonaisala sc/Str
@@ -61,7 +81,7 @@
                                                        :lammonlahde sc/Any
                                                        :varusteet sc/Any
                                                        :jaahdytysmuoto sc/Any
-                                                       :asuinhuoneistot sc/Any
+                                                       :asuinhuoneistot {:huoneisto [Huoneisto]}
                                                        :liitettyJatevesijarjestelmaanKytkin sc/Bool})
                (sc/optional-key :rakennelmanTiedot) (util/map-keys sc/optional-key
                                                       {:yksilointitieto sc/Str
