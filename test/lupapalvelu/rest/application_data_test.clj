764132a24eb889392289a90c3bd96f2592d8eb25
Jouni Honkala <Jouni.Honkala@solita.fi>
2016-11-24 16:29:41 +0200

    refactor

--- test/lupapalvelu/rest/application_data_test.clj
+++ test/lupapalvelu/rest/application_data_test.clj
@@ -1,12 +1,11 @@
-(ns lupapalvelu.rest.open-application-data-test
+(ns lupapalvelu.rest.application-data-test
   (:require [midje.sweet :refer :all]
             [midje.util :refer [testable-privates]]
             [lupapalvelu.document.rakennuslupa-canonical-test :refer [application-rakennuslupa
                                                                       application-tyonjohtajan-nimeaminen
                                                                       application-tyonjohtajan-nimeaminen-v2]]
             [lupapalvelu.rest.applications-data :as applications-data]
-            [lupapalvelu.rest.schemas :refer :all]
-            [schema.core :as sc]))
+            [lupapalvelu.rest.schemas :refer :all]))
 
 (testable-privates lupapalvelu.rest.applications-data process-applications)
 
@@ -17,6 +16,6 @@
     (fact "No documents -> Should fail"
       (process-applications [(dissoc rl :documents)]) => [])
     (fact "Unsupported permit type -> Should fail"
-          (process-applications [(assoc rl :permitType "YA")]) => [])
+      (process-applications [(assoc rl :permitType "YA")]) => [])
     (fact "Broken municipality code -> Should fail"
       (process-applications [(assoc rl :municipality "lasdflasdfaa")]) => [])))
