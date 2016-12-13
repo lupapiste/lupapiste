4d6a54983329633214e59588d995894d7d9879aa
Antti Rämö <antti.ramo@solita.fi>
2016-12-13 09:49:08 +0200

    added group headers in attachment type selection autocomplete

--- resources/private/ui-components/attachments/type/attachment-type-autocomplete-model.js
+++ resources/private/ui-components/attachments/type/attachment-type-autocomplete-model.js
@@ -16,11 +16,13 @@ LUPAPISTE.AttachmentTypeAutocompleteModel = function(params) {
     });
   });
 
+  function typeGroupIs(group, type) {
+    return type["type-group"] === group;
+  }
+
   var options = ko.pureComputed(function() {
     if (params.selectedTypeGroup()) {
-      return _.filter(attachmentTypes(), function(type) {
-        return type["type-group"] === params.selectedTypeGroup().value;
-      });
+      return _.filter(attachmentTypes(), _.partial(typeGroupIs, params.selectedTypeGroup().value));
     } else {
       return attachmentTypes();
     }
@@ -28,10 +30,28 @@ LUPAPISTE.AttachmentTypeAutocompleteModel = function(params) {
 
   self.query = ko.observable("");
 
+  function buildGroupTitle(typeGroup) {
+    return loc(["attachmentType", typeGroup, "_group_label"].join("."));
+  }
+
+  function withGroupHeaders(data) {
+    return _(data)
+      .map("type-group")
+      .uniq()
+      .map(function(typeGroup) {
+        var header = {"type-group": typeGroup, "title": buildGroupTitle(typeGroup), groupHeader: true};
+        var types = _.filter(data, _.partial(typeGroupIs, typeGroup));
+        return _.concat(header, types);
+      })
+      .flatten()
+      .value();
+  }
+
+
   self.data = ko.pureComputed(function() {
-    return util.filterDataByQuery({data: options(),
-                                   query: self.query(),
-                                   selected: self.selected(),
-                                   label: "title"});
+    return withGroupHeaders(util.filterDataByQuery({data: options(),
+                                                    query: self.query(),
+                                                    selected: self.selected(),
+                                                    label: "title"}));
   });
 };
