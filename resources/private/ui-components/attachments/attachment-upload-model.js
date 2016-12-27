27d2c2b27b63799d32f6dbef5c4026165a14b615
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-12-27 13:40:53 +0200

    waiting observable is ready only after binding attachments is done

--- resources/private/ui-components/attachments/attachment-upload-model.js
+++ resources/private/ui-components/attachments/attachment-upload-model.js
@@ -4,7 +4,7 @@ LUPAPISTE.AttachmentUploadModel = function( params ) {
 
   var service = lupapisteApp.services.attachmentsService;
 
-  self.componentTemplate = params.template || "attachment-upload-link-template";
+  self.componentTemplate = params.template || "attachment-upload-button-template";
 
   self.id = params.id;
   self.ltext = params.ltext;
@@ -14,12 +14,20 @@ LUPAPISTE.AttachmentUploadModel = function( params ) {
   self.upload = new LUPAPISTE.UploadModel(self, { allowMultiple:false });
   self.upload.init();
 
+  self.bindWaiting = ko.observable();
+
+  self.waiting = self.disposedComputed(function() {
+    return self.upload.waiting() || self.bindWaiting();
+  });
+
   self.disposedSubscribe(self.upload.files, function(files) {
     if (!_.isEmpty(files)) {
       var fileId = _.last(files).fileId;
+      self.bindWaiting(true);
       var status = service.bindAttachment( self.id, fileId );
       var statusSubscription = self.disposedSubscribe(status, function(status) {
         if ( service.pollJobStatusFinished(status) ) {
+          self.bindWaiting(false);
           self.upload.clearFile( fileId );
           self.unsubscribe(statusSubscription);
           self.sendEvent("attachment-upload", "finished", { ok: ko.unwrap(status) === service.JOB_DONE,
