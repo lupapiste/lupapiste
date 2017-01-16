50c427dc5da5fb77f0d507a584941cbe1269717b
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-01-16 15:08:15 +0200

    ui styling

--- resources/private/authority-admin-components/inspection-summary-templates/inspection-summary-template-bubble-model.js
+++ resources/private/authority-admin-components/inspection-summary-templates/inspection-summary-template-bubble-model.js
@@ -4,5 +4,13 @@ LUPAPISTE.InspectionSummaryTemplateBubbleModel = function(params) {
 
   ko.utils.extend(self, new LUPAPISTE.ComponentBaseModel());
 
+  self.templateId = ko.observable();
+  self.name = ko.observable("bar");
+  self.itemsText = ko.observable("bar1");
   self.bubbleVisible = params.bubbleVisible;
+
+  self.okVisible = ko.observable(true);
+  self.cancelVisible = ko.observable(true);
+
+
 };
