d0eea305193293b49bac01c66fa902aeb2a4cb2b
Antti Slunga <Antti.Slunga@solita.fi>
2017-01-19 14:36:50 +0200

    LPK-2070 Unselect archived documents

--- resources/private/application/archival-summary.js
+++ resources/private/application/archival-summary.js
@@ -175,7 +175,7 @@
   function selectIfArchivable(attachment) {
     var tila = util.getIn(attachment, ["metadata", "tila"]);
     if (attachment.archivable() && tila !== "arkistoitu") {
-      attachment.sendToArchive(true);
+      attachment.sendToArchive(!attachment.sendToArchive());
     }
   }
 
@@ -290,8 +290,20 @@
       _.forEach(self.archivedDocuments(), function(doc) {
         var tila = util.getIn(doc, ["metadata", "tila"]);
         if (tila !== "arkistoitu") {
-          doc.sendToArchive(true);
+          doc.sendToArchive(!doc.sendToArchive());
         }
+      });
+    };
+
+    self.unselectAll = function() {
+      _.forEach(archivedPreAttachments(), function(attachment) {
+        attachment.sendToArchive(false);
+      });
+      _.forEach(archivedPostAttachments(), function(attachment) {
+        attachment.sendToArchive(false);
+      });
+      _.forEach(self.archivedDocuments(), function(doc) {
+        doc.sendToArchive(false);
       });
     };
 
