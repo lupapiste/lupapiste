bdea32cee76f738ae9e773e5ce2373404cadf5a7
Antti Rämö <antti.ramo@solita.fi>
2017-01-20 09:33:35 +0200

    fix multioperation support for attachment tag grouping

--- src/lupapalvelu/attachment/tag_groups.clj
+++ src/lupapalvelu/attachment/tag_groups.clj
@@ -1,5 +1,6 @@
 (ns lupapalvelu.attachment.tag-groups
   (:require [clojure.set :refer [intersection union]]
+            [sade.util :refer [fn->] :as util]
             [sade.strings :as ss]
             [lupapalvelu.attachment.tags :as att-tags]
             [lupapalvelu.attachment.type :as att-type]))
@@ -23,42 +24,21 @@
   (->> (if (sequential? op) (mapv :id op) [(:id op)])
        (remove nil?)))
 
-(defn- hierarchy-level-tags [hierarchy]
-  (->> hierarchy
-       (map first)
-       (set)))
-
 (defn- tag-set [attachment]
   (-> attachment :tags set))
 
-(defn- tags-for-hierarchy-level [hierarchy attachment]
-  (let [tags (intersection (hierarchy-level-tags hierarchy)
-                           (tag-set attachment))]
-    (if (empty? tags)
-      #{}
-      tags)))
-
-(defn- all-tags-for-hierarchy-level [hierarchy attachments]
-  (apply union (map (partial tags-for-hierarchy-level hierarchy) attachments)))
+(defn- attachments-tag-sets-with-tag [tag attachments-tags]
+  (-> (filter (fn-> (contains? tag)) attachments-tags)
+      not-empty))
 
 (defn- filter-tag-groups
   "remove the tag groups whose tag is not found in the attachments"
-  [attachments hierarchy]
+  [attachments-tag-sets hierarchy]
   (letfn [(filter-recursively [[parent & children :as group]]
-            (if children
-              (apply concat [[parent] (filter-tag-groups attachments children)])
-              group))]
-    (->> hierarchy
-         (filter (comp (all-tags-for-hierarchy-level hierarchy attachments) first))
-         (map filter-recursively))))
-
-(defn- for-operation? [op-id attachment]
-  (= op-id (-> attachment :op :id)))
-
-(defn- operation-tag-group [attachments hierarchy op-id]
-  (concat [(att-tags/op-id->tag op-id)]
-          (filter-tag-groups (filter (partial for-operation? op-id) attachments)
-                             hierarchy)))
+            (when-let [attachments-tag-sets-in-group (attachments-tag-sets-with-tag parent attachments-tag-sets)]
+              (->> (filter-tag-groups attachments-tag-sets-in-group children)
+                   (cons parent))))]
+    (keep filter-recursively hierarchy)))
 
 (defn- partition-by-tag
   "Return [pre tagged post], where pre is all groups before one with
@@ -77,8 +57,7 @@
         op-ids               (-> (mapcat get-operation-ids attachments) distinct)]
     (if operation
       (concat pre
-              (map (partial operation-tag-group attachments (rest operation))
-                   op-ids)
+              (map (fn-> att-tags/op-id->tag (cons (rest operation))) op-ids)
               post)
       hierarchy)))
 
@@ -94,5 +73,5 @@
   ([{attachments :attachments :as application} hierarchy]
    (let [enriched-attachments (map add-tags attachments)]
      (->> hierarchy
-          (filter-tag-groups enriched-attachments)
-          (add-operation-tag-groups enriched-attachments)))))
+          (add-operation-tag-groups enriched-attachments)
+          (filter-tag-groups (map tag-set enriched-attachments))))))
