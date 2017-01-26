60802232f3f8fb31319fe556a74ade63d1667840
Antti Rämö <antti.ramo@solita.fi>
2017-01-26 10:40:40 +0200

    rerun groupType cleanup migration for ya-applications

--- src/lupapalvelu/migration/migrations.clj
+++ src/lupapalvelu/migration/migrations.clj
@@ -2701,7 +2701,7 @@
       {$and [{:permitType "YA"}
              {$or [{:attachments {$elemMatch {:groupType {$ne nil}}}}
                    {:attachments {$elemMatch {:op {$ne nil}}}}]}]}]
-  (defmigration remove-groupType-and-op-from-YA-applications
+  (defmigration remove-groupType-and-op-from-YA-applications-v2
     {:apply-when (pos? (mongo/count :applications attachments-with-groupType-or-op-match))}
     (update-applications-array :attachments
                                set-attachment-groupType-and-op-nil
