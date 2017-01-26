a819dbdaec85b7724376c9b1abe0e9392f7bb9b6
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-01-26 16:41:16 +0200

    experimenting with state

--- src/lupapalvelu/ui/checklist-summary.cljs
+++ src/lupapalvelu/ui/checklist-summary.cljs
@@ -3,15 +3,23 @@
 
 (enable-console-print!)
 
-(rum/defc checklist-summary < {:will-unmount (fn [state] (println "Will-unmount" state) state)
-                               :will-mount   (fn [state] (println "Will-mount" state) state)}
+(def state (atom {:rows []}))
+(def rows  (rum/cursor-in state [:rows]))
+
+(rum/defc summary-row [row-data]
+  [:tr [:td "Cell"] [:td (:text row-data)] [:td "Faa"]])
+
+(rum/defc checklist-summary < rum/reactive
   []
-  [:h1 "Tarkastusasiakirjan yhteenveto"]
-  [:table
-   [:tbody
-    (doall
-      (for [n (take 5 (range))]
-        [:tr [:td "Cell"] [:td (js/util.randomElementId (str "id-" n "-"))] [:td "Faa"]]))]])
+  [:div
+   [:h1 "Tarkastusasiakirjan yhteenveto"]
+   [:table
+    [:tbody
+     (doall
+       (for [row (rum/react rows)]
+         (summary-row row)))]]
+   [:button {:on-click (fn [_] (swap! rows conj {:id (str "row" (rand-int 50)) :text "Faa"}))}
+    "Click"]])
 
 (defn ^:export start [domId]
   (rum/mount (checklist-summary) (.getElementById js/document (name domId))))
