23e660b69353d3b8085861bd8769a893159b74fe
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-01-26 14:42:50 +0200

    use ReactDOM.unmountComponentAtNode when knockout component is disposed

--- resources/private/cljs-component/cljs-component.js
+++ resources/private/cljs-component/cljs-component.js
@@ -7,5 +7,25 @@ LUPAPISTE.CljsComponentModel = function(params) {
   $.getScript('/lp-static/js/'+self.componentName+'.js', function() {
       lupapalvelu.ui[_.snakeCase(self.componentName)].start(self.componentName);
   });
+
+  self.dispose = function(param) {
+    ReactDOM.unmountComponentAtNode(document.getElementById(self.componentName));
+  }
 };
+/*
+ko.components.register("cljs-component", {
+  viewModel: {createViewModel: function(params, componentInfo) {
+            // - 'params' is an object whose key/value pairs are the parameters
+            //   passed from the component binding or custom element
+            // - 'componentInfo.element' is the element the component is being
+            //   injected into. When createViewModel is called, the template has
+            //   already been injected into this element, but isn't yet bound.
+            // - 'componentInfo.templateNodes' is an array containing any DOM
+            //   nodes that have been supplied to the component. See below.
+            // Return the desired view model instance, e.g.:
+            return new LUPAPISTE.CljsComponentModel(params, componentInfo.element);
+  }},
+  template: {element: "cljs-component-template"}
+});*/
+
 
