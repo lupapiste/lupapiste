08ee6af544425d750bf004cbb3aa8738c76058e7
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-01-27 13:54:22 +0200

    PoC: subscribe to knockout observable and update component state from ko.subscribe

--- src/lupapalvelu/ui/checklist-summary.cljs
+++ src/lupapalvelu/ui/checklist-summary.cljs
@@ -3,18 +3,30 @@
 
 (enable-console-print!)
 
-(def state (atom {:rows []}))
+(def state (atom {:rows []
+                  :input ""}))
 (def rows  (rum/cursor-in state [:rows]))
+(def texti  (rum/cursor-in state [:input]))
 
 (rum/defc summary-row [row-data]
   [:tr [:td "Cell"] [:td (:text row-data)] [:td "Faa"]])
 
+(defn ko-subscription [value]
+  (swap! state assoc :input value))
+
 (rum/defc checklist-summary < rum/reactive
+                              {:init (fn [init-state props]
+                                       (swap! state assoc :input ((-> (aget props ":rum/initial-state")
+                                                                      :rum/args
+                                                                      first
+                                                                      (aget "texti"))))
+                                       init-state)}
   [ko-app]
-  (js/console.log "from cljs" (aget ko-app "texti"))
+  (.subscribe (aget ko-app "texti") ko-subscription)
   [:div
    [:h1 "Tarkastusasiakirjan yhteenveto"]
-   [:pre ((aget ko-app "texti"))]
+   ; [:pre ((aget ko-app "texti")) " -> oneshot, is not updated when ko subscription changes, only when component is re-rendered"]
+   [:pre (rum/react texti) "-> rum/react"]
    [:input ]
    [:table
     [:tbody
