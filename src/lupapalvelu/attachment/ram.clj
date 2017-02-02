1160ccb73b8181f9902d60d88568c8e85c471695
Antti Rämö <antti.ramo@solita.fi>
2017-02-02 07:58:37 +0200

    fix new ram notification recipients functions

--- src/lupapalvelu/attachment/ram.clj
+++ src/lupapalvelu/attachment/ram.clj
@@ -26,7 +26,7 @@
 
 (defn notify-new-ram-attachment! [application attachment-id created]
   (notifications/notify! :new-ram-notification {:application application
-                                                :recipients  (some->> application :handlers (map :userId) not-empty #(usr/get-users {:id {$in %}}))
+                                                :recipients  (some->> application :handlers (map :userId) not-empty (#(usr/get-users {:id {$in %}})))
                                                 :data        {:attachment-id attachment-id
                                                               :created-date  (util/to-local-date created)}}))
 
