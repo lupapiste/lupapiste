a339c5008866933f0500f37ec98551e308babc3c
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-02-14 17:48:23 +0200

    indentation

--- src/lupapalvelu/ui/components.cljs
+++ src/lupapalvelu/ui/components.cljs
@@ -2,20 +2,20 @@
   (:require [rum.core :as rum]))
 
 (rum/defc select [change-fn data-test-id value options]
-          [:select.form-entry.is-middle
-           {:on-change #(change-fn (.. % -target -value))
-            :data-test-id data-test-id
-            :value     value}
-           (map (fn [[k v]] [:option {:value k} v]) options)])
+  [:select.form-entry.is-middle
+   {:on-change    #(change-fn (.. % -target -value))
+    :data-test-id data-test-id
+    :value        value}
+   (map (fn [[k v]] [:option {:value k} v]) options)])
 
 (defn confirm-dialog [titleKey messageKey callback]
-          (.send js/hub
-                 "show-dialog"
-                 #js
-                   {:ltitle titleKey
-                    :size   "medium"
-                    :component "yes-no-dialog"
-                    :componentParams #js {:ltext     messageKey
-                                          :yesFn     callback
-                                          :lyesTitle "ok"
-                                          :lnoTitle  "cancel"}}))
+  (.send js/hub
+         "show-dialog"
+         #js
+           {:ltitle titleKey
+            :size   "medium"
+            :component "yes-no-dialog"
+            :componentParams #js {:ltext     messageKey
+                                  :yesFn     callback
+                                  :lyesTitle "ok"
+                                  :lnoTitle  "cancel"}}))
