3beb5154c46544b06988143a587b3eb9cda7a164
Jaakko Ruutiainen <Jaakko.Ruutiainen@sito.fi>
2017-02-22 10:58:40 +0200

    Add urban plan document search for Valkeakoski Add building convention document search for Valkeakoski

--- test/lupapalvelu/wfs_test.clj
+++ test/lupapalvelu/wfs_test.clj
@@ -9,13 +9,25 @@
 (facts "plan-info-config"
 
   (fact "default"
-    (let [{:keys [url layers format]} (plan-info-config "999")]
+    (let [{:keys [url layers format]} (plan-info-config "999" "plan-info")]
       url => (str (env/value :geoserver :host) (env/value :geoserver :wms :path))
       layers => "999_asemakaavaindeksi"
       format => "application/vnd.ogc.gml"))
 
   (fact "Mikkeli"
-    (let [{:keys [url layers format]} (plan-info-config "491")]
+    (let [{:keys [url layers format]} (plan-info-config "491" "plan-info")]
       url => "http://194.111.49.141/WMSMikkeli.mapdef?"
       layers => "Asemakaavaindeksi"
+      format => "text/xml"))
+
+  (fact "Valkeakoski"
+    (let [{:keys [url layers format]} (plan-info-config "908" "plan-info")]
+      url => "http://193.208.197.20/ValkeakoskiWMS.mapdef?"
+      layers => "Asemakaavaindeksi"
+      format => "text/xml"))
+
+  (fact "Valkeakoski-rakennustapaohje"
+    (let [{:keys [url layers format]} (plan-info-config "908" "rakennustapaohje")]
+      url => "http://193.208.197.20/ValkeakoskiWMS.mapdef?"
+      layers => "Rakennustapaohjeindeksi"
       format => "text/xml")))
