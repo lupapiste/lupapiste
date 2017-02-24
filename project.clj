dcf844bc357e695185a05170e59132ff1d255cb5
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-02-24 10:39:12 +0200

    exclude ring from compojure

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  ; Web frameworks
                  [ring "1.5.1" :exclusions [commons-fileupload org.clojure/tools.reader]]
                  [noir "1.3.0" :exclusions [compojure clj-stacktrace org.clojure/tools.macro ring hiccup bultitude]]
-                 [compojure "1.1.9" :exclusions [org.clojure/tools.macro]]
+                 [compojure "1.1.9" :exclusions [org.clojure/tools.macro ring]]
                  [metosin/ring-swagger "0.22.12"]
                  [metosin/ring-swagger-ui "2.2.5-0"]
 
