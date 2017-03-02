2bbd82379ecf6d3575b2596e04c10e4b67470bd6
Jaakko Ruutiainen <Jaakko.Ruutiainen@sito.fi>
2017-03-02 15:03:03 +0200

    New Oskari version

--- project.clj
+++ project.clj
@@ -121,7 +121,7 @@
 
                  ;; Lupapiste libraries
                  ; Oskari map (https://github.com/lupapiste/oskari)
-                 [lupapiste/oskari "0.9.58"]
+                 [lupapiste/oskari "0.9.60"]
                  ; Shared domain code (https://github.com/lupapiste/commons)
                  [lupapiste/commons "0.7.89"]
                  ; Smoke test lib (https://github.com/lupapiste/mongocheck)
