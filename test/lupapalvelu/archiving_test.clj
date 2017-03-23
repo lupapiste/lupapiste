a0c511ba57037e027f9f25ce64f90bad3c1a64b5
Antti Slunga <Antti.Slunga@solita.fi>
2017-03-23 13:42:21 +0200

    LPK-2797 Valid YA state check based on post-verdict-states graph

--- test/lupapalvelu/archiving_test.clj
+++ test/lupapalvelu/archiving_test.clj
@@ -6,17 +6,19 @@
 (facts "Should archiving valid YA applications"
   (fact "Valid YA application with valid state - should be archived"
     (valid-ya-state? {:id "LP-XXX-2017-00001" :permitType "YA" :state "verdictGiven"}) => true
-    (valid-ya-state? {:id "LP-XXX-2017-00002" :permitType "YA" :state "finished"}) => true)
-       ;; pitäskö olla kaikki post verdict statet?
+    (valid-ya-state? {:id "LP-XXX-2017-00002" :permitType "YA" :state "finished"}) => true
+    (valid-ya-state? {:id "LP-XXX-2017-00003" :permitType "YA" :state "extinct"}) => true
+    (valid-ya-state? {:id "LP-XXX-2017-00004" :permitType "YA" :state "appealed"}) => true
+    (valid-ya-state? {:id "LP-XXX-2017-00005" :permitType "YA" :state "closed"}) => true)
   (fact "Valid YA application with invalid state - should not be archived"
-    (valid-ya-state? {:id "LP-XXX-2017-00003" :permitType "YA" :state "open"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00004" :permitType "YA" :state "submitted"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00005" :permitType "YA" :state "sent"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00006" :permitType "YA" :state "draft"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00007" :permitType "YA" :state "complementNeeded"}) => false)
-  (fact "Valid R application with valid R state - should not be archived"
-    (valid-ya-state? {:id "LP-XXX-2017-00008" :permitType "R" :state "verdictGiven"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00009" :permitType "R" :state "closed"}) => false
-    (valid-ya-state? {:id "LP-XXX-2017-00009" :permitType "R" :state "foremanVerdictGiven"}) => false)
-  (fact "Invalid R application with invalid YA state - should not be archived"
-    (valid-ya-state? {:id "LP-XXX-2017-00010" :permitType "R" :state "finished"}) => false))
+    (valid-ya-state? {:id "LP-XXX-2017-00006" :permitType "YA" :state "open"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00007" :permitType "YA" :state "submitted"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00008" :permitType "YA" :state "sent"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00009" :permitType "YA" :state "draft"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00010" :permitType "YA" :state "complementNeeded"}) => false)
+  (fact "Valid R application with valid R state - should not be in valid YA state"
+    (valid-ya-state? {:id "LP-XXX-2017-00011" :permitType "R" :state "verdictGiven"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00012" :permitType "R" :state "closed"}) => false
+    (valid-ya-state? {:id "LP-XXX-2017-00013" :permitType "R" :state "foremanVerdictGiven"}) => false)
+  (fact "Invalid R application with invalid YA state - should not be in valid YA state"
+    (valid-ya-state? {:id "LP-XXX-2017-00014" :permitType "R" :state "finished"}) => false))
