40918db3ccb819f1be3f23f02cc888fb00772a33
Antti Rämö <antti.ramo@solita.fi>
2017-04-06 10:53:12 +0300

    display warning message in construction site doc if property id is entered manually

--- resources/private/global-models/application-model.js
+++ resources/private/global-models/application-model.js
@@ -31,6 +31,7 @@ LUPAPISTE.ApplicationModel = function() {
   self.municipality = ko.observable();
   self.permitType = ko.observable("R");
   self.propertyId = ko.observable();
+  self.propertyIdSource = ko.observable();
   self.title = ko.observable();
   self.created = ko.observable();
   self.modified = ko.observable();
