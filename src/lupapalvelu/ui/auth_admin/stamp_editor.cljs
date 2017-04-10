0b9eac0f616e00a3beaeec314a695b0a93205d3c
Antti Rämö <antti.ramo@solita.fi>
2017-04-10 12:38:57 +0300

    added stamp selector in authority admin stamp editor

--- src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
+++ src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
@@ -1,7 +1,8 @@
 (ns lupapalvelu.ui.auth-admin.stamp-editor
   (:require [rum.core :as rum]
+            [clojure.string :as string]
             [lupapalvelu.ui.authorization :as auth]
-            [lupapalvelu.ui.common :refer [query command] :as common]
+            [lupapalvelu.ui.common :refer [query command loc] :as common]
             [lupapalvelu.ui.components :as uc]
             [lupapalvelu.ui.util :as jsutil]
             [lupapalvelu.ui.hub :as hub]
@@ -26,16 +27,44 @@
                             (->> (:stamps state)
                                  (find-by-key :id selected-id))))))
 
+(defn- update-stamp-view [id]
+  (swap! component-state assoc-in [:view :selected-stamp-id] id))
+
+(defn- refresh
+  ([] (refresh nil))
+  ([cb]
+   (query :stamps
+          (fn [data]
+            (swap! component-state assoc :stamps (:stamps data))
+            (when cb (cb data)))
+          :organization-id (-> @component-state :organization-id))))
+
+(rum/defc stamp-select [stamps selection]
+  (uc/select update-stamp-view
+             "stamp-select"
+             selection
+             (cons ["" (loc "choose")]
+                   (map (juxt :id :name) stamps))))
+
+(defn init
+  [init-state props]
+  (let [[org-id auth-model] (-> (aget props ":rum/initial-state") :rum/args)]
+    (swap! component-state assoc :organization-id org-id :auth-models {:global-auth-model auth-model})
+    (when (auth/ok? auth-model :stamps) (refresh))
+    init-state))
+
 (rum/defc stamp-editor < rum/reactive
-  {:init (constantly nil)
-   :will-unmount (constantly nil) #_(fn [& _] (reset! component-state empty-component-state))}
-  [org-id application-auth-model]
-  [:div "Hello from stamp editor"])
+  {:init init
+   :will-unmount (fn [& _] (reset! component-state empty-component-state))}
+  [org-id global-auth-model]
+  [:div
+   [:h1 (js/loc "stamp-editor.tab.title")]
+   [:div (stamp-select (rum/react (rum/cursor-in component-state [:stamps])) (rum/react (rum/cursor-in component-state [:view :selected-stamp-id])))]])
 
 (defonce args (atom {}))
 
 (defn mount-component []
-  (rum/mount (stamp-editor (:org-id @args) (:auth-model @args))
+  (rum/mount (stamp-editor ((:org-id @args)) (:auth-model @args))
              (.getElementById js/document (:dom-id @args))))
 
 (defn ^:export start [domId componentParams]
