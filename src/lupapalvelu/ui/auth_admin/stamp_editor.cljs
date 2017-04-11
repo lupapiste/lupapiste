0dfe21b257e39459a6598aa962a497df539231c2
Antti Rämö <antti.ramo@solita.fi>
2017-04-11 15:08:04 +0300

    added new stamp button in stamp editor

--- src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
+++ src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
@@ -8,10 +8,15 @@
             [lupapalvelu.ui.hub :as hub]
             [lupapalvelu.ui.rum-util :as rum-util]))
 
-(defn find-by-key
-  "Return item from sequence col of maps where element k (keyword) matches value v."
-  [k v col]
-  (some (fn [m] (when (= v (get m k)) m)) col))
+(defn find-by-id
+  [v col]
+  (some #(when (= v (:id %)) %) col))
+
+(def empty-stamp   {:name     ""
+                    :position {:x 0 :y 0}
+                    :page     :first
+                    :qr-code  true
+                    :rows     [[]]})
 
 (def empty-component-state {:stamps []
                             :view {:bubble-visible false
@@ -20,11 +25,11 @@
 (def component-state  (atom empty-component-state))
 
 (def selected-stamp (rum-util/derived-atom
-                        [component-state]
-                        (fn [state]
-                          (when-let [selected-id (get-in state [:view :selected-stamp-id])]
-                            (->> (:stamps state)
-                                 (find-by-key :id selected-id))))))
+                     [component-state]
+                     (fn [state]
+                       (or (-> (get-in state [:view :selected-stamp-id])
+                               (find-by-id (:stamps state)))
+                           empty-stamp))))
 
 (defn- update-stamp-view [id]
   (swap! component-state assoc-in [:view :selected-stamp-id] id))
@@ -45,6 +50,15 @@
              (cons ["" (loc "choose")]
                    (map (juxt :id :name) (rum/react stamps)))))
 
+(rum/defc new-stamp-button [bubble-visible selected-stamp-id]
+  [:button.positive
+   {:on-click (fn [_]
+                (reset! selected-stamp-id nil)
+                (reset! bubble-visible true))
+    :data-test-id "open-create-stamp-bubble"}
+   [:i.lupicon-circle-plus]
+   [:span (loc "stamp-editor.new-stamp.button")]])
+
 (defn init
   [init-state props]
   (let [[auth-model] (-> (aget props ":rum/initial-state") :rum/args)]
@@ -59,14 +73,19 @@
      ;; TODO: Editor here
      ]))
 
-(rum/defc stamp-editor < rum/reactive
+(rum/defc stamp-editor
   {:init init
    :will-unmount (fn [& _] (reset! component-state empty-component-state))}
   [global-auth-model]
-  [:div
-   [:h1 (loc "stamp-editor.tab.title")]
-   [:div (stamp-select (rum/cursor-in component-state [:stamps]) (rum/cursor-in component-state [:view :selected-stamp-id]))]
-   [:div.row.edit-stamp-bubble (edit-stamp-bubble (rum/cursor-in component-state [:view :bubble-visible]))]])
+  (let [stamps            (rum/cursor component-state :stamps)
+        selected-stamp-id (rum/cursor-in component-state [:view :selected-stamp-id])
+        bubble-visible    (rum/cursor-in component-state [:view :bubble-visible])]
+    [:div
+     [:h1 (loc "stamp-editor.tab.title")]
+     [:div
+      (stamp-select stamps selected-stamp-id)
+      (new-stamp-button bubble-visible selected-stamp-id)]
+     [:div.row.edit-sta (edit-stamp-bubble bubble-visible)]]))
 
 (defonce args (atom {}))
 
