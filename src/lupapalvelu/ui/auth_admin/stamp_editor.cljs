628de3bb39dfe60f19507f1a2c191091d1bb2d5b
Juhana Laurinharju <juhana.laurinharju@solita.fi>
2017-04-20 11:11:33 +0300

    Basic layout for stamp template editor

--- src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
+++ src/lupapalvelu/ui/auth_admin/stamp_editor.cljs
@@ -66,17 +66,84 @@
     (when (auth/ok? auth-model :stamp-templates) (refresh))
     init-state))
 
+(rum/defc stamp-btn [text]
+  [:button.stamp-editor-btn {:draggable true}
+   [:i.lupicon-circle-plus]
+   [:span text]])
+
+(rum/defc stamp-container [& stamp-names]
+  [:div {:style {:margin-bottom "5px"}}
+   (map stamp-btn stamp-names)])
+
+(def add-here-placeholder
+  [:button.stamp-row-placeholder
+   [:span "Pudota tähän"]])
+
+(rum/defc stamp-row-btn [text]
+  [:button.stamp-row-btn
+   [:span text]
+   [:i.lupicon-circle-remove]])
+
+(rum/defc stamp-row [{:keys [row-number stamps]}]
+  (if (and row-number stamps)
+    (let [stamp-buttons (map stamp-row-btn stamps)]
+      [:div.stamp-row
+       [:button.stamp-row-label  [:span (str "Rivi " row-number)]]
+       stamp-buttons
+       add-here-placeholder])
+    [:span "error: stamp-row"]))
+
+(rum/defc form-entry [label-string]
+  [:span.form-entry
+   [:label.form-label.form-label-string label-string]
+   [:input.form-input.text]])
+
 (rum/defc edit-stamp-bubble < rum/reactive
   [visible?]
   (when (rum/react visible?)
     [:div.edit-stamp-bubble
+     [:div.group-buttons
+      {:style {:background-color "#f6f6f6"
+               :border "1px solid #dddddd"}}
+      ;;TODO: onks joku otsikkorivicontainer-luokka josta tulis toi oikee harmaa taustaväri ja muut tyylit niinku haitareissa?
+      (form-entry "Leiman nimi:")
+      [:button.secondary.is-right
+       [:i.lupicon-remove]
+       [:span "Poista"]]]
+     [:div.form-group {:style {:display :block}}
+      [:div.form-group {:style {:width "60%"
+                                :display :inline-block}}
+       [:label.form-label.form-label-group "Leiman sijainti"]
+       [:div
+        (form-entry "Oikeasta reunasta (mm)")
+        (form-entry "Alareunasta (mm)")]
+       [:div
+        (form-entry "Leiman tausta") ;;TODO: vaihda dropdowniin
+        (form-entry "Leimattava sivu")] ;;TODO: vaihda dropdowniin
+       ]
+      [:div.form-group {:style {:width "35%"
+                                :border "1px solid"
+                                :display :inline-block}}
+       [:div
+        "sisältöä"]]
+      [:div.form-group
+       [:label.form-label.form-label-group "Leiman sisältö"]
+       (stamp-container "Vapaa teksti" "Kuluva pvm" "Päätös pvm" "Kuntalupatunnus" "Käyttäjä" "Organisaatio" "LP-tunnus" "Rakennutunnus")
+       [:div "Raahaa ylläolevia leiman sisältökenttiä..."]
+       [:div ;;many rows
+        (stamp-row {:row-number 1
+                    :stamps ["Asian tunnus" "Kuntalupatunnus"]}1)
+        (stamp-row {:row-number 2
+                    :stamps ["Hiphei, laitoin tähän tekstiä" "Kuluva pvm" "juupeli juu"]})
+        (stamp-row {:row-number 3
+                    :stamps []})]]]
      ;; TODO: Editor here
      ]))
 
 (rum/defc stamp-editor
+  [global-auth-model]
   {:init init
    :will-unmount (fn [& _] (reset! component-state empty-component-state))}
-  [global-auth-model]
   (let [stamps            (rum/cursor component-state :stamps)
         selected-stamp-id (rum/cursor-in component-state [:view :selected-stamp-id])
         bubble-visible    (rum/cursor-in component-state [:view :bubble-visible])]
