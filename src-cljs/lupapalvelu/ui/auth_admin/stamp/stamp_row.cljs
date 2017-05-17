f845db68dda5bd0a9f082d80ae62883f9a2ae9e1
Markus Penttila <markus.penttila@solita.fi>
2017-05-17 13:27:14 +0300

    Fix drag-and-drop data transfer on browsers other than Chrome

--- src-cljs/lupapalvelu/ui/auth_admin/stamp/stamp_row.cljs
+++ src-cljs/lupapalvelu/ui/auth_admin/stamp/stamp_row.cljs
@@ -12,15 +12,15 @@
 
 (rum/defcs stamp-row < rum/reactive
                        (rum/local 0 ::drag-event-sum)
-  [state {:keys [index rows-cursor drag-source debug-data]}]
+  [state {:keys [index rows-cursor drag-source]}]
   (let [stamp-row-cursor (rum/cursor rows-cursor index)
         drag-event-sum (::drag-event-sum state)
+        drag-element (rum/react (rum/cursor-in component-state [:editor :drag-element]))
         closest-elem (rum/cursor-in component-state [:editor :closest-element])
         field-buttons (for [[idx _] (stamp-util/indexed (rum/react stamp-row-cursor))]
                         (rum/with-key
                           (stamp-row-field {:data       (rum/cursor-in stamp-row-cursor [idx])
                                             :remove     (remove-button rows-cursor index stamp-row-cursor idx)
-                                            :debug-data debug-data
                                             :row-number index
                                             :row-index  idx})
                           idx))
@@ -33,24 +33,18 @@
         on-drag-leave (fn [_]
                         (swap! drag-event-sum dec))
         is-dragged-over? (pos? @drag-event-sum)
-        mouse-move-handler (fn [e]
-                             (let [dt (-> e .-dataTransfer)
-                                   op-type (.-type (aget (.-items dt) 0))
-                                   effect (case op-type
-                                            "newfield" "copy"
-                                            "movefield" "move"
-                                            nil)]
-                               (when effect
-                                 (.preventDefault e)
-                                 (set! (-> e .-dataTransfer .-dropEffect) effect)))
-                             (let [client-x (aget e "clientX")
-                                   client-y (aget e "clientY")
-                                   data {:row-mouse   {:client-x client-x
-                                                       :client-y client-y}
-                                         :current-row index}
-                                   dom-data (stamp-util/dom-data)]
-                               (reset! closest-elem (stamp-util/closest-with-edge
-                                                      (merge data dom-data)))))
+        on-drag-over (fn [e]
+                       (when-let [drag-type (:type drag-element)]
+                         (.preventDefault e)
+                         (set! (-> e .-dataTransfer .-dropEffect) (name drag-type)))
+                       (let [client-x (aget e "clientX")
+                             client-y (aget e "clientY")
+                             data {:row-mouse   {:client-x client-x
+                                                 :client-y client-y}
+                                   :current-row index}
+                             dom-data (stamp-util/dom-data)]
+                         (reset! closest-elem (stamp-util/closest-with-edge
+                                                (merge data dom-data)))))
         placeholder-location (stamp-util/closest->placeholder-position (rum/react closest-elem))
         placeholder-row? (= index (first placeholder-location))
         split-pos (or (second placeholder-location) 0)
@@ -62,7 +56,7 @@
         [before after] (when (number? split-pos) (split-at split-pos field-buttons))]
     [:div.stamp-row {:on-drag-enter   on-drag-enter
                      :on-drag-leave   on-drag-leave
-                     :on-drag-over    mouse-move-handler
+                     :on-drag-over    on-drag-over
                      :on-drop         on-drop
                      :data-row-number index}
      [:div.stamp-row-label [:span (str (loc "stamp-editor.row") " " (inc index))]]
