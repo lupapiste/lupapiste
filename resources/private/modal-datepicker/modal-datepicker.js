7fbedd21db2a9e96f0fb4b9fb6e60bceac4c4b36
Antti Slunga <Antti.Slunga@solita.fi>
2017-05-26 13:04:36 +0300

    LPK-2867 Revert unneeded changes

--- resources/private/modal-datepicker/modal-datepicker.js
+++ resources/private/modal-datepicker/modal-datepicker.js
@@ -77,27 +77,12 @@ LUPAPISTE.ModalDatepickerModel = function() {
 
   //Open the dialog
 
-  self.openWithConfig = function(config, app) {c
+  self.openWithConfig = function(config, app) {
     self.config = config;
     if (self.config) {
       self.reset(app);
       LUPAPISTE.ModalDialog.open(self.dialogSelector);
     }
   };
-
-  hub.subscribe("modal-datepicker", function(data) {
-    console.log("Availlaan datepicker");
-    var config =
-      {checkIntegrationAvailability: false,
-       dateParameter       : data.value,
-       extraParameters     : {lang: loc.getCurrentLanguage()},
-       dateSelectorLabel   : data.dateSelectorLabel,
-       dialogHeader        : data.dialogHeader,
-       dialogHelpParagraph : data.dialogHelpParagraph,
-       dialogButtonSend    : data.dialogButtonSend,
-       areYouSureMessage   : data.areYouSureMessage};
-    self.config = config;
-    LUPAPISTE.ModalDialog.open(self.dialogSelector);
-  });
 
 };
