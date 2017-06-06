bd8223732285978854b44044142b8dd2ed8d2aea
Antti Salmivaara <antti.salmivaara@solita.fi>
2017-06-06 17:32:35 +0300

    add ok option functionality

--- src/lupapalvelu/organization.clj
+++ src/lupapalvelu/organization.clj
@@ -97,6 +97,7 @@
    (sc/optional-key :app-required-fields-filling-obligatory) sc/Bool
    (sc/optional-key :assignments-enabled) sc/Bool
    (sc/optional-key :extended-construction-waste-report-enabled) sc/Bool
+   (sc/optional-key :automatic-ok-for-attachments-enabled) sc/Bool
    (sc/optional-key :areas) sc/Any
    (sc/optional-key :areas-wgs84) sc/Any
    (sc/optional-key :calendars-enabled) sc/Bool
@@ -262,6 +263,9 @@
    (let [organization (get-organization organization-id)
          default (get-in organization [:name :fi] (str "???ORG:" (:id organization) "???"))]
      (get-in organization [:name lang] default))))
+
+(defn get-organization-auto-ok [organization-id]
+  (:automatic-ok-for-attachments-enabled (get-organization organization-id)))
 
 (defn resolve-organizations
   ([municipality]
