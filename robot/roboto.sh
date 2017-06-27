af03c680b99020a495937cf36b45ca02cb9d130e
Markus Penttila <markus.penttila@solita.fi>
2017-06-27 11:13:07 +0300

    Fix Roboto Chrome version check

--- robot/roboto.sh
+++ robot/roboto.sh
@@ -265,7 +265,7 @@ check_env() {
          ;;
       "chrome" )
          CG=$(google-chrome --version)
-         echo "$CG" | grep -q "^Google Chrome (5[8-9]|60)\." || versionfail "Major version '$CG' of Chrome may not work yet. Update ${BASH_SOURCE}:${LINENO} if this is fine."
+         echo "$CG" | grep -q "^Google Chrome \(5[8-9]\|60\)\." || versionfail "Major version '$CG' of Chrome may not work yet. Update ${BASH_SOURCE}:${LINENO} if this is fine."
          # Clean up old Chrome temp files if there
          rm -rf /tmp/.com.google.Chrome.*
          rm -rf /tmp/.org.chromium*
