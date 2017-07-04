d8198579b0fb0f4d3f9702a9febe23ffaa877774
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-07-04 15:27:34 +0300

    invite-with-role reintroduced for LPK-2976

--- resources/private/ui-components/docgen/person-select/docgen-person-select-model.js
+++ resources/private/ui-components/docgen/person-select/docgen-person-select-model.js
@@ -11,6 +11,7 @@ LUPAPISTE.DocgenPersonSelectModel = function( params ) {
   self.selectValue = ko.observable( self.value() );
   var partiesModel = _.get(params, "docModel.partiesModel");
   self.appId = partiesModel.applicationId;
+  self.myNs = self.path.slice(0, self.path.length - 1).join(".");
 
   var collectionFn = _.get(params, "docModel.getCollection") || function() { return "documents" };
   var collection = collectionFn();
@@ -28,11 +29,10 @@ LUPAPISTE.DocgenPersonSelectModel = function( params ) {
 
   self.setUserToDocumentOk = ko.observable(false);
   if (self.authModel.ok("set-user-to-document")) {
-    var myNs = self.path.slice(0, self.path.length - 1).join(".");
     self.disposedSubscribe(self.selectValue, function(value) {
       if (value !== self.value()) {
         if (!_.isEmpty(value)) {
-          ajax.command("set-user-to-document", { id: self.appId, documentId: self.documentId, userId: value, path: myNs, collection: collection })
+          ajax.command("set-user-to-document", { id: self.appId, documentId: self.documentId, userId: value, path: self.myNs, collection: collection })
             .success( _.partial( repository.load, self.appId, _.noop))
             .error(function(e) {
               if (e.text !== "error.application-does-not-have-given-auth") {
@@ -47,30 +47,16 @@ LUPAPISTE.DocgenPersonSelectModel = function( params ) {
     self.setUserToDocumentOk(true);
   }
 
-  /* // new invite
-         if (authorizationModel.ok("invite-with-role")) {
-           var button =
-             $("<button>", {
-               "class": "icon-remove positive",
-               text: loc("personSelector.invite"),
-               click: function () {
-                 $("#invite-document-name").val(self.schemaName).change();
-                 $("#invite-document-path").val(myNs).change();
-                 $("#invite-document-id").val(self.docId).change();
-                 LUPAPISTE.ModalDialog.open("#dialog-valtuutus");
-                 return false;
-               }
-             });
-           if (options && options.dataTestSpecifiers) {
-             button.attr("data-test-id", "application-invite-" + self.schemaName);
-           }
-           button.appendTo(span);
-         } */
   self.inviteWithRoleOk = ko.observable(false);
   if (self.authModel.ok("invite-with-role")) {
     self.inviteWithRoleOk(true);
   }
 
-  self.invite = function() { alert(this); }
+  self.invite = function() {
+    $("#invite-document-name").val(self.schemaName).change();
+    $("#invite-document-path").val(self.myNs).change();
+    $("#invite-document-id").val(self.documentId).change();
+    LUPAPISTE.ModalDialog.open("#dialog-valtuutus");
+  };
 
 };
