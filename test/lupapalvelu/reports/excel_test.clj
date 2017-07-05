fe1836f89d8c23a36868797ef539eb4f74b8064c
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-07-05 12:20:14 +0300

    add more restrictions for sheet-name generation

--- test/lupapalvelu/reports/excel_test.clj
+++ test/lupapalvelu/reports/excel_test.clj
@@ -1,31 +1,35 @@
 (ns lupapalvelu.reports.excel-test
   (:require [dk.ative.docjure.spreadsheet :as spreadsheet]
-            [lupapalvelu.reports.excel :refer :all]
             [clojure.test :refer [is]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.properties :as prop]))
+            [clojure.test.check.properties :as prop]
+            [lupapalvelu.reports.excel :refer :all]
+            [sade.strings :as ss]
+            [sade.schemas :as ssc]))
 
 
 (defspec sheet-generation-test
-  20
+  15
   (prop/for-all [sheets (gen/let
-                          [columns (gen/such-that not-empty (gen/vector gen/keyword))
+                          [columns (gen/not-empty (gen/vector gen/keyword))
                            sheet-count (gen/such-that (fn [i] (< 0 i)) gen/int)]
-                          (gen/such-that
-                            not-empty
+                          (gen/not-empty
                             (gen/vector-distinct-by
-                              :sheet-name
-                              (gen/hash-map :sheet-name (gen/not-empty gen/string-alphanumeric)
+                              (comp ss/upper-case :sheet-name)
+                              (gen/hash-map :sheet-name (gen/such-that
+                                                          (ssc/max-length-constraint 31)
+                                                          (gen/not-empty gen/string-alphanumeric)
+                                                          30)
                                             :header (gen/return (map str columns))
                                             :row-fn (gen/return (apply juxt columns))
-                                            :data (gen/such-that not-empty
-                                                                 (gen/vector
-                                                                   (gen/fmap
-                                                                     (fn [row] (zipmap columns row))
-                                                                     (gen/vector
-                                                                       (gen/not-empty gen/string-ascii)
-                                                                       (count columns))))))
+                                            :data (gen/not-empty
+                                                    (gen/vector
+                                                      (gen/fmap
+                                                        (fn [row] (zipmap columns row))
+                                                        (gen/vector
+                                                          (gen/not-empty gen/string-ascii)
+                                                          (count columns))))))
                               {:num-elements sheet-count})))]
                 (is (= (count (spreadsheet/sheet-seq (create-workbook sheets)))
                        (count sheets)))))
