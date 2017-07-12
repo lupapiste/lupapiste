43c19193111ff0efd00b8ba6a37d36dfb88bf166
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-07-12 14:03:47 +0300

    Revert "move org generator to generator namespace"
    This reverts commit 39288420e0395be816d0b3d452b8650a5d55bc55.

--- test/lupapalvelu/generators/organization.clj
+++ test/lupapalvelu/generators/organization.clj
@@ -10,18 +10,3 @@
   (gen/fmap str (gen/large-integer* {:min 100, :max 999})))
 
 (ssg/register-generator org/OrgId org-id-gen)
-
-(def org-with-const-id
-  (gen/fmap (fn [org] (assoc org :id "100"))
-            (ssg/generator org/Organization)))
-
-(defn generate
-  "Generates a set of organizations with different ids"
-  ([] (generate 10))
-  ([num]
-   (gen/let [org-ids (gen/set (ssg/generator org/OrgId) {:num-elements num
-                                                         :max-tries 50})
-             orgs (gen/vector org-with-const-id num)]
-            (let [fix-id (fn [id org] (assoc org :id id))
-                  with-fixed-ids (map fix-id org-ids orgs)]
-              (set with-fixed-ids)))))
