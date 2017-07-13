c83bc2d84d6ac87222433a527f3c223a024c6f6d
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-07-13 14:09:51 +0300

    non ascii

--- src/lupapalvelu/integrations/ely.clj
+++ src/lupapalvelu/integrations/ely.clj
@@ -3,10 +3,10 @@
 
 (def r-statement-types
   "ELY statement types for R.
-  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusjärjestyspäätökset; rakennus-, toimenpide-, purkamis- ja maisematyöluvat.
+  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusj\u00e4rjestysp\u00e4\u00e4t\u00f6kset; rakennus-, toimenpide-, purkamis- ja maisematy\u00f6luvat.
   07.01.09 Rakennussuojelu
-  06.05.09  Teiden suoja- ja näkemäalueelle rakentaminen (poikkeamisluvat).
-  06.05.10 Teiden suoja- ja näkemäalueen ulkopuolelle rakentaminen (naapurin kuuleminen)"
+  06.05.09  Teiden suoja- ja n\u00e4kem\u00e4alueelle rakentaminen (poikkeamisluvat).
+  06.05.10 Teiden suoja- ja n\u00e4kem\u00e4alueen ulkopuolelle rakentaminen (naapurin kuuleminen)"
   ["Lausuntopyynt\u00f6 maisematy\u00f6luvasta"
    "Lausuntopyynt\u00f6 purkamisaikomuksesta"
    "Lausuntopyynt\u00f6 purkamislupahakemuksesta"
@@ -17,9 +17,9 @@
 
 (def p-statement-types
   "ELY statement types for P.
-  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusjärjestyspäätökset; rakennus-, toimenpide-, purkamis- ja maisematyöluvat.
-  06.05.09  Teiden suoja- ja näkemäalueelle rakentaminen (poikkeamisluvat).
-  06.05.10 Teiden suoja- ja näkemäalueen ulkopuolelle rakentaminen (naapurin kuuleminen)"
+  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusj\u00e4rjestysp\u00e4\u00e4t\u00f6kset; rakennus-, toimenpide-, purkamis- ja maisematy\u00f6luvat.
+  06.05.09  Teiden suoja- ja n\u00e4kem\u00e4alueelle rakentaminen (poikkeamisluvat).
+  06.05.10 Teiden suoja- ja n\u00e4kem\u00e4alueen ulkopuolelle rakentaminen (naapurin kuuleminen)"
   ["Lausuntopyynt\u00f6 poikkeamishakemuksesta"
    "Lausuntopyynt\u00f6 maisematy\u00f6luvasta"
    "Lausuntopyynt\u00f6 purkamisaikomuksesta"
@@ -36,11 +36,11 @@
 
 (def ymp-statement-types
   "ELY statement types for YM, YI, YL, VVVL, MAL.
-  07.00.06 Ympäristönsuojelulain valvontamenettely
+  07.00.06 Ymp\u00e4rist\u00f6nsuojelulain valvontamenettely
   07.00.13 Pohjavesien suojelu
-  07.00.32 Maa-ainesten otto ja käsittely
-  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusjärjestyspäätökset; rakennus-, toimenpide-, purkamis- ja maisematyöluvat."
-  ["Lausuntopyynt\u00f6 maisematy\u00f6luvasta"             ; from 07.01.05 Poikkeamisluvat... maisematyöluvat -->
+  07.00.32 Maa-ainesten otto ja k\u00e4sittely
+  07.01.05 Poikkeamisluvat tai poikkeamis-, suunnittelutarveratkaisu- ja rakennusj\u00e4rjestysp\u00e4\u00e4t\u00f6kset; rakennus-, toimenpide-, purkamis- ja maisematy\u00f6luvat."
+  ["Lausuntopyynt\u00f6 maisematy\u00f6luvasta"             ; from 07.01.05 Poikkeamisluvat... maisematy\u00f6luvat -->
    "Lausuntopyynt\u00f6 ymp\u00e4rist\u00f6nsuojelulain valvontamenettelyst\u00e4"
    "Lausuntopyynt\u00f6 pohjavesien suojelusuunnitelmasta"
    "Lausuntopyynt\u00f6 maa-ainesten otosta ja k\u00e4sittelyst\u00e4"])
