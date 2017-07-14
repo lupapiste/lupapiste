2b5b5fcb2dcc09c88ab8872e235e5672fd581074
Markus Rauhala <markus.rauhala@evolta.fi>
2017-07-14 15:08:40 +0300

    Authority admin stamp preview css

--- src-cljs/lupapalvelu/ui/auth_admin/stamp/editor.cljs
+++ src-cljs/lupapalvelu/ui/auth_admin/stamp/editor.cljs
@@ -92,8 +92,8 @@
   (let [stamps (rum/cursor component-state :stamps)
         editor-state (rum/cursor component-state :editor)
         selected-stamp-id (rum/cursor component-state :selected-stamp-id)]
-    [:div.container
-     [:h1 (loc "stamp-editor.tab.title")]
+    [:div
+     [:h2 (loc "stamp-editor.tab.title")]
      [:div.stamp-selection
       (stamp-select stamps selected-stamp-id)
       (new-stamp-button selected-stamp-id editor-state)]
