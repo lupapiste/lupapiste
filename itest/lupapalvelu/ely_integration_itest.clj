abb0cfd8a5f60dd939ddd75cbbcc319d73b6706e
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-07-18 16:42:14 +0300

    add itest for XML generation

--- itest/lupapalvelu/ely_integration_itest.clj
+++ itest/lupapalvelu/ely_integration_itest.clj
@@ -1,18 +1,48 @@
 (ns lupapalvelu.ely-integration-itest
   (:require [midje.sweet :refer :all]
+            [clojure.data.xml :as xml]
+            [clojure.java.io :as io]
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.integrations.ely :as ely]
-            [lupapalvelu.domain :as domain]))
+            [lupapalvelu.xml.asianhallinta.core :as ah]
+            [lupapalvelu.xml.validator :as validator]
+            [sade.core :refer [now]]
+            [sade.env :as env]
+            [sade.xml :as sxml]))
 
 (apply-remote-minimal)
 
 (facts "ELY statement-request"
   (let [app (create-and-submit-application mikko :propertyId sipoo-property-id :operation "poikkeamis")
         statement-subtype "Lausuntopyynt\u00f6 poikkeamishakemuksesta"]
-    (generate-documents app mikko)
+    (generate-documents app mikko)                          ; generate documents so generated XML is valid
 
     (fact "request XML is created"
       (command sonja :ely-statement-request :id (:id app) :subtype statement-subtype :saateText "moro") => ok?
+      (let [output-dir (str (:output-dir ah/ely-config) "/")
+            target-file-name (str "target/Downloaded-" (:id app) "-" (now) "_ely_statement_request.xml")
+            filename-starts-with (:id app)
+            xml-file (if get-files-from-sftp-server?
+                       (io/file (get-file-from-server
+                                  (env/value :ely :sftp-user)
+                                  (subs (env/value :fileserver-address) 7)
+                                  filename-starts-with
+                                  target-file-name
+                                  (str ah/ah-from-dir "/")))
+                       (io/file (get-local-filename output-dir filename-starts-with)))
+            xml-as-string (slurp xml-file)
+            xml (xml/parse (io/reader xml-file))]
+        (fact "file exists" (.exists xml-file) => true)
+        (fact "XML file is valid"
+          (validator/validate xml-as-string (:permitType app) "ah-1.3"))
+        (fact "Application IDs match"
+          (sxml/get-text xml [:UusiAsia :HakemusTunnus]) => (:id app))
+        (fact "Tyyppi is correct"
+          (sxml/get-text xml [:UusiAsia :Tyyppi]) => "Lausuntopyynt\u00f6")
+        (fact "TyypinTarkenne is correct"
+          (sxml/get-text xml [:UusiAsia :TyypinTarkenne]) => statement-subtype)))
+
+    (fact "ELY statement is saved to application"
       (let [statements (:statements (query-application mikko (:id app)))
             ely-statement (first statements)]
         (count statements) => 1
