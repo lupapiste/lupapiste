0131fee2e6da4edaead389fc8227d22be673f812
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-08-09 14:33:43 +0300

    LPK-3002 attachments with no files filtered out

--- src/lupapalvelu/printing_order/printing_order_api.clj
+++ src/lupapalvelu/printing_order/printing_order_api.clj
@@ -4,7 +4,8 @@
             [lupapalvelu.roles :as roles]
             [lupapalvelu.attachment :as att]
             [lupapalvelu.states :as states]
-            [lupapalvelu.attachment.tag-groups :as att-tag-groups]))
+            [lupapalvelu.attachment.tag-groups :as att-tag-groups]
+            [sade.util :as util]))
 
 (defquery attachments-for-printing-order
   {:parameters       [id]
@@ -13,6 +14,7 @@
    :user-authz-roles roles/all-authz-roles
    :org-authz-roles  roles/reader-org-authz-roles}
   [{application :application :as command}]
-  (ok :attachments (map (partial att/enrich-attachment-with-trigger-tags nil)
-                        (att/sorted-attachments command))
+  (ok :attachments (->> (att/sorted-attachments command)
+                        (map att/enrich-attachment)
+                        (filter (fn [att] (util/contains-value? (:tags att) :hasFile))))
       :tagGroups (att-tag-groups/attachment-tag-groups application)))
