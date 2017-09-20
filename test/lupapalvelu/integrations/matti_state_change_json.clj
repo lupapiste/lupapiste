0421666a0166f5611b4c1c958153cad7875df6cf
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-09-20 14:47:38 +0300

    add test case for new building

--- test/lupapalvelu/integrations/matti_state_change_json.clj
+++ test/lupapalvelu/integrations/matti_state_change_json.clj
@@ -6,34 +6,89 @@
             [sade.schemas :as ssc]
             [sade.schema-generators :as ssg]))
 
-(def test-doc {:schema-info {:name "rakennuksen-muuttaminen"
-                             :op {:id "57603a99edf02d7047774554"}}
-               :data {:buildingId {:value "100840657D"
+(def old-building-test-doc
+  {:schema-info {:name "rakennuksen-muuttaminen"
+                 :op {:id "57603a99edf02d7047774554"}}
+   :data {:buildingId {:value "100840657D"
+                       :sourceValue "100840657D"}
+          :valtakunnallinenNumero {:value "100840657D"
+                                   :modified 1491470000000
+                                   :source "krysp"
                                    :sourceValue "100840657D"}
-                      :valtakunnallinenNumero {:value "100840657D"
-                                               :modified 1491470000000
-                                               :source "krysp"
-                                               :sourceValue "100840657D"}
-                      :tunnus {:value "100840657D"}
-                      :id "1321321"}})
+          :tunnus {:value "100840657D"}
+          :id "1321321"}
+   :id "foo-old"})
+
+#_(def old-building-manuaalinen-rakennusnumero                ; VTJ-PRT is not known and user has inpute buildingId manually
+  {:schema-info {:name "rakennuksen-muuttaminen"
+                 :op {:id "57603a99edf02d7047774554"}}
+   :data {:buildingId {:value "100840657D"
+                       :sourceValue "100840657D"}
+          :valtakunnallinenNumero {:value "100840657D"
+                                   :modified 1491470000000
+                                   :source "krysp"
+                                   :sourceValue "100840657D"}
+          :tunnus {:value "100840657D"}
+          :id "1321321"}
+   :id "foo-old"})
+
+(def new-building-data
+  {:id "foo-new"
+   :schema-info {:op {:id "57603a99edf02d7047774554"
+                      :name "kerrostalo-rivitalo",
+                      :description nil,
+                      :created 1505822832171}
+                 :name "uusiRakennus"}
+   :data {:rakennuksenOmistajat {:0 {}}
+          :varusteet {}
+          :verkostoliittymat {}
+          :huoneistot {:0 {}}
+          :tunnus {:value "Tunnus"}
+          :mitat {}
+          :valtakunnallinenNumero {:value "123"}}})
+
+(defn app-with-docs [documents]
+  (let [location [444444.0 6666666.0]
+        wgs (coord/convert "EPSG:3067" "WGS84" 5 location)]
+    {:id               (ssg/generate ssc/ApplicationId)
+     :propertyId       "29703401070010"
+     :infoRequest      false :permitType "R"
+     :primaryOperation {:name "pientalo-laaj" :id "57603a99edf02d7047774554"}
+     :address          "address"
+     :municipality     "123"
+     :applicant        "Tero Testaaja"
+     :location         location
+     :location-wgs84   wgs
+     :documents        documents}))
 
 (facts "state-changes for R app"
-  (facts "existing buildings"
-    (let [location [444444.0 6666666.0]
-          wgs (coord/convert "EPSG:3067" "WGS84" 5 location)
-          app {:id               (ssg/generate ssc/ApplicationId)
-               :propertyId       "29703401070010"
-               :infoRequest      false :permitType "R"
-               :primaryOperation {:name "pientalo-laaj" :id (ssg/generate ssc/ObjectIdStr)}
-               :address          "address"
-               :municipality     "123"
-               :applicant        "Tero Testaaja"
-               :location         location
-               :location-wgs84   wgs
-               :documents        [test-doc]}
-          test-fn (fn [app]
-                    (when-let [new-state (sm/next-state app)]
-                      (fact {:midje/description (str "data is valid for - " (name new-state))}
-                        (mjson/state-change-data app new-state) => map?)
-                      (recur (assoc app :state (name new-state)))))]
-      (test-fn (assoc app :state (name (first (sm/application-state-seq app))))))))
+  (letfn
+    [(valid-fn
+       [data]
+       (fact {:midje/description (str "data is valid for - " (:state data))}
+         data => map?))
+     (existing-building-test
+       [data]
+       (fact "has buildingId and new:false"
+         (-> data :operations first :building) => (just [[:new false]
+                                                         [:buildingId
+                                                          (get-in old-building-test-doc
+                                                                  [:data :valtakunnallinenNumero :value])]])))
+     (new-building-test
+       [data]
+       (fact "building has new:true"
+         (-> data :operations first :building) => (just [[:new true]])))
+     (run-test
+       [app test-fn]
+       (when-let [new-state (sm/next-state app)]
+         (let [state-change-data (mjson/state-change-data app new-state)]
+           (valid-fn state-change-data)
+           (test-fn state-change-data))
+         (recur (assoc app :state (name new-state)) test-fn)))]
+
+    (facts "existing buildings"
+      (let [app (app-with-docs [old-building-test-doc])]
+        (run-test (assoc app :state (name (first (sm/application-state-seq app)))) existing-building-test)))
+    (facts "new buildings"
+      (let [app (app-with-docs [new-building-data])]
+        (run-test (assoc app :state (name (first (sm/application-state-seq app)))) new-building-test )))))
