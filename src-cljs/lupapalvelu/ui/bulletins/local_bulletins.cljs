632ecce49cc0521fc08470908827a0b6b6fa7c04
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-09-29 15:54:55 +0300

    styling

--- src-cljs/lupapalvelu/ui/bulletins/local_bulletins.cljs
+++ src-cljs/lupapalvelu/ui/bulletins/local_bulletins.cljs
@@ -5,10 +5,29 @@
 
 (rum/defc local-bulletins < rum/reactive
   [_]
-  [:div.full.content.orange-bg
-   [:div.content-center
-    [:h1.slogan "Kunnan julkipanolista"]
-    [:p.ingress.intro "Kunnan julkipanolista"]]])
+  [:div
+   [:div.full.content.orange-bg
+    [:div.content-center
+     [:h1.slogan.municipal-heading "Kunnan julkipanolista"]
+     [:h2.slogan.municipal-heading "Kunnan julkipanolista"]]]
+   [:div.full.content
+    [:div.content-center.municipal-caption
+     [:p "Kunnan rakennuslupapäätökset annetaan julkipanon jälkeen, jolloin niiden katsotaan tulleen asianosaisten tietoon. Oikaisuvaatimusaika on 30 päivää."]
+     [:p "Kunnan rakennuslupapäätökset annetaan julkipanon jälkeen, jolloin niiden katsotaan tulleen asianosaisten tietoon."]
+     [:p "Kunnan rakennuslupapäätökset annetaan julkipanon jälkeen, jolloin niiden katsotaan tulleen asianosaisten tietoon."]]]
+   [:div.full.content
+    [:div.content-center
+     [:table.application-bulletins-list
+      [:thead
+       [:tr
+        [:th "Pykälä (§)"]
+        [:th "Lupatunnus"]
+        [:th "Rakennuspaikka"]
+        [:th "Asia/Toimenpide"]
+        [:th "Päättäjä"]
+        [:th "Päätöksen antopäivä"]
+        [:th "Viimeinen oikaisuvaatimuspäivä"]]
+       [:tbody]]]]]])
 
 (defn mount-component []
   (rum/mount (local-bulletins)
