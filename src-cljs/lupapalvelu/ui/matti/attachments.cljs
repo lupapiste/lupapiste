cdbfc9b15cc8b8d2f4e7bba3b2ab6fdd2d945bef
Vespe Savikko <vespe.savikko@evolta.fi>
2017-10-09 19:02:19 +0300

    LPK-3292 Enable/disable support for FileuploadService.
    Enable/disable for attachments batch table.

--- src-cljs/lupapalvelu/ui/matti/attachments.cljs
+++ src-cljs/lupapalvelu/ui/matti/attachments.cljs
@@ -1,9 +1,11 @@
 (ns lupapalvelu.ui.matti.attachments
   "Attachments related components and utilities for Matti."
-  (:require [lupapalvelu.ui.attachment.components :as att]
+  (:require [clojure.string :as s]
+            [lupapalvelu.ui.attachment.components :as att]
             [lupapalvelu.ui.attachment.file-upload :as upload]
             [lupapalvelu.ui.common :as common]
             [lupapalvelu.ui.components :as components]
+            [lupapalvelu.ui.hub :as hub]
             [lupapalvelu.ui.matti.path :as path]
             [lupapalvelu.ui.matti.service :as service]
             [rum.core :as rum]
@@ -56,7 +58,8 @@
 
 (rum/defcs type-selector < rum/reactive
   (rum/local nil ::types)
-  [{types* ::types} {:keys [schema files* fields*] :as options} filedata]
+  [{types* ::types} {:keys [schema files* fields* binding?*]
+                     :as   options} filedata]
   (attachment-types types*)
   (let [att-types (some->> (rum/react types*)
                            (filter (fn [{:keys [type-group]}]
@@ -68,25 +71,27 @@
                                    :text  (type-loc type-group type-id)
                                    :value (kw-type type)})))]
     (when (seq att-types)
-      (let [filename (keyword (:filename filedata))
+      (let [filename       (keyword (:filename filedata))
             {value :value} (util/find-by-key :value
                                              (:default schema)
                                              att-types)
-            set-type-fn (fn [type]
-                          (set-field fields* filedata :type type)
-                          (set-field fields* filedata :contents nil))]
+            set-type-fn    (fn [type]
+                             (set-field fields* filedata :type type)
+                             (set-field fields* filedata :contents nil))]
         (when (and value
                    (-> (field-info fields* filedata) :type nil?))
           (set-field fields* filedata :type value))
         (components/autocomplete value
                                  {:items     att-types
                                   :callback  set-type-fn
-                                  :disabled? (not= (:state filedata)
-                                                   :success)})))))
+                                  :disabled? (or (rum/react binding?*)
+                                                 (not= (:state filedata)
+                                                       :success))})))))
 
 (rum/defcs contents-editor < rum/reactive
   (rum/local [] ::types)
-  [{types*  ::types} {fields* :fields*} {filename :filename :as filedata}]
+  [{types*  ::types} {:keys [fields* binding?*]} {filename :filename
+                                                  :as filedata}]
   (attachment-types types*)
   (let [{:keys [type contents]} (field-info fields* filedata)
         att-types               (rum/react types*)
@@ -111,8 +116,9 @@
                              {:items     items
                               :callback  set-fn
                               :required? true
-                              :disabled? (not= (:state filedata)
-                                               :success)})))))
+                              :disabled? (or (rum/react binding?*)
+                                             (not= (:state filedata)
+                                                   :success))})))))
 
 (defn- td-error [msg]
   [:td.batch--error {:colSpan 2} msg])
@@ -121,67 +127,112 @@
   [:td.batch--progress {:colSpan 2}
    [:div [:span {:style {:width (str percentage "%")}} " "]]])
 
-(defn- remove-button [{:keys [files* fields*]} {:keys [filename file-id]}]
-  [:i.lupicon-remove.primary
-   {:on-click (fn []
-                (when file-id
-                  (service/delete-file file-id))
-                (swap! files*
-                       (partial remove #(util/=as-kw filename (:filename %))))
-                (swap! fields*
-                       dissoc (keyword filename)))}])
+(defn- remove-file [{:keys [files* fields*]} {:keys [filename file-id]}]
+  (when file-id
+    (service/delete-file file-id))
+  (swap! files*
+         (partial remove #(util/=as-kw filename (:filename %))))
+  (swap! fields*
+         dissoc (keyword filename)))
+
+(rum/defc batch-buttons < rum/reactive
+  [{:keys [schema files* fields* binding?*] :as options}]
+  (let [binding? (rum/react binding?*)]
+    [:tfoot.batch--buttons
+     [:tr
+      [:td {:colSpan 4}
+       [:button.primary.outline
+        {:on-click #(doseq [filedata @files*]
+                      (remove-file options filedata))
+         :disabled binding?}
+        (path/loc :cancel)]
+       [:button.positive
+        ;;
+        {:disabled (or binding?
+                       (some (util/fn-> :contents s/blank?)
+                             (vals (rum/react fields*)))
+                       (not-every? (util/fn-> :state #{:bad :success :failed})
+                                   (rum/react files*)))
+         :on-click #(reset! binding?* true)}
+        (if binding?
+          [:i.lupicon-refresh.icon-spin]
+          [:i.lupicon-check])
+        [:span (path/loc :attachment.batch-ready)]]]]]))
 
 (rum/defc attachments-batch < rum/reactive
   "Metadata editor for file upload. The name is a hat-tip to the
   AttachmentBatchModel."
-  [{:keys [schema files* fields*] :as options}]
+  [{:keys [schema files* fields* binding?*] :as options}]
   (when (-> files* rum/react seq)
-    [:div
-     [:table.attachment-batch-table
-      [:thead
-       [:tr
-        [:th [:span (path/loc :attachment.file)]]
-        [:th [:span.batch-required (path/loc :application.attachmentType)]]
-        [:th [:span.batch-required (path/loc :application.attachmentContents)]]
-        [:th.td-center (path/loc :remove)]]]
-      [:tbody
-       (for [{:keys [filename state] :as filedata} @files*]
+    (let [binding? (rum/react binding?*)]
+      [:div
+       [:table.attachment-batch-table
+        [:thead
          [:tr
-          [:td.batch--file (fileinfo-link filedata)]
-          (case state
-            :bad (td-error (:message filedata))
-            :failed (td-error (path/loc :file.upload.failed))
-            :progress (td-progress (:progress filedata))
-            [
-             [:td.batch--type
-              {:key (path/unique-id "batch-type")}
-              (type-selector options filedata)]
-             [:td.batch--contents
-              {:key (path/unique-id "batch-contents")}
-              (contents-editor options filedata)]])
-          [:td.td-center (remove-button options filedata)]
-          ])]]]))
+          [:th [:span (path/loc :attachment.file)]]
+          [:th [:span.batch-required (path/loc :application.attachmentType)]]
+          [:th [:span.batch-required (path/loc :application.attachmentContents)]]
+          [:th.td-center (path/loc :remove)]]]
+        [:tbody
+         (for [{:keys [filename state] :as filedata} @files*]
+           [:tr
+            [:td.batch--file (fileinfo-link filedata)]
+            (case state
+              :bad (td-error (:message filedata))
+              :failed (td-error (path/loc :file.upload.failed))
+              :progress (td-progress (:progress filedata))
+              [
+               [:td.batch--type
+                {:key (path/unique-id "batch-type")}
+                (type-selector options filedata)]
+               [:td.batch--contents
+                {:key (path/unique-id "batch-contents")}
+                (contents-editor options filedata)]])
+            [:td.td-center
+             (when-not binding?
+               [:i.lupicon-remove.primary
+                {:on-click #(remove-file options filedata)}])]])]
+        (batch-buttons options)]
+       ])))
 
-(rum/defc matti-attachments < rum/reactive
-  "Displays and supports adding new attachments."
+(rum/defc add-file-label < rum/reactive
+  "Add file label button as a separate component for binding?* atom's
+  benefit."
+  [binding?* input-id]
+  (let [binding? (rum/react binding?*)]
+    (hub/send "fileuploadService::toggle-enabled"
+              {:input   input-id
+               :enabled (not binding?)})
+    [:label.btn.positive.batch--add-file
+     {:for   input-id
+      :class (common/css-flags :disabled binding?)}
+     [:i.lupicon-circle-plus]
+     [:span (path/loc :attachment.addFile)]]))
+
+
+
+(rum/defc matti-attachments
+  "Displays and supports adding new attachments. This cannot be
+  reactive since we want the input-id to remain somewhat constant."
   [{:keys [schema path state] :as options}]
-  (let [files*  (atom [])
-        fields* (atom {})]
+  (let [files*    (atom [])
+        fields*   (atom {})
+        binding?* (atom false)]
     [:div
-     (when (path/enabled? options)
+     (when path/enabled?
        (att/upload-wrapper {:callback  (upload/file-monitors files*)
                             :dropzone  (:dropzone schema)
                             :multiple? (:multiple? schema)
                             :component (fn [{:keys [input input-id]}]
                                          [:div
                                           input
-                                          [:label.btn.positive {:for input-id }
-                                           [:i.lupicon-circle-plus]
-                                           [:span (path/loc :attachment.addFile)]]])}))
+                                          (add-file-label binding?*
+                                                          input-id)])}))
      (attachments-batch (assoc options
-                               :files*  files*
-                               :fields* fields*))
+                              :files*  files*
+                              :fields* fields*
+                              :binding?* binding?*))
 
 
-     (components/debug-atom files* "Files")
-     (components/debug-atom fields* "Fields")]))
+    (components/debug-atom files* "Files")
+    (components/debug-atom fields* "Fields")]))
