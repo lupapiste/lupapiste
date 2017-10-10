673b02488a7c745ed2d2500ee2db8d5231cab8b6
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-10-10 09:54:27 +0300

    itests

--- itest/lupapalvelu/matti_verdict_bulletins_itest.clj
+++ itest/lupapalvelu/matti_verdict_bulletins_itest.clj
@@ -8,13 +8,33 @@
 
 (apply-remote-minimal)
 
-(facts ""
+(facts "verdict bulletins for matti-style verdicts"
   (let [{id :id} (init-verdict-template sipoo "r") =not=> nil?]
-    (set-template-draft-values id :bulletin-op-description "Kerrostalolle lupa")
-    (publish-verdict-template sipoo id) => ok?
-    (let [{app-id :id} (create-and-submit-application sonja :operation "kerrostalo-rivitalo"
-                                                      :propertyId sipoo-property-id
-                                                      :x 406898.625 :y 6684125.375
-                                                      :address "Hitantine 108")
-          {verdict :verdict} (command sonja :new-matti-verdict-draft :id app-id :template-id id) => ok?]
-      (-> verdict :data) => (contains {:bulletin-op-description "Kerrostalolle lupa"}))))
+    (fact "publish verdict template"
+      (set-template-draft-values id :bulletin-op-description "Kerrostalolle lupa")
+      (publish-verdict-template sipoo id) => ok?)
+    (fact "create a verdict from the template"
+      (let [{app-id :id} (create-and-submit-application sonja :operation "kerrostalo-rivitalo"
+                                                        :propertyId sipoo-property-id
+                                                        :x 406898.625 :y 6684125.375
+                                                        :address "Hitantine 108")
+            {verdict :verdict} (command sonja :new-matti-verdict-draft :id app-id :template-id id) => ok?]
+        (-> verdict :data) => (contains {:bulletin-op-description "Kerrostalolle lupa"})
+        (fact "bulletin appears in search"
+          (command sonja :upsert-matti-verdict-bulletin :id app-id :verdict-id (:id verdict)) => ok?
+          (let [{data :data ok :ok} (datatables pena :application-bulletins :page 1 :searchText ""
+                                                     :municipality nil :state nil :sort nil)]
+               ok => true
+               (count data) => 1))
+        (fact "create a second verdict"
+          (let [{verdict2 :verdict} (command sonja :new-matti-verdict-draft :id app-id :template-id id)
+                _ (command sonja :edit-matti-verdict :id app-id :verdict-id (:id verdict2)
+                                                     :path [:bulletin-op-description] :value "Toimenpidelupa kaivolle kans")]
+            (fact "upsert second bulletin"
+              (command sonja :upsert-matti-verdict-bulletin :id app-id :verdict-id (:id verdict2)) => ok?
+              (let [{data :data ok :ok} (datatables pena :application-bulletins :page 1 :searchText ""
+                                                         :municipality nil :state nil :sort nil)]
+                ok => true
+                (fact "both verdicts appear in bulletins search as separate items"
+                  (count data) => 2
+                  (map :id data) => [(str app-id "_" (:id verdict)) (str app-id "_" (:id verdict2))])))))))))
