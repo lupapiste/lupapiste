ba0df3e377c0d76142a858fad6aa271630f766bc
Markus Penttila <markus.penttila@evolta.fi>
2017-10-18 12:41:35 +0300

    Midje the right way

--- test/lupapalvelu/fixture/minimal_test.clj
+++ test/lupapalvelu/fixture/minimal_test.clj
@@ -1,23 +1,23 @@
 (ns lupapalvelu.fixture.minimal-test
   (:require [midje.sweet :refer :all]
-            [midje.emission.plugins.util :as emit]
             [schema.core :as sc]
             [lupapalvelu.fixture.minimal :as fm]
             [lupapalvelu.user :as usr]
             [lupapalvelu.organization :as org]
             [lupapalvelu.company :as company]))
 
-(fact "users in minimal fixture must match user schema"
+(facts "users in minimal fixture must match user schema"
   (doseq [u fm/users]
-    (emit/emit-one-line (str "Checking user id: " (:id u)))
-    (sc/check usr/User u) => nil))
+    (fact {:midje/description (str "id: " (:id u))}
+      (sc/check usr/User u) => nil)))
 
-(fact "organizations in minimal fixture must match organization schema"
+(facts "organizations in minimal fixture must match organization schema"
   (doseq [o fm/organizations]
-    (emit/emit-one-line (str "Checking organization id: " (:id o)))
-    (sc/check org/Organization o) => nil))
+    (fact {:midje/description (str "id: " (:id o))}
+      (sc/check org/Organization o) => nil)))
 
-(fact "companies in minimal fixture must match company schema"
+(facts "companies in minimal fixture must match company schema"
   (doseq [co fm/companies]
-    (emit/emit-one-line (str "Checking company id: " (:id co)))
-    (sc/check company/Company co) => nil))
+    (fact {:midje/description (str "id: " (:id co))}
+      (sc/check company/Company co) => nil)))
+
