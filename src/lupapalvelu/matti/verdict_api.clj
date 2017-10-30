c7cca2a709c83dfdff115cb34567db4a4d5c38b6
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-10-30 15:49:09 +0200

    remove matti verdict bulletins code for now

--- src/lupapalvelu/matti/verdict_api.clj
+++ src/lupapalvelu/matti/verdict_api.clj
@@ -156,37 +156,3 @@ TODO: create tasks and PDF, application state change, attachments locking."
 
 (defn- get-search-fields [fields app]
   (into {} (map #(hash-map % (% app)) fields)))
-
-(sc/defn ^:private create-bulletin [application created verdict-id & [updates]] :- bulletins/ApplicationBulletin
-  (let [verdict (util/find-by-id verdict-id (:matti-verdicts application))
-        app-snapshot (-> (bulletins/create-bulletin-snapshot application)
-                         (dissoc :verdicts :matti-verdicts)
-                         (merge
-                           updates
-                           {:application-id (:id application)
-                            :matti-verdict verdict
-                            :bulletinOpDescription (-> verdict :data :bulletinOpDescription)}))
-        search-fields [:municipality :address :matti-verdict :_applicantIndex
-                       :application-id
-                       :bulletinState :applicant :organization :bulletinOpDescription]
-        search-updates (get-search-fields search-fields app-snapshot)]
-    (bulletins/snapshot-updates app-snapshot search-updates created)))
-
-(defcommand upsert-matti-verdict-bulletin
-  {:description      ""
-   :feature          :matti
-   :user-roles       #{:authority}
-   :parameters       [id verdict-id]
-   :input-validators [(partial action/non-blank-parameters [:id :verdict-id])]
-   :pre-checks       [(verdict-exists :editable?)]
-   :states           states/give-verdict-states}
-  [{application :application created :created}]
-  (let [today-long (tc/to-long (t/today-at-midnight))
-        updates (create-bulletin application created verdict-id
-                                 {:bulletinState :verdictGiven
-                                  :verdictGivenAt       today-long
-                                  :appealPeriodStartsAt today-long
-                                  :appealPeriodEndsAt   (tc/to-long (t/plus (t/today-at-midnight) (t/days 14)))  ;; TODO!!!
-                                  :verdictGivenText ""})]
-    (bulletins/upsert-bulletin-by-id (str id "_" verdict-id) updates)
-    (ok)))
