731447307d331ec02af046899c6ca252ed5aeb79
Jouni Honkala <Jouni.Honkala@solita.fi>
2017-11-02 16:27:37 +0200

    command sequence order in create-and-send-application fixed

--- test-utils/lupapalvelu/itest_util.clj
+++ test-utils/lupapalvelu/itest_util.clj
@@ -506,8 +506,8 @@
   "Returns the application map"
   [apikey & args]
   (let [id    (apply create-app-id apikey args)
-        _     (command apikey :update-app-bulletin-op-description :id id :description "otsikko julkipanoon")
         _     (command apikey :submit-application :id id)
+        _     (command apikey :update-app-bulletin-op-description :id id :description "otsikko julkipanoon")
         resp  (command apikey :approve-application :id id :lang "fi")]
     (fact "Submit OK" resp => ok?)
     (query-application apikey id)))
