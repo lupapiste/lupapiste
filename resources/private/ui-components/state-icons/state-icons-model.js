34a57875feec3a1745a8a09245e10f53c601e651
Antti Salmivaara <antti.salmivaara@evolta.fi>
2017-11-24 11:50:42 +0200

    add bubble hovers to attachments page state icons

--- resources/private/ui-components/state-icons/state-icons-model.js
+++ resources/private/ui-components/state-icons/state-icons-model.js
@@ -70,48 +70,111 @@ LUPAPISTE.StateIconsModel = function( params ) {
     return util.getIn(attachment, ["metadata", "tila"]) === "arkistoitu";
   }
 
-  function signer(attachment) {
+  function approverInfo(attachment) {
+
+      var approverInfo = "";
+      var state = util.getIn(attachment, ["approvals", attachment.latestVersion.originalFileId, "state"]);
+
+      if (state === "ok") {
+
+          var getApproval = function (attachment) {
+              var latestApprovedMajorVersions = attachment.versions.length === 1
+                  ? [attachment.latestVersion]
+                  : _(attachment.versions)
+                      .filter(function (v) {
+                          return v.version.major === attachment.latestVersion.version.major
+                      });
+              var firstApprovedVersionId = latestApprovedMajorVersions.length === 1
+                  ? latestApprovedMajorVersions[0].originalFileId
+                  : _(latestApprovedMajorVersions)
+                      .filter(function (v) {
+                          return v.version.minor === 0;
+                      })
+                      .first()
+                      .originalFileId;
+              return util.getIn(attachment, ["approvals", firstApprovedVersionId])
+          };
+
+          var approval = getApproval(attachment);
+          if (approval) {
+              approverInfo = sprintf("%s %s:\n %s %s",
+                                     loc(["document.approved"]),
+                                     moment(approval.timestamp).format("D.M.YYYY HH:mm"),
+                                     approval.user.firstName,
+                                     approval.user.lastName);
+          }
+      } else if (state === "requires_authority_action") {
+
+      }
+
+      return approverInfo;
+  }
+
+  function signerInfo(attachment) {
     return _(attachment .signatures)
            .filter(function( s ) {
              return versionString( s ) === versionString( attachment.latestVersion );
            })
            .map( function( s ) {
-             return sprintf( "%s %s", s.user.firstName, s.user.lastName);
+             return sprintf( "%s %s:\n %s %s",
+                             loc(["attachment.signed"]),
+                             moment(s.created).format( "D.M.YYYY HH:mm"),
+                             s.user.firstName,
+                             s.user.lastName);
            })
            .join( "\n");
+  }
+
+  function stamperInfo(attachment) {
+      var user = attachment.latestVersion.user;
+      return sprintf( "%s %s:\n %s %s",
+          loc(["stamp.comment"]),
+          moment(attachment.latestVersion.created).format( "D.M.YYYY HH:mm" ),
+          user.firstName,
+          user.lastName);
   }
 
   self.stateIcons = function() {
     return _( [[approved,                {css: "lupicon-circle-check positive",
                                           icon: "approved",
-                                          title: ""}],
+                                          title: "",
+                                          info: approverInfo(attachment)}],
                [needsAttention,          {css: "lupicon-circle-attention negative",
                                           icon: "rejected",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [signed,                  {css: "lupicon-circle-pen positive",
                                           icon: "signed",
-                                          title: signer(attachment)}],
+                                          title: "",
+                                          info: signerInfo(attachment)}],
                [requiresAuthorityAction, {css: "lupicon-circle-star primary",
                                           icon: "state",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [stamped,                 {css: "lupicon-circle-stamp positive",
                                           icon: "stamped",
-                                          title: ""}],
+                                          title: "",
+                                          info: stamperInfo(attachment)}],
                [sent,                    {css: "lupicon-circle-arrow-up positive",
                                           icon: "sent",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [sentToCaseManagement,    {css: "lupicon-circle-arrow-up positive",
                                           icon: "sent-to-case-management",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [forPrinting,             {css: "lupicon-circle-section-sign positive",
                                           icon: "for-printing",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [notPublic,               {css: "lupicon-lock primary",
                                           icon: "not-public",
-                                          title: ""}],
+                                          title: "",
+                                          info: ""}],
                [archived,                {css: "lupicon-archives positive",
                                           icon: "archived",
-                                          title: loc("arkistoitu")}]] )
+                                          title: "",
+                                          info: loc("arkistoitu")}]])
       .filter(function(icon) { return _.first(icon)(attachment); })
       .map(_.last)
       .value();
