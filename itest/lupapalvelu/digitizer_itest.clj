6ea51860a717643a14e69d7021c8f0ef74a4460b
Antti Slunga <Antti.Slunga@solita.fi>
2017-11-29 10:22:15 +0200

    Digitizer iTest fix

--- itest/lupapalvelu/digitizer_itest.clj
+++ itest/lupapalvelu/digitizer_itest.clj
@@ -3,18 +3,16 @@
             [lupapalvelu.itest-util :refer :all]
             [lupapalvelu.mongo :as mongo]
             [lupapalvelu.fixture.core :as fixture]
-            [midje.sweet :refer :all]
-            [sade.core :refer [def- now]]))
+            [midje.sweet :refer :all]))
 
 (apply-remote-minimal)
 
 (facts "Create digitizer project with default location"
-  (mongo/with-db test-db-name
 
     (fact "Admin can set default location for organization"
       (command jarvenpaa :set-default-digitalization-location :x "404262.00" :y "6694511.00") => ok?
 
-      (let [organization (mongo/by-id :organizations "186-R")]
+      (let [organization (first (:organizations (query admin :organizations)))]
         (get-in organization [:default-digitalization-location :x]) => "404262.00"
         (get-in organization [:default-digitalization-location :y]) => "6694511.00"))
 
@@ -30,13 +28,13 @@
                                       :createWithoutBuildings true
                                       :createWithDefaultLocation true)
           app-id (:id response)
-          application (mongo/by-id :applications app-id)]
+          application (query-application digitoija app-id)]
 
       (fact "Digitizer can create project without location"
         response => ok?
         (:address application) => "Sijainti puuttuu"
-        (first (:location application)) => 404262.00
-        (second (:location application)) => 6694511.00)
+        (:x (:location application)) => 404262.00
+        (:y (:location application)) => 6694511.00)
 
       (fact "Digitizer can change location"
         (command digitoija :change-location
@@ -45,13 +43,13 @@
                            :y 6664511.00
                            :address "New address"
                            :propertyId "18677128000000") => ok?
-        (let [application (mongo/by-id :applications app-id)]
+        (let [application (query-application digitoija app-id)]
           (:address application) => "New address"
-          (first (:location application)) => 400062.00
-          (second (:location application)) => 6664511.00))
+          (:x (:location application)) => 400062.00
+          (:y (:location application)) => 6664511.00))
 
       (fact "Operations, buildings and documents are changed correctly")
-        (let [application (mongo/by-id :applications app-id)
+        (let [application (query-application digitoija app-id)
               primary-operation-id (get-in application [:primaryOperation :id])
               secondary-operation-id (:id (first (:secondaryOperations application)))
               first-building-id (:operationId (first (:buildings application)))
@@ -60,4 +58,4 @@
               first-building-doc-op-id (get-in (first documents) [:schema-info :op :id])
               second-building-doc-op-id (get-in (second documents) [:schema-info :op :id])]
           (= primary-operation-id first-building-id first-building-doc-op-id) => true?
-          (= secondary-operation-id second-building-id second-building-doc-op-id) => true?))))
+          (= secondary-operation-id second-building-id second-building-doc-op-id) => true?)))
