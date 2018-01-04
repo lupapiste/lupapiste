6eb17fc5c60c8eae09906d92e4980660e4b5a77b
Antti Slunga <Antti.Slunga@solita.fi>
2018-01-04 12:17:10 +0200

    LPK-3474 Possibility to edit VTJ-PRT in archiving projects

--- src/lupapalvelu/document/schemas.clj
+++ src/lupapalvelu/document/schemas.clj
@@ -805,7 +805,7 @@
                                {:name "P2/P3"}
                                {:name "P1/P2/P3"}]}]})
 
-(def rakennustunnus {:name national-building-id :type :string  :subtype :rakennustunnus :hidden true :readonly true})
+(def rakennustunnus {:name national-building-id :type :string  :subtype :rakennustunnus :hidden true :readonly false})
 
 (def rakennuksen-tiedot-ilman-huoneistoa [kaytto
                                           mitat
