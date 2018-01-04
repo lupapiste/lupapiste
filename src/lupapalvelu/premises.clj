db3f2c4249a55ba37fa9398b3e7da36c2a07b882
Antti Salmivaara <antti.salmivaara@evolta.fi>
2018-01-04 18:37:30 +0200

    LPK-3401 reorganize premises upload handling

--- src/lupapalvelu/premises.clj
+++ src/lupapalvelu/premises.clj
@@ -1,13 +1,19 @@
 (ns lupapalvelu.premises
-  (:require [lupapalvelu.document.persistence :as doc-persistence]
+  (:require [lupapalvelu.action :as action]
+            [lupapalvelu.attachment :as att]
+            [lupapalvelu.document.persistence :as doc-persistence]
+            [lupapalvelu.file-upload :as file-upload]
             [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.user :as usr]
+            [lupapalvelu.xls-muuntaja-client :as xmc]
             [monger.operators :refer [$set]]
+            [noir.response :as resp]
             [sade.core :refer :all]
             [sade.strings :as ss]
             [sade.util :as util]
             [slingshot.slingshot :refer [throw+]]
             [swiss.arrows :refer :all]
-            [lupapalvelu.action :as action]))
+            [taoensso.timbre :as timbre]))
 
 (defn get-huoneistot-from-application [doc-id application]
   (-> (util/find-by-id doc-id (:documents application)) :data :huoneistot))
@@ -45,10 +51,13 @@
                                          :values {"1" true
                                                   "0" false}}})
 
-(defn header-pairing-with-cells [vecs]
-  (let [headers (map #(.toLowerCase %) (first vecs))
-        data (rest vecs)]
-    (map #(zipmap headers %) data)))
+(defn validate-header [header]
+  (let [ifc-headers (set (cons "sijaintikerros" (keys ifc-to-lupapiste-keys)))]
+    (= (set header) ifc-headers)))
+
+(defn- header-data-map [vecs]
+  {:header-row (mapv #(.toLowerCase %) (first vecs))
+   :data       (rest vecs)})
 
 (defn split-with-semicolon [row]
   (map #(ss/split % #";") row))
@@ -61,17 +70,23 @@
       (ss/split #"\n")
       (pick-only-header-and-data-rows)
       (split-with-semicolon)
-      (header-pairing-with-cells)))
+      (header-data-map)))
 
 (defn item->update [premises-number [ifc-key ifc-val]]
   (let [lp-key (-> ifc-key ifc-to-lupapiste-keys :key)
         lp-val (-> ifc-key ifc-to-lupapiste-keys :values)]
     (when (and (not (empty? ifc-val)) lp-key)
-      [(ss/join "." ["huoneistot" premises-number lp-key])
+      [(mapv keyword ["huoneistot" (str premises-number) lp-key])
        (if (map? lp-val) (-> ifc-val lp-val) (ss/replace ifc-val #"," "."))])))
 
 (defn premise->updates [premise premises-number]
   (remove nil? (map #(item->update premises-number %) premise)))
+
+(defn data->model-updates [header-row data]
+  (let [premise-data (map #(zipmap header-row %) data)]
+    (reduce #(concat %1 (premise->updates (nth premise-data %2) %2))
+            []
+            (range (count premise-data)))))
 
 (defn remove-old-premises [command doc]
   (let [paths (->> command
@@ -81,9 +96,43 @@
                    (map (fn [huoneisto-key] [:huoneistot huoneisto-key])))]
     (doc-persistence/remove-document-data command doc paths "documents")))
 
-(defn save-premises-data [premise-data command doc]
-  (let [updates (reduce #(concat %1 (premise->updates (nth premise-data %2) %2))
-                        []
-                        (range (count premise-data)))]
-    (remove-old-premises command doc)
-    (doc-persistence/update! command doc updates "documents")))
+(defn save-premises-data [model-updates {application :application timestamp :created {role :role} :user :as command} doc-id]
+  (let [document                                         (doc-persistence/by-id application "documents" doc-id)
+        update-paths                                     (map first model-updates)
+        {:keys [mongo-query mongo-updates post-results]} (apply doc-persistence/validated-model-updates application "documents" document
+                                                                (doc-persistence/transform document model-updates) timestamp nil)]
+    (when-not document (fail! :error.document-not-found))
+    (doc-persistence/validate-against-whitelist! document update-paths role)
+    (doc-persistence/validate-readonly-updates! document update-paths)
+    (remove-old-premises command doc-id)
+    (action/update-application command mongo-query mongo-updates)
+    (ok :results post-results)))
+
+(defn upload-premises-data [{user :user application :application created :created :as command} files doc]
+  (let [app-id                    (:id application)
+        {:keys [header-row data]} (-> files (first) (xmc/xls-2-csv) :data (csv-data->ifc-coll))
+        premises-data             (when (validate-header header-row)
+                                    (data->model-updates header-row data))
+        file-updated?             (when-not (empty? premises-data)
+                                    (-> premises-data (save-premises-data command doc) :ok))
+        save-response             (when file-updated?
+                                    (timbre/info "Premises updated by premises Excel file in application")
+                                    (->> (first files)
+                                         ((fn [file] {:filename (:filename file) :content (:tempfile file)}))
+                                         (file-upload/save-file)))
+        file-linked?              (when (:fileId save-response)
+                                    (= 1 (att/link-files-to-application app-id [(:fileId save-response)])))
+        return-map                (cond
+                                    file-updated? {:ok true}
+                                    (empty? premises-data) {:ok false :text "error.illegal-premises-excel"}
+                                    :else {:ok false})]
+    (when file-linked?
+      (let [old-ifc-fileId      (-> application :ifc-data :fileId)]
+        (action/update-application command {$set {:ifc-data {:fileId   (:fileId save-response)
+                                                             :filename (:filename save-response)
+                                                             :modified created
+                                                             :user     (usr/summary user)}}})
+        (when old-ifc-fileId (mongo/delete-file-by-id old-ifc-fileId))))
+    (->> return-map
+         (resp/json)
+         (resp/status 200))))
--- src/lupapalvelu/premises_api.clj
+++ src/lupapalvelu/premises_api.clj
@@ -3,27 +3,21 @@
             [lupapalvelu.attachment :as att]
             [lupapalvelu.file-upload :as file-upload]
             [lupapalvelu.mime :as mime]
-            [lupapalvelu.mongo :as mongo]
             [lupapalvelu.permit :as permit]
             [lupapalvelu.premises :as premises]
             [lupapalvelu.roles :as roles]
             [lupapalvelu.states :as states]
-            [lupapalvelu.user :as usr]
-            [lupapalvelu.xls-muuntaja-client :as xmc]
             [monger.operators :refer [$set]]
-            [noir.response :as resp]
             [sade.core :refer :all]
             [slingshot.slingshot :refer [throw+]]
-            [swiss.arrows :refer :all]
-            [taoensso.timbre :as timbre]
-            [lupapalvelu.domain :as domain]))
+            [swiss.arrows :refer :all]))
 
 ;;
 ;;  Validators and pre-checks
 ;;
 
-(defn- primary-operation-pre-check [{:keys [application]}]
-  (when (not (= "kerrostalo-rivitalo" (-> application :primaryOperation :name)))
+(defn- uusiRakennus-pre-check [{:keys [application]}]
+  (when-not (some #(= "uusiRakennus" (-> % :schema-info :name)) (:documents application))
     (fail :error.illegal-primary-operation)))
 
 (defn- file-size-positive [{{files :files} :data}]
@@ -34,13 +28,17 @@
   (when-not (-> files (first) :filename (mime/allowed-file?))
     (fail :error.file-upload.illegal-file-type)))
 
+;;
+;;  Upload
+;;
+
 (defraw upload-premises-data
   {:user-roles       #{:applicant :authority :oirAuthority}
    :parameters       [doc id files]
    :user-authz-roles (conj roles/default-authz-writer-roles :foreman)
    :pre-checks       [(action/some-pre-check att/allowed-only-for-authority-when-application-sent
                                              (permit/validate-permit-type-is :R))
-                      primary-operation-pre-check
+                      uusiRakennus-pre-check
                       action/disallow-impersonation]
    :input-validators [(partial action/non-blank-parameters [:id :doc])
                       validate-mime-type
@@ -50,29 +48,5 @@
                       :authority    states/pre-verdict-states
                       :oirAuthority states/pre-verdict-states}
    :feature          :premises-upload}
-  [{user :user application :application created :created :as command}]
-  (let [app-id        (:id application)
-        premises-data (-> files (first) (xmc/xls-2-csv) :data (premises/csv-data->ifc-coll))
-        file-updated? (when-not (empty? premises-data)
-                        (-> premises-data (premises/save-premises-data command doc) :ok))
-        save-response (when file-updated?
-                        (timbre/info "Premises updated by premises Excel file in application")
-                        (->> (first files)
-                             ((fn [file] {:filename (:filename file) :content (:tempfile file)}))
-                             (file-upload/save-file)))
-        file-linked?  (when (:fileId save-response)
-                        (= 1 (att/link-files-to-application app-id [(:fileId save-response)])))
-        return-map    (cond
-                        file-updated? {:ok true}
-                        (empty? premises-data) {:ok false :text "error.illegal-premises-excel"}
-                        :else {:ok false})]
-    (when file-linked?
-      (let [old-ifc-fileId      (-> application :ifc-data :fileId)]
-        (action/update-application command {$set {:ifc-data  {:fileId    (:fileId save-response)
-                                                                      :filename  (:filename save-response)
-                                                                      :modified  created
-                                                                      :user      (usr/summary user)}}})
-        (when old-ifc-fileId (mongo/delete-file-by-id old-ifc-fileId))))
-    (->> return-map
-         (resp/json)
-         (resp/status 200))))
+  [command]
+  (premises/upload-premises-data command files doc))
