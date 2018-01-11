53e08e4792a5ff89a004ac46e0dea9353917e9e4
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2018-01-11 17:54:02 +0200

    LP-365577 allow application-authority call notice & tags commands
    use some-pre-check with statement-giver-pre-check to achieve this

--- itest/lupapalvelu/notice_api_itest.clj
+++ itest/lupapalvelu/notice_api_itest.clj
@@ -106,15 +106,25 @@
             (query teppo :application-organization-tags :id id) => err
             (query luukas :application-organization-tags :id id) => fail?
             (query pena :application-organization-tags :id id) => fail?))))
-    (facts "Authority + application statementGiver combo LP-365545"
+    (facts "Authority + application statementGiver combo LP-365545 & LP-365577"
       (fact "Ronja can see notices"
         (query ronja :authority-notice :id id) => ok?)
-      ; invite Ronja as statementGiver to reveal bug presented in LP-365545
+      (fact "Ronja could change urgnecy"
+        ronja => (allowed? :change-urgency :id id :urgency "foo"))
+      (fact "Ronja could add notice & tags"
+        ronja => (allowed? :add-authority-notice :id id :authorityNotice "foo")
+        ronja => (allowed? :add-application-tags :id id :tags ["foo"]))
+      ; invite Ronja as statementGiver to reveal bug presented in LP-365545 & LP-365577
       (command sonja :request-for-statement :functionCode nil :id id
                :selectedPersons [{:name "R0NJ4" :text "Testi" :email (email-for "ronja")}]
                :saateText "ronja testi" :dueDate 1450994400000) => ok?
       (fact "Ronja can still see notices"
-        (query ronja :authority-notice :id id) => ok?))
+        (query ronja :authority-notice :id id) => ok?)
+      (fact "Ronja can still change urgnecy"
+        ronja => (allowed? :change-urgency :id id :urgency "foo"))
+      (fact "Ronja can still add notice & tags"
+        ronja => (allowed? :add-authority-notice :id id :authorityNotice "foo")
+        ronja => (allowed? :add-application-tags :id id :tags ["foo"])))
     (facts "Guests and notice panel"
       (fact "Add Veikko as a guest authority"
         (command sipoo :update-guest-authority-organization
