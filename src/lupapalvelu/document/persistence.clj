e5e48cff79b968e48a8c5662e67b7f9b9a672778
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2018-01-15 14:03:16 +0200

    LP-365602 fix regression from LPK-3474: support whitelist for :permitType and :roles
    Problem was that enriching document for frontend had a broken conditional, which is now fixed and obeys :permitType and :roles settings as "(and)".
    Also add :roles [:authority] for ARK, to ensure only authorities can edit valtakunnallinenNumero...

--- src/lupapalvelu/document/persistence.clj
+++ src/lupapalvelu/document/persistence.clj
@@ -130,14 +130,17 @@
   (when-not (#{"documents" "tasks"} collection)
     (fail :error.unknown-type)))
 
+(defn validate-whitelist-properties [value-mapping [k v]]
+  (some #{(get value-mapping k)} v))
+
 (defn validate-against-whitelist! [document update-paths user-role {permitType :permitType}]
   (let [doc-schema (model/get-document-schema document)]
-    (doseq [path update-paths]
-      (let [{whitelist :whitelist} (model/find-by-name (:body doc-schema) path)]
-        (when-not (or (empty? whitelist)
-                      (some #{(keyword user-role)} (:roles whitelist))
-                      (some #{(keyword permitType)} (:permitType whitelist)))
-          (unauthorized!))))))
+    (doseq [path update-paths
+            :let [{whitelist :whitelist} (model/find-by-name (:body doc-schema) path)
+                  whitelist-key-mapping {:roles (keyword user-role)
+                                         :permitType (keyword permitType)}]]
+      (when-not (every? (partial validate-whitelist-properties whitelist-key-mapping) (dissoc whitelist :otherwise))
+        (unauthorized!)))))
 
 (defn- sent? [{state :state}]
   (and state (= "sent" (name state))))
