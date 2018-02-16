07e5b44725b5c0c42336623f297d4465874e662d
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2018-02-16 08:43:17 +0200

    use pena as apikey to create more realistic scenario

--- itest/lupapalvelu/pate_itest.clj
+++ itest/lupapalvelu/pate_itest.clj
@@ -1121,7 +1121,7 @@
                                          ["manuaalinen_rakennusnro" "789"]])
                       => ok?))
                   (fact "Add pientalo operation to the application"
-                    (command sonja :add-operation :id app-id
+                    (command pena :add-operation :id app-id
                              :operation "pientalo") => ok?)
                   (let [{:keys [documents
                                 secondaryOperations]} (query-application sonja app-id)
@@ -1156,11 +1156,11 @@
                                           :paloluokka             ""
                                           :order                  "1"}})
                     (fact "Add tag and description to pientalo"
-                      (command sonja :update-doc :id app-id
+                      (command pena :update-doc :id app-id
                                :doc doc-id
                                :collection "documents"
                                :updates [["tunnus" "Hao"]]) => ok?
-                      (command sonja :update-op-description :id app-id
+                      (command pena :update-op-description :id app-id
                                :op-id op-id-pientalo
                                :desc "Hen piaoliang!") => ok?)
                     (fact "national-id update pre-verdict"
@@ -1211,7 +1211,7 @@
                     (fact "Cannot publish in the complementNeeded state"
                       (command sonja :publish-pate-verdict :id app-id
                                :verdict-id verdict-id)
-                      => fail?)
+                      => (partial expected-failure? :error.command-illegal-state))
                     (fact "Preview fetch works, though"
                       (:headers (raw sonja :preview-pate-verdict :id app-id
                                      :verdict-id verdict-id))
