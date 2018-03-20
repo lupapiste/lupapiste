1259505415b05609ca719caf9965ac9833a5c9fe
Joni Hämäläinen <joni.hamalainen@evolta.fi>
2018-03-20 14:05:18 +0200

    LPK-3683 implement :propertyMunicipalityCache to save municipality information to mongo db for 24h

--- src/lupapalvelu/property.clj
+++ src/lupapalvelu/property.clj
@@ -3,15 +3,27 @@
             [sade.core :refer :all]
             [sade.env :as env]
             [sade.property :as sprop]
-            [lupapalvelu.wfs :as wfs]))
+            [lupapalvelu.mongo :as mongo]
+            [lupapalvelu.wfs :as wfs])
+  (:import (com.mongodb WriteConcern)))
+
+(defn property-info-from-wfs [property-id]
+  (->> (wfs/get-property-location-info-by-property-id property-id)
+       (wfs/location-feature-to-property-info)))
 
 (defn location-data-by-property-id-from-wfs [property-id]
   "Queries KTJKii WFS for property location information."
-  (let [start  (double (now))
-        result (->> (wfs/get-property-location-info-by-property-id property-id)
-                    (wfs/location-feature-to-property-info))]
-    (debugf "Municipality for property id from KTJKii took %.3f seconds" (/ (- (now) start) 1000))
-    result))
+  (or
+    (mongo/select-one :propertyMunicipalityCache {:propertyId property-id} [:municipality])
+    (let [start  (double (now))
+          result (property-info-from-wfs property-id)]
+      (debugf "Municipality for property id from KTJKii took %.3f seconds" (/ (- (now) start) 1000))
+      (when (:municipality result)
+        (mongo/insert :propertyMunicipalityCache
+                      (mongo/with-mongo-meta (merge result {:propertyId property-id}))
+                      WriteConcern/UNACKNOWLEDGED)
+        (debugf "Inserted property %s to :propertyMunicipalityCache" property-id))
+      result)))
 
 (defn municipality-by-property-id-from-wfs
   "Queries KTJKii WFS for property location information. Returns municipality code as string."
