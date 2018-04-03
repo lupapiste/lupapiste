24dc5bc43c7244b78d8c0f6cea6d8f76ce849076
Antti Slunga <Antti.Slunga@solita.fi>
2018-04-03 15:29:32 +0300

    LPK-3508 Commons version update

--- project.clj
+++ project.clj
@@ -131,7 +131,7 @@
                  ; Oskari map (https://github.com/lupapiste/oskari)
                  [lupapiste/oskari "0.9.60"]
                  ; Shared domain code (https://github.com/lupapiste/commons)
-                 [lupapiste/commons "0.9.8"]
+                 [lupapiste/commons "0.9.9"]
                  ; Smoke test lib (https://github.com/lupapiste/mongocheck)
                  [lupapiste/mongocheck "0.1.3"]
                  ; iText fork with bug fixes and upgraded dependencies (https://github.com/lupapiste/OpenPDF)
