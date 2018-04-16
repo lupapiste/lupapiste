50b7d68a39b6ecc510fe01f0c9b8766248510aa9
Joni Hämäläinen <joni.hamalainen@evolta.fi>
2018-04-16 11:34:04 +0300

    added tests for basic JMS operations: queue, consumers and producers

---
+++ test/lupapalvelu/integrations/jms_test.clj
@@ -0,0 +1,41 @@
+(ns lupapalvelu.integrations.jms-test
+  (:require [midje.sweet :refer :all]
+            [taoensso.timbre :refer [warnf]]
+            [sade.env :as env]
+            [lupapalvelu.integrations.jms :refer :all])
+  (:import (javax.jms Queue MessageConsumer)))
+
+(def test-atom (atom nil))
+
+(if-not (and (env/feature? :embedded-artemis) (env/feature? :jms))
+  (warnf "JMS not enabled for unit testing")
+  (facts "JMS"
+    (fact "Embedded Artemis OK"
+      (let [broker @(ns-resolve 'artemis-server 'embedded-broker)]
+        (.isStarted broker) => true
+        (str broker) => #"ActiveMQServerImpl"))
+    (fact "Queue" (queue "tester") => (partial instance? Queue))
+    (facts "Consumers"
+      (fact "own fn"
+        (let [consumer (create-consumer "tester" #(reset! test-atom %))]
+          consumer => (partial instance? MessageConsumer)))
+      (fact "nippy"
+        (let [nippy-consumer (create-nippy-consumer "nippy.queue" #(reset! test-atom %))]
+          nippy-consumer => (partial instance? MessageConsumer))))
+    (facts "Producers"
+      (fact "Default producer is text message"
+        (let [prod (create-producer "tester")]
+          prod => fn?
+          (fact "successful send"
+            (prod "test1") => nil
+            (Thread/sleep 10)                               ; non-deterministic wait for message delivery
+            @test-atom => "test1")
+          (fact "wrong type -> exception"
+            (prod {:foo 1}) => (throws ClassCastException))))
+      (fact "Nippy producer can be also used"
+        (let [nippy (create-nippy-producer "nippy.queue")]
+          nippy => fn?
+          (fact "successful send"
+            (nippy {:works? true}) => nil
+            (Thread/sleep 10) ; non-deterministic wait for message delivery
+            @test-atom => {:works? true}))))))
