0c017d5c77280d7a1ea38bc8f6a113fa6914520f
Jarppe <jarppe@metosin.fi>
2018-04-26 15:35:45 +0300

    WS

--- src/lupapalvelu/organization_api.clj
+++ src/lupapalvelu/organization_api.clj
@@ -1006,7 +1006,7 @@
    :optional-parameters [org lang]
    :input-validators    [org/valid-feed-format org/valid-org i18n/valid-language]
    :user-roles          #{:anonymous}}
-  ((memo/ttl waste-ads/waste-ads :ttl/threshold 900000)     ; 15 min
+  ((memo/ttl waste-ads/waste-ads :ttl/threshold 900000)                         ; 15 min
     (ss/upper-case org)
     (-> fmt ss/lower-case keyword)
     (-> (or lang :fi) ss/lower-case keyword)))
