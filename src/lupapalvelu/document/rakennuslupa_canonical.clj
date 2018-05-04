d9d1d134ec580e989dc2ed3702550f8bfb087c50
Antti Slunga <Antti.Slunga@solita.fi>
2018-05-04 14:11:11 +0300

    PATE-74 HotFix: huoneisto info to kuntaGML only for pate organizations

--- src/lupapalvelu/document/rakennuslupa_canonical.clj
+++ src/lupapalvelu/document/rakennuslupa_canonical.clj
@@ -8,7 +8,8 @@
             [lupapalvelu.i18n :as i18n]
             [lupapalvelu.document.canonical-common :refer :all]
             [lupapalvelu.document.tools :as tools]
-            [lupapalvelu.document.schemas :as schemas]))
+            [lupapalvelu.document.schemas :as schemas]
+            [lupapalvelu.organization :as org]))
 
 (defn- muutostapa
   "If muutostapa is hidden and has a default value via schema the
@@ -97,9 +98,11 @@
                                       :julkisivumateriaali (:julkisivu rakenne))
         lammitystapa (:lammitystapa lammitys)
         huoneistot-data (get-huoneisto-data huoneistot (:name info))
-        huoneistot {:huoneisto huoneistot-data
-                    :asuntojenPintaala (get-huoneistot-pintaala huoneistot-data)
-                    :asuntojenLkm (get-huoneistot-lkm huoneistot-data)}
+        huoneistot (if (org/pate-org? (:organization application))
+                     {:huoneisto huoneistot-data
+                      :asuntojenPintaala (get-huoneistot-pintaala huoneistot-data)
+                      :asuntojenLkm (get-huoneistot-lkm huoneistot-data)}
+                     {:huoneisto huoneistot-data})
         vaestonsuoja-value (ss/trim (get-in toimenpide [:varusteet :vaestonsuoja]))
         vaestonsuoja (when (ss/numeric? vaestonsuoja-value) (util/->int vaestonsuoja-value))
         rakennuksen-tiedot-basic-info {:kayttotarkoitus (:kayttotarkoitus kaytto)
