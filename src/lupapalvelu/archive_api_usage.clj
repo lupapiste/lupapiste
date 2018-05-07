ba8cbc335509de2e7c70811312591f3a96427f63
Ville Outamaa <ville.outamaa@solita.fi>
2018-05-07 12:45:17 +0300

    LPK-3761 Drop invalid archive API usage log entries

--- src/lupapalvelu/archive_api_usage.clj
+++ src/lupapalvelu/archive_api_usage.clj
@@ -1,6 +1,8 @@
 (ns lupapalvelu.archive-api-usage
+  ""
   (:require [monger.operators :refer :all]
             [schema.core :as sc]
+            [taoensso.timbre :refer [errorf]]
             [sade.core :refer [now]]
             [sade.env :as env]
             [sade.schemas :as ssc]
@@ -13,9 +15,9 @@
 (sc/defschema ArchiveApiUsageLogEntry
   "Schema for API usage log entries, produced by Onkalo"
   {:organization             org/OrgId
-   :timestamp                ssc/Nat  ; Timestamp of download, change to :downloaded
-   (sc/optional-key :logged) ssc/Nat  ; Timestamp of logging, added when sent to Mongo
-   sc/Keyword                sc/Any}) ; Rest of the data TBD
+   :timestamp                ssc/Nat   ; Timestamp of download, change to :downloaded
+   (sc/optional-key :logged) ssc/Nat   ; Timestamp of logging, added when sent to Mongo
+   sc/Keyword                sc/Any})  ; Rest of the data TBD
 
 (def archive-api-usage-collection :archive-api-usage)
 
@@ -25,18 +27,12 @@
   (mongo/insert archive-api-usage-collection
                 (assoc entry :logged log-timestamp)))
 
-(defn get-archive-api-usage-logs [start-ts end-ts]
-  (mongo/snapshot archive-api-usage-collection
-                  {:logged (util/assoc-when {}
-                                            $gte start-ts
-                                            $lt  end-ts)}))
-
 (when (env/feature? :jms)
 (defn- message-handler
   [payload]
-  ;; TODO validate data, on error log and drop
-  (println payload)
-  (log-archive-api-usage! payload (now)))
+  (if-let [schema-error (sc/check ArchiveApiUsageLogEntry payload)]
+    (errorf "Archive API usage log entry does not match schema: %s" schema-error)
+    (log-archive-api-usage! payload (now))))
 
 (def archive-api-usage-queue "onkalo.api-usage")
 
--- test/lupapalvelu/archive_api_usage_test.clj
+++ test/lupapalvelu/archive_api_usage_test.clj
@@ -12,20 +12,28 @@
 
 (if-not (and (env/feature? :embedded-artemis) (env/feature? :jms))
   (warnf "JMS not enabled for unit testing")
+
   (facts "Archive API usage"
          (with-redefs [lupapalvelu.mongo/insert
                        (fn [collection entry]
                          (when (= collection archive-api-usage-collection)
                            (swap! mongo-mock conj entry)))]
+
            (fact "consumed message is inserted into collection"
                  (onkalo-mock {:organization "753-R" :timestamp 12345})
                  (Thread/sleep 100)
                  (->> @mongo-mock count) => 1
                  (->> @mongo-mock first :timestamp) => 12345)
+
            (fact "multiple messages are inserted"
                  (onkalo-mock {:organization "753-R" :timestamp 12345})
                  (onkalo-mock {:organization "753-R" :timestamp 12346})
                  (onkalo-mock {:organization "753-R" :timestamp 12347})
                  (Thread/sleep 100)
                  (->> @mongo-mock count) => 3
-                 (->> @mongo-mock last :timestamp) => 12347))))
+                 (->> @mongo-mock last :timestamp) => 12347)
+
+           (fact "messages that do not match schema are dropped"
+                 (onkalo-mock {:does :not :match :schema})
+                 (Thread/sleep 100)
+                 (->> @mongo-mock count) => 0))))
