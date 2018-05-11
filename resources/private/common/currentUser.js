efec5c2147a1959cd24ca4d7d7abf754fe542265
Jarppe <jarppe@metosin.fi>
2018-05-11 13:58:23 +0300

    WS.

--- resources/private/common/currentUser.js
+++ resources/private/common/currentUser.js
@@ -4,28 +4,28 @@ LUPAPISTE.CurrentUser = function() {
   var self = this;
 
   var defaults = {
-    id:             "",
-    email:          "",
-    role:           "",
-    oir:            "",
-    organizations:  [],
-    firstName:      "",
-    lastName:       "",
-    phone:          "",
-    username:       "",
-    language:       "",
-    orgAuthz:       undefined,
+    id: "",
+    email: "",
+    role: "",
+    oir: "",
+    organizations: [],
+    firstName: "",
+    lastName: "",
+    phone: "",
+    username: "",
+    language: "",
+    orgAuthz: undefined,
     company: {
-      id:   undefined,
+      id: undefined,
       role: undefined
     },
     notification: {
-      title:          undefined,
-      titleI18nkey:   undefined,
-      message:        undefined,
+      title: undefined,
+      titleI18nkey: undefined,
+      message: undefined,
       messageI18nkey: undefined
     },
-    defaultFilter: {id: "", foremanFilterId: "",  companyFilterId: ""},
+    defaultFilter: {id: "", foremanFilterId: "", companyFilterId: ""},
     applicationFilters: [],
     foremanFilters: [],
     companyApplicationFilters: []
@@ -33,22 +33,24 @@ LUPAPISTE.CurrentUser = function() {
 
   var fbPixel = null;
   if (LUPAPISTE.config.facebook && LUPAPISTE.config.facebook.url && analytics && analytics.isTrackingEnabled()) {
-    fbPixel = function () {
+    fbPixel = function() {
       var fbUrl = LUPAPISTE.config.facebook.url;
       return "<img height='1' width='1' style='display:none' src='" + fbUrl + "'/>";
     };
   }
 
   function constructor(user) {
-    if( !_.isEmpty(user) && !user.language && !user.virtual ) {
+    if (!_.isEmpty(user) && !user.language && !user.virtual) {
       user.language = loc.getCurrentLanguage();
       ajax.command("update-user", _.pick(user, ["firstName", "lastName", "language"]))
-        .success(function() {
-          hub.send("indicator", {style: "primary",
-                                 message: "user.language.note",
-                                 sticky: true, html: true});
-        })
-        .call();
+          .success(function() {
+            hub.send("indicator", {
+              style: "primary",
+              message: "user.language.note",
+              sticky: true, html: true
+            });
+          })
+          .call();
     }
     if (user.firstLogin) {
       hub.send("first-login", {user: user});
@@ -62,9 +64,11 @@ LUPAPISTE.CurrentUser = function() {
     if (_.isFunction(fbPixel)) {
       // send a bit to Facebook about firstLogin
       info("Triggering first login Facebook pixel");
-      hub.send("indicator", {style: "hidden",
-                             rawMessage: fbPixel(),
-                             html: true});
+      hub.send("indicator", {
+        style: "hidden",
+        rawMessage: fbPixel(),
+        html: true
+      });
     }
   });
 
@@ -76,18 +80,18 @@ LUPAPISTE.CurrentUser = function() {
   // Hash is cleared when returned to the application list. But the
   // we need to wrap it to the observable in order to trigger
   // isAuthority and isApplicant computeds.
-  var hash = ko.observable();
+  const hash = ko.observable();
 
-  $(window).on( "hashchange", function() {
+  $(window).on("hashchange", function() {
     // Null-safe regarding location
-    hash( _.get( window, "location.hash" ));
-  } );
+    hash(_.get(window, "location.hash"));
+  });
 
   function isOutsideAuthority() {
     var app = lupapisteApp.models.application;
     return self.role() === "authority"
-      && app && _.includes( hash(), app.id())
-      && !_.get( self.orgAuthz(), app.organization());
+      && app && _.includes(hash(), app.id())
+      && !_.get(self.orgAuthz(), app.organization());
   }
 
   self.isAuthorityAdmin = ko.pureComputed(function() {
@@ -173,9 +177,9 @@ LUPAPISTE.CurrentUser = function() {
   });
 
   // Role in the context of the current application.
-  self.applicationRole = _.cond( [[self.isAuthority, _.constant( "authority")],
-                                  [self.isApplicant, _.constant( "applicant")],
-                                  [_.stubTrue, self.role ]]);
+  self.applicationRole = _.cond([[self.isAuthority, _.constant("authority")],
+    [self.isApplicant, _.constant("applicant")],
+    [_.stubTrue, self.role]]);
 
   self.isCompanyUser = ko.pureComputed(function() {
     return !_.isEmpty(ko.unwrap(self.company.id()));
@@ -195,7 +199,7 @@ LUPAPISTE.CurrentUser = function() {
   });
 
   function getNotificationFields(notification) {
-    if(notification.titleI18nkey() && notification.messageI18nkey()) {
+    if (notification.titleI18nkey() && notification.messageI18nkey()) {
       return {
         title: loc(notification.titleI18nkey()),
         msg: loc(notification.messageI18nkey())
@@ -213,22 +217,23 @@ LUPAPISTE.CurrentUser = function() {
   ko.computed(function() {
     if (self.showNotification()) {
       var fields = getNotificationFields(self.notification);
-      hub.send("show-dialog", {title: fields.title,
-                               id: "user-notification-dialog",
-                               size: "medium",
-                               component: "ok-dialog",
-                               closeOnClick: true,
-                               componentParams: {text: fields.msg}
-                              });
+      hub.send("show-dialog", {
+        title: fields.title,
+        id: "user-notification-dialog",
+        size: "medium",
+        component: "ok-dialog",
+        closeOnClick: true,
+        componentParams: {text: fields.msg}
+      });
     }
   });
 
   hub.subscribe({eventType: "dialog-close", id: "user-notification-dialog"}, function() {
     ajax.command("remove-user-notification")
-      .complete(function () {
-        hub.send("reload-current-user");
-      })
-      .call();
+        .complete(function() {
+          hub.send("reload-current-user");
+        })
+        .call();
   });
 
   hub.subscribe("login", function(data) {
@@ -239,11 +244,11 @@ LUPAPISTE.CurrentUser = function() {
 
   hub.subscribe("reload-current-user", function() {
     ajax.query("user")
-      .success(function (res) {
-        if (res.user) {
-          constructor(res.user);
-        }
-      })
-      .call();
+        .success(function(res) {
+          if (res.user) {
+            constructor(res.user);
+          }
+        })
+        .call();
   });
 };
