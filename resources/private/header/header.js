8cd81a5eb98202dc591c04dbd21302a6c01bf100
Jarppe <jarppe@metosin.fi>
2018-05-15 13:52:03 +0300

    WS

--- resources/private/header/header.js
+++ resources/private/header/header.js
@@ -7,26 +7,26 @@
     self.languages = loc.getSupportedLanguages();
     self.languageMenuVisible = ko.observable(false);
     self.currentLanguage = loc.getCurrentLanguage();
-    self.changeLanguage = function( lang ) {
-      if( lang !== self.currentLanguage ) {
-        hub.send("change-lang", { lang: lang });
+    self.changeLanguage = function(lang) {
+      if (lang !== self.currentLanguage) {
+        hub.send("change-lang", {lang: lang});
       }
-      self.languageMenuVisible( false );
+      self.languageMenuVisible(false);
     };
     self.toggleLanguageMenu = function() {
-      self.languageMenuVisible( !self.languageMenuVisible() );
+      self.languageMenuVisible(!self.languageMenuVisible());
     };
 
-    var cancel =   _.partial( self.languageMenuVisible, false);
+    const cancel = _.partial(self.languageMenuVisible, false);
 
     // We cancel (aka close the menu), on dialog-close event
     // that denotes user pressing esc key and clicks outside
     // of menu area.
-    hub.subscribe( "dialog-close", cancel);
-    $(document).on( "click", ".language-menu", function( e ) {
+    hub.subscribe("dialog-close", cancel);
+    $(document).on("click", ".language-menu", function(e) {
       e.stopPropagation();
     });
-    $(document).on( "click", cancel );
+    $(document).on("click", cancel);
   }
 
   const langs = new Languages();
