498c33d2ada515f6642fe605dfd5ca2aa7397f81
Jouni Honkala <jouni.honkala@evolta.fi>
2018-05-23 11:06:41 +0300

    nitpicker alert fixed

--- test/lupapalvelu/xml/krysp/rakennuslupa_canonical_to_krysp_xml_test.clj
+++ test/lupapalvelu/xml/krysp/rakennuslupa_canonical_to_krysp_xml_test.clj
@@ -410,4 +410,4 @@
           app-info (-> xml_s xml/parse cr/strip-xml-namespaces)]
 
       (validator/validate xml_s (:permitType jatkolupa-application) "2.2.2")
-      (fact "asiankuvaus" (xml/get-text app-info [:rakennusvalvontaasianKuvaus]) => "Pari vuotta jatko-aikaa, että saadaan rakennettua loppuun."))))
+      (fact "asiankuvaus" (xml/get-text app-info [:rakennusvalvontaasianKuvaus]) => "Pari vuotta jatko-aikaa, ett\u00e4 saadaan rakennettua loppuun."))))
