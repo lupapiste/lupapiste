f154e28cf4f3a4a148493f7bd678362769c50105
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-01 11:50:40 +0300

    PATE-78 Fix non-ascii characters

--- test/lupapalvelu/prev_permit_test.clj
+++ test/lupapalvelu/prev_permit_test.clj
@@ -4,10 +4,10 @@
 
 (def tyonjohtaja
   {:alkamisPvm "2013-09-13Z"
-   :tyonjohtajaRooliKoodi "KVV-työnjohtaja"
+   :tyonjohtajaRooliKoodi "KVV-ty\u00F6njohtaja"
    :henkilo {:sahkopostiosoite "lappajarven-testityonjohtajat@jabadabaduu.fi"
              :puhelin "0400-123456"
-             :nimi {:sukunimi "Testilä"
+             :nimi {:sukunimi "Testinen"
                     :etunimi "Tiina"}
              :osoite {:postinumero "00102"
                       :osoitenimi {:teksti "Mannerheimintie 1"}
@@ -20,7 +20,7 @@
 (fact "tyonjohtaja -element from KRYSP is converted into tyonjohtaja-v2 document"
   (let [document (tyonjohtaja->tj-document tyonjohtaja)]
     (fact "It contains the value of :tyonjohtajaRooliKoodi from input"
-      (get-in document [:data :kuntaRoolikoodi :value]) => "KVV-työnjohtaja")
+      (get-in document [:data :kuntaRoolikoodi :value]) => "KVV-ty\u00F6njohtaja")
     (fact "Education information is preserved"
       (get-in document [:data :patevyys-tyonjohtaja :koulutusvalinta :value]) => "LVI-teknikko")
     (fact "Name and social security info are in place"
