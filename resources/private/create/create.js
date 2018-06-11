2e7000039f9990e8d69341064dac71129cdb23fd
Joni Hämäläinen <joni.hamalainen@evolta.fi>
2018-06-11 15:00:52 +0300

    PATE-128 own command for conversion debug

--- resources/private/create/create.js
+++ resources/private/create/create.js
@@ -371,6 +371,21 @@
         })
         .call();
     };
+      self.convertApplication = function() {
+        var lupatunnus = _.trim((self.kuntalupatunnusFromPrevPermit() || "" )).split(/\s+/).join( " ");
+        ajax.command("convert-application", {kuntalupatunnus: lupatunnus})
+            .processing(self.processing)
+            .pending(self.pending)
+            .success(function(data) {
+              self.clear();
+              console.log(data);
+              pageutil.openApplicationPage({id: data.id});
+            })
+            .error(function(d) {
+              notify.ajaxError(d);
+            })
+            .call();
+    };
   }
 
   var model = new CreateApplicationModel();
