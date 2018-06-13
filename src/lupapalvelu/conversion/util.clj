d0cf8e57c340f006d8bc671052cedf5d60b01e15
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-13 16:39:28 +0300

    PATE-110 Tweak regex

--- src/lupapalvelu/conversion/util.clj
+++ src/lupapalvelu/conversion/util.clj
@@ -4,11 +4,11 @@
 
 (def general-permit-id-regex
   "So-called 'general' format, e.g. 63-0447-12-A"
-  #"\d{2}-\d{4}-\d{2}-[A-Z]+")
+  #"\d{2}-\d{4}-\d{2}-[A-Z]{1,3}")
 
 (def database-permit-id-regex
   "So-called 'database' format, e.g. 12-0477-A 63"
-  #"\d{2}-\d{4}-[A-Z]+ \d{2}")
+  #"\d{2}-\d{4}-[A-Z]{1,3} \d{2}")
 
 (defn destructure-permit-id
   "Split a permit id into a map of parts. Works regardless of which of the two
