731019297a2dfcfffec78b75ce17bafc014c7331
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2018-06-19 10:33:02 +0300

    LPK-3777 Use lupapalvelu.document.data-schema instead of hand-rolled document schemas in ValidPlacementApplication.

--- src/lupapalvelu/integrations/allu_schemas.clj
+++ src/lupapalvelu/integrations/allu_schemas.clj
@@ -6,6 +6,7 @@
             [sade.schemas :refer [NonBlankStr Email Zipcode Tel Hetu FinnishY FinnishOVTid Kiinteistotunnus
                                   ApplicationId]]
             [sade.municipality :refer [municipality-codes]]
+            [lupapalvelu.document.data-schema :as dds]
             [lupapalvelu.document.canonical-common :refer [ya-operation-type-to-schema-name-key]]
             [lupapalvelu.integrations.geojson-2008-schemas :as geo :refer [SingleGeometry]]))
 
@@ -14,21 +15,21 @@
 ;;;; # Generalia
 
 (defschema DateTimeNoMs
-  "ISO-8601/RFC-3339 aikaleima sekuntien tarkkuudella"
+  "ISO-8601/RFC-3339 date time without milliseconds"
   (sc/pred (fn [s] (try (tf/parse (tf/formatters :date-time-no-ms) s)
                         (catch Exception _ false)))
            "ISO-8601/RFC-3339 date time without milliseconds"))
 
 (defschema ISO-3166-alpha-2
-  "Kaksikirjaiminen maakoodi (esim. 'FI')"
+  "Two letter country code (e.g. 'FI')"
   (apply sc/enum (map :alpha-2 countries/countries)))
 
 (defschema ISO-3166-alpha-3
-  "Kolmikirjaiminen maakoodi (esim. 'FIN')"
+  "Two letter country code (e.g. 'FIN')"
   (apply sc/enum (map :alpha-3 countries/countries)))
 
-(defschema MunicipalityCode
-  "Kolminumeroinen kuntatunnus"
+(defschema FinnishMunicipalityCode
+  "Three digit Finnish municipality code"
   (apply sc/enum municipality-codes))
 
 (defschema FeaturelessGeoJSON-2008
@@ -44,66 +45,17 @@
 
 ;;;; # Lupapiste applications
 
-(defschema ValidAddress
-  {:katu                 {:value NonBlankStr}
-   :postinumero          {:value Zipcode}
-   :postitoimipaikannimi {:value NonBlankStr}
-   :maa                  {:value ISO-3166-alpha-3}})
-
-(defschema ValidContactInfo
-  {:puhelin {:value Tel}
-   :email   {:value Email}})
-
-(defschema ValidPersonDoc
-  {:henkilotiedot {:etunimi  {:value NonBlankStr}
-                   :sukunimi {:value NonBlankStr}
-                   :hetu     {:value Hetu}}
-   :osoite        ValidAddress
-   :yhteystiedot  ValidContactInfo})
-
-(defschema ValidCompanyDoc
-  {:yritysnimi           {:value NonBlankStr}
-   :liikeJaYhteisoTunnus {:value FinnishY}
-   :osoite               ValidAddress
-   :yhteyshenkilo        {:henkilotiedot {:etunimi  {:value NonBlankStr}
-                                          :sukunimi {:value NonBlankStr}}
-                          :yhteystiedot  ValidContactInfo}})
-
-(defschema ValidCustomerDoc
-  {:schema-info {:subtype (sc/enum :hakija :maksaja)}
-   :data        {:_selected {:value (sc/enum "henkilo" "yritys")}
-                 :henkilo   ValidPersonDoc
-                 :yritys    ValidCompanyDoc}})
-
-(defschema ValidApplicantDoc
-  (assoc-in ValidCustomerDoc [:schema-info :subtype] (sc/eq :hakija)))
-
-(defschema ValidPaymentInfo
-  {:verkkolaskuTunnus {:value sc/Str}
-   :ovtTunnus         {:value FinnishOVTid}
-   :valittajaTunnus   {:value NonBlankStr}})
-
-(defschema ValidPayeeDoc
-  (-> ValidCustomerDoc
-      (assoc-in [:schema-info :subtype] (sc/eq :maksaja))
-      (assoc-in [:data :laskuviite] {:value sc/Str})
-      (assoc-in [:data :yritys :verkkolaskutustieto] ValidPaymentInfo)))
-
-(def ValidDescriptionDoc
-  {:schema-info {:subtype (sc/eq :hankkeen-kuvaus)}
-   :data        {:kayttotarkoitus {:value NonBlankStr}}})
-
 (defschema ValidPlacementApplication
   {:id               ApplicationId
    :permitSubtype    NonBlankStr
    :organization     NonBlankStr
    :propertyId       Kiinteistotunnus
-   :municipality     MunicipalityCode
+   :municipality     FinnishMunicipalityCode
    :address          NonBlankStr
    :primaryOperation {:name (apply sc/enum (map name (keys ya-operation-type-to-schema-name-key)))}
-   :documents        [(sc/one ValidApplicantDoc "applicant")
-                      (sc/one ValidDescriptionDoc "description")
-                      (sc/one ValidPayeeDoc "payee")]
+   :documents        [(sc/one (dds/doc-data-schema "hakija-ya" true) "applicant")
+                      (sc/one (dds/doc-data-schema "yleiset-alueet-hankkeen-kuvaus-sijoituslupa" true) "description")
+                      (sc/one (dds/doc-data-schema "yleiset-alueet-maksaja" true) "payee")]
    :location-wgs84   [(sc/one sc/Num "longitude") (sc/one sc/Num "latitude")]
    :drawings         [{:geometry-wgs84 SingleGeometry}]})
 
