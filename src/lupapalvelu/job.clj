d14cccc425cb09c1261b8cb25590ea282885b77f
Markus Penttila <markus.penttila@evolta.fi>
2018-07-05 23:50:54 +0300

    LPK-2551: Store jobs in database

--- src/lupapalvelu/job.clj
+++ src/lupapalvelu/job.clj
@@ -1,67 +1,74 @@
 (ns lupapalvelu.job
   (:refer-clojure :exclude [update])
-  (:require [slingshot.slingshot :refer [throw+]]))
-
-(defonce jobs (ref {}))
-(defonce job-id (atom 0))
-(defonce deliverer (agent nil))
-
-(defn- next-job-id []
-  (str (swap! job-id inc)))
-
-(defn- delivered [value]
-  (doto (promise)
-    (deliver value)))
+  (:require [slingshot.slingshot :refer [throw+]]
+            [lupapalvelu.mongo :as mongo]
+            [monger.operators :refer :all]
+            [sade.core :refer [now]])
+  (:import [org.bson.types ObjectId]
+           [java.util Date]))
 
 (defn check-status [data]
   (if (every? #{:done :error} (map #(get-in % [:status]) (vals data)))
     :done
     :running))
 
-(defn- create-job [id initial-value]
-  {:id id
+(defn- create-job [initial-value]
+  {:id      (ObjectId.)
    :version 0
-   :value initial-value
-   :status (check-status initial-value)
-   :next (promise)})
+   :value   initial-value
+   :status  (check-status initial-value)
+   :created (Date.)})
 
-(defn- find-job [id]
-  (or (get @jobs id) (throw+ {:error :not-found :message (str "unknown job: id=" id)})))
+(defn- stringify-keys [{:keys [value] :as job}]
+  (when job
+    (->> value
+         (map (fn [[k v]] [(name k) v]))
+         (into {})
+         (assoc job :value))))
+
+(defn- find-job [^String id]
+  (or (-> (mongo/by-id :jobs (ObjectId. id))
+          stringify-keys)
+      (throw+ {:error :not-found :message (str "unknown job: id=" id)})))
 
 (defn- trim [job]
   (when job
-    (select-keys job [:id :version :status :value])))
+    (-> (select-keys job [:id :version :status :value])
+        (update-in [:id] str))))
+
+(defn- store-in-db [job]
+  (mongo/insert :jobs job))
 
 (defn start [initial-value]
-  (let [id (next-job-id)
-        job (create-job id initial-value)]
-    (dosync
-      (alter jobs assoc id job))
+  (let [job (create-job initial-value)]
+    (store-in-db job)
     (trim job)))
 
-(defn- job-changed [_ old-job new-job]
-  (deliver (:next old-job) (trim new-job)))
-
 (defn update [id f & args]
-  (dosync
-    (let [old-job (find-job id)
-          new-value (apply f (:value old-job) args)
-          new-job (assoc old-job :version (inc (:version old-job))
-                         :value new-value
-                         :status (check-status new-value)
-                         :next (promise))]
-      (alter jobs assoc id new-job)
-      (send deliverer job-changed old-job new-job)
-      (:version new-job))))
+  (let [old-job (find-job id)
+        new-value (apply f (:value old-job) args)
+        new-job (assoc old-job :version (inc (:version old-job))
+                               :value new-value
+                               :status (check-status new-value))]
+    (mongo/update-by-id :jobs (:id old-job) {$set (dissoc new-job :id)})
+    (:version new-job)))
 
-(defn- get-update-promise [id version]
-  (let [job (find-job id)]
-    (if (= version (:version job))
-      (:next job)
-      (delivered job))))
+(def query-spacing 800)
+
+(defn- wait-for-job-update [^String id version timeout start-ts]
+  (if-let [updated (->> (mongo/find-maps :jobs {:_id (ObjectId. id)
+                                                :version {$gt version}})
+                        (map mongo/with-id)
+                        first)]
+    (trim updated)
+    (when (< (- (now) start-ts) timeout)
+      (Thread/sleep query-spacing)
+      (wait-for-job-update id version timeout start-ts))))
 
 (defn status [id version timeout]
-  (let [v (deref (get-update-promise id version) timeout :timeout)]
-    (if (= v :timeout)
-      {:result :timeout}
-      {:result :update :job (trim v)})))
+  (let [job (find-job id)]
+    (if (<= version (:version job))
+      {:result :update :job (trim job)}
+      (if-let [job (wait-for-job-update id version timeout (now))]
+        {:result :update :job job}
+        {:result :timeout}))))
