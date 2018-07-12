55926c4090932a30ab3782b8cef7ba11ca845770
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-07-12 17:25:12 +0300

    LPK-3724 Parse the SAML response

--- src/lupapalvelu/ident/ad_login.clj
+++ src/lupapalvelu/ident/ad_login.clj
@@ -1,5 +1,7 @@
 (ns lupapalvelu.ident.ad-login
-  (:require [taoensso.timbre :as timbre :refer [trace debug info warn error errorf fatal]]
+  (:require [clojure.data.xml :as xml]
+            [taoensso.timbre :as timbre :refer [trace debug info warn error errorf fatal]]
+            [sade.xml :as sxml]
             [noir.core :refer [defpage]]
             [noir.response :as response]
             [noir.request :as request]
@@ -41,11 +43,31 @@
   {:app-name "Lupapiste"
    :base-uri "http://localhost:8000"
    :idp-uri  "https://localhost:7000" ;; The dockerized, locally running mock-saml instance (https://github.com/lupapiste/mock-saml)
-   :idp-cert (parse-certificate (slurp "./idp-public-cert.pem")) ;; This needs to live in Mongo
+   :idp-cert (parse-certificate (slurp "./idp-public-cert.pem")) ;; This needs to live in Mongo's organizations collection
    :keystore-file "./keystore"
    :keystore-password (System/getenv "KEYSTORE_PASS") ;; The default password from the dev guide, needs to be set in environment variable
    :key-alias "jetty"
    })
+
+(defn xml-tree->edn
+  "Takes an xml tree returned by the clojure.data.xml parser, recursively parses it into as readable and flat Clojure map
+  as is feasible."
+  [element]
+  (cond
+    (nil? element) nil
+    (string? element) element
+    (and (= 1 (count element)) (string? (first element))) (first element)
+    (and (map? element) (= :AttributeValue (:tag element))) (xml-tree->edn (first (:content element)))
+    (and (map? element) (empty? element)) {}
+    (and (= 1 (count element)) (sequential? element)) (xml-tree->edn (first element))
+    (sequential? element) (mapv xml-tree->edn element)
+    (and (map? element) (= :Attribute (:tag element))) {(keyword (:Name (:attrs element))) (xml-tree->edn (:content element))}
+    (map? element) {(:tag element) (xml-tree->edn (:content element))}
+    :else nil))
+
+(defn parse-saml-resp [xml-string]
+  (let [xmltree (-> xml-string xml/parse-str)]
+    xmltree))
 
 (def decrypter
   (saml-sp/make-saml-decrypter (:keystore-file config)
@@ -66,24 +88,39 @@
                                           (:key-alias config)
                                           :algorithm :sha256)))
 
+(def resp (atom {}))
 
-(defpage "/api/saml/ad-login" []
+(defpage [:post "/api/saml/ad-login"] {params :params session :session}
   (let [sessionid (ident-util/session-id)
         trid      (security/random-password)
         paths     {:success "/from-ad/:trid"
                    :error "/api/saml/ad/error"
                    :cancel "/"}
-        login-path "http://localhost:7000"
-        req (request/ring-request)]
+        req (request/ring-request)
+        xml-response (saml-shared/base64->inflate->str (get-in req [:params :SAMLResponse]))
+        _ (swap! resp assoc :xml-resp xml-response)
+        _ (swap! resp assoc :req req)
+        relay-state (get-in req [:params :RelayState])
+        [valid-relay-state? continue-url] (saml-routes/valid-hmac-relay-state? (:secret-key-spec mutables) relay-state)
+        ;; saml-resp (saml-sp/xml-string->saml-resp xml-response) ;; Why in the world does this not work here when it works with the SAME INPUT in saml-test?!
+        ;; Should return openSaml Java object but throws NullPointerException.
+        idp-cert (:idp-cert config)
+        ; valid-signature? (if idp-cert
+        ;                    (saml-sp/validate-saml-response-signature saml-resp idp-cert)
+        ;                    false)
+        ]
     (do
       (println "Joo-o")
-      (println sessionid)
-      (println trid)
       (println "Ja pyyntö seuraa: ")
-      ; (util/pspit req "esimerkkipyyntö.edn")
-      (println req))
+      (println req)
+      (println xml-response)
+      (println relay-state)
+      (println valid-relay-state?)
+      (println continue-url)
+      ; (println saml-resp)
+      ; (println valid-signature?)
     ; (mongo/update :vetuma {:sessionid sessionid :trid trid} {:sessionid sessionid :trid trid :paths paths :created-at (Date.)} :upsert true)
-    (response/redirect (str "/from-ad/" trid))))
+    (response/redirect (str "/from-ad/anders.autologin")))))
 
 (defpage [:post "/from-ad/:trid"] {:keys [trid] :as params}
   (let [valid? params
