214dad2e5f6f5ebf188079dc47fae6b35e84c6ad
Ville Outamaa <ville.outamaa@solita.fi>
2018-07-30 16:38:43 +0300

    Revert "LPK-3853 Read-only attachments cannot be stamped."
    This reverts commit 13f23ded27972e21c1246bd951aa3f887809a395.

--- src/lupapalvelu/attachment_api.clj
+++ src/lupapalvelu/attachment_api.clj
@@ -117,14 +117,6 @@
                    attachments))
     (fail :error.attachment-is-locked)))
 
-(defn- no-attachment-is-read-only [{{attachments :attachments} :application {:keys [files]} :data}]
-  (when (and (seq files)
-             (some (fn [{:keys [id readOnly]}]
-                     (and (contains? (set files) id)
-                          readOnly))
-                   attachments))
-    (fail :error.attachment-is-read-only)))
-
 ;;
 ;; Attachments
 ;;
@@ -649,8 +641,7 @@
                       (partial action/parameters-matching-schema [:stamp] JSONStampSchema)
                       (partial action/supported-lang :lang)]
    :pre-checks       [any-attachment-has-version
-                      no-attachment-is-archived
-                      no-attachment-is-read-only]
+                      no-attachment-is-archived]
    :user-roles       #{:authority}
    :states           states/post-submitted-states
    :description      "Stamps all attachments of given application"}
