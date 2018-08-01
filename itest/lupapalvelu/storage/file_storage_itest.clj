1af8f5e2255b8ff5d4ec4d8a1dcf7840e0a5f1a9
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2018-08-01 10:10:08 +0300

    Fix user-attachment-exists? tests by making them use storage-system like the other fs tests.

--- itest/lupapalvelu/storage/file_storage_itest.clj
+++ itest/lupapalvelu/storage/file_storage_itest.clj
@@ -103,8 +103,8 @@
                 (verify-content dl)))
 
             (fact "user files' existence can be checked"
-              (fs/user-attachment-exists? user-id id) => true
-              (fs/user-attachment-exists? user-id (str (uuid/v1))) => false)
+              (fs/user-attachment-exists? user-id id storage-system) => true
+              (fs/user-attachment-exists? user-id (str (uuid/v1)) storage-system) => false)
 
             (fact "user file can be deleted"
               (fs/delete-user-attachment user-id id storage-system) => nil
