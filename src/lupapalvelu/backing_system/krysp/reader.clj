e542373a13a5637b5d7033d591f59709fce5b420
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-08-20 16:51:59 +0300

    PATE-113 Remove unnecessary comp from reader/->viitelupatunnukset

--- src/lupapalvelu/backing_system/krysp/reader.clj
+++ src/lupapalvelu/backing_system/krysp/reader.clj
@@ -671,7 +671,7 @@
   "Takes a parsed XML document, returns a list of viitelupatunnus -ids (in 'permit-id'-format) found therein."
   [xml]
   (->> (select xml [:rakennusvalvontaAsiatieto :viitelupatieto])
-       (map (comp #(get-in % [:LupaTunnus :kuntalupatunnus]) cr/all-of))))
+       (map #(get-in % [:LupaTunnus :kuntalupatunnus]) cr/all-of)))
 
 (defn is-foreman-application? [xml]
   (let [permit-type (-> xml ->kuntalupatunnus (ss/split #"-") last)]
