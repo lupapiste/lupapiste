c1f928e6e1d6be7226629440301c683a8dc3018c
Antti Salmivaara <antti.salmivaara@evolta.fi>
2018-08-20 12:30:26 +0300

    LPK-1368 Add error and foreman handling

--- src/lupapalvelu/application_api.clj
+++ src/lupapalvelu/application_api.clj
@@ -642,14 +642,15 @@
         krysp?             (org/krysp-integration? organization (permit/permit-type application))
         warranty?          (and (permit/is-ya-permit (permit/permit-type application)) (util/=as-kw state :closed) (not krysp?))
         terminal-but-not-canceled? (and (states/terminal-state? (sm/state-graph application) (keyword state))
-                                        (not= :canceled (keyword state)))]
+                                        (not= :canceled (keyword state)))
+        foremanVerdictGiven? (-> state (keyword) (= :foremanVerdictGiven))]
+    (when (or terminal-but-not-canceled? foremanVerdictGiven?)
+      (attachment/save-comments-as-attachment command {:state state}))
     (if warranty?
       (update-application command (util/deep-merge
                                     (app-state/state-transition-update (keyword state) (:created command) application user)
                                     {$set (app/warranty-period (:created command))}))
       (update-application command (app-state/state-transition-update (keyword state) (:created command) application user)))
-    (when terminal-but-not-canceled?
-      (attachment/save-comments-as-attachment command))
     (when-not archiving-project?
       (archiving-util/mark-application-archived-if-done application (:created command) user))))
 
--- src/lupapalvelu/attachment.clj
+++ src/lupapalvelu/attachment.clj
@@ -1032,20 +1032,22 @@
        (remove #(-> % :target :type (keyword) (= :attachment)))
        (empty?)))
 
-(defn save-comments-as-attachment [{lang :lang application :application created :created :as command}]
+(defn save-comments-as-attachment [{lang :lang application :application created :created :as command} & [{state :state}]]
   (when-not (comments-empty? application)
-    (let [comments-pdf (comment/get-comments-as-pdf lang application)
-          content (:pdf-file-stream comments-pdf)
-          existing-keskustelu (util/find-by-key :type {:type-id "keskustelu" :type-group "muut"} (:attachments application))
-          file-options {:filename (format "%s-%s.pdf" (:id application) (i18n/localize lang :conversation.title))
-                        :content  content
-                        :size     (.available content)}
-          attachment-options {:attachment-type {:type-id    :keskustelu
-                                                :type-group :muut}
-                              :attachment-id   (when existing-keskustelu (:id existing-keskustelu))
-                              :created         created
-                              :required        false}]
-      (upload-and-attach! command attachment-options file-options))))
+    (let [comments-pdf (comment/get-comments-as-pdf lang (if state (assoc application :state state) application))]
+      (if (:ok comments-pdf)
+        (let [content (:pdf-file-stream comments-pdf)
+              existing-keskustelu (util/find-by-key :type {:type-id "keskustelu" :type-group "muut"} (:attachments application))
+              file-options {:filename (format "%s-%s.pdf" (:id application) (i18n/localize lang :conversation.title))
+                            :content  content
+                            :size     (.available content)}
+              attachment-options {:attachment-type {:type-id    :keskustelu
+                                                    :type-group :muut}
+                                  :attachment-id   (when existing-keskustelu (:id existing-keskustelu))
+                                  :created         created
+                                  :required        false}]
+          (upload-and-attach! command attachment-options file-options))
+        (fail! :error.discussion-pdf-generation-failed)))))
 
 ;;
 ;; Pre-checks
