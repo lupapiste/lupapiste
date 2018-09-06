40e165daf2317cdefe91ccac17396bd1ce7fbfae
Markus Penttila <markus.penttila@evolta.fi>
2018-09-06 22:54:58 +0300

    Update chrome version check

--- robot/roboto.sh
+++ robot/roboto.sh
@@ -289,7 +289,7 @@ check_env() {
          ;;
       "chrome" | "headlesschrome" )
          CG=$(google-chrome --version)
-         echo "$CG" | grep -q "^Google Chrome \(6[6-8]\)\." || versionfail "Major version '$CG' of Chrome may not work yet. Update ${BASH_SOURCE}:${LINENO} if this is fine."
+         echo "$CG" | grep -q "^Google Chrome \(6[7-9]\)\." || versionfail "Major version '$CG' of Chrome may not work yet. Update ${BASH_SOURCE}:${LINENO} if this is fine."
          # Clean up old Chrome temp files if there
          rm -rf /tmp/.com.google.Chrome.*
          rm -rf /tmp/.org.chromium*
