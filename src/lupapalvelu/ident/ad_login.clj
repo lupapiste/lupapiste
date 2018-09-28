b89c0826e37c4a0ee2eac1911ac49b2501856708
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-09-28 10:28:52 +0300

    LPK-3724 Make the SAML SSO flow stateless

--- src/lupapalvelu/ident/ad_login.clj
+++ src/lupapalvelu/ident/ad_login.clj
@@ -1,5 +1,5 @@
 (ns lupapalvelu.ident.ad-login
-  (:require [taoensso.timbre :refer [trace debug info warn error errorf fatal]]
+  (:require [taoensso.timbre :refer [trace debug info warn error fatal]]
             [noir.core :refer [defpage]]
             [noir.response :as response]
             [noir.request :as request]
@@ -33,40 +33,6 @@
     (map? element) (into {} (for [[k v] element] [(keyword k) (parse-saml-info v)]))
     :else element))
 
-(def ad-config
-  (let [c (env/get-config)
-        {:keys [keystore key-password]} (:ssl c)
-        key-alias "jetty"]
-    (atom
-      {:app-name "Lupapiste"
-       :base-uri (:host c)
-       :keystore-file keystore
-       :keystore-password key-password
-       :key-alias key-alias
-       :mutables (assoc (saml-sp/generate-mutables)
-                        :xml-signer (saml-sp/make-saml-signer keystore
-                                                              key-password
-                                                              key-alias
-                                                              :algorithm :sha256))
-       :sp-cert (saml-shared/get-certificate-b64 keystore key-password key-alias)
-       :decrypter (saml-sp/make-saml-decrypter keystore key-password key-alias)
-       :organizational-settings {}})))
-
-(defn add-organization-data-to-config! [orgid]
-  (if-let [ad-login (:ad-login (get-organization orgid))]
-    (let [{:keys [enabled idp-cert idp-uri trusted-domains]} ad-login
-          acs-uri (str (:host (env/get-config)) "/api/saml/ad-login/" orgid)]
-      (swap! ad-config assoc-in [:organizational-settings (keyword orgid)] {:enabled enabled
-                                                                            :idp-cert (parse-certificate idp-cert)
-                                                                            :idp-uri idp-uri
-                                                                            :trusted-domains trusted-domains
-                                                                            :saml-req-factory! (saml-sp/create-request-factory
-                                                                                                 (:mutables @ad-config)
-                                                                                                 idp-uri
-                                                                                                 saml-routes/saml-format
-                                                                                                 "Lupapiste"
-                                                                                                 acs-uri)}))))
-
 (defn resolve-roles
   "Takes a seq of user roles from the SAML, returns a set of corresponding LP roles."
   [org-roles ad-params]
@@ -95,25 +61,54 @@
                    {:user (usr/session-summary user)})]
     response))
 
+(defn make-ad-config [orgid]
+  (let [c (env/get-config)
+        {:keys [keystore key-password]} (:ssl c)
+        key-alias "jetty"
+        ad-login (-> orgid get-organization :ad-login)
+        {:keys [enabled idp-cert idp-uri trusted-domains]} ad-login
+        acs-uri (str (:host (env/get-config)) "/api/saml/ad-login/" orgid)
+        mutables (assoc (saml-sp/generate-mutables)
+                        :xml-signer (saml-sp/make-saml-signer keystore
+                                                              key-password
+                                                              key-alias
+                                                              :algorithm :sha256))]
+    {:app-name "Lupapiste"
+     :base-uri (:host c)
+     :keystore-file keystore
+     :keystore-password key-password
+     :key-alias key-alias
+     :mutables mutables
+     :sp-cert (saml-shared/get-certificate-b64 keystore key-password key-alias)
+     :decrypter (saml-sp/make-saml-decrypter keystore key-password key-alias)
+     :organizational-settings {:enabled enabled
+                               :idp-cert (parse-certificate idp-cert)
+                               :idp-uri idp-uri
+                               :trusted-domains trusted-domains
+                               :saml-req-factory! (saml-sp/create-request-factory
+                                                    mutables
+                                                    idp-uri
+                                                    saml-routes/saml-format
+                                                    "Lupapiste"
+                                                    acs-uri)}}))
+
 (defpage [:get "/api/saml/ad-login/:orgid"] {orgid :orgid}
-  (let [org-data (get-in (add-organization-data-to-config! orgid) [:organizational-settings (keyword orgid)])
-        saml-request ((:saml-req-factory! org-data))
-        hmac-relay-state (saml-routes/create-hmac-relay-state (:secret-key-spec (:mutables @ad-config)) "target")
-        req (request/ring-request)]
-      (saml-sp/get-idp-redirect (:idp-uri org-data)
+  (let [ad-config (make-ad-config orgid)
+        saml-req-factory! (get-in ad-config [:organizational-settings :saml-req-factory!])
+        saml-request (saml-req-factory!)
+        ; Even though we don't use state in our SAML login, a relay state token is created since 'saml-sp/get-idp-redirect' expects it.
+        ; It's just not validated when receiving the response.
+        hmac-relay-state (saml-routes/create-hmac-relay-state (:secret-key-spec (:mutables ad-config)) "target")]
+      (saml-sp/get-idp-redirect (get-in ad-config [:organizational-settings :idp-uri])
                                 saml-request
                                 hmac-relay-state)))
 
 (defpage [:post "/api/saml/ad-login/:orgid"] {orgid :orgid}
-  (let [req (request/ring-request)
+  (let [ad-config (make-ad-config orgid)
+        req (request/ring-request)
         xml-response (saml-shared/base64->inflate->str (get-in req [:params :SAMLResponse]))
-        relay-state (get-in req [:params :RelayState])
-        [valid-relay-state? continue-url] (saml-routes/valid-hmac-relay-state? (:secret-key-spec (:mutables @ad-config)) relay-state)
         saml-resp (saml-sp/xml-string->saml-resp xml-response)
-        idp-cert (get-in @ad-config [:organizational-settings (keyword orgid) :idp-cert])
-        _ (println saml-resp)
-        _ (println "IDP-CERT: " idp-cert)
-        _ (println "VALIDOIDAAN SERTTI: " (saml-sp/validate-saml-response-signature saml-resp idp-cert))
+        idp-cert (get-in ad-config [:organizational-settings :idp-cert])
         valid-signature? (if-not idp-cert
                            false
                            (try
@@ -122,25 +117,18 @@
                                (do
                                  (error (.getMessage e))
                                  false))))
-        valid? (and valid-relay-state? valid-signature?)
-        _ (info (str "HMAC relay state was " (if valid-relay-state? "valid" "invalid")))
         _ (info (str "SAML message signature was " (if valid-signature? "valid" "invalid")))
-        _ (info (str "SAML response validation " (if valid? "was successful" "failed")))
-        saml-info (when valid? (saml-sp/saml-resp->assertions saml-resp (:decrypter @ad-config)))
+        saml-info (when valid-signature? (saml-sp/saml-resp->assertions saml-resp (:decrypter ad-config)))
         parsed-saml-info (parse-saml-info saml-info)
         {:keys [email firstName lastName groups]} (get-in parsed-saml-info [:assertions :attrs])
         _ (clojure.pprint/pprint parsed-saml-info)
         ad-role-map (-> orgid (get-organization) :ad-login :role-mapping)
-        authz (resolve-roles ad-role-map groups) ;; Disable this check for now
-        _ (println valid-signature?)]  ;; groups or whatever the correct parameter is)
+        authz (resolve-roles ad-role-map groups)]
     (cond
-      (and valid? (seq authz)) (validated-login req orgid firstName lastName email authz)
-      valid? (do
-               (error "User does not have organization authorization")
-               (response/redirect (format "%s/app/fi/welcome#!/login" (:host (env/get-config)))))
-      (false? valid-signature?) (do
-                                  (error "Certificate was invalid")
-                                  (response/status 403 (response/content-type "text/plain" "Certificate was invalid")))
+      (and valid-signature? (seq authz)) (validated-login req orgid firstName lastName email authz)
+      valid-signature? (do
+                         (error "User does not have organization authorization")
+                         (response/redirect (format "%s/app/fi/welcome#!/login" (:host (env/get-config)))))
       :else (do
               (error "SAML validation failed")
               (response/status 403 (response/content-type "text/plain" "Validation of SAML response failed"))))))
