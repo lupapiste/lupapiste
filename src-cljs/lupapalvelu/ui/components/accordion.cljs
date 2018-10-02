70681e08723dbf7620fecf904af7955ffaa9d638
Juha Heimonen <juha.heimonen@flowa.fi>
2018-10-02 14:12:33 +0300

    Fix accordion button logic
    do'h, it's an atom, frank.

--- src-cljs/lupapalvelu/ui/components/accordion.cljs
+++ src-cljs/lupapalvelu/ui/components/accordion.cljs
@@ -3,10 +3,10 @@
 
 
 (rum/defc caret-accordion-toggle-up []
-  [:button "foo"])
+  [:button [:i.lupicon-chevron-up {:style {:color "#ffffff"}}]])
 
 (rum/defc caret-accordion-toggle-down []
-  [:button "foo"])
+  [:button [:i.lupicon-chevron-down {:style {:color "#ffffff"}}]])
 
 (def caret-toggle {:caret-up caret-accordion-toggle-up
                    :caret-down caret-accordion-toggle-down})
@@ -21,7 +21,7 @@
      [:div.roll-up-header
       (if accordion-toggle-component [:div.accordion-toggle-container
                                       {:on-click #(reset! is-open? (not @is-open?))}
-                                      (if is-open?
+                                      (if @is-open?
                                         ((:caret-up accordion-toggle-component))
                                         ((:caret-down accordion-toggle-component)))])]
      [:div.rollup-accordion-content.rollup__open accordion-content-component]]))
