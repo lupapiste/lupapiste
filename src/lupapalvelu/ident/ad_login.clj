8d71096a84134c91b1867614371497858d444576
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-10-15 13:59:00 +0300

    LPK-3724 Read certificate and key from env vars instead of keystore

--- src/lupapalvelu/ident/ad_login.clj
+++ src/lupapalvelu/ident/ad_login.clj
@@ -4,6 +4,7 @@
             [noir.core :refer [defpage]]
             [noir.response :as resp]
             [noir.request :as request]
+            [lupapalvelu.ident.ad-login-util :as ad-util]
             [lupapalvelu.organization :as org]
             [lupapalvelu.user :as usr]
             [monger.operators :refer [$set]]
@@ -15,40 +16,20 @@
             [sade.util :as util]
             [saml20-clj.sp :as saml-sp]
             [saml20-clj.routes :as saml-routes]
-            [saml20-clj.shared :as saml-shared]))
+            [saml20-clj.shared :as saml-shared]
+            [saml20-clj.xml :as saml-xml])
+  (:import [org.apache.xml.security Init]
+           [org.apache.xml.security.utils Constants ElementProxy]
+           [org.apache.xml.security.transforms Transforms]
+           [org.apache.xml.security.c14n Canonicalizer]
+           [java.io ByteArrayInputStream]
+           [java.util Base64]))
 
-(defn- parse-certificate
-  "Strip the -----BEGIN CERTIFICATE----- and -----END CERTIFICATE----- headers and newlines
-  from certificate."
-  [certstring]
-  (ss/replace certstring #"[\n ]|(BEGIN|END) CERTIFICATE|-{5}" ""))
-
-(defn remove-namespaces-from-kws
-  "Convert namespaced map keys like :http://schemas.microsoft.com/identity/claims/tenantid -> :tenantid"
-  [m]
-  (into {} (for [[k v] m]
-             (let [newkey (-> k name (ss/split #"/") last keyword)]
-               [newkey v]))))
-
-(defn parse-saml-info
-  "The saml-info map returned by saml20-clj comes in a wacky format, so its best to
-  parse it into a more manageable form (without string keys or single-element lists etc)."
-  [element]
-  (cond
-    (and (seq? element) (= (count element) 1)) (parse-saml-info (first element))
-    (seq? element) (mapv parse-saml-info element)
-    (map? element) (into {} (for [[k v] (remove-namespaces-from-kws element)] [(keyword k) (parse-saml-info v)]))
-    :else element))
-
-(defn resolve-roles
-  "Takes a map of corresponding roles (key = role in Lupis, value is AD-group)
-  and a seq of user roles from the SAML, returns a set of corresponding LP roles."
-  [org-roles ad-params]
-  (let [ad-roles-set (if (string? ad-params) #{ad-params} (set ad-params))
-        orgAuthz     (for [[lp-role ad-role] org-roles]
-                       (when (ad-roles-set ad-role)
-                         (name lp-role)))]
-    (->> orgAuthz (remove nil?) set)))
+(def ad-config
+  {:sp-cert (ad-util/parse-certificate (slurp "./idp-public-cert.pem"))
+   :private-key (ad-util/parse-certificate (slurp "./idp-private-key.pem"))
+   :acs-uri (format "%s/api/saml/ad-login/%s" (env/value :host) "609-R")
+   :app-name "Lupapiste"})
 
 (defn validated-login [req org-id firstName lastName email orgAuthz]
   (let [user-data {:firstName firstName
@@ -69,13 +50,18 @@
                    {:user (usr/session-summary user)})]
     response))
 
+(defpage [:get "/api/saml/metadata"] []
+  (let [{:keys [app-name sp-cert acs-uri]} ad-config]
+    (resp/status 200 (saml-sp/metadata app-name acs-uri sp-cert))))
+
 (defpage [:get "/api/saml/ad-login/:org-id"] {org-id :org-id}
   (let [{:keys [enabled idp-uri]} (-> org-id org/get-organization :ad-login)
+        {:keys [sp-cert private-key]} ad-config
         acs-uri (format "%s/api/saml/ad-login/%s" (env/value :host) org-id)
         saml-req-factory! (saml-sp/create-request-factory
                             #(str "_" (uuid/v1))
                             (constantly 0) ; :saml-id-timeouts
-                            false
+                            (ad-util/make-saml-signer sp-cert private-key)
                             idp-uri
                             saml-routes/saml-format
                             "Lupapiste"
@@ -89,16 +75,17 @@
         (infof "Complete request string: %s" full-querystring)
         (saml-sp/get-idp-redirect idp-uri saml-request relay-state))
       (do
-        (errorf "Organization %s does not have valid AD-login settings or has disabled AD-login" org-id)
+        (errorf "Organization %s does not have valid AD-login settings or has AD-login disabled" org-id)
         (resp/redirect (format "%s/app/fi/welcome#!/login" (env/value :host)))))))
 
 (defpage [:post "/api/saml/ad-login/:org-id"] {org-id :org-id}
-  (let [idp-cert (some-> org-id org/get-organization :ad-login :idp-cert parse-certificate)
+  (let [idp-cert (some-> org-id org/get-organization :ad-login :idp-cert ad-util/parse-certificate)
         req (request/ring-request)
+        decrypter (ad-util/make-saml-decrypter (:private-key ad-config))
         xml-response (saml-shared/base64->inflate->str (get-in req [:params :SAMLResponse])) ; The raw XML string
         saml-resp (saml-sp/xml-string->saml-resp xml-response) ; An OpenSAML object
-        saml-info (saml-sp/saml-resp->assertions saml-resp false) ; The response as a Clojure map
-        parsed-saml-info (parse-saml-info saml-info) ; The response as a "normal" Clojure map
+        saml-info (saml-sp/saml-resp->assertions saml-resp decrypter) ; The response as a Clojure map
+        parsed-saml-info (ad-util/parse-saml-info saml-info) ; The response as a "normal" Clojure map
         _ (infof "Received XML response for organization %s: %s" org-id xml-response)
         _ (infof "SAML response for organization %s: %s" org-id saml-info)
         _ (infof "Parsed SAML response for organization %s: %s" org-id parsed-saml-info)
@@ -117,7 +104,7 @@
         _ (infof "firstName: %s, lastName: %s, groups: %s, email: %s" firstName, lastName groups email)
         ad-role-map (-> org-id org/get-organization :ad-login :role-mapping)
         _ (infof "AD-role map: %s" ad-role-map)
-        authz (resolve-roles ad-role-map groups)
+        authz (ad-util/resolve-roles ad-role-map groups)
         _ (infof "Resolved authz: %s" authz)]
     (cond
       (and valid-signature? (seq authz)) (validated-login req org-id firstName lastName email authz)
