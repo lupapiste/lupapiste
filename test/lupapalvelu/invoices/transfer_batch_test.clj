baab7f39d8f60374473b761b3a37a7167d564ab8
Vespe Savikko <vespe.savikko@evolta.fi>
2018-12-07 17:32:26 +0200

    LPK-4007 Update transfer batch tests.
    Invoice creator email is no longer stored.

--- test/lupapalvelu/invoices/transfer_batch_test.clj
+++ test/lupapalvelu/invoices/transfer_batch_test.clj
@@ -4,26 +4,26 @@
             [sade.core]))
 
 (facts "->invoice-transfer-batch-db"
-       (let [user-data {:foo       "some-value"
-                        :id        "penan-id"
-                        :firstName "pena"
-                        :lastName  "panaani"
-                        :role      "authority"
-                        :email     "pena@panaani.fi"
-                        :username  "pena"}
-             org-id "foo-org-id"
-             transfer-batch-data {}
-             test-timestamp 123456]
+  (let [user-data           {:foo       "some-value"
+                             :id        "penan-id"
+                             :firstName "pena"
+                             :lastName  "panaani"
+                             :role      "authority"
+                             :email     "pena@panaani.fi"
+                             :username  "pena"}
+        org-id              "foo-org-id"
+        transfer-batch-data {}
+        test-timestamp      123456]
          (fact "returns proper invoice-transfer-batch-ready-to-be-stored-to-database"
-               (with-redefs [sade.core/now (fn [] test-timestamp)]
-                 (->invoice-transfer-batch-db transfer-batch-data org-id user-data)) => {:organization-id org-id
-                                                                        :created test-timestamp
-                                                                        :created-by {:id        "penan-id"
-                                                                                         :firstName "pena"
-                                                                                         :lastName  "panaani"
-                                                                                         :role      "authority"
-                                                                                         :email     "pena@panaani.fi"
-                                                                                     :username  "pena"}
-                                                                        :invoices []
-                                                                                         :number-of-rows 0
-                                                                                         :sum {:currency "" :major 0 :minor 0 :text ""}})))
+           (with-redefs [sade.core/now (fn [] test-timestamp)]
+             (->invoice-transfer-batch-db transfer-batch-data org-id user-data))
+           => {:organization-id org-id
+               :created         test-timestamp
+               :created-by      {:id        "penan-id"
+                                 :firstName "pena"
+                                 :lastName  "panaani"
+                                 :role      "authority"
+                                 :username  "pena"}
+               :invoices        []
+               :number-of-rows  0
+               :sum             {:currency "" :major 0 :minor 0 :text ""}})))
