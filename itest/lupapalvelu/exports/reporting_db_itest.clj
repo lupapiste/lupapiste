729837e3d5a27583a4d67f7b5a6b9144c6e9af4f
Ville Outamaa <ville.outamaa@solita.fi>
2019-01-03 08:39:40 +0200

    LPK-4036 Improve itest legibility

--- itest/lupapalvelu/exports/reporting_db_itest.clj
+++ itest/lupapalvelu/exports/reporting_db_itest.clj
@@ -19,20 +19,25 @@
     (:applications (export-applications 0 (now))) => empty?)
   (let [timestamp-before-application-is-created (now)
         application-id (create-app-id pena :operation "markatilan-laajentaminen")]
+
     (fact "One app created, but is not exported because the modified timestamp is not within the given time interval"
       (:applications (export-applications 0 timestamp-before-application-is-created))
       => empty?)
+
     (fact "The created app is exported if the modified timestamp is within the given time interval"
       (:applications (export-applications timestamp-before-application-is-created (now)))
       => (just [(contains {:id application-id})])))
+
   (let [timestamp-before-applications-are-created (now)
         first-application-id (create-app-id pena :operation "aita")
         timestamp-between-applications (now)
         second-application-id (create-app-id pena :operation "kerrostalo-rivitalo")]
+
     (fact "Two apps created, but only one is exported if the timestamp of the second one is not within the time interval"
           (:applications (export-applications timestamp-before-applications-are-created
                                               timestamp-between-applications))
           => (just [(contains {:id first-application-id})]))
+
     (fact "Both apps are exported if their timestamps are contained within the time interval"
           (:applications (export-applications timestamp-before-applications-are-created (now)))
           => (just [(contains {:id first-application-id})
